20eed64b8576f71559e75315b872901891f7b406
Translation Server <translation@plainblack.com>
2007-01-07 18:31:34 +0000

    update from trans

--- RedNeck/RedNeck/AdminConsole.pm
+++ RedNeck/RedNeck/AdminConsole.pm
@@ -1,6 +1,21 @@
 package WebGUI::i18n::RedNeck::AdminConsole;
 
 our $I18N = {
+	'toggle off' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'toggle on' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'admin console' => {
+		message => q|x|,
+		lastUpdated => 1168194688
+	},
+
 };
 
 1;
