67bafe2f13dfe427e5b1ae7fe260aba7dabeb235
Translation Server <translation@plainblack.com>
2007-01-12 00:30:34 +0000

    update_from_translation_server

--- Dutch/Dutch/AdminConsole.pm
+++ Dutch/Dutch/AdminConsole.pm
@@ -1,6 +1,21 @@
 package WebGUI::i18n::Dutch::AdminConsole;
 
 our $I18N = {
+	'toggle off' => {
+		message => q|Verberg beheer console<br />|,
+		lastUpdated => 1168560976
+	},
+
+	'toggle on' => {
+		message => q|Vertoon beheer console<br />|,
+		lastUpdated => 1168560995
+	},
+
+	'admin console' => {
+		message => q|Beheer console<br />|,
+		lastUpdated => 1168560955
+	},
+
 };
 
 1;
--- Dutch/Dutch/Asset_SyndicatedContent.pm
+++ Dutch/Dutch/Asset_SyndicatedContent.pm
@@ -1,6 +1,251 @@
 package WebGUI::i18n::Dutch::Asset_SyndicatedContent;
 
 our $I18N = {
+	'displayMode' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'rssUrl' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'rss.url.2.0' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'rss.url.1.0' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'72 description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'71' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'rss.url.0.91' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'assetName' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'site_link' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'syndicated content asset template variables body' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'displayModeLabel' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'channel.link' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'syndicated content asset template variables title' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'1' => {
+		message => q|URL voor RSS bestand<br />|,
+		lastUpdated => 1168561036
+	},
+
+	'1 description' => {
+		message => q|<p>Verstrek de exacte URL ( begin met http://) van het syndicated content RDF of RSS bestand. De syndicated content wordt &eacute;&eacute;n keer peer uur geladen vanaf deze URL.</p><p>U kunt syndicated content vinden op de volgende adressen:</p><div> <ul><li><a href="http://www.newsisfree.com/">http://www.newsisfree.com</a></li><li><a href="http://www.syndic8.com/">http://www.syndic8.com</a></li><li><a href="http://www.voidstar.com/node.php?id=144">http://www.voidstar.com/node.php?id=144</a></li><li><a href="http://my.userland.com/">http://my.userland.com</a></li><li><a href="http://www.webreference.com/services/news/">http://www.webreference.com/services/news/</a></li><li><a href="http://w.moreover.com/">http://w.moreover.com/</a></li></ul> </div><p>&nbsp;</p><p>Op dit moment kan WebGUI omgaan met RSS versies .90, .91, 1.0, and 2.0.&nbsp; Atom feeds worden op dit moment nog niet ondersteund. Waarschijnlijk voldoen andere RSS-achtige bronnen ook.</p><p>Om een samengestelde RSS bron te creeren ( een RSS die informatie haalt uit meerdere RSS bronnen), voeg dan een lijst van URLs in, &eacute;&eacute;n per regel, in plaat van een enkele URL. Onderdelen worden gesorteerd in de volgorde waarin WebGUI deze ontvangt. </p>|,
+		lastUpdated => 1168561501
+	},
+
+	'72' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'get syndicated content' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'hasTermsLabel description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'process macros in rss url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'rssTabName' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'61' => {
+		message => q|Syndicated Content, Toevoegen/Bewerken<br />|,
+		lastUpdated => 1168561695
+	},
+
+	'item_loop' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'hasTerms' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'site_title' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'hasTermsLabel' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'rss.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'title' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'3 description' => {
+		message => q|Voer het maximum aantal nieuwsregels in wat mag worden weergegeven. Voer een nul in voor een onbeperkt aantal nieuwsregels. Alle nieuwsregels van de URL worden geladen, alhoewel slechts het maximum aantal wordt weergegeven. |,
+		lastUpdated => 1168561651
+	},
+
+	'interleaved' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'cache timeout help' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'channel.title' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'displayModeSubtext' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'channel.description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'process macros in rss url description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'cache timeout' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'templateId' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'displayModeLabel description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'link' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'cacheTimeout' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'3' => {
+		message => q|Maximum aantal nieuwsregels|,
+		lastUpdated => 1168561526
+	},
+
+	'grouped' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'maxHeadlines' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'new_rss_site' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'rss.url.0.9' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'4' => {
+		message => q|Bewerk Syndicated Content<br />|,
+		lastUpdated => 1168561667
+	},
+
+	'RSS Feed Title Suffix' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'73' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'processMacrosInRssUrl' => {
+		message => q||,
+		lastUpdated => 
+	},
+
 };
 
 1;
