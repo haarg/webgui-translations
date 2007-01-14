e707fc1b35d17294bc28e1bc0d8b618a6e453626
Translation Server <translation@plainblack.com>
2007-01-14 21:06:12 +0000

    update_from_translation_server

--- Bulgarian/Bulgarian/Macros.pm
+++ Bulgarian/Bulgarian/Macros.pm
@@ -1,6 +1,56 @@
 package WebGUI::i18n::Bulgarian::Macros;
 
 our $I18N = {
+	'macro disabled' => {
+		message => q|Този Макрос не е разрешен в конфигурационния файл на WebGUI и не може да се използва на този сайт.<br />|,
+		lastUpdated => 1168808480
+	},
+
+	'macros list body' => {
+		message => q|Правенето на макрос достъпен за използване въвВашия сайт е процес с две стъпки.|,
+		lastUpdated => 1168808654
+	},
+
+	'macro enabled' => {
+		message => q|Този Макрос е разрешен в конфигурационния файл на WebGUI и може да се използва на този сайт.|,
+		lastUpdated => 1168808543
+	},
+
+	'macro shortcut' => {
+		message => q|Кратък клавиш на макроса|,
+		lastUpdated => 1168808598
+	},
+
+	'macro enabled header' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'topicName' => {
+		message => q|Макроси|,
+		lastUpdated => 1168808763
+	},
+
+	'macro name' => {
+		message => q|Име на макроса|,
+		lastUpdated => 1168808566
+	},
+
+	'macros list title' => {
+		message => q|Макроси, списък на наличните|,
+		lastUpdated => 1168808732
+	},
+
+	'macros using body' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'macros using title' => {
+		message => q|Макроси, Използване<br />|,
+		lastUpdated => 1168808751
+	},
+
 };
 
 1;
