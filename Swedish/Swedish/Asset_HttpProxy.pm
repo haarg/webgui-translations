31e0add6fd334cd697ee9ae4d2fc00d43672d9fe
Translation Server <translation@plainblack.com>
2007-01-18 06:50:32 +0000

    update_from_translation_server

--- Swedish/Swedish/Asset_HttpProxy.pm
+++ Swedish/Swedish/Asset_HttpProxy.pm
@@ -1,6 +1,191 @@
 package WebGUI::i18n::Swedish::Asset_HttpProxy;
 
 our $I18N = {
+	'content' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'assetName' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'2' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'1' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'1 description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'13 description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'http proxy template body' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'http proxy template title description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'http proxy template title' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'content.leading' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'4 description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'5 description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'14' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'use ampersand help' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'10' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'no recursion' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'11' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'cache timeout' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'13' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'no frame error message' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'cache timeout description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'6 description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'6' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'14 description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'may not leave error message' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'12 description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'content.trailing' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'9' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'12' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'use ampersand' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'8' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'8 description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'4' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'stop.at' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'search.for' => {
+		message => q|s&ouml;k efter|,
+		lastUpdated => 1169049624
+	},
+
+	'5' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'header' => {
+		message => q||,
+		lastUpdated => 
+	},
+
 };
 
 1;
