20ef6038969bd1ff39c6963b1e5f1b67eea70e4a
Translation Server <translation@plainblack.com>
2007-01-30 16:59:19 +0000

    update_from_translation_server

--- polish/polish/AdminConsole.pm
+++ polish/polish/AdminConsole.pm
@@ -1,6 +1,21 @@
 package WebGUI::i18n::polish::AdminConsole;
 
 our $I18N = {
+	'toggle off' => {
+		message => q|Ukryj panel<br />|,
+		lastUpdated => 1169563774
+	},
+
+	'toggle on' => {
+		message => q|Pokaż panel<br />|,
+		lastUpdated => 1169563732
+	},
+
+	'admin console' => {
+		message => q|Panel administracyjny |,
+		lastUpdated => 1169563534
+	},
+
 };
 
 1;
