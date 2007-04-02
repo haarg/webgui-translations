7fcd6f5315c0850d0d39aec599479b8b31f3a52a
Translation Server <translation@plainblack.com>
2007-04-02 07:24:00 +0000

    update_from_translation_server

--- French/French/Asset_Event.pm
+++ French/French/Asset_Event.pm
@@ -1,6 +1,26 @@
 package WebGUI::i18n::French::Asset_Event;
 
 our $I18N = {
+	'add/edit body' => {
+		message => q|ajouter / &eacute;diter le contenu<br />|,
+		lastUpdated => 1170784058
+	},
+
+	'assetName' => {
+		message => q|nom|,
+		lastUpdated => 1170784096
+	},
+
+	'add/edit title' => {
+		message => q|ajouter / &eacute;diter le titre<br />|,
+		lastUpdated => 1170784078
+	},
+
+	'locale' => {
+		message => q|repr&eacute;sentation des dates<br />|,
+		lastUpdated => 1170784155
+	},
+
 };
 
 1;
