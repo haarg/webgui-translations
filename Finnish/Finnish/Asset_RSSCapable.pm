6ba0fa0ec9cb941e8f382f6bb9903194a265d370
Translation Server <translation@plainblack.com>
2007-04-11 11:09:32 +0000

    update_from_translation_server

--- Finnish/Finnish/Asset_RSSCapable.pm
+++ Finnish/Finnish/Asset_RSSCapable.pm
@@ -1,6 +1,41 @@
 package WebGUI::i18n::Finnish::Asset_RSSCapable;
 
 our $I18N = {
+	'rssTemplateId hoverHelp' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'rssTemplateId label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'rssEnabled hoverHelp' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'rssEnabled label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'rss capable body' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'rss capable title' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'assetName' => {
+		message => q|RSS kykenevä|,
+		lastUpdated => 1170842386
+	},
+
 };
 
 1;
