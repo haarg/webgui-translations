eddfcac1e9b25d2758b2101d57237a13a1a29c35
colin <colin@ba3075da-5205-0410-b03c-ad9aa4846add>
2008-02-13 22:49:39 +0000

    remove some crufty files

--- Dutch/Dutch/Asset_SQLReport/extras/tinymce2/jscripts/tiny_mce/langs/en.js.pm
+++
@@ -1,6 +0,0 @@
-package WebGUI::i18n::Dutch::Asset_SQLReport/extras/tinymce2/jscripts/tiny_mce/langs/en.js;
-
-our $I18N = {
-};
-
-1;
--- Dutch/Dutch/Asset_SQLReport/extras/tinymce2/jscripts/tiny_mce/themes/advanced/editor_template.js.pm
+++
@@ -1,6 +0,0 @@
-package WebGUI::i18n::Dutch::Asset_SQLReport/extras/tinymce2/jscripts/tiny_mce/themes/advanced/editor_template.js;
-
-our $I18N = {
-};
-
-1;
