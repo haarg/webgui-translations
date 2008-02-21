9495c9a97a5108943d9fddc6bf17b6147bd41edb
Translation Server <translation@plainblack.com>
2008-02-21 20:07:26 +0000

    update_from_translation_server

--- French/French/Macro_GroupText.pm
+++ French/French/Macro_GroupText.pm
@@ -2,13 +2,13 @@ package WebGUI::i18n::French::Macro_GroupText;
 
 our $I18N = {
 	'group not found' => {
-		message => q|Le Groupe /% est introuvable|,
-		lastUpdated => 1202892648
+		message => q|Le Groupe %s est introuvable|,
+		lastUpdated => 1203624426
 	},
 
 	'macroName' => {
 		message => q|Texte du Groupe|,
-		lastUpdated => 1202892662
+		lastUpdated => 1203624440
 	},
 
 };
