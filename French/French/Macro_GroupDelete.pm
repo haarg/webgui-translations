de72bda5678219f25d219688237cc0df5bbd15f9
Translation Server <translation@plainblack.com>
2008-02-21 23:45:50 +0000

    update_from_translation_server

--- French/French/Macro_GroupDelete.pm
+++ French/French/Macro_GroupDelete.pm
@@ -2,18 +2,18 @@ package WebGUI::i18n::French::Macro_GroupDelete;
 
 our $I18N = {
 	'group.text' => {
-		message => q||,
-		lastUpdated => 
+		message => q|La chaine de texte cliquable par l'utilisateur.|,
+		lastUpdated => 1203637522
 	},
 
 	'group.url' => {
-		message => q||,
-		lastUpdated => 
+		message => q|L'url avec l'action d'effacer l'utilisateur du groupe.|,
+		lastUpdated => 1203637464
 	},
 
 	'group delete title' => {
-		message => q|Variables de mod&egrave;le de mise en page (templates) de la macro &quot;Effacer le groupe&quot;|,
-		lastUpdated => 1202892741
+		message => q|Variables du gabarit de la macro &quot;Effacer le groupe&quot;|,
+		lastUpdated => 1203637543
 	},
 
 	'macroName' => {
