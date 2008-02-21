e51659d3ac095c03fdd0d95101cbbda46166a3db
Translation Server <translation@plainblack.com>
2008-02-21 23:42:16 +0000

    update_from_translation_server

--- French/French/Asset_RSSFromParent.pm
+++ French/French/Asset_RSSFromParent.pm
@@ -1,6 +1,96 @@
 package WebGUI::i18n::French::Asset_RSSFromParent;
 
 our $I18N = {
+	'description.parent' => {
+		message => q|La description du parent de ce module|,
+		lastUpdated => 1203636544
+	},
+
+	'title.parent' => {
+		message => q|Le titre du parent de ce module|,
+		lastUpdated => 1203636895
+	},
+
+	'link.item' => {
+		message => q|L'url de ce module|,
+		lastUpdated => 1203636995
+	},
+
+	'assetName' => {
+		message => q|RSS du Parent|,
+		lastUpdated => 1203636492
+	},
+
+	'link.parent' => {
+		message => q|L'url du parent de ce module|,
+		lastUpdated => 1203637014
+	},
+
+	'generator' => {
+		message => q|Une chaine identifiant que ce flux RSS a &eacute;t&eacute; g&eacute;n&eacute;r&eacute; par WebGUI ainsi que par quelle version de WebGUI.|,
+		lastUpdated => 1203636702
+	},
+
+	'rss from parent title' => {
+		message => q|RSS du Parent|,
+		lastUpdated => 1203636969
+	},
+
+	'guid' => {
+		message => q|Un alias pour le lien. Dans le flux RSS, guid est l'unique identifiant pour l'objet.|,
+		lastUpdated => 1203636757
+	},
+
+	'docs' => {
+		message => q|L'URL http://blogs.law.harvard.edu/tech/rss, pour les sp&eacute;cifications RSS 2.0.|,
+		lastUpdated => 1203636608
+	},
+
+	'webMaster' => {
+		message => q|L'adresse email soci&eacute;t&eacute; dans les param&egrave;tres WebGUI.|,
+		lastUpdated => 1203636875
+	},
+
+	'description.item' => {
+		message => q|La description de ce module|,
+		lastUpdated => 1203636523
+	},
+
+	'rss from parent body' => {
+		message => q|L'unique propos de ce module est de fournir un gabarit de base pour g&eacute;n&eacute;rer les flux RSS. Sont list&eacute;s les variables du gabarit de base disponibles pour n'importe quel module g&eacute;n&eacute;rant un flux RSS valide. Les modules ont d'autres variables additionnelles.|,
+		lastUpdated => 1203637320
+	},
+
+	'link' => {
+		message => q|Lien|,
+		lastUpdated => 1203636840
+	},
+
+	'item_loop' => {
+		message => q|Un boucle contenant les informations sur tous les modules suivants le parent.|,
+		lastUpdated => 1203636824
+	},
+
+	'description' => {
+		message => q|description|,
+		lastUpdated => 1203636502
+	},
+
+	'lastBuildDate' => {
+		message => q|La date de la derni&egrave;re modification du contenu du parent dans un format pour RSS (RFC 822).|,
+		lastUpdated => 1203637131
+	},
+
+	'pubDate' => {
+		message => q|La date de la derni&egrave;re modification de cet objet dans un format pour RSS (RFC 822).|,
+		lastUpdated => 1203637086
+	},
+
+	'title.item' => {
+		message => q|Le titre de ce module|,
+		lastUpdated => 1203636906
+	},
+
 };
 
 1;
