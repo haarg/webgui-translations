4df82efb68eda81ae09dbd2fc76930c4d2dd7260
Translation Server <translation@plainblack.com>
2008-02-21 23:18:10 +0000

    update_from_translation_server

--- French/French/Asset_Template.pm
+++ French/French/Asset_Template.pm
@@ -1,6 +1,281 @@
 package WebGUI::i18n::French::Asset_Template;
 
 our $I18N = {
+	'site name' => {
+		message => q|Nom du site|,
+		lastUpdated => 1203635594
+	},
+
+	'heading' => {
+		message => q|Heading|,
+		lastUpdated => 1203635695
+	},
+
+	'style wizard' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'assetName' => {
+		message => q|Gabarit|,
+		lastUpdated => 1203635408
+	},
+
+	'head block' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'header background color' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'session.setting.companyName' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'body background color description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'parser description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'visited link color' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'plugin name' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'template parsers' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'template variable title' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'header text color description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'namespace description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'session.var.adminOn' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'webgui.status' => {
+		message => q|Le statut de cette version de WebGUI (stable, beta, gamma, ect).|,
+		lastUpdated => 1203635504
+	},
+
+	'template parsers list title' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'session.user.firstDayOfWeek' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'choose a layout' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'head block description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'header text color' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'header background color description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'template error' => {
+		message => q|Il y a une erreur dans ce gabarit. |,
+		lastUpdated => 1203635572
+	},
+
+	'page background color description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'parser' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'link color' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'default parser' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'site name description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'body background color' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'session.user.username' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'body content' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'session.config.extrasurl' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page background color' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'session scratch variables' => {
+		message => q|<p>N'importe quelle variable sera disponible dans le gabarit avec la syntaxe:</p><p>&nbsp; &lt;tmpl_var session.scratch.<em>variable</em>&gt;</p>|,
+		lastUpdated => 1203635852
+	},
+
+	'logo subtext' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'body text color description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'logo description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'webgui.version' => {
+		message => q|La version de WebGUI sur votre site.|,
+		lastUpdated => 1203635450
+	},
+
+	'show in forms description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'menu background color description' => {
+		message => q|La couleur de fond pour la partie menu de la page.|,
+		lastUpdated => 1203635788
+	},
+
+	'namespace' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'visited link color description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'edit template' => {
+		message => q|Modifier le gabarit|,
+		lastUpdated => 1203635666
+	},
+
+	'template parsers list body' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'session form variables' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'menu' => {
+		message => q|Menu|,
+		lastUpdated => 1203635603
+	},
+
+	'session.setting.anonymousRegistration' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'plugin enabled header' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'logo' => {
+		message => q|Logo|,
+		lastUpdated => 1203635651
+	},
+
+	'link color description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'show in forms' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'body text color' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'template description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'menu background color' => {
+		message => q|Coleur de fond du Menu|,
+		lastUpdated => 1203635743
+	},
+
 };
 
 1;
