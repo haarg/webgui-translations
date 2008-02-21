9e802d9bf737dc8e98bd760b1fcdbee1ee4eae25
Translation Server <translation@plainblack.com>
2008-02-21 23:09:17 +0000

    update_from_translation_server

--- French/French/Asset_FilePile.pm
+++ French/French/Asset_FilePile.pm
@@ -2,73 +2,73 @@ package WebGUI::i18n::French::Asset_FilePile;
 
 our $I18N = {
 	'upload files description' => {
-		message => q|Emplacement des fichiers &agrave; transferer sur votre ordinateur. Vous pouvez envoyer jusqu'&agrave; 100 fichiers d'un coup. La taille ne devant pas exc&eacute;der 100MB en tout.|,
-		lastUpdated => 1202561258
+		message => q|Emplacement des fichiers &agrave; transf&eacute;rer sur votre ordinateur. Vous pouvez envoyer jusqu'&agrave; 100 fichiers d'un coup. La taille ne devant pas exc&eacute;der 100MB au total.|,
+		lastUpdated => 1203635352
 	},
 
 	'108 description' => {
-		message => q|Le propri&eacute;taire d'un 'asset' est g&eacute;n&eacute;ralement la personne qui lui a donn&eacute; naissance. Cet personne a toujours les droits de le voir et de le modifier.|,
-		lastUpdated => 1202560343
+		message => q|Le propri&eacute;taire d'un module est g&eacute;n&eacute;ralement la personne qui lui a donn&eacute; naissance. Cet personne a toujours les droits de le voir et de le modifier.|,
+		lastUpdated => 1203635119
 	},
 
 	'upload files' => {
-		message => q|Transf&eacute;rer les fichier sur le serveur|,
-		lastUpdated => 1202560969
+		message => q|Transf&eacute;rer les fichiers sur le serveur|,
+		lastUpdated => 1203635324
 	},
 
 	'872' => {
 		message => q|Qui peut le voir?|,
-		lastUpdated => 1202560556
+		lastUpdated => 1203635228
 	},
 
 	'108' => {
 		message => q|Propri&eacute;taire|,
-		lastUpdated => 1202560171
+		lastUpdated => 1203635216
 	},
 
 	'assetName' => {
 		message => q|Pile de fichiers|,
-		lastUpdated => 1202560920
+		lastUpdated => 1203635312
 	},
 
 	'872 description' => {
-		message => q|Choisir quel groupe d'utilisateur peut voir cet 'asset'. Assigner le groupe 'EVERYBODY' si vous souhaitez qu'il soit vu par les visiteurs et les utilisateurs identifi&eacute;s.|,
-		lastUpdated => 1202560675
+		message => q|Choisir quel groupe d'utilisateurs peut voir ce module. Assigner le groupe 'EVERYBODY' si vous souhaitez qu'il soit vu par les visiteurs et les utilisateurs identifi&eacute;s.|,
+		lastUpdated => 1203635255
 	},
 
 	'871 description' => {
-		message => q|Choisir le groupe d'utilisateur pouvant modifier cet 'asset'. Les droits de voir l'asset lui sont aussi attribu&eacute;s.|,
-		lastUpdated => 1202560543
+		message => q|Choisir le groupe d'utilisateurs pouvant modifier ce module. Les droits pour le voir lui sont aussi attribu&eacute;s.|,
+		lastUpdated => 1203635199
 	},
 
 	'871' => {
 		message => q|Qui peut l'&eacute;diter?|,
-		lastUpdated => 1202560362
+		lastUpdated => 1203635222
 	},
 
 	'940 description' => {
-		message => q|S&eacute;lectionner 'OUI' pour ouvrir cet asset dans une nouvelle fen&ecirc;tre.|,
-		lastUpdated => 1202560858
+		message => q|S&eacute;lectionner 'OUI' pour ouvrir ce module dans une nouvelle fen&ecirc;tre.|,
+		lastUpdated => 1203635096
 	},
 
 	'886' => {
 		message => q|Cacher de la navigation?|,
-		lastUpdated => 1202560690
+		lastUpdated => 1203635266
 	},
 
 	'940' => {
 		message => q|Ouvrir dans une nouvelle fen&ecirc;tre?|,
-		lastUpdated => 1202560799
+		lastUpdated => 1203635299
 	},
 
 	'add pile' => {
 		message => q|Rajouter une pile de fichiers|,
-		lastUpdated => 1202560901
+		lastUpdated => 1203635307
 	},
 
 	'886 description' => {
-		message => q|Condition ou non pour que l'asset soit cach&eacute; &agrave; la navigation et dans le plan de site.|,
-		lastUpdated => 1202560784
+		message => q|Condition ou non pour que le module soit cach&eacute; &agrave; la navigation et dans les plans de site.|,
+		lastUpdated => 1203635291
 	},
 
 };
