4b33888c809641772fae17b40ea073cf01627afd
Translation Server <translation@plainblack.com>
2008-02-21 23:27:28 +0000

    update_from_translation_server

--- French/French/Asset_RSSCapable.pm
+++ French/French/Asset_RSSCapable.pm
@@ -1,6 +1,31 @@
 package WebGUI::i18n::French::Asset_RSSCapable;
 
 our $I18N = {
+	'rssTemplateId hoverHelp' => {
+		message => q|Le gabarit &agrave; utiliser pour le flux RSS de ce module.|,
+		lastUpdated => 1203635978
+	},
+
+	'rssTemplateId label' => {
+		message => q|Gabarit RSS|,
+		lastUpdated => 1203635953
+	},
+
+	'rssEnabled hoverHelp' => {
+		message => q|L'activation ou non du flux RSS de ce module. Si disponiblen un flux RSS du module parent sera cr&eacute;e et g&eacute;r&eacute; comme un enfant suppl&eacute;mentaire pour ce propos. Si non disponible, aucun enfant ne sera cr&eacute;&eacute; et l'exitant sera effac&eacute;.|,
+		lastUpdated => 1203636146
+	},
+
+	'rssEnabled label' => {
+		message => q|Activer RSS|,
+		lastUpdated => 1203636002
+	},
+
+	'assetName' => {
+		message => q|Capacit&eacute; RSS |,
+		lastUpdated => 1203636173
+	},
+
 };
 
 1;
