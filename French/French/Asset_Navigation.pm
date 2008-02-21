e7a01cda088e50202597a39bbfa261d706638769
Translation Server <translation@plainblack.com>
2008-02-21 22:39:07 +0000

    update_from_translation_server

--- French/French/Asset_Navigation.pm
+++ French/French/Asset_Navigation.pm
@@ -1,6 +1,561 @@
 package WebGUI::i18n::French::Asset_Navigation;
 
 our $I18N = {
+	'' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'showUnprivilegedPages' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'32' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'showHiddenPages' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.parent.newWindow' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'Ancestor End Point' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'reverse page loop' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'currentPage.hasViewableSiblings' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'Descendants' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.inBranch' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'currentPage.ownerUserId' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.parent.parentId' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.isHidden' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'Ancestor End Point description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'Siblings' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'mimeType variable' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.hasViewableChildren' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'reverse page loop description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'indent' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'Infinity' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'currentPage.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'showSystemPages' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.title' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'Relative To Current URL' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'mimeType' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.indent_loop' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'31' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.newWindow' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.depthIsN' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'Self' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'30 description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.isChild' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.assetId' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.isRankedLast' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'Start Point Type' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'Start Point Type description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.isTopOfBranch' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'Relatives To Include description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'1096' => {
+		message => q|Gabarit de navigation|,
+		lastUpdated => 1203633310
+	},
+
+	'page.isRankedFirst' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.indent' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'Pedigree' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'mimeType description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'currentPage.title' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'navigation asset template variables title' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'startPoint' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.parent.assetId' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page_loop' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'currentPage.isHome' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'reversePageLoop' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.depthDiff_loop' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.hasChild' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'currentPage.menuTitle' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.parent.menuTitle' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.depthDiffIsN' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.parent.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.isBranchRoot' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'Start Point' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'assetName' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'currentPage.hasChild' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'Ancestors' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'Descendant End Point description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'Start Point description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.isSibling' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.depthDiff' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'1097' => {
+		message => q|<p>Ces variables sont disponibles dans les gabarits de navigation:</p><p><strong>currentPage</strong>&nbsp; se r&eacute;f&eacute;re &agrave; la page actuelle visit&eacute;e par l'utilisateur. <strong>page</strong> se r&eacute;f&eacute;re &agrave; une page dans la boucle <strong>page_loop</strong> </p>|,
+		lastUpdated => 1203633491
+	},
+
+	'assetsToInclude' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'30' => {
+		message => q|Monter les pages syst&egrave;me?|,
+		lastUpdated => 1203633527
+	},
+
+	'currentPage.synopsis' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.rank' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.parent.title' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'currentPage.assetId' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'descendantEndPoint' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'Relative To Root' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.isViewable' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'currentPage.hasSibling' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'preview' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.isCurrent' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.absDepth' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'ancestorEndPoint' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'32 description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.isParent' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'currentPage.parentId' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.parentId' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.isDescendent' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'Specific URL' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'currentPage.newWindow' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'Descendant End Point' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'22' => {
+		message => q|Modifier la navigation|,
+		lastUpdated => 1203633504
+	},
+
+	'page.isSystem' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.menuTitle' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'1096 description' => {
+		message => q|Choisir un gabarit &agrave; utiliser pour afficher votre navigation?|,
+		lastUpdated => 1203633357
+	},
+
+	'page.relDepth' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.parent.rank' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'Relatives To Include' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.synopsis' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'currentPage.hasViewableChildren' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'startType' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'templateId' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.ownerUserId' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'31 description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.parent.synopsis' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.isContainer' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.isAncestor' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.isUtility' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'navigation asset template variables body' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.parent.ownerUserId' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.inBranchRoot' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'currentPage.rank' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'page.relativeDepthIsN' => {
+		message => q||,
+		lastUpdated => 
+	},
+
 };
 
 1;
