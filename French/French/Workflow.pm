56758fa49a7e92ca2da2016483d8b7458a696172
Translation Server <translation@plainblack.com>
2008-02-22 12:39:55 +0000

    update_from_translation_server

--- French/French/Workflow.pm
+++ French/French/Workflow.pm
@@ -2,13 +2,13 @@ package WebGUI::i18n::French::Workflow;
 
 our $I18N = {
 	'workflow type count' => {
-		message => q||,
-		lastUpdated => 
+		message => q|<h2>%d %s Flux de t&acirc;ches<br /></h2>|,
+		lastUpdated => 1203681296
 	},
 
 	'edit priority unknown error' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Il y avait une erreur inconnue en mettant &agrave; jour la priorit&eacute; du flux de t&acirc;ches.SVP, r&eacute;essayez plus tard.|,
+		lastUpdated => 1203682659
 	},
 
 	'mode' => {
@@ -17,68 +17,68 @@ our $I18N = {
 	},
 
 	'parallel' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Parall&egrave;le|,
+		lastUpdated => 1203681454
 	},
 
 	'edit priority bad request' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Vous avez lanc&eacute; une mauvaise requ&ecirc;te.|,
+		lastUpdated => 1203682236
 	},
 
 	'edit priority instance not found error' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Je ne peux pas trouver ce flux de t&acirc;ches. Peut-&ecirc;tre qu'il a fini de travailler.|,
+		lastUpdated => 1203682304
 	},
 
 	'last state header' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Dernier &eacute;tat|,
+		lastUpdated => 1203682708
 	},
 
 	'edit priority success' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Priorit&eacute; du flux de t&acirc;ches mis &agrave; jour avec succ&egrave;s.|,
+		lastUpdated => 1203682552
 	},
 
 	'manage workflows' => {
-		message => q||,
-		lastUpdated => 
+		message => q|G&eacute;rer  les flux de t&acirc;ches.|,
+		lastUpdated => 1203682076
 	},
 
 	'enabled' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Activ&eacute;|,
+		lastUpdated => 1203681532
 	},
 
 	'edit workflow' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Modifier le flux de t&acirc;ches|,
+		lastUpdated => 1203682058
 	},
 
 	'workflowId' => {
-		message => q||,
-		lastUpdated => 
+		message => q|ID du flux de t&acirc;ches|,
+		lastUpdated => 1203681273
 	},
 
 	'title help' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Etiquette compr&eacute;hensible pour facilement identifier l'action du fluxde t&acirc;ches.|,
+		lastUpdated => 1203681877
 	},
 
 	'serial' => {
-		message => q||,
-		lastUpdated => 
+		message => q|S&eacute;rie|,
+		lastUpdated => 1203681468
 	},
 
 	'description help' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Remplir par une description d&eacute;taill&eacute;e de l'action de ce flux de t&acirc;ches et pour quoi il est utilis&eacute; pour une future r&eacute;f&eacute;rence.|,
+		lastUpdated => 1203682498
 	},
 
 	'edit priority no spectre error' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Spectre <strong>ne marche pas</strong>.|,
+		lastUpdated => 1203682516
 	},
 
 	'add a new workflow' => {
@@ -87,138 +87,138 @@ our $I18N = {
 	},
 
 	'description' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Description|,
+		lastUpdated => 1203681508
 	},
 
 	'mode help' => {
-		message => q||,
-		lastUpdated => 
+		message => q|<p>Le mode d'un flux de t&acirc;ches dertermine sa pr&eacute;s&eacute;ance. (<strong>Comment </strong>et <strong>quand </strong>un flux de t&acirc;ches marche)</p><p><strong>Mode parall&egrave;le</strong> : fonctionnement asynchrone pour les instances de ce flux de t&acirc;ches si elle existe en nombre.</p><p><strong>Mode ingleton </strong>: fonctionnement asynchrone et exactement une instance &agrave; un temps donn&eacute; et si un nouveau flux de t&acirc;ches de ce type est cr&eacute;&eacute; pendant que l'original est ex&eacute;cut&eacute;, le syst&egrave;me s'en d&eacute;barrassera.</p>         <p><strong>Mode en serial</strong>&nbsp; : fonctionnement asynchrone des instances de ce type une &agrave; la fois, dans l'ordre dans lequel ils ont &eacute;t&eacute; cr&eacute;&eacute;s.</p>         <p><strong>Mode temps r&eacute;el</strong> : Fonctionnement synchrone (imm&eacute;diatement) de toutes les activit&eacute;s lui &eacute;tant associ&eacute;es. Tous les flux de t&acirc;ches ne supportent pas le mode temps r&eacute;el, et vous ne pouvez pas lancer n'importe quel flux de t&acirc;ches en mode temps r&eacute;el. (Example : approuver une version tag). </p>|,
+		lastUpdated => 1203683855
 	},
 
 	'title' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Titre|,
+		lastUpdated => 1203681342
 	},
 
 	'edit priority setting error' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Il y a une erreur de param&egrave;tres dans la nouvelle priorit&eacute;.|,
+		lastUpdated => 1203682600
 	},
 
 	'realtime' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Temps r&eacute;el|,
+		lastUpdated => 1203681412
 	},
 
 	'disabled' => {
-		message => q||,
-		lastUpdated => 
+		message => q|D&eacute;sactiv&eacute;|,
+		lastUpdated => 1203681522
 	},
 
 	'title header' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Titre|,
+		lastUpdated => 1203682561
 	},
 
 	'is enabled help' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Si d&eacute;fini par 'oui', alors le syst&egrave;me sera autoris&eacute; &agrave; cr&eacute;er et lancer des instances de ce flux de t&acirc;ches.|,
+		lastUpdated => 1203682178
 	},
 
 	'object type' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Type d'onjet|,
+		lastUpdated => 1203681701
 	},
 
 	'last run time header' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Dernier lancement|,
+		lastUpdated => 1203682694
 	},
 
 	'priority header' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Actuelle/originel priorit&eacute;|,
+		lastUpdated => 1203681930
 	},
 
 	'spectre not running error' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Spectre ne marche pas.|,
+		lastUpdated => 1203681787
 	},
 
 	'singleton' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Singleton|,
+		lastUpdated => 1203681399
 	},
 
 	'topicName' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Gestionnaire de t&acirc;ches|,
+		lastUpdated => 1203681331
 	},
 
 	'activity header' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Activit&eacute; en cours|,
+		lastUpdated => 1203681648
 	},
 
 	'run' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Lancer|,
+		lastUpdated => 1203681353
 	},
 
 	'no object' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Pas d'objet|,
+		lastUpdated => 1203681631
 	},
 
 	'edit priority update priority' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Mettre &agrave; jour la priorit&eacute;|,
+		lastUpdated => 1203682208
 	},
 
 	'are you sure you want to delete this workflow' => {
-		message => q||,
-		lastUpdated => 
+		message => q|&Ecirc;tes-vous s&ucirc;r de vouloir effacer ce flux de t&acirc;ches et toutes ses instances lanc&eacute;s?|,
+		lastUpdated => 1203682408
 	},
 
 	'object type help' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Quel type d'objet voulez-vous que ce flux de t&acirc;che soit capable de manipuler?|,
+		lastUpdated => 1203683934
 	},
 
 	'object type help2' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Le type de flux de t&acirc;ches que vous avez d&eacute;cid&eacute; d'ajouter.|,
+		lastUpdated => 1203683984
 	},
 
 	'is enabled' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Activ&eacute;?|,
+		lastUpdated => 1203682088
 	},
 
 	'confirm delete activity' => {
-		message => q||,
-		lastUpdated => 
+		message => q|&Ecirc;tes-vous s&ucirc;r de vouloir effacer l'activit&eacute; de ce flux de t&acirc;ches?|,
+		lastUpdated => 1203681968
 	},
 
 	'edit priority cancel' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Annuler|,
+		lastUpdated => 1203682245
 	},
 
 	'edit priority no info error' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Spectre <strong>marche</strong>, mas je n'&eacute;tais pas capable de mettre &agrave; jour cette priorit&eacute;.|,
+		lastUpdated => 1203682341
 	},
 
 	'show running workflows' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Monter les flux de t&acirc;ches en activit&eacute;.|,
+		lastUpdated => 1203681691
 	},
 
 	'spectre no info error' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Spectre <strong>marche</strong>, mais je suis incapable d'obtenir d'information d&eacute;taill&eacute;es sur le flux de t&acirc;ches.|,
+		lastUpdated => 1203681619
 	},
 
 };
