d360422ae5c2f98a37c52228f0a63e79275ca09a
Translation Server <translation@plainblack.com>
2008-02-22 12:47:59 +0000

    update_from_translation_server

--- French/French/Workflow_Activity.pm
+++ French/French/Workflow_Activity.pm
@@ -2,8 +2,8 @@ package WebGUI::i18n::French::Workflow_Activity;
 
 our $I18N = {
 	'title help' => {
-		message => q|Mettre un nom ipour identifier l'activit&eacute;.|,
-		lastUpdated => 1203631943
+		message => q|Mettre un nom pour identifier l'activit&eacute;.|,
+		lastUpdated => 1203684459
 	},
 
 	'description help' => {
@@ -13,22 +13,22 @@ our $I18N = {
 
 	'description' => {
 		message => q|Description|,
-		lastUpdated => 1203072148
+		lastUpdated => 1203684396
 	},
 
 	'topicName' => {
 		message => q|Activit&eacute;s du flux de t&acirc;ches|,
-		lastUpdated => 1203631932
+		lastUpdated => 1203684471
 	},
 
 	'activity enabled header' => {
-		message => q|Activit&eacute; permisse?|,
-		lastUpdated => 1203072193
+		message => q|Activit&eacute; disponible?|,
+		lastUpdated => 1203684383
 	},
 
 	'activities list body' => {
-		message => q||,
-		lastUpdated => 1203631988
+		message => q|<p>Il existe 2 proc&eacute;dures pour que l'activit&eacute; d'un flux de t&acirc;ches soit disponible pour votre site :</p><p>1. L'activit&eacute; doit &ecirc;tre plac&eacute; dans le r&eacute;pertoire des activit&eacute;s dans le code source WebGUI: lib/WebGUI/Workflow/Activities.</p><p>2. L'activit&eacute; doit &ecirc;tre disponible dans le ficher de configuration de WebGUI.conf, dans la section &quot;workflowActivities&quot;.</p><p>La table suivante montrent quelles sont activit&eacute;s install&eacute;es sur votre site et configur&eacute;es dans votre fichier WebGUI.conf. Ca ne veut pas dire que l'activit&eacute; est utilis&eacute;e dans un flux de t&acirc;ches.</p>|,
+		lastUpdated => 1203684351
 	},
 
 	'title' => {
@@ -37,13 +37,13 @@ our $I18N = {
 	},
 
 	'list of installed activities' => {
-		message => q|Liste des activit&eacute;s du gestionnaire de t&acirc;ches install&eacute;es|,
-		lastUpdated => 1203072087
+		message => q|Liste des activit&eacute;s de flux de t&acirc;ches install&eacute;es|,
+		lastUpdated => 1203684435
 	},
 
 	'activity name' => {
 		message => q|Nom de l'activit&eacute;|,
-		lastUpdated => 1203072157
+		lastUpdated => 1203684390
 	},
 
 };
