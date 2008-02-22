cd5e751359ecd163b6a4e4c3a0012d4e0fcaf371
Translation Server <translation@plainblack.com>
2008-02-22 10:50:52 +0000

    update_from_translation_server

--- French/French/Asset_Wobject.pm
+++ French/French/Asset_Wobject.pm
@@ -1,6 +1,121 @@
 package WebGUI::i18n::French::Asset_Wobject;
 
 our $I18N = {
+	'1073 description' => {
+		message => q|S&eacute;lectionner un gabarit de la liste &agrave; assigner &agrave; votre Wobjet si il est vu directement. Si le Wobjet est affich&eacute; comme une partie du module page, le gabarit du module page est utilis&eacute; &agrave; la place.|,
+		lastUpdated => 1203676730
+	},
+
+	'add' => {
+		message => q|Ajouter|,
+		lastUpdated => 1203676860
+	},
+
+	'1079' => {
+		message => q|Style imprimable|,
+		lastUpdated => 1203676749
+	},
+
+	'displayTitle' => {
+		message => q|Une variable conditionnelle si le titre devra &ecirc;tre affich&eacute; ou non.|,
+		lastUpdated => 1203676970
+	},
+
+	'827' => {
+		message => q|Variables du gabarit Wobjet|,
+		lastUpdated => 1203676839
+	},
+
+	'assetName' => {
+		message => q|Wobjet|,
+		lastUpdated => 1203676916
+	},
+
+	'edit' => {
+		message => q|Modifier|,
+		lastUpdated => 1203676982
+	},
+
+	'85 description' => {
+		message => q|Une zone de contenu dans laquelle vous pouvez placer tout le contenu que vous souhaitez. Par example, avant une FAQ, il y a habituellement un paragraphe d&eacute;crivant le contenu de la FAQ.|,
+		lastUpdated => 1203677201
+	},
+
+	'42' => {
+		message => q|Confirmez SVP|,
+		lastUpdated => 1203676784
+	},
+
+	'174' => {
+		message => q|Afficher le titre?|,
+		lastUpdated => 1203676761
+	},
+
+	'assetId' => {
+		message => q|L'unique identifiant que WebGUI utilise pour contr&ocirc;ler ce module.|,
+		lastUpdated => 1203676905
+	},
+
+	'wobject template variables title' => {
+		message => q|Variables du module gabarit Wobjet|,
+		lastUpdated => 1203677255
+	},
+
+	'44' => {
+		message => q|Oui, je suis certain.|,
+		lastUpdated => 1203676803
+	},
+
+	'1073' => {
+		message => q|Gabarit|,
+		lastUpdated => 1203676580
+	},
+
+	'174 description' => {
+		message => q|Voulez-vous afficher le titre du Wobjet? Dans certains sites, l'affichage des titres n'est pas n&eacute;cessaire.|,
+		lastUpdated => 1203677305
+	},
+
+	'85' => {
+		message => q|Description|,
+		lastUpdated => 1203676851
+	},
+
+	'styleTemplateId' => {
+		message => q|L'assetid de ce gabarit pour afficher le module.|,
+		lastUpdated => 1203676567
+	},
+
+	'1079 description' => {
+		message => q|Cela d&eacute;fini le stule d'impression pour la page par un autre style que celui par d&eacute;faut de WebGUI. Il se comporte de la m&ecirc;me fa&ccedil;on au <strong>Gabarit de Style</strong> quand il est utilis&eacute;.|,
+		lastUpdated => 1203677443
+	},
+
+	'description' => {
+		message => q|La description du Wobjet.|,
+		lastUpdated => 1203676930
+	},
+
+	'45' => {
+		message => q|Non, j'ai fait une erreur.|,
+		lastUpdated => 1203676817
+	},
+
+	'originalURL' => {
+		message => q|Si le Wobjet est un raccourci, alors son URL sera directement celle du module original.|,
+		lastUpdated => 1203677048
+	},
+
+	'title' => {
+		message => q|Le titre de Wobjet.|,
+		lastUpdated => 1203676542
+	},
+
+	'printableStyleTemplateId' => {
+		message => q|L'assetId du gabarit pour afficher le module pour impressiion.|,
+		lastUpdated => 1203677090
+	},
+
 };
 
 1;
