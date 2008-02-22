e69ef2fc6703dc09f64ae472d53ae7fd3ad753cb
Translation Server <translation@plainblack.com>
2008-02-22 08:39:20 +0000

    update_from_translation_server

--- French/French/Asset_Search.pm
+++ French/French/Asset_Search.pm
@@ -2,133 +2,133 @@ package WebGUI::i18n::French::Asset_Search;
 
 our $I18N = {
 	'search asset template variables title' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Variables du gabarit du module recherche|,
+		lastUpdated => 1203669450
 	},
 
 	'search' => {
 		message => q|Rechercher|,
-		lastUpdated => 1202649051
+		lastUpdated => 1203669442
 	},
 
 	'search template' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Gabarit de recherche|,
+		lastUpdated => 1203669482
 	},
 
 	'classLimiter' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Une cha&icirc;ne contenant tous les types de module &agrave; rechercher.|,
+		lastUpdated => 1203669380
 	},
 
 	'class limiter' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Limiter aux classes de module:|,
+		lastUpdated => 1203669372
 	},
 
 	'assetName' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Recherche|,
+		lastUpdated => 1203669358
 	},
 
 	'synopsis' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Le r&eacute;sum&eacute; du module trouv&eacute; dans la recherche.|,
+		lastUpdated => 1203669509
 	},
 
 	'no_results' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Etiquette pour dire &agrave; l'utilisateur d'aucun r&eacute;sultat n'a &eacute;t&eacute; trouv&eacute;.<br /> |,
+		lastUpdated => 1203669434
 	},
 
 	'assetId' => {
-		message => q||,
-		lastUpdated => 
+		message => q|L'assetID du module trouv&eacute; dans la recherche.|,
+		lastUpdated => 1203669352
 	},
 
 	'url' => {
-		message => q||,
-		lastUpdated => 
+		message => q|L'URL du module trouv&eacute; dans la recherche.|,
+		lastUpdated => 1203669540
 	},
 
 	'searchRoot' => {
-		message => q||,
-		lastUpdated => 
+		message => q|L'ID du module ou la recherche commencera.|,
+		lastUpdated => 1203668978
 	},
 
 	'form_submit' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Un bouton pour lancer la recherche par l'utilisateur. (submit)|,
+		lastUpdated => 1203669407
 	},
 
 	'class limiter description' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Cela limitera les types de module recherch&eacute;s &agrave; ceux que vous allez selectionner.|,
+		lastUpdated => 1203668303
 	},
 
 	'result_set' => {
-		message => q||,
-		lastUpdated => 
+		message => q|R&eacute;sultats de recherche pagin&eacute;s avec les contr&ocirc;les de pagination.|,
+		lastUpdated => 1203668657
 	},
 
 	'useContainers help' => {
-		message => q||,
-		lastUpdated => 
+		message => q|D&eacute;finir &quot;OUI&quot; si vous voulez que les r&eacute;sultats de recherche retourne la page enti&egrave;re &agrave; la place des modules individuels.|,
+		lastUpdated => 1203669310
 	},
 
 	'templateId' => {
-		message => q||,
-		lastUpdated => 
+		message => q|L'ID du gabarit utilis&eacute; pour afficher le module de recherche.|,
+		lastUpdated => 1203669522
 	},
 
 	'search root' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Racine de la recherche|,
+		lastUpdated => 1203669468
 	},
 
 	'form_footer' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Code HTML &agrave; la fin du formulaire de recherche|,
+		lastUpdated => 1203668403
 	},
 
 	'useContainers' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Utiliser les URL du container dans les recherches?|,
+		lastUpdated => 1203669550
 	},
 
 	'search root description' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Le module selectionn&eacute; et tous ses descendants seront recherch&eacute;s.|,
+		lastUpdated => 1203669495
 	},
 
 	'results_found' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Une valeur conditionnelle valant 'True' si la recherche trouve des r&eacute;sultats.|,
+		lastUpdated => 1203668732
 	},
 
 	'form_header' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Code HTML au d&eacute;but du formulaire de recherche|,
+		lastUpdated => 1203668419
 	},
 
 	'form_keywords' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Un formulaire pour que l'utilisateur entre les m&ocirc;ts-cl&eacute; pour la recherche.|,
+		lastUpdated => 1203669399
 	},
 
 	'search template description' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Un gabarit pour afficher le formulaire de recherche et les resultats.|,
+		lastUpdated => 1203668946
 	},
 
 	'no results' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Aucun r&eacute;sultat n'a &eacute;t&eacute; trouv&eacute;.|,
+		lastUpdated => 1203669427
 	},
 
 	'title' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Le titre du module trouv&eacute; dans la recherche.|,
+		lastUpdated => 1203669532
 	},
 
 };
