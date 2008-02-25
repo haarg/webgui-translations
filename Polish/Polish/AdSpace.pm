b2333e2cc53715aca814f82a1c9492fff9f59adb
Translation Server <translation@plainblack.com>
2008-02-25 17:59:45 +0000

    update_from_translation_server

--- Polish/Polish/AdSpace.pm
+++ Polish/Polish/AdSpace.pm
@@ -2,258 +2,258 @@ package WebGUI::i18n::Polish::AdSpace;
 
 our $I18N = {
 	'is active' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Aktywny?|,
+		lastUpdated => 1203961647
 	},
 
 	'priority' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Priorytet|,
+		lastUpdated => 1203961738
 	},
 
 	'add an ad' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Dodaj boks reklamowy.|,
+		lastUpdated => 1203961140
 	},
 
 	'image help' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Plik obrazka, kt&oacute;ry będzie wyświetlany przy każdym odsłonięciu reklamy.|,
+		lastUpdated => 1203961574
 	},
 
 	'text color' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Kolor tekstu|,
+		lastUpdated => 1203962116
 	},
 
 	'edit ad space' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Edycja obszaru boksu reklamowego|,
+		lastUpdated => 1203961374
 	},
 
 	'ad text' => {
-		message => q|Tekst ogłoszenia|,
-		lastUpdated => 1198510420
+		message => q|Tekst boksu reklamowego|,
+		lastUpdated => 1203961015
 	},
 
 	'url' => {
-		message => q||,
-		lastUpdated => 
+		message => q|URL|,
+		lastUpdated => 1203962206
 	},
 
 	'background color' => {
 		message => q|Kolor tła|,
-		lastUpdated => 1198510470
+		lastUpdated => 1203961149
 	},
 
 	'manage ad spaces' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Zarządzaj boksem reklamowym.|,
+		lastUpdated => 1203961702
 	},
 
 	'title help' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Etykieta - tytuł.|,
+		lastUpdated => 1203962175
 	},
 
 	'text color help' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Wartość szesnastkowa koloru tekstu w boksie reklamowym.|,
+		lastUpdated => 1203962153
 	},
 
 	'description help' => {
-		message => q||,
-		lastUpdated => 
+		message => q| Pełny opis miejsca, w kt&oacute;rym reklama będzie ulokowana na stronie.|,
+		lastUpdated => 1203961365
 	},
 
 	'border color help' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Wartość szesnastkowa koloru ramki użytej do obramowania tekstu w boksie reklamowym.|,
+		lastUpdated => 1203961237
 	},
 
 	'rich help' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Wprowadź kod, kt&oacute;ry zostanie użyty do generowania boksu reklamowego by go wyświetlić.|,
+		lastUpdated => 1203962101
 	},
 
 	'name' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Nazwa|,
+		lastUpdated => 1203961709
 	},
 
 	'border color' => {
 		message => q|Kolor ramki|,
-		lastUpdated => 1198510555
+		lastUpdated => 1203961203
 	},
 
 	'description' => {
 		message => q|Opis|,
-		lastUpdated => 1203347055
+		lastUpdated => 1203961345
 	},
 
 	'priority help' => {
-		message => q||,
-		lastUpdated => 
+		message => q| Mechanizm determinujący częstotliwość wyświetlania banerka reklamowego. Im niższa wartość tym częściej będzie on wyświetlany. Pamiętaj, mechanizm ten może nie zachowywać dokładnych proporcji wyświetleń między obrazkami o r&oacute;zżym priorytecie, ponieważ zależy to od ilości odsłon strony. Im większy ruch na stronie tym większy wpływ na wyświetlanie ma nadany obrazkowi priorytet.|,
+		lastUpdated => 1203961965
 	},
 
 	'height help' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Jak wysoki (w pixelach) powinien być obszar reklamy.|,
+		lastUpdated => 1203961529
 	},
 
 	'width help' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Jak szeroki (w pikselach) powinien być obszar reklamy w boksie reklamowym.|,
+		lastUpdated => 1203962357
 	},
 
 	'clicks bought' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Wykupione kliknięcia|,
+		lastUpdated => 1203961251
 	},
 
 	'ad id' => {
-		message => q|id ogłoszenia|,
-		lastUpdated => 1198510351
+		message => q|Identyfikator boksu reklamowego|,
+		lastUpdated => 1203960993
 	},
 
 	'used' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Użyto|,
+		lastUpdated => 1203962326
 	},
 
 	'error heading' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Wystąpił błąd w boksie reklamowym:|,
+		lastUpdated => 1203961422
 	},
 
 	'title' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Tytuł|,
+		lastUpdated => 1203962162
 	},
 
 	'type' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Typ|,
+		lastUpdated => 1203962191
 	},
 
 	'type help' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Jaki typ reklamy będzie wyświetlany?|,
+		lastUpdated => 1203962200
 	},
 
 	'add ad space body' => {
-		message => q||,
-		lastUpdated => 
+		message => q|<p>W tym miejscu znajdziesz listę wszelkich możliwych boks&oacute;w reklamowych, kt&oacute;re zostały stworzone na potrzeby WebGUI. Ikony przyporządkowano boksom reklamowych pozwalają na ich edytowanie lub kasowanie. Znajduję się tutaj r&oacute;wnież link pozwalający na dodawanie nowych boks&oacute;w reklamowych.</p>|,
+		lastUpdated => 1203961132
 	},
 
 	'width' => {
 		message => q|Szerokość|,
-		lastUpdated => 1203347025
+		lastUpdated => 1203962332
 	},
 
 	'impressions bought' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Zakupione wyświetlenia - gwarantowane wyświetlenia reklamy|,
+		lastUpdated => 1203961603
 	},
 
 	'topicName' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Etykieta reklamy|,
+		lastUpdated => 1203962182
 	},
 
 	'is active help' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Czy ma być dodany do rotacji?|,
+		lastUpdated => 1203961657
 	},
 
 	'confirm ad space delete' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Czy jesteś pewien, że chcesz usunąć obszar reklamy wraz z załączonymi plikami? |,
+		lastUpdated => 1203961339
 	},
 
 	'name help' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Nazwa jako ciąg znak&oacute;w użyta w kombinacji z makrem AdSpace umiejscawia reklamę na stronie. Ciąg znak&oacute;w nie powinien zawierać spacji i mieć alfa-numeryczny charakter.|,
+		lastUpdated => 1203961732
 	},
 
 	'url help' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Adres URL do kt&oacute;rego kierować będzie reklama. Zauważ, że nie może być użyty dla reklam Rich Media.|,
+		lastUpdated => 1203962304
 	},
 
 	'text' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Tekst|,
+		lastUpdated => 1203962107
 	},
 
 	'edit this ad space' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Edytuj obszar boksu reklamowego.|,
+		lastUpdated => 1203961394
 	},
 
 	'ad text help' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Zaleca się przegotowanie tekst&oacute;w do boks&oacute;w reklamowych na maksymalnie 255 znak&oacute;w. Proszę pamiętać, że ilość tekstu zależy od przewidywanej wielkości reklamy i jeśli będzie on zbyt długi, jego część może nie być widoczna. Zalecana ilość znak&oacute;w w tekście reklamy to 60 znak&oacute;w lub mniej.|,
+		lastUpdated => 1203961040
 	},
 
 	'rich' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Rich Media|,
+		lastUpdated => 1203962085
 	},
 
 	'background color help' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Wartość szesnastkowa koloru dla tła w boksie reklamowym.|,
+		lastUpdated => 1203961192
 	},
 
 	'confirm ad delete' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Czy jesteś pewien, że chcesz usunąć tą reklamę?|,
+		lastUpdated => 1203961316
 	},
 
 	'ad space id' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Identyfikator obszaru boksu reklamowego|,
+		lastUpdated => 1203961002
 	},
 
 	'error invalid characters' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Wprowadzono nieprawidłowy znak w polu tytułu. Proszę korzystać wyłącznie z liter, liczb i odstęp&oacute;w.|,
+		lastUpdated => 1203961473
 	},
 
 	'clicks bought help' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Jak wiele kliknięć zakupił reklamobiorca? W przypadku dużej liczby wyświetleń można nadać wartość zero.|,
+		lastUpdated => 1203961298
 	},
 
 	'height' => {
 		message => q|Wysokość|,
-		lastUpdated => 1198510610
+		lastUpdated => 1203961505
 	},
 
 	'image' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Obrazek|,
+		lastUpdated => 1203961536
 	},
 
 	'macro code prompt' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Użyj tego kodu by umieścić własny boks reklamowy:|,
+		lastUpdated => 1203961691
 	},
 
 	'add ad space' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Dodaj obszar banera reklamowego.|,
+		lastUpdated => 1203961052
 	},
 
 	'edit advertisement' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Edycja reklamy|,
+		lastUpdated => 1203961383
 	},
 
 	'impressions bought help' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Jak wiele wyświetleń zakupił reklamobiorca? Jeśli zakupił on dużą liczbę kliknięć można tą wartość ustawić na zero.|,
+		lastUpdated => 1203962377
 	},
 
 };
