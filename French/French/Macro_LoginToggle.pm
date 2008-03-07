e70652ff81c152bc86ed94c0b01411ffa9d4e63d
Translation Server <translation@plainblack.com>
2008-03-07 12:11:48 +0000

    update_from_translation_server

--- French/French/Macro_LoginToggle.pm
+++ French/French/Macro_LoginToggle.pm
@@ -2,33 +2,33 @@ package WebGUI::i18n::French::Macro_LoginToggle;
 
 our $I18N = {
 	'716' => {
-		message => q|Connection|,
-		lastUpdated => 1203622448
+		message => q|Connexion|,
+		lastUpdated => 1204891857
 	},
 
 	'toggle.url' => {
-		message => q|L'URL pour la connection ou d&eacute;connection.|,
-		lastUpdated => 1203622618
+		message => q|L'URL pour la connexion ou d&eacute;connexion.|,
+		lastUpdated => 1204891902
 	},
 
 	'717' => {
-		message => q|D&eacute;connection|,
-		lastUpdated => 1203622442
+		message => q|D&eacute;connexion|,
+		lastUpdated => 1204891864
 	},
 
 	'macroName' => {
-		message => q|Bo&icirc;te de connection|,
-		lastUpdated => 1203622497
+		message => q|Bo&icirc;te de connexion|,
+		lastUpdated => 1204891880
 	},
 
 	'login toggle title' => {
-		message => q|Variables du gabarit de la macro 'bo&icirc;te de connection' (Login Toggle)|,
-		lastUpdated => 1203622488
+		message => q|Variables du gabarit de la macro 'bo&icirc;te de connexion' (Login Toggle)|,
+		lastUpdated => 1204891873
 	},
 
 	'toggle.text' => {
-		message => q|Etiquette pour connection et d&eacute;connection (d&eacute;pend du l'&eacute;tat de la macro) ou du texte fourni dans la macro.|,
-		lastUpdated => 1203622605
+		message => q|Etiquette pour connexion et d&eacute;connexion (d&eacute;pend du l'&eacute;tat de la macro) ou du texte fourni dans la macro.|,
+		lastUpdated => 1204891892
 	},
 
 };
