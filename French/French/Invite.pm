d015ffa23cbd4d4c59c83cd8622573a70e6b4cf2
Translation Server <translation@plainblack.com>
2008-03-07 16:11:56 +0000

    update_from_translation_server

--- French/French/Invite.pm
+++ French/French/Invite.pm
@@ -2,28 +2,28 @@ package WebGUI::i18n::French::Invite;
 
 our $I18N = {
 	'invalid invite code' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Code d'invitation invalide|,
+		lastUpdated => 1204906101
 	},
 
 	'formHeader' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Code HTML de l'ent&ecirc;te de formulaire.|,
+		lastUpdated => 1204906084
 	},
 
 	'invite form template body' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Ce gabarit est utilis&eacute; pour la personnalisation du formulaire que les utilisateurs remplissent pour inviter un ami &agrave; cr&eacute;er un compte.|,
+		lastUpdated => 1204904721
 	},
 
 	'default invite' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Je suis membre d'un site pour lequel vous devriez &ecirc;tre int&eacute;ress&eacute;. Je vous envoie cette invitation pour que vous m'y rejoigniez. Cliquez sur le lien suivant pour vous inscrire.|,
+		lastUpdated => 1204905865
 	},
 
 	'topicName' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Invitations utilisateur.|,
+		lastUpdated => 1204905738
 	},
 
 	'inviteFormError' => {
@@ -32,13 +32,13 @@ our $I18N = {
 	},
 
 	'invite a friend title' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Inviter un ami|,
+		lastUpdated => 1204906228
 	},
 
 	'emailAddressForm' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Code HTML du champ adresse mail.|,
+		lastUpdated => 1204905888
 	},
 
 	'already a member message' => {
@@ -52,93 +52,93 @@ our $I18N = {
 	},
 
 	'subjectForm' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Code HTML du champ sujet.|,
+		lastUpdated => 1204905666
 	},
 
 	'missing email' => {
-		message => q||,
-		lastUpdated => 
+		message => q|L'invitation ne peut-&ecirc;tre envoy&eacute; car vous n'avez pas specifier une adresse mail.|,
+		lastUpdated => 1204904858
 	},
 
 	'missing message' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Votre invitation doit comporter un message.|,
+		lastUpdated => 1204905599
 	},
 
 	'invitation sent' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Votre invitation a &eacute;t&eacute; envoy&eacute;e.|,
+		lastUpdated => 1204906156
 	},
 
 	'messageLabel' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Message|,
+		lastUpdated => 1204904818
 	},
 
 	'submitButton' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Envoyer|,
+		lastUpdated => 1204905686
 	},
 
 	'emailAddressLabel' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Adresse mail|,
+		lastUpdated => 1204906034
 	},
 
 	'invitationMessage' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Le message entr&eacute; par l'utilisateur, nettoy&eacute; du HTML pour pr&eacute;venir les attaques XSS.|,
+		lastUpdated => 1204906211
 	},
 
 	'invalid invite code message' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Le code d'invitation dans l'URL est invalide.|,
+		lastUpdated => 1204906133
 	},
 
 	'registrationUrl' => {
-		message => q||,
-		lastUpdated => 
+		message => q|l'URL que le destinataire devra utiliser pour accepter l'invitation.|,
+		lastUpdated => 1204905647
 	},
 
 	'invite email template title' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Gabarit du mail d'invitation|,
+		lastUpdated => 1204904640
 	},
 
 	'messageForm' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Code HTML du champ message.|,
+		lastUpdated => 1204904792
 	},
 
 	'invite email template body' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Ce gabarit est utilis&eacute; pour personnaliser l'invitation envoy&eacute;e par mail.|,
+		lastUpdated => 1204904504
 	},
 
 	'subjectLabel' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Sujet|,
+		lastUpdated => 1204905676
 	},
 
 	'missing subject' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Votre invitation doit avoir un sujet.|,
+		lastUpdated => 1204905616
 	},
 
 	'invite form template title' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Gabarit du formulaire d'invitation|,
+		lastUpdated => 1204904753
 	},
 
 	'formFooter' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Code HTML du pied de formulaire.|,
+		lastUpdated => 1204906065
 	},
 
 	'title' => {
-		message => q||,
-		lastUpdated => 
+		message => q|Invitation|,
+		lastUpdated => 1204905707
 	},
 
 };
