74b72aa840366d9eb59f147f09f182a373b16a33
Translation Server <translation@plainblack.com>
2008-03-08 21:39:01 +0000

    update_from_translation_server

--- French/French/Asset_Survey.pm
+++ French/French/Asset_Survey.pm
@@ -1,6 +1,1236 @@
 package WebGUI::i18n::French::Asset_Survey;
 
 our $I18N = {
+	'duration.minutes' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'84 description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'32' => {
+		message => q|Faux|,
+		lastUpdated => 1205010729
+	},
+
+	'questions.correct.count' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'90' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'71' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'102' => {
+		message => q|Nom de la section|,
+		lastUpdated => 1205009027
+	},
+
+	'85 description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'export.questions.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'user.isFirstResponse' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'questionsPerPage' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'response.user.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'18' => {
+		message => q|Editer r&eacute;ponse|,
+		lastUpdated => 1205010040
+	},
+
+	'question.count' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'question.comment.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'13 description' => {
+		message => q|Qui peux voir les r&eacute;sultats du test ?|,
+		lastUpdated => 1205009791
+	},
+
+	'16' => {
+		message => q|M&eacute;langer les r&eacute;ponses?|,
+		lastUpdated => 1205009946
+	},
+
+	'groupToViewReports' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'44' => {
+		message => q|Etes vous sure de vouloir supprimer cette question, ses r&eacute;ponses et ses r&eacute;sultats?|,
+		lastUpdated => 1205011704
+	},
+
+	'export.responses.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'55' => {
+		message => q|Voir les r&eacute;ponses.|,
+		lastUpdated => 1205011974
+	},
+
+	'84' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'27' => {
+		message => q|Ajouter une r&eacute;ponse d'&eacute;chelle d'opinion (d'accord/pas d'accord).|,
+		lastUpdated => 1205010646
+	},
+
+	'form.header' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'57' => {
+		message => q|Commentaires|,
+		lastUpdated => 1205012032
+	},
+
+	'start.newresponse.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'question.comment' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'744' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'questions.correct.percent.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'20' => {
+		message => q|Cette r&eacute;ponse est-elle correcte?|,
+		lastUpdated => 1205010199
+	},
+
+	'cannot delete the last answer' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'user.canRespondAgain' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'answer.comment' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'title' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'31' => {
+		message => q|Vrai|,
+		lastUpdated => 1205010721
+	},
+
+	'question.allowComment' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'35' => {
+		message => q|Presque d'accord|,
+		lastUpdated => 1205011484
+	},
+
+	'duration.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'11' => {
+		message => q|Mode|,
+		lastUpdated => 1205009663
+	},
+
+	'question.edit.id' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'20 description' => {
+		message => q|Si vous avez r&eacute;gl&eacute; le test en mode Quiz, alors vous pouvez d&eacute;finir si cette r&eacute;ponse est juste ou non. |,
+		lastUpdated => 1205010291
+	},
+
+	'response.count.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'78' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'export.composite.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'report.gradebook.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'thanks.quiz.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'106' => {
+		message => q|Section|,
+		lastUpdated => 1205009112
+	},
+
+	'65' => {
+		message => q|Exporter un r&eacute;sum&eacute; sommaire.|,
+		lastUpdated => 1205012230
+	},
+
+	'29' => {
+		message => q|Ajouter une r&eacute;ponse texte.|,
+		lastUpdated => 1205010686
+	},
+
+	'duration.seconds.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'response.user.name' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'maxResponsesPerUser' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'user.canTakeSurvey' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'form.submit' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'58' => {
+		message => q|Vue d'ensemble statistique|,
+		lastUpdated => 1205012051
+	},
+
+	'responseTemplateId' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'15' => {
+		message => q|Autoriser les commentaires?|,
+		lastUpdated => 1205009868
+	},
+
+	'81' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'60' => {
+		message => q|Retour au questionnaire.|,
+		lastUpdated => 1205012109
+	},
+
+	'1091' => {
+		message => q|Gabarit de la vue d'ensemble statistique|,
+		lastUpdated => 1205009654
+	},
+
+	'73' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'101' => {
+		message => q|Ajouter une r&eacute;ponse zone de texte|,
+		lastUpdated => 1205009001
+	},
+
+	'76' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'86' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'62' => {
+		message => q|Exporter les r&eacute;ponses.|,
+		lastUpdated => 1205012141
+	},
+
+	'quiz.noprivs.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'answer.response.percent' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'67' => {
+		message => q|Utilisateur|,
+		lastUpdated => 1205012259
+	},
+
+	'17' => {
+		message => q|Question, Ajout/Edition|,
+		lastUpdated => 1205010025
+	},
+
+	'2' => {
+		message => q|Editer le test|,
+		lastUpdated => 1205010144
+	},
+
+	'questions.correct.count.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'question.question' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'start.date.epoch' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'82' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'questions.total' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'survey.noprivs.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'show.comments.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'export.answers.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'question_loop' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'gradebook template' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'14' => {
+		message => q|Question|,
+		lastUpdated => 1205009810
+	},
+
+	'question.add.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'69' => {
+		message => q|Supprimer cette r&eacute;ponse.|,
+		lastUpdated => 1205012301
+	},
+
+	'report response.count.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'duration.minutes.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'49' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'24' => {
+		message => q|Ajouter une r&eacute;ponse &agrave; choix multiple.|,
+		lastUpdated => 1205010501
+	},
+
+	'questions.correct.percent' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'view template description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'question.edit.question' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'user.canViewReports' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'104' => {
+		message => q|Ajouter une nouvelle section|,
+		lastUpdated => 1205009072
+	},
+
+	'15 description' => {
+		message => q|Si positionn&eacute; &agrave; Oui, l'utilisateur pourra ajouter un commentaire &agrave; sa r&eacute;ponse.|,
+		lastUpdated => 1205009914
+	},
+
+	'question.response' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'view template' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'export.questions.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'what next question description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'end.date.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'79' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'response template' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'23' => {
+		message => q|Ajouter une nouvelle r&eacute;ponse.|,
+		lastUpdated => 1205010472
+	},
+
+	'delete.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'response.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'templateId' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'14 description' => {
+		message => q|C'est une question qui sera pos&eacute;e &agrave; l'utilisateur.|,
+		lastUpdated => 1205009846
+	},
+
+	'form.footer' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'questions.sofar.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'response.count' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'21 description' => {
+		message => q|Utilis&eacute; pour d&eacute;finir la question qui suit celle-ci quand l'ordre des questions pour le test est r&eacute;gl&eacute; &agrave; &quot;response&quot; (r&eacute;ponse).|,
+		lastUpdated => 1205010435
+	},
+
+	'answer' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'47' => {
+		message => q|Vous avez termin&eacute; ce Quiz.|,
+		lastUpdated => 1205011826
+	},
+
+	'8' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'section.edit.sectionName' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'start.newresponse.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'1087' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'37' => {
+		message => q|Pas d'accord|,
+		lastUpdated => 1205011515
+	},
+
+	'43' => {
+		message => q|Jamais|,
+		lastUpdated => 1205011620
+	},
+
+	'5' => {
+		message => q|S&eacute;quentiel|,
+		lastUpdated => 1205011900
+	},
+
+	'33' => {
+		message => q|Tout &agrave; fait d'accord|,
+		lastUpdated => 1205010898
+	},
+
+	'21' => {
+		message => q|Aller &agrave;|,
+		lastUpdated => 1205010301
+	},
+
+	'63' => {
+		message => q|Exporter les questions.|,
+		lastUpdated => 1205012155
+	},
+
+	'mode' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'7' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'26' => {
+		message => q|Ajouter une r&eacute;ponse d'&eacute;chelle de fr&eacute;quence (jamais/toujours).|,
+		lastUpdated => 1205010603
+	},
+
+	'80' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'end.date.epoch' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'delete.all.responses.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'response.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'report.overview.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'answer_loop' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'72' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'106 description' => {
+		message => q|A quelle section cette question doit-&ecirc;tre ajout&eacute;e?|,
+		lastUpdated => 1205009140
+	},
+
+	'question.id' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'question.edit.controls' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'comment.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'74' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'question.add.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'delete.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'response.id' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'61' => {
+		message => q|Voir les r&eacute;sultats.|,
+		lastUpdated => 1205012128
+	},
+
+	'end.date.human' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'103' => {
+		message => q|Section Test, Ajout/Edition|,
+		lastUpdated => 1205009056
+	},
+
+	'83 description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'survey.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'groupToTakeSurvey' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'start.time.human' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'11 description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'survey asset template variables title' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'19 description' => {
+		message => q|La r&eacute;ponse &agrave; cette question.|,
+		lastUpdated => 1205010082
+	},
+
+	'10' => {
+		message => q|Quiz|,
+		lastUpdated => 1205008899
+	},
+
+	'Survey_id' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'response_loop' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'question.comment.field' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'answer.isCorrect' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'overview template description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'response template description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'section.add.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'107' => {
+		message => q|Aucune|,
+		lastUpdated => 1205009152
+	},
+
+	'48' => {
+		message => q|Vous ne pouvez pas participer &agrave; ce questionnaire.|,
+		lastUpdated => 1205011874
+	},
+
+	'87' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'77' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'mode.isSurvey' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'16 description' => {
+		message => q|Si positionn&eacute; &agrave; Oui, les r&eacute;ponses seront m&eacute;lang&eacute;es pour chaque utilisateur.|,
+		lastUpdated => 1205009995
+	},
+
+	'comment_loop' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'question.answer' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'questionOrder' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'50' => {
+		message => q|Suivant|,
+		lastUpdated => 1205011913
+	},
+
+	'39' => {
+		message => q|Pas d'opinion|,
+		lastUpdated => 1205011541
+	},
+
+	'12 description' => {
+		message => q|Quels utilisateurs peuvent participer au test ?|,
+		lastUpdated => 1205009750
+	},
+
+	'64' => {
+		message => q|Exporter les r&eacute;sultats.|,
+		lastUpdated => 1205012201
+	},
+
+	'question.isCorrect' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'1089' => {
+		message => q|Gabarit de r&eacute;ponse|,
+		lastUpdated => 1205009579
+	},
+
+	'12' => {
+		message => q|Qui peut participer au test?|,
+		lastUpdated => 1205009724
+	},
+
+	'41' => {
+		message => q|Souvent|,
+		lastUpdated => 1205011559
+	},
+
+	'defaultSectionId' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'thanks.survey.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'52' => {
+		message => q|Score|,
+		lastUpdated => 1205011937
+	},
+
+	'8 description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'answer.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'56' => {
+		message => q|Voir les commentaires.|,
+		lastUpdated => 1205012018
+	},
+
+	'what next answer description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'45' => {
+		message => q|Non, j'ai fait une erreur.|,
+		lastUpdated => 1205011726
+	},
+
+	'66' => {
+		message => q|R&eacute;sultats|,
+		lastUpdated => 1205012241
+	},
+
+	'19' => {
+		message => q|R&eacute;ponse|,
+		lastUpdated => 1205010049
+	},
+
+	'export.answers.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'54' => {
+		message => q|Pourcentage|,
+		lastUpdated => 1205011955
+	},
+
+	'question.isRadioList' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'questions.sofar.count' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'question.response.total' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'export.responses.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'70' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'assetName' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'section.add.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'response.count.correct' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'section.questions_loop' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'report.gradebook.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'88' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'question.answer.field' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'end.time.human' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'30' => {
+		message => q|Ajouter une nouvelle question.|,
+		lastUpdated => 1205010699
+	},
+
+	'overviewTemplateId' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'100' => {
+		message => q|Ajouter une r&eacute;ponse HTML Area (texte)|,
+		lastUpdated => 1205008966
+	},
+
+	'25' => {
+		message => q|&nbsp;Ajouter une r&eacute;ponse Oui/Non.|,
+		lastUpdated => 1205010521
+	},
+
+	'81 description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'28' => {
+		message => q|Ajouter une question.|,
+		lastUpdated => 1205010658
+	},
+
+	'duration.seconds' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'40' => {
+		message => q|Toujours|,
+		lastUpdated => 1205011550
+	},
+
+	'83' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'75' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'anonymous' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'answer.response.count' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'report question_loop' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'response.percent' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'overview template' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'59' => {
+		message => q|Vue d'ensemble statistique.|,
+		lastUpdated => 1205012075
+	},
+
+	'gradebook template description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'53' => {
+		message => q|R&eacute;ponses|,
+		lastUpdated => 1205011945
+	},
+
+	'survey.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'delete.all.responses.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'22' => {
+		message => q|Type de r&eacute;ponse|,
+		lastUpdated => 1205010455
+	},
+
+	'42' => {
+		message => q|Occasionnellement|,
+		lastUpdated => 1205011611
+	},
+
+	'46' => {
+		message => q|Merci de prendre le temps de compl&eacute;ter le questionnaire.|,
+		lastUpdated => 1205011796
+	},
+
+	'section.edit.id' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'start.date.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'13' => {
+		message => q|Qui peut voir les &eacute;tats ?|,
+		lastUpdated => 1205009766
+	},
+
+	'section.edit_loop' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'105' => {
+		message => q|Etes vous sure de vouloir supprimer cette section?|,
+		lastUpdated => 1205009102
+	},
+
+	'6' => {
+		message => q|Au hasard|,
+		lastUpdated => 1205012093
+	},
+
+	'section.edit.controls' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'report.overview.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'report answer.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'response.percent.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'85' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'start.date.human' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'36' => {
+		message => q|Presque pas d'accord|,
+		lastUpdated => 1205011505
+	},
+
+	'gradebookTemplateId' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'51' => {
+		message => q|Commentaires?|,
+		lastUpdated => 1205011928
+	},
+
+	'9' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'response.isComplete' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'question' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'what next description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'38' => {
+		message => q|Pas du tout d'accord|,
+		lastUpdated => 1205011431
+	},
+
+	'show.responses.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'745' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'34' => {
+		message => q|D'accord|,
+		lastUpdated => 1205011452
+	},
+
+	'export.composite.label' => {
+		message => q||,
+		lastUpdated => 
+	},
+
 };
 
 1;
