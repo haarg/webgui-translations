a9adb7a010b0dd1158637cc7467dd140d7b2a910
Translation Server <translation@plainblack.com>
2008-04-23 09:25:59 +0000

    update_from_translation_server

--- Swedish/Swedish/Asset_Post.pm
+++ Swedish/Swedish/Asset_Post.pm
@@ -1,6 +1,606 @@
 package WebGUI::i18n::Swedish::Asset_Post;
 
 our $I18N = {
+	'post received' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'dateUpdated' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'title.form.textarea' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'userDefinedN.form.textarea' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'523' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'content' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'relativeUrl' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'newWindow' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'synopsis.form' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'isNewPost' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'unsubscribeLinkText' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'threadId' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'isHidden' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'hasRated' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'formatted.content' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'groupIdEdit' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'visitorName.form' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'preview.title' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'meta_X_value' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'delete.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'form.header' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'name' => {
+		message => q|namn|,
+		lastUpdated => 1208942651
+	},
+
+	'new file description' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'sticky.form' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'pending' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'username' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'preview.synopsis' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'title.form' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'image.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'isNewThread' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'userDefined1' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'title' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'isImage' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'userDefinedN.form.htmlarea' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'preview.userDefinedN' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'storageId' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'user.isModerator' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'notify url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'userDefined5' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'reply.userDefinedN' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'help url.raw' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'dateSubmitted' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'attachment.form' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'reply.withQuote.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'karmaScale.form' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'views' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'attachment.icon' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'reply.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'usePreview' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'add/edit post template title' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'meta_loop' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'userProfile.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'userDefinedN.form.float' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'meta_X_form' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'notify.subscription.message' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'attachment_loop' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'userId' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'form.submit' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'archive.form' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'userDefined2' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'isEdit' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'filename' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'lock.form' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'form.preview' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'reply.synopsis' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'attachment.name' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'user.isPoster' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'user.isVisitor' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'userDefinedN.form' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'contentType.form' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'rating.value' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'assetName' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'userDefined3' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'isThread' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'875' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'edit.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'reply.content' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'synopsis' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'ownerUserId' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'extraHeadTags' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'avatar.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'isPrototype' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'field' => {
+		message => q|f&auml;lt|,
+		lastUpdated => 1208942635
+	},
+
+	'karmaIsEnabled' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'title.short' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'dateUpdated.human' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'value' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'rate.url.thumbsUp' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'archived' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'isReply' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'notification template body' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'groupIdView' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'icon' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'assetSize' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'menuTitle' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'status' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'By' => {
+		message => q|av|,
+		lastUpdated => 1208942597
+	},
+
+	'user.canEdit' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'rating' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'Edited_on' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'reply.title' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'isPackage' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'userDefinedN.form.yesNo' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'content.form' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'contentType' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'dateSubmitted.human' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'thumbnail' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'form.footer' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'userDefined4' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'preview.content' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'subscribe.form' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'post template variables title' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'unsubscribeUrl' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'post asset variables title' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'notification template title' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'approved' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'rate.url.thumbsDown' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'attachment.url' => {
+		message => q||,
+		lastUpdated => 
+	},
+
+	'image.thumbnail' => {
+		message => q||,
+		lastUpdated => 
+	},
+
 };
 
 1;
