d1b0dae5b6bb7ca7bd2bfe83bd3256f9ae5570cd
Translation Server <translation@plainblack.com>
2008-07-08 09:24:17 +0000

    Update from translation server

--- German/German/VersionTag.pm
+++ German/German/VersionTag.pm
@@ -1,297 +1,243 @@
 package WebGUI::i18n::German::VersionTag;
 use utf8;
-
 our $I18N = {
-	'approval message help' => {
-		message => q|Geben sie eine Nachricht ein, die an den Freigebenden mitsamt dem Freigabelink und den Tag Daten gesendet wird. |,
-		lastUpdated => 1179997362
-	},
-
-	'manage pending versions' => {
-		message => q|Verwalte schwebende Versionen.|,
-		lastUpdated => 1179996466
-	},
-
-	'approve/deny' => {
-		message => q|Freigeben/Ablehnen|,
-		lastUpdated => 1179996644
-	},
-
-	'comments' => {
-		message => q|Kommentare|,
-		lastUpdated => 1166615325
-	},
-
-	'who to notify help' => {
-		message => q|Benachrichtige Person, die Tag erzeugt, committet oder die Leute, die unter diesem Tag arbeiten durften. |,
-		lastUpdated => 1166618065
-	},
-
-	'notify message help' => {
-		message => q|Geben sie eine Nachricht ein, die mit den Tag Daten gesendet wird. |,
-		lastUpdated => 1166617334
-	},
-
-	'set tag' => {
-		message => q|Mach mich zum aktuellen Tag |,
-		lastUpdated => 1166617668
-	},
-
-	'committed on' => {
-		message => q|Committed on|,
-		lastUpdated => 1179996289
-	},
-
-	'created on' => {
-		message => q|Erzeugt am|,
-		lastUpdated => 1179996367
-	},
-
-	'revisions in tag' => {
-		message => q|Revisionen im Tag|,
-		lastUpdated => 1179996510
-	},
-
-	'committed by' => {
-		message => q|Committed von|,
-		lastUpdated => 1179996283
-	},
-
-	'do on deny' => {
-		message => q|Bei Ablehnung ausf&uuml;hren|,
-		lastUpdated => 1179996703
-	},
-
-	'commit version tag' => {
-		message => q|Commit Version Tag|,
-		lastUpdated => 1179996256
-	},
-
-	'version tag name description' => {
-		message => q|Geben sie einen Namen f&uuml;r diesen Version Tag ein. Wichtig, um ihn sp&auml;ter referenzieren zu k&ouml;nnen, falls sie einen Commit, Rollback oder weitere &Auml;nderungen machen wollen. |,
-		lastUpdated => 1166617932
-	},
-
-	'deny' => {
-		message => q|Ablehnen |,
-		lastUpdated => 1166616218
-	},
-
-	'committed versions' => {
-		message => q|Committed Versionen|,
-		lastUpdated => 1179996296
-	},
-
-	'rollback version tag confirm' => {
-		message => q|Sind sie sich sicher, da&szlig; sie diesen Version Tag und seine Inhalt l&ouml;schen wollen? Dieser kann nach dem L&ouml;schen nicht wieder hergestellt werden. |,
-		lastUpdated => 1166617530
-	},
-
-	'tag creator' => {
-		message => q|Tag Ersteller|,
-		lastUpdated => 1179996529
-	},
-
-	'created by' => {
-		message => q|Erzeugt von|,
-		lastUpdated => 1179996361
-	},
-
-	'bad commit' => {
-		message => q|Während der Bearbeitung des Inhalts trat ein Problem auf. Setzen Sie sich bitte mit Ihrem Systemverwalter in Verbindung.|,
-		lastUpdated => 1192966747
-	},
-
-	'workflow' => {
-		message => q|Workflow|,
-		lastUpdated => 1157929028
-	},
-
-	'commit' => {
-		message => q|Commit|,
-		lastUpdated => 1166615513
-	},
-
-	'comments help' => {
-		message => q|F&uuml;gen sie Kommentare und Feedback zu diesem Tag hinzu. Diese sind f&uuml;r alle sichtbar, die diesen Tag w&auml;hrend der Ver&ouml;ffentlichung und dem Commit sowie zuk&uuml;nftig ansehen. |,
-		lastUpdated => 1166615504
-	},
-
-	'who to notify' => {
-		message => q|Wen benachrichtigen? |,
-		lastUpdated => 1166617990
-	},
-
-	'add a version tag' => {
-		message => q|Version Tag hinzuf&uuml;gen.|,
-		lastUpdated => 1179996262
-	},
-
-	'approve/deny help' => {
-		message => q|Wollen sie den Tag freigeben oder ablehnen?|,
-		lastUpdated => 1179996658
-	},
-
-	'version tag name' => {
-		message => q|Name des Version Tag|,
-		lastUpdated => 1179996574
-	},
-
-	'edit version tag' => {
-		message => q|Version Tag bearbeiten|,
-		lastUpdated => 1179996383
-	},
-
-	'tag committer' => {
-		message => q|Tag Committer|,
-		lastUpdated => 1179996523
-	},
-
-	'version tag name description commit' => {
-		message => q|Name des Version Tag, den sie committen wollen.|,
-		lastUpdated => 1179996584
-	},
-
-	'approval message' => {
-		message => q|Nachricht Freigabe|,
-		lastUpdated => 1179996604
-	},
-
-	'version tags' => {
-		message => q|Version Tags|,
-		lastUpdated => 1179996590
-	},
-
-	'topicName' => {
-		message => q|Versionskontrolle|,
-		lastUpdated => 1166617739
-	},
-
-	'approve' => {
-		message => q|Freigeben|,
-		lastUpdated => 1179996637
-	},
-
-	'notify about version tag' => {
-		message => q|Benachrichtige &uuml;ber Version Tag|,
-		lastUpdated => 1166617274
-	},
-
-	'rollback' => {
-		message => q|Rollback|,
-		lastUpdated => 1166617544
-	},
-
-	'request approval for version tag' => {
-		message => q|Beantrage Freigabe f&uuml;r Version Tag|,
-		lastUpdated => 1179996503
-	},
-
-	'rollback version tag' => {
-		message => q|Rollback Version Tag|,
-		lastUpdated => 1179996516
-	},
-
-	'commit accepted' => {
-		message => q|Ihr Tag wurde zur Ausf&uuml;hrung &uuml;bergeben. Es kann eine Zeit dauern bevor ihre &Auml;nderungen wirksam werden. Wohin wollen sie als n&auml;chstes? |,
-		lastUpdated => 1166615018
-	},
-
-	'group to approve' => {
-		message => q|Gruppe f&uuml;r Best&auml;tigung|,
-		lastUpdated => 1179996407
-	},
-
-	'pending versions' => {
-		message => q|Schwebende Versionen|,
-		lastUpdated => 1179996487
-	},
-
-	'back to home' => {
-		message => q|Zur&uuml;ck nach Home.|,
-		lastUpdated => 1179996273
-	},
-
-	'trash version tag' => {
-		message => q|L&ouml;sche Version Tag|,
-		lastUpdated => 1179996557
-	},
-
-	'content versioning' => {
-		message => q|Inhalt Versionsverwaltung|,
-		lastUpdated => 1179996350
-	},
-
-	'export version tag to html' => {
-		message => q|Export Version Tag nach HTML|,
-		lastUpdated => 1179996391
-	},
-
-	'unlock version tag' => {
-		message => q|Entsperre Version Tag|,
-		lastUpdated => 1179996566
-	},
-
-	'manage committed versions' => {
-		message => q|Verwalte commited Versionen|,
-		lastUpdated => 1179996460
-	},
-
-	'do on deny help' => {
-		message => q|Welcher Workflow soll ausgef&uuml;hrt werden, falls der Tag abgelehnt wird?|,
-		lastUpdated => 1166618282
-	},
-
-	'group to use' => {
-		message => q|Welche Gruppe|,
-		lastUpdated => 1179996444
-	},
-
-	'commit version tag confirm' => {
-		message => q|Sind sie sich sicher, da&szlig; sie den Version Tag und seinen Inhalt ver&ouml;ffentlichen wollen? |,
-		lastUpdated => 1179997369
-	},
-
-	'current tag is called' => {
-		message => q|Sie arbeiten zur Zeit unter dem Tag&nbsp; |,
-		lastUpdated => 1166616197
-	},
-
-	'purge revision prompt' => {
-		message => q|Sind sie sich sicher, da&szlig; sie diese Revision des Assets l&ouml;schen wollen? Nach dem L&ouml;schen kann diese nicht wieder hergestellt werden.|,
-		lastUpdated => 1179997378
-	},
-
-	'group to use help' => {
-		message => q|Welche Gruppe darf diesen Tag benutzen?|,
-		lastUpdated => 1179996453
-	},
-
-	'notify message' => {
-		message => q|Benachrichtigungsnachricht |,
-		lastUpdated => 1166617300
-	},
-
-	'manage versions' => {
-		message => q|Verwalte Versionen.|,
-		lastUpdated => 1179996481
-	},
-
-	'workflow help' => {
-		message => q|W&auml;hlen einen Workflow, der den Version Tag bearbeiten soll. |,
-		lastUpdated => 1166618138
-	},
-
-	'group to approve help' => {
-		message => q|Welche Gruppe soll benachrichtigt werden und ist berechtigt den Tag zu best&auml;tigen bzw. abzulehnen? |,
-		lastUpdated => 1166616558
-	},
-
-	'comments description commit' => {
-		message => q|F&uuml;gen sie Kommentare oder Feedback zu diesem Tag hinzu. Diese sind f&uuml;r alle sichtbar, die zuk&uuml;nftig diesen Tag ansehen. |,
-		lastUpdated => 1166615405
-	},
-
-};
+  'approval message help' => {
+    'lastUpdated' => 1179997362,
+    'message' => 'Geben sie eine Nachricht ein, die an den Freigebenden mitsamt dem Freigabelink und den Tag Daten gesendet wird. '
+  },
+  'approve/deny' => {
+    'lastUpdated' => 1179996644,
+    'message' => 'Freigeben/Ablehnen'
+  },
+  'manage pending versions' => {
+    'lastUpdated' => 1179996466,
+    'message' => 'Verwalte schwebende Versionen.'
+  },
+  'comments' => {
+    'lastUpdated' => 1166615325,
+    'message' => 'Kommentare'
+  },
+  'who to notify help' => {
+    'lastUpdated' => 1166618065,
+    'message' => 'Benachrichtige Person, die Tag erzeugt, committet oder die Leute, die unter diesem Tag arbeiten durften. '
+  },
+  'notify message help' => {
+    'lastUpdated' => 1166617334,
+    'message' => 'Geben sie eine Nachricht ein, die mit den Tag Daten gesendet wird. '
+  },
+  'set tag' => {
+    'lastUpdated' => 1166617668,
+    'message' => 'Mach mich zum aktuellen Tag '
+  },
+  'committed on' => {
+    'lastUpdated' => 1179996289,
+    'message' => 'Committed on'
+  },
+  'created on' => {
+    'lastUpdated' => 1179996367,
+    'message' => 'Erzeugt am'
+  },
+  'revisions in tag' => {
+    'lastUpdated' => 1179996510,
+    'message' => 'Revisionen im Tag'
+  },
+  'committed by' => {
+    'lastUpdated' => 1179996283,
+    'message' => 'Committed von'
+  },
+  'do on deny' => {
+    'lastUpdated' => 1179996703,
+    'message' => 'Bei Ablehnung ausf&uuml;hren'
+  },
+  'commit version tag' => {
+    'lastUpdated' => 1179996256,
+    'message' => 'Commit Version Tag'
+  },
+  'version tag name description' => {
+    'lastUpdated' => 1166617932,
+    'message' => 'Geben sie einen Namen f&uuml;r diesen Version Tag ein. Wichtig, um ihn sp&auml;ter referenzieren zu k&ouml;nnen, falls sie einen Commit, Rollback oder weitere &Auml;nderungen machen wollen. '
+  },
+  'deny' => {
+    'lastUpdated' => 1166616218,
+    'message' => 'Ablehnen '
+  },
+  'rollback version tag confirm' => {
+    'lastUpdated' => 1166617530,
+    'message' => 'Sind sie sich sicher, da&szlig; sie diesen Version Tag und seine Inhalt l&ouml;schen wollen? Dieser kann nach dem L&ouml;schen nicht wieder hergestellt werden. '
+  },
+  'committed versions' => {
+    'lastUpdated' => 1179996296,
+    'message' => 'Committed Versionen'
+  },
+  'tag creator' => {
+    'lastUpdated' => 1179996529,
+    'message' => 'Tag Ersteller'
+  },
+  'bad commit' => {
+    'lastUpdated' => 1192966747,
+    'message' => "W\x{e4}hrend der Bearbeitung des Inhalts trat ein Problem auf. Setzen Sie sich bitte mit Ihrem Systemverwalter in Verbindung."
+  },
+  'created by' => {
+    'lastUpdated' => 1179996361,
+    'message' => 'Erzeugt von'
+  },
+  'commit' => {
+    'lastUpdated' => 1166615513,
+    'message' => 'Commit'
+  },
+  'workflow' => {
+    'lastUpdated' => 1157929028,
+    'message' => 'Workflow'
+  },
+  'who to notify' => {
+    'lastUpdated' => 1166617990,
+    'message' => 'Wen benachrichtigen? '
+  },
+  'comments help' => {
+    'lastUpdated' => 1166615504,
+    'message' => 'F&uuml;gen sie Kommentare und Feedback zu diesem Tag hinzu. Diese sind f&uuml;r alle sichtbar, die diesen Tag w&auml;hrend der Ver&ouml;ffentlichung und dem Commit sowie zuk&uuml;nftig ansehen. '
+  },
+  'add a version tag' => {
+    'lastUpdated' => 1179996262,
+    'message' => 'Version Tag hinzuf&uuml;gen.'
+  },
+  'approve/deny help' => {
+    'lastUpdated' => 1179996658,
+    'message' => 'Wollen sie den Tag freigeben oder ablehnen?'
+  },
+  'version tag name' => {
+    'lastUpdated' => 1179996574,
+    'message' => 'Name des Version Tag'
+  },
+  'edit version tag' => {
+    'lastUpdated' => 1179996383,
+    'message' => 'Version Tag bearbeiten'
+  },
+  'tag committer' => {
+    'lastUpdated' => 1179996523,
+    'message' => 'Tag Committer'
+  },
+  'version tag name description commit' => {
+    'lastUpdated' => 1179996584,
+    'message' => 'Name des Version Tag, den sie committen wollen.'
+  },
+  'approval message' => {
+    'lastUpdated' => 1179996604,
+    'message' => 'Nachricht Freigabe'
+  },
+  'version tags' => {
+    'lastUpdated' => 1179996590,
+    'message' => 'Version Tags'
+  },
+  'notify about version tag' => {
+    'lastUpdated' => 1166617274,
+    'message' => 'Benachrichtige &uuml;ber Version Tag'
+  },
+  'topicName' => {
+    'lastUpdated' => 1166617739,
+    'message' => 'Versionskontrolle'
+  },
+  'approve' => {
+    'lastUpdated' => 1179996637,
+    'message' => 'Freigeben'
+  },
+  'request approval for version tag' => {
+    'lastUpdated' => 1179996503,
+    'message' => 'Beantrage Freigabe f&uuml;r Version Tag'
+  },
+  'rollback' => {
+    'lastUpdated' => 1166617544,
+    'message' => 'Rollback'
+  },
+  'rollback version tag' => {
+    'lastUpdated' => 1179996516,
+    'message' => 'Rollback Version Tag'
+  },
+  'group to approve' => {
+    'lastUpdated' => 1179996407,
+    'message' => 'Gruppe f&uuml;r Best&auml;tigung'
+  },
+  'commit accepted' => {
+    'lastUpdated' => 1166615018,
+    'message' => 'Ihr Tag wurde zur Ausf&uuml;hrung &uuml;bergeben. Es kann eine Zeit dauern bevor ihre &Auml;nderungen wirksam werden. Wohin wollen sie als n&auml;chstes? '
+  },
+  'back to home' => {
+    'lastUpdated' => 1179996273,
+    'message' => 'Zur&uuml;ck nach Home.'
+  },
+  'pending versions' => {
+    'lastUpdated' => 1179996487,
+    'message' => 'Schwebende Versionen'
+  },
+  'trash version tag' => {
+    'lastUpdated' => 1179996557,
+    'message' => 'L&ouml;sche Version Tag'
+  },
+  'export version tag to html' => {
+    'lastUpdated' => 1179996391,
+    'message' => 'Export Version Tag nach HTML'
+  },
+  'content versioning' => {
+    'lastUpdated' => 1179996350,
+    'message' => 'Inhalt Versionsverwaltung'
+  },
+  'unlock version tag' => {
+    'lastUpdated' => 1179996566,
+    'message' => 'Entsperre Version Tag'
+  },
+  'do on deny help' => {
+    'lastUpdated' => 1166618282,
+    'message' => 'Welcher Workflow soll ausgef&uuml;hrt werden, falls der Tag abgelehnt wird?'
+  },
+  'manage committed versions' => {
+    'lastUpdated' => 1179996460,
+    'message' => 'Verwalte commited Versionen'
+  },
+  'group to use' => {
+    'lastUpdated' => 1179996444,
+    'message' => 'Welche Gruppe'
+  },
+  'commit version tag confirm' => {
+    'lastUpdated' => 1179997369,
+    'message' => 'Sind sie sich sicher, da&szlig; sie den Version Tag und seinen Inhalt ver&ouml;ffentlichen wollen? '
+  },
+  'current tag is called' => {
+    'lastUpdated' => 1166616197,
+    'message' => 'Sie arbeiten zur Zeit unter dem Tag&nbsp; '
+  },
+  'continue with workflow' => {
+    'lastUpdated' => 1215509035,
+    'message' => '-- Diesen Workflow fortsetzen'
+  },
+  'notify message' => {
+    'lastUpdated' => 1166617300,
+    'message' => 'Benachrichtigungsnachricht '
+  },
+  'group to use help' => {
+    'lastUpdated' => 1179996453,
+    'message' => 'Welche Gruppe darf diesen Tag benutzen?'
+  },
+  'purge revision prompt' => {
+    'lastUpdated' => 1179997378,
+    'message' => 'Sind sie sich sicher, da&szlig; sie diese Revision des Assets l&ouml;schen wollen? Nach dem L&ouml;schen kann diese nicht wieder hergestellt werden.'
+  },
+  'manage versions' => {
+    'lastUpdated' => 1179996481,
+    'message' => 'Verwalte Versionen.'
+  },
+  'workflow help' => {
+    'lastUpdated' => 1166618138,
+    'message' => 'W&auml;hlen einen Workflow, der den Version Tag bearbeiten soll. '
+  },
+  'group to approve help' => {
+    'lastUpdated' => 1166616558,
+    'message' => 'Welche Gruppe soll benachrichtigt werden und ist berechtigt den Tag zu best&auml;tigen bzw. abzulehnen? '
+  },
+  'comments description commit' => {
+    'lastUpdated' => 1166615405,
+    'message' => 'F&uuml;gen sie Kommentare oder Feedback zu diesem Tag hinzu. Diese sind f&uuml;r alle sichtbar, die zuk&uuml;nftig diesen Tag ansehen. '
+  }
+}
+;
 
 1;
