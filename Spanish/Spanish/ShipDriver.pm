e1262d01a037c92f06b854384fb23fd27dc7c003
Translation Server <translation@plainblack.com>
2008-07-16 21:50:49 +0000

    Update from translation server

--- Spanish/Spanish/ShipDriver.pm
+++ Spanish/Spanish/ShipDriver.pm
@@ -1,7 +1,23 @@
 package WebGUI::i18n::Spanish::ShipDriver;
 use utf8;
-
 our $I18N = {
-};
+  'enabled help' => {
+    'lastUpdated' => 1216244994,
+    'message' => 'Las personas que usan comercio en tu sitio tendr&aacute;n habilitado este Driver de Compras?'
+  },
+  'label' => {
+    'lastUpdated' => 1216245003,
+    'message' => 'Etiqueta'
+  },
+  'label help' => {
+    'lastUpdated' => 1216245041,
+    'message' => 'Un nombre para tu Driver de Compras. Elija algo claro, facil de entender, y menos de 100 caracteres.'
+  },
+  'enabled' => {
+    'lastUpdated' => 1216244920,
+    'message' => 'Habilitado?'
+  }
+}
+;
 
 1;
