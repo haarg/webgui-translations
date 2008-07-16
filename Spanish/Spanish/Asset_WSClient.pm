65e02f0c829ff829bd6f4fdf72e6c53424c4f701
Translation Server <translation@plainblack.com>
2008-07-16 22:49:09 +0000

    Update from translation server

--- Spanish/Spanish/Asset_WSClient.pm
+++ Spanish/Spanish/Asset_WSClient.pm
@@ -1,7 +1,11 @@
 package WebGUI::i18n::Spanish::Asset_WSClient;
 use utf8;
-
 our $I18N = {
-};
+  '25' => {
+    'lastUpdated' => 1216248531,
+    'message' => 'Aqui hubo un problema con el llamado al SOAP:'
+  }
+}
+;
 
 1;
