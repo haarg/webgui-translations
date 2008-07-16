3a47f8d882927f99d2847f51d8c7c972c5dde032
Translation Server <translation@plainblack.com>
2008-07-16 22:28:47 +0000

    Update from translation server

--- Spanish/Spanish/Operation_FormHelpers.pm
+++ Spanish/Spanish/Operation_FormHelpers.pm
@@ -1,7 +1,35 @@
 package WebGUI::i18n::Spanish::Operation_FormHelpers;
 use utf8;
-
 our $I18N = {
-};
+  'Folder name' => {
+    'lastUpdated' => 1216247270,
+    'message' => 'Nombre de la Carpeta'
+  },
+  'Create new folder' => {
+    'lastUpdated' => 1216247246,
+    'message' => 'Crear nueva carpeta'
+  },
+  'Upload new image' => {
+    'lastUpdated' => 1216247289,
+    'message' => 'Subir nueva imagen'
+  },
+  'Create' => {
+    'lastUpdated' => 1216247237,
+    'message' => 'Crear'
+  },
+  'File' => {
+    'lastUpdated' => 1216247255,
+    'message' => 'Archivo'
+  },
+  'Cancel' => {
+    'lastUpdated' => 1216247231,
+    'message' => 'Cancelar'
+  },
+  'Upload' => {
+    'lastUpdated' => 1216247280,
+    'message' => 'Subir'
+  }
+}
+;
 
 1;
--- Spanish/Spanish/PayDriver_Cash.pm
+++ Spanish/Spanish/PayDriver_Cash.pm
@@ -1,7 +1,43 @@
 package WebGUI::i18n::Spanish::PayDriver_Cash;
 use utf8;
-
 our $I18N = {
-};
+  'country' => {
+    'lastUpdated' => 1216247161,
+    'message' => 'Pa&iacute;s'
+  },
+  'firstName' => {
+    'lastUpdated' => 1216247207,
+    'message' => 'Primer nombre'
+  },
+  'phone' => {
+    'lastUpdated' => 1216247172,
+    'message' => 'N&uacute;mero Telef&oacute;nico'
+  },
+  'state' => {
+    'lastUpdated' => 1216247220,
+    'message' => 'Departamento'
+  },
+  'city' => {
+    'lastUpdated' => 1216247153,
+    'message' => 'Ciudad'
+  },
+  'other' => {
+    'lastUpdated' => 1216247186,
+    'message' => 'Otro'
+  },
+  'lastName' => {
+    'lastUpdated' => 1216247194,
+    'message' => 'Apellido'
+  },
+  'label' => {
+    'lastUpdated' => 1216247138,
+    'message' => 'Efectivo'
+  },
+  'cash' => {
+    'lastUpdated' => 1216247146,
+    'message' => 'Efectivo'
+  }
+}
+;
 
 1;
