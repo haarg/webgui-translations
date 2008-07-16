a0ccd3aba92e71ee325f5225b8027516533735dc
Translation Server <translation@plainblack.com>
2008-07-16 22:00:22 +0000

    Update from translation server

--- Spanish/Spanish/Asset_Wobject.pm
+++ Spanish/Spanish/Asset_Wobject.pm
@@ -1,122 +1,99 @@
 package WebGUI::i18n::Spanish::Asset_Wobject;
 use utf8;
-
 our $I18N = {
-	'1073 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'add' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'1079' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'displayTitle' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'827' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'assetName' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'edit' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'85 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'42' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'174' => {
-		message => q|Mostrar el t&iacute;tulo?|,
-		lastUpdated => 1207601941
-	},
-
-	'assetId' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'wobject template variables title' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'44' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'1073' => {
-		message => q|Plantilla de Estilo|,
-		lastUpdated => 1207601904
-	},
-
-	'174 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'85' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'styleTemplateId' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'1079 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'45' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'originalURL' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'title' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'printableStyleTemplateId' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-};
+  '1073 description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'add' => {
+    'lastUpdated' => 1216245355,
+    'message' => 'Adicionar'
+  },
+  '1079' => {
+    'lastUpdated' => 1216245263,
+    'message' => 'Estilo Imprimible'
+  },
+  'displayTitle' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  '827' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'assetName' => {
+    'lastUpdated' => 1216245396,
+    'message' => 'Wobject'
+  },
+  'edit' => {
+    'lastUpdated' => 1216245189,
+    'message' => 'Editar'
+  },
+  '85 description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  '42' => {
+    'lastUpdated' => 1216245209,
+    'message' => 'Favor Confirmar'
+  },
+  '174' => {
+    'lastUpdated' => 1207601941,
+    'message' => 'Mostrar el t&iacute;tulo?'
+  },
+  'assetId' => {
+    'lastUpdated' => 1216245388,
+    'message' => 'El identificador &uacute;nico que usa WebGUI para controlar este activo.'
+  },
+  'wobject template variables title' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  '44' => {
+    'lastUpdated' => 1216245299,
+    'message' => 'Si, estoy seguro.'
+  },
+  '1073' => {
+    'lastUpdated' => 1207601904,
+    'message' => 'Plantilla de Estilo'
+  },
+  '174 description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  '85' => {
+    'lastUpdated' => 1216245341,
+    'message' => 'Descripci&oacute;n'
+  },
+  'styleTemplateId' => {
+    'lastUpdated' => 1216245564,
+    'message' => 'El assetId de la plantilla para mostrar el Activo.'
+  },
+  '1079 description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'description' => {
+    'lastUpdated' => 1216245412,
+    'message' => 'La descripci&oacute;n de este wobject.'
+  },
+  '45' => {
+    'lastUpdated' => 1216245311,
+    'message' => 'No, he comedito un error.'
+  },
+  'originalURL' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'title' => {
+    'lastUpdated' => 1216245111,
+    'message' => 'Un t&iacute;tulo para este wobject.'
+  },
+  'printableStyleTemplateId' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  }
+}
+;
 
 1;
