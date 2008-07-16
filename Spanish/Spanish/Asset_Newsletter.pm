1f0d3582e4fb6ea6a2903bd735aa06b6e3a82f2f
Translation Server <translation@plainblack.com>
2008-07-16 22:35:38 +0000

    Update from translation server

--- Spanish/Spanish/Asset_Newsletter.pm
+++ Spanish/Spanish/Asset_Newsletter.pm
@@ -1,7 +1,23 @@
 package WebGUI::i18n::Spanish::Asset_Newsletter;
 use utf8;
-
 our $I18N = {
-};
+  'newsletterDescription' => {
+    'lastUpdated' => 1216247732,
+    'message' => 'Lo que sea que este en el campo de descripci&oacute;n de este bolet&iacute;n'
+  },
+  'assetName' => {
+    'lastUpdated' => 1216247635,
+    'message' => 'Bolet&iacute;n'
+  },
+  'edit title' => {
+    'lastUpdated' => 1216247687,
+    'message' => 'Editar Bolet&iacute;n'
+  },
+  'newsletterTitle' => {
+    'lastUpdated' => 1216247675,
+    'message' => 'Como sea que se haya llamado este bolet&iacute;n.'
+  }
+}
+;
 
 1;
