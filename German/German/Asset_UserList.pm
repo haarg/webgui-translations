3c427f39d2c7f975901eb1fa13a7c342a78f46f9
Translation Server <translation@plainblack.com>
2008-07-28 08:47:35 +0000

    Update from translation server

--- German/German/Asset_UserList.pm
+++ German/German/Asset_UserList.pm
@@ -1,21 +1,17 @@
 package WebGUI::i18n::German::Asset_UserList;
 use utf8;
 our $I18N = {
-  'No users message' => {
-    'lastUpdated' => 1217229726,
-    'message' => 'Keine Benutzer gefunden'
+  'or label' => {
+    'lastUpdated' => 1217232695,
+    'message' => 'Oder'
   },
-  'includeInSearch_PROFILEFIELDNAME_hidden' => {
-    'lastUpdated' => 1217230435,
-    'message' => 'Ein verstecktes Formularelement, das anzeigt, ob das Profilfeld PROFILEFIELDNAME durchsucht wird. Dies hat nur eine Auswirkung, falls &quot;limitSearch tmpl_var&quot; ein Teil des Suchformulars ist.'
+  'searchFormTypeSelect label' => {
+    'lastUpdated' => 1217234112,
+    'message' => '<p>W&auml;hle Suchtyp</p>'
   },
-  'alphabetSearch_loop_hasResults' => {
-    'lastUpdated' => 1217230083,
-    'message' => 'Eine Bedingung, die wahr ist, falls die alphabetische Suche ein Ergebnis erbracht hat.'
-  },
-  'assetName' => {
-    'lastUpdated' => 1217230209,
-    'message' => 'Benutzerliste'
+  'searchFormSubmit' => {
+    'lastUpdated' => 1217233979,
+    'message' => 'Ein Eingabe Formularelement f&uuml;r das Suchformular'
   },
   'alphabet' => {
     'lastUpdated' => 1217229837,
@@ -24,6 +20,158 @@ our $I18N = {
   'Group to hide description' => {
     'lastUpdated' => 1217229633,
     'message' => 'W&auml;hle eine Gruppe, die in der Benutzerliste nicht angezeigt wird. Standardwert ist die Gruppe \'Admins\'.'
+  },
+  'showOnlyVisibleAsNamed label' => {
+    'lastUpdated' => 1217234396,
+    'message' => 'Zeige nur sichtbare Felder als benannte tmpl_vars'
+  },
+  'sort order description' => {
+    'lastUpdated' => 1217234476,
+    'message' => 'Sortiere in aufsteigender oder absteigender Reihenfolge.'
+  },
+  'search_PROFILEFIELDNAME_form' => {
+    'lastUpdated' => 1217234154,
+    'message' => 'Das Formularelement, das an dieses Feld gebunden ist um eine&nbsp;normale Suche im Profilfeld PROFILEFIELDNAME durchzuf&uuml;hren. Beispiel &lt;tmpl_var searchExact_email_form&gt;.'
+  },
+  'overridePublicEmail label' => {
+    'lastUpdated' => 1217232786,
+    'message' => '&Uuml;berschreibe Email &ouml;ffentlich'
+  },
+  'and label' => {
+    'lastUpdated' => 1217230189,
+    'message' => 'Und'
+  },
+  'overridePublicProfile description' => {
+    'lastUpdated' => 1217232842,
+    'message' => 'Falls auf Ja gesetzt, wird die Einstellung \'Mache Profil &ouml;ffentlich\' in den Benutzerprofil Einstellungen ignoriert. Die Benutzerliste zeigt das Profil jedes Benutzers immer.'
+  },
+  'Profile not public message' => {
+    'lastUpdated' => 1217229736,
+    'message' => 'Profil nicht &ouml;ffentlich'
+  },
+  'Users per page label' => {
+    'lastUpdated' => 1217229784,
+    'message' => 'Benutzer pro Seite'
+  },
+  'Users per page description' => {
+    'lastUpdated' => 1217229775,
+    'message' => 'Die Anzahl der Benutzer pro Seite'
+  },
+  'usersPerPage' => {
+    'lastUpdated' => 1217234848,
+    'message' => 'Die Anzahl der Benutzer pro Seite'
+  },
+  'profileField_sortByURL' => {
+    'lastUpdated' => 1217233614,
+    'message' => '<p>Die URL, um die Benutzerliste nach diesem Profilfeld zu sortieren. Die Standard Sortierreihenfolge ist aufsteigend. Nach dem Klick auf den sortiere nach Link f&uuml;r ein Profilfeld wird die Sortierreihenfolge umgekehrt.</p>'
+  },
+  'searchFormTypeAnd' => {
+    'lastUpdated' => 1217234011,
+    'message' => 'Ein verstecktes Formularelement, um den Suchtyp auf \'und\' zu setzen.'
+  },
+  'alphabetSearch_loop_label' => {
+    'lastUpdated' => 1217230144,
+    'message' => 'Label f&uuml;r diese alphabetische Suche. Normalerweise ein Buchstabe des Alphabets.'
+  },
+  'user_profile_PROFILEFIELDNAME_value' => {
+    'lastUpdated' => 1217234777,
+    'message' => 'Der Wert des&nbsp;Profilfelds mit dem Namen PROFILEFIELDNAME in dem Benutzerprofil des Benutzers. Beispiel &lt;tmpl_var user_profile_firstName_value&gt;'
+  },
+  'alphabet label' => {
+    'lastUpdated' => 1217229876,
+    'message' => 'Alphabet'
+  },
+  'user_id' => {
+    'lastUpdated' => 1217234562,
+    'message' => 'Die &quot;userId\' des Benutzers'
+  },
+  'profileField_PROFILEFIELDNAME_sortByURL' => {
+    'lastUpdated' => 1217233603,
+    'message' => 'Die URL, um die Benutzerliste nach PROFILEFIELDNAME zu sortieren. Beispiel &lt;tmpl_var profileField_lastName_sortByURL&gt;. Diese tmpl_var existiert ausserhalb der profileField_loop. Die Standard Sortierreihenfolge ist aufsteigend. Nach Klick auf den Sortieren Link f&uuml;r ein Profilfeld, wird die Sortierreihenfolge umgekehrt.'
+  },
+  'showGroupId' => {
+    'lastUpdated' => 1217234211,
+    'message' => 'Nur Benutzer, die in dieser Gruppe sind, werden angezeigt.'
+  },
+  'submit search label' => {
+    'lastUpdated' => 1217234483,
+    'message' => 'Suche'
+  },
+  'alphabetSearch_loop_hasResults' => {
+    'lastUpdated' => 1217230083,
+    'message' => 'Eine Bedingung, die wahr ist, falls die alphabetische Suche ein Ergebnis erbracht hat.'
+  },
+  'search_PROFILEFIELDNAME_text' => {
+    'lastUpdated' => 1217234188,
+    'message' => 'Eine Texteingabe, um eine&nbsp;normale Suche im Profilfeld PROFILEFIELDNAME durchzuf&uuml;hren. Beispiel &lt;tmpl_var searchExact_email_text&gt;.'
+  },
+  'user_loop' => {
+    'lastUpdated' => 1217234588,
+    'message' => 'Eine Schleife, die die Benuzter dieser Benutzerliste enth&auml;lt.'
+  },
+  'Edit/Add UserList' => {
+    'lastUpdated' => 1217229521,
+    'message' => 'Hinzuf&uuml;gen / Bearbeiten der Benutzerliste'
+  },
+  'user_profile_PROFILEFIELDNAME_file' => {
+    'lastUpdated' => 1217234697,
+    'message' => 'Die Datei f&uuml;r das Profilfeld mit dem Namen PROFILEFIELDNAME in dem Benutzerprofil des Benutzers. Beispiel &lt;tmpl_var user_profile_firstName_file&gt;. Diese ist verf&uuml;gbar, falls das Profilfeld ein Bild oder eine Datei ist.'
+  },
+  'UserList Template' => {
+    'lastUpdated' => 1217229760,
+    'message' => 'Template Benutzerliste'
+  },
+  'alphabetSearchField description' => {
+    'lastUpdated' => 1217229941,
+    'message' => 'W&auml;hle ein Profilfeld, f&uuml;r das die alphabetische Suche durchgef&uuml;hrt wird.'
+  },
+  'overridePublicProfile label' => {
+    'lastUpdated' => 1217232859,
+    'message' => '<p>&Uuml;berschreibe Profil &ouml;ffentlich</p>'
+  },
+  'showOnlyVisibleAsNamed' => {
+    'lastUpdated' => 1217234374,
+    'message' => 'Wahrheitswert. Falls wahr, werden nur Felder, die auf \'sichtbar\' in den Benutzerprofileinstellungen gesetzt sind, verf&uuml;gbar als benannte tmpl_vars'
+  },
+  'searchFormTypeSelect' => {
+    'lastUpdated' => 1217234085,
+    'message' => 'Eine Auswahlbox, f&uuml;r die Wahl des Suchtyps.'
+  },
+  'profileField_loop' => {
+    'lastUpdated' => 1217233375,
+    'message' => 'Eine Schleife, die Profilfelder enth&auml;lt'
+  },
+  'template description' => {
+    'lastUpdated' => 1217234499,
+    'message' => 'W&auml;hle ein Template f&uuml;r diese Benutzerliste.'
+  },
+  'descending' => {
+    'lastUpdated' => 1217230217,
+    'message' => 'Absteigend'
+  },
+  'search in label' => {
+    'lastUpdated' => 1217233770,
+    'message' => 'Suche in:'
+  },
+  'includeInSearch_PROFILEFIELDNAME_hidden' => {
+    'lastUpdated' => 1217230435,
+    'message' => 'Ein verstecktes Formularelement, das anzeigt, ob das Profilfeld PROFILEFIELDNAME durchsucht wird. Dies hat nur eine Auswirkung, falls &quot;limitSearch tmpl_var&quot; ein Teil des Suchformulars ist.'
+  },
+  'assetName' => {
+    'lastUpdated' => 1217230209,
+    'message' => 'Benutzerliste'
+  },
+  'searchFormQuery_form' => {
+    'lastUpdated' => 1217233950,
+    'message' => 'Eine Texteingab f&uuml;r die Suchanfrage.'
+  },
+  'query label' => {
+    'lastUpdated' => 1217233762,
+    'message' => '<p>Suchanfrage</p>'
+  },
+  'searchExact_PROFILEFIELDNAME_form' => {
+    'lastUpdated' => 1217233841,
+    'message' => 'Das Formularelement, das an dieses Feld gebunden ist um eine exakte Suche im Profilfeld PROFILEFIELDNAME durchzuf&uuml;hren. Beispiel &lt;tmpl_var searchExact_email_form&gt;.'
   },
   'Group to show label' => {
     'lastUpdated' => 1217229708,
@@ -37,41 +185,25 @@ our $I18N = {
     'lastUpdated' => 1217229751,
     'message' => 'Hinzuf&uuml;gen / Bearbeiten Benutzerliste'
   },
-  'and label' => {
-    'lastUpdated' => 1217230189,
-    'message' => 'Und'
+  'sort order' => {
+    'lastUpdated' => 1217234458,
+    'message' => 'Sortierreihenfolge'
   },
-  'Group to hide label' => {
-    'lastUpdated' => 1217229656,
-    'message' => 'Nicht anzuzeigende Gruppe'
+  'user_profile_loop' => {
+    'lastUpdated' => 1217234825,
+    'message' => 'Eine Schleife, die die Profilfelder des Benutzers enth&auml;lt.'
   },
-  'alphabetSearch_loop' => {
-    'lastUpdated' => 1217230051,
-    'message' => 'Eine Schleife, die die Elemente f&uuml;r eine alphabetische Suche enth&auml;lt.'
+  'profile_value' => {
+    'lastUpdated' => 1217233734,
+    'message' => 'Der Wert des Profilfeldes dieses Benutzers.'
   },
-  'Edit/Add UserList' => {
-    'lastUpdated' => 1217229521,
-    'message' => 'Hinzuf&uuml;gen / Bearbeiten der Benutzerliste'
+  'sort by' => {
+    'lastUpdated' => 1217234404,
+    'message' => 'Sortiere nach'
   },
-  'Users per page label' => {
-    'lastUpdated' => 1217229784,
-    'message' => 'Benutzer pro Seite'
-  },
-  'Profile not public message' => {
-    'lastUpdated' => 1217229736,
-    'message' => 'Profil nicht &ouml;ffentlich'
-  },
-  'Group to show description' => {
-    'lastUpdated' => 1217229691,
-    'message' => 'Nur Benutzer aus dieser Gruppe werden in der Benutzerliste angezeigt. Standardwert ist die Gruppe \'Everyone\'.'
-  },
-  'Users per page description' => {
-    'lastUpdated' => 1217229775,
-    'message' => 'Die Anzahl der Benutzer pro Seite'
-  },
-  'alphabetSearchField label' => {
-    'lastUpdated' => 1217229979,
-    'message' => 'Feld f&uuml;r alphabetische Suche'
+  'showOnlyVisibleAsNamed description' => {
+    'lastUpdated' => 1217234364,
+    'message' => 'Falls auf Ja gesetzt,&nbsp;werden nur Felder, die auf \'sichtbar\' in den Benutzerprofileinstellungen gesetzt sind, verf&uuml;gbar als benannte tmpl_vars'
   },
   'alphabetSearchField' => {
     'lastUpdated' => 1217229903,
@@ -81,33 +213,25 @@ our $I18N = {
     'lastUpdated' => 1217230202,
     'message' => 'Aufsteigend'
   },
-  'includeInSearch_PROFILEFIELDNAME_checkBox' => {
-    'lastUpdated' => 1217230394,
-    'message' => 'Eine CheckBox, die der Benutzer benutzen kann, um zu bestimmen, ob das Profilfeld PROFILEFIELDNAME durchsucht wird oder nicht. Dies hat nur eine Auswirkung, falls &quot;limitSearch tmpl_var&quot; ein Teil des Suchformulars ist.'
+  'profile_emailNotPublic' => {
+    'lastUpdated' => 1217233660,
+    'message' => 'Eine Bedingung, die wahr ist, falls die Email Adresse des Benutzers nicht &ouml;ffentlich ist. Ist nur wahr f&uuml;r das \'email\' Profilfeld.'
   },
-  'alphabetSearch_loop_label' => {
-    'lastUpdated' => 1217230144,
-    'message' => 'Label f&uuml;r diese alphabetische Suche. Normalerweise ein Buchstabe des Alphabets.'
+  'user_name' => {
+    'lastUpdated' => 1217234602,
+    'message' => 'Der Benutzername des Benutzers.'
   },
-  'UserList Template' => {
-    'lastUpdated' => 1217229760,
-    'message' => 'Template Benutzerliste'
+  'limitSearch' => {
+    'lastUpdated' => 1217231419,
+    'message' => 'Verstecktes Formularelement, das angibt, das die&nbsp;Suche auf bestimmte Profilfelder begrenzt ist. Benutze includeInSearch_PROFILEFIELDNAME_hidden oder includeInSearch_PROFILEFIELDNAME_checkBox tmpl_vars, um die Felder, auf die die Suche begrenzt ist, auszuw&auml;hlen.'
   },
-  'alphabetSearchField description' => {
-    'lastUpdated' => 1217229941,
-    'message' => 'W&auml;hle ein Profilfeld, f&uuml;r das die alphabetische Suche durchgef&uuml;hrt wird.'
+  'profile_file' => {
+    'lastUpdated' => 1217233716,
+    'message' => 'Die Datei f&uuml;r dieses Profilfeld. Nur verf&uuml;gbar, falls das Profilfeld ein Bild oder eine Datei ist.'
   },
-  'alphabet label' => {
-    'lastUpdated' => 1217229876,
-    'message' => 'Alphabet'
-  },
-  'Email not public message' => {
-    'lastUpdated' => 1217229538,
-    'message' => 'Email nicht &ouml;ffentlich'
-  },
-  'alphabet description' => {
-    'lastUpdated' => 1217229863,
-    'message' => 'Alphabet, das f&uuml;r die alphabetische Suche benutzt wird. Dies ist eine Zeichenkette bestehend aus komma separierten Werten'
+  'searchFormTypeOr' => {
+    'lastUpdated' => 1217234034,
+    'message' => 'Ein verstecktes Formularelement, um den Suchtyp auf \'oder\' zu setzen.'
   },
   'hideGroupId' => {
     'lastUpdated' => 1217230234,
@@ -117,9 +241,85 @@ our $I18N = {
     'lastUpdated' => 1217230182,
     'message' => 'Die Url dieser alphabetischen Suche.'
   },
-  'descending' => {
-    'lastUpdated' => 1217230217,
-    'message' => 'Absteigend'
+  'username label' => {
+    'lastUpdated' => 1217234833,
+    'message' => 'Benutzername'
+  },
+  'template label' => {
+    'lastUpdated' => 1217234510,
+    'message' => 'Benutzerliste Template'
+  },
+  'No users message' => {
+    'lastUpdated' => 1217229726,
+    'message' => 'Keine Benutzer gefunden'
+  },
+  'profileField_PROFILEFIELDNAME_label' => {
+    'lastUpdated' => 1217232922,
+    'message' => 'Das Label f&uuml;r das Profilfeld mit dem Feldnamen PROFILEFIELDNAME. Beispiel &lt;tmpl_var profileField_lastName_label&gt;. Diese tmpl_var existiert ausserhalb der profileField_loop.'
+  },
+  'user_profile_emailNotPublic' => {
+    'lastUpdated' => 1217234805,
+    'message' => 'Eine Bedingung, die wahr ist, falls die Email Adresse des Benutzers nicht &ouml;ffentlich ist.'
+  },
+  'sort by description' => {
+    'lastUpdated' => 1217234447,
+    'message' => 'Standardm&auml;&szlig;ig werden alle Benutzer in sortierter Reihenfolge angezeigt. Benutze dieses Feld, um auszuw&auml;hlen nach welchem Feld sortiert werden soll.'
+  },
+  'numberOfProfileFields' => {
+    'lastUpdated' => 1217232688,
+    'message' => 'Die Anzahl der Profilfelder.'
+  },
+  'Group to hide label' => {
+    'lastUpdated' => 1217229656,
+    'message' => 'Nicht anzuzeigende Gruppe'
+  },
+  'alphabetSearch_loop' => {
+    'lastUpdated' => 1217230051,
+    'message' => 'Eine Schleife, die die Elemente f&uuml;r eine alphabetische Suche enth&auml;lt.'
+  },
+  'profileField_label' => {
+    'lastUpdated' => 1217233348,
+    'message' => '<p>Das Label f&uuml;r dieses Profilfeld</p>'
+  },
+  'templateId' => {
+    'lastUpdated' => 1217234543,
+    'message' => 'Die ID des Templates, das f&uuml;r die Anzeige des Benutzerliste Assets benutzt wird.'
+  },
+  'Group to show description' => {
+    'lastUpdated' => 1217229691,
+    'message' => 'Nur Benutzer aus dieser Gruppe werden in der Benutzerliste angezeigt. Standardwert ist die Gruppe \'Everyone\'.'
+  },
+  'alphabetSearchField label' => {
+    'lastUpdated' => 1217229979,
+    'message' => 'Feld f&uuml;r alphabetische Suche'
+  },
+  'overridePublicEmail description' => {
+    'lastUpdated' => 1217232762,
+    'message' => 'Falls auf Ja gesetzt, wird die Einstellung \'Mache Email Adresse &ouml;ffentlich\' in den Profil Einstellungen ignoriert. Die Benutzerliste zeigt die Email Adressen immer.'
+  },
+  'searchFormHeader' => {
+    'lastUpdated' => 1217233926,
+    'message' => 'Das Anfang Tag des Suchformulars.'
+  },
+  'includeInSearch_PROFILEFIELDNAME_checkBox' => {
+    'lastUpdated' => 1217230394,
+    'message' => 'Eine CheckBox, die der Benutzer benutzen kann, um zu bestimmen, ob das Profilfeld PROFILEFIELDNAME durchsucht wird oder nicht. Dies hat nur eine Auswirkung, falls &quot;limitSearch tmpl_var&quot; ein Teil des Suchformulars ist.'
+  },
+  'searchExact_PROFILEFIELDNAME_text' => {
+    'lastUpdated' => 1217233887,
+    'message' => 'Eine Texteingabe, um eine exakte Suche im Profilfeld PROFILEFIELDNAME durchzuf&uuml;hren. Beispiel &lt;tmpl_var searchExact_email_text&gt;.'
+  },
+  'Email not public message' => {
+    'lastUpdated' => 1217229538,
+    'message' => 'Email nicht &ouml;ffentlich'
+  },
+  'searchFormFooter' => {
+    'lastUpdated' => 1217233933,
+    'message' => 'Das Ende Tag des Sucheformulars.'
+  },
+  'alphabet description' => {
+    'lastUpdated' => 1217229863,
+    'message' => 'Alphabet, das f&uuml;r die alphabetische Suche benutzt wird. Dies ist eine Zeichenkette bestehend aus komma separierten Werten'
   }
 }
 ;
