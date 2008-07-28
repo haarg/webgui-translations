d57fe7e04ce58483cd37885140765d54a73728e6
Translation Server <translation@plainblack.com>
2008-07-28 07:39:19 +0000

    Update from translation server

--- German/German/Asset_UserList.pm
+++ German/German/Asset_UserList.pm
@@ -1,7 +1,127 @@
 package WebGUI::i18n::German::Asset_UserList;
 use utf8;
-
 our $I18N = {
-};
+  'No users message' => {
+    'lastUpdated' => 1217229726,
+    'message' => 'Keine Benutzer gefunden'
+  },
+  'includeInSearch_PROFILEFIELDNAME_hidden' => {
+    'lastUpdated' => 1217230435,
+    'message' => 'Ein verstecktes Formularelement, das anzeigt, ob das Profilfeld PROFILEFIELDNAME durchsucht wird. Dies hat nur eine Auswirkung, falls &quot;limitSearch tmpl_var&quot; ein Teil des Suchformulars ist.'
+  },
+  'alphabetSearch_loop_hasResults' => {
+    'lastUpdated' => 1217230083,
+    'message' => 'Eine Bedingung, die wahr ist, falls die alphabetische Suche ein Ergebnis erbracht hat.'
+  },
+  'assetName' => {
+    'lastUpdated' => 1217230209,
+    'message' => 'Benutzerliste'
+  },
+  'alphabet' => {
+    'lastUpdated' => 1217229837,
+    'message' => 'Alphabet, das f&uuml;r die alphabetische Suche benutzt wird. Dies ist eine Zeichenkette bestehend aus komma separierten Werten'
+  },
+  'Group to hide description' => {
+    'lastUpdated' => 1217229633,
+    'message' => 'W&auml;hle eine Gruppe, die in der Benutzerliste nicht angezeigt wird. Standardwert ist die Gruppe \'Admins\'.'
+  },
+  'Group to show label' => {
+    'lastUpdated' => 1217229708,
+    'message' => 'Anzuzeigende Gruppe'
+  },
+  'id label' => {
+    'lastUpdated' => 1217230239,
+    'message' => 'Id'
+  },
+  'UserList Add/Edit' => {
+    'lastUpdated' => 1217229751,
+    'message' => 'Hinzuf&uuml;gen / Bearbeiten Benutzerliste'
+  },
+  'and label' => {
+    'lastUpdated' => 1217230189,
+    'message' => 'Und'
+  },
+  'Group to hide label' => {
+    'lastUpdated' => 1217229656,
+    'message' => 'Nicht anzuzeigende Gruppe'
+  },
+  'alphabetSearch_loop' => {
+    'lastUpdated' => 1217230051,
+    'message' => 'Eine Schleife, die die Elemente f&uuml;r eine alphabetische Suche enth&auml;lt.'
+  },
+  'Edit/Add UserList' => {
+    'lastUpdated' => 1217229521,
+    'message' => 'Hinzuf&uuml;gen / Bearbeiten der Benutzerliste'
+  },
+  'Users per page label' => {
+    'lastUpdated' => 1217229784,
+    'message' => 'Benutzer pro Seite'
+  },
+  'Profile not public message' => {
+    'lastUpdated' => 1217229736,
+    'message' => 'Profil nicht &ouml;ffentlich'
+  },
+  'Group to show description' => {
+    'lastUpdated' => 1217229691,
+    'message' => 'Nur Benutzer aus dieser Gruppe werden in der Benutzerliste angezeigt. Standardwert ist die Gruppe \'Everyone\'.'
+  },
+  'Users per page description' => {
+    'lastUpdated' => 1217229775,
+    'message' => 'Die Anzahl der Benutzer pro Seite'
+  },
+  'alphabetSearchField label' => {
+    'lastUpdated' => 1217229979,
+    'message' => 'Feld f&uuml;r alphabetische Suche'
+  },
+  'alphabetSearchField' => {
+    'lastUpdated' => 1217229903,
+    'message' => 'Feld f&uuml;r die alphabetische Suche.'
+  },
+  'ascending' => {
+    'lastUpdated' => 1217230202,
+    'message' => 'Aufsteigend'
+  },
+  'includeInSearch_PROFILEFIELDNAME_checkBox' => {
+    'lastUpdated' => 1217230394,
+    'message' => 'Eine CheckBox, die der Benutzer benutzen kann, um zu bestimmen, ob das Profilfeld PROFILEFIELDNAME durchsucht wird oder nicht. Dies hat nur eine Auswirkung, falls &quot;limitSearch tmpl_var&quot; ein Teil des Suchformulars ist.'
+  },
+  'alphabetSearch_loop_label' => {
+    'lastUpdated' => 1217230144,
+    'message' => 'Label f&uuml;r diese alphabetische Suche. Normalerweise ein Buchstabe des Alphabets.'
+  },
+  'UserList Template' => {
+    'lastUpdated' => 1217229760,
+    'message' => 'Template Benutzerliste'
+  },
+  'alphabetSearchField description' => {
+    'lastUpdated' => 1217229941,
+    'message' => 'W&auml;hle ein Profilfeld, f&uuml;r das die alphabetische Suche durchgef&uuml;hrt wird.'
+  },
+  'alphabet label' => {
+    'lastUpdated' => 1217229876,
+    'message' => 'Alphabet'
+  },
+  'Email not public message' => {
+    'lastUpdated' => 1217229538,
+    'message' => 'Email nicht &ouml;ffentlich'
+  },
+  'alphabet description' => {
+    'lastUpdated' => 1217229863,
+    'message' => 'Alphabet, das f&uuml;r die alphabetische Suche benutzt wird. Dies ist eine Zeichenkette bestehend aus komma separierten Werten'
+  },
+  'hideGroupId' => {
+    'lastUpdated' => 1217230234,
+    'message' => 'Benutzer dieser Gruppe werden nicht angezeigt.'
+  },
+  'alphabetSearch_loop_searchURL' => {
+    'lastUpdated' => 1217230182,
+    'message' => 'Die Url dieser alphabetischen Suche.'
+  },
+  'descending' => {
+    'lastUpdated' => 1217230217,
+    'message' => 'Absteigend'
+  }
+}
+;
 
 1;
