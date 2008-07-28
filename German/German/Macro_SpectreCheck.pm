70207e01d3968091601e5215bf4b81f31b2d96bb
Translation Server <translation@plainblack.com>
2008-07-28 09:17:11 +0000

    Update from translation server

--- German/German/Macro_SpectreCheck.pm
+++ German/German/Macro_SpectreCheck.pm
@@ -1,7 +1,19 @@
 package WebGUI::i18n::German::Macro_SpectreCheck;
 use utf8;
-
 our $I18N = {
-};
+  'spectre is down' => {
+    'lastUpdated' => 1217236611,
+    'message' => 'Spectre l&auml;uft nicht'
+  },
+  'macroName' => {
+    'lastUpdated' => 1217236597,
+    'message' => 'Mein Asset'
+  },
+  'spectre ok' => {
+    'lastUpdated' => 1217236627,
+    'message' => 'Spectre ist OK'
+  }
+}
+;
 
 1;
