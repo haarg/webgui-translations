ccaeaba12e0d567cad4a3f177469a072422efe84
Translation Server <translation@plainblack.com>
2008-07-28 09:56:11 +0000

    Update from translation server

--- German/German/Asset_WSClient.pm
+++ German/German/Asset_WSClient.pm
@@ -1,318 +1,255 @@
 package WebGUI::i18n::German::Asset_WSClient;
 use utf8;
-
-
 our $I18N = {
-	'27 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'32' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'21' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'cacheTTL' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'26' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'numResults' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'decodeUtf8' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'72' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'paginateVar' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'paginateAfter' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'13 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'16' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'9 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'27' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'4 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'20' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'uri' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'debugMode' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'11 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'31' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'35' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'11' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'16 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'29' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'12' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'15' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'8 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'19' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'callMethod' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'preprocessMacros' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'72 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'assetName' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'2' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'httpHeader' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'30' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'25' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'28' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'5 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'14' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'params' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'soapError' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'28 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'24' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'proxy' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'3 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'15 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'22' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'disableWobject' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'23' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'13' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'templateId' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'14 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'3' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'9' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'execute_by_default' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'sharedCache' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'ws client asset template variables title' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'8' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'4' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'2 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'5' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'results' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-};
+  '27 description' => {
+    'lastUpdated' => 1217237919,
+    'message' => 'Wie lange in Sekunden sollen SOAP R&uuml;ckgabewerte gecached werden. Setze auf 1 um Caching abzuschalten.'
+  },
+  'callMethod' => {
+    'lastUpdated' => 1217238681,
+    'message' => 'Der Name der Funktion, die vom Server ausgef&uuml;hrt werden soll.'
+  },
+  '32' => {
+    'lastUpdated' => 1217238323,
+    'message' => '<strong>Debug:</strong> Error: Konnte nicht mit dem SOAP Server verbinden.'
+  },
+  'preprocessMacros' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'cacheTTL' => {
+    'lastUpdated' => 1217238661,
+    'message' => 'Wie lange in Sekunden sollen SOAP R&uuml;ckgabewerte gecached werden.'
+  },
+  '21' => {
+    'lastUpdated' => 1217237692,
+    'message' => 'Keine Ergebnisse f&uuml;r diese Abfrage.'
+  },
+  '72 description' => {
+    'lastUpdated' => 1217238502,
+    'message' => 'W&auml;hle ein Template f&uuml;r die Anzeige der Ausgabe des Web Service Client Assets.'
+  },
+  'assetName' => {
+    'lastUpdated' => 1217238619,
+    'message' => 'Web Service Client'
+  },
+  '26' => {
+    'lastUpdated' => 1217237828,
+    'message' => 'Konnte nicht mit dem SOAP Server verbinden.'
+  },
+  'httpHeader' => {
+    'lastUpdated' => 1217238863,
+    'message' => 'Alternativer HTTP Header, der benutzt werden kann, um den Standard MIME Typ dieser Seite zu &uuml;berschreiben.'
+  },
+  '2' => {
+    'lastUpdated' => 1217237657,
+    'message' => 'SOAP URI oder WSDL'
+  },
+  'numResults' => {
+    'lastUpdated' => 1217238904,
+    'message' => 'Anzahl der Zeilen, die vom Client gefunden wurden, falls ein Array zur&uuml;ckgegeben wurde.'
+  },
+  'decodeUtf8' => {
+    'lastUpdated' => 1217238772,
+    'message' => '<p>Gibt an, ob SOAP UTF-8 R&uuml;ckgabewerte in das&nbsp;Encoding der Seite konvertiert werden sollen oder nicht.</p>'
+  },
+  '72' => {
+    'lastUpdated' => 1217238476,
+    'message' => 'Web Service Client Template'
+  },
+  'paginateVar' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  '30' => {
+    'lastUpdated' => 1217238245,
+    'message' => '<strong>Debug:</strong> Error: Angegebener URI/WSDL hat ein ung&uuml;ltiges Format.'
+  },
+  'paginateAfter' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  '16' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  '13 description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  '9 description' => {
+    'lastUpdated' => 1217238609,
+    'message' => 'Falls Sie Debug und Fehler Meldungen angezeigt bekommen wollen, selektieren Sie dieses Auswahlfeld.'
+  },
+  '25' => {
+    'lastUpdated' => 1217237808,
+    'message' => 'Es gab ein Problem mit dem SOAP Aufruf:'
+  },
+  '27' => {
+    'lastUpdated' => 1217237861,
+    'message' => 'Cache wird ung&uuml;ltig'
+  },
+  '28' => {
+    'lastUpdated' => 1217237926,
+    'message' => 'Cache'
+  },
+  '5 description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  '4 description' => {
+    'lastUpdated' => 1217238437,
+    'message' => 'Die SOAP Methode ist der Name der Funktion die vom SOAP Server ausgef&uuml;hrt werden soll. F&uuml;ge jeden extra Parameter in das SOAP Aufruf Parameter Feld unten ein.'
+  },
+  '14' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'debugMode' => {
+    'lastUpdated' => 1217238716,
+    'message' => 'Wahrheitswert, der angibt, ob Debug und Fehler Meldungen angezeigt werden sollen oder nicht.'
+  },
+  '20' => {
+    'lastUpdated' => 1217237672,
+    'message' => 'Web Service Client editieren'
+  },
+  'uri' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'params' => {
+    'lastUpdated' => 1217238936,
+    'message' => '<p>Vom Benutzer eingegebenen Parameter, als Perl Code.</p>'
+  },
+  'soapError' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  '28 description' => {
+    'lastUpdated' => 1217238103,
+    'message' => 'Standardm&auml;ssig werden SOAP Aufrufe pro Benutzer Session gecached. Falls &quot;Global&quot; gew&auml;hlt wird,&nbsp;k&ouml;nnen die R&uuml;ckgabewerte von verschiedenen Benutzern gemeinsam benutzt werden.'
+  },
+  '24' => {
+    'lastUpdated' => 1217237790,
+    'message' => 'SOAP R&uuml;ckgabewert hat Typ:'
+  },
+  '11 description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'proxy' => {
+    'lastUpdated' => 1217238959,
+    'message' => 'Der volle Name des SOAP Servers und/oder Skripts.'
+  },
+  '31' => {
+    'lastUpdated' => 1217238288,
+    'message' => '<strong>Debug:</strong> Error: Es gab ein Problem&nbsp;mit dem&nbsp;SOAP Aufruf.'
+  },
+  '3 description' => {
+    'lastUpdated' => 1217238206,
+    'message' => 'Der SOAP Proxy ist der volle Name des Servers und/oder Skripts, der/das auf SOAP Aufrufe wartet. Beispiel: http://mydomain.com/cgi-bin/soaplistener.pl'
+  },
+  '35' => {
+    'lastUpdated' => 1217238353,
+    'message' => '<strong>Debug:</strong> Kein Template angegeben, Standard wird benutzt.'
+  },
+  '11' => {
+    'lastUpdated' => 1217237542,
+    'message' => 'Standardm&auml;ssig ausf&uuml;hren?'
+  },
+  '15 description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  '22' => {
+    'lastUpdated' => 1217237727,
+    'message' => 'Parse error f&uuml;r SOAP Parameter.'
+  },
+  'disableWobject' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  '16 description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  '13' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  '23' => {
+    'lastUpdated' => 1217237769,
+    'message' => 'Die angegebene URI/WSDL hat ein nicht passendes Format.'
+  },
+  '29' => {
+    'lastUpdated' => 1217238110,
+    'message' => 'Session'
+  },
+  'templateId' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  '14 description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  '3' => {
+    'lastUpdated' => 1217238128,
+    'message' => 'SOAP Proxy'
+  },
+  'sharedCache' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'execute_by_default' => {
+    'lastUpdated' => 1217238824,
+    'message' => 'Wahrheitswert, der angibt, ob der WSClient auf standardm&auml;ssige Ausf&uuml;hrung gesetzt ist oder nicht.'
+  },
+  '9' => {
+    'lastUpdated' => 1217238574,
+    'message' => 'Debug?'
+  },
+  '12' => {
+    'lastUpdated' => 1217237565,
+    'message' => 'Nachricht, falls keine Ergebnisse'
+  },
+  'ws client asset template variables title' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  '15' => {
+    'lastUpdated' => 1217237590,
+    'message' => 'UTF-8 Daten dekodieren?'
+  },
+  '8' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  '4' => {
+    'lastUpdated' => 1217238368,
+    'message' => 'SOAP Methode/Aufruf'
+  },
+  '8 description' => {
+    'lastUpdated' => 1217238565,
+    'message' => 'Falls Sie WebGUI Makros in der Abfrage benutzen, wollen Sie wahrscheinlich dieses Auswahlfeld selektieren.'
+  },
+  '2 description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  '19' => {
+    'lastUpdated' => 1217237640,
+    'message' => 'Global'
+  },
+  'results' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  '5' => {
+    'lastUpdated' => 1217238449,
+    'message' => 'SOAP Aufruf Parameter'
+  }
+}
+;
 
 1;
