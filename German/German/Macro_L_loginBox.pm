517b4cc71f9a4a600bef31d9418f8ad33af2328b
Translation Server <translation@plainblack.com>
2008-07-28 09:19:33 +0000

    Update from translation server

--- German/German/Macro_L_loginBox.pm
+++ German/German/Macro_L_loginBox.pm
@@ -1,97 +1,83 @@
 package WebGUI::i18n::German::Macro_L_loginBox;
 use utf8;
-
 our $I18N = {
-	'username.form' => {
-		message => q|Formularfeld für den Nutzername|,
-		lastUpdated => 1157909322
-	},
-
-	'logout.label' => {
-		message => q|Internationalisierte Meldung beim Abmelden|,
-		lastUpdated => 1157909133
-	},
-
-	'account.display.url' => {
-		message => q|URL um einen Account anzuzeigen|,
-		lastUpdated => 1157909200
-	},
-
-	'customText' => {
-		message => q|Text der an das Makro übergeben wird, um anzuzeigen, dass der Benutzer angemeldet ist.|,
-		lastUpdated => 1168878412
-	},
-
-	'48' => {
-		message => q|Hallo|,
-		lastUpdated => 1157908917
-	},
-
-	'login box title' => {
-		message => q|Anmelde-Box Makro Template Variable|,
-		lastUpdated => 1192966833
-	},
-
-	'account.create.url' => {
-		message => q|URL um einen Account anzulegen|,
-		lastUpdated => 1157908958
-	},
-
-	'form.login' => {
-		message => q|Aktion die beim Login ausgeführt wird.|,
-		lastUpdated => 1157909168
-	},
-
-	'form.footer' => {
-		message => q|Formular Fußbereich|,
-		lastUpdated => 1157909250
-	},
-
-	'password.label' => {
-		message => q|Internationalisiertes Label für "Paßwort"|,
-		lastUpdated => 1157909090
-	},
-
-	'form.header' => {
-		message => q|Formular Kopfbereich|,
-		lastUpdated => 1157909259
-	},
-
-	'account.create.label' => {
-		message => q|Internationalisiertes Label für "Account anlegen"|,
-		lastUpdated => 1157909181
-	},
-
-	'macroName' => {
-		message => q|Login Box|,
-		lastUpdated => 1157909011
-	},
-
-	'username.label' => {
-		message => q|Internationalisiertes Label für "Nutzername"|,
-		lastUpdated => 1157909108
-	},
-
-	'hello.label' => {
-		message => q|Internationalisiertes Willkommens-Meldung|,
-		lastUpdated => 1157909147
-	},
-
-	'password.form' => {
-		message => q|Formularelement für das Paßwort|,
-		lastUpdated => 1157909308
-	},
-
-	'49' => {
-		message => q|Zum Abmelden hier klicken|,
-		lastUpdated => 1157908930
-	},
-
-	'user.isVisitor' => {
-		message => q|Wahr wenn der Nutzer Besucher ist|,
-		lastUpdated => 1157909060
-	},
-
-};
+  'form.footer' => {
+    'lastUpdated' => 1157909250,
+    'message' => "Formular Fu\x{df}bereich"
+  },
+  'password.label' => {
+    'lastUpdated' => 1157909090,
+    'message' => "Internationalisiertes Label f\x{fc}r \"Pa\x{df}wort\""
+  },
+  'username.form' => {
+    'lastUpdated' => 1157909322,
+    'message' => "Formularfeld f\x{fc}r den Nutzername"
+  },
+  'account.create.label' => {
+    'lastUpdated' => 1157909181,
+    'message' => "Internationalisiertes Label f\x{fc}r \"Account anlegen\""
+  },
+  'form.header' => {
+    'lastUpdated' => 1157909259,
+    'message' => 'Formular Kopfbereich'
+  },
+  'username.label' => {
+    'lastUpdated' => 1157909108,
+    'message' => "Internationalisiertes Label f\x{fc}r \"Nutzername\""
+  },
+  'macroName' => {
+    'lastUpdated' => 1157909011,
+    'message' => 'Login Box'
+  },
+  'account.display.url' => {
+    'lastUpdated' => 1157909200,
+    'message' => 'URL um einen Account anzuzeigen'
+  },
+  'logout.label' => {
+    'lastUpdated' => 1157909133,
+    'message' => 'Internationalisierte Meldung beim Abmelden'
+  },
+  'hello.label' => {
+    'lastUpdated' => 1157909147,
+    'message' => 'Internationalisiertes Willkommens-Meldung'
+  },
+  'customText' => {
+    'lastUpdated' => 1168878412,
+    'message' => "Text der an das Makro \x{fc}bergeben wird, um anzuzeigen, dass der Benutzer angemeldet ist."
+  },
+  'login box title' => {
+    'lastUpdated' => 1192966833,
+    'message' => 'Anmelde-Box Makro Template Variable'
+  },
+  '48' => {
+    'lastUpdated' => 1157908917,
+    'message' => 'Hallo'
+  },
+  'helpvar form.returnUrl' => {
+    'lastUpdated' => 1217236748,
+    'message' => 'Falls dieses versteckte Formularelement pr&auml;sent ist, wird dem Benutzer nach dem Anmelden&nbsp;die aktuellen Seite angezeigt'
+  },
+  'password.form' => {
+    'lastUpdated' => 1157909308,
+    'message' => "Formularelement f\x{fc}r das Pa\x{df}wort"
+  },
+  '49' => {
+    'lastUpdated' => 1157908930,
+    'message' => 'Zum Abmelden hier klicken'
+  },
+  'account.create.url' => {
+    'lastUpdated' => 1157908958,
+    'message' => 'URL um einen Account anzulegen'
+  },
+  'form.login' => {
+    'lastUpdated' => 1157909168,
+    'message' => "Aktion die beim Login ausgef\x{fc}hrt wird."
+  },
+  'user.isVisitor' => {
+    'lastUpdated' => 1157909060,
+    'message' => 'Wahr wenn der Nutzer Besucher ist'
+  }
+}
+;
 
 1;
--- German/German/Macro_SQL.pm
+++ German/German/Macro_SQL.pm
@@ -1,23 +1,23 @@
 package WebGUI::i18n::German::Macro_SQL;
 use utf8;
-
-
 our $I18N = {
-	'illegal query' => {
-		message => q|Kann diese Art von Abfrage (Query) nicht ausf&uuml;hren. |,
-		lastUpdated => 1178723148
-	},
-
-	'macroName' => {
-		message => q|SQL|,
-		lastUpdated => 1178723159
-	},
-
-	'sql error' => {
-		message => q|<strong>SQL Makro Ausf&uuml;hrung fehlgeschlagen:</strong>%s|,
-		lastUpdated => 1178723643
-	},
-
-};
+  'illegal query' => {
+    'lastUpdated' => 1178723148,
+    'message' => 'Kann diese Art von Abfrage (Query) nicht ausf&uuml;hren. '
+  },
+  'macroName' => {
+    'lastUpdated' => 1178723159,
+    'message' => 'SQL'
+  },
+  'database access not allowed' => {
+    'lastUpdated' => 1217236668,
+    'message' => 'Die Datenbank erlaubt keine Zugriffe aus einem Makro heraus.'
+  },
+  'sql error' => {
+    'lastUpdated' => 1178723643,
+    'message' => '<strong>SQL Makro Ausf&uuml;hrung fehlgeschlagen:</strong>%s'
+  }
+}
+;
 
 1;
