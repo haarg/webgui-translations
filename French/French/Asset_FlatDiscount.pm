607a4c20bb46be4abdd69086a55aa958290f7f30
Translation Server <translation@plainblack.com>
2008-09-19 11:45:55 +0000

    Update from translation server

--- French/French/Asset_FlatDiscount.pm
+++ French/French/Asset_FlatDiscount.pm
@@ -1,7 +1,11 @@
 package WebGUI::i18n::French::Asset_FlatDiscount;
 use utf8;
-
 our $I18N = {
-};
+  'add to cart' => {
+    'lastUpdated' => 1220534431,
+    'message' => 'ajouter pour charroyer'
+  }
+}
+;
 
 1;
--- French/French/Image_Graph.pm
+++ French/French/Image_Graph.pm
@@ -1,7 +1,15 @@
 package WebGUI::i18n::French::Image_Graph;
 use utf8;
-
 our $I18N = {
-};
+  'background color' => {
+    'lastUpdated' => 1218349245,
+    'message' => 'Couleur de l\'arri&egrave;re plan'
+  },
+  'background color description' => {
+    'lastUpdated' => 1220224260,
+    'message' => 'description de la couleur de l\'arri&egrave;re-plan'
+  }
+}
+;
 
 1;
