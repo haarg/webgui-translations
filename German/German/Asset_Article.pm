8b7c262b9084f3f6b02f1970f3f4bd0a0baf4e60
Translation Server <translation@plainblack.com>
2008-09-24 09:52:49 +0000

    Update from translation server

--- German/German/Asset_Article.pm
+++ German/German/Asset_Article.pm
@@ -1,238 +1,191 @@
 package WebGUI::i18n::German::Asset_Article;
 use utf8;
-
-
 our $I18N = {
-	'description.first.sentence' => {
-		message => q|Der erste Satz der Beschreibung. S&auml;tze sind definiert durch die Anzahl der Punkte im Text.|,
-		lastUpdated => 1157928529
-	},
-
-	'article template description' => {
-		message => q|F&uuml;r das Layout w&auml;hlen Sie ein Template aus der Liste aus. Jedes WObject kann nur Templates aus dem eigenen Namensraum nutzen. So k&ouml;nnen z.B: Artikel nur Templates aus demArtikel-Namensraum nutzen, ein Seitenlayout nur Template aus dem Seiten-Namensraum.&nbsp;|,
-		lastUpdated => 1182331219
-	},
-
-	'linkTitle' => {
-		message => q|Der Titel des Links im Artikel.|,
-		lastUpdated => 1182331184
-	},
-
-	'7' => {
-		message => q|Link Text|,
-		lastUpdated => 1153126076
-	},
-
-	'description.first.10words' => {
-		message => q|Die ersten 10 W&ouml;rter der Beschreibung. Ein Wort ist definiert als Zeichenkette, getrennt durch Leerzeichen. Daher z&auml;hlen auch HTML-Entities und -Tags als Wort.|,
-		lastUpdated => 1157928320
-	},
-
-	'assetName' => {
-		message => q|Artikel|,
-		lastUpdated => 1152969958
-	},
-
-	'description.full' => {
-		message => q|Die ganze Beschreibung ohne Seitenumbruch.|,
-		lastUpdated => 1157928555
-	},
-
-	'description.first.paragraph' => {
-		message => q|Der erste Absatz der Beschreibung. Abs&auml;tze sind definiert durch die Anzahl der Zeilenumbr&uuml;che im Text.|,
-		lastUpdated => 1157928453
-	},
-
-	'description.first.2paragraphs' => {
-		message => q|Die ersten beiden Abs&auml;tze der Beschreibung. Abs&auml;tze sind definiert durch die Anzahl der Zeilenumbr&uuml;che im Text.|,
-		lastUpdated => 1157928433
-	},
-
-	'72' => {
-		message => q|Artikel Template|,
-		lastUpdated => 1182331121
-	},
-
-	'url' => {
-		message => q|Der URL für den Download der Datei.|,
-		lastUpdated => 1153264775
-	},
-
-	'attachments help' => {
-		message => q|H&auml;ngen Sie Dateien und Bilder direkt an diesen Arikel an. Beachten Sie, dass diese Dateien nicht &uuml;ber den Asset-Manager oder f&uuml;r andere Assets erreichbar sind.|,
-		lastUpdated => 1182330964
-	},
-
-	'28' => {
-		message => q|Antworten ansehen|,
-		lastUpdated => 1152969858
-	},
-
-	'description.first.4sentences' => {
-		message => q|Die ersten vier S&auml;tze der Beschreibung. S&auml;tze sind definiert durch die Anzahl der Punkte im Text.|,
-		lastUpdated => 1157928515
-	},
-
-	'iconUrl' => {
-		message => q|Der URL zum Dateityp-Icon dieser Datei.|,
-		lastUpdated => 1157927731
-	},
-
-	'description' => {
-		message => q|Die paginierte Beschreibung|,
-		lastUpdated => 1182331163
-	},
-
-	'link url description' => {
-		message => q|Wenn Sie den Linktitel eingegeben haben, erg&auml;nzen Sie nun hier den URL. |,
-		lastUpdated => 1157928720
-	},
-
-	'new.template' => {
-		message => q|<p>Artikel haben die Eigenschaft, dass sie ihr Template &auml;ndern k&ouml;nnen. So k&ouml;nnen Sie ihren Besuchern unterschiedliche Ansichten des Arikels anbieten. Dazu erstellen Sie einen Link, bei dem die URL folgenderma&szlig;en aufgebaut ist (ersetzen Sie <em>999</em> durch die ID des Templates, das sie nutzen m&ouml;chten):</p>&lt;a href=&quot;&lt;tmpl_var new.template&gt;<em>999</em>&quot;&gt;Lesen Sie weiter...&lt;/a&gt;|,
-		lastUpdated => 1157928957
-	},
-
-	'image.url' => {
-		message => q|Der URL des angeh&auml;ngten Bildes.|,
-		lastUpdated => 1182331177
-	},
-
-	'article asset template variables body' => {
-		message => q|Jedes Asset bietet ihren Templates einen Satz an Variablen, die auf den Asset Eigenschaften basieren. Einige dieser Variablen k&ouml;nnen n&uuml;tzlich sein, andere nicht. |,
-		lastUpdated => 1168851450
-	},
-
-	'isImage' => {
-		message => q|Eine boolesche Variable die zeigt ob ein Bild gesetzt ist oder nicht.<br />|,
-		lastUpdated => 1152970181
-	},
-
-	'storageId' => {
-		message => q|Die ID des Speicher Objekts f&uuml;r dieses Asset, das f&uuml;r die Speicherung von Anh&auml;ngen benutzt wird.<br />|,
-		lastUpdated => 1168851572
-	},
-
-	'cache timeout help' => {
-		message => q|Da alle Nutzer dieses Asset in gleicher Darstellung sehen, kann es &uuml;ber einen langen Zeitraum gecached werden um die Performance zu steigern. Wie lange soll es gecached werden?|,
-		lastUpdated => 1182331230
-	},
-
-	'description.first.3sentences' => {
-		message => q|Die ersten drei S&auml;tze der Beschreibung. S&auml;tze sind definiert durch die Anzahl der Punkte im Text.|,
-		lastUpdated => 1157928503
-	},
-
-	'article asset template variables title' => {
-		message => q|Artikel Template Variablen.|,
-		lastUpdated => 1182331128
-	},
-
-	'description.first.2sentences' => {
-		message => q|Die ersten beiden S&auml;tze der Beschreibung. S&auml;tze sind definiert durch die Anzahl der Punkte im Text.|,
-		lastUpdated => 1157928492
-	},
-
-	'description.first.50words' => {
-		message => q|Die ersten 50 W&ouml;rter der Beschreibung. Ein Wort ist definiert als Zeichenkette, getrennt durch Leerzeichen. Daher z&auml;hlen auch HTML-Entities und -Tags als Wort.|,
-		lastUpdated => 1157928346
-	},
-
-	'description.first.75words' => {
-		message => q|Die ersten 75 W&ouml;rter der Beschreibung. Ein Wort ist definiert als Zeichenkette, getrennt durch Leerzeichen. Daher z&auml;hlen auch HTML-Entities und -Tags als Wort.|,
-		lastUpdated => 1157928358
-	},
-
-	'linkURL' => {
-		message => q|Die URL f&uuml;r den Link im Artikel.|,
-		lastUpdated => 1182331191
-	},
-
-	'attachment.icon' => {
-		message => q|Die URL zum Symbol f&uuml;r diesen Anhangtyp.|,
-		lastUpdated => 1182331138
-	},
-
-	'cache timeout' => {
-		message => q|Cache Timeout|,
-		lastUpdated => 1157927744
-	},
-
-	'templateId' => {
-		message => q|Die ID des Templates, das f&uuml;r die Anzeige des Assets benutzt wird.<br />|,
-		lastUpdated => 1168851601
-	},
-
-	'attachments' => {
-		message => q|Anh&auml;nge|,
-		lastUpdated => 1157927601
-	},
-
-	'description.first.100words' => {
-		message => q|Die ersten 100 W&ouml;rter der Beschreibung. Ein Wort ist definiert als Zeichenkette, getrennt durch Leerzeichen. Daher z&auml;hlen auch HTML-Entities und -Tags als Wort.|,
-		lastUpdated => 1157928278
-	},
-
-	'attachment_loop' => {
-		message => q|Eine Schleife, die alle Anh&auml;nge enth&auml;lt.<br />|,
-		lastUpdated => 1157927592
-	},
-
-	'thumbnailUrl' => {
-		message => q|Der URL zum Vorschaubild f&uuml;r diese Datei.|,
-		lastUpdated => 1157928984
-	},
-
-	'cacheTimeout' => {
-		message => q|Zeitspanne, in der der Artikel gecached wird.|,
-		lastUpdated => 1182331154
-	},
-
-	'12' => {
-		message => q|Artikel bearbeiten|,
-		lastUpdated => 1153126052
-	},
-
-	'8' => {
-		message => q|Link URL|,
-		lastUpdated => 1152969947
-	},
-
-	'filename' => {
-		message => q|Der Name der Datei.|,
-		lastUpdated => 1182331170
-	},
-
-	'description.first.25words' => {
-		message => q|Die ersten 25 W&ouml;rter der Beschreibung. Ein Wort ist definiert als Zeichenkette, getrennt durch Leerzeichen. Daher z&auml;hlen auch HTML-Entities und -Tags als Wort.|,
-		lastUpdated => 1157928333
-	},
-
-	'attachment.url' => {
-		message => q|Die URL zum Download des Anhangs.|,
-		lastUpdated => 1152970040
-	},
-
-	'73' => {
-		message => q|Die folgenden Template Variablen sind im Artikel Template verf&uuml;gbar.<br />|,
-		lastUpdated => 1152969935
-	},
-
-	'image.thumbnail' => {
-		message => q|Der URL zum Vorschaubild f&uuml;r das angeh&auml;ngte Bild.<br />|,
-		lastUpdated => 1157928581
-	},
-
-	'attachment.name' => {
-		message => q|Der Dateiname f&uuml;r diesen Anhang.|,
-		lastUpdated => 1182331146
-	},
-
-	'link title description' => {
-		message => q|Wenn Sie einen Link an den Artikel anf&uuml;gen m&ouml;chten, geben Sie den Linktitel in diesem Feld ein.|,
-		lastUpdated => 1182330948
-	},
-
-};
+  '12' => {
+    'lastUpdated' => 1153126052,
+    'message' => 'Artikel bearbeiten'
+  },
+  '28' => {
+    'lastUpdated' => 1152969858,
+    'message' => 'Antworten ansehen'
+  },
+  '7' => {
+    'lastUpdated' => 1153126076,
+    'message' => 'Link Text'
+  },
+  '72' => {
+    'lastUpdated' => 1182331121,
+    'message' => 'Artikel Template'
+  },
+  '73' => {
+    'lastUpdated' => 1152969935,
+    'message' => 'Die folgenden Template Variablen sind im Artikel Template verf&uuml;gbar.<br />'
+  },
+  '8' => {
+    'lastUpdated' => 1152969947,
+    'message' => 'Link URL'
+  },
+  'article asset template variables body' => {
+    'lastUpdated' => 1168851450,
+    'message' => 'Jedes Asset bietet ihren Templates einen Satz an Variablen, die auf den Asset Eigenschaften basieren. Einige dieser Variablen k&ouml;nnen n&uuml;tzlich sein, andere nicht. '
+  },
+  'article asset template variables title' => {
+    'lastUpdated' => 1182331128,
+    'message' => 'Artikel Template Variablen.'
+  },
+  'article template description' => {
+    'lastUpdated' => 1182331219,
+    'message' => 'F&uuml;r das Layout w&auml;hlen Sie ein Template aus der Liste aus. Jedes WObject kann nur Templates aus dem eigenen Namensraum nutzen. So k&ouml;nnen z.B: Artikel nur Templates aus demArtikel-Namensraum nutzen, ein Seitenlayout nur Template aus dem Seiten-Namensraum.&nbsp;'
+  },
+  'assetName' => {
+    'lastUpdated' => 1152969958,
+    'message' => 'Artikel'
+  },
+  'attachment.icon' => {
+    'lastUpdated' => 1182331138,
+    'message' => 'Die URL zum Symbol f&uuml;r diesen Anhangtyp.'
+  },
+  'attachment.name' => {
+    'lastUpdated' => 1182331146,
+    'message' => 'Der Dateiname f&uuml;r diesen Anhang.'
+  },
+  'attachment.url' => {
+    'lastUpdated' => 1152970040,
+    'message' => 'Die URL zum Download des Anhangs.'
+  },
+  'attachment_loop' => {
+    'lastUpdated' => 1157927592,
+    'message' => 'Eine Schleife, die alle Anh&auml;nge enth&auml;lt.<br />'
+  },
+  'attachments' => {
+    'lastUpdated' => 1157927601,
+    'message' => 'Anh&auml;nge'
+  },
+  'attachments help' => {
+    'lastUpdated' => 1222249958,
+    'message' => "H\x{e4}ngen Sie Dateien und Bilder direkt an diesen Artikel an. Beachten Sie, dass diese Dateien nicht &uuml;ber den Asset-Manager oder f&uuml;r andere Assets erreichbar sind."
+  },
+  'cache timeout' => {
+    'lastUpdated' => 1157927744,
+    'message' => 'Cache Timeout'
+  },
+  'cache timeout help' => {
+    'lastUpdated' => 1182331230,
+    'message' => 'Da alle Nutzer dieses Asset in gleicher Darstellung sehen, kann es &uuml;ber einen langen Zeitraum gecached werden um die Performance zu steigern. Wie lange soll es gecached werden?'
+  },
+  'cacheTimeout' => {
+    'lastUpdated' => 1182331154,
+    'message' => 'Zeitspanne, in der der Artikel gecached wird.'
+  },
+  'description' => {
+    'lastUpdated' => 1182331163,
+    'message' => 'Die paginierte Beschreibung'
+  },
+  'description.first.100words' => {
+    'lastUpdated' => 1157928278,
+    'message' => 'Die ersten 100 W&ouml;rter der Beschreibung. Ein Wort ist definiert als Zeichenkette, getrennt durch Leerzeichen. Daher z&auml;hlen auch HTML-Entities und -Tags als Wort.'
+  },
+  'description.first.10words' => {
+    'lastUpdated' => 1157928320,
+    'message' => 'Die ersten 10 W&ouml;rter der Beschreibung. Ein Wort ist definiert als Zeichenkette, getrennt durch Leerzeichen. Daher z&auml;hlen auch HTML-Entities und -Tags als Wort.'
+  },
+  'description.first.25words' => {
+    'lastUpdated' => 1157928333,
+    'message' => 'Die ersten 25 W&ouml;rter der Beschreibung. Ein Wort ist definiert als Zeichenkette, getrennt durch Leerzeichen. Daher z&auml;hlen auch HTML-Entities und -Tags als Wort.'
+  },
+  'description.first.2paragraphs' => {
+    'lastUpdated' => 1157928433,
+    'message' => 'Die ersten beiden Abs&auml;tze der Beschreibung. Abs&auml;tze sind definiert durch die Anzahl der Zeilenumbr&uuml;che im Text.'
+  },
+  'description.first.2sentences' => {
+    'lastUpdated' => 1157928492,
+    'message' => 'Die ersten beiden S&auml;tze der Beschreibung. S&auml;tze sind definiert durch die Anzahl der Punkte im Text.'
+  },
+  'description.first.3sentences' => {
+    'lastUpdated' => 1157928503,
+    'message' => 'Die ersten drei S&auml;tze der Beschreibung. S&auml;tze sind definiert durch die Anzahl der Punkte im Text.'
+  },
+  'description.first.4sentences' => {
+    'lastUpdated' => 1157928515,
+    'message' => 'Die ersten vier S&auml;tze der Beschreibung. S&auml;tze sind definiert durch die Anzahl der Punkte im Text.'
+  },
+  'description.first.50words' => {
+    'lastUpdated' => 1157928346,
+    'message' => 'Die ersten 50 W&ouml;rter der Beschreibung. Ein Wort ist definiert als Zeichenkette, getrennt durch Leerzeichen. Daher z&auml;hlen auch HTML-Entities und -Tags als Wort.'
+  },
+  'description.first.75words' => {
+    'lastUpdated' => 1157928358,
+    'message' => 'Die ersten 75 W&ouml;rter der Beschreibung. Ein Wort ist definiert als Zeichenkette, getrennt durch Leerzeichen. Daher z&auml;hlen auch HTML-Entities und -Tags als Wort.'
+  },
+  'description.first.paragraph' => {
+    'lastUpdated' => 1157928453,
+    'message' => 'Der erste Absatz der Beschreibung. Abs&auml;tze sind definiert durch die Anzahl der Zeilenumbr&uuml;che im Text.'
+  },
+  'description.first.sentence' => {
+    'lastUpdated' => 1157928529,
+    'message' => 'Der erste Satz der Beschreibung. S&auml;tze sind definiert durch die Anzahl der Punkte im Text.'
+  },
+  'description.full' => {
+    'lastUpdated' => 1157928555,
+    'message' => 'Die ganze Beschreibung ohne Seitenumbruch.'
+  },
+  'filename' => {
+    'lastUpdated' => 1182331170,
+    'message' => 'Der Name der Datei.'
+  },
+  'iconUrl' => {
+    'lastUpdated' => 1157927731,
+    'message' => 'Der URL zum Dateityp-Icon dieser Datei.'
+  },
+  'image.thumbnail' => {
+    'lastUpdated' => 1157928581,
+    'message' => 'Der URL zum Vorschaubild f&uuml;r das angeh&auml;ngte Bild.<br />'
+  },
+  'image.url' => {
+    'lastUpdated' => 1182331177,
+    'message' => 'Der URL des angeh&auml;ngten Bildes.'
+  },
+  'isImage' => {
+    'lastUpdated' => 1152970181,
+    'message' => 'Eine boolesche Variable die zeigt ob ein Bild gesetzt ist oder nicht.<br />'
+  },
+  'link title description' => {
+    'lastUpdated' => 1182330948,
+    'message' => 'Wenn Sie einen Link an den Artikel anf&uuml;gen m&ouml;chten, geben Sie den Linktitel in diesem Feld ein.'
+  },
+  'link url description' => {
+    'lastUpdated' => 1157928720,
+    'message' => 'Wenn Sie den Linktitel eingegeben haben, erg&auml;nzen Sie nun hier den URL. '
+  },
+  'linkTitle' => {
+    'lastUpdated' => 1182331184,
+    'message' => 'Der Titel des Links im Artikel.'
+  },
+  'linkURL' => {
+    'lastUpdated' => 1182331191,
+    'message' => 'Die URL f&uuml;r den Link im Artikel.'
+  },
+  'new.template' => {
+    'lastUpdated' => 1157928957,
+    'message' => '<p>Artikel haben die Eigenschaft, dass sie ihr Template &auml;ndern k&ouml;nnen. So k&ouml;nnen Sie ihren Besuchern unterschiedliche Ansichten des Arikels anbieten. Dazu erstellen Sie einen Link, bei dem die URL folgenderma&szlig;en aufgebaut ist (ersetzen Sie <em>999</em> durch die ID des Templates, das sie nutzen m&ouml;chten):</p>&lt;a href=&quot;&lt;tmpl_var new.template&gt;<em>999</em>&quot;&gt;Lesen Sie weiter...&lt;/a&gt;'
+  },
+  'storageId' => {
+    'lastUpdated' => 1168851572,
+    'message' => 'Die ID des Speicher Objekts f&uuml;r dieses Asset, das f&uuml;r die Speicherung von Anh&auml;ngen benutzt wird.<br />'
+  },
+  'templateId' => {
+    'lastUpdated' => 1168851601,
+    'message' => 'Die ID des Templates, das f&uuml;r die Anzeige des Assets benutzt wird.<br />'
+  },
+  'thumbnailUrl' => {
+    'lastUpdated' => 1157928984,
+    'message' => 'Der URL zum Vorschaubild f&uuml;r diese Datei.'
+  },
+  'url' => {
+    'lastUpdated' => 1153264775,
+    'message' => "Der URL f\x{fc}r den Download der Datei."
+  }
+}
+;
 
 1;
