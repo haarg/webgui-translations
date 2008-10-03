18035e8d694e57976c1113f5a477b0c5ef32b685
Translation Server <translation@plainblack.com>
2008-10-03 18:12:38 +0000

    Update from translation server

--- Spanish/Spanish/Asset_Sku.pm
+++ Spanish/Spanish/Asset_Sku.pm
@@ -1,21 +1,37 @@
 package WebGUI::i18n::Spanish::Asset_Sku;
 use utf8;
 our $I18N = {
-  'shop' => {
-    'lastUpdated' => 1215795639,
-    'message' => 'Tienda'
-  },
-  'display title' => {
-    'lastUpdated' => 1215795585,
-    'message' => 'Mostrar T&iacute;tulo?'
+  'assetName' => {
+    'lastUpdated' => 1223057388,
+    'message' => 'Sku'
   },
   'description' => {
     'lastUpdated' => 1215795548,
     'message' => 'Descripci&oacute;n'
   },
+  'display title' => {
+    'lastUpdated' => 1215795585,
+    'message' => 'Mostrar T&iacute;tulo?'
+  },
+  'shop' => {
+    'lastUpdated' => 1215795639,
+    'message' => 'Tienda'
+  },
+  'sku' => {
+    'lastUpdated' => 1223057399,
+    'message' => 'SKU'
+  },
+  'sku help' => {
+    'lastUpdated' => 1223057479,
+    'message' => 'Soportes de Stock Keeping Unit, que es s&oacute;lo un t&eacute;rmino de fantas&iacute;a de un c&oacute;digo de inventario o n&uacute;mero de producto.'
+  },
   'vendor' => {
     'lastUpdated' => 1215795534,
     'message' => 'Vendedor'
+  },
+  'vendor help' => {
+    'lastUpdated' => 1223057546,
+    'message' => 'Que persona/empresa definida en el sistema de comercio deber&iacute;a obtener cr&eacute;dito por la venta de este item, si es el caso?'
   }
 }
 ;
