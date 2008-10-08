c356cd0131faf733b7359dfb52d8bdf0c3456684
Translation Server <translation@plainblack.com>
2008-10-08 22:08:49 +0000

    Update from translation server

--- Spanish/Spanish/Macro_L_loginBox.pm
+++ Spanish/Spanish/Macro_L_loginBox.pm
@@ -1,102 +1,83 @@
 package WebGUI::i18n::Spanish::Macro_L_loginBox;
 use utf8;
-
 our $I18N = {
-	'username.form' => {
-		message => q|Elemento del formulario para el nombre de usuario|,
-		lastUpdated => 1208187156
-	},
-
-	'logout.label' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'account.display.url' => {
-		message => q|URL para mostrar la cuenta|,
-		lastUpdated => 1207730362
-	},
-
-	'customText' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'48' => {
-		message => q|Hola|,
-		lastUpdated => 1207730193
-	},
-
-	'login box title' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'account.create.url' => {
-		message => q|URL para crear una cuenta|,
-		lastUpdated => 1207730344
-	},
-
-	'form.login' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'form.footer' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'password.label' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'form.header' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'account.create.label' => {
-		message => q|Internacionalizada etiqueta para &quot;Crear cuenta&quot;|,
-		lastUpdated => 1207730277
-	},
-
-	'macroName' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'username.label' => {
-		message => q|Etiqueta internacionalizada para el nombre de usuario|,
-		lastUpdated => 1208187183
-	},
-
-	'hello.label' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'helpvar form.returnUrl' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'password.form' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'49' => {
-		message => q|Click aqui para salir|,
-		lastUpdated => 1207730214
-	},
-
-	'user.isVisitor' => {
-		message => q|Verdadero si el usuario es &quot;visitor&quot;|,
-		lastUpdated => 1207730435
-	},
-
-};
+  '48' => {
+    'lastUpdated' => 1207730193,
+    'message' => 'Hola'
+  },
+  '49' => {
+    'lastUpdated' => 1207730214,
+    'message' => 'Click aqui para salir'
+  },
+  'account.create.label' => {
+    'lastUpdated' => 1223503168,
+    'message' => 'Etiqueta para &quot;Crear cuenta&quot;'
+  },
+  'account.create.url' => {
+    'lastUpdated' => 1207730344,
+    'message' => 'URL para crear una cuenta'
+  },
+  'account.display.url' => {
+    'lastUpdated' => 1207730362,
+    'message' => 'URL para mostrar la cuenta'
+  },
+  'customText' => {
+    'lastUpdated' => 1223503241,
+    'message' => 'El texto empleado por el Macro para indicar que el usuario ha ingresado al sistema. En la mayor&iacute;a de los casos, se utilizar&aacute; este texto en lugar del contenido de las variables logout.label y logout.url.'
+  },
+  'form.footer' => {
+    'lastUpdated' => 1223503270,
+    'message' => 'Pi&eacute; de p&aacute;gina del formulario.'
+  },
+  'form.header' => {
+    'lastUpdated' => 1223503284,
+    'message' => 'Encabezado del formulario.'
+  },
+  'form.login' => {
+    'lastUpdated' => 1223503318,
+    'message' => 'Acci&oacute;n a efectuar durante el ingreso.'
+  },
+  'hello.label' => {
+    'lastUpdated' => 1223503347,
+    'message' => 'Mensaje de bienvenida.'
+  },
+  'helpvar form.returnUrl' => {
+    'lastUpdated' => 1223503411,
+    'message' => 'Si este elemento est&aacute; presente en el formulario, el usuario ser&aacute; dirigido de vuelta a la p&aacute;gina actual una vez que haya completado el ingreso.'
+  },
+  'login box title' => {
+    'lastUpdated' => 1223503433,
+    'message' => 'Macro para la Plantilla de Ingreso.'
+  },
+  'logout.label' => {
+    'lastUpdated' => 1223503444,
+    'message' => 'Mensaje de despedida.'
+  },
+  'macroName' => {
+    'lastUpdated' => 1223503464,
+    'message' => 'Di&aacute;logo de Ingreso'
+  },
+  'password.form' => {
+    'lastUpdated' => 1223503668,
+    'message' => 'Elemento del formulario para la clave.'
+  },
+  'password.label' => {
+    'lastUpdated' => 1223503711,
+    'message' => 'Etiqueta para el campo de &quot;clave&quot;'
+  },
+  'user.isVisitor' => {
+    'lastUpdated' => 1207730435,
+    'message' => 'Verdadero si el usuario es &quot;visitor&quot;'
+  },
+  'username.form' => {
+    'lastUpdated' => 1208187156,
+    'message' => 'Elemento del formulario para el nombre de usuario'
+  },
+  'username.label' => {
+    'lastUpdated' => 1208187183,
+    'message' => 'Etiqueta internacionalizada para el nombre de usuario'
+  }
+}
+;
 
 1;
--- Spanish/Spanish/Macro_LoginToggle.pm
+++ Spanish/Spanish/Macro_LoginToggle.pm
@@ -1,37 +1,31 @@
 package WebGUI::i18n::Spanish::Macro_LoginToggle;
 use utf8;
-
 our $I18N = {
-	'716' => {
-		message => q|Ingresar|,
-		lastUpdated => 1207848115
-	},
-
-	'toggle.url' => {
-		message => q|La URL para ingresar o salir|,
-		lastUpdated => 1207848209
-	},
-
-	'717' => {
-		message => q|Salir|,
-		lastUpdated => 1207848129
-	},
-
-	'macroName' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'login toggle title' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'toggle.text' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-};
+  '716' => {
+    'lastUpdated' => 1207848115,
+    'message' => 'Ingresar'
+  },
+  '717' => {
+    'lastUpdated' => 1207848129,
+    'message' => 'Salir'
+  },
+  'login toggle title' => {
+    'lastUpdated' => 1223503134,
+    'message' => 'Macros para el selector Ingresar/Salir en Plantillas'
+  },
+  'macroName' => {
+    'lastUpdated' => 1223503058,
+    'message' => 'Selector Ingresar/Salir'
+  },
+  'toggle.text' => {
+    'lastUpdated' => 1223503114,
+    'message' => 'La etiqueta en idioma local para Ingresar o Salir (dependiendo del estado del Macro), o el texto a suministrar para el Macro.'
+  },
+  'toggle.url' => {
+    'lastUpdated' => 1207848209,
+    'message' => 'La URL para ingresar o salir'
+  }
+}
+;
 
 1;
