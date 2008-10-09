558bd9b27b86d6dcc472a101b9c19c80298c46f8
Translation Server <translation@plainblack.com>
2008-10-09 18:36:53 +0000

    Update from translation server

--- German/German/Activity_RequestApprovalForVersionTag_ByCommitterGroup.pm
+++ German/German/Activity_RequestApprovalForVersionTag_ByCommitterGroup.pm
@@ -2,8 +2,16 @@ package WebGUI::i18n::German::Activity_RequestApprovalForVersionTag_ByCommitterG
 use utf8;
 our $I18N = {
   'committerGroupId description' => {
-    'lastUpdated' => 1217253756,
+    'lastUpdated' => 1223577296,
     'message' => 'Die Gruppe, die eine Freigabe ben&ouml;tigt, um diese Aktivit&auml;t zu benutzen. Falls der Committer kein Mitglied dieser Gruppe ist, f&auml;hrt der Workflow mit der n&auml;chsten Aktivit&auml;t fort.'
+  },
+  'committerGroupId label' => {
+    'lastUpdated' => 1223577347,
+    'message' => "Commiter-Gruppe f\x{fc}r die Freigabe"
+  },
+  'topicName' => {
+    'lastUpdated' => 1223577309,
+    'message' => 'Anfrage zur Freigabe durch die Committer-Gruppe'
   }
 }
 ;
