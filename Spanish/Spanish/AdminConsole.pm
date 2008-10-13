7c7604685b7acc83b9c44b97f71eb12cc0f48136
Translation Server <translation@plainblack.com>
2008-10-13 19:18:55 +0000

    Update from translation server

--- Spanish/Spanish/AdminConsole.pm
+++ Spanish/Spanish/AdminConsole.pm
@@ -1,17 +1,17 @@
 package WebGUI::i18n::Spanish::AdminConsole;
 use utf8;
 our $I18N = {
-  'toggle off' => {
-    'lastUpdated' => 1169609931,
-    'message' => 'Esconder consola de administraci&oacute;n'
-  },
   'admin console' => {
-    'lastUpdated' => 1216239062,
-    'message' => 'Panel administrativo'
+    'lastUpdated' => 1223925452,
+    'message' => 'Panel Administrativo'
+  },
+  'toggle off' => {
+    'lastUpdated' => 1223925479,
+    'message' => 'Esconder Panel Administrativo'
   },
   'toggle on' => {
-    'lastUpdated' => 1169609951,
-    'message' => 'Mostrar consola de administraci&oacute;n'
+    'lastUpdated' => 1223925492,
+    'message' => 'Mostrar Panel Administrativo'
   }
 }
 ;
