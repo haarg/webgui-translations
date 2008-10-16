0a2f89fc513e2c24012c510bd6697ee554b4acd5
Translation Server <translation@plainblack.com>
2008-10-16 00:45:47 +0000

    Update from translation server

--- Spanish/Spanish/Macro_AdminBar.pm
+++ Spanish/Spanish/Macro_AdminBar.pm
@@ -1,82 +1,91 @@
 package WebGUI::i18n::Spanish::Macro_AdminBar;
 use utf8;
-
 our $I18N = {
-	'icon' => {
-		message => q|&iacute;cono|,
-		lastUpdated => 1199912164
-	},
-
-	'macroName' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'name' => {
-		message => q|nombre|,
-		lastUpdated => 1199912173
-	},
-
-	'12' => {
-		message => q|Ocultal panel de administraci&oacute;n.|,
-		lastUpdated => 1204316255
-	},
-
-	'1082' => {
-		message => q|Portapapeles|,
-		lastUpdated => 1204315447
-	},
-
-	'admin bar title' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'376' => {
-		message => q|Paquete|,
-		lastUpdated => 1204316271
-	},
-
-	'399' => {
-		message => q|Validar esta p&aacute;gina.|,
-		lastUpdated => 1204316299
-	},
-
-	'adminbar_loop' => {
-		message => q|Un bucle que contenga las diferentes listas de datos para mostrar.|,
-		lastUpdated => 1204316348
-	},
-
-	'url' => {
-		message => q|url|,
-		lastUpdated => 1199912249
-	},
-
-	'label' => {
-		message => q|etiqueta|,
-		lastUpdated => 1199912279
-	},
-
-	'title' => {
-		message => q|t&iacute;tulo|,
-		lastUpdated => 1199912181
-	},
-
-	'1083' => {
-		message => q|Nuevo Contenido|,
-		lastUpdated => 1204315472
-	},
-
-	'commit my changes' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'items' => {
-		message => q|Un bucle que contiene la lista de los art&iacute;culos de esta categor&iacute;a.|,
-		lastUpdated => 1204316989
-	},
-
-};
+  '1082' => {
+    'lastUpdated' => 1204315447,
+    'message' => 'Portapapeles'
+  },
+  '1083' => {
+    'lastUpdated' => 1204315472,
+    'message' => 'Nuevo Contenido'
+  },
+  '12' => {
+    'lastUpdated' => 1204316255,
+    'message' => 'Ocultal panel de administraci&oacute;n.'
+  },
+  '376' => {
+    'lastUpdated' => 1204316271,
+    'message' => 'Paquete'
+  },
+  '399' => {
+    'lastUpdated' => 1204316299,
+    'message' => 'Validar esta p&aacute;gina.'
+  },
+  'admin bar title' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'adminbar_loop' => {
+    'lastUpdated' => 1204316348,
+    'message' => 'Un bucle que contenga las diferentes listas de datos para mostrar.'
+  },
+  'basic' => {
+    'lastUpdated' => 1224117726,
+    'message' => 'B&aacute;sico'
+  },
+  'commit my changes' => {
+    'lastUpdated' => 1224117746,
+    'message' => 'Comfirmar mis cambios'
+  },
+  'community' => {
+    'lastUpdated' => 1224117765,
+    'message' => 'Comunidad'
+  },
+  'icon' => {
+    'lastUpdated' => 1199912164,
+    'message' => '&iacute;cono'
+  },
+  'intranet' => {
+    'lastUpdated' => 1224117775,
+    'message' => 'Intranet'
+  },
+  'items' => {
+    'lastUpdated' => 1204316989,
+    'message' => 'Un bucle que contiene la lista de los art&iacute;culos de esta categor&iacute;a.'
+  },
+  'label' => {
+    'lastUpdated' => 1199912279,
+    'message' => 'etiqueta'
+  },
+  'macroName' => {
+    'lastUpdated' => 1224117799,
+    'message' => 'AdminBar'
+  },
+  'name' => {
+    'lastUpdated' => 1199912173,
+    'message' => 'nombre'
+  },
+  'packages' => {
+    'lastUpdated' => 1224117812,
+    'message' => 'Paquetes'
+  },
+  'prototypes' => {
+    'lastUpdated' => 1224117825,
+    'message' => 'Prototipos'
+  },
+  'title' => {
+    'lastUpdated' => 1199912181,
+    'message' => 't&iacute;tulo'
+  },
+  'url' => {
+    'lastUpdated' => 1199912249,
+    'message' => 'url'
+  },
+  'utilities' => {
+    'lastUpdated' => 1224117838,
+    'message' => 'Utilidades'
+  }
+}
+;
 
 1;
