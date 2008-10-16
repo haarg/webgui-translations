f7f01c85e9bd6ef0db7ae24f76abd0c0b95d36f6
Translation Server <translation@plainblack.com>
2008-10-16 00:46:57 +0000

    Update from translation server

--- Spanish/Spanish/Macro_AssetProxy.pm
+++ Spanish/Spanish/Macro_AssetProxy.pm
@@ -1,12 +1,19 @@
 package WebGUI::i18n::Spanish::Macro_AssetProxy;
 use utf8;
-
 our $I18N = {
-	'invalid url' => {
-		message => q|url inv&aacute;lida|,
-		lastUpdated => 1199912146
-	},
-
-};
+  'asset in clipboard' => {
+    'lastUpdated' => 1224117947,
+    'message' => 'AssetProxy: el Activo est&aacute; en el Portapapeles'
+  },
+  'asset in trash' => {
+    'lastUpdated' => 1224117971,
+    'message' => 'AssetProxy: el Activo est&aacute; en la Papelera'
+  },
+  'invalid url' => {
+    'lastUpdated' => 1224118005,
+    'message' => 'AssetProxy: URL al Activo es inv&aacute;lido.'
+  }
+}
+;
 
 1;
