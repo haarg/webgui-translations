81fd48ba4943ba829313f54c58662c7b51dd0d15
Translation Server <translation@plainblack.com>
2008-10-16 01:17:10 +0000

    Update from translation server

--- Spanish/Spanish/Macro_AdminToggle.pm
+++ Spanish/Spanish/Macro_AdminToggle.pm
@@ -1,37 +1,31 @@
 package WebGUI::i18n::Spanish::Macro_AdminToggle;
 use utf8;
-
 our $I18N = {
-	'516' => {
-		message => q|Mostrar Panel Administrativo|,
-		lastUpdated => 1208187263
-	},
-
-	'toggle.url' => {
-		message => q|La URL para activar o desactivar el modo Admin.|,
-		lastUpdated => 1208187317
-	},
-
-	'517' => {
-		message => q|Ocultar Panel Administrativo|,
-		lastUpdated => 1208187279
-	},
-
-	'admin toggle title' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'macroName' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'toggle.text' => {
-		message => q|La etiqueta internacionalizada para mostrar u ocultar el panel administrativo (dependiendo del estado de la macro), o el texto que supla a la macro.|,
-		lastUpdated => 1208209043
-	},
-
-};
+  '516' => {
+    'lastUpdated' => 1208187263,
+    'message' => 'Mostrar Panel Administrativo'
+  },
+  '517' => {
+    'lastUpdated' => 1208187279,
+    'message' => 'Ocultar Panel Administrativo'
+  },
+  'admin toggle title' => {
+    'lastUpdated' => 1224119820,
+    'message' => 'Variables para Plantillas del Macro Admin Toggle'
+  },
+  'macroName' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'toggle.text' => {
+    'lastUpdated' => 1208209043,
+    'message' => 'La etiqueta internacionalizada para mostrar u ocultar el panel administrativo (dependiendo del estado de la macro), o el texto que supla a la macro.'
+  },
+  'toggle.url' => {
+    'lastUpdated' => 1208187317,
+    'message' => 'La URL para activar o desactivar el modo Admin.'
+  }
+}
+;
 
 1;
--- Spanish/Spanish/Macro_EditableToggle.pm
+++ Spanish/Spanish/Macro_EditableToggle.pm
@@ -1,7 +1,31 @@
 package WebGUI::i18n::Spanish::Macro_EditableToggle;
 use utf8;
-
 our $I18N = {
-};
+  '516' => {
+    'lastUpdated' => 1224118048,
+    'message' => 'Activar Administraci&oacute;n'
+  },
+  '517' => {
+    'lastUpdated' => 1224118062,
+    'message' => 'Desactivar Administraci&oacute;n'
+  },
+  'editable toggle title' => {
+    'lastUpdated' => 1224118107,
+    'message' => 'Variables de Plantilla para el Toggle de Edici&oacute;n'
+  },
+  'macroName' => {
+    'lastUpdated' => 1224118140,
+    'message' => 'Toggle de Edici&oacute;n'
+  },
+  'toggle.text' => {
+    'lastUpdated' => 1224119722,
+    'message' => ''
+  },
+  'toggle.url' => {
+    'lastUpdated' => 1224118197,
+    'message' => 'El URL para activar o desactivar el modo de Administraci&oacute;n.'
+  }
+}
+;
 
 1;
