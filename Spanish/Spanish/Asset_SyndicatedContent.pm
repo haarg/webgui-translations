c31cbd127f8770c31a6e9eb7cf97f4a8b78f488a
Translation Server <translation@plainblack.com>
2008-10-20 17:07:29 +0000

    Update from translation server

--- Spanish/Spanish/Asset_SyndicatedContent.pm
+++ Spanish/Spanish/Asset_SyndicatedContent.pm
@@ -1,287 +1,231 @@
 package WebGUI::i18n::Spanish::Asset_SyndicatedContent;
 use utf8;
-
 our $I18N = {
-	'displayMode' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'rssUrl' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'rss.url.2.0' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'rss.url.1.0' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'72 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'rss.url.0.91' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'assetName' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'descriptionFirst2sentences' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'site_link' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'displayModeLabel' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'channel.link' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'syndicated content asset template variables title' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'1' => {
-		message => q|URL al archivo RSS|,
-		lastUpdated => 1207597325
-	},
-
-	'1 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'72' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'get syndicated content' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'hasTermsLabel description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'descriptionFirst50words' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'process macros in rss url' => {
-		message => q|Procesar Macros en RSS URLs|,
-		lastUpdated => 1208549302
-	},
-
-	'descriptionFirst3sentences' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'rssTabName' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'item_loop' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'hasTerms' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'site_title' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'descriptionFirst100words' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'hasTermsLabel' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'rss.url' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'title' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'3 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'interleaved' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'cache timeout help' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'descriptionFirst2paragraphs' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'channel.title' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'descriptionFirstParagraph' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'displayModeSubtext' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'channel.description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'process macros in rss url description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'cache timeout' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'descriptionFirst75words' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'templateId' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'displayModeLabel description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'link' => {
-		message => q|Una URL directamente al contenido del item.|,
-		lastUpdated => 1208549379
-	},
-
-	'cacheTimeout' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'3' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'grouped' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'descriptionFirst4sentences' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'maxHeadlines' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'descriptionFirstSentence' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'descriptionFirst25words' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'new_rss_site' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'rss.url.0.9' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'4' => {
-		message => q|Editar Contenido Sindicado|,
-		lastUpdated => 1207597737
-	},
-
-	'RSS Feed Title Suffix' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'descriptionFirst10words' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'processMacrosInRssUrl' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-};
+  '1' => {
+    'lastUpdated' => 1224521926,
+    'message' => 'URL al archivo RSS'
+  },
+  '1 description' => {
+    'lastUpdated' => 1224522269,
+    'message' => '<p>Indique el URL exacto (comenzando con http://) al archivo RDF o RSS del contenido a sindicar. El contenido a sindicar ser&aacute; descargado desde ese URL una vez cada hora.&nbsp;</p><p>Puede encontrar contenido a sindicar en:&nbsp;</p> <div> <ul><li><a href="http://www.newsisfree.com/">http://www.newsisfree.com</a></li><li><a href="http://www.syndic8.com/">http://www.syndic8.com</a></li><li><a href="http://www.voidstar.com/node.php?id=144">http://www.voidstar.com/node.php?id=144</a></li><li><a href="http://my.userland.com/">http://my.userland.com</a></li><li><a href="http://www.webreference.com/services/news/">http://www.webreference.com/services/news/</a></li><li><a href="http://w.moreover.com/">http://w.moreover.com/</a></li></ul> </div> <p>WebGUI puede manejar RSS versiones .90, .91, 1.0 y 2.0. No soporta fuentes Atom por ahora. Es probable que otros archivos con un formato similar a RSS funcionen.</p><p>Para crear un RSS agregado (uno que obtiene titulares desde varias fuentes RSS), incluya la lista de URLs, uno por l&iacute;nea. Los titulares ser&aacute;n ordenados seg&uacute;n la fecha en la que WebGUI recibi&oacute; la historia por primera vez.</p>'
+  },
+  '3' => {
+    'lastUpdated' => 1224522247,
+    'message' => 'N&uacute;mero M&aacute;ximo de Titulares'
+  },
+  '3 description' => {
+    'lastUpdated' => 1224522341,
+    'message' => 'Introduzca la cantidad m&aacute;xima de titulares que deben ser presentados. Indique cero para permitir una cantidad ilimitada de titulares. Note que todos los titulares son consultados a&uacute;n cuando s&oacute;lo se presenten un subconjunto de ellos.'
+  },
+  '4' => {
+    'lastUpdated' => 1224522378,
+    'message' => 'Editar Contenido Sindicado'
+  },
+  '72' => {
+    'lastUpdated' => 1224522390,
+    'message' => 'Plantilla para Contenido Sindicado'
+  },
+  '72 description' => {
+    'lastUpdated' => 1224522412,
+    'message' => 'Seleccione la Plantilla con la cual presentar el Contenido Sindicado.'
+  },
+  'RSS Feed Title Suffix' => {
+    'lastUpdated' => 1224522426,
+    'message' => 'Fuente RSS 2.0'
+  },
+  'assetName' => {
+    'lastUpdated' => 1224522434,
+    'message' => 'Contenido Sindicado'
+  },
+  'cache timeout' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'cache timeout help' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'cacheTimeout' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'channel.description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'channel.link' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'channel.title' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'descriptionFirst100words' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'descriptionFirst10words' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'descriptionFirst25words' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'descriptionFirst2paragraphs' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'descriptionFirst2sentences' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'descriptionFirst3sentences' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'descriptionFirst4sentences' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'descriptionFirst50words' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'descriptionFirst75words' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'descriptionFirstParagraph' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'descriptionFirstSentence' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'displayMode' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'displayModeLabel' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'displayModeLabel description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'displayModeSubtext' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'get syndicated content' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'grouped' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'hasTerms' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'hasTermsLabel' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'hasTermsLabel description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'interleaved' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'item_loop' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'link' => {
+    'lastUpdated' => 1208549379,
+    'message' => 'Una URL directamente al contenido del item.'
+  },
+  'maxHeadlines' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'new_rss_site' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'process macros in rss url' => {
+    'lastUpdated' => 1208549302,
+    'message' => 'Procesar Macros en RSS URLs'
+  },
+  'process macros in rss url description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'processMacrosInRssUrl' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'rss.url' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'rss.url.0.9' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'rss.url.0.91' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'rss.url.1.0' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'rss.url.2.0' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'rssTabName' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'rssUrl' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'site_link' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'site_title' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'syndicated content asset template variables title' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'templateId' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'title' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  }
+}
+;
 
 1;
