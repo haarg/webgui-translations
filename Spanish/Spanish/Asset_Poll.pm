c6d6c748978ce64513053a88a4c5c8a69b9540ac
Translation Server <translation@plainblack.com>
2008-10-20 15:50:09 +0000

    Update from translation server

--- Spanish/Spanish/Asset_Poll.pm
+++ Spanish/Spanish/Asset_Poll.pm
@@ -2,212 +2,212 @@ package WebGUI::i18n::Spanish::Asset_Poll;
 use utf8;
 our $I18N = {
   '10' => {
-    'lastUpdated' => 1204217590,
-    'message' => 'Restablecer votos?'
+    'lastUpdated' => 1224516176,
+    'message' => '&iquest;Poner en cero la votaci&oacute;n?'
   },
   '10 description' => {
-    'lastUpdated' => 1204217621,
-    'message' => 'Restablecer los votos en esta encuesta. Esta opci&oacute;n s&oacute;lo est&aacute; disponible cuando se edita una encuesta existente.'
+    'lastUpdated' => 1224516211,
+    'message' => 'Restablecer a cero la votaci&oacute;n en la Encuesta. Esta opci&oacute;n s&oacute;lo est&aacute; disponible cuando se edita una Encuesta existente.'
   },
   '11' => {
-    'lastUpdated' => 1204217641,
-    'message' => 'Vote!'
+    'lastUpdated' => 1224516222,
+    'message' => '&iexcl;Votar!'
   },
   '12' => {
-    'lastUpdated' => 1204217650,
-    'message' => 'Total de votos'
+    'lastUpdated' => 1224516240,
+    'message' => 'Votos Recibidos'
   },
   '20' => {
-    'lastUpdated' => 1204217661,
+    'lastUpdated' => 1224516248,
     'message' => 'Karma por voto'
   },
   '20 description' => {
-    'lastUpdated' => 1204217690,
-    'message' => '&iquest;Cu&aacute;nto karma se debe dar a un usuario cuando votan? Esta opci&oacute;n s&oacute;lo est&aacute; disponible si el karma est&aacute; activado en la configuraci&oacute;n. La cantidad por defecto es 0.'
+    'lastUpdated' => 1224516281,
+    'message' => '&iquest;Cu&aacute;nto karma se debe dar a un usuario cuando emite un voto? Esta opci&oacute;n s&oacute;lo est&aacute; disponible si el karma est&aacute; activado en la configuraci&oacute;n. La cantidad de karma por defecto es 0.'
   },
   '3' => {
-    'lastUpdated' => 1204217717,
-    'message' => 'Activo'
+    'lastUpdated' => 1224516296,
+    'message' => 'Activa'
   },
   '3 description' => {
-    'lastUpdated' => 1204217739,
-    'message' => 'Si esta casilla est&aacute; marcada, los usuarios podr&aacute;n votar. De lo contrario s&oacute;lo podr&aacute;s ver los resultados de la encuesta.'
+    'lastUpdated' => 1224516312,
+    'message' => 'Si esta casilla est&aacute; marcada, los usuarios podr&aacute;n votar. De lo contrario s&oacute;lo podr&aacute;n ver los resultados de la encuesta.'
   },
   '4' => {
-    'lastUpdated' => 1204217751,
-    'message' => 'Qui&eacute;n puede votar?'
+    'lastUpdated' => 1224516328,
+    'message' => '&iquest;Qui&eacute;nes puede votar?'
   },
   '4 description' => {
-    'lastUpdated' => 1204217866,
-    'message' => 'Elija un grupo que puede votar en esta encuesta. El grupo por defecto es Todos.'
+    'lastUpdated' => 1224516348,
+    'message' => 'Elija el grupo que puede votar en esta Encuesta. El grupo por defecto es Todos.'
   },
   '5' => {
-    'lastUpdated' => 1204217887,
-    'message' => 'Ancho de gr&aacute;fico'
+    'lastUpdated' => 1224516363,
+    'message' => 'Anchura del gr&aacute;fico'
   },
   '5 description' => {
-    'lastUpdated' => 1204217914,
-    'message' => '<div id="result_box" dir="ltr">La anchura del gr&aacute;fico de los resultados de una encuesta. El ancho se mide en p&iacute;xeles. El valor por defecto es 150 p&iacute;xeles de ancho.</div>'
+    'lastUpdated' => 1224516416,
+    'message' => 'Anchura en pixels del gr&aacute;fico construido para mostrar los resultados de una Encuesta. El valor por omisi&oacute;n es 150 pixels de ancho.'
   },
   '6' => {
-    'lastUpdated' => 1204217931,
+    'lastUpdated' => 1224516424,
     'message' => 'Pregunta'
   },
   '6 description' => {
-    'lastUpdated' => 1204217963,
+    'lastUpdated' => 1224516433,
     'message' => '&iquest;Cu&aacute;l es la pregunta que le gustar&iacute;a hacer a sus usuarios?'
   },
   '7' => {
-    'lastUpdated' => 1204217972,
+    'lastUpdated' => 1224516438,
     'message' => 'Respuestas'
   },
   '7 description' => {
-    'lastUpdated' => 1204218028,
-    'message' => 'Introduzca las posibles respuestas a su pregunta. Introduzca s&oacute;lo una respuesta en cada l&iacute;nea. Encuestas s&oacute;lo son capaces con 20 posibles respuestas.'
+    'lastUpdated' => 1224516489,
+    'message' => 'Introduzca las respuestas posibles para su pregunta. Escriba una respuesta por l&iacute;nea hasta un m&aacute;ximo de 20 l&iacute;neas.'
   },
   '72' => {
-    'lastUpdated' => 1204218061,
-    'message' => 'Seleccionar respuestas aleatoriamente ?'
+    'lastUpdated' => 1224516523,
+    'message' => '&iquest;Presentar respuestas al azar?'
   },
   '72 description' => {
-    'lastUpdated' => 1204218121,
-    'message' => 'Con el fin de tener la seguridad de que el orden de las respuestas en la encuesta no sesgo a sus usuarios, a menudo es &uacute;til para presentar las opciones en un orden aleatorio cada vez que se muestran. Seleccione &quot;s&iacute;&quot; a seleccionar aleatoriamente las respuestas en la encuesta.'
+    'lastUpdated' => 1224516613,
+    'message' => 'Para evitar que el orden de las respuestas pueda crear un sesgo en sus usuarios, puede ser &uacute;til reordenarlas al azar cada vez que son presentadas. Indique &quot;s&iacute;&quot; para ordenar las respuestas al azar antes de cada presentaci&oacute;n.'
   },
   '73' => {
-    'lastUpdated' => 1204218149,
-    'message' => 'Plantilla de la encuesta'
+    'lastUpdated' => 1224516628,
+    'message' => 'Plantilla para Encuesta'
   },
   '73 description' => {
-    'lastUpdated' => 1204218172,
-    'message' => 'Selecciona una plantilla para mostrar tu encuesta'
+    'lastUpdated' => 1224516650,
+    'message' => 'Seleccione la plantilla utilizada para mostrar la Encuesta'
   },
   '8' => {
-    'lastUpdated' => 1204218197,
-    'message' => '(Entre una respuesta por l&iacute;nea, no mas de 20)'
+    'lastUpdated' => 1224516674,
+    'message' => '(Escriba una respuesta por l&iacute;nea, no m&aacute;s de 20)'
   },
   '9' => {
-    'lastUpdated' => 1204218213,
-    'message' => 'Editar encuesta'
+    'lastUpdated' => 1224516682,
+    'message' => 'Editar Encuesta'
   },
   'aN' => {
-    'lastUpdated' => 1204218258,
-    'message' => '<div id="result_box" dir="ltr">Las variables a1 hasta a20 almacenan las respuestas de la encuesta.</div>'
+    'lastUpdated' => 1224516709,
+    'message' => 'Las variables a1 hasta a20 almacenan las respuestas posibles para la Encuesta.'
   },
   'active' => {
-    'lastUpdated' => 1221663075,
-    'message' => 'Un condicional que ser&aacute; cierto si la encuesta est&aacute; abierta a recibir votos.'
+    'lastUpdated' => 1224516737,
+    'message' => 'Un condicional que indica si la encuesta est&aacute; abierta a recibir votos (activa).'
   },
   'answer.form' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224516763,
+    'message' => 'El bot&oacute;n selector de esta respuesta.'
   },
   'answer.graphWidth' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224516814,
+    'message' => 'La anchura que debe tener el gr&aacute;fico a generar para esta respuesta. Se interpreta como un porcentaje del tama&ntilde;o total para el gr&aacute;fico.'
   },
   'answer.number' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224516842,
+    'message' => 'El ordinal de esta respuesta.'
   },
   'answer.percent' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224516859,
+    'message' => 'El porcentaje de la votaci&oacute;n total que ha recibido esta respuesta.'
   },
   'answer.text' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224516868,
+    'message' => 'El texto de esta respuesta.'
   },
   'answer.total' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224516884,
+    'message' => 'El n&uacute;mero total de votos que ha recibido esta respuesta.'
   },
   'answer_loop' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224516910,
+    'message' => 'Una iteraci&oacute;n que contiene la informaci&oacute;n para todas las respuestas de la Encuesta.'
   },
   'assetName' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224516923,
+    'message' => 'Encuesta'
   },
   'canVote' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224516953,
+    'message' => 'Un condicional que indica si el usuario actual puede votar en esta Encuesta.'
   },
   'form.end' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224516970,
+    'message' => 'El final del formulario para la Encuesta.'
   },
   'form.start' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224516981,
+    'message' => 'El inicio del formulario para la Encuesta.'
   },
   'form.submit' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224516993,
+    'message' => 'El bot&oacute;n para emitir votos en la Encuesta.'
   },
   'generate graph' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224517008,
+    'message' => 'Generar el gr&aacute;fico'
   },
   'generate graph description' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224517041,
+    'message' => 'Seleccione \'Si\' para activar la generaci&oacute;n del gr&aacute;fico con resultados.'
   },
   'generateGraph' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224517063,
+    'message' => 'Un condicional que indica si la Encuesta debe generar un gr&aacute;fico con los resultados.'
   },
   'graphConfiguration' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224517076,
+    'message' => 'Tipo de gr&aacute;fico a generar.'
   },
   'graphUrl' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224517136,
+    'message' => 'URL para el gr&aacute;fico generado por la Encuesta. Solamente puede definirse si la Encuesta tiene activada la generaci&oacute;n de un gr&aacute;fico con los resultados y el gr&aacute;fico ha sido configurado correctamente.'
   },
   'graphWidth' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224517151,
+    'message' => 'Anchura del gr&aacute;fico a generar (en pixels).'
   },
   'hasImageGraph' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224517180,
+    'message' => 'Un condicional que indica si la Encuesta tiene un gr&aacute;fico generado.'
   },
   'karmaPerVote' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224517573,
+    'message' => 'La cantidad de karma que se otorga a los usuarios cuando votan.'
   },
   'poll asset template variables body' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224517633,
+    'message' => 'Cada Activo provee un conjunto de variables basadas en sus propiedades internas, que pueden ser utilizadas en la mayor&iacute;a de las Plantillas. La utilidad de las variables depende de la plantilla particular.'
   },
   'poll asset template variables title' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224517650,
+    'message' => 'Variables para Plantillas del Activo Encuesta'
   },
   'question' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224517672,
+    'message' => 'La pregunta a formular en la Encuesta.'
   },
   'randomizeAnswers' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1224517694,
+    'message' => 'Un condicional que indica si las respuestas deben reordenarse al azar antes de cada presentaci&oacute;n.'
   },
   'responses.label' => {
-    'lastUpdated' => 1215812365,
-    'message' => 'La etiqueta para el total de respuestas.&nbsp; &quot;Total Votos&quot;'
+    'lastUpdated' => 1224517733,
+    'message' => 'La etiqueta para el total de respuestas. &quot;Votos Recibidos&quot;'
   },
   'responses.total' => {
-    'lastUpdated' => 1215812332,
-    'message' => 'El n&uacute;mero total de votos que han sido colocados en esta encuesta.'
+    'lastUpdated' => 1224517755,
+    'message' => 'El n&uacute;mero total de votos recibidos en esta encuesta.'
   },
   'templateId' => {
-    'lastUpdated' => 1215812301,
-    'message' => 'El Id del template usado para mostrar este activo.'
+    'lastUpdated' => 1224517782,
+    'message' => 'ID de la Plantilla usada para mostrar el Activo Encuesta.'
   },
   'voteGroup' => {
-    'lastUpdated' => 1215812272,
-    'message' => 'El ID del grupo que puede votar en la encuesta.'
+    'lastUpdated' => 1224517800,
+    'message' => 'ID del Grupo que puede votar en la Encuesta.'
   }
 }
 ;
