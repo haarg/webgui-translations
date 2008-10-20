eb342600ab5d60f7cb6dc67d777bb4fbc4b9b460
Translation Server <translation@plainblack.com>
2008-10-20 20:46:14 +0000

    Update from translation server

--- Spanish/Spanish/Asset_ZipArchive.pm
+++ Spanish/Spanish/Asset_ZipArchive.pm
@@ -1,7 +1,99 @@
 package WebGUI::i18n::Spanish::Asset_ZipArchive;
 use utf8;
-
 our $I18N = {
-};
+  'assetName' => {
+    'lastUpdated' => 1224534880,
+    'message' => 'Archivo ZIP'
+  },
+  'bad_archive' => {
+    'lastUpdated' => 1224534920,
+    'message' => 'No es un Archivo ZIP v&aacute;lido. Por favor utilice archivos ZIP o TAR.'
+  },
+  'controls' => {
+    'lastUpdated' => 1224534938,
+    'message' => 'Controles para Administraci&oacute;n del Activo.'
+  },
+  'error' => {
+    'lastUpdated' => 1224534968,
+    'message' => 'Cualquier error reportado durante la carga o descarga de archivos en el Archivo ZIP.'
+  },
+  'fileIcon' => {
+    'lastUpdated' => 1224534986,
+    'message' => 'Icono asociado al tipo de archivo'
+  },
+  'fileUrl' => {
+    'lastUpdated' => 1224534996,
+    'message' => 'URL al archivo inicial'
+  },
+  'noFileSpecified' => {
+    'lastUpdated' => 1224535011,
+    'message' => 'Error: no se especific&oacute; ning&uacute;n archivo.'
+  },
+  'noFileSpecified var' => {
+    'lastUpdated' => 1224535116,
+    'message' => 'Mensaje de error a emitir cuando no se especifique un archivo.'
+  },
+  'noInitialPage' => {
+    'lastUpdated' => 1224535054,
+    'message' => 'Error: no se especific&oacute; p&aacute;gina inicial.'
+  },
+  'noInitialPage var' => {
+    'lastUpdated' => 1224535098,
+    'message' => 'Mensaje de error a emitir cuando no se especifique p&aacute;gina inicial.'
+  },
+  'show page' => {
+    'lastUpdated' => 1224535129,
+    'message' => 'P&aacute;gina Inicial'
+  },
+  'show page description' => {
+    'lastUpdated' => 1224535170,
+    'message' => 'Escriba el nombre del archivo que sirve como base para este Archivo ZIP. Esta ser&aacute; la p&aacute;gina que ser&aacute; servida inicialmente.'
+  },
+  'showPage' => {
+    'lastUpdated' => 1224535204,
+    'message' => 'El nombre del archivo contenido en el Archivo ZIP que ser&aacute; mostrado cuando el Activo sea examinado.'
+  },
+  'template label' => {
+    'lastUpdated' => 1224535218,
+    'message' => 'Plantilla para Archivo ZIP'
+  },
+  'templateId' => {
+    'lastUpdated' => 1224535242,
+    'message' => 'ID de la Plantilla utilizada para mostrar los contenidos del Archivo ZIP.'
+  },
+  'unzip_error' => {
+    'lastUpdated' => 1224535272,
+    'message' => 'El Archivo ZIP no pudo ser expandido. Por favor cargue un archivo v&aacute;lido.'
+  },
+  'za_error' => {
+    'lastUpdated' => 1224535374,
+    'message' => 'Este Activo solamente acepta archivos ZIP y TAR.'
+  },
+  'za_show_error' => {
+    'lastUpdated' => 1224535399,
+    'message' => 'Debe indicar una p&aacute;gina inicial para los usuarios.'
+  },
+  'zip archive asset variables body' => {
+    'lastUpdated' => 1224535435,
+    'message' => 'Las siguientes variables se incluyen en las Plantillas para Archivos ZIP.'
+  },
+  'zip archive asset variables title' => {
+    'lastUpdated' => 1224535460,
+    'message' => 'Archivo Zip, Variables para Plantillas'
+  },
+  'zip archive template body' => {
+    'lastUpdated' => 1224535486,
+    'message' => 'Las siguientes variables para plantillas est&aacute;n disponibles para los Archivos ZIP:'
+  },
+  'zip archive template title' => {
+    'lastUpdated' => 1224535504,
+    'message' => 'Archivo ZIP, Variables para Plantillas'
+  },
+  'zip_error' => {
+    'lastUpdated' => 1224535558,
+    'message' => 'Ocurri&oacute; un error al tratar de expandir el archivo. Verifique que el archivo no est&aacute; protegido con clave y que es accesible desde el sistema operativo.'
+  }
+}
+;
 
 1;
