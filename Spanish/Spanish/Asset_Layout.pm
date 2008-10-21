0206b6a66e40335ba44e5277c8372997d875543f
Translation Server <translation@plainblack.com>
2008-10-21 13:02:26 +0000

    Update from translation server

--- Spanish/Spanish/Asset_Layout.pm
+++ Spanish/Spanish/Asset_Layout.pm
@@ -1,122 +1,107 @@
 package WebGUI::i18n::Spanish::Asset_Layout;
 use utf8;
-
 our $I18N = {
-	'dragger.init' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'content' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'assetsToHide' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'layout asset template variables title' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'assetName' => {
-		message => q|Diagrama de p&aacute;gina|,
-		lastUpdated => 1207242142
-	},
-
-	'contentPositions' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'dragger.icon' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'asset order label' => {
-		message => q|Adicionar nuevos Activos|,
-		lastUpdated => 1207240416
-	},
-
-	'asset order hoverhelp' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'assets to hide description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'id' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'asset order desc' => {
-		message => q|En la parte de arriba|,
-		lastUpdated => 1207240304
-	},
-
-	'templateId' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'position1_loop' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'asset order asc' => {
-		message => q|En la parte baja.|,
-		lastUpdated => 1207240290
-	},
-
-	'847' => {
-		message => q|Volver a la p&aacute;gina actual.|,
-		lastUpdated => 1207240250
-	},
-
-	'assets to hide' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'layout template body' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'showAdmin' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'layout asset template variables body' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'layout template title' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'template description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'823' => {
-		message => q|Ir a la nueva p&aacute;gina.|,
-		lastUpdated => 1207240214
-	},
-
-};
+  '823' => {
+    'lastUpdated' => 1224593136,
+    'message' => 'Ir a la p&aacute;gina reci&eacute;n creada.'
+  },
+  '847' => {
+    'lastUpdated' => 1224593147,
+    'message' => 'Regresar a la p&aacute;gina actual.'
+  },
+  'asset order asc' => {
+    'lastUpdated' => 1224593336,
+    'message' => 'Hacia el fondo'
+  },
+  'asset order desc' => {
+    'lastUpdated' => 1224593349,
+    'message' => 'Hacia el tope'
+  },
+  'asset order hoverHelp' => {
+    'lastUpdated' => 1224593319,
+    'message' => 'Indique si el nuevo contenido que se agrega en las posiciones para contenido de la p&aacute;gina, deben agregarse hacia el tope o hacia el fondo.'
+  },
+  'asset order hoverhelp' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'asset order label' => {
+    'lastUpdated' => 1224593371,
+    'message' => 'Los nuevos Activos se agregan'
+  },
+  'assetName' => {
+    'lastUpdated' => 1224593403,
+    'message' => 'Diagrama de P&aacute;gina'
+  },
+  'assets to hide' => {
+    'lastUpdated' => 1224593601,
+    'message' => 'Esconder los Activos'
+  },
+  'assets to hide description' => {
+    'lastUpdated' => 1224593583,
+    'message' => 'La lista presenta un selector por cada Activo hijo del Diagrama de P&aacute;gina. Seleccione aquellos Activos que no deben ser mostrados en el Activo Diagrama de P&aacute;gina.'
+  },
+  'assetsToHide' => {
+    'lastUpdated' => 1224593642,
+    'message' => 'Los ID de los Activos que no deben mostrarse en el Activo Diagrama de P&aacute;gina, separados por saltos de l&iacute;nea.'
+  },
+  'content' => {
+    'lastUpdated' => 1224593662,
+    'message' => 'El contenido del Activo.'
+  },
+  'contentPositions' => {
+    'lastUpdated' => 1224593711,
+    'message' => 'Una cadena que describe las posiciones en las cuales pueden presentarse Activos, as&iacute; como la ubicaci&oacute;n y el orden de los Activos en cada posici&oacute;n.'
+  },
+  'dragger.icon' => {
+    'lastUpdated' => 1224593791,
+    'message' => 'Un &iacute;cono empleado para cambiar la posici&oacute;n del Activo mediante una operaci&oacute;n de click y arrastre. Si la variable showAdmin es falsa, esta variable estar&aacute; vac&iacute;a.'
+  },
+  'dragger.init' => {
+    'lastUpdated' => 1224593843,
+    'message' => 'Los fragmentos HTML y Javascript necesarios para que la funcionalidad de click y arrastre opere. Si la variable showAdmin es falsa, esta variable estar&aacute; vac&iacute;a.'
+  },
+  'id' => {
+    'lastUpdated' => 1224593853,
+    'message' => 'ID del Activo'
+  },
+  'isUncommitted' => {
+    'lastUpdated' => 1224593887,
+    'message' => 'Un condicional que es cierto si el contenido es definitivo (committed).'
+  },
+  'layout asset template variables body' => {
+    'lastUpdated' => 1224593925,
+    'message' => 'Cada Activo provee una serie de variables basadas en sus propiedades internas, que pueden ser aprovechadas en las plantillas.'
+  },
+  'layout asset template variables title' => {
+    'lastUpdated' => 1224593943,
+    'message' => 'Variables para Plantillas del Activo Diagrama de P&aacute;gina'
+  },
+  'layout template body' => {
+    'lastUpdated' => 1224593976,
+    'message' => 'Las siguientes variables para plantillas est&aacute;n disponibles en el Activo Diagrama de P&aacute;gina:'
+  },
+  'layout template title' => {
+    'lastUpdated' => 1224593991,
+    'message' => 'Plantilla para Diagrama de P&aacute;gina'
+  },
+  'position1_loop' => {
+    'lastUpdated' => 1224594051,
+    'message' => 'Cada posici&oacute;n en la plantilla tiene una iteraci&oacute;n asociada que contiene todos los Activos a presentar en ella. Aquellos Activos que no hayan sido ubicados en una posici&oacute;n espec&iacute;fica, son ubicados en la Posici&oacute;n 1.'
+  },
+  'showAdmin' => {
+    'lastUpdated' => 1224594081,
+    'message' => 'Un condicional que indica si el modo administrativo est&aacute; activo.'
+  },
+  'template description' => {
+    'lastUpdated' => 1224594120,
+    'message' => 'Seleccione la plantilla a usar para la presentaci&oacute;n del Activo Diagrama de P&aacute;gina y sus contenidos.'
+  },
+  'templateId' => {
+    'lastUpdated' => 1224594136,
+    'message' => 'ID de la plantilla empleada para presentar el Activo.'
+  }
+}
+;
 
 1;
