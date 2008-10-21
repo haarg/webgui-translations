359b770bf326c52c75633822b88ef6d614bc90ea
Translation Server <translation@plainblack.com>
2008-10-21 22:17:46 +0000

    Update from translation server

--- Spanish/Spanish/Asset_Search.pm
+++ Spanish/Spanish/Asset_Search.pm
@@ -1,137 +1,111 @@
 package WebGUI::i18n::Spanish::Asset_Search;
 use utf8;
-
 our $I18N = {
-	'search asset template variables title' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'search' => {
-		message => q|Buscar|,
-		lastUpdated => 1209486904
-	},
-
-	'search template' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'classLimiter' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'class limiter' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'assetName' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'synopsis' => {
-		message => q|El resumen del activo encontrado en la b&uacute;squeda.|,
-		lastUpdated => 1209487068
-	},
-
-	'no_results' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'assetId' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'url' => {
-		message => q|La URL del activo encontrada en la b&uacute;squeda.|,
-		lastUpdated => 1209487187
-	},
-
-	'searchRoot' => {
-		message => q|El ID del activo donde la b&uacute;squeda comenzar&iacute;a.|,
-		lastUpdated => 1209487109
-	},
-
-	'form_submit' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'class limiter description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'result_set' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'useContainers help' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'templateId' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'search root' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'form_footer' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'useContainers' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'search root description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'results_found' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'form_header' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'form_keywords' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'search template description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'no results' => {
-		message => q|No fue encontrado ning&uacute;n resultado.|,
-		lastUpdated => 1209487024
-	},
-
-	'title' => {
-		message => q|El t&iacute;tulo del Activo encontrado en la b&uacute;squeda|,
-		lastUpdated => 1208549428
-	},
-
-};
+  'assetId' => {
+    'lastUpdated' => 1224625407,
+    'message' => 'ID del Activo encontrado por la B&uacute;squeda.'
+  },
+  'assetName' => {
+    'lastUpdated' => 1224625397,
+    'message' => 'B&uacute;squeda'
+  },
+  'class limiter' => {
+    'lastUpdated' => 1224625498,
+    'message' => 'Solamente Activos de tipo:'
+  },
+  'class limiter description' => {
+    'lastUpdated' => 1224625511,
+    'message' => 'Esto limitar&aacute; la B&uacute;squeda de manera que solamente se efect&uacute;e sobre Activos de los tipos indicados.'
+  },
+  'classLimiter' => {
+    'lastUpdated' => 1224625482,
+    'message' => 'Una cadena con los tipos de Activo a buscar.'
+  },
+  'form_footer' => {
+    'lastUpdated' => 1224625533,
+    'message' => 'C&oacute;digo HTML para finalizar el formulario de B&uacute;squeda.'
+  },
+  'form_header' => {
+    'lastUpdated' => 1224625552,
+    'message' => 'C&oacute;digo HTML para el encabezado del formulario de b&uacute;squeda'
+  },
+  'form_keywords' => {
+    'lastUpdated' => 1224625590,
+    'message' => 'Un formulario para permitir al usuario introducir las palabras claves de la B&uacute;squeda.'
+  },
+  'form_submit' => {
+    'lastUpdated' => 1224625608,
+    'message' => 'El bot&oacute;n que permite al usuario iniciar la B&uacute;squeda.'
+  },
+  'no results' => {
+    'lastUpdated' => 1224625670,
+    'message' => 'La b&uacute;squeda no encontr&oacute; nada.'
+  },
+  'no_results' => {
+    'lastUpdated' => 1224625695,
+    'message' => 'El mensaje para indicar al usuario que no se encontraron resultados durante la B&uacute;squeda.'
+  },
+  'result_set' => {
+    'lastUpdated' => 1224625831,
+    'message' => 'Conjunto resultante mostrado p&aacute;gina a p&aacute;gina y con controles de movimiento de p&aacute;ginas. Las variables indicadas a continuaci&oacute;n son las m&aacute;s importantes, sin embargo recuerde que todas las propiedades de los Activos encontrados est&aacute;n expuestas a trav&eacute;s de sus variables para plantilla particulares, y pueden ser utilizadas en &eacute;sta plantilla.'
+  },
+  'results_found' => {
+    'lastUpdated' => 1224625848,
+    'message' => 'Un condicional que indica si se encontraron resultados o no.'
+  },
+  'search' => {
+    'lastUpdated' => 1224625957,
+    'message' => 'Buscar'
+  },
+  'search asset template variables title' => {
+    'lastUpdated' => 1224626006,
+    'message' => 'Variables para Plantilla del Activo B&uacute;squeda'
+  },
+  'search root' => {
+    'lastUpdated' => 1224626258,
+    'message' => 'Ra&iacute;z de B&uacute;squeda'
+  },
+  'search root description' => {
+    'lastUpdated' => 1224626312,
+    'message' => 'La B&uacute;squeda comenzar&aacute; a partir del Activo seleccionado, cubriendo ese y todos los Activos por debajo en la jerarqu&iacute;a.'
+  },
+  'search template' => {
+    'lastUpdated' => 1224626334,
+    'message' => 'Plantilla de B&uacute;squeda'
+  },
+  'search template description' => {
+    'lastUpdated' => 1224626371,
+    'message' => 'La plantilla a utilizar para presentar tanto el formulario como los resultados de la B&uacute;squeda.'
+  },
+  'searchRoot' => {
+    'lastUpdated' => 1224626402,
+    'message' => 'ID del Activo donde comenz&oacute; la B&uacute;squeda.'
+  },
+  'synopsis' => {
+    'lastUpdated' => 1224626436,
+    'message' => 'El resumen del Activo encontrado durante la B&uacute;squeda.'
+  },
+  'templateId' => {
+    'lastUpdated' => 1224626496,
+    'message' => 'ID de la Plantilla utilizada para mostrar el Activo B&uacute;squeda.'
+  },
+  'title' => {
+    'lastUpdated' => 1224626513,
+    'message' => 'El t&iacute;tulo del Activo encontrado durante la B&uacute;squeda'
+  },
+  'url' => {
+    'lastUpdated' => 1224626682,
+    'message' => 'URL del Activo encontrado durante la B&uacute;squeda.'
+  },
+  'useContainers' => {
+    'lastUpdated' => 1224627421,
+    'message' => '&iquest;P&aacute;ginas completas como resultados?'
+  },
+  'useContainers help' => {
+    'lastUpdated' => 1224627392,
+    'message' => 'Indique &quot;Si&quot; para que los resultados de la b&uacute;squeda retornen las p&aacute;ginas completas conteniendo los Activos encontrados, en lugar de simplemente los Activos.'
+  }
+}
+;
 
 1;
