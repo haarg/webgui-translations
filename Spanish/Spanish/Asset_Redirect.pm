b570d71a781aa9b523feba2f4fa73335d58dd84a
Translation Server <translation@plainblack.com>
2008-10-21 12:32:57 +0000

    Update from translation server

--- Spanish/Spanish/Asset_Redirect.pm
+++ Spanish/Spanish/Asset_Redirect.pm
@@ -1,47 +1,55 @@
 package WebGUI::i18n::Spanish::Asset_Redirect;
 use utf8;
-
 our $I18N = {
-	'edit the redirect properties' => {
-		message => q|Editar las propiedades de&nbsp; la redirecci&oacute;n.|,
-		lastUpdated => 1212184372
-	},
-
-	'redirect url' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'assetName' => {
-		message => q|Redireccionar|,
-		lastUpdated => 1212184308
-	},
-
-	'redirect url description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'go to the redirect url' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'self_referential' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'go to the redirect parent page' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'what do you want to do with this redirect' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-};
+  '301 Moved Permanently' => {
+    'lastUpdated' => 1224591995,
+    'message' => '301 Movido Permanentemente'
+  },
+  '302 Moved Temporarily' => {
+    'lastUpdated' => 1224592009,
+    'message' => '302 Movido Temporalmente'
+  },
+  'Redirect Type' => {
+    'lastUpdated' => 1224592020,
+    'message' => 'Tipo de Redirecci&oacute;n'
+  },
+  'assetName' => {
+    'lastUpdated' => 1224592029,
+    'message' => 'Redirecci&oacute;n'
+  },
+  'edit the redirect properties' => {
+    'lastUpdated' => 1224592046,
+    'message' => 'Editar propiedades de la Redirecci&oacute;n.'
+  },
+  'go to the redirect parent page' => {
+    'lastUpdated' => 1224592193,
+    'message' => 'Ir a la p&aacute;gina padre de la Redirecci&oacute;n'
+  },
+  'go to the redirect url' => {
+    'lastUpdated' => 1224592218,
+    'message' => 'Ir al URL de redirecci&oacute;n.'
+  },
+  'redirect type description' => {
+    'lastUpdated' => 1224592243,
+    'message' => 'El tipo de encabezado HTTP que el Activo Redirecci&oacute;n enviar&aacute;.'
+  },
+  'redirect url' => {
+    'lastUpdated' => 1224592299,
+    'message' => 'Redirigir hacia el URL'
+  },
+  'redirect url description' => {
+    'lastUpdated' => 1224592283,
+    'message' => 'URL al cual redirigir al usuario.'
+  },
+  'self_referential' => {
+    'lastUpdated' => 1224592349,
+    'message' => 'La Redirecci&oacute;n redirige a si misma.'
+  },
+  'what do you want to do with this redirect' => {
+    'lastUpdated' => 1224592367,
+    'message' => '&iquest;Qu&eacute; desea hacer con esta Redirecci&oacute;n?'
+  }
+}
+;
 
 1;
