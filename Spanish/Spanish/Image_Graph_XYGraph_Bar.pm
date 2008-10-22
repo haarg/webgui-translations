f78aea8a5fca5c701fc37fa32a10d2e78f910f98
Translation Server <translation@plainblack.com>
2008-10-22 15:35:25 +0000

    Update from translation server

--- Spanish/Spanish/Image_Graph_XYGraph_Bar.pm
+++ Spanish/Spanish/Image_Graph_XYGraph_Bar.pm
@@ -1,7 +1,23 @@
 package WebGUI::i18n::Spanish::Image_Graph_XYGraph_Bar;
 use utf8;
-
 our $I18N = {
-};
+  'bar spacing' => {
+    'lastUpdated' => 1224689652,
+    'message' => 'Espacio entre Barras'
+  },
+  'bar spacing description' => {
+    'lastUpdated' => 1224689684,
+    'message' => 'Establece la distancia en pixels entre las barras de un grupo.'
+  },
+  'group spacing' => {
+    'lastUpdated' => 1224689700,
+    'message' => 'Espacio entre Grupos'
+  },
+  'group spacing description' => {
+    'lastUpdated' => 1224689714,
+    'message' => 'Establece la distancia en pixels entre grupos de barras.'
+  }
+}
+;
 
 1;
