36d787f0351d0038cb74bd88e6a7d2a998bef540
Translation Server <translation@plainblack.com>
2008-10-22 15:33:33 +0000

    Update from translation server

--- Spanish/Spanish/Image_Graph_XYGraph.pm
+++ Spanish/Spanish/Image_Graph_XYGraph.pm
@@ -1,7 +1,79 @@
 package WebGUI::i18n::Spanish::Image_Graph_XYGraph;
 use utf8;
-
 our $I18N = {
-};
+  'axis color' => {
+    'lastUpdated' => 1224689004,
+    'message' => 'Color para Ejes'
+  },
+  'axis color description' => {
+    'lastUpdated' => 1224689035,
+    'message' => 'Color a utilizar para los ejes del Gr&aacute;fico.'
+  },
+  'chart height' => {
+    'lastUpdated' => 1224689053,
+    'message' => 'Altura'
+  },
+  'chart height description' => {
+    'lastUpdated' => 1224689101,
+    'message' => 'Altura del Gr&aacute;fico XY en pixels. Debe ser menor que la altura del Gr&aacute;fico que le contiene.'
+  },
+  'chart width' => {
+    'lastUpdated' => 1224689114,
+    'message' => 'Anchura'
+  },
+  'chart width description' => {
+    'lastUpdated' => 1224689135,
+    'message' => 'Anchura del Gr&aacute;fico XY en pixels. Debe ser menor que la anchura del Gr&aacute;fico que le contiene.'
+  },
+  'draw axis' => {
+    'lastUpdated' => 1224689198,
+    'message' => 'Mostrar Ejes'
+  },
+  'draw axis description' => {
+    'lastUpdated' => 1224689171,
+    'message' => 'Seleccione &quot;Si&quot; si desea dibujar los ejes del Gr&aacute;fico XY.'
+  },
+  'draw labels' => {
+    'lastUpdated' => 1224689209,
+    'message' => 'Mostrar Etiquetas'
+  },
+  'draw labels description' => {
+    'lastUpdated' => 1224689233,
+    'message' => 'Seleccione &quot;Si&quot; para mostrar las etiquetas del Gr&aacute;fico XY.'
+  },
+  'draw mode' => {
+    'lastUpdated' => 1224689248,
+    'message' => 'Modo de Presentaci&oacute;n'
+  },
+  'draw mode description' => {
+    'lastUpdated' => 1224689432,
+    'message' => 'Define la forma en que se presentar&aacute;n los conjuntos de datos. Si se selecciona &quot;Lado a lado&quot;, las barras de diferentes conjuntos de datos ser&aacute;n presentadas una al lado de otra, facilitando la comparaci&oacute;n de las medidas individuales. Si se seleccionado &quot;Apilado&quot;, las barras de diferentes conjuntos de datos ser&aacute;n presentadas una encima de otra, facilitando la comparaci&oacute;n de los valores totales. No tiene ning&uacute;n efecto en Gr&aacute;ficos con un conjunto de datos &uacute;nico (por ejemplo, una Encuesta).'
+  },
+  'draw rulers' => {
+    'lastUpdated' => 1224689463,
+    'message' => 'Mostrar Gu&iacute;as'
+  },
+  'draw rulers description' => {
+    'lastUpdated' => 1224689488,
+    'message' => 'Seleccione &quot;Si&quot; para mostrar las gu&iacute;as en los ejes.'
+  },
+  'ruler color' => {
+    'lastUpdated' => 1224689505,
+    'message' => 'Color para Gu&iacute;as'
+  },
+  'ruler color description' => {
+    'lastUpdated' => 1224689523,
+    'message' => 'Color a utilizar para las Gu&iacute;as del Gr&aacute;fico.'
+  },
+  'y granularity' => {
+    'lastUpdated' => 1224689602,
+    'message' => 'Separaci&oacute;n en Eje Y'
+  },
+  'y granularity description' => {
+    'lastUpdated' => 1224689584,
+    'message' => 'Define la distancia entre las Gu&iacute;as y las Etiquetas sobre el Eje Y.'
+  }
+}
+;
 
 1;
