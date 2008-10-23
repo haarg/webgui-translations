5349a714ca09c6fd9acb6986b139e8e958d60ac6
Translation Server <translation@plainblack.com>
2008-10-23 21:38:28 +0000

    Update from translation server

--- German/German/Asset_Template.pm
+++ German/German/Asset_Template.pm
@@ -1,283 +1,235 @@
 package WebGUI::i18n::German::Asset_Template;
 use utf8;
-
-
 our $I18N = {
-	'site name' => {
-		message => q|Name der Site|,
-		lastUpdated => 1179922614
-	},
-
-	'style wizard' => {
-		message => q|Style Wizard|,
-		lastUpdated => 1179922645
-	},
-
-	'heading' => {
-		message => q|Heading|,
-		lastUpdated => 1179921396
-	},
-
-	'head block' => {
-		message => q|Head Block|,
-		lastUpdated => 1179921129
-	},
-
-	'assetName' => {
-		message => q|Template|,
-		lastUpdated => 1179920785
-	},
-
-	'header background color' => {
-		message => q|Hintergrundfarbe Header|,
-		lastUpdated => 1179921282
-	},
-
-	'session.setting.companyName' => {
-		message => q|Firmenname; konfiguriert in den WebGUI Einstellungen.|,
-		lastUpdated => 1179922436
-	},
-
-	'parser description' => {
-		message => q|Falls ihre Konfigdatei mehrer Template Engines auflistet, dann w&auml;hlen Sie den Typ des Templates, damit WebGUI das Template der richtigen Engine &uuml;bergeben kann.|,
-		lastUpdated => 1179921993
-	},
-
-	'body background color description' => {
-		message => q|Die Hintergrundfarbe des Bodies.|,
-		lastUpdated => 1179921012
-	},
-
-	'visited link color' => {
-		message => q|Farbe der besuchten Links|,
-		lastUpdated => 1179922792
-	},
-
-	'plugin name' => {
-		message => q|Name des Parsers|,
-		lastUpdated => 1179922010
-	},
-
-	'template variable title' => {
-		message => q|Template Variablen|,
-		lastUpdated => 1179922782
-	},
-
-	'template parsers' => {
-		message => q|Template Parser|,
-		lastUpdated => 1179922723
-	},
-
-	'header text color description' => {
-		message => q|Die Farbe des Textes im Header.|,
-		lastUpdated => 1179921388
-	},
-
-	'namespace description' => {
-		message => q|Welchen Typ hat dieses Template?|,
-		lastUpdated => 1179921634
-	},
-
-	'session.var.adminOn' => {
-		message => q|Bedingung, die wahr ist, falls sich der aktuelle Benutzer im Admin Modus befindet.|,
-		lastUpdated => 1179922514
-	},
-
-	'webgui.status' => {
-		message => q|Der Release Status dieser WebGUI Version (stabel, beta, gamma, etc.)|,
-		lastUpdated => 1179922845
-	},
-
-	'session.user.firstDayOfWeek' => {
-		message => q|Der konfigurierte erste Tag der Woche; konfiguriert im Profil des aktuellen Benutzers.|,
-		lastUpdated => 1179922474
-	},
-
-	'template parsers list title' => {
-		message => q|Liste von Template Parsern|,
-		lastUpdated => 1179922770
-	},
-
-	'choose a layout' => {
-		message => q|W&auml;hlen Sie ein Layout f&uuml;r diesen Style:|,
-		lastUpdated => 1179921086
-	},
-
-	'head block description' => {
-		message => q|F&uuml;gen Sie hier Meta Tags, Skript Tags, Link Tabellen, Style Sheets und alles das ein, was im Head Block (<head> ... </head>) ihres HTML Dokuments stehen soll.|,
-		lastUpdated => 1179921269
-	},
-
-	'header text color' => {
-		message => q|Textfarbe Header|,
-		lastUpdated => 1179921377
-	},
-
-	'header background color description' => {
-		message => q|Die Hintergrundfarbe ihres Headers oder des Banner Bereichs der Seite.|,
-		lastUpdated => 1179921369
-	},
-
-	'template error' => {
-		message => q|Sie haben einen Syntaxfehler in ihrem Template. Bitte korrigieren Sie diesen.|,
-		lastUpdated => 1179922716
-	},
-
-	'page background color description' => {
-		message => q|Die Hintergrundfarbe der ganzen Seite.|,
-		lastUpdated => 1179921655
-	},
-
-	'parser' => {
-		message => q|Template Typ|,
-		lastUpdated => 1179921667
-	},
-
-	'link color' => {
-		message => q|Linkfarbe|,
-		lastUpdated => 1179921410
-	},
-
-	'default parser' => {
-		message => q|Standard Parser|,
-		lastUpdated => 1179921095
-	},
-
-	'site name description' => {
-		message => q|Der Name ihrer Website|,
-		lastUpdated => 1179922626
-	},
-
-	'body background color' => {
-		message => q|Body Hintergrund Farbe|,
-		lastUpdated => 1179921002
-	},
-
-	'session.user.username' => {
-		message => q|Der Benutzername des aktuelle Benutzers.|,
-		lastUpdated => 1179922488
-	},
-
-	'body content' => {
-		message => q|Body Inhalt hier.|,
-		lastUpdated => 1179921028
-	},
-
-	'session.config.extrasurl' => {
-		message => q|Die URL des extras Verzeichnisses auf dem Server; konfiguriert in der WebGUI.conf Datei.|,
-		lastUpdated => 1179922369
-	},
-
-	'page background color' => {
-		message => q|Hintergrundfarbe der Seite|,
-		lastUpdated => 1179921644
-	},
-
-	'session scratch variables' => {
-		message => q|<strong>Session Scratch Variablen</strong><br />Jede scratch Variable ist in folgender Form im Template verf&uuml;gbar:<br /> &lt;tmpl_var session.scratch.<em>variable</em>&gt;|,
-		lastUpdated => 1179922262
-	},
-
-	'logo subtext' => {
-		message => q|Das Logo darf h&ouml;chstens 200 Pixel breit und 100 Pixel hoch sein.|,
-		lastUpdated => 1179921574
-	},
-
-	'logo description' => {
-		message => q|Sie k&ouml;nnen dieses Feld benutzen, um ein graphisches Logo in ihr Style einzbinden. Das Logo darf h&ouml;chstens 200 Pixel breit und 100 Pixel hoch sein.|,
-		lastUpdated => 1179921549
-	},
-
-	'body text color description' => {
-		message => q|Die Farbe des Textes im Body.|,
-		lastUpdated => 1179921052
-	},
-
-	'webgui.version' => {
-		message => q|Version ihrer WebGUI Installation|,
-		lastUpdated => 1179922882
-	},
-
-	'show in forms description' => {
-		message => q|Soll dieses Template in der Liste der Templates dieses Namensbereichs angezeigt werden?|,
-		lastUpdated => 1179922600
-	},
-
-	'namespace' => {
-		message => q|Namensbereich|,
-		lastUpdated => 1179921622
-	},
-
-	'menu background color description' => {
-		message => q|Die Hintergrundfarbe des Men&uuml;bereichs ihrer Seite.|,
-		lastUpdated => 1179921615
-	},
-
-	'edit template' => {
-		message => q|Template bearbeiten|,
-		lastUpdated => 1179921120
-	},
-
-	'visited link color description' => {
-		message => q|Die Farbe der besuchten Links auf ihrer Site. Standard ist lila.|,
-		lastUpdated => 1179922813
-	},
-
-	'session form variables' => {
-		message => q|<strong>Session Formular Variablen</strong><br />Jede Formular Variable ist im Template in folgender Form verf&uuml;gbar:<br /> &lt;tmpl_var session.form.<em>variable</em>&gt;<br /> Gibt es mehr als einen Wert f&uuml;r eine Formular Variable, so wird nur der letzte zur&uuml;ckgegeben.|,
-		lastUpdated => 1179922203
-	},
-
-	'menu' => {
-		message => q|Men&uuml;|,
-		lastUpdated => 1179921582
-	},
-
-	'template parsers list body' => {
-		message => q|Die folgenden Template Parser sind auf ihrer Site installiert und k&ouml;nnen aktiviert werden.|,
-		lastUpdated => 1179922751
-	},
-
-	'session.setting.anonymousRegistration' => {
-		message => q|Gibt an, ob eine anonyme Registrierung eralaubt ist oder nicht; konfiguriert in den WebGUI Einstellungen.|,
-		lastUpdated => 1179922427
-	},
-
-	'plugin enabled header' => {
-		message => q|Aktiviert?|,
-		lastUpdated => 1179922001
-	},
-
-	'logo' => {
-		message => q|Logo|,
-		lastUpdated => 1179921487
-	},
-
-	'link color description' => {
-		message => q|Farbe der Links auf ihrer Seite. Standard ist blau.|,
-		lastUpdated => 1179921483
-	},
-
-	'show in forms' => {
-		message => q|In Formularen anzeigen?|,
-		lastUpdated => 1179922524
-	},
-
-	'template description' => {
-		message => q|Erzeugen Sie ihr Template, in dem sie Template Kommandos und Variablen, Makros und HTML benutzen.|,
-		lastUpdated => 1179922687
-	},
-
-	'body text color' => {
-		message => q|Body Textfarbe|,
-		lastUpdated => 1179921036
-	},
-
-	'menu background color' => {
-		message => q|Hintergrundfarbe des Men&uuml;s|,
-		lastUpdated => 1179921596
-	},
-
-};
+  'assetName' => {
+    'lastUpdated' => 1179920785,
+    'message' => 'Template'
+  },
+  'body background color' => {
+    'lastUpdated' => 1179921002,
+    'message' => 'Body Hintergrund Farbe'
+  },
+  'body background color description' => {
+    'lastUpdated' => 1179921012,
+    'message' => 'Die Hintergrundfarbe des Bodies.'
+  },
+  'body content' => {
+    'lastUpdated' => 1179921028,
+    'message' => 'Body Inhalt hier.'
+  },
+  'body text color' => {
+    'lastUpdated' => 1179921036,
+    'message' => 'Body Textfarbe'
+  },
+  'body text color description' => {
+    'lastUpdated' => 1179921052,
+    'message' => 'Die Farbe des Textes im Body.'
+  },
+  'choose a layout' => {
+    'lastUpdated' => 1179921086,
+    'message' => 'W&auml;hlen Sie ein Layout f&uuml;r diesen Style:'
+  },
+  'default parser' => {
+    'lastUpdated' => 1179921095,
+    'message' => 'Standard Parser'
+  },
+  'edit template' => {
+    'lastUpdated' => 1179921120,
+    'message' => 'Template bearbeiten'
+  },
+  'head block' => {
+    'lastUpdated' => 1179921129,
+    'message' => 'Head Block'
+  },
+  'head block description' => {
+    'lastUpdated' => 1179921269,
+    'message' => 'F&uuml;gen Sie hier Meta Tags, Skript Tags, Link Tabellen, Style Sheets und alles das ein, was im Head Block (<head> ... </head>) ihres HTML Dokuments stehen soll.'
+  },
+  'header background color' => {
+    'lastUpdated' => 1179921282,
+    'message' => 'Hintergrundfarbe Header'
+  },
+  'header background color description' => {
+    'lastUpdated' => 1179921369,
+    'message' => 'Die Hintergrundfarbe ihres Headers oder des Banner Bereichs der Seite.'
+  },
+  'header text color' => {
+    'lastUpdated' => 1179921377,
+    'message' => 'Textfarbe Header'
+  },
+  'header text color description' => {
+    'lastUpdated' => 1179921388,
+    'message' => 'Die Farbe des Textes im Header.'
+  },
+  'heading' => {
+    'lastUpdated' => 1179921396,
+    'message' => 'Heading'
+  },
+  'link color' => {
+    'lastUpdated' => 1179921410,
+    'message' => 'Linkfarbe'
+  },
+  'link color description' => {
+    'lastUpdated' => 1179921483,
+    'message' => 'Farbe der Links auf ihrer Seite. Standard ist blau.'
+  },
+  'logo' => {
+    'lastUpdated' => 1179921487,
+    'message' => 'Logo'
+  },
+  'logo description' => {
+    'lastUpdated' => 1179921549,
+    'message' => 'Sie k&ouml;nnen dieses Feld benutzen, um ein graphisches Logo in ihr Style einzbinden. Das Logo darf h&ouml;chstens 200 Pixel breit und 100 Pixel hoch sein.'
+  },
+  'logo subtext' => {
+    'lastUpdated' => 1179921574,
+    'message' => 'Das Logo darf h&ouml;chstens 200 Pixel breit und 100 Pixel hoch sein.'
+  },
+  'make duplicate label' => {
+    'lastUpdated' => 1224797765,
+    'message' => 'Diese Vorlage duplizieren und bearbeiten'
+  },
+  'menu' => {
+    'lastUpdated' => 1179921582,
+    'message' => 'Men&uuml;'
+  },
+  'menu background color' => {
+    'lastUpdated' => 1179921596,
+    'message' => 'Hintergrundfarbe des Men&uuml;s'
+  },
+  'menu background color description' => {
+    'lastUpdated' => 1179921615,
+    'message' => 'Die Hintergrundfarbe des Men&uuml;bereichs ihrer Seite.'
+  },
+  'namespace' => {
+    'lastUpdated' => 1179921622,
+    'message' => 'Namensbereich'
+  },
+  'namespace description' => {
+    'lastUpdated' => 1179921634,
+    'message' => 'Welchen Typ hat dieses Template?'
+  },
+  'page background color' => {
+    'lastUpdated' => 1179921644,
+    'message' => 'Hintergrundfarbe der Seite'
+  },
+  'page background color description' => {
+    'lastUpdated' => 1179921655,
+    'message' => 'Die Hintergrundfarbe der ganzen Seite.'
+  },
+  'parser' => {
+    'lastUpdated' => 1179921667,
+    'message' => 'Template Typ'
+  },
+  'parser description' => {
+    'lastUpdated' => 1179921993,
+    'message' => 'Falls ihre Konfigdatei mehrer Template Engines auflistet, dann w&auml;hlen Sie den Typ des Templates, damit WebGUI das Template der richtigen Engine &uuml;bergeben kann.'
+  },
+  'plugin enabled header' => {
+    'lastUpdated' => 1179922001,
+    'message' => 'Aktiviert?'
+  },
+  'plugin name' => {
+    'lastUpdated' => 1179922010,
+    'message' => 'Name des Parsers'
+  },
+  'session form variables' => {
+    'lastUpdated' => 1179922203,
+    'message' => '<strong>Session Formular Variablen</strong><br />Jede Formular Variable ist im Template in folgender Form verf&uuml;gbar:<br /> &lt;tmpl_var session.form.<em>variable</em>&gt;<br /> Gibt es mehr als einen Wert f&uuml;r eine Formular Variable, so wird nur der letzte zur&uuml;ckgegeben.'
+  },
+  'session scratch variables' => {
+    'lastUpdated' => 1179922262,
+    'message' => '<strong>Session Scratch Variablen</strong><br />Jede scratch Variable ist in folgender Form im Template verf&uuml;gbar:<br /> &lt;tmpl_var session.scratch.<em>variable</em>&gt;'
+  },
+  'session.config.extrasurl' => {
+    'lastUpdated' => 1179922369,
+    'message' => 'Die URL des extras Verzeichnisses auf dem Server; konfiguriert in der WebGUI.conf Datei.'
+  },
+  'session.setting.anonymousRegistration' => {
+    'lastUpdated' => 1179922427,
+    'message' => 'Gibt an, ob eine anonyme Registrierung eralaubt ist oder nicht; konfiguriert in den WebGUI Einstellungen.'
+  },
+  'session.setting.companyName' => {
+    'lastUpdated' => 1179922436,
+    'message' => 'Firmenname; konfiguriert in den WebGUI Einstellungen.'
+  },
+  'session.user.firstDayOfWeek' => {
+    'lastUpdated' => 1179922474,
+    'message' => 'Der konfigurierte erste Tag der Woche; konfiguriert im Profil des aktuellen Benutzers.'
+  },
+  'session.user.username' => {
+    'lastUpdated' => 1179922488,
+    'message' => 'Der Benutzername des aktuelle Benutzers.'
+  },
+  'session.var.adminOn' => {
+    'lastUpdated' => 1179922514,
+    'message' => 'Bedingung, die wahr ist, falls sich der aktuelle Benutzer im Admin Modus befindet.'
+  },
+  'show in forms' => {
+    'lastUpdated' => 1179922524,
+    'message' => 'In Formularen anzeigen?'
+  },
+  'show in forms description' => {
+    'lastUpdated' => 1179922600,
+    'message' => 'Soll dieses Template in der Liste der Templates dieses Namensbereichs angezeigt werden?'
+  },
+  'site name' => {
+    'lastUpdated' => 1179922614,
+    'message' => 'Name der Site'
+  },
+  'site name description' => {
+    'lastUpdated' => 1179922626,
+    'message' => 'Der Name ihrer Website'
+  },
+  'style wizard' => {
+    'lastUpdated' => 1179922645,
+    'message' => 'Style Wizard'
+  },
+  'template description' => {
+    'lastUpdated' => 1179922687,
+    'message' => 'Erzeugen Sie ihr Template, in dem sie Template Kommandos und Variablen, Makros und HTML benutzen.'
+  },
+  'template error' => {
+    'lastUpdated' => 1179922716,
+    'message' => 'Sie haben einen Syntaxfehler in ihrem Template. Bitte korrigieren Sie diesen.'
+  },
+  'template parsers' => {
+    'lastUpdated' => 1179922723,
+    'message' => 'Template Parser'
+  },
+  'template parsers list body' => {
+    'lastUpdated' => 1179922751,
+    'message' => 'Die folgenden Template Parser sind auf ihrer Site installiert und k&ouml;nnen aktiviert werden.'
+  },
+  'template parsers list title' => {
+    'lastUpdated' => 1179922770,
+    'message' => 'Liste von Template Parsern'
+  },
+  'template variable title' => {
+    'lastUpdated' => 1179922782,
+    'message' => 'Template Variablen'
+  },
+  'visited link color' => {
+    'lastUpdated' => 1179922792,
+    'message' => 'Farbe der besuchten Links'
+  },
+  'visited link color description' => {
+    'lastUpdated' => 1179922813,
+    'message' => 'Die Farbe der besuchten Links auf ihrer Site. Standard ist lila.'
+  },
+  'warning default template' => {
+    'lastUpdated' => 1224797895,
+    'message' => 'Sie versuchen eine Standardvorlage zu bearbeiten. Jede &Auml;nderung k&ouml;nnte verloren gehen, wenn Sie WebGUI aktualisieren. Besser w&auml;re es, diese Vorlage zu duplizieren.'
+  },
+  'webgui.status' => {
+    'lastUpdated' => 1179922845,
+    'message' => 'Der Release Status dieser WebGUI Version (stabel, beta, gamma, etc.)'
+  },
+  'webgui.version' => {
+    'lastUpdated' => 1179922882,
+    'message' => 'Version ihrer WebGUI Installation'
+  }
+}
+;
 
 1;
