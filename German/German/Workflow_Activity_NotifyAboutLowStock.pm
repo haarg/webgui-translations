b4cc0f221c0a578fb1cce504bfe12848b14e69b3
Translation Server <translation@plainblack.com>
2008-10-23 22:10:38 +0000

    Update from translation server

--- German/German/Workflow_Activity_NotifyAboutLowStock.pm
+++ German/German/Workflow_Activity_NotifyAboutLowStock.pm
@@ -1,5 +1,27 @@
 package WebGUI::i18n::German::Workflow_Activity_NotifyAboutLowStock;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'activityName' => {
+    'lastUpdated' => 1224799611,
+    'message' => 'Benachrichtigung niedriger Lagerst&uuml;ckzahl'
+  },
+  'group to notify' => {
+    'lastUpdated' => 1224799632,
+    'message' => 'Gruppe zur Benachrichtigung'
+  },
+  'group to notify help' => {
+    'lastUpdated' => 1224799654,
+    'message' => 'Gruppe zur Benachrichtigung'
+  },
+  'warning limit' => {
+    'lastUpdated' => 1224799734,
+    'message' => 'Grenze zur Benachrichtigung'
+  },
+  'warning limit help' => {
+    'lastUpdated' => 1224799826,
+    'message' => 'Wenn ein Lagerst&uuml;ckzahl eines Produkts diese Menge unterschreitet, werden Sie benachrichtigt.'
+  }
+}
+;
 
 1;
