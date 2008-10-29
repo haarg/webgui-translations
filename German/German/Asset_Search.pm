b7b5b4ed8f827b6044e7e8456a2051d8eaf752e9
Translation Server <translation@plainblack.com>
2008-10-29 19:07:55 +0000

    Update from translation server

--- German/German/Asset_Search.pm
+++ German/German/Asset_Search.pm
@@ -1,127 +1,111 @@
 package WebGUI::i18n::German::Asset_Search;
 use utf8;
-
 our $I18N = {
-	'search asset template variables title' => {
-		message => q|Suche Asset Template Variablen|,
-		lastUpdated => 1180009185
-	},
-
-	'search' => {
-		message => q|Suche|,
-		lastUpdated => 1170407642
-	},
-
-	'search template' => {
-		message => q|Suche Template|,
-		lastUpdated => 1180009218
-	},
-
-	'classLimiter' => {
-		message => q|Eine Zeichenkette, die alle Typen von Assets enth&auml;lt, nach denen gesucht wird.<br />|,
-		lastUpdated => 1170407519
-	},
-
-	'class limiter' => {
-		message => q|Limitiere Asset Klassen zu:|,
-		lastUpdated => 1170407448
-	},
-
-	'assetName' => {
-		message => q|Suche|,
-		lastUpdated => 1152970201
-	},
-
-	'synopsis' => {
-		message => q|Die Zusammenfassung des Assets, das gefunden wurde.|,
-		lastUpdated => 1179837930
-	},
-
-	'no_results' => {
-		message => q|Ein internationalisiertes Label, das dem Benutzer mitteilt, dass keine Suchergebnisse vorliegen.|,
-		lastUpdated => 1199979135
-	},
-
-	'assetId' => {
-		message => q|Die assetId des Assets, das gefunden wurde.|,
-		lastUpdated => 1199979047
-	},
-
-	'url' => {
-		message => q|URL zum Asset, das gefunden wurde.|,
-		lastUpdated => 1179837959
-	},
-
-	'searchRoot' => {
-		message => q|Die ID des Assets, bei dem die Suche beginnt.|,
-		lastUpdated => 1180009192
-	},
-
-	'form_submit' => {
-		message => q|Button, der es dem Benutzer erlaubt, die Suche zu starten.|,
-		lastUpdated => 1170407561
-	},
-
-	'class limiter description' => {
-		message => q|Dies limitiert die Suche auf Asset Typen, die Sie selektieren.<br />|,
-		lastUpdated => 1170407487
-	},
-
-	'result_set' => {
-		message => q|Resultat der Suche, aufgeteilt auf mehrere Seiten. |,
-		lastUpdated => 1170407630
-	},
-
-	'templateId' => {
-		message => q|Die ID des Templates, das f&uuml;r die Darstellung des Suche Assets benutzt wird.<br />|,
-		lastUpdated => 1170407870
-	},
-
-	'search root' => {
-		message => q|Suche Root |,
-		lastUpdated => 1170407663
-	},
-
-	'form_footer' => {
-		message => q|HTML Code f&uuml;r das Ende des Such Formulars|,
-		lastUpdated => 1180009211
-	},
-
-	'search root description' => {
-		message => q|Das Asset, das Sie selektieren und alle sich darunter liegenden Assets werden durchsucht. |,
-		lastUpdated => 1170407701
-	},
-
-	'results_found' => {
-		message => q|Eine bedingte Variable, die wahr ist, falls Suchergebnisse vorliegen.|,
-		lastUpdated => 1199979186
-	},
-
-	'form_header' => {
-		message => q|HTML Code f&uuml;r Beginn des Such Formulars|,
-		lastUpdated => 1180009206
-	},
-
-	'form_keywords' => {
-		message => q|Ein Formular, in das der Benuter Schlagw&ouml;rter f&uuml;r die Suche eingeben kann. |,
-		lastUpdated => 1170407906
-	},
-
-	'search template description' => {
-		message => q|Ein Template, das das Suchformular und die Suchresultate darstellt.<br />|,
-		lastUpdated => 1170407825
-	},
-
-	'no results' => {
-		message => q|Keine Suchergebnisse.|,
-		lastUpdated => 1199979064
-	},
-
-	'title' => {
-		message => q|Der Titel des Assets, das gefunden wurde.|,
-		lastUpdated => 1179837943
-	},
-
-};
+  'assetId' => {
+    'lastUpdated' => 1199979047,
+    'message' => 'Die assetId des Assets, das gefunden wurde.'
+  },
+  'assetName' => {
+    'lastUpdated' => 1152970201,
+    'message' => 'Suche'
+  },
+  'class limiter' => {
+    'lastUpdated' => 1170407448,
+    'message' => 'Limitiere Asset Klassen zu:'
+  },
+  'class limiter description' => {
+    'lastUpdated' => 1170407487,
+    'message' => 'Dies limitiert die Suche auf Asset Typen, die Sie selektieren.<br />'
+  },
+  'classLimiter' => {
+    'lastUpdated' => 1170407519,
+    'message' => 'Eine Zeichenkette, die alle Typen von Assets enth&auml;lt, nach denen gesucht wird.<br />'
+  },
+  'form_footer' => {
+    'lastUpdated' => 1180009211,
+    'message' => 'HTML Code f&uuml;r das Ende des Such Formulars'
+  },
+  'form_header' => {
+    'lastUpdated' => 1180009206,
+    'message' => 'HTML Code f&uuml;r Beginn des Such Formulars'
+  },
+  'form_keywords' => {
+    'lastUpdated' => 1170407906,
+    'message' => 'Ein Formular, in das der Benuter Schlagw&ouml;rter f&uuml;r die Suche eingeben kann. '
+  },
+  'form_submit' => {
+    'lastUpdated' => 1170407561,
+    'message' => 'Button, der es dem Benutzer erlaubt, die Suche zu starten.'
+  },
+  'no results' => {
+    'lastUpdated' => 1199979064,
+    'message' => 'Keine Suchergebnisse.'
+  },
+  'no_results' => {
+    'lastUpdated' => 1199979135,
+    'message' => 'Ein internationalisiertes Label, das dem Benutzer mitteilt, dass keine Suchergebnisse vorliegen.'
+  },
+  'result_set' => {
+    'lastUpdated' => 1170407630,
+    'message' => 'Resultat der Suche, aufgeteilt auf mehrere Seiten. '
+  },
+  'results_found' => {
+    'lastUpdated' => 1199979186,
+    'message' => 'Eine bedingte Variable, die wahr ist, falls Suchergebnisse vorliegen.'
+  },
+  'search' => {
+    'lastUpdated' => 1170407642,
+    'message' => 'Suche'
+  },
+  'search asset template variables title' => {
+    'lastUpdated' => 1180009185,
+    'message' => 'Suche Asset Template Variablen'
+  },
+  'search root' => {
+    'lastUpdated' => 1170407663,
+    'message' => 'Suche Root '
+  },
+  'search root description' => {
+    'lastUpdated' => 1170407701,
+    'message' => 'Das Asset, das Sie selektieren und alle sich darunter liegenden Assets werden durchsucht. '
+  },
+  'search template' => {
+    'lastUpdated' => 1180009218,
+    'message' => 'Suche Template'
+  },
+  'search template description' => {
+    'lastUpdated' => 1170407825,
+    'message' => 'Ein Template, das das Suchformular und die Suchresultate darstellt.<br />'
+  },
+  'searchRoot' => {
+    'lastUpdated' => 1180009192,
+    'message' => 'Die ID des Assets, bei dem die Suche beginnt.'
+  },
+  'synopsis' => {
+    'lastUpdated' => 1179837930,
+    'message' => 'Die Zusammenfassung des Assets, das gefunden wurde.'
+  },
+  'templateId' => {
+    'lastUpdated' => 1170407870,
+    'message' => 'Die ID des Templates, das f&uuml;r die Darstellung des Suche Assets benutzt wird.<br />'
+  },
+  'title' => {
+    'lastUpdated' => 1179837943,
+    'message' => 'Der Titel des Assets, das gefunden wurde.'
+  },
+  'url' => {
+    'lastUpdated' => 1179837959,
+    'message' => 'URL zum Asset, das gefunden wurde.'
+  },
+  'useContainers' => {
+    'lastUpdated' => 1225307159,
+    'message' => 'Bei Ergebnislisten Container-Urls benutzen?'
+  },
+  'useContainers help' => {
+    'lastUpdated' => 1225307265,
+    'message' => 'W&auml;hlen Sie &quot;Ja&quot;, wenn Suchergebnisse komplette Seiten anstelle von Einzelteilen enthalten sollen. '
+  }
+}
+;
 
 1;
