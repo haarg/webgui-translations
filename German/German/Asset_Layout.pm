29e48ab52a02f494cdd2acebd9a75cddbec2cb10
Translation Server <translation@plainblack.com>
2008-10-29 22:08:03 +0000

    Update from translation server

--- German/German/Asset_Layout.pm
+++ German/German/Asset_Layout.pm
@@ -1,89 +1,101 @@
 package WebGUI::i18n::German::Asset_Layout;
 use utf8;
 our $I18N = {
-  'dragger.init' => {
-    'lastUpdated' => 1170405916,
-    'message' => 'HTML und Javascript ist Voraussetzung f&uuml;r &quot;click and drag&quot;. Wenn showAdmin false ist, ist diese Variable leer.<br />'
-  },
-  'content' => {
-    'lastUpdated' => 1178864154,
-    'message' => 'Der verarbeitete (rendered) Inhalt diese Assets.'
-  },
-  'assetName' => {
-    'lastUpdated' => 1149226936,
-    'message' => 'Seitenlayout'
-  },
-  'layout asset template variables title' => {
-    'lastUpdated' => 1170406368,
-    'message' => 'Seitenlayout Template Variablen'
-  },
-  'assetsToHide' => {
-    'lastUpdated' => 1170405069,
-    'message' => 'Die IDs der Assets, die in diesem Asset nicht gezeigt werden sollen. Getrennt durch Unix-Style Zeilenvorschubzeichen.'
-  },
-  'dragger.icon' => {
-    'lastUpdated' => 1170405906,
-    'message' => 'Icon f&uuml;r die &Auml;nderung der Position eines Assets via &quot;click and drag&quot;. Wenn showAdmin false ist, ist diese Variable leer. '
-  },
-  'contentPositions' => {
-    'lastUpdated' => 1170405720,
-    'message' => 'Zeichenkette, die die verschiedenen Pl&auml;tze angibt, an denen Assets dargestellt werden. Ausserdem, welche Assets an welchem Platz und in welcher Reihenfolge.<br /> '
-  },
-  'asset order label' => {
-    'lastUpdated' => 1217316339,
-    'message' => 'Neues Asset hinzuf&uuml;gen'
-  },
-  'assets to hide description' => {
-    'lastUpdated' => 1178864160,
-    'message' => 'Diese Liste enth&auml;lt ein Auswahlfeld f&uuml;r jedes Asset auf der Seite. W&auml;hlen Sie hier die Assets aus, die nicht auf der Seite dargestellt werden sollen. '
-  },
-  'id' => {
-    'lastUpdated' => 1178864167,
-    'message' => 'Die Asset ID dieses Assets.'
-  },
-  'position1_loop' => {
-    'lastUpdated' => 1178864308,
-    'message' => 'Jede Position in dem Template hat eine Schleife, die einen Satz an Assets enth&auml;lt, die angezeigt werden. Assets, die nicht an einer bestimmten Stelle platziert wurden, werden an Position 1 gesetzt.'
-  },
-  'templateId' => {
-    'lastUpdated' => 1178864374,
-    'message' => 'Die ID des Templates, das f&uuml;r die Anzeige dieses Assets benutzt wird.'
+  '823' => {
+    'lastUpdated' => 1178864135,
+    'message' => 'Zur neuen Seite gehen.'
   },
   '847' => {
     'lastUpdated' => 1178864141,
     'message' => 'Zur&uuml;ck zur aktuellen Seite.'
   },
+  'asset order asc' => {
+    'lastUpdated' => 1225318059,
+    'message' => 'Am Ende der Seite'
+  },
+  'asset order desc' => {
+    'lastUpdated' => 1225318073,
+    'message' => 'AmAnfanf der Seite'
+  },
+  'asset order hoverHelp' => {
+    'lastUpdated' => 1225318038,
+    'message' => 'W&auml;hlen Sie, wie neue Assets hinzugef&uuml;gt werden. (Zum Anfangs- oder zur Endposition der Seite)'
+  },
+  'asset order label' => {
+    'lastUpdated' => 1217316339,
+    'message' => 'Neues Asset hinzuf&uuml;gen'
+  },
+  'assetName' => {
+    'lastUpdated' => 1149226936,
+    'message' => 'Seitenlayout'
+  },
   'assets to hide' => {
     'lastUpdated' => 1178864147,
     'message' => 'Assets zum Verbergen'
+  },
+  'assets to hide description' => {
+    'lastUpdated' => 1178864160,
+    'message' => 'Diese Liste enth&auml;lt ein Auswahlfeld f&uuml;r jedes Asset auf der Seite. W&auml;hlen Sie hier die Assets aus, die nicht auf der Seite dargestellt werden sollen. '
+  },
+  'assetsToHide' => {
+    'lastUpdated' => 1170405069,
+    'message' => 'Die IDs der Assets, die in diesem Asset nicht gezeigt werden sollen. Getrennt durch Unix-Style Zeilenvorschubzeichen.'
+  },
+  'content' => {
+    'lastUpdated' => 1178864154,
+    'message' => 'Der verarbeitete (rendered) Inhalt diese Assets.'
+  },
+  'contentPositions' => {
+    'lastUpdated' => 1170405720,
+    'message' => 'Zeichenkette, die die verschiedenen Pl&auml;tze angibt, an denen Assets dargestellt werden. Ausserdem, welche Assets an welchem Platz und in welcher Reihenfolge.<br /> '
+  },
+  'dragger.icon' => {
+    'lastUpdated' => 1170405906,
+    'message' => 'Icon f&uuml;r die &Auml;nderung der Position eines Assets via &quot;click and drag&quot;. Wenn showAdmin false ist, ist diese Variable leer. '
+  },
+  'dragger.init' => {
+    'lastUpdated' => 1170405916,
+    'message' => 'HTML und Javascript ist Voraussetzung f&uuml;r &quot;click and drag&quot;. Wenn showAdmin false ist, ist diese Variable leer.<br />'
+  },
+  'id' => {
+    'lastUpdated' => 1178864167,
+    'message' => 'Die Asset ID dieses Assets.'
   },
   'isUncommitted' => {
     'lastUpdated' => 1217316368,
     'message' => 'Ein Wahrheitswert, der anzeigt, ob das Asset committed wurde oder nicht'
   },
-  'layout template body' => {
-    'lastUpdated' => 1178864216,
-    'message' => 'Die folgenden Variablen sind in Seitenlayout Templates verf&uuml;gbar:'
-  },
   'layout asset template variables body' => {
     'lastUpdated' => 1170406352,
     'message' => 'Jedes Asset bietet ihren Templates einen Satz an Variablen, die auf den Asset Eigenschaften basieren. Einige dieser Variablen k&ouml;nnen n&uuml;tzlich sein, andere nicht.'
   },
-  'showAdmin' => {
-    'lastUpdated' => 1178864350,
-    'message' => 'Bedingung, die angibt, ob sich der aktuelle Benutzer im Admin Modus befindet und dieses Asset bearbeiten kann.'
+  'layout asset template variables title' => {
+    'lastUpdated' => 1170406368,
+    'message' => 'Seitenlayout Template Variablen'
+  },
+  'layout template body' => {
+    'lastUpdated' => 1178864216,
+    'message' => 'Die folgenden Variablen sind in Seitenlayout Templates verf&uuml;gbar:'
   },
   'layout template title' => {
     'lastUpdated' => 1178864208,
     'message' => 'Seitenlayout Template'
   },
-  '823' => {
-    'lastUpdated' => 1178864135,
-    'message' => 'Zur neuen Seite gehen.'
+  'position1_loop' => {
+    'lastUpdated' => 1178864308,
+    'message' => 'Jede Position in dem Template hat eine Schleife, die einen Satz an Assets enth&auml;lt, die angezeigt werden. Assets, die nicht an einer bestimmten Stelle platziert wurden, werden an Position 1 gesetzt.'
+  },
+  'showAdmin' => {
+    'lastUpdated' => 1178864350,
+    'message' => 'Bedingung, die angibt, ob sich der aktuelle Benutzer im Admin Modus befindet und dieses Asset bearbeiten kann.'
   },
   'template description' => {
     'lastUpdated' => 1149227092,
     'message' => 'W&auml;hlen Sie ein Template zur Darstellung der Assets auf der Seite aus. '
+  },
+  'templateId' => {
+    'lastUpdated' => 1178864374,
+    'message' => 'Die ID des Templates, das f&uuml;r die Anzeige dieses Assets benutzt wird.'
   }
 }
 ;
