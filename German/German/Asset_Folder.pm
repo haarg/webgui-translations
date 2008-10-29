533f2cadde0cdc5eee2457280120ea7a583a90ea
Translation Server <translation@plainblack.com>
2008-10-29 22:04:42 +0000

    Update from translation server

--- German/German/Asset_Folder.pm
+++ German/German/Asset_Folder.pm
@@ -1,227 +1,207 @@
 package WebGUI::i18n::German::Asset_Folder;
 use utf8;
-
 our $I18N = {
-	'What Next description' => {
-		message => q|Wollen Sie nach dem Erstellen des Ordners zur&uuml;ck zur Seite, in der sie den Ordner erstellt haben oder direkt zum neu erstellten Ordner? |,
-		lastUpdated => 1166611737
-	},
-
-	'assetName' => {
-		message => q|Ordner|,
-		lastUpdated => 1166610346
-	},
-
-	'folder template body' => {
-		message => q|Folgende Variablen sind in Ordner Templates verf&uuml;gbar:<br />|,
-		lastUpdated => 1166611911
-	},
-
-	'sort alphabetically' => {
-		message => q|Alphabetische Sortierung?|,
-		lastUpdated => 1178864503
-	},
-
-	'add file label' => {
-		message => q|Dateien hinzuf&uuml;gen.|,
-		lastUpdated => 1178864405
-	},
-
-	'synopsis' => {
-		message => q|Synopsis dieses Assets. |,
-		lastUpdated => 1166611585
-	},
-
-	'folder id' => {
-		message => q|assetId des Ordners.|,
-		lastUpdated => 1166611270
-	},
-
-	'url' => {
-		message => q|URL|,
-		lastUpdated => 1166610552
-	},
-
-	'controls' => {
-		message => q|Editier Kontroll Balken f&uuml;r dieses Kind.|,
-		lastUpdated => 1178864428
-	},
-
-	'id' => {
-		message => q|assetId des Assets.|,
-		lastUpdated => 1178864489
-	},
-
-	'folder template title' => {
-		message => q|Ordner Template|,
-		lastUpdated => 1166611991
-	},
-
-	'canEdit' => {
-		message => q|Bedingung, die anzeigt, ob dieses Asset vom aktuellen Benutzer editiert werden kann. |,
-		lastUpdated => 1166610710
-	},
-
-	'date.epoch' => {
-		message => q|Datum der letzten &Auml;nderung, relativ zur Epoche|,
-		lastUpdated => 1166610844
-	},
-
-	'visitorCacheTimeout' => {
-		message => q|Wie lange soll Asset gecached werden (in Sekunden).<br /> |,
-		lastUpdated => 1168865553
-	},
-
-	'folder template description' => {
-		message => q|Dieses Men&uuml; erlaubt das Ausw&auml;hlen eines Templates f&uuml;r die Darstellung der Ordner Inhalte.<br />|,
-		lastUpdated => 1166611976
-	},
-
-	'addFile.url' => {
-		message => q|URL zum Hinzuf&uuml;gen von Dateien zum Ordner.|,
-		lastUpdated => 1178864411
-	},
-
-	'sort alphabetically help' => {
-		message => q|Wollen sie die Elemente dieses Orders alphabetisch sortieren? Wenn sie nein w&auml;hlen, werden sie nach ihrem Rang sortiert.<br />|,
-		lastUpdated => 1166611525
-	},
-
-	'icon.small' => {
-		message => q|URL zum kleinen Icon, das das Asset repr&auml;sentiert.<br />|,
-		lastUpdated => 1166612052
-	},
-
-	'asset template variables body' => {
-		message => q|Jedes Asset bietet ihren Templates einen Satz an Variablen, die auf den Asset Eigenschaften basieren. Einige dieser Variablen k&ouml;nnen n&uuml;tzlich sein, andere nicht.|,
-		lastUpdated => 1168864845
-	},
-
-	'folder icon.big' => {
-		message => q|gro&szlig;es Ordner Icon|,
-		lastUpdated => 1178864463
-	},
-
-	'isImage' => {
-		message => q|Bedingung, die anzeigt, ob dieses Asset ein Bild Asset ist.<br /> |,
-		lastUpdated => 1166611399
-	},
-
-	'type' => {
-		message => q|Typ dieses Assets.|,
-		lastUpdated => 1178864511
-	},
-
-	'title' => {
-		message => q|Titel|,
-		lastUpdated => 1166610587
-	},
-
-	'folder menuTitle' => {
-		message => q|Der Me&uuml;titel des Ordners.|,
-		lastUpdated => 1201601414
-	},
-
-	'addFile.label' => {
-		message => q|Das internationalisierte Label f&uuml;r das Hinzuf&uuml;gen von Dateien zum Ordner.<br />|,
-		lastUpdated => 1166611794
-	},
-
-	'folder icon.small' => {
-		message => q|kleines Ordner Icon|,
-		lastUpdated => 1178864452
-	},
-
-	'folder url' => {
-		message => q|URL des Ordners.|,
-		lastUpdated => 1166611282
-	},
-
-	'menuTitle' => {
-		message => q|Der Men&uuml;titel des Assets.|,
-		lastUpdated => 1201601432
-	},
-
-	'asset template variables title' => {
-		message => q|Ordner Asset Template Variablen|,
-		lastUpdated => 1168865650
-	},
-
-	'size' => {
-		message => q|Gr&ouml;&szlig;e des Assets, formatiert.|,
-		lastUpdated => 1178864496
-	},
-
-	'visitor cache timeout' => {
-		message => q|Cache Timeout f&uuml;r Besucher|,
-		lastUpdated => 1178864522
-	},
-
-	'templateId' => {
-		message => q|Die ID des Templates, das f&uuml;r die Anzeige des Ordnerinhalts benutzt wird.|,
-		lastUpdated => 1168865626
-	},
-
-	'847' => {
-		message => q|Gehe zur&uuml;ck zur aktuellen Seite.|,
-		lastUpdated => 1166611849
-	},
-
-	'canView' => {
-		message => q|Bedingung, die anzeigt, ob der aktuelle Benutzer dieses Asset ansehen kann.<br />|,
-		lastUpdated => 1166610749
-	},
-
-	'sortAlphabetically' => {
-		message => q|Bedingung, die anzeigt, da&szlig; Unterordner und Dateien alphabetisch geordnet werden.<br />|,
-		lastUpdated => 1168865522
-	},
-
-	'file.url' => {
-		message => q|Wenn dieses Asset ein Datei Asset ist (Datei, Bild oder Zip Archiv), die URL f&uuml;r den Download der aktuellen Datei. Andernfalls ist diese Variable nicht definiert. |,
-		lastUpdated => 1166610951
-	},
-
-	'folder title' => {
-		message => q|Titel des Ordners.|,
-		lastUpdated => 1178864471
-	},
-
-	'icon.big' => {
-		message => q|URL zum Icon, das das Asset repr&auml;sentiert.|,
-		lastUpdated => 1178864480
-	},
-
-	'subfolder_loop' => {
-		message => q|Schleife, die alle Kinder des Ordners enth&auml;lt, die selbst wieder Ordner sind.<br />|,
-		lastUpdated => 1166611564
-	},
-
-	'file_loop' => {
-		message => q|Schleife, die alle Assets enth&auml;lt, die Kinder des Ordners aber selbst keine Ordner sind. |,
-		lastUpdated => 1166611017
-	},
-
-	'thumbnail.url' => {
-		message => q|Falls dieses Asset ein Bild ist, URL zu seinem Thumbnail.<br />|,
-		lastUpdated => 1166611619
-	},
-
-	'visitor cache timeout help' => {
-		message => q|Cache Timeout f&uuml;r Besucher Hilfe<br />|,
-		lastUpdated => 1166610535
-	},
-
-	'isFile' => {
-		message => q|Bedingung, die anzeigt, ob dieses Asset eine Datei Asset ist.<br />|,
-		lastUpdated => 1166611346
-	},
-
-	'823' => {
-		message => q|Gehe zur neuen Seite.|,
-		lastUpdated => 1178864398
-	},
-
-};
+  '823' => {
+    'lastUpdated' => 1178864398,
+    'message' => 'Gehe zur neuen Seite.'
+  },
+  '847' => {
+    'lastUpdated' => 1166611849,
+    'message' => 'Gehe zur&uuml;ck zur aktuellen Seite.'
+  },
+  'What Next description' => {
+    'lastUpdated' => 1166611737,
+    'message' => 'Wollen Sie nach dem Erstellen des Ordners zur&uuml;ck zur Seite, in der sie den Ordner erstellt haben oder direkt zum neu erstellten Ordner? '
+  },
+  'add file label' => {
+    'lastUpdated' => 1178864405,
+    'message' => 'Dateien hinzuf&uuml;gen.'
+  },
+  'addFile.label' => {
+    'lastUpdated' => 1166611794,
+    'message' => 'Das internationalisierte Label f&uuml;r das Hinzuf&uuml;gen von Dateien zum Ordner.<br />'
+  },
+  'addFile.url' => {
+    'lastUpdated' => 1178864411,
+    'message' => 'URL zum Hinzuf&uuml;gen von Dateien zum Ordner.'
+  },
+  'asset template variables body' => {
+    'lastUpdated' => 1168864845,
+    'message' => 'Jedes Asset bietet ihren Templates einen Satz an Variablen, die auf den Asset Eigenschaften basieren. Einige dieser Variablen k&ouml;nnen n&uuml;tzlich sein, andere nicht.'
+  },
+  'asset template variables title' => {
+    'lastUpdated' => 1168865650,
+    'message' => 'Ordner Asset Template Variablen'
+  },
+  'assetName' => {
+    'lastUpdated' => 1166610346,
+    'message' => 'Ordner'
+  },
+  'canEdit' => {
+    'lastUpdated' => 1166610710,
+    'message' => 'Bedingung, die anzeigt, ob dieses Asset vom aktuellen Benutzer editiert werden kann. '
+  },
+  'canView' => {
+    'lastUpdated' => 1166610749,
+    'message' => 'Bedingung, die anzeigt, ob der aktuelle Benutzer dieses Asset ansehen kann.<br />'
+  },
+  'controls' => {
+    'lastUpdated' => 1178864428,
+    'message' => 'Editier Kontroll Balken f&uuml;r dieses Kind.'
+  },
+  'date.epoch' => {
+    'lastUpdated' => 1166610844,
+    'message' => 'Datum der letzten &Auml;nderung, relativ zur Epoche'
+  },
+  'editForm sortOrder ascending' => {
+    'lastUpdated' => 1225317873,
+    'message' => 'Aufsteigend'
+  },
+  'editForm sortOrder descending' => {
+    'lastUpdated' => 1225317857,
+    'message' => 'Absteigend'
+  },
+  'editForm sortOrder description' => {
+    'lastUpdated' => 1225317845,
+    'message' => 'Die Reihenfolge, in der Dateien und Verzeichnisse sortiert werden k&ouml;nnen.'
+  },
+  'editForm sortOrder label' => {
+    'lastUpdated' => 1225317785,
+    'message' => 'Sortierungsfolge'
+  },
+  'file.url' => {
+    'lastUpdated' => 1166610951,
+    'message' => 'Wenn dieses Asset ein Datei Asset ist (Datei, Bild oder Zip Archiv), die URL f&uuml;r den Download der aktuellen Datei. Andernfalls ist diese Variable nicht definiert. '
+  },
+  'file_loop' => {
+    'lastUpdated' => 1166611017,
+    'message' => 'Schleife, die alle Assets enth&auml;lt, die Kinder des Ordners aber selbst keine Ordner sind. '
+  },
+  'folder icon.big' => {
+    'lastUpdated' => 1178864463,
+    'message' => 'gro&szlig;es Ordner Icon'
+  },
+  'folder icon.small' => {
+    'lastUpdated' => 1178864452,
+    'message' => 'kleines Ordner Icon'
+  },
+  'folder id' => {
+    'lastUpdated' => 1166611270,
+    'message' => 'assetId des Ordners.'
+  },
+  'folder menuTitle' => {
+    'lastUpdated' => 1201601414,
+    'message' => 'Der Me&uuml;titel des Ordners.'
+  },
+  'folder template body' => {
+    'lastUpdated' => 1166611911,
+    'message' => 'Folgende Variablen sind in Ordner Templates verf&uuml;gbar:<br />'
+  },
+  'folder template description' => {
+    'lastUpdated' => 1166611976,
+    'message' => 'Dieses Men&uuml; erlaubt das Ausw&auml;hlen eines Templates f&uuml;r die Darstellung der Ordner Inhalte.<br />'
+  },
+  'folder template title' => {
+    'lastUpdated' => 1166611991,
+    'message' => 'Ordner Template'
+  },
+  'folder title' => {
+    'lastUpdated' => 1178864471,
+    'message' => 'Titel des Ordners.'
+  },
+  'folder url' => {
+    'lastUpdated' => 1166611282,
+    'message' => 'URL des Ordners.'
+  },
+  'helpvar canAddFile' => {
+    'lastUpdated' => 1225317735,
+    'message' => 'Ist aktiviert, wenn der Benutzer eine Datei zum Ordner hinzuf&uuml;gen kann.'
+  },
+  'helpvar canEdit' => {
+    'lastUpdated' => 1225317767,
+    'message' => 'Ist aktiviert, wenn der Benutzer das Verzeichnis &auml;ndern kann.'
+  },
+  'icon.big' => {
+    'lastUpdated' => 1178864480,
+    'message' => 'URL zum Icon, das das Asset repr&auml;sentiert.'
+  },
+  'icon.small' => {
+    'lastUpdated' => 1166612052,
+    'message' => 'URL zum kleinen Icon, das das Asset repr&auml;sentiert.<br />'
+  },
+  'id' => {
+    'lastUpdated' => 1178864489,
+    'message' => 'assetId des Assets.'
+  },
+  'isFile' => {
+    'lastUpdated' => 1166611346,
+    'message' => 'Bedingung, die anzeigt, ob dieses Asset eine Datei Asset ist.<br />'
+  },
+  'isImage' => {
+    'lastUpdated' => 1166611399,
+    'message' => 'Bedingung, die anzeigt, ob dieses Asset ein Bild Asset ist.<br /> '
+  },
+  'menuTitle' => {
+    'lastUpdated' => 1201601432,
+    'message' => 'Der Men&uuml;titel des Assets.'
+  },
+  'size' => {
+    'lastUpdated' => 1178864496,
+    'message' => 'Gr&ouml;&szlig;e des Assets, formatiert.'
+  },
+  'sort alphabetically' => {
+    'lastUpdated' => 1178864503,
+    'message' => 'Alphabetische Sortierung?'
+  },
+  'sort alphabetically help' => {
+    'lastUpdated' => 1166611525,
+    'message' => 'Wollen sie die Elemente dieses Orders alphabetisch sortieren? Wenn sie nein w&auml;hlen, werden sie nach ihrem Rang sortiert.<br />'
+  },
+  'sortAlphabetically' => {
+    'lastUpdated' => 1168865522,
+    'message' => 'Bedingung, die anzeigt, da&szlig; Unterordner und Dateien alphabetisch geordnet werden.<br />'
+  },
+  'subfolder_loop' => {
+    'lastUpdated' => 1166611564,
+    'message' => 'Schleife, die alle Kinder des Ordners enth&auml;lt, die selbst wieder Ordner sind.<br />'
+  },
+  'synopsis' => {
+    'lastUpdated' => 1166611585,
+    'message' => 'Synopsis dieses Assets. '
+  },
+  'templateId' => {
+    'lastUpdated' => 1168865626,
+    'message' => 'Die ID des Templates, das f&uuml;r die Anzeige des Ordnerinhalts benutzt wird.'
+  },
+  'thumbnail.url' => {
+    'lastUpdated' => 1166611619,
+    'message' => 'Falls dieses Asset ein Bild ist, URL zu seinem Thumbnail.<br />'
+  },
+  'title' => {
+    'lastUpdated' => 1166610587,
+    'message' => 'Titel'
+  },
+  'type' => {
+    'lastUpdated' => 1178864511,
+    'message' => 'Typ dieses Assets.'
+  },
+  'url' => {
+    'lastUpdated' => 1166610552,
+    'message' => 'URL'
+  },
+  'visitor cache timeout' => {
+    'lastUpdated' => 1178864522,
+    'message' => 'Cache Timeout f&uuml;r Besucher'
+  },
+  'visitor cache timeout help' => {
+    'lastUpdated' => 1166610535,
+    'message' => 'Cache Timeout f&uuml;r Besucher Hilfe<br />'
+  },
+  'visitorCacheTimeout' => {
+    'lastUpdated' => 1168865553,
+    'message' => 'Wie lange soll Asset gecached werden (in Sekunden).<br /> '
+  }
+}
+;
 
 1;
