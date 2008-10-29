29bc35991deb3987d7a8ae11d8c7964de42df5d7
Translation Server <translation@plainblack.com>
2008-10-29 22:38:46 +0000

    Update from translation server

--- German/German/Tax.pm
+++ German/German/Tax.pm
@@ -1,7 +1,55 @@
 package WebGUI::i18n::German::Tax;
 use utf8;
-
 our $I18N = {
-};
+  'add a tax' => {
+    'lastUpdated' => 1225319631,
+    'message' => 'Stuerinformation hinzuf&uuml;gen'
+  },
+  'city' => {
+    'lastUpdated' => 1225319638,
+    'message' => 'Ort'
+  },
+  'city help' => {
+    'lastUpdated' => 1225319660,
+    'message' => 'Ort, wof&uuml;r die Steuer gilt'
+  },
+  'code' => {
+    'lastUpdated' => 1225319675,
+    'message' => 'Postleitzahl'
+  },
+  'code help' => {
+    'lastUpdated' => 1225319741,
+    'message' => 'Postleitzahl wof&uuml;r die Steuer g&uuml;ltig ist'
+  },
+  'country' => {
+    'lastUpdated' => 1225319750,
+    'message' => 'Land'
+  },
+  'country help' => {
+    'lastUpdated' => 1225319768,
+    'message' => 'Land wof&uuml;r die Steuer g&uuml;ltig ist'
+  },
+  'delete' => {
+    'lastUpdated' => 1225319795,
+    'message' => 'L&ouml;schen'
+  },
+  'state' => {
+    'lastUpdated' => 1225319809,
+    'message' => 'Bundesland'
+  },
+  'state help' => {
+    'lastUpdated' => 1225319835,
+    'message' => 'Bundesland, wof&uuml;r die Steuer g&uuml;ltig ist.'
+  },
+  'tax rate' => {
+    'lastUpdated' => 1225319846,
+    'message' => 'Steuersatz'
+  },
+  'tax rate help' => {
+    'lastUpdated' => 1225319908,
+    'message' => 'Geben Sie den Steuersatz in Prozent ein. Z.B.: 19% = 19.'
+  }
+}
+;
 
 1;
