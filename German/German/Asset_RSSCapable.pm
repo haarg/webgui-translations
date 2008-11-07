90a3cfa534b599304398ba0b3f36ca8e10184b6f
Translation Server <translation@plainblack.com>
2008-11-07 00:55:10 +0000

    Update from translation server

--- German/German/Asset_RSSCapable.pm
+++ German/German/Asset_RSSCapable.pm
@@ -1,33 +1,27 @@
 package WebGUI::i18n::German::Asset_RSSCapable;
 use utf8;
-
-
 our $I18N = {
-	'rssEnabled label' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'rssEnabled hoverHelp' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'rssTemplateId label' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'rssTemplateId hoverHelp' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'assetName' => {
-		message => q|RSSF&auml;higkeit|,
-		lastUpdated => 1164282518
-	},
-
-};
+  'assetName' => {
+    'lastUpdated' => 1164282518,
+    'message' => 'RSSF&auml;higkeit'
+  },
+  'rssEnabled hoverHelp' => {
+    'lastUpdated' => 1226019299,
+    'message' => '<p>RSS-Feed f&uuml;r dieses Asset einschalten? Wenn eingeschaltet, wird ein RSS-from-Parent-Asset erzeugt und als zus&auml;tzliches Kind f&uuml;r diesen Zweck verwaltet, Wenn nicht eingeschaltet, wird kein solches Kind erzeugt und existierende werden gel&ouml;scht</p>'
+  },
+  'rssEnabled label' => {
+    'lastUpdated' => 1226019145,
+    'message' => 'RSS einschalten'
+  },
+  'rssTemplateId hoverHelp' => {
+    'lastUpdated' => 1226019132,
+    'message' => 'Die Vorlage f&uuml;r den RSS-Feed dieses Assets'
+  },
+  'rssTemplateId label' => {
+    'lastUpdated' => 1226019107,
+    'message' => 'RSS-Vorlage'
+  }
+}
+;
 
 1;
