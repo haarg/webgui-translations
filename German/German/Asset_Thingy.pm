9b272b48a4156403de287cf94ea70926d4932959
Translation Server <translation@plainblack.com>
2008-11-18 09:39:23 +0000

    Update from translation server

--- German/German/Asset_Thingy.pm
+++ German/German/Asset_Thingy.pm
@@ -1,7 +1,63 @@
 package WebGUI::i18n::German::Asset_Thingy;
 use utf8;
-
 our $I18N = {
-};
+  'assetName' => {
+    'lastUpdated' => 1227001102,
+    'message' => 'Ding'
+  },
+  'name' => {
+    'lastUpdated' => 1227001018,
+    'message' => "Bezeichnung des Dings (Entit\x{e4}t)"
+  },
+  'pretext label' => {
+    'lastUpdated' => 1227000900,
+    'message' => 'Text vorab'
+  },
+  'size description' => {
+    'lastUpdated' => 1227000883,
+    'message' => "Einstellung der Gr\x{f6}\x{df}e (Zeichenbreite) der meisten Feldern"
+  },
+  'size label' => {
+    'lastUpdated' => 1227000793,
+    'message' => 'Größe'
+  },
+  'skip label' => {
+    'lastUpdated' => 1227000776,
+    'message' => 'nicht beachten'
+  },
+  'sort by label' => {
+    'lastUpdated' => 1227000752,
+    'message' => 'Sortieren nach'
+  },
+  'vertical description' => {
+    'lastUpdated' => 1227000728,
+    'message' => "Diese Eigenschaft bestimmt ob Kn\x{f6}pfe horizontal oder vertikal angezeigt werden."
+  },
+  'vertical label' => {
+    'lastUpdated' => 1227000570,
+    'message' => 'Vertikal'
+  },
+  'view last edited label' => {
+    'lastUpdated' => 1227000599,
+    'message' => 'Anzeige letzter Änderung'
+  },
+  'view thingy default label' => {
+    'lastUpdated' => 1227001071,
+    'message' => 'Ding Standardanzeige'
+  },
+  'viewScreenTitle' => {
+    'lastUpdated' => 1227000413,
+    'message' => 'Titel Anzeige-Bildschirm'
+  },
+  'width description' => {
+    'lastUpdated' => 1227000642,
+    'message' => 'Breite von Textfeldern in Pixel'
+  },
+  'width label' => {
+    'lastUpdated' => 1227000610,
+    'message' => 'Breite'
+  }
+}
+;
 
 1;
