c98b98aeff89027a774c08f5fad3f0b0688f3d7f
Translation Server <translation@plainblack.com>
2008-11-18 09:57:53 +0000

    Update from translation server

--- German/German/Asset_Thingy.pm
+++ German/German/Asset_Thingy.pm
@@ -1,13 +1,69 @@
 package WebGUI::i18n::German::Asset_Thingy;
 use utf8;
 our $I18N = {
+  'add field label' => {
+    'lastUpdated' => 1227002067,
+    'message' => 'Feld hinzufügen'
+  },
+  'add label' => {
+    'lastUpdated' => 1227001907,
+    'message' => 'Hinzufügen'
+  },
+  'add other thing label' => {
+    'lastUpdated' => 1227001926,
+    'message' => "Weitere Tabelle hinzuf\x{fc}gen"
+  },
+  'add thing label' => {
+    'lastUpdated' => 1227001889,
+    'message' => "Tabelle hinzuf\x{fc}gen"
+  },
+  'addThing_url' => {
+    'lastUpdated' => 1227002010,
+    'message' => 'Diese URL verweist zum Hinzufügen einer neuen Tabelle'
+  },
+  'add_url' => {
+    'lastUpdated' => 1227002250,
+    'message' => "URL zum Bildschirm Datensatz in diese Tabelle einf\x{fc}gen."
+  },
+  'after save label' => {
+    'lastUpdated' => 1227002054,
+    'message' => 'Nach der Sicherung'
+  },
   'assetName' => {
     'lastUpdated' => 1227001413,
     'message' => 'Tabelle'
   },
+  'default save button label' => {
+    'lastUpdated' => 1227001838,
+    'message' => 'Sichern'
+  },
+  'default thing label' => {
+    'lastUpdated' => 1227001824,
+    'message' => 'Standardtabelle'
+  },
+  'default value label' => {
+    'lastUpdated' => 1227001811,
+    'message' => 'Standardwert'
+  },
+  'default view label' => {
+    'lastUpdated' => 1227001796,
+    'message' => 'Standardanzeige'
+  },
   'delete label' => {
     'lastUpdated' => 1227001205,
     'message' => 'Löschen'
+  },
+  'display label' => {
+    'lastUpdated' => 1227001779,
+    'message' => 'Anzeige'
+  },
+  'duplicates label' => {
+    'lastUpdated' => 1227001767,
+    'message' => "Was soll mit Doppelteintr\x{e4}ge passieren?"
+  },
+  'edit label' => {
+    'lastUpdated' => 1227001701,
+    'message' => 'Bearbeiten'
   },
   'edit screen title label' => {
     'lastUpdated' => 1227001301,
@@ -24,6 +80,18 @@ our $I18N = {
   'pretext label' => {
     'lastUpdated' => 1227000900,
     'message' => 'Text vorab'
+  },
+  'search button label' => {
+    'lastUpdated' => 1227001661,
+    'message' => 'Suchen'
+  },
+  'search label' => {
+    'lastUpdated' => 1227001649,
+    'message' => 'Suchen'
+  },
+  'search thing label' => {
+    'lastUpdated' => 1227001639,
+    'message' => 'Suchen'
   },
   'size description' => {
     'lastUpdated' => 1227000883,
@@ -52,6 +120,18 @@ our $I18N = {
   'view last edited label' => {
     'lastUpdated' => 1227000599,
     'message' => 'Anzeige letzter Änderung'
+  },
+  'view screen tab label' => {
+    'lastUpdated' => 1227001561,
+    'message' => 'Anzeigebildschirm'
+  },
+  'view screen title label' => {
+    'lastUpdated' => 1227001591,
+    'message' => 'Titel Anzeigebildschirm'
+  },
+  'view template label' => {
+    'lastUpdated' => 1227001612,
+    'message' => 'Anzeigevorlage'
   },
   'view thingy default label' => {
     'lastUpdated' => 1227001458,
