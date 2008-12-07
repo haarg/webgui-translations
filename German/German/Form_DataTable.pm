60e4dd44b3271c7d097c2443139c5993b743e878
Translation Server <translation@plainblack.com>
2008-12-07 22:07:18 +0000

    Update from translation server

--- German/German/Form_DataTable.pm
+++ German/German/Form_DataTable.pm
@@ -1,9 +1,77 @@
 package WebGUI::i18n::German::Form_DataTable;
 use utf8;
 our $I18N = {
+  'add column' => {
+    'lastUpdated' => 1228687037,
+    'message' => 'Spalte hinzuf&uuml;gen'
+  },
+  'add row' => {
+    'lastUpdated' => 1228687045,
+    'message' => 'Zeile hinzuf&uuml;gen'
+  },
+  'cancel' => {
+    'lastUpdated' => 1228687051,
+    'message' => 'Abbrechen'
+  },
+  'delete column' => {
+    'lastUpdated' => 1228687060,
+    'message' => 'L&ouml;schen'
+  },
+  'delete confirm' => {
+    'lastUpdated' => 1228687078,
+    'message' => 'Sind Sie sicher, dass Sie diese Zeilen l&ouml;schen m&ouml;chten?'
+  },
+  'delete rows' => {
+    'lastUpdated' => 1228687092,
+    'message' => 'Ausgew&auml;hlte Zeilen l&ouml;schen'
+  },
+  'edit schema' => {
+    'lastUpdated' => 1228687127,
+    'message' => 'Schema bearbeiten'
+  },
+  'format date' => {
+    'lastUpdated' => 1228687133,
+    'message' => 'Datum'
+  },
+  'format email' => {
+    'lastUpdated' => 1228687140,
+    'message' => 'E-Mail'
+  },
+  'format link' => {
+    'lastUpdated' => 1228687145,
+    'message' => 'URL'
+  },
+  'format number' => {
+    'lastUpdated' => 1228687151,
+    'message' => 'Nummer'
+  },
+  'format text' => {
+    'lastUpdated' => 1228687157,
+    'message' => 'Text'
+  },
   'help' => {
     'lastUpdated' => 1228400866,
     'message' => 'Hilfe'
+  },
+  'help add row' => {
+    'lastUpdated' => 1228687199,
+    'message' => 'Anklicken von &quot;Zeile hinzuf&uuml;gen&quot; f&uuml;gt eine Zeile am Ende der Tabelle ein.'
+  },
+  'help default sort' => {
+    'lastUpdated' => 1228687265,
+    'message' => 'Standardm&auml;&szlig;ig wird die Tabelle so sortiert, wie sie es war, als sie gespeichert wurde.'
+  },
+  'help edit cell' => {
+    'lastUpdated' => 1228687337,
+    'message' => 'Eine Tabellenzelle doppelt anklicken, um sie zu bearbeiten. Die Tabulator-Taste speichert die aktuelle Zelle und &ouml;ffnet die n&auml;chste.'
+  },
+  'help reorder column' => {
+    'lastUpdated' => 1228687464,
+    'message' => 'Um Spalten anders anzuordnen mit der Maus ziehen und loslassen.'
+  },
+  'help select row' => {
+    'lastUpdated' => 1228687582,
+    'message' => 'Eine Zeile anklicken, um sie auszuw&auml;hlen. Durch Dr&uuml;cken von Umschalttaste oder STRG k&ouml;nnen mehrere Zeilen ausgew&auml;hlt werden.'
   },
   'ok' => {
     'lastUpdated' => 1228400826,
@@ -12,6 +80,18 @@ our $I18N = {
   'save' => {
     'lastUpdated' => 1228400845,
     'message' => 'Speichern'
+  },
+  'save failure' => {
+    'lastUpdated' => 1228687604,
+    'message' => 'Speichern fehlgeschlagen. Bitte nochmals versuchen.'
+  },
+  'save success' => {
+    'lastUpdated' => 1228687618,
+    'message' => 'Tabelle erfolgreich gespeichert.'
+  },
+  'topicName' => {
+    'lastUpdated' => 1228687628,
+    'message' => 'DataTable'
   }
 }
 ;
