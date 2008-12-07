7cefa96fde21db35c36343f17da9cf9120b55e16
Translation Server <translation@plainblack.com>
2008-12-07 21:34:31 +0000

    Update from translation server

--- German/German/Macro_MiniCart.pm
+++ German/German/Macro_MiniCart.pm
@@ -4,6 +4,34 @@ our $I18N = {
   'items' => {
     'lastUpdated' => 1226018735,
     'message' => 'Eine Schleife, die Informationen &uuml;ber alle Artikel des Warenkorbes enth&auml;lt'
+  },
+  'minicart title' => {
+    'lastUpdated' => 1228685384,
+    'message' => 'Vorlagen-Variablen f&uuml;r MiniCart-Makro'
+  },
+  'name' => {
+    'lastUpdated' => 1228685415,
+    'message' => 'Der konfigurierte Titel f&uuml;r den Artikel'
+  },
+  'price' => {
+    'lastUpdated' => 1228685435,
+    'message' => 'Der Preis pro Einheit f&uuml;r den Artikel'
+  },
+  'quantity' => {
+    'lastUpdated' => 1228685476,
+    'message' => 'Die Anzahl dieser Artikel im Warenkorb'
+  },
+  'totalItems' => {
+    'lastUpdated' => 1228685507,
+    'message' => 'Die Gesamtzahl der Artikel im Warenkorb'
+  },
+  'totalPrice' => {
+    'lastUpdated' => 1228685591,
+    'message' => 'Der Gesamtpreis aller Artikel im Warenkorb, formatiert mit zwei Nachkommastellen.'
+  },
+  'url' => {
+    'lastUpdated' => 1228685657,
+    'message' => 'Ein URL, um detailliertere Informationen &uuml;ber diesen artikel anzuzeigen - vom Cart\'s viewItem Bildschirm'
   }
 }
 ;
