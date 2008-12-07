dd8ceab1fe65c4fd33cf6df5f188a50bf035954d
Translation Server <translation@plainblack.com>
2008-12-07 22:52:23 +0000

    Update from translation server

--- German/German/Asset_WeatherData.pm
+++ German/German/Asset_WeatherData.pm
@@ -14,48 +14,48 @@ our $I18N = {
     'message' => 'Vorlage'
   },
   'WeatherData template title' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1228689971,
+    'message' => 'Wetterdaten-Vorlage'
   },
   'Your list of default weather locations' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1228690025,
+    'message' => '<p>Ihre Liste der Wetter-Orte, jeden auf einer eigenen Zeile:</p><p>City, ST || Zip Code || City, Country </p>'
   },
   'assetName' => {
     'lastUpdated' => 1226015253,
     'message' => 'Wetterdaten'
   },
   'cityState' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1228690074,
+    'message' => 'Die Stadt und der Staat der NOAA. Dies ist oft das gleich wie <strong>query</strong>'
   },
   'iconUrl' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1228690106,
+    'message' => 'Der URL zu einem Icon, das die Wetterlage visualisiert.'
   },
   'licenseKey' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1228690124,
+    'message' => 'Lizenzschl&uuml;ssel f&uuml;r weather.com'
   },
   'licenseKey help' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1228690293,
+    'message' => 'Sie haben diesen Schl&uuml;ssel von weather.com per E-Mail erhalten, nachdem Sie sich f&uuml;r den Weather XML Data Feed angemeldet hatten.'
   },
   'locations' => {
     'lastUpdated' => 1226015162,
     'message' => 'Die Liste der Orte, die der Nutzer eingegeben hat'
   },
   'ourLocations.loop' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1228690230,
+    'message' => 'eine Schleife, die Wetterinformationen zu allen konfigurierten Benutzer-Orten enth&auml;lt.'
   },
   'partnerId' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1228690250,
+    'message' => 'Partner-Id f&uuml;r weather.com'
   },
   'partnerId help' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1228690286,
+    'message' => 'Sie haben diese ID von weather.com per E-Mail erhalten, nachdem Sie sich f&uuml;r den Weather XML Data Feed angemeldet hatten.'
   },
   'query' => {
     'lastUpdated' => 1212756698,
@@ -70,12 +70,12 @@ our $I18N = {
     'message' => 'Temperatur in Fahrenheit'
   },
   'templateId' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1228690321,
+    'message' => 'Die ID der Vorlage, die genutzt wird um dieses Asset anzuzeigen.'
   },
   'weatherdata asset template variables title' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => 1228690336,
+    'message' => '<div align="left">WeatherData Asset Template Variables</div>'
   },
   'you need a weather.com key' => {
     'lastUpdated' => 1226015232,
