6642fb0a0772ae2d1a38273d7a9000a5b949541f
Translation Server <translation@plainblack.com>
2008-12-07 22:35:13 +0000

    Update from translation server

--- German/German/Asset_Shelf.pm
+++ German/German/Asset_Shelf.pm
@@ -1,6 +1,10 @@
 package WebGUI::i18n::German::Asset_Shelf;
 use utf8;
 our $I18N = {
+  'assetName' => {
+    'lastUpdated' => 1228688960,
+    'message' => 'Regal'
+  },
   'export' => {
     'lastUpdated' => 1226014779,
     'message' => 'Produkte exportieren'
@@ -9,9 +13,37 @@ our $I18N = {
     'lastUpdated' => 1226014792,
     'message' => 'Produkte importieren'
   },
+  'import successful' => {
+    'lastUpdated' => 1228688927,
+    'message' => 'Ihre Produkte wurden importiert'
+  },
   'price' => {
     'lastUpdated' => 1226014842,
     'message' => 'Der Preis f&uuml;r diesen Artikel'
+  },
+  'products' => {
+    'lastUpdated' => 1228689124,
+    'message' => '<p>Eine Schleife, die eine Liste der Produkte enth&auml;lt, die den Schl&uuml;sselworten entsprechen, die f&uuml;r dieses Regal festgelegt wurden. Jeder Eintrag in der Schleife enth&auml;lt alle Eigenschaften der passenden Artikelnummer zuz&uuml;glich der folgenden Variablen.</p>'
+  },
+  'shelf template' => {
+    'lastUpdated' => 1228689139,
+    'message' => 'Regal-Vorlage'
+  },
+  'shelf template help' => {
+    'lastUpdated' => 1228689182,
+    'message' => 'W&auml;hlen Sie die Vorlage, die die Liste der Produkte anzeigt, die diesem Regal zugeordnet sind.'
+  },
+  'shelves' => {
+    'lastUpdated' => 1228689245,
+    'message' => 'Eine Schleife, die eine Liste der Regale enth&auml;lt, die Kinder dieser im Asset-Baum sind. Jeder Eintrag der Schleife einh&auml;lt alle Eigenschaften des Regals.'
+  },
+  'subcategories' => {
+    'lastUpdated' => 1228689254,
+    'message' => 'Unterkategorien'
+  },
+  'thumbnailUrl' => {
+    'lastUpdated' => 1228689305,
+    'message' => 'Der URL zum einem Vorschaubild f&uuml;r diese Artikelnummer.&nbsp; Wenn es kein Vorschaubild gibt, ist diese Variable leer.'
   }
 }
 ;
