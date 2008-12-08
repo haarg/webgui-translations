2c7e618003e0296cd25e21a94700e0296c10e273
Translation Server <translation@plainblack.com>
2008-12-08 15:41:42 +0000

    Update from translation server

--- German/German/Shop.pm
+++ German/German/Shop.pm
@@ -1,25 +1,361 @@
 package WebGUI::i18n::German::Shop;
 use utf8;
 our $I18N = {
+  'Backordered' => {
+    'lastUpdated' => 1228748598,
+    'message' => 'Nicht vorr&auml;tig'
+  },
+  'Cancelled' => {
+    'lastUpdated' => 1228748625,
+    'message' => 'Storniert'
+  },
+  'NotShipped' => {
+    'lastUpdated' => 1228748641,
+    'message' => 'Nicht versandt'
+  },
+  'Payment Information' => {
+    'lastUpdated' => 1228748656,
+    'message' => 'Bezahl-Informationen'
+  },
+  'Preferred Payment Type' => {
+    'lastUpdated' => 1228748672,
+    'message' => 'Bevorzugte Zahlungsart'
+  },
   'Shipped' => {
     'lastUpdated' => 1228174743,
     'message' => 'Versandt'
+  },
+  'add a new address' => {
+    'lastUpdated' => 1228748699,
+    'message' => 'Eine neue Adresse hinzuf&uuml;gen'
+  },
+  'add a vendor' => {
+    'lastUpdated' => 1228748732,
+    'message' => 'Einen Anbieter hinzuf&uuml;gen'
+  },
+  'add credit message' => {
+    'lastUpdated' => 1228749526,
+    'message' => '%s wurde dem %s Kredit-Accout hinzugef&uuml;gt mit einem Gesamtkreditbetrag von %s'
+  },
+  'add payment method' => {
+    'lastUpdated' => 1228748766,
+    'message' => 'Eine Zahlungsart hinzuf&uuml;gen'
+  },
+  'add shipper' => {
+    'lastUpdated' => 1228748793,
+    'message' => 'Eine Versandart hinzuf&uuml;gen'
+  },
+  'add to cart' => {
+    'lastUpdated' => 1228748807,
+    'message' => 'In den Warenkorb legen'
+  },
+  'addButton help' => {
+    'lastUpdated' => 1228748835,
+    'message' => 'Ein Button, mit dem der Benutzer eine neue Adresse hinzuf&uuml;gen kann.'
+  },
+  'address' => {
+    'lastUpdated' => 1228748842,
+    'message' => 'Adresse'
+  },
+  'address book template' => {
+    'lastUpdated' => 1228748854,
+    'message' => 'Adressbuch-Vorlage'
+  },
+  'address book template help' => {
+    'lastUpdated' => 1228748877,
+    'message' => 'Diese Vorlage legt das Aussehen des Adressbuches fest'
+  },
+  'address help' => {
+    'lastUpdated' => 1228748896,
+    'message' => 'Eine HTML-formatierte Adresse'
+  },
+  'address labelField help' => {
+    'lastUpdated' => 1228748973,
+    'message' => 'Ein Feld, das die Adressebezeichnung enth&auml;lt. Beispiele: Heimatanschrift oder Arbeit'
+  },
+  'address loop help' => {
+    'lastUpdated' => 1228749055,
+    'message' => 'Eine schleife, die die Liste der Adressen dieses Adressbuches und die Bearbeitungsm&ouml;glichkeiten enth&auml;lt'
+  },
+  'address nameField help' => {
+    'lastUpdated' => 1228749090,
+    'message' => 'Ein Feld, das den Namen der Person/Firma f&uuml;r diese Adresse enth&auml;lt.'
+  },
+  'address1 help' => {
+    'lastUpdated' => 1228749103,
+    'message' => 'Die erste Adresszeile'
+  },
+  'address1Field help' => {
+    'lastUpdated' => 1228749545,
+    'message' => 'Das Feld f&uuml;r die erste Adresszeile'
+  },
+  'address2 help' => {
+    'lastUpdated' => 1228749116,
+    'message' => 'Die zweite Adresszeile'
+  },
+  'address2Field help' => {
+    'lastUpdated' => 1228749558,
+    'message' => 'Das Feld f&uuml;r die zweite Adreszeile'
+  },
+  'address3 help' => {
+    'lastUpdated' => 1228749127,
+    'message' => 'Die dritte Adresszeile'
+  },
+  'address3Field help' => {
+    'lastUpdated' => 1228749572,
+    'message' => 'Das Feld f&uuml;r die dritte Adresszeile'
+  },
+  'addresses loop help' => {
+    'lastUpdated' => 1228749592,
+    'message' => 'Eine Schleife, die alle Adressen dieses Adressbuches enth&auml;lt'
   },
   'amount' => {
     'lastUpdated' => 1228174754,
     'message' => 'Betrag'
   },
+  'amount help' => {
+    'lastUpdated' => 1228749627,
+    'message' => 'Der Gesamtbetrag dieser Transaktion, formatiert mit zwei Nachkommastellen.'
+  },
+  'available' => {
+    'lastUpdated' => 1228749136,
+    'message' => 'Verf&uuml;gbar'
+  },
+  'biweekly' => {
+    'lastUpdated' => 1228749149,
+    'message' => 'Zwei Wochen'
+  },
+  'cancel recurring transaction' => {
+    'lastUpdated' => 1228749671,
+    'message' => 'Wiederkehrende Transaktion abbrechen'
+  },
+  'cancelRecurringUrl' => {
+    'lastUpdated' => 1228749700,
+    'message' => 'Ein URL, mit dem der Bentuzer eine wiederkehrende Transaktion abbrechen kann.'
+  },
+  'cart template' => {
+    'lastUpdated' => 1228749715,
+    'message' => 'Warenkorb-Vorlage'
+  },
+  'cart template help' => {
+    'lastUpdated' => 1228749735,
+    'message' => 'Diese Vorlage bestimmt, wie der Warenkorb dargestellt wird.'
+  },
+  'cashier' => {
+    'lastUpdated' => 1228749754,
+    'message' => 'Kasse'
+  },
+  'checkout button' => {
+    'lastUpdated' => 1228749778,
+    'message' => 'Zur Kasse'
+  },
+  'checkoutButton help' => {
+    'lastUpdated' => 1228749830,
+    'message' => 'Der Button, den der Benutzer anklicken muss, um zur Auswahl der Bezahlungsmethode zu gelangen.'
+  },
+  'choose payment gateway message' => {
+    'lastUpdated' => 1228749841,
+    'message' => 'Wie m&ouml;chten Sie bezahlen?'
+  },
+  'choose shipping button' => {
+    'lastUpdated' => 1228749871,
+    'message' => 'W&auml;hlen Sie Adresse und Bezahlung.'
+  },
+  'chooseShippingButton help' => {
+    'lastUpdated' => 1228749909,
+    'message' => 'Anklicken dieses Buttons f&uuml;hrt den Benutzer zur Auswahl einer Versandadresse aus dem Adressbuch.'
+  },
   'city' => {
     'lastUpdated' => 1228174762,
     'message' => 'Stadt'
+  },
+  'city help' => {
+    'lastUpdated' => 1228749929,
+    'message' => 'Die Stadt zu dieser Adresse'
+  },
+  'cityField help' => {
+    'lastUpdated' => 1228749947,
+    'message' => 'Ein Feld, das die Stadt der Adresse enth&auml;lt'
+  },
+  'code' => {
+    'lastUpdated' => 1228749956,
+    'message' => 'Postleitzahl'
+  },
+  'code help' => {
+    'lastUpdated' => 1228749973,
+    'message' => 'Die Postleitzahl dieser Adresse'
+  },
+  'codeField help' => {
+    'lastUpdated' => 1228750069,
+    'message' => 'Ein Feld, das die Postleitzahl zu dieser Adresse enth&auml;lt'
+  },
+  'company url' => {
+    'lastUpdated' => 1228750080,
+    'message' => 'Firmen-URL'
   },
   'date' => {
     'lastUpdated' => 1228174772,
     'message' => '<p>Datum&nbsp;</p>'
   },
+  'email' => {
+    'lastUpdated' => 1228749164,
+    'message' => 'E-Mail'
+  },
+  'firstName' => {
+    'lastUpdated' => 1228749171,
+    'message' => 'Vorname'
+  },
+  'lastName' => {
+    'lastUpdated' => 1228749186,
+    'message' => 'Nachname'
+  },
+  'name' => {
+    'lastUpdated' => 1228749193,
+    'message' => 'Name'
+  },
+  'organization' => {
+    'lastUpdated' => 1228749201,
+    'message' => 'Organisation'
+  },
+  'price' => {
+    'lastUpdated' => 1228749212,
+    'message' => 'Preis'
+  },
+  'print' => {
+    'lastUpdated' => 1228750205,
+    'message' => 'Drucken'
+  },
+  'quarterly' => {
+    'lastUpdated' => 1228749224,
+    'message' => 'Drei Monate'
+  },
   'tax' => {
     'lastUpdated' => 1228174791,
     'message' => 'Steuer'
+  },
+  'tax help' => {
+    'lastUpdated' => 1228750492,
+    'message' => 'Der Steuerbetrag aller Artikel im Warenkorb'
+  },
+  'taxes' => {
+    'lastUpdated' => 1228749240,
+    'message' => 'Steuern'
+  },
+  'taxes help' => {
+    'lastUpdated' => 1228750527,
+    'message' => 'Der Steuerbetrag dieser Transaktion, formatiert mit zwei Nachkommastellen.'
+  },
+  'thank you message' => {
+    'lastUpdated' => 1228750567,
+    'message' => 'Vielen Dank f&uuml;r Ihren Einkauf. Bitte speichern Sie diesen Beleg.'
+  },
+  'too many of this item' => {
+    'lastUpdated' => 1228750598,
+    'message' => 'Sie k&ouml;nnen nicht so viele %s in ihren Warenkorb legen.'
+  },
+  'total' => {
+    'lastUpdated' => 1228750450,
+    'message' => 'Summe'
+  },
+  'totalItems help' => {
+    'lastUpdated' => 1228750623,
+    'message' => 'Die Gesamtzahl der Artikel in Ihrem Warenkorb'
+  },
+  'totalPrice help' => {
+    'lastUpdated' => 1228750657,
+    'message' => 'Die aktuelle Gesamtsumme Ihres Warenkorbes'
+  },
+  'tracking number' => {
+    'lastUpdated' => 1228750669,
+    'message' => 'Trackingnummer'
+  },
+  'transaction id' => {
+    'lastUpdated' => 1228750680,
+    'message' => 'Transaktions-ID'
+  },
+  'transactionCode' => {
+    'lastUpdated' => 1228750735,
+    'message' => 'Der Transaktionscode oder die Transaktions-ID, die vom Bezahl-Gateway zugewiesen wurde. Wird nicht von allen Bezahlgateways unterst&uuml;tzt.'
+  },
+  'transactionId' => {
+    'lastUpdated' => 1228750438,
+    'message' => 'Der eindeutige Bezeichner f&uuml;r diese Transaktion'
+  },
+  'transactions' => {
+    'lastUpdated' => 1228750379,
+    'message' => 'Transaktionen'
+  },
+  'update' => {
+    'lastUpdated' => 1228749254,
+    'message' => 'Aktualisieren'
+  },
+  'update cart button' => {
+    'lastUpdated' => 1228750230,
+    'message' => 'Warenkorb aktualisieren'
+  },
+  'updateButton help' => {
+    'lastUpdated' => 1228750278,
+    'message' => 'Anklicken dieses Buttons speichert Ihre &Auml;nderungen des Warenkorbes und kalkuliert die Preise neu.'
+  },
+  'use this address' => {
+    'lastUpdated' => 1228750289,
+    'message' => 'Diese Adresse verwenden'
+  },
+  'useButton help' => {
+    'lastUpdated' => 1228750334,
+    'message' => 'Ein Button, mit dem der Benutzer eine existierende Adresse f&uuml;r die Verwandung in einem Formular ausw&auml;hlen kann.'
+  },
+  'userId' => {
+    'lastUpdated' => 1228750362,
+    'message' => 'Die Benutzer-ID des Benutzers, der diese Trransaktion ausgef&uuml;hrt hat.'
+  },
+  'username' => {
+    'lastUpdated' => 1228749285,
+    'message' => 'Benutzer'
+  },
+  'username help' => {
+    'lastUpdated' => 1228749320,
+    'message' => 'Der Name des BEnutzer, der diese Transaktion ausgef&uuml;hrt hat. Spart eine Benutzernamen-Suche.'
+  },
+  'vendors' => {
+    'lastUpdated' => 1228749341,
+    'message' => 'Lieferant'
+  },
+  'view cart' => {
+    'lastUpdated' => 1228749332,
+    'message' => 'Warenkorb ansehen'
+  },
+  'view my purchases template' => {
+    'lastUpdated' => 1228750757,
+    'message' => 'Vorlage die meine Eink&auml;ufe anzeigt'
+  },
+  'view my purchases template help' => {
+    'lastUpdated' => 1228750801,
+    'message' => 'Diese Vorlage zeigt detaillierte Informationen &uuml;ber die Transaktionen des Benutzers an.'
+  },
+  'viewDetailURL' => {
+    'lastUpdated' => 1228750832,
+    'message' => 'Ein URL, um detailliertere Informatinoen &uuml;ber die Transaktionen des Benutzers anzuzeigen'
+  },
+  'viewItemUrl' => {
+    'lastUpdated' => 1228750877,
+    'message' => 'Ein URL um detailliertere Informationen &uuml;ber diesen Artikel dieser Transaktion anzuzeigen'
+  },
+  'weekly' => {
+    'lastUpdated' => 1228749264,
+    'message' => 'Woche'
+  },
+  'who can manage' => {
+    'lastUpdated' => 1228750114,
+    'message' => 'Wer darf verwalten?'
+  },
+  'who can manage help' => {
+    'lastUpdated' => 1228750415,
+    'message' => 'Die Gruppe mit den Verwaltungsrechten im Shop-System.'
+  },
+  'yearly' => {
+    'lastUpdated' => 1228749272,
+    'message' => 'Jahr'
   }
 }
 ;
