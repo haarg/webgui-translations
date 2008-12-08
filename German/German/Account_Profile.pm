0f1a273aeade70e260f22e352fcbb9454d524e29
Translation Server <translation@plainblack.com>
2008-12-08 15:56:32 +0000

    Update from translation server

--- German/German/Account_Profile.pm
+++ German/German/Account_Profile.pm
@@ -1,5 +1,139 @@
 package WebGUI::i18n::German::Account_Profile;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'back label' => {
+    'lastUpdated' => 1228751240,
+    'message' => 'Zur&uuml;ck zur Site'
+  },
+  'back to profile label' => {
+    'lastUpdated' => 1228751229,
+    'message' => 'Zur&uuml;ck zum Profil'
+  },
+  'edit my profile' => {
+    'lastUpdated' => 1228751219,
+    'message' => 'Mein Profil bearbeiten'
+  },
+  'error label' => {
+    'lastUpdated' => 1228751210,
+    'message' => 'Fehler'
+  },
+  'friends only' => {
+    'lastUpdated' => 1228751185,
+    'message' => 'Ihr Profil ist von Freunden einsehbar.'
+  },
+  'friends only label' => {
+    'lastUpdated' => 1228751203,
+    'message' => 'Nur Freunde'
+  },
+  'member since' => {
+    'lastUpdated' => 1228751195,
+    'message' => 'Mitglied seit'
+  },
+  'private label' => {
+    'lastUpdated' => 1228751167,
+    'message' => 'Nicht &ouml;ffentlich'
+  },
+  'private profile' => {
+    'lastUpdated' => 1228751154,
+    'message' => 'Ihr Profil ist nicht &ouml;ffentlich'
+  },
+  'profile as others label' => {
+    'lastUpdated' => 1228751263,
+    'message' => 'Mein Profil so anzeigen, wie es von anderen gesehen wird.'
+  },
+  'profile edit layout template hoverHelp' => {
+    'lastUpdated' => 1228751783,
+    'message' => 'W&auml;hlen Sie ein Layout aus der Liste, zur Anzeige der verschiednenen Account-Plugins, die vom aktuellen Benutzer betrachtet werden k&ouml;nnen, und des Inhalts des aktuell ausgew&auml;hlten.'
+  },
+  'profile edit layout template label' => {
+    'lastUpdated' => 1228751709,
+    'message' => 'Layout-Vorlage anzeigen'
+  },
+  'profile edit template hoverHelp' => {
+    'lastUpdated' => 1228751326,
+    'message' => 'W&auml;hlen Sie die Hauptvorlage, um ein Profil zu bearbeiten'
+  },
+  'profile edit template label' => {
+    'lastUpdated' => 1228751289,
+    'message' => 'Vorlage bearbeiten'
+  },
+  'profile error template hoverHelp' => {
+    'lastUpdated' => 1228751380,
+    'message' => 'W&auml;hlen Sie ein Vorlage, mit der Fehler im Profil dem Benutzer angezeigt werden.'
+  },
+  'profile error template label' => {
+    'lastUpdated' => 1228751340,
+    'message' => 'Fehler-Vorlage'
+  },
+  'profile layout template hoverHelp' => {
+    'lastUpdated' => 1228751746,
+    'message' => '<p>W&auml;hlen Sie ein Layout aus der Liste, zur Anzeige der verschiednenen Account-Plugins, die vom aktuellen Benutzer bearbeitet werden k&ouml;nnen, und des Inhalts des aktuell ausgew&auml;hlten.</p>'
+  },
+  'profile layout template label' => {
+    'lastUpdated' => 1228751560,
+    'message' => 'Layout-Vorlage bearbeiten'
+  },
+  'profile not public error' => {
+    'lastUpdated' => 1228751542,
+    'message' => 'Dieses Benutzerprofil ist nicht &ouml;ffentlich einsehbar.'
+  },
+  'profile style template hoverHelp' => {
+    'lastUpdated' => 1228751519,
+    'message' => 'W&auml;hlen Sie eine Stil-Vorlage aus der Liste, in die der Nachrichteneingang eingebettet werden soll.'
+  },
+  'profile style template label' => {
+    'lastUpdated' => 1228751466,
+    'message' => 'Stil-Vorlage'
+  },
+  'profile view template hoverHelp' => {
+    'lastUpdated' => 1228751456,
+    'message' => 'W&auml;hlen Sie die Hauptvorlage, um ein Profil anzuzeigen'
+  },
+  'profile view template label' => {
+    'lastUpdated' => 1228751438,
+    'message' => 'Vorlage anzeigen'
+  },
+  'public label' => {
+    'lastUpdated' => 1228751140,
+    'message' => '&Ouml;ffentlich'
+  },
+  'public profile' => {
+    'lastUpdated' => 1228751129,
+    'message' => 'Ihr Profil ist &ouml;ffentlich'
+  },
+  'required field' => {
+    'lastUpdated' => 1228751117,
+    'message' => 'Pflichtfeld'
+  },
+  'save changes' => {
+    'lastUpdated' => 1228751097,
+    'message' => '&Auml;nderungen speichern'
+  },
+  'send friend request label' => {
+    'lastUpdated' => 1228751406,
+    'message' => 'Freund-Anfrage senden'
+  },
+  'send private message label' => {
+    'lastUpdated' => 1228751085,
+    'message' => 'Private Nachricht senden'
+  },
+  'set by admin' => {
+    'lastUpdated' => 1228751068,
+    'message' => 'Der Site-Administrator hat dieses Feld als nicht sichtbar markiert.'
+  },
+  'title' => {
+    'lastUpdated' => 1228751018,
+    'message' => 'Profil'
+  },
+  'view my profile' => {
+    'lastUpdated' => 1228751035,
+    'message' => 'Mein Profil anzeigen'
+  },
+  'visitor profile restricted' => {
+    'lastUpdated' => 1228751427,
+    'message' => 'Das Benutzerprofil ist eingeschr&auml;nkt sichtbar.'
+  }
+}
+;
 
 1;
