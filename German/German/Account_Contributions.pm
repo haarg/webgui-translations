1da6c75765d05f3b48d7dd8a600ca1d2e22d6ab6
Translation Server <translation@plainblack.com>
2008-12-08 16:01:39 +0000

    Update from translation server

--- German/German/Account_Contributions.pm
+++ German/German/Account_Contributions.pm
@@ -1,5 +1,55 @@
 package WebGUI::i18n::German::Account_Contributions;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'contrib layout template hoverHelp' => {
+    'lastUpdated' => 1228752092,
+    'message' => 'W&auml;hlen Sie eine Layout-Vorlage, in die der Inhalt der unterschiedlichen Methoden innerhalb des Beitr&auml;ge-Karteirreiters eingebettet werden soll.'
+  },
+  'contrib layout template label' => {
+    'lastUpdated' => 1228752018,
+    'message' => 'Layout-Vorlage'
+  },
+  'contrib style template hoverHelp' => {
+    'lastUpdated' => 1228752008,
+    'message' => 'W&auml;hlen Sie eine Stil-Vorlage aus der Liste, in die der Beitr&auml;ge-Karteireiter eingebettet werden soll.'
+  },
+  'contrib style template label' => {
+    'lastUpdated' => 1228751961,
+    'message' => 'Stil-Vorlage'
+  },
+  'contrib view template hoverHelp' => {
+    'lastUpdated' => 1228751950,
+    'message' => 'W&auml;hlen Sie eine Vorlage f&uuml;r die Anzeige der Benutzer-Beitr&auml;ge'
+  },
+  'contrib view template label' => {
+    'lastUpdated' => 1228751931,
+    'message' => 'Beitrags-Vorlage anzeigen'
+  },
+  'contribution count' => {
+    'lastUpdated' => 1228751912,
+    'message' => 'Beitr&auml;ge insgesamt'
+  },
+  'date label' => {
+    'lastUpdated' => 1228751868,
+    'message' => 'Datum'
+  },
+  'no contributions' => {
+    'lastUpdated' => 1228751862,
+    'message' => 'Keine Beitr&auml;ge'
+  },
+  'title' => {
+    'lastUpdated' => 1228751826,
+    'message' => 'Beitr&auml;ge'
+  },
+  'title label' => {
+    'lastUpdated' => 1228751832,
+    'message' => 'Titel'
+  },
+  'type label' => {
+    'lastUpdated' => 1228751838,
+    'message' => 'Typ'
+  }
+}
+;
 
 1;
