638042c20ab820bb183353d444f8d64dd401c1bf
Translation Server <translation@plainblack.com>
2008-12-08 14:50:12 +0000

    Update from translation server

--- German/German/Account_Shop.pm
+++ German/German/Account_Shop.pm
@@ -1,5 +1,31 @@
 package WebGUI::i18n::German::Account_Shop;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'manage purchases label' => {
+    'lastUpdated' => 1228747782,
+    'message' => 'Eink&auml;ufe verwalten'
+  },
+  'shop layout template hoverHelp' => {
+    'lastUpdated' => 1228747768,
+    'message' => 'W&auml;hlen Sie eine Layout-Vorlage in die der Inhalt der verschiedenen Methoden innerhalb des Shop-Karteireiters eingebunden werden soll.'
+  },
+  'shop layout template label' => {
+    'lastUpdated' => 1228747727,
+    'message' => 'Layout-Vorlage'
+  },
+  'shop style template hoverHelp' => {
+    'lastUpdated' => 1228747714,
+    'message' => 'W&auml;hlen Sie eine Stil-Vorlage aus der Liste, um darin den Shop-Karteireiter einzubetten.'
+  },
+  'shop style template label' => {
+    'lastUpdated' => 1228747679,
+    'message' => 'Stil-Vorlage'
+  },
+  'title' => {
+    'lastUpdated' => 1228747667,
+    'message' => 'Shop'
+  }
+}
+;
 
 1;
--- German/German/Account_User.pm
+++ German/German/Account_User.pm
@@ -1,5 +1,31 @@
 package WebGUI::i18n::German::Account_User;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'templates in auth method message' => {
+    'lastUpdated' => 1228747577,
+    'message' => 'Die Anzeige-Vorlage f&uuml;r dieses Modul finden Sie in den Einstellungen f&uuml;r die Authentifzierungsmodule, die f&uuml;r Ihre Site installiert sind.'
+  },
+  'title' => {
+    'lastUpdated' => 1228747432,
+    'message' => 'Account'
+  },
+  'user layout template hoverHelp' => {
+    'lastUpdated' => 1228747513,
+    'message' => 'W&auml;hlen Sie eine Layout-Vorlage in die der Inhalt der verschiedenen Methoden innerhalb des Account-Karteireiters eingebunden werden soll.'
+  },
+  'user layout template label' => {
+    'lastUpdated' => 1228747591,
+    'message' => 'Layout-Vorlage'
+  },
+  'user style template hoverHelp' => {
+    'lastUpdated' => 1228747700,
+    'message' => 'W&auml;hlen Sie eine Stil-Vorlage aus der Liste, um darin den Account-Karteireiter einzubetten.'
+  },
+  'user style template label' => {
+    'lastUpdated' => 1228747655,
+    'message' => 'Stil-Vorlage'
+  }
+}
+;
 
 1;
