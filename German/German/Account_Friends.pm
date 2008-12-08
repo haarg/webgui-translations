c74c6eb1694af12ddd523122c0126c96b4a0959b
Translation Server <translation@plainblack.com>
2008-12-08 16:45:27 +0000

    Update from translation server

--- German/German/Account_Friends.pm
+++ German/German/Account_Friends.pm
@@ -1,17 +1,97 @@
 package WebGUI::i18n::German::Account_Friends;
 use utf8;
 our $I18N = {
+  'add to network label' => {
+    'lastUpdated' => 1228754447,
+    'message' => 'Zum Netzwerk hinzuf&uuml;gen'
+  },
+  'adding user message' => {
+    'lastUpdated' => 1228754431,
+    'message' => 'Sie f&uuml;gen %s zu Ihrem Netzwerk hinzu'
+  },
   'back label' => {
     'lastUpdated' => 1228691832,
     'message' => 'Zur&uuml;ck zur Site'
+  },
+  'back to profile label' => {
+    'lastUpdated' => 1228754381,
+    'message' => 'Zur&uuml;ck zum Profil'
+  },
+  'back to user profile' => {
+    'lastUpdated' => 1228754462,
+    'message' => 'Zur&uuml;ck zum Benutzerprofil'
+  },
+  'cancel button label' => {
+    'lastUpdated' => 1228754336,
+    'message' => 'Abbrechen'
+  },
+  'default friend comments' => {
+    'lastUpdated' => 1228754523,
+    'message' => '<p>%s, ich w&uuml;rde Sie gerne in mein Freunde-Netzwerk aufnehmen. </p><p>Vielen Dank, &amp;s</p>'
+  },
+  'does not want to be a friend' => {
+    'lastUpdated' => 1228754093,
+    'message' => 'Der Benutzer m&ouml;chte nicht zu den Freunden hinzugef&uuml;gt werden.'
+  },
+  'edit my friends' => {
+    'lastUpdated' => 1228752377,
+    'message' => 'Meine Feeunde bearbeiten'
   },
   'error label' => {
     'lastUpdated' => 1228691840,
     'message' => 'Fehler'
   },
+  'error user is already friend' => {
+    'lastUpdated' => 1228754119,
+    'message' => 'Dieser Benutzer ist bereits Mitgleid in Ihrem Freunde-Netzwerk'
+  },
+  'error user is already invited' => {
+    'lastUpdated' => 1228754164,
+    'message' => 'Sie haben diesem Beenutzer bereits eine Einladung zum Beitritt zu Ihrem Freunde-Netzwerk gesandt.'
+  },
+  'friend removed' => {
+    'lastUpdated' => 1228754210,
+    'message' => '%s wurde von Ihrer Freunde-Liste entfernt'
+  },
+  'friends as others label' => {
+    'lastUpdated' => 1228754366,
+    'message' => 'Meine Freunde betrachten, wie sie von anderen gesehen werden'
+  },
+  'member since' => {
+    'lastUpdated' => 1228754178,
+    'message' => 'Mitglied seit'
+  },
   'no access' => {
     'lastUpdated' => 1228691877,
     'message' => 'Sie haben keine Berechtigung, die Freunde-Liste dieses Benutzers zu betrachten.'
+  },
+  'no friend selected' => {
+    'lastUpdated' => 1228754257,
+    'message' => 'Sieh haben keinen Freund zum L&ouml;schen von Ihrere Freunde-Liste ausgew&auml;hlt.'
+  },
+  'not a friend error' => {
+    'lastUpdated' => 1228754306,
+    'message' => 'Der zum L&ouml;schen ausgew&auml;hlte Benutzer ist nicht Mitglied in Ihrem Freunde-Netzwerk'
+  },
+  'remove confirm message' => {
+    'lastUpdated' => 1228754634,
+    'message' => 'Sind Sie sicher, dass Sie %s aus Ihrer Freunde-Liste l&ouml;schen m&ouml;chten?'
+  },
+  'remove confirm no' => {
+    'lastUpdated' => 1228754541,
+    'message' => 'Nein, ich habe mich geirrt.'
+  },
+  'remove confirm yes' => {
+    'lastUpdated' => 1228754559,
+    'message' => 'Ja, diesen Benutzer l&ouml;schen.'
+  },
+  'remove from friends label' => {
+    'lastUpdated' => 1228754575,
+    'message' => 'Aus dem Freunde-Netzwerk l&ouml;schen'
+  },
+  'send invite button label' => {
+    'lastUpdated' => 1228754271,
+    'message' => 'Einladung schicken'
   },
   'send private message label' => {
     'lastUpdated' => 1228691928,
