20b5ab637a27167a912010dbbd9cc75c9e8473ad
Translation Server <translation@plainblack.com>
2008-12-09 11:00:36 +0000

    Update from translation server

--- Dutch/Dutch/Asset_Layout.pm
+++ Dutch/Dutch/Asset_Layout.pm
@@ -1,127 +1,103 @@
 package WebGUI::i18n::Dutch::Asset_Layout;
 use utf8;
-
 our $I18N = {
-	'dragger.init' => {
-		message => q|Voor het werken van de klik en sleep functies is HTML en Javascript nodig. Deze variabele is leeg als admin niet getoond wordt.|,
-		lastUpdated => 1208523993
-	},
-
-	'content' => {
-		message => q|De bewerkte inhoud van de asset.|,
-		lastUpdated => 1149258297
-	},
-
-	'assetsToHide' => {
-		message => q|De asset-ID's van alle assets die niet afbeeld zouden moeten worden in dit asset. Deze ID's worden gescheiden door "\n", waarmee een nieuwe regel wordt aangeduid in *nix (unix, linux, ...) systemen. |,
-		lastUpdated => 1170688133
-	},
-
-	'layout asset template variables title' => {
-		message => q|Pagina asset sjabloonvariabelen|,
-		lastUpdated => 1170689046
-	},
-
-	'assetName' => {
-		message => q|Pagina layout|,
-		lastUpdated => 1148916634
-	},
-
-	'asset order hoverHelp' => {
-		message => q|Kies of nieuwe en niet gepositioneerde assets aan de boven-of onderkant van de eerste inhoud positie van de pagina toegevoegd moeten worden.|,
-		lastUpdated => 1213628542
-	},
-
-	'contentPositions' => {
-		message => q|Een variabele (string) die de verschillende plaatsen bevat waarop assets weergegeven kunnen worden en welke assets op welke plaats moeten staan en in welke volgorde.|,
-		lastUpdated => 1170688337
-	},
-
-	'dragger.icon' => {
-		message => q|Een icoon waarmee de asset verplaatst kan worden door middel van het klikken en slepen met de muis. Deze variabele is leeg als admin niet getoond wordt. |,
-		lastUpdated => 1207840091
-	},
-
-	'asset order label' => {
-		message => q|Voeg nieuwe assets toe|,
-		lastUpdated => 1205853348
-	},
-
-	'assets to hide description' => {
-		message => q|Deze lijst bevat een aanvinkveld voor elk subasset van de pagina layout. Elke asset die niet in de pagina layout asset getoond moet worden, moet hier worden aangevinkt.|,
-		lastUpdated => 1149258519
-	},
-
-	'id' => {
-		message => q|De assets identificatie van de asset.|,
-		lastUpdated => 1149258041
-	},
-
-	'asset order desc' => {
-		message => q|Tot bovenaan|,
-		lastUpdated => 1205853269
-	},
-
-	'templateId' => {
-		message => q|De ID van het sjabloon dat gebruikt wordt om dit asset weer te geven.|,
-		lastUpdated => 1170689067
-	},
-
-	'position1_loop' => {
-		message => q|Elke positie in het sjabloon heeft een kring, die de set met asstes bevat die daarbinnen getoond moeten worden. Assets die niet specifiek een plaats toegekend hebben gekregen worden in positie 1 gezet.|,
-		lastUpdated => 1149257560
-	},
-
-	'asset order asc' => {
-		message => q|Tot onderaan|,
-		lastUpdated => 1205853260
-	},
-
-	'847' => {
-		message => q|Ga terug naar de huidige pagina.|,
-		lastUpdated => 1148916623
-	},
-
-	'assets to hide' => {
-		message => q|Assets die verborgen moeten worden|,
-		lastUpdated => 1148916654
-	},
-
-	'isUncommitted' => {
-		message => q|Een functie die aangeeft of dit Asset wel of niet is ingezonden.|,
-		lastUpdated => 1213628755
-	},
-
-	'layout template body' => {
-		message => q|De volgende variabelen zijn beschikbaar voor de sjablonen voor pagina layout:|,
-		lastUpdated => 1207901221
-	},
-
-	'layout asset template variables body' => {
-		message => q|Elk asset maakt een set variabelen beschikbaar voor de meeste van zijn sjablonen. Deze variabelen worden intern gebruikt waardoor ze niet allemaal even bruikbaar zijn voor het gebruik in sjablonen. |,
-		lastUpdated => 1170688987
-	},
-
-	'showAdmin' => {
-		message => q|Een functie die laat zien of de huidige gebruiker de Adminmode heeft aangezet en deze asset kan bewerken.|,
-		lastUpdated => 1149257430
-	},
-
-	'layout template title' => {
-		message => q|Pagina indeling sjabloon|,
-		lastUpdated => 1175508682
-	},
-
-	'template description' => {
-		message => q|Kies een sjabloon uit de lijst voor het tonen van de inhoud van de Pagina layout asset en de de onderliggende elementen.|,
-		lastUpdated => 1150445573
-	},
-
-	'823' => {
-		message => q|Ga naar de nieuwe pagina.|,
-		lastUpdated => 1148916608
-	},
-
-};
+  '823' => {
+    'lastUpdated' => 1148916608,
+    'message' => 'Ga naar de nieuwe pagina.'
+  },
+  '847' => {
+    'lastUpdated' => 1148916623,
+    'message' => 'Ga terug naar de huidige pagina.'
+  },
+  'asset order asc' => {
+    'lastUpdated' => 1205853260,
+    'message' => 'Tot onderaan'
+  },
+  'asset order desc' => {
+    'lastUpdated' => 1205853269,
+    'message' => 'Tot bovenaan'
+  },
+  'asset order hoverHelp' => {
+    'lastUpdated' => 1213628542,
+    'message' => 'Kies of nieuwe en niet gepositioneerde assets aan de boven-of onderkant van de eerste inhoud positie van de pagina toegevoegd moeten worden.'
+  },
+  'asset order label' => {
+    'lastUpdated' => 1205853348,
+    'message' => 'Voeg nieuwe assets toe'
+  },
+  'assetName' => {
+    'lastUpdated' => 1148916634,
+    'message' => 'Pagina layout'
+  },
+  'assets to hide' => {
+    'lastUpdated' => 1228818370,
+    'message' => 'Te verbergen assets'
+  },
+  'assets to hide description' => {
+    'lastUpdated' => 1149258519,
+    'message' => 'Deze lijst bevat een aanvinkveld voor elk subasset van de pagina layout. Elke asset die niet in de pagina layout asset getoond moet worden, moet hier worden aangevinkt.'
+  },
+  'assetsToHide' => {
+    'lastUpdated' => 1170688133,
+    'message' => 'De asset-ID\'s van alle assets die niet afbeeld zouden moeten worden in dit asset. Deze ID\'s worden gescheiden door "\\n", waarmee een nieuwe regel wordt aangeduid in *nix (unix, linux, ...) systemen. '
+  },
+  'content' => {
+    'lastUpdated' => 1149258297,
+    'message' => 'De bewerkte inhoud van de asset.'
+  },
+  'contentPositions' => {
+    'lastUpdated' => 1170688337,
+    'message' => 'Een variabele (string) die de verschillende plaatsen bevat waarop assets weergegeven kunnen worden en welke assets op welke plaats moeten staan en in welke volgorde.'
+  },
+  'dragger.icon' => {
+    'lastUpdated' => 1207840091,
+    'message' => 'Een icoon waarmee de asset verplaatst kan worden door middel van het klikken en slepen met de muis. Deze variabele is leeg als admin niet getoond wordt. '
+  },
+  'dragger.init' => {
+    'lastUpdated' => 1208523993,
+    'message' => 'Voor het werken van de klik en sleep functies is HTML en Javascript nodig. Deze variabele is leeg als admin niet getoond wordt.'
+  },
+  'id' => {
+    'lastUpdated' => 1149258041,
+    'message' => 'De assets identificatie van de asset.'
+  },
+  'isUncommitted' => {
+    'lastUpdated' => 1213628755,
+    'message' => 'Een functie die aangeeft of dit Asset wel of niet is ingezonden.'
+  },
+  'layout asset template variables body' => {
+    'lastUpdated' => 1170688987,
+    'message' => 'Elk asset maakt een set variabelen beschikbaar voor de meeste van zijn sjablonen. Deze variabelen worden intern gebruikt waardoor ze niet allemaal even bruikbaar zijn voor het gebruik in sjablonen. '
+  },
+  'layout asset template variables title' => {
+    'lastUpdated' => 1170689046,
+    'message' => 'Pagina asset sjabloonvariabelen'
+  },
+  'layout template body' => {
+    'lastUpdated' => 1207901221,
+    'message' => 'De volgende variabelen zijn beschikbaar voor de sjablonen voor pagina layout:'
+  },
+  'layout template title' => {
+    'lastUpdated' => 1175508682,
+    'message' => 'Pagina indeling sjabloon'
+  },
+  'position1_loop' => {
+    'lastUpdated' => 1149257560,
+    'message' => 'Elke positie in het sjabloon heeft een kring, die de set met asstes bevat die daarbinnen getoond moeten worden. Assets die niet specifiek een plaats toegekend hebben gekregen worden in positie 1 gezet.'
+  },
+  'showAdmin' => {
+    'lastUpdated' => 1149257430,
+    'message' => 'Een functie die laat zien of de huidige gebruiker de Adminmode heeft aangezet en deze asset kan bewerken.'
+  },
+  'template description' => {
+    'lastUpdated' => 1150445573,
+    'message' => 'Kies een sjabloon uit de lijst voor het tonen van de inhoud van de Pagina layout asset en de de onderliggende elementen.'
+  },
+  'templateId' => {
+    'lastUpdated' => 1170689067,
+    'message' => 'De ID van het sjabloon dat gebruikt wordt om dit asset weer te geven.'
+  }
+}
+;
 
 1;
--- Dutch/Dutch/Asset_MatrixListing.pm
+++ Dutch/Dutch/Asset_MatrixListing.pm
@@ -9,9 +9,17 @@ our $I18N = {
     'lastUpdated' => 1227602529,
     'message' => 'Beste'
   },
+  'contact maintainer label' => {
+    'lastUpdated' => 1228818412,
+    'message' => 'Contact voor onderhoud'
+  },
   'count label' => {
     'lastUpdated' => 1227873718,
     'message' => 'Aantal'
+  },
+  'description description' => {
+    'lastUpdated' => 1228818455,
+    'message' => 'Geef een algemene beschrijving van het product. Details kunnen in de velden hieronder gespecificeerd worden.'
   },
   'description label' => {
     'lastUpdated' => 1227874474,
@@ -21,9 +29,37 @@ our $I18N = {
     'lastUpdated' => 1227873845,
     'message' => 'Bewerken/toevoegen lijst'
   },
+  'last updated label' => {
+    'lastUpdated' => 1228818466,
+    'message' => 'Laatst bewerkt'
+  },
+  'maintainer description' => {
+    'lastUpdated' => 1228818564,
+    'message' => 'Als dit leeeggelaten wordt, zal dit de naam zijn van de gebruiker die deze product lijst heeft gemaakt. Degene die deze lijst onderhoudt kan deze lijst bewerken, zelfs als ze niet in de groep zitten die lijsten kan toevoegen of bewerken.'
+  },
+  'maintainer label' => {
+    'lastUpdated' => 1228818626,
+    'message' => 'Onderhouder'
+  },
   'manufacturer label' => {
     'lastUpdated' => 1227873753,
     'message' => 'Fabrikant'
+  },
+  'manufacturerName description' => {
+    'lastUpdated' => 1228818659,
+    'message' => 'De naam van het bedrijf die het product fabriceerd of de service leveren in deze lijst.'
+  },
+  'manufacturerName label' => {
+    'lastUpdated' => 1228818667,
+    'message' => 'Bedrijfsnaam'
+  },
+  'manufacturerURL description' => {
+    'lastUpdated' => 1228818690,
+    'message' => 'Als het bedrijf een website hheft, vul dan hier het URL in.'
+  },
+  'manufacturerURL label' => {
+    'lastUpdated' => 1228818706,
+    'message' => 'URL Bedrijf'
   },
   'mean label' => {
     'lastUpdated' => 1227873743,
@@ -37,9 +73,29 @@ our $I18N = {
     'lastUpdated' => 1227874485,
     'message' => 'Bericht verzonden'
   },
+  'productURL description' => {
+    'lastUpdated' => 1228818738,
+    'message' => 'Als het product een eigen website heeft, vul dan hier het gehele URL in.'
+  },
   'productURL label' => {
     'lastUpdated' => 1227875150,
     'message' => 'Produkt URL'
+  },
+  'respectable label' => {
+    'lastUpdated' => 1228820018,
+    'message' => 'Respectabel'
+  },
+  'screenshots description' => {
+    'lastUpdated' => 1228820317,
+    'message' => 'Voeg een plaatje van het product toe of een screenshot als het een software pakket is.'
+  },
+  'screenshots label' => {
+    'lastUpdated' => 1228820399,
+    'message' => 'Screenshots/Foto\'s'
+  },
+  'template description' => {
+    'lastUpdated' => 1228820427,
+    'message' => 'Selecteer een sjabloon voor het tonen van het standaard scherm van de Matrix.'
   },
   'template label' => {
     'lastUpdated' => 1227873735,
