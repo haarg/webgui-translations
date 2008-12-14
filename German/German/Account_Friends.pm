8611409e5d616a221640f9bb46836582b4f19bbd
Translation Server <translation@plainblack.com>
2008-12-14 19:47:15 +0000

    Update from translation server

--- German/German/Account_Friends.pm
+++ German/German/Account_Friends.pm
@@ -1,77 +1,157 @@
 package WebGUI::i18n::German::Account_Friends;
 use utf8;
 our $I18N = {
+  'add to friends confirmation' => {
+    'lastUpdated' => 1229276080,
+    'message' => 'Eine E-Mail wurde an %s versendet mit der Bitte, Sie in sein/ihr Freunde-Netzwerk aufzunehmen.'
+  },
   'add to network label' => {
     'lastUpdated' => 1228754447,
     'message' => 'Zum Netzwerk hinzuf&uuml;gen'
   },
   'adding user message' => {
-    'lastUpdated' => 1228754431,
-    'message' => 'Sie f&uuml;gen %s zu Ihrem Netzwerk hinzu'
+    'lastUpdated' => 1229283409,
+    'message' => "Sie f\x{fc}gen %s zu Ihrem Netzwerk hinzu."
   },
   'back label' => {
-    'lastUpdated' => 1228691832,
-    'message' => 'Zur&uuml;ck zur Site'
+    'lastUpdated' => 1229283431,
+    'message' => 'Zurück zur Seite'
   },
   'back to profile label' => {
-    'lastUpdated' => 1228754381,
-    'message' => 'Zur&uuml;ck zum Profil'
+    'lastUpdated' => 1229283444,
+    'message' => "Zur\x{fc}ck zum Profil"
   },
   'back to user profile' => {
-    'lastUpdated' => 1228754462,
-    'message' => 'Zur&uuml;ck zum Benutzerprofil'
+    'lastUpdated' => 1229283461,
+    'message' => 'Zurück zum Benutzerprofil'
   },
   'cancel button label' => {
     'lastUpdated' => 1228754336,
     'message' => 'Abbrechen'
   },
   'default friend comments' => {
-    'lastUpdated' => 1228754523,
-    'message' => '<p>%s, ich w&uuml;rde Sie gerne in mein Freunde-Netzwerk aufnehmen. </p><p>Vielen Dank, &amp;s</p>'
+    'lastUpdated' => 1229283090,
+    'message' => '%s, ich würde Sie gerne in mein Freunde-Netzwerk aufnehmen. Vielen Dank, %s'
   },
   'does not want to be a friend' => {
-    'lastUpdated' => 1228754093,
-    'message' => 'Der Benutzer m&ouml;chte nicht zu den Freunden hinzugef&uuml;gt werden.'
+    'lastUpdated' => 1229283366,
+    'message' => 'Der Benutzer möchte nicht zu Ihren Freunden hinzugefügt werden.'
   },
   'edit my friends' => {
-    'lastUpdated' => 1228752377,
-    'message' => 'Meine Feeunde bearbeiten'
+    'lastUpdated' => 1229283143,
+    'message' => 'Meine Freunde bearbeiten'
   },
   'error label' => {
     'lastUpdated' => 1228691840,
     'message' => 'Fehler'
   },
   'error user is already friend' => {
-    'lastUpdated' => 1228754119,
-    'message' => 'Dieser Benutzer ist bereits Mitgleid in Ihrem Freunde-Netzwerk'
+    'lastUpdated' => 1229283833,
+    'message' => 'Dieser Benutzer ist bereits Mitglied in Ihrem Freunde-Netzwerk.'
   },
   'error user is already invited' => {
-    'lastUpdated' => 1228754164,
-    'message' => 'Sie haben diesem Beenutzer bereits eine Einladung zum Beitritt zu Ihrem Freunde-Netzwerk gesandt.'
+    'lastUpdated' => 1229283878,
+    'message' => 'Sie haben diesem Benutzer bereits eine Einladung zum Beitritt zu Ihrem Freunde-Netzwerk gesandt.'
   },
   'friend removed' => {
-    'lastUpdated' => 1228754210,
-    'message' => '%s wurde von Ihrer Freunde-Liste entfernt'
+    'lastUpdated' => 1229277274,
+    'message' => '%s wurde von Ihrer Freunde-Liste entfernt.'
   },
   'friends as others label' => {
     'lastUpdated' => 1228754366,
     'message' => 'Meine Freunde betrachten, wie sie von anderen gesehen werden'
   },
+  'friends confirm template hoverHelp' => {
+    'lastUpdated' => 1229283707,
+    'message' => 'Wählen Sie eine Schablone für die Bestätigung, dass eine Einladung in das Freunde-Netzwerk versandt wurde!'
+  },
+  'friends confirm template label' => {
+    'lastUpdated' => 1229281323,
+    'message' => 'Versandbestätigungs-Schablone'
+  },
+  'friends edit template hoverHelp' => {
+    'lastUpdated' => 1229283719,
+    'message' => "W\x{e4}hlen Sie die Schablone zur Bearbeitung Ihrer Freunde!"
+  },
+  'friends edit template label' => {
+    'lastUpdated' => 1229280969,
+    'message' => 'Bearbeitungs-Schablone'
+  },
+  'friends error template hoverHelp' => {
+    'lastUpdated' => 1229282802,
+    'message' => 'Wählen Sie eine Schablone zur Anzeige von Fehlermeldungen!'
+  },
+  'friends error template label' => {
+    'lastUpdated' => 1229280939,
+    'message' => 'Fehlermeldungs-Schablone'
+  },
+  'friends is off' => {
+    'lastUpdated' => 1229277515,
+    'message' => "Die Freunde-Funktion ist nicht aktiviert. Um diese Seite sehen zu k\x{f6}nnen, m\x{fc}ssen Sie in ihrem Profil die Freunde-Funktion aktivieren."
+  },
+  'friends layout template hoverHelp' => {
+    'lastUpdated' => 1229283738,
+    'message' => 'Wählen Sie eine Schablone zur Anzeige der Konten-Plugins, welche vom gegenwärtigen Benutzer bearbeitet werden können sowie zur Anzeige des Inhalts des gegenwärtig gewählten Plugins!'
+  },
+  'friends layout template label' => {
+    'lastUpdated' => 1229283908,
+    'message' => 'Bearbeitungslayout-Schablone'
+  },
+  'friends remove confirm template hoverHelp' => {
+    'lastUpdated' => 1229283323,
+    'message' => "W\x{e4}hlen Sie eine Schablone zur Anzeige von Best\x{e4}tigungsmeldungen f\x{fc}r die Entfernung eines Freundes."
+  },
+  'friends remove confirm template label' => {
+    'lastUpdated' => 1229280839,
+    'message' => 'Entfernungsbestätigungs-Schablone'
+  },
+  'friends send request template hoverHelp' => {
+    'lastUpdated' => 1229282684,
+    'message' => 'Wählen Sie die Schablone für den Versand von Freundschaftsanfragen!'
+  },
+  'friends send request template label' => {
+    'lastUpdated' => 1229282579,
+    'message' => 'Freundschaftsanfragen-Schablone'
+  },
+  'friends style template hoverHelp' => {
+    'lastUpdated' => 1229283770,
+    'message' => 'Wählen Sie eine Stil-Schablone für die Darstellung der umgebenden Seite!'
+  },
+  'friends style template label' => {
+    'lastUpdated' => 1229280811,
+    'message' => 'Stil-Schablone'
+  },
+  'friends view template hoverHelp' => {
+    'lastUpdated' => 1229283759,
+    'message' => "W\x{e4}hlen Sie die prim\x{e4}re Schablone zur Darstellung von Freunden!"
+  },
+  'friends view template label' => {
+    'lastUpdated' => 1229281388,
+    'message' => 'Darstellungs-Schablone'
+  },
   'member since' => {
     'lastUpdated' => 1228754178,
     'message' => 'Mitglied seit'
   },
+  'message sent label' => {
+    'lastUpdated' => 1229283609,
+    'message' => 'Freundschaftsanfrage versandt'
+  },
   'no access' => {
-    'lastUpdated' => 1228691877,
-    'message' => 'Sie haben keine Berechtigung, die Freunde-Liste dieses Benutzers zu betrachten.'
+    'lastUpdated' => 1229283955,
+    'message' => 'Sie haben keine Berechtigung, die Freundeliste dieses Benutzers anzuschauen.'
   },
   'no friend selected' => {
-    'lastUpdated' => 1228754257,
-    'message' => 'Sieh haben keinen Freund zum L&ouml;schen von Ihrere Freunde-Liste ausgew&auml;hlt.'
+    'lastUpdated' => 1229283995,
+    'message' => "Sieh haben keinen Freund zum L\x{f6}schen von Ihrer Freundeliste ausgew\x{e4}hlt."
   },
   'not a friend error' => {
-    'lastUpdated' => 1228754306,
-    'message' => 'Der zum L&ouml;schen ausgew&auml;hlte Benutzer ist nicht Mitglied in Ihrem Freunde-Netzwerk'
+    'lastUpdated' => 1229283692,
+    'message' => "Der zum L\x{f6}schen ausgew\x{e4}hlte Benutzer ist nicht in der Liste Ihrer Freunde."
+  },
+  'remove confirm label' => {
+    'lastUpdated' => 1229283575,
+    'message' => 'Bestätigung Entfernung eines Freundes'
   },
   'remove confirm message' => {
     'lastUpdated' => 1228754634,
@@ -82,12 +162,12 @@ our $I18N = {
     'message' => 'Nein, ich habe mich geirrt.'
   },
   'remove confirm yes' => {
-    'lastUpdated' => 1228754559,
-    'message' => 'Ja, diesen Benutzer l&ouml;schen.'
+    'lastUpdated' => 1229283478,
+    'message' => "Ja, diesen Benutzer l\x{f6}schen."
   },
   'remove from friends label' => {
-    'lastUpdated' => 1228754575,
-    'message' => 'Aus dem Freunde-Netzwerk l&ouml;schen'
+    'lastUpdated' => 1229283495,
+    'message' => 'Aus dem Freunde-Netzwerk löschen'
   },
   'send invite button label' => {
     'lastUpdated' => 1228754271,
@@ -98,8 +178,8 @@ our $I18N = {
     'message' => 'Private Nachricht senden'
   },
   'sending to message' => {
-    'lastUpdated' => 1228691910,
-    'message' => 'Die folgende Nachricht wird mit Ihrer Einladung mitgesandt:'
+    'lastUpdated' => 1229283547,
+    'message' => 'Die folgende Nachricht wird mit Ihrer Einladung mitversandt:'
   },
   'title' => {
     'lastUpdated' => 1228174951,
