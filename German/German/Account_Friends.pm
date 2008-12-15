e6755dee035462265f957e41f65940e18a84f4f6
Translation Server <translation@plainblack.com>
2008-12-15 09:19:05 +0000

    Update from translation server

--- German/German/Account_Friends.pm
+++ German/German/Account_Friends.pm
@@ -11,19 +11,19 @@ our $I18N = {
   },
   'adding user message' => {
     'lastUpdated' => 1229283409,
-    'message' => "Sie f\x{fc}gen %s zu Ihrem Netzwerk hinzu."
+    'message' => 'Sie fügen %s zu Ihrem Netzwerk hinzu.'
   },
   'back label' => {
     'lastUpdated' => 1229283431,
-    'message' => 'Zurück zur Seite'
+    'message' => "Zur\x{fc}ck zur Seite"
   },
   'back to profile label' => {
     'lastUpdated' => 1229283444,
-    'message' => "Zur\x{fc}ck zum Profil"
+    'message' => 'Zurück zum Profil'
   },
   'back to user profile' => {
     'lastUpdated' => 1229283461,
-    'message' => 'Zurück zum Benutzerprofil'
+    'message' => "Zur\x{fc}ck zum Benutzerprofil"
   },
   'cancel button label' => {
     'lastUpdated' => 1228754336,
@@ -31,11 +31,11 @@ our $I18N = {
   },
   'default friend comments' => {
     'lastUpdated' => 1229283090,
-    'message' => '%s, ich würde Sie gerne in mein Freunde-Netzwerk aufnehmen. Vielen Dank, %s'
+    'message' => "%s, ich w\x{fc}rde Sie gerne in mein Freunde-Netzwerk aufnehmen. Vielen Dank, %s"
   },
   'does not want to be a friend' => {
     'lastUpdated' => 1229283366,
-    'message' => 'Der Benutzer möchte nicht zu Ihren Freunden hinzugefügt werden.'
+    'message' => "Der Benutzer m\x{f6}chte nicht zu Ihren Freunden hinzugef\x{fc}gt werden."
   },
   'edit my friends' => {
     'lastUpdated' => 1229283143,
@@ -63,15 +63,15 @@ our $I18N = {
   },
   'friends confirm template hoverHelp' => {
     'lastUpdated' => 1229283707,
-    'message' => 'Wählen Sie eine Schablone für die Bestätigung, dass eine Einladung in das Freunde-Netzwerk versandt wurde!'
+    'message' => "W\x{e4}hlen Sie eine Schablone f\x{fc}r die Best\x{e4}tigung, dass eine Einladung in das Freunde-Netzwerk versandt wurde!"
   },
   'friends confirm template label' => {
     'lastUpdated' => 1229281323,
-    'message' => 'Versandbestätigungs-Schablone'
+    'message' => "Versandbest\x{e4}tigungs-Schablone"
   },
   'friends edit template hoverHelp' => {
     'lastUpdated' => 1229283719,
-    'message' => "W\x{e4}hlen Sie die Schablone zur Bearbeitung Ihrer Freunde!"
+    'message' => 'Wählen Sie die Schablone zur Bearbeitung Ihrer Freunde!'
   },
   'friends edit template label' => {
     'lastUpdated' => 1229280969,
@@ -79,7 +79,7 @@ our $I18N = {
   },
   'friends error template hoverHelp' => {
     'lastUpdated' => 1229282802,
-    'message' => 'Wählen Sie eine Schablone zur Anzeige von Fehlermeldungen!'
+    'message' => "W\x{e4}hlen Sie eine Schablone zur Anzeige von Fehlermeldungen!"
   },
   'friends error template label' => {
     'lastUpdated' => 1229280939,
@@ -87,11 +87,11 @@ our $I18N = {
   },
   'friends is off' => {
     'lastUpdated' => 1229277515,
-    'message' => "Die Freunde-Funktion ist nicht aktiviert. Um diese Seite sehen zu k\x{f6}nnen, m\x{fc}ssen Sie in ihrem Profil die Freunde-Funktion aktivieren."
+    'message' => 'Die Freunde-Funktion ist nicht aktiviert. Um diese Seite sehen zu können, müssen Sie in ihrem Profil die Freunde-Funktion aktivieren.'
   },
   'friends layout template hoverHelp' => {
     'lastUpdated' => 1229283738,
-    'message' => 'Wählen Sie eine Schablone zur Anzeige der Konten-Plugins, welche vom gegenwärtigen Benutzer bearbeitet werden können sowie zur Anzeige des Inhalts des gegenwärtig gewählten Plugins!'
+    'message' => "W\x{e4}hlen Sie eine Schablone zur Anzeige der Konten-Plugins, welche vom gegenw\x{e4}rtigen Benutzer bearbeitet werden k\x{f6}nnen sowie zur Anzeige des Inhalts des gegenw\x{e4}rtig gew\x{e4}hlten Plugins!"
   },
   'friends layout template label' => {
     'lastUpdated' => 1229283908,
@@ -99,15 +99,15 @@ our $I18N = {
   },
   'friends remove confirm template hoverHelp' => {
     'lastUpdated' => 1229283323,
-    'message' => "W\x{e4}hlen Sie eine Schablone zur Anzeige von Best\x{e4}tigungsmeldungen f\x{fc}r die Entfernung eines Freundes."
+    'message' => 'Wählen Sie eine Schablone zur Anzeige von Bestätigungsmeldungen für die Entfernung eines Freundes.'
   },
   'friends remove confirm template label' => {
     'lastUpdated' => 1229280839,
-    'message' => 'Entfernungsbestätigungs-Schablone'
+    'message' => "Entfernungsbest\x{e4}tigungs-Schablone"
   },
   'friends send request template hoverHelp' => {
     'lastUpdated' => 1229282684,
-    'message' => 'Wählen Sie die Schablone für den Versand von Freundschaftsanfragen!'
+    'message' => "W\x{e4}hlen Sie die Schablone f\x{fc}r den Versand von Freundschaftsanfragen!"
   },
   'friends send request template label' => {
     'lastUpdated' => 1229282579,
@@ -115,7 +115,7 @@ our $I18N = {
   },
   'friends style template hoverHelp' => {
     'lastUpdated' => 1229283770,
-    'message' => 'Wählen Sie eine Stil-Schablone für die Darstellung der umgebenden Seite!'
+    'message' => "W\x{e4}hlen Sie eine Stil-Schablone f\x{fc}r die Darstellung der umgebenden Seite!"
   },
   'friends style template label' => {
     'lastUpdated' => 1229280811,
@@ -123,7 +123,7 @@ our $I18N = {
   },
   'friends view template hoverHelp' => {
     'lastUpdated' => 1229283759,
-    'message' => "W\x{e4}hlen Sie die prim\x{e4}re Schablone zur Darstellung von Freunden!"
+    'message' => 'Wählen Sie die primäre Schablone zur Darstellung von Freunden!'
   },
   'friends view template label' => {
     'lastUpdated' => 1229281388,
@@ -142,16 +142,16 @@ our $I18N = {
     'message' => 'Sie haben keine Berechtigung, die Freundeliste dieses Benutzers anzuschauen.'
   },
   'no friend selected' => {
-    'lastUpdated' => 1229283995,
-    'message' => "Sieh haben keinen Freund zum L\x{f6}schen von Ihrer Freundeliste ausgew\x{e4}hlt."
+    'lastUpdated' => 1229332738,
+    'message' => 'Sie haben keinen Freund zum L&ouml;schen von Ihrer Freundeliste ausgew&auml;hlt.'
   },
   'not a friend error' => {
     'lastUpdated' => 1229283692,
-    'message' => "Der zum L\x{f6}schen ausgew\x{e4}hlte Benutzer ist nicht in der Liste Ihrer Freunde."
+    'message' => 'Der zum Löschen ausgewählte Benutzer ist nicht in der Liste Ihrer Freunde.'
   },
   'remove confirm label' => {
     'lastUpdated' => 1229283575,
-    'message' => 'Bestätigung Entfernung eines Freundes'
+    'message' => "Best\x{e4}tigung Entfernung eines Freundes"
   },
   'remove confirm message' => {
     'lastUpdated' => 1228754634,
@@ -163,11 +163,11 @@ our $I18N = {
   },
   'remove confirm yes' => {
     'lastUpdated' => 1229283478,
-    'message' => "Ja, diesen Benutzer l\x{f6}schen."
+    'message' => 'Ja, diesen Benutzer löschen.'
   },
   'remove from friends label' => {
     'lastUpdated' => 1229283495,
-    'message' => 'Aus dem Freunde-Netzwerk löschen'
+    'message' => "Aus dem Freunde-Netzwerk l\x{f6}schen"
   },
   'send invite button label' => {
     'lastUpdated' => 1228754271,
