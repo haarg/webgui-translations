7566c1c23c477887861236688eea71f6fdd98565
Translation Server <translation@plainblack.com>
2008-12-23 00:38:26 +0000

    Update from translation server

--- German/German/Account.pm
+++ German/German/Account.pm
@@ -2,8 +2,8 @@ package WebGUI::i18n::German::Account;
 use utf8;
 our $I18N = {
   'Error: Cannot instantiate template' => {
-    'lastUpdated' => 1229992069,
-    'message' => 'Fehler: Kann keine Instanz der Vorlage %s f&uuml;r doe Klasse %s bilden'
+    'lastUpdated' => 1229992699,
+    'message' => 'Fehler: Kann keine Instanz der Vorlage %s f&uuml;r die Klasse %s bilden'
   }
 }
 ;
