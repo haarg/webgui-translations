fa3dd628cdad046b42ba512e48ae0c107c4834ed
Translation Server <translation@plainblack.com>
2008-12-23 00:36:14 +0000

    Update from translation server

--- German/German/Asset_StockData.pm
+++ German/German/Asset_StockData.pm
@@ -1,393 +1,319 @@
 package WebGUI::i18n::German::Asset_StockData;
 use utf8;
-
-
 our $I18N = {
-	'help_stock_list_template' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'failover_label' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.avg_vol' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'finish_button_label' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'display_template_label_description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.div' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.div_date' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'default_stock_label' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'edit_title' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'displayTemplateId' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.net.icon' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'add_button_label' => {
-		message => q|Hinzuf&uuml;gen &gt;&gt;<br />|,
-		lastUpdated => 1157856495
-	},
-
-	'stock_source' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stock_source_description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.date' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'extrasFolder' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.symbol' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.nav' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'source' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.last' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.net.isUp' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'display_template_label' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'template_label_description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.errormsg' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.ex_div' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.exchange' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.cap' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'delete_confirm' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'lastUpdate.us' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.eps' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.year_range' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'lastUpdate.intl' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'defaultStocks' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'isVisitor' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'lastUpdate.default' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.day_range' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.year_low' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'help_stock_list_display_template' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.pe' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'symbol_edit_label' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.name' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.high' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'default_stock_label_description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.p_change' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.yield' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.volume' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'assetName' => {
-		message => q|Bestandsdaten|,
-		lastUpdated => 1170406657
-	},
-
-	'editUrl' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.volume.millions' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.method' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.div_yield' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'symbol_label' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.time' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'symbol_exists' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.net' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'failover' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.low' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'symbol_header' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.year_high' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.ask' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'no_symbol' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stock data asset template variables title' => {
-		message => q|Bestandsdaten Asset Template Variablen<br />|,
-		lastUpdated => 1170406674
-	},
-
-	'templateId' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'default_stock_description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.net.noChange' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.success' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.bid' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stock.display.url' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'symbol_label_description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'template_label' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.open' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'no_symbol_error' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'failover_description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.net.isDown' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.loop' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'failover_label_description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stocks.close' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-};
+  'Last Update' => {
+    'lastUpdated' => 1229992546,
+    'message' => 'Letzte Aktualisierung'
+  },
+  'add_button_label' => {
+    'lastUpdated' => 1157856495,
+    'message' => 'Hinzuf&uuml;gen &gt;&gt;<br />'
+  },
+  'assetName' => {
+    'lastUpdated' => 1170406657,
+    'message' => 'Bestandsdaten'
+  },
+  'defaultStocks' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'default_stock_description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'default_stock_label' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'default_stock_label_description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'delete_confirm' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'displayTemplateId' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'display_template_label' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'display_template_label_description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'editUrl' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'edit_title' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'extrasFolder' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'failover' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'failover_description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'failover_label' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'failover_label_description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'finish_button_label' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'help_stock_list_display_template' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'help_stock_list_template' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'isVisitor' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'lastUpdate.default' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'lastUpdate.intl' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'lastUpdate.us' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'no_symbol' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'no_symbol_error' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'source' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stock data asset template variables title' => {
+    'lastUpdated' => 1170406674,
+    'message' => 'Bestandsdaten Asset Template Variablen<br />'
+  },
+  'stock.display.url' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stock_source' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stock_source_description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.ask' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.avg_vol' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.bid' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.cap' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.close' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.date' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.day_range' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.div' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.div_date' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.div_yield' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.eps' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.errormsg' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.ex_div' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.exchange' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.high' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.last' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.loop' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.low' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.method' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.name' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.nav' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.net' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.net.icon' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.net.isDown' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.net.isUp' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.net.noChange' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.open' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.p_change' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.pe' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.success' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.symbol' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.time' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.volume' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.volume.millions' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.year_high' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.year_low' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.year_range' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'stocks.yield' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'symbol_edit_label' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'symbol_exists' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'symbol_header' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'symbol_label' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'symbol_label_description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'templateId' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'template_label' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'template_label_description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  }
+}
+;
 
 1;
