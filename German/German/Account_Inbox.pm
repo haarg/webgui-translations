c7d92972e68dd7884c702aa17716ae427c74eb3b
Translation Server <translation@plainblack.com>
2009-01-06 05:05:52 +0000

    Update from translation server

--- German/German/Account_Inbox.pm
+++ German/German/Account_Inbox.pm
@@ -1,5 +1,151 @@
 package WebGUI::i18n::German::Account_Inbox;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'accept button label' => {
+    'lastUpdated' => 1231216784,
+    'message' => 'Akzeptieren'
+  },
+  'already a member' => {
+    'lastUpdated' => 1231216905,
+    'message' => 'Die von Ihnen eingetragene Adresse ist hier bereits für %s registriert'
+  },
+  'back label' => {
+    'lastUpdated' => 1231216923,
+    'message' => "Zur\x{fc}ck zum Posteingang"
+  },
+  'blocked error' => {
+    'lastUpdated' => 1231216958,
+    'message' => 'Dieser Benutzer wünscht keine private Nachrichten zu empfangen'
+  },
+  'cancel label' => {
+    'lastUpdated' => 1231216968,
+    'message' => 'Abbrechen'
+  },
+  'compose message label' => {
+    'lastUpdated' => 1231216985,
+    'message' => 'Neue Nachricht zusammenstellen'
+  },
+  'date label' => {
+    'lastUpdated' => 1231216994,
+    'message' => 'Datum'
+  },
+  'delete label' => {
+    'lastUpdated' => 1231217003,
+    'message' => 'Löschen'
+  },
+  'deny button label' => {
+    'lastUpdated' => 1231217012,
+    'message' => 'Ablehnen'
+  },
+  'error label' => {
+    'lastUpdated' => 1231217022,
+    'message' => 'Fehler'
+  },
+  'from label' => {
+    'lastUpdated' => 1231217029,
+    'message' => 'Von'
+  },
+  'inbox error message template hoverHelp' => {
+    'lastUpdated' => 1231217087,
+    'message' => 'Wählen Sie die Vorlage zur Anzeige von Fehlern beim Posteingang'
+  },
+  'inbox error message template label' => {
+    'lastUpdated' => 1231217129,
+    'message' => 'Vorlage Posteingang Fehler'
+  },
+  'inbox layout template hoverHelp' => {
+    'lastUpdated' => 1231217376,
+    'message' => 'Wählen Sie ein Layout zur Anzeige der aktuellen und sonstigen Konten-Erweiterungen, die vom aktuellen Benutzer geändert werden können'
+  },
+  'inbox layout template label' => {
+    'lastUpdated' => 1231217403,
+    'message' => "\x{c4}ndern Layout Vorlage"
+  },
+  'no friends hover' => {
+    'lastUpdated' => 1231218342,
+    'message' => "Sie m\x{fc}ssen aktivierte Freunde im System haben um neue private Nachrichten ohne vorherige Profilbestimmung verschicken zu k\x{f6}nnen.  "
+  },
+  'no invitations' => {
+    'lastUpdated' => 1231217997,
+    'message' => 'Sie haben keine ausstehende Einladungen'
+  },
+  'no message error' => {
+    'lastUpdated' => 1231217941,
+    'message' => 'Sie haben keine Nachricht eingetragen'
+  },
+  'no reply error' => {
+    'lastUpdated' => 1231217906,
+    'message' => 'Sie dürfen diese Nachricht nicht beantworten'
+  },
+  'no self error' => {
+    'lastUpdated' => 1231217870,
+    'message' => 'Sie dürfen keine Nachricht an Ihre eigene Emailadresse senden'
+  },
+  'no subject error' => {
+    'lastUpdated' => 1231217826,
+    'message' => "Sie haben keinen Betreff f\x{fc}r diese Nachricht angegeben"
+  },
+  'no user error' => {
+    'lastUpdated' => 1231217802,
+    'message' => 'Sie haben keinen Empfänger für diese Nachricht angegeben'
+  },
+  'previous label' => {
+    'lastUpdated' => 1231217775,
+    'message' => "Zur\x{fc}ck"
+  },
+  'received label' => {
+    'lastUpdated' => 1231217754,
+    'message' => 'Empfangen'
+  },
+  'recipients label' => {
+    'lastUpdated' => 1231217744,
+    'message' => "Empf\x{e4}nger hinzuf\x{fc}gen"
+  },
+  'remove text' => {
+    'lastUpdated' => 1231217728,
+    'message' => 'Löschen'
+  },
+  'reply label' => {
+    'lastUpdated' => 1231217717,
+    'message' => 'Antworten'
+  },
+  'reply message label' => {
+    'lastUpdated' => 1231217690,
+    'message' => 'Antwort zur Nachricht'
+  },
+  'send label' => {
+    'lastUpdated' => 1231217662,
+    'message' => 'Senden'
+  },
+  'status label' => {
+    'lastUpdated' => 1231217653,
+    'message' => 'Status'
+  },
+  'subject label' => {
+    'lastUpdated' => 1231217645,
+    'message' => 'Betreff'
+  },
+  'system message error' => {
+    'lastUpdated' => 1231217632,
+    'message' => 'Diese Nachricht wurde vom System verschickt. Sie können keine Antwort senden.'
+  },
+  'title' => {
+    'lastUpdated' => 1231217562,
+    'message' => "Tab-Label f\x{fc}r Posteingang Konto-Erg\x{e4}nzung"
+  },
+  'to label' => {
+    'lastUpdated' => 1231217504,
+    'message' => 'An'
+  },
+  'update label' => {
+    'lastUpdated' => 1231217494,
+    'message' => 'Aktualisieren'
+  },
+  'view my profile' => {
+    'lastUpdated' => 1231217482,
+    'message' => 'Mein Profil ansehen, sowie andere es sehen können.'
+  }
+}
+;
 
 1;
