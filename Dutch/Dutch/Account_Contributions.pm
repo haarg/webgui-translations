1c2988862820fb0e821f5f3406be8e45e6953af1
Translation Server <translation@plainblack.com>
2009-01-11 22:15:55 +0000

    Update from translation server

--- Dutch/Dutch/Account_Contributions.pm
+++ Dutch/Dutch/Account_Contributions.pm
@@ -1,6 +1,10 @@
 package WebGUI::i18n::Dutch::Account_Contributions;
 use utf8;
 our $I18N = {
+  'account contributions layout template' => {
+    'lastUpdated' => 1231712146,
+    'message' => 'Account bijdrage layout sjabloon'
+  },
   'contrib layout template hoverHelp' => {
     'lastUpdated' => 1227608010,
     'message' => 'Kies een layout sjabloon waarmee de inhoud van de verschillende methoden in de bijlage tab getoond wordt.'
@@ -29,13 +33,37 @@ our $I18N = {
     'lastUpdated' => 1227607855,
     'message' => 'Aantal bijdragen'
   },
+  'contributions_loop' => {
+    'lastUpdated' => 1231712129,
+    'message' => 'Een functie met informatie over alle bijdragen van deze gebruiker.'
+  },
+  'contributions_total' => {
+    'lastUpdated' => 1231712104,
+    'message' => 'Het totaal aantal bijdragen van de gebruiker.'
+  },
+  'contributions_variables' => {
+    'lastUpdated' => 1231712088,
+    'message' => 'Elke asset variabele dat de asset heeft in deze loop. De gewone, vaak gebruikte zullen de title, url, menutitle, dateCreated, synopsis, lastModified en dergelijke zijn.'
+  },
   'date label' => {
     'lastUpdated' => 1227607834,
     'message' => 'Datum'
   },
+  'dateStamp_url' => {
+    'lastUpdated' => 1231711948,
+    'message' => 'Het URL om bijdragen te sorteren op datum. Let erop dat dit URL ook de richting van sorteren om kan draaien van oplopend naar aflopend en andersom.'
+  },
+  'has_contributions' => {
+    'lastUpdated' => 1231711989,
+    'message' => 'Een functie die waar is als de gebruiker bijdragen heeft gemaakt voor de site.'
+  },
   'no contributions' => {
     'lastUpdated' => 1227607826,
     'message' => 'Geen bijdragen weer te geven'
+  },
+  'rpp_url' => {
+    'lastUpdated' => 1231711936,
+    'message' => 'Het URL om het aantal resultaten per pagina aan te passen.'
   },
   'title' => {
     'lastUpdated' => 1227607806,
@@ -45,9 +73,25 @@ our $I18N = {
     'lastUpdated' => 1227607841,
     'message' => 'Titel'
   },
+  'title_url' => {
+    'lastUpdated' => 1231711886,
+    'message' => 'Het URL om bijdragen te sorteren op titel. Let erop dat dit URL ook de richting van sorteren om kan draaien van oplopend naar aflopend en andersom.'
+  },
   'type label' => {
     'lastUpdated' => 1227607847,
     'message' => 'Type'
+  },
+  'type_url' => {
+    'lastUpdated' => 1231711863,
+    'message' => 'URL om bijdragen te sorteren op type. Let erop dat dit URL ook de richting van sorteren om kan draaien van oplopend naar aflopend en andersom.'
+  },
+  'user_full_name' => {
+    'lastUpdated' => 1231711717,
+    'message' => 'De naam van de gebruiker van wie deze bijdragen bekeken worden.'
+  },
+  'user_member_since' => {
+    'lastUpdated' => 1231711778,
+    'message' => 'De datum, als epoch, waarop de gebruiker, wiens bijdragen bekeken worden, een account heeft aangemaakt bij deze site.'
   }
 }
 ;
