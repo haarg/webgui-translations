ed9ab7149f11face5c8bbbbd68caf2483f5e1349
Translation Server <translation@plainblack.com>
2009-01-12 00:41:27 +0000

    Update from translation server

---
+++ Danish/Danish/Account_Friends.pm
@@ -0,0 +1,119 @@
+package WebGUI::i18n::Danish::Account_Friends;
+use utf8;
+our $I18N = {
+  'add to friends confirmation' => {
+    'lastUpdated' => 1231719591,
+    'message' => '<p>Der er blevet sendt en email til %s med din foresp&oslash;rgsel om at blive tilf&oslash;jet til din vens netv&aelig;rk.</p>'
+  },
+  'add to network label' => {
+    'lastUpdated' => 1231719086,
+    'message' => 'Tilf&oslash;j til netv&aelig;rk'
+  },
+  'adding user message' => {
+    'lastUpdated' => 1231719135,
+    'message' => 'Du er ved at tilf&oslash;je %s til dit netv&aelig;rk'
+  },
+  'back label' => {
+    'lastUpdated' => 1231719197,
+    'message' => 'tilbage til siden'
+  },
+  'back to profile label' => {
+    'lastUpdated' => 1231719214,
+    'message' => 'tilbage til profilen'
+  },
+  'back to user profile' => {
+    'lastUpdated' => 1231719244,
+    'message' => 'tilbage til brugerprofilen'
+  },
+  'cancel button label' => {
+    'lastUpdated' => 1231719258,
+    'message' => 'Annuler'
+  },
+  'default friend comments' => {
+    'lastUpdated' => 1231719314,
+    'message' => '%s. jeg vil gerne tilf&oslash;je dig til mit vennenetv&aelig;rk. Tak, %s'
+  },
+  'does not want to be a friend' => {
+    'lastUpdated' => 1231719369,
+    'message' => 'Denne bruger &oslash;nsker ikke at blive tilf&oslash;jet som ven.'
+  },
+  'edit my friends' => {
+    'lastUpdated' => 1231719399,
+    'message' => 'Rediger mine venner'
+  },
+  'error label' => {
+    'lastUpdated' => 1231719428,
+    'message' => 'Fejl'
+  },
+  'error user is already friend' => {
+    'lastUpdated' => 1231719467,
+    'message' => 'Denne bruger er allerede en del af dit vennenetv&aelig;rk'
+  },
+  'error user is already invited' => {
+    'lastUpdated' => 1231719573,
+    'message' => 'Du har allerede sendt en invitation til brugeren om at tilslutte sig dit vennenetv&aelig;rk'
+  },
+  'friend removed' => {
+    'lastUpdated' => 1231719618,
+    'message' => '%s er slette fra din venneliste'
+  },
+  'friends as others label' => {
+    'lastUpdated' => 1231719698,
+    'message' => 'Se mine venner som andre ser dem'
+  },
+  'friends confirm template hoverHelp' => {
+    'lastUpdated' => 1231719992,
+    'message' => 'V&aelig;lg formen du &oslash;nsker at&nbsp;bruge n&aring;r&nbsp;en venneanmodning er blever afsendt'
+  },
+  'friends confirm template label' => {
+    'lastUpdated' => 1231719954,
+    'message' => 'Venneanmoding bekr&aelig;ftelsesform'
+  },
+  'friends edit template hoverHelp' => {
+    'lastUpdated' => 1231720048,
+    'message' => 'V&aelig;lg formen til redigering af venner'
+  },
+  'friends edit template label' => {
+    'lastUpdated' => 1231720067,
+    'message' => 'Rediger formen'
+  },
+  'friends error template hoverHelp' => {
+    'lastUpdated' => 1231720114,
+    'message' => 'V&aelig;lg formen til at vise vennefejl'
+  },
+  'friends error template label' => {
+    'lastUpdated' => 1231720137,
+    'message' => 'Fejlform'
+  },
+  'friends is off' => {
+    'lastUpdated' => 1231720224,
+    'message' => 'Du har ikke venner sl&aring;et til. For at se denne side skal du redigere din profil og sl&aring; venneindstillingen til.'
+  },
+  'friends layout template hoverHelp' => {
+    'lastUpdated' => 1231720348,
+    'message' => 'V&aelig;lg et layout fra listen til at vise de forskellige kontoindstillinger der kan &aelig;ndres af brugeren s&aring;vel som indholdet af den valgte'
+  },
+  'friends layout template label' => {
+    'lastUpdated' => 1231720373,
+    'message' => 'Rediger layoutformen'
+  },
+  'friends remove confirm template hoverHelp' => {
+    'lastUpdated' => 1231720441,
+    'message' => 'V&aelig;lg formen til at vise bekr&aelig;ftelse p&aring; at du &oslash;nsker at slette en ven'
+  },
+  'friends remove confirm template label' => {
+    'lastUpdated' => 1231720702,
+    'message' => '<p>Fjerne venner bekr&aelig;ftelsesform</p>'
+  },
+  'friends send request template hoverHelp' => {
+    'lastUpdated' => 1231720738,
+    'message' => 'V&aelig;lg formen til afsending af venneforsp&oslash;rgsel'
+  },
+  'friends send request template label' => {
+    'lastUpdated' => 1231720770,
+    'message' => 'Send Venneforesp&oslash;rgsel form'
+  }
+}
+;
+
+1;
