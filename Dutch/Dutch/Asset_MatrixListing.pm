f900f7b58e5b2992ed91713f6abc20d8ed75a969
Translation Server <translation@plainblack.com>
2009-01-22 09:09:23 +0000

    Update from translation server

--- Dutch/Dutch/Asset_MatrixListing.pm
+++ Dutch/Dutch/Asset_MatrixListing.pm
@@ -1,281 +1,281 @@
 package WebGUI::i18n::Dutch::Asset_MatrixListing;
 use utf8;
 our $I18N = {
-  'Approve this listing.' => {
-    'lastUpdated' => 1231709679,
-    'message' => 'Deze lijst goedkeuren'
-  },
-  'CATEGORY_NAME_loop' => {
-    'lastUpdated' => 1231711228,
-    'message' => 'Een functie die eigenschappen en waarden bevat voor de eigenschappen die gemaakt zijn voor elke vategorie in deze matrix. De naam van de functie is de naam van de categorie. De spaties worden vervangen door een koppelteken (-) en _loop wordt aan het einde toegevoegd. Als er dus een categorie &quot;toeters en bellen&quot; is zal de functie &quot;toeters-en-bellen_loop&quot; heten.'
-  },
-  'Delete this listing.' => {
-    'lastUpdated' => 1231709698,
-    'message' => 'Verwijder deze lijst.'
-  },
-  'Edit this listing.' => {
-    'lastUpdated' => 1231709708,
-    'message' => 'Bewerk deze lijst.'
-  },
-  'assetName' => {
-    'lastUpdated' => 1227619646,
-    'message' => 'Matrixlijsten'
-  },
-  'attribute_loop' => {
-    'lastUpdated' => 1231709738,
-    'message' => 'Een functie die de eigenschappen van de matrix bevat.'
-  },
-  'best label' => {
-    'lastUpdated' => 1227602529,
-    'message' => 'Beste'
-  },
-  'categoryLabel' => {
-    'lastUpdated' => 1231709755,
-    'message' => 'Het label van deze categorie.'
-  },
-  'clicks' => {
-    'lastUpdated' => 1231710015,
-    'message' => 'Het totaal aantal keren dat deze lijst gekozen is.'
-  },
-  'comment label' => {
-    'lastUpdated' => 1229201158,
-    'message' => 'Reacties'
-  },
-  'comments' => {
-    'lastUpdated' => 1231709775,
-    'message' => 'Het commentaar bij deze lijst.'
-  },
-  'compares' => {
-    'lastUpdated' => 1231709802,
-    'message' => 'Het totale aantal vergelijkingen die met deze lijst zijn gedaan.'
-  },
-  'contact maintainer label' => {
-    'lastUpdated' => 1228818412,
-    'message' => 'Contact voor onderhoud'
-  },
-  'count label' => {
-    'lastUpdated' => 1227873718,
-    'message' => 'Aantal'
-  },
-  'description' => {
-    'lastUpdated' => 1231709816,
-    'message' => 'De omschrijving van deze lijst.'
-  },
-  'description description' => {
-    'lastUpdated' => 1228818455,
-    'message' => 'Geef een algemene beschrijving van het product. Details kunnen in de velden hieronder gespecificeerd worden.'
-  },
-  'description label' => {
-    'lastUpdated' => 1227874474,
-    'message' => 'Beschrijving'
-  },
-  'detail template help title' => {
-    'lastUpdated' => 1231709966,
-    'message' => 'Detail matrixlijst sjabloon variabelen'
-  },
-  'edit listing template help title' => {
-    'lastUpdated' => 1231709987,
-    'message' => 'Bewerk matrixlijst sjabloon variabelen'
-  },
-  'edit matrix listing title' => {
-    'lastUpdated' => 1227873845,
-    'message' => 'Bewerken/toevoegen lijst'
-  },
-  'emailForm' => {
-    'lastUpdated' => 1231709851,
-    'message' => 'Een formulier dat gebruikt wordt om een e-mail naar de onderhouder van de lijst te sturen.'
-  },
-  'emailSent' => {
-    'lastUpdated' => 1231709881,
-    'message' => 'Een functie die aangeeft of er een e-mail bericht is verzonden of niet.'
-  },
-  'fieldType' => {
-    'lastUpdated' => 1231709663,
-    'message' => 'Het veldtype van deze eigenschap.'
-  },
-  'form' => {
-    'lastUpdated' => 1231709483,
-    'message' => 'Het complete formulier voor bewerken.'
-  },
-  'general comment label' => {
-    'lastUpdated' => 1229201183,
-    'message' => 'Algemene reacties'
-  },
-  'isPending' => {
-    'lastUpdated' => 1231709513,
-    'message' => 'Een functie die aangeeft of deze lijst goedgekeurd is of niet.'
-  },
-  'label' => {
-    'lastUpdated' => 1231709531,
-    'message' => 'Het label van deze eigenschap.'
-  },
-  'last updated label' => {
-    'lastUpdated' => 1228818466,
-    'message' => 'Laatst bewerkt'
-  },
-  'lastUpdated_date' => {
-    'lastUpdated' => 1231709560,
-    'message' => 'De staum waarop deze lijst het laatst is bijgewerkt.'
-  },
-  'lastUpdated_epoch' => {
-    'lastUpdated' => 1231709581,
-    'message' => 'De epoch datum waarop deze lijst het laatst is bijgewerkt.'
-  },
-  'maintainer description' => {
-    'lastUpdated' => 1228818564,
-    'message' => 'Als dit leeeggelaten wordt, zal dit de naam zijn van de gebruiker die deze product lijst heeft gemaakt. Degene die deze lijst onderhoudt kan deze lijst bewerken, zelfs als ze niet in de groep zitten die lijsten kan toevoegen of bewerken.'
-  },
-  'maintainer label' => {
-    'lastUpdated' => 1228818626,
-    'message' => 'Onderhouder'
-  },
-  'manufacturer label' => {
-    'lastUpdated' => 1227873753,
-    'message' => 'Fabrikant'
-  },
-  'manufacturerName description' => {
-    'lastUpdated' => 1228818659,
-    'message' => 'De naam van het bedrijf die het product fabriceerd of de service leveren in deze lijst.'
-  },
-  'manufacturerName label' => {
-    'lastUpdated' => 1228818667,
-    'message' => 'Bedrijfsnaam'
-  },
-  'manufacturerURL description' => {
-    'lastUpdated' => 1228818690,
-    'message' => 'Als het bedrijf een website hheft, vul dan hier het URL in.'
-  },
-  'manufacturerURL label' => {
-    'lastUpdated' => 1228818706,
-    'message' => 'URL Bedrijf'
-  },
-  'manufacturerUrl' => {
-    'lastUpdated' => 1231709595,
-    'message' => 'Het URL van de fabrikant.'
-  },
-  'manufacturerUrl_click' => {
-    'lastUpdated' => 1231709930,
-    'message' => 'Het URL van de fabrikant als het aan keren dat deze gekozen is geregistreerd moet worden.'
-  },
-  'mean label' => {
-    'lastUpdated' => 1227873743,
-    'message' => 'Gemiddelde'
-  },
-  'median label' => {
-    'lastUpdated' => 1227873967,
-    'message' => 'Mediaan'
-  },
-  'message sent message' => {
-    'lastUpdated' => 1227874485,
-    'message' => 'Bericht verzonden'
-  },
-  'product name description' => {
-    'lastUpdated' => 1231191193,
-    'message' => 'Voer de naam van het product in. Als er verschillende revisies van het product bestaan, is het het beste om de namen hetzelfde te houden.'
-  },
-  'product name label' => {
-    'lastUpdated' => 1229201246,
-    'message' => 'Productnaam'
-  },
-  'productName' => {
-    'lastUpdated' => 1231709639,
-    'message' => 'De naam van deze lijst. Dit is hetzelfde als de asset titel van deze matrixlijst.'
-  },
-  'productURL description' => {
-    'lastUpdated' => 1228818738,
-    'message' => 'Als het product een eigen website heeft, vul dan hier het gehele URL in.'
-  },
-  'productURL label' => {
-    'lastUpdated' => 1227875150,
-    'message' => 'Produkt URL'
-  },
-  'productUrl' => {
-    'lastUpdated' => 1231709257,
-    'message' => 'Het URL van de fabrikant voor deze lijst.'
-  },
-  'productUrl_click' => {
-    'lastUpdated' => 1231709308,
-    'message' => 'Het URL van het product als het aantal keren dat deze lijst gekozen is geregistreerd moet worden.'
-  },
-  'rate submit label' => {
-    'lastUpdated' => 1231191200,
-    'message' => 'Beoordeel'
-  },
-  'ratings' => {
-    'lastUpdated' => 1231709229,
-    'message' => 'Een overzicht van de waarderingen van deze lijst en een formulier om deze lijst te beoordelen als de gebruiker dit nog niet heeft gedaan.'
-  },
-  'report error label' => {
-    'lastUpdated' => 1229201296,
-    'message' => 'Rapporteer een fout'
-  },
-  'request type label' => {
-    'lastUpdated' => 1229201283,
-    'message' => 'Type verzoek'
-  },
-  'respectable label' => {
-    'lastUpdated' => 1228820018,
-    'message' => 'Respectabel'
-  },
-  'screenshots' => {
-    'lastUpdated' => 1231709451,
-    'message' => 'De schermafdrukken van de lijst die met behulp van de ukplayer getoond worden.'
-  },
-  'screenshots description' => {
-    'lastUpdated' => 1228820317,
-    'message' => 'Voeg een plaatje van het product toe of een screenshot als het een software pakket is.'
-  },
-  'screenshots label' => {
-    'lastUpdated' => 1228820399,
-    'message' => 'Screenshots/Foto\'s'
-  },
-  'send button label' => {
-    'lastUpdated' => 1229201217,
-    'message' => 'Verzenden...'
-  },
-  'show ratings' => {
-    'lastUpdated' => 1229201271,
-    'message' => 'Laat waarderingen zien'
-  },
-  'template description' => {
-    'lastUpdated' => 1228820427,
-    'message' => 'Selecteer een sjabloon voor het tonen van het standaard scherm van de Matrix.'
-  },
-  'template label' => {
-    'lastUpdated' => 1227873735,
-    'message' => 'Matrix lijst sjabloon'
-  },
-  'value' => {
-    'lastUpdated' => 1231708814,
-    'message' => 'De waarde van deze eigenschap.'
-  },
-  'version' => {
-    'lastUpdated' => 1231708796,
-    'message' => 'Het versienummer van de lijst'
-  },
-  'version description' => {
-    'lastUpdated' => 1227619734,
-    'message' => 'Voer het versie/editie/model nummer in voor het product.'
-  },
-  'version label' => {
-    'lastUpdated' => 1227619418,
-    'message' => 'Versie/model'
-  },
-  'views' => {
-    'lastUpdated' => 1231708772,
-    'message' => 'Het totaal aantal keren dat deze lijst ontvangen is.'
-  },
-  'web site label' => {
-    'lastUpdated' => 1227619410,
-    'message' => 'Website'
-  },
-  'worst label' => {
-    'lastUpdated' => 1227602538,
-    'message' => 'Slechtste'
-  },
-  'your email label' => {
-    'lastUpdated' => 1229201231,
-    'message' => 'Uw e-mail adres'
+  "Approve this listing." => {
+    "lastUpdated" => "1231709679",
+    "message" => "Deze lijst goedkeuren"
+  },
+  "CATEGORY_NAME_loop" => {
+    "lastUpdated" => "1232615327",
+    "message" => "Een functie die eigenschappen en waarden bevat voor de eigenschappen die gemaakt zijn voor elke categorie in deze matrix. De naam van de functie is de naam van de categorie. De spaties worden vervangen door een koppelteken (-) en _loop wordt aan het einde toegevoegd. Als er dus een categorie &quot;toeters en bellen&quot; is zal de functie &quot;toeters-en-bellen_loop&quot; heten."
+  },
+  "Delete this listing." => {
+    "lastUpdated" => "1231709698",
+    "message" => "Verwijder deze lijst."
+  },
+  "Edit this listing." => {
+    "lastUpdated" => "1231709708",
+    "message" => "Bewerk deze lijst."
+  },
+  "assetName" => {
+    "lastUpdated" => "1227619646",
+    "message" => "Matrixlijsten"
+  },
+  "attribute_loop" => {
+    "lastUpdated" => "1231709738",
+    "message" => "Een functie die de eigenschappen van de matrix bevat."
+  },
+  "best label" => {
+    "lastUpdated" => "1227602529",
+    "message" => "Beste"
+  },
+  "categoryLabel" => {
+    "lastUpdated" => "1231709755",
+    "message" => "Het label van deze categorie."
+  },
+  "clicks" => {
+    "lastUpdated" => "1231710015",
+    "message" => "Het totaal aantal keren dat deze lijst gekozen is."
+  },
+  "comment label" => {
+    "lastUpdated" => "1229201158",
+    "message" => "Reacties"
+  },
+  "comments" => {
+    "lastUpdated" => "1231709775",
+    "message" => "Het commentaar bij deze lijst."
+  },
+  "compares" => {
+    "lastUpdated" => "1231709802",
+    "message" => "Het totale aantal vergelijkingen die met deze lijst zijn gedaan."
+  },
+  "contact maintainer label" => {
+    "lastUpdated" => "1228818412",
+    "message" => "Contact voor onderhoud"
+  },
+  "count label" => {
+    "lastUpdated" => "1227873718",
+    "message" => "Aantal"
+  },
+  "description" => {
+    "lastUpdated" => "1231709816",
+    "message" => "De omschrijving van deze lijst."
+  },
+  "description description" => {
+    "lastUpdated" => "1228818455",
+    "message" => "Geef een algemene beschrijving van het product. Details kunnen in de velden hieronder gespecificeerd worden."
+  },
+  "description label" => {
+    "lastUpdated" => "1227874474",
+    "message" => "Beschrijving"
+  },
+  "detail template help title" => {
+    "lastUpdated" => "1231709966",
+    "message" => "Detail matrixlijst sjabloon variabelen"
+  },
+  "edit listing template help title" => {
+    "lastUpdated" => "1231709987",
+    "message" => "Bewerk matrixlijst sjabloon variabelen"
+  },
+  "edit matrix listing title" => {
+    "lastUpdated" => "1227873845",
+    "message" => "Bewerken/toevoegen lijst"
+  },
+  "emailForm" => {
+    "lastUpdated" => "1231709851",
+    "message" => "Een formulier dat gebruikt wordt om een e-mail naar de onderhouder van de lijst te sturen."
+  },
+  "emailSent" => {
+    "lastUpdated" => "1231709881",
+    "message" => "Een functie die aangeeft of er een e-mail bericht is verzonden of niet."
+  },
+  "fieldType" => {
+    "lastUpdated" => "1231709663",
+    "message" => "Het veldtype van deze eigenschap."
+  },
+  "form" => {
+    "lastUpdated" => "1231709483",
+    "message" => "Het complete formulier voor bewerken."
+  },
+  "general comment label" => {
+    "lastUpdated" => "1229201183",
+    "message" => "Algemene reacties"
+  },
+  "isPending" => {
+    "lastUpdated" => "1231709513",
+    "message" => "Een functie die aangeeft of deze lijst goedgekeurd is of niet."
+  },
+  "label" => {
+    "lastUpdated" => "1231709531",
+    "message" => "Het label van deze eigenschap."
+  },
+  "last updated label" => {
+    "lastUpdated" => "1228818466",
+    "message" => "Laatst bewerkt"
+  },
+  "lastUpdated_date" => {
+    "lastUpdated" => "1231709560",
+    "message" => "De staum waarop deze lijst het laatst is bijgewerkt."
+  },
+  "lastUpdated_epoch" => {
+    "lastUpdated" => "1231709581",
+    "message" => "De epoch datum waarop deze lijst het laatst is bijgewerkt."
+  },
+  "maintainer description" => {
+    "lastUpdated" => "1228818564",
+    "message" => "Als dit leeeggelaten wordt, zal dit de naam zijn van de gebruiker die deze product lijst heeft gemaakt. Degene die deze lijst onderhoudt kan deze lijst bewerken, zelfs als ze niet in de groep zitten die lijsten kan toevoegen of bewerken."
+  },
+  "maintainer label" => {
+    "lastUpdated" => "1228818626",
+    "message" => "Onderhouder"
+  },
+  "manufacturer label" => {
+    "lastUpdated" => "1227873753",
+    "message" => "Fabrikant"
+  },
+  "manufacturerName description" => {
+    "lastUpdated" => "1228818659",
+    "message" => "De naam van het bedrijf die het product fabriceerd of de service leveren in deze lijst."
+  },
+  "manufacturerName label" => {
+    "lastUpdated" => "1228818667",
+    "message" => "Bedrijfsnaam"
+  },
+  "manufacturerURL description" => {
+    "lastUpdated" => "1228818690",
+    "message" => "Als het bedrijf een website hheft, vul dan hier het URL in."
+  },
+  "manufacturerURL label" => {
+    "lastUpdated" => "1228818706",
+    "message" => "URL Bedrijf"
+  },
+  "manufacturerUrl" => {
+    "lastUpdated" => "1231709595",
+    "message" => "Het URL van de fabrikant."
+  },
+  "manufacturerUrl_click" => {
+    "lastUpdated" => "1231709930",
+    "message" => "Het URL van de fabrikant als het aan keren dat deze gekozen is geregistreerd moet worden."
+  },
+  "mean label" => {
+    "lastUpdated" => "1227873743",
+    "message" => "Gemiddelde"
+  },
+  "median label" => {
+    "lastUpdated" => "1227873967",
+    "message" => "Mediaan"
+  },
+  "message sent message" => {
+    "lastUpdated" => "1227874485",
+    "message" => "Bericht verzonden"
+  },
+  "product name description" => {
+    "lastUpdated" => "1231191193",
+    "message" => "Voer de naam van het product in. Als er verschillende revisies van het product bestaan, is het het beste om de namen hetzelfde te houden."
+  },
+  "product name label" => {
+    "lastUpdated" => "1229201246",
+    "message" => "Productnaam"
+  },
+  "productName" => {
+    "lastUpdated" => "1231709639",
+    "message" => "De naam van deze lijst. Dit is hetzelfde als de asset titel van deze matrixlijst."
+  },
+  "productURL description" => {
+    "lastUpdated" => "1228818738",
+    "message" => "Als het product een eigen website heeft, vul dan hier het gehele URL in."
+  },
+  "productURL label" => {
+    "lastUpdated" => "1227875150",
+    "message" => "Produkt URL"
+  },
+  "productUrl" => {
+    "lastUpdated" => "1231709257",
+    "message" => "Het URL van de fabrikant voor deze lijst."
+  },
+  "productUrl_click" => {
+    "lastUpdated" => "1231709308",
+    "message" => "Het URL van het product als het aantal keren dat deze lijst gekozen is geregistreerd moet worden."
+  },
+  "rate submit label" => {
+    "lastUpdated" => "1231191200",
+    "message" => "Beoordeel"
+  },
+  "ratings" => {
+    "lastUpdated" => "1231709229",
+    "message" => "Een overzicht van de waarderingen van deze lijst en een formulier om deze lijst te beoordelen als de gebruiker dit nog niet heeft gedaan."
+  },
+  "report error label" => {
+    "lastUpdated" => "1229201296",
+    "message" => "Rapporteer een fout"
+  },
+  "request type label" => {
+    "lastUpdated" => "1229201283",
+    "message" => "Type verzoek"
+  },
+  "respectable label" => {
+    "lastUpdated" => "1228820018",
+    "message" => "Respectabel"
+  },
+  "screenshots" => {
+    "lastUpdated" => "1231709451",
+    "message" => "De schermafdrukken van de lijst die met behulp van de ukplayer getoond worden."
+  },
+  "screenshots description" => {
+    "lastUpdated" => "1228820317",
+    "message" => "Voeg een plaatje van het product toe of een screenshot als het een software pakket is."
+  },
+  "screenshots label" => {
+    "lastUpdated" => "1228820399",
+    "message" => "Screenshots/Foto's"
+  },
+  "send button label" => {
+    "lastUpdated" => "1229201217",
+    "message" => "Verzenden..."
+  },
+  "show ratings" => {
+    "lastUpdated" => "1229201271",
+    "message" => "Laat waarderingen zien"
+  },
+  "template description" => {
+    "lastUpdated" => "1228820427",
+    "message" => "Selecteer een sjabloon voor het tonen van het standaard scherm van de Matrix."
+  },
+  "template label" => {
+    "lastUpdated" => "1227873735",
+    "message" => "Matrix lijst sjabloon"
+  },
+  "value" => {
+    "lastUpdated" => "1231708814",
+    "message" => "De waarde van deze eigenschap."
+  },
+  "version" => {
+    "lastUpdated" => "1231708796",
+    "message" => "Het versienummer van de lijst"
+  },
+  "version description" => {
+    "lastUpdated" => "1227619734",
+    "message" => "Voer het versie/editie/model nummer in voor het product."
+  },
+  "version label" => {
+    "lastUpdated" => "1227619418",
+    "message" => "Versie/model"
+  },
+  "views" => {
+    "lastUpdated" => "1231708772",
+    "message" => "Het totaal aantal keren dat deze lijst ontvangen is."
+  },
+  "web site label" => {
+    "lastUpdated" => "1227619410",
+    "message" => "Website"
+  },
+  "worst label" => {
+    "lastUpdated" => "1227602538",
+    "message" => "Slechtste"
+  },
+  "your email label" => {
+    "lastUpdated" => "1229201231",
+    "message" => "Uw e-mail adres"
   }
 }
 ;
