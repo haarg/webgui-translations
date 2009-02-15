55a89cbe87677283d20e15b3e0bddc62b576761b
Translation Server <translation@plainblack.com>
2009-02-15 23:05:49 +0000

    Update from translation server

--- Dutch/Dutch/Form_Captcha.pm
+++ Dutch/Dutch/Form_Captcha.pm
@@ -1,17 +1,23 @@
 package WebGUI::i18n::Dutch::Form_Captcha;
 use utf8;
-
 our $I18N = {
-	'topicName' => {
-		message=>q|Captcha|,
-		lastUpdated=>1146665475,
-	},
+  'captcha failure' => {
+    'lastUpdated' => '1234738983',
+    'message' => 'U dient de zes cijfers, die u in het plaatje ziet, in te geven.'
+  },
+  'recaptcha failure' => {
+    'lastUpdated' => '1234739140',
+    'message' => 'Om te bewijzen, dat u dit formulier zelf invult, dient u de letters, die  u in het plaatje ziet, in het formulier in te geven.'
+  },
+  'topicName' => {
+    'lastUpdated' => '1146665475',
+    'message' => 'Captcha'
+  },
+  'verify your humanity' => {
+    'lastUpdated' => '1155559759',
+    'message' => 'Anti robotcode'
+  }
+}
+;
 
-	'verify your humanity' => {
-		message=>q|Anti robotcode|,
-		lastUpdated=>1155559759,
-	},
-
-};
-
-1
+1;
