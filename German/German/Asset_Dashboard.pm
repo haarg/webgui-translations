1105f20cc3f75e82747e082571263ca1607f05ec
Translation Server <translation@plainblack.com>
2009-02-15 22:54:35 +0000

    Update from translation server

--- German/German/Asset_Dashboard.pm
+++ German/German/Asset_Dashboard.pm
@@ -1,53 +1,47 @@
 package WebGUI::i18n::German::Asset_Dashboard;
 use utf8;
-
-
 our $I18N = {
-	'assets to hide' => {
-		message => q|Assets zum Verbergen|,
-		lastUpdated => 1180078541
-	},
-
-	'assetName' => {
-		message => q|Dashboard|,
-		lastUpdated => 1164282567
-	},
-
-	'dashboard usersGroupId description' => {
-		message => q|Gruppe, deren Mitglieder ihre eigenen Personalisierungen/Pr&auml;ferenzen speicher k&ouml;nnen. Falls ein Benutzer in der "Wer kann das Asset sehen?" Grupe ist, darf dieser die Anordnung von Dashlets personalisieren (Positionen werden in Cookies gespeichert), sie d&uuml;rfen aber die Eigenschaften der einzelnen Dashlets nicht &auml;ndern.|,
-		lastUpdated => 1180079494
-	},
-
-	'dashboard adminsGroupId field label' => {
-		message => q|Wer darf verwalten?|,
-		lastUpdated => 1180079197
-	},
-
-	'dashboard usersGroupId field label' => {
-		message => q|Wer darf personalisieren?|,
-		lastUpdated => 1180079505
-	},
-
-	'assets to hide description' => {
-		message => q|Diese Liste ent&auml;lt ein Auswahlfeld f&uuml;r jedes Asset auf der Seite. W&auml;hlen Sie hier die Assets aus, die nicht auf der Seite dargestellt werden sollen.|,
-		lastUpdated => 1180078717
-	},
-
-	'dashboard template field label' => {
-		message => q|Dashboard Template|,
-		lastUpdated => 1180079239
-	},
-
-	'dashboard adminsGroupId description' => {
-		message => q|Welche Gruppe kann dieses Dashboard administrieren. Hinzuf&uuml;gen/Bearbeiten/Entfernen verf&uuml;gbarer Dashlets, Pr&auml;ferenzen und Templates.|,
-		lastUpdated => 1180079188
-	},
-
-	'dashboard template description' => {
-		message => q|W&auml;hlen Sie ein Dashboard/Portal Template. Standard ist das <strong>Three Column Layout</strong>|,
-		lastUpdated => 1180079232
-	},
-
-};
+  'assetName' => {
+    'lastUpdated' => '1164282567',
+    'message' => 'Dashboard'
+  },
+  'assets to hide' => {
+    'lastUpdated' => '1180078541',
+    'message' => 'Assets zum Verbergen'
+  },
+  'assets to hide description' => {
+    'lastUpdated' => '1180078717',
+    'message' => 'Diese Liste ent&auml;lt ein Auswahlfeld f&uuml;r jedes Asset auf der Seite. W&auml;hlen Sie hier die Assets aus, die nicht auf der Seite dargestellt werden sollen.'
+  },
+  'dashboard adminsGroupId description' => {
+    'lastUpdated' => '1180079188',
+    'message' => 'Welche Gruppe kann dieses Dashboard administrieren. Hinzuf&uuml;gen/Bearbeiten/Entfernen verf&uuml;gbarer Dashlets, Pr&auml;ferenzen und Templates.'
+  },
+  'dashboard adminsGroupId field label' => {
+    'lastUpdated' => '1180079197',
+    'message' => 'Wer darf verwalten?'
+  },
+  'dashboard template description' => {
+    'lastUpdated' => '1180079232',
+    'message' => 'W&auml;hlen Sie ein Dashboard/Portal Template. Standard ist das <strong>Three Column Layout</strong>'
+  },
+  'dashboard template field label' => {
+    'lastUpdated' => '1180079239',
+    'message' => 'Dashboard Template'
+  },
+  'dashboard usersGroupId description' => {
+    'lastUpdated' => '1180079494',
+    'message' => 'Gruppe, deren Mitglieder ihre eigenen Personalisierungen/Pr&auml;ferenzen speicher k&ouml;nnen. Falls ein Benutzer in der "Wer kann das Asset sehen?" Grupe ist, darf dieser die Anordnung von Dashlets personalisieren (Positionen werden in Cookies gespeichert), sie d&uuml;rfen aber die Eigenschaften der einzelnen Dashlets nicht &auml;ndern.'
+  },
+  'dashboard usersGroupId field label' => {
+    'lastUpdated' => '1180079505',
+    'message' => 'Wer darf personalisieren?'
+  },
+  'hide new content list' => {
+    'lastUpdated' => '1234738453',
+    'message' => '"Neuer Inhalt" verbergen'
+  }
+}
+;
 
 1;
