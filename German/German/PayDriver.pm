1725d7cb67f2e28418ab627429fb072bd430b661
Translation Server <translation@plainblack.com>
2009-02-15 22:40:40 +0000

    Update from translation server

--- German/German/PayDriver.pm
+++ German/German/PayDriver.pm
@@ -1,7 +1,35 @@
 package WebGUI::i18n::German::PayDriver;
 use utf8;
-
 our $I18N = {
-};
+  'a sale has been made' => {
+    'lastUpdated' => '1234737219',
+    'message' => 'Es wurde einen Verkauf getätigt. Bestell-Nr: '
+  },
+  'credit card' => {
+    'lastUpdated' => '1234737231',
+    'message' => 'Kreditkarte'
+  },
+  'email receipt template' => {
+    'lastUpdated' => '1234737487',
+    'message' => 'Email-Rechnungsvorlage'
+  },
+  'email receipt template help' => {
+    'lastUpdated' => '1234737405',
+    'message' => 'Rechnungsvorlage für den Versand per Email'
+  },
+  'enabled' => {
+    'lastUpdated' => '1234737498',
+    'message' => 'Freigegeben'
+  },
+  'enabled help' => {
+    'lastUpdated' => '1234737576',
+    'message' => 'Diese Einstellung gibt diese Möglichkeit zur Zahlung frei.'
+  },
+  'error processing payment' => {
+    'lastUpdated' => '1234737617',
+    'message' => 'Fehlerbearbeitung Zahlung'
+  }
+}
+;
 
 1;
