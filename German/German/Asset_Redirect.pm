95299489a710fccfa20f4076bb08795d2f6681b7
Translation Server <translation@plainblack.com>
2009-02-18 16:13:09 +0000

    Update from translation server

--- German/German/Asset_Redirect.pm
+++ German/German/Asset_Redirect.pm
@@ -2,51 +2,51 @@ package WebGUI::i18n::German::Asset_Redirect;
 use utf8;
 our $I18N = {
   '301 Moved Permanently' => {
-    'lastUpdated' => 1225318143,
+    'lastUpdated' => '1225318143',
     'message' => '301 Permanent ge&auml;ndert'
   },
   '302 Moved Temporarily' => {
-    'lastUpdated' => 1225318162,
+    'lastUpdated' => '1225318162',
     'message' => '302 Tempor&auml;r ge&auml;ndert'
   },
   'Redirect Type' => {
-    'lastUpdated' => 1225318180,
+    'lastUpdated' => '1225318180',
     'message' => 'Type der Weiterleitung'
   },
   'assetName' => {
-    'lastUpdated' => 1170408144,
+    'lastUpdated' => '1170408144',
     'message' => 'Redirect'
   },
   'edit the redirect properties' => {
-    'lastUpdated' => 1170408164,
+    'lastUpdated' => '1170408164',
     'message' => 'Bearbeiten Redirect Eigenschaften.<br />'
   },
   'go to the redirect parent page' => {
-    'lastUpdated' => 1170408206,
+    'lastUpdated' => '1170408206',
     'message' => 'Gehe zu Redirects Vorg&auml;nger.'
   },
   'go to the redirect url' => {
-    'lastUpdated' => 1170408218,
+    'lastUpdated' => '1170408218',
     'message' => 'Gehe zu Redirect URL.<br />'
   },
   'redirect type description' => {
-    'lastUpdated' => 1225318230,
+    'lastUpdated' => '1225318230',
     'message' => 'Type des HTTP-Headers einer Weiterleitung '
   },
   'redirect url' => {
-    'lastUpdated' => 1170408573,
-    'message' => 'Redirect URL<br />'
+    'lastUpdated' => '1234964157',
+    'message' => 'Weiterleitung zu URL<br />'
   },
   'redirect url description' => {
-    'lastUpdated' => 1170408596,
+    'lastUpdated' => '1170408596',
     'message' => 'Die URL, an die der Benutzer weitergeleitet wird.<br />'
   },
   'self_referential' => {
-    'lastUpdated' => 1170408630,
+    'lastUpdated' => '1170408630',
     'message' => 'Redirect zeigt auf sich selbst '
   },
   'what do you want to do with this redirect' => {
-    'lastUpdated' => 1170408673,
+    'lastUpdated' => '1170408673',
     'message' => 'Was wollen Sie mit diesem Redirect machen?<br /> '
   }
 }
--- German/German/Workflow.pm
+++ German/German/Workflow.pm
@@ -2,190 +2,190 @@ package WebGUI::i18n::German::Workflow;
 use utf8;
 our $I18N = {
   'activity header' => {
-    'lastUpdated' => 1193132772,
-    'message' => "Gegenw\x{e4}rtige T\x{e4}tigkeit"
+    'lastUpdated' => '1193132772',
+    'message' => 'Gegenwärtige Tätigkeit'
   },
   'add a new workflow' => {
-    'lastUpdated' => 1168590624,
+    'lastUpdated' => '1168590624',
     'message' => 'Neuen Workflow hinzuf&uuml;gen'
   },
   'are you sure you want to delete this workflow' => {
-    'lastUpdated' => 1178707393,
+    'lastUpdated' => '1178707393',
     'message' => 'Sind Sie sich sicher, dass Sie diesen Workflow und alle laufenden Instanzen desselben l&ouml;schen wollen?'
   },
   'bad workflow activity code' => {
-    'lastUpdated' => 1228174667,
+    'lastUpdated' => '1228174667',
     'message' => 'Syntaxfehler %s. Diese Aktivit&auml;t kann nicht editiert werden.'
   },
   'confirm delete activity' => {
-    'lastUpdated' => 1178707430,
+    'lastUpdated' => '1178707430',
     'message' => 'Sind Sie sich sicher, dass Sie diese Aktivit&auml;t von diesem Workflow entfernen wollen?'
   },
   'description' => {
-    'lastUpdated' => 1168590678,
+    'lastUpdated' => '1168590678',
     'message' => 'Beschreibung'
   },
   'description help' => {
-    'lastUpdated' => 1178707516,
+    'lastUpdated' => '1178707516',
     'message' => 'Beschreiben Sie hier im Detail, was dieser Workflow f&uuml;r eine Aufgabe hat und was dieser tut.'
   },
   'disabled' => {
-    'lastUpdated' => 1168590636,
+    'lastUpdated' => '1168590636',
     'message' => 'Deaktiviert'
   },
   'edit priority bad request' => {
-    'lastUpdated' => 1200441111,
+    'lastUpdated' => '1200441111',
     'message' => 'Ung&uuml;ltige Anfrage'
   },
   'edit priority cancel' => {
-    'lastUpdated' => 1199974330,
+    'lastUpdated' => '1199974330',
     'message' => 'Abbrechen'
   },
   'edit priority instance not found error' => {
-    'lastUpdated' => 1199974385,
+    'lastUpdated' => '1199974385',
     'message' => 'Workflow konnte nicht gefunden werden. Vielleicht wurde er beendet.'
   },
   'edit priority no info error' => {
-    'lastUpdated' => 1199974439,
+    'lastUpdated' => '1199974439',
     'message' => 'Spectre <b>l&auml;uft noch</b>, aber es war nicht m&ouml;glich, die Priorit&auml;t zu &auml;ndern.'
   },
   'edit priority no spectre error' => {
-    'lastUpdated' => 1199974499,
+    'lastUpdated' => '1199974499',
     'message' => 'Spectre <b>l&auml;uft nicht mehr.<br>
 Nicht m&ouml;glich, Workflow Information zu bekommen.'
   },
   'edit priority setting error' => {
-    'lastUpdated' => 1199974521,
+    'lastUpdated' => '1199974521',
     'message' => 'Beim Setzen der neuen Priorit&auml;t ist ein Fehler aufgetreten.'
   },
   'edit priority success' => {
-    'lastUpdated' => 1199974548,
+    'lastUpdated' => '1199974548',
     'message' => 'Priorit&auml;t des Workflow wurde erfolgreich ge&auml;ndert.'
   },
   'edit priority unknown error' => {
-    'lastUpdated' => 1199974595,
+    'lastUpdated' => '1199974595',
     'message' => 'Beim &Auml;ndern der Workflow Priorit&auml;t ist ein unbekannter Fehler aufgetreten. Bitte versuchen sie es sp&auml;ter noch einmal.'
   },
   'edit priority update priority' => {
-    'lastUpdated' => 1199974609,
+    'lastUpdated' => '1199974609',
     'message' => 'Priorit&auml;t &auml;ndern'
   },
   'edit workflow' => {
-    'lastUpdated' => 1178707527,
+    'lastUpdated' => '1178707527',
     'message' => 'Workflow bearbeiten'
   },
   'enabled' => {
-    'lastUpdated' => 1168590671,
+    'lastUpdated' => '1168590671',
     'message' => 'Aktiviert'
   },
   'form control none label' => {
-    'lastUpdated' => 1216637314,
+    'lastUpdated' => '1216637314',
     'message' => 'Kein'
   },
   'is enabled' => {
-    'lastUpdated' => 1168866675,
+    'lastUpdated' => '1168866675',
     'message' => 'Aktiviert?'
   },
   'is enabled help' => {
-    'lastUpdated' => 1178707954,
+    'lastUpdated' => '1178707954',
     'message' => 'Wenn diese hier auf &quot;Ja&quot; gesetzt ist, dann kann das System laufenden Instanzen dieses Workflows ausf&uuml;hren. '
   },
   'last run time header' => {
-    'lastUpdated' => 1193133215,
+    'lastUpdated' => '1193133215',
     'message' => 'Letzter Run Time'
   },
   'last state header' => {
-    'lastUpdated' => 1193133206,
+    'lastUpdated' => '1193133206',
     'message' => 'Letzter Stand'
   },
   'manage workflows' => {
-    'lastUpdated' => 1168867508,
+    'lastUpdated' => '1168867508',
     'message' => 'Alle Workflows verwalten.'
   },
   'mode' => {
-    'lastUpdated' => 1199974972,
+    'lastUpdated' => '1199974972',
     'message' => 'Modus'
   },
   'mode help' => {
-    'lastUpdated' => 1216637380,
+    'lastUpdated' => '1216637380',
     'message' => 'Der Modus eines Workflows bestimmt, wann und wie ein Workflow ausgef&uuml;hrt wird.'
   },
   'no object' => {
-    'lastUpdated' => 1168590690,
+    'lastUpdated' => '1168590690',
     'message' => 'Kein Objekt'
   },
   'object type' => {
-    'lastUpdated' => 1168867484,
+    'lastUpdated' => '1168867484',
     'message' => 'Objektart'
   },
   'object type help' => {
-    'lastUpdated' => 1168867435,
+    'lastUpdated' => '1168867435',
     'message' => 'Welche Objektarten soll dieser Workflow verarbeiten k&ouml;nnen?'
   },
   'object type help2' => {
-    'lastUpdated' => 1168867468,
+    'lastUpdated' => '1168867468',
     'message' => 'Die Art des Workflows, der hinzugef&uuml;gt werden soll.'
   },
   'parallel' => {
-    'lastUpdated' => 1193133093,
+    'lastUpdated' => '1193133093',
     'message' => 'Parallel'
   },
   'priority header' => {
-    'lastUpdated' => 1193133173,
-    'message' => "Gegenw\x{e4}rtige/urspr\x{fc}ngliche Priorit\x{e4}t"
+    'lastUpdated' => '1193133173',
+    'message' => 'Gegenwärtige/ursprüngliche Priorität'
   },
   'realtime' => {
-    'lastUpdated' => 1193133106,
+    'lastUpdated' => '1193133106',
     'message' => 'Echtzeit'
   },
   'run' => {
-    'lastUpdated' => 1168590704,
+    'lastUpdated' => '1168590704',
     'message' => 'Start'
   },
   'serial' => {
-    'lastUpdated' => 1193132808,
+    'lastUpdated' => '1193132808',
     'message' => 'Serial'
   },
   'show running workflows' => {
-    'lastUpdated' => 1168867206,
+    'lastUpdated' => '1168867206',
     'message' => 'Laufende Workflows anzeigen.'
   },
   'singleton' => {
-    'lastUpdated' => 1193133006,
+    'lastUpdated' => '1193133006',
     'message' => 'Singleton'
   },
   'spectre no info error' => {
-    'lastUpdated' => 1193132980,
-    'message' => "Spectre l\x{e4}uft, aber ich war nicht im Stande, detailierte Workflow-Information zu bekommen."
+    'lastUpdated' => '1193132980',
+    'message' => 'Spectre läuft, aber ich war nicht im Stande, detailierte Workflow-Information zu bekommen.'
   },
   'spectre not running error' => {
-    'lastUpdated' => 1193133080,
-    'message' => "Spectre l\x{e4}uft nicht.
-Ich war nicht im Stande, detailierte Workflow-Information zu bekommen."
+    'lastUpdated' => '1234973578',
+    'message' => 'Spectre läuft nicht.
+Ich war nicht im Stande, detaillierte Workflow-Information zu bekommen.'
   },
   'title' => {
-    'lastUpdated' => 1164282440,
+    'lastUpdated' => '1164282440',
     'message' => 'Titel'
   },
   'title header' => {
-    'lastUpdated' => 1193132819,
+    'lastUpdated' => '1193132819',
     'message' => 'Titel'
   },
   'title help' => {
-    'lastUpdated' => 1168867153,
+    'lastUpdated' => '1168867153',
     'message' => 'Ein menschenlesbarer Text, der einfach beschreibt, was
 der Workflow macht.'
   },
   'topicName' => {
-    'lastUpdated' => 1168867163,
+    'lastUpdated' => '1168867163',
     'message' => 'Workflow'
   },
   'workflow type count' => {
-    'lastUpdated' => 1193132842,
+    'lastUpdated' => '1193132842',
     'message' => '%d %s Workflows'
   },
   'workflowId' => {
-    'lastUpdated' => 1168590647,
+    'lastUpdated' => '1168590647',
     'message' => 'Workflow ID'
   }
 }
