77d7afcd92ddd565f6f467b60df1171dec09a613
Translation Server <translation@plainblack.com>
2009-02-23 16:45:14 +0000

    Update from translation server

--- Russian/Russian/Account_Inbox.pm
+++ Russian/Russian/Account_Inbox.pm
@@ -1,5 +1,11 @@
 package WebGUI::i18n::Russian::Account_Inbox;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'accept button label' => {
+    'lastUpdated' => '1234547199',
+    'message' => "\x{41f}\x{440}\x{438}\x{43d}\x{44f}\x{442}\x{44c}"
+  }
+}
+;
 
 1;
