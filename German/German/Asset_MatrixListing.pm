f75b9fc30498480ef49c2f88d887396d26430657
Translation Server <translation@plainblack.com>
2009-03-13 16:24:45 +0000

    Update from translation server

--- German/German/Asset_MatrixListing.pm
+++ German/German/Asset_MatrixListing.pm
@@ -1,5 +1,11 @@
 package WebGUI::i18n::German::Asset_MatrixListing;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'count label' => {
+    'lastUpdated' => '1236959869',
+    'message' => 'Anzahl'
+  }
+}
+;
 
 1;
