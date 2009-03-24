454d5057dd7c40cd8cb92f69be5d99139da7e25d
Translation Server <translation@plainblack.com>
2009-03-24 10:43:11 +0000

    Update from translation server

--- Turkish/Turkish/Macros.pm
+++ Turkish/Turkish/Macros.pm
@@ -1,57 +1,47 @@
 package WebGUI::i18n::Turkish::Macros;
 use utf8;
-
 our $I18N = {
-	'macro disabled' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'macros list body' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'macro enabled' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'macro shortcut' => {
-		message => q|makro kısayolu|,
-		lastUpdated => 1211334506
-	},
-
-	'macro enabled header' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'topicName' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'macro name' => {
-		message => q|makro adı|,
-		lastUpdated => 1211334492
-	},
-
-	'macros list title' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'macros using body' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'macros using title' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-};
+  'macro disabled' => {
+    'lastUpdated' => '1237890854',
+    'message' => "Bu makro WebGUI ayar dosyas\x{131}nda etkinle\x{15f}tirilmedi ve bu sitede kullan\x{131}lamaz."
+  },
+  'macro enabled' => {
+    'lastUpdated' => '1237890888',
+    'message' => "Bu makro WebGUI ayar dosyas\x{131}nda etkinle\x{15f}tirildi ve bu sitede kullan\x{131}labilir."
+  },
+  'macro enabled header' => {
+    'lastUpdated' => '1237890908',
+    'message' => 'Makro Etkin Mi?'
+  },
+  'macro name' => {
+    'lastUpdated' => '1237890931',
+    'message' => "Makro Ad\x{131}"
+  },
+  'macro shortcut' => {
+    'lastUpdated' => '1237890938',
+    'message' => "Makro K\x{131}sayolu"
+  },
+  'macros list body' => {
+    'lastUpdated' => '1237891365',
+    'message' => ''
+  },
+  'macros list title' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'macros using body' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'macros using title' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'topicName' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  }
+}
+;
 
 1;
