2365767247dc3edf585f3ad6af317b2fe7f1e8ef
Translation Server <translation@plainblack.com>
2009-03-24 10:17:58 +0000

    Update from translation server

--- Turkish/Turkish/Account_Profile.pm
+++ Turkish/Turkish/Account_Profile.pm
@@ -1,5 +1,91 @@
 package WebGUI::i18n::Turkish::Account_Profile;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'back label' => {
+    'lastUpdated' => '1237889041',
+    'message' => 'siteye geri dön'
+  },
+  'back to profile label' => {
+    'lastUpdated' => '1237889050',
+    'message' => 'profile geri dön'
+  },
+  'edit my profile' => {
+    'lastUpdated' => '1237889062',
+    'message' => 'profilimi düzenle'
+  },
+  'error label' => {
+    'lastUpdated' => '1237889069',
+    'message' => 'Hata'
+  },
+  'friends only' => {
+    'lastUpdated' => '1237889105',
+    'message' => "Profiliniz Arkada\x{15f}lar\x{131}n\x{131}za g\x{f6}r\x{fc}n\x{fc}r"
+  },
+  'friends only label' => {
+    'lastUpdated' => '1237889125',
+    'message' => "Sadece Arkada\x{15f}lara"
+  },
+  'member since' => {
+    'lastUpdated' => '1237889182',
+    'message' => 'Üyelik Tarihi'
+  },
+  'private label' => {
+    'lastUpdated' => '1237889191',
+    'message' => 'Özel'
+  },
+  'private profile' => {
+    'lastUpdated' => '1237889207',
+    'message' => 'Profiliniz Özel'
+  },
+  'profile as others label' => {
+    'lastUpdated' => '1237889237',
+    'message' => "profilimi di\x{11f}erlerinin g\x{f6}rd\x{fc}\x{11f}\x{fc} gibi g\x{f6}ster"
+  },
+  'profile edit layout template hoverHelp' => {
+    'lastUpdated' => '1237889298',
+    'message' => "Kullan\x{131}c\x{131}n\x{131}n profilini d\x{fc}zenlemek i\x{e7}in listeden bir yerle\x{15f}im se\x{e7}in."
+  },
+  'profile edit layout template label' => {
+    'lastUpdated' => '1237889314',
+    'message' => "Yerle\x{15f}im \x{15e}ablonunu G\x{f6}ster"
+  },
+  'profile edit template hoverHelp' => {
+    'lastUpdated' => '1237889371',
+    'message' => "Bir profili d\x{fc}zenlemek i\x{e7}in ana \x{15f}ablonu se\x{e7}in"
+  },
+  'profile edit template label' => {
+    'lastUpdated' => '1237889381',
+    'message' => "\x{15e}ablonu D\x{fc}zenle"
+  },
+  'profile error template hoverHelp' => {
+    'lastUpdated' => '1237889432',
+    'message' => "Kullan\x{131}c\x{131}ya profil hatalar\x{131}n\x{131} g\x{f6}stermek i\x{e7}in kullan\x{131}lacak \x{15f}ablonu se\x{e7}in"
+  },
+  'profile error template label' => {
+    'lastUpdated' => '1237889441',
+    'message' => "Hata \x{15e}ablonu"
+  },
+  'profile layout template hoverHelp' => {
+    'lastUpdated' => '1237889691',
+    'message' => "Etkin\x{a0}kullan\x{131}c\x{131} taraf\x{131}ndan kullan\x{131}labilen \x{e7}e\x{15f}itli hesap eklentilerini ve se\x{e7}ilen eklentinin de i\x{e7}eri\x{11f}ini g\x{f6}stermek i\x{e7}in\x{a0}kullan\x{131}lacak \x{15f}ablonu listeden\x{a0}se\x{e7}in."
+  },
+  'profile layout template label' => {
+    'lastUpdated' => '1237889710',
+    'message' => "Yerle\x{15f}im \x{15e}ablonunu D\x{fc}zenle"
+  },
+  'profile not public error' => {
+    'lastUpdated' => '1237889765',
+    'message' => "Bu kullan\x{131}c\x{131}n\x{131}n profili genele a\x{e7}\x{131}k\x{a0}de\x{11f}il."
+  },
+  'profile style template hoverHelp' => {
+    'lastUpdated' => '1237889853',
+    'message' => "Profili g\x{f6}r\x{fc}nt\x{fc}lemek i\x{e7}in kullan\x{131}lacak stil \x{15f}ablonunu listeden se\x{e7}in."
+  },
+  'profile style template label' => {
+    'lastUpdated' => '1237889866',
+    'message' => "Stil \x{15e}ablonu"
+  }
+}
+;
 
 1;
