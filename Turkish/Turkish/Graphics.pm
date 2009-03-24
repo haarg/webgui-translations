fbf33e2da38834c5a77dd55aac2a21b16f4af5c1
Translation Server <translation@plainblack.com>
2009-03-24 10:27:15 +0000

    Update from translation server

--- Turkish/Turkish/Graphics.pm
+++ Turkish/Turkish/Graphics.pm
@@ -1,117 +1,95 @@
 package WebGUI::i18n::Turkish::Graphics;
 use utf8;
-
 our $I18N = {
-	'stroke alpha' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'fill alpha' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'font name description' => {
-		message => q|Kullanıcı y&uuml;z&uuml;nde bu yazı tipi i&ccedil;in kullanılacak etiket.|,
-		lastUpdated => 1206980765
-	},
-
-	'stroke color description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'fill alpha description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'font file description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'stroke alpha description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'fill color' => {
-		message => q|Renk ile doldur|,
-		lastUpdated => 1206980126
-	},
-
-	'stroke color' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'add palette' => {
-		message => q|Yeni Palet ekle|,
-		lastUpdated => 1207006154
-	},
-
-	'palette name' => {
-		message => q|Palet ismi|,
-		lastUpdated => 1206980149
-	},
-
-	'font name' => {
-		message => q|Yazı Tipi|,
-		lastUpdated => 1206980367
-	},
-
-	'font file' => {
-		message => q|Yazı Tipi Dosyası|,
-		lastUpdated => 1207006169
-	},
-
-	'color name description' => {
-		message => q|Bu renk i&ccedil;in isim.|,
-		lastUpdated => 1206980096
-	},
-
-	'fill color description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'palette name description' => {
-		message => q|Bu paleti kullanıcı y&uuml;z&uuml;nde g&ouml;sterirken kullanılacak isim.|,
-		lastUpdated => 1206980233
-	},
-
-	'manage graphics' => {
-		message => q|Grafikler|,
-		lastUpdated => 1206980311
-	},
-
-	'color name' => {
-		message => q|Renk ismi|,
-		lastUpdated => 1206980104
-	},
-
-	'add color' => {
-		message => q|Bu palete renk ekle.|,
-		lastUpdated => 1206980002
-	},
-
-	'add font' => {
-		message => q|Yeni yazı tipi ekle|,
-		lastUpdated => 1206980041
-	},
-
-	'manage palettes' => {
-		message => q|Paletler|,
-		lastUpdated => 1206980332
-	},
-
-	'manage fonts' => {
-		message => q|Yazı Tipleri|,
-		lastUpdated => 1206980350
-	},
-
-};
+  'add color' => {
+    'lastUpdated' => '1206980002',
+    'message' => 'Bu palete renk ekle.'
+  },
+  'add font' => {
+    'lastUpdated' => '1206980041',
+    'message' => "Yeni yaz\x{131} tipi ekle"
+  },
+  'add palette' => {
+    'lastUpdated' => '1237890058',
+    'message' => 'Yeni palet ekle'
+  },
+  'color name' => {
+    'lastUpdated' => '1206980104',
+    'message' => 'Renk ismi'
+  },
+  'color name description' => {
+    'lastUpdated' => '1237890046',
+    'message' => "Bu rengin ad\x{131}."
+  },
+  'fill alpha' => {
+    'lastUpdated' => '1237890000',
+    'message' => "Saydaml\x{131}\x{11f}\x{131} doldur"
+  },
+  'fill alpha description' => {
+    'lastUpdated' => '1237890026',
+    'message' => "Dolgu renginin saydaml\x{131}\x{11f}\x{131}n\x{131} belirtir."
+  },
+  'fill color' => {
+    'lastUpdated' => '1206980126',
+    'message' => 'Renk ile doldur'
+  },
+  'fill color description' => {
+    'lastUpdated' => '1237890083',
+    'message' => 'Bu renk için dolgu rengi'
+  },
+  'font file' => {
+    'lastUpdated' => '1237890096',
+    'message' => "Yaz\x{131} tipi dosyas\x{131}"
+  },
+  'font file description' => {
+    'lastUpdated' => '1237890144',
+    'message' => "Yaz\x{131} tipini i\x{e7}eren dosya. TrueType (.ttf) yaz\x{131}tipi olmal\x{131}d\x{131}r."
+  },
+  'font name' => {
+    'lastUpdated' => '1237890105',
+    'message' => "Yaz\x{131} tipi ad\x{131}"
+  },
+  'font name description' => {
+    'lastUpdated' => '1237890379',
+    'message' => "Kullan\x{131}c\x{131} aray\x{fc}z\x{fc}nde bu yaz\x{131} tipi i\x{e7}in kullan\x{131}lacak etiket."
+  },
+  'manage fonts' => {
+    'lastUpdated' => '1206980350',
+    'message' => "Yaz\x{131} Tipleri"
+  },
+  'manage graphics' => {
+    'lastUpdated' => '1206980311',
+    'message' => 'Grafikler'
+  },
+  'manage palettes' => {
+    'lastUpdated' => '1206980332',
+    'message' => 'Paletler'
+  },
+  'palette name' => {
+    'lastUpdated' => '1206980149',
+    'message' => 'Palet ismi'
+  },
+  'palette name description' => {
+    'lastUpdated' => '1237890355',
+    'message' => "Bu paleti kullan\x{131}c\x{131} aray\x{fc}z\x{fc}nde g\x{f6}sterirken kullan\x{131}lacak etiket."
+  },
+  'stroke alpha' => {
+    'lastUpdated' => '1237890257',
+    'message' => "F\x{131}r\x{e7}an\x{131}n saydaml\x{131}\x{11f}\x{131}"
+  },
+  'stroke alpha description' => {
+    'lastUpdated' => '1237890269',
+    'message' => "F\x{131}r\x{e7}a\x{a0}renginin saydaml\x{131}\x{11f}\x{131}n\x{131} tan\x{131}mlar"
+  },
+  'stroke color' => {
+    'lastUpdated' => '1237890394',
+    'message' => "F\x{131}r\x{e7}a rengi"
+  },
+  'stroke color description' => {
+    'lastUpdated' => '1237890426',
+    'message' => "Bu renk i\x{e7}in f\x{131}r\x{e7}a rengi."
+  }
+}
+;
 
 1;
