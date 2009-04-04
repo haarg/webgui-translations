246445d1530a7d3d4a88b24acf7263c66180d505
Translation Server <translation@plainblack.com>
2009-04-04 21:24:47 +0000

    Update from translation server

--- German/German/Asset_Sku.pm
+++ German/German/Asset_Sku.pm
@@ -2,75 +2,83 @@ package WebGUI::i18n::German::Asset_Sku;
 use utf8;
 our $I18N = {
   'Add a Variant' => {
-    'lastUpdated' => 1226873073,
-    'message' => "Variante hinzuf\x{fc}gen"
+    'lastUpdated' => '1226873073',
+    'message' => 'Variante hinzufügen'
   },
   'Set Product price and SKU' => {
-    'lastUpdated' => 1228689360,
+    'lastUpdated' => '1228689360',
     'message' => 'Atikelpreis und -nummer festlegen'
   },
+  'add to cart' => {
+    'lastUpdated' => '1238880191',
+    'message' => 'Zum Warenkorb hinzufügen'
+  },
   'assetName' => {
-    'lastUpdated' => 1226014917,
+    'lastUpdated' => '1226014917',
     'message' => 'Artikelnummer'
   },
   'cancel recurring message' => {
-    'lastUpdated' => 1228689439,
+    'lastUpdated' => '1228689439',
     'message' => 'Eine wiederkehrende Transaktion %s mit einem Artikel mit Bezeichnung %s für einen Benutzer mit Namen %s wurde abgebrochen.'
   },
   'description' => {
-    'lastUpdated' => 1226014879,
+    'lastUpdated' => '1226014879',
     'message' => 'Beschreibung'
   },
   'description help' => {
-    'lastUpdated' => 1226015029,
+    'lastUpdated' => '1226015029',
     'message' => 'Beschreibung für den Artikel oder die Dienstleistung'
   },
   'display title' => {
-    'lastUpdated' => 1226014892,
+    'lastUpdated' => '1226014892',
     'message' => 'Titel anzeigen'
   },
   'display title help' => {
-    'lastUpdated' => 1226873127,
+    'lastUpdated' => '1226873127',
     'message' => 'Geben Sie an ob der Titel angezeigt werden soll.'
   },
   'override tax rate' => {
-    'lastUpdated' => 1228689454,
+    'lastUpdated' => '1228689454',
     'message' => 'Steuersatz überschreiben?'
   },
   'override tax rate help' => {
-    'lastUpdated' => 1228689545,
+    'lastUpdated' => '1228689545',
     'message' => 'Wöchten Sie den Standardsteuersatz für diesen Artikel &uuml;berschreiben? Wird gew&ouml;hnlich in Gebieten genutzt, die einen besonderen oder keinen Steuersatz auf Grundbedarfsartikel wie Essen oder Kleidung haben.'
   },
+  'see more' => {
+    'lastUpdated' => '1238880229',
+    'message' => 'mehr...'
+  },
   'shop' => {
-    'lastUpdated' => 1226014934,
+    'lastUpdated' => '1226014934',
     'message' => 'Shop'
   },
   'sku' => {
-    'lastUpdated' => 1226014907,
+    'lastUpdated' => '1226014907',
     'message' => 'Artikelnummer'
   },
   'sku help' => {
-    'lastUpdated' => 1228689563,
+    'lastUpdated' => '1228689563',
     'message' => 'Artikelnummer'
   },
   'sku properties title' => {
-    'lastUpdated' => 1228689580,
+    'lastUpdated' => '1228689580',
     'message' => 'Eigenschaften der Artikelnummer'
   },
   'tax rate override' => {
-    'lastUpdated' => 1228689592,
+    'lastUpdated' => '1228689592',
     'message' => 'Steuersatz üBerschreiben'
   },
   'tax rate override help' => {
-    'lastUpdated' => 1228689651,
+    'lastUpdated' => '1228689651',
     'message' => 'Welcher neue Prozentwert soll benutzt werden um die Steuer dieses Artikels zu berechnen.'
   },
   'vendor' => {
-    'lastUpdated' => 1226014990,
+    'lastUpdated' => '1226014990',
     'message' => 'Verkäufer'
   },
   'vendor help' => {
-    'lastUpdated' => 1226873235,
+    'lastUpdated' => '1226873235',
     'message' => 'Falls vorhanden, welcher Person oder Firma, die im Verkaufssystem definiert wurde, soll den Verkauf angerechnet werden?'
   }
 }
