b852ec77dfe43f3a6225825691c53d98cad6dee7
Translation Server <translation@plainblack.com>
2009-04-04 21:38:51 +0000

    Update from translation server

--- German/German/Asset_Shelf.pm
+++ German/German/Asset_Shelf.pm
@@ -1,48 +1,57 @@
 package WebGUI::i18n::German::Asset_Shelf;
 use utf8;
 our $I18N = {
+  'addToCartForm' => {
+    'lastUpdated' => '1238881121',
+    'message' => 'Wenn dieses Produkt es unterstützt, das Formular zum Aufnehmen in den Warenkorb.
+Es enthält einen Knopf und alle weiter notwendigen Elemente dazu.'
+  },
   'assetName' => {
-    'lastUpdated' => 1228688960,
+    'lastUpdated' => '1228688960',
     'message' => 'Regal'
   },
   'export' => {
-    'lastUpdated' => 1226014779,
+    'lastUpdated' => '1226014779',
     'message' => 'Produkte exportieren'
   },
   'import' => {
-    'lastUpdated' => 1226014792,
+    'lastUpdated' => '1226014792',
     'message' => 'Produkte importieren'
   },
   'import successful' => {
-    'lastUpdated' => 1228688927,
+    'lastUpdated' => '1228688927',
     'message' => 'Ihre Produkte wurden importiert'
   },
   'price' => {
-    'lastUpdated' => 1226014842,
+    'lastUpdated' => '1226014842',
     'message' => 'Der Preis für diesen Artikel'
   },
+  'product_url' => {
+    'lastUpdated' => '1238880970',
+    'message' => 'Die Url zu dieser Artikelnummer'
+  },
   'products' => {
-    'lastUpdated' => 1228689124,
+    'lastUpdated' => '1228689124',
     'message' => '<p>Eine Schleife, die eine Liste der Produkte enthält, die den Schlüsselworten entsprechen, die f&uuml;r dieses Regal festgelegt wurden. Jeder Eintrag in der Schleife enth&auml;lt alle Eigenschaften der passenden Artikelnummer zuz&uuml;glich der folgenden Variablen.</p>'
   },
   'shelf template' => {
-    'lastUpdated' => 1228689139,
+    'lastUpdated' => '1228689139',
     'message' => 'Regal-Vorlage'
   },
   'shelf template help' => {
-    'lastUpdated' => 1228689182,
+    'lastUpdated' => '1228689182',
     'message' => 'Wählen Sie die Vorlage, die die Liste der Produkte anzeigt, die diesem Regal zugeordnet sind.'
   },
   'shelves' => {
-    'lastUpdated' => 1228689245,
+    'lastUpdated' => '1228689245',
     'message' => 'Eine Schleife, die eine Liste der Regale enthält, die Kinder dieser im Asset-Baum sind. Jeder Eintrag der Schleife einh&auml;lt alle Eigenschaften des Regals.'
   },
   'subcategories' => {
-    'lastUpdated' => 1228689254,
+    'lastUpdated' => '1228689254',
     'message' => 'Unterkategorien'
   },
   'thumbnailUrl' => {
-    'lastUpdated' => 1228689305,
+    'lastUpdated' => '1228689305',
     'message' => 'Der URL zum einem Vorschaubild für diese Artikelnummer.&nbsp; Wenn es kein Vorschaubild gibt, ist diese Variable leer.'
   }
 }
