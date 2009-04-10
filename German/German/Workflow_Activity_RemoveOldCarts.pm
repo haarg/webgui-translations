21f5e82e207768f7f29c3e94f0379d76410ac0a6
Translation Server <translation@plainblack.com>
2009-04-10 22:30:19 +0000

    Update from translation server

--- German/German/Workflow_Activity_RemoveOldCarts.pm
+++ German/German/Workflow_Activity_RemoveOldCarts.pm
@@ -1,5 +1,19 @@
 package WebGUI::i18n::German::Workflow_Activity_RemoveOldCarts;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'activityName' => {
+    'lastUpdated' => '1239402552',
+    'message' => 'Alte Warenkörbe entfernen'
+  },
+  'cart timeout' => {
+    'lastUpdated' => '1239402570',
+    'message' => 'Timeout Warenkorb'
+  },
+  'cart timeout help' => {
+    'lastUpdated' => '1239402608',
+    'message' => 'Wie lange dürfen Warenkörbe bestehen, bevor sie gelöscht werden?'
+  }
+}
+;
 
 1;
