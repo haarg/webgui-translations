cc53c85ce750b6fa7d3fb3b091ebbb52b2a3bcad
Translation Server <translation@plainblack.com>
2009-04-10 22:31:35 +0000

    Update from translation server

--- German/German/Asset_FlatDiscount.pm
+++ German/German/Asset_FlatDiscount.pm
@@ -2,11 +2,11 @@ package WebGUI::i18n::German::Asset_FlatDiscount;
 use utf8;
 our $I18N = {
   'add to cart' => {
-    'lastUpdated' => 1226016790,
-    'message' => 'In den Einkaufswagen legen'
+    'lastUpdated' => '1239402679',
+    'message' => 'In den Warenkorb legen'
   },
   'template' => {
-    'lastUpdated' => 1226016811,
+    'lastUpdated' => '1226016811',
     'message' => 'Vorlage'
   }
 }
