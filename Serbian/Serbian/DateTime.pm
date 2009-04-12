fc08bcd5f21b3e265c347b6a4b8bc53191dca5cf
Translation Server <translation@plainblack.com>
2009-04-12 20:53:19 +0000

    Update from translation server

--- Serbian/Serbian/DateTime.pm
+++ Serbian/Serbian/DateTime.pm
@@ -1,6 +1,87 @@
 package WebGUI::i18n::Serbian::DateTime;
 use utf8;
-
-our $I18N = {};
+our $I18N = {
+  'april' => {
+    'lastUpdated' => '1239569581',
+    'message' => 'april'
+  },
+  'august' => {
+    'lastUpdated' => '1239569572',
+    'message' => 'avgust'
+  },
+  'december' => {
+    'lastUpdated' => '1239569380',
+    'message' => 'decembar'
+  },
+  'february' => {
+    'lastUpdated' => '1239569388',
+    'message' => 'februar'
+  },
+  'friday' => {
+    'lastUpdated' => '1239569400',
+    'message' => 'petak'
+  },
+  'january' => {
+    'lastUpdated' => '1239569411',
+    'message' => 'januar'
+  },
+  'july' => {
+    'lastUpdated' => '1239569420',
+    'message' => 'jul'
+  },
+  'june' => {
+    'lastUpdated' => '1239569432',
+    'message' => 'jun'
+  },
+  'march' => {
+    'lastUpdated' => '1239569442',
+    'message' => 'mart'
+  },
+  'may' => {
+    'lastUpdated' => '1239569453',
+    'message' => 'maj'
+  },
+  'monday' => {
+    'lastUpdated' => '1239569465',
+    'message' => 'ponedeljak'
+  },
+  'november' => {
+    'lastUpdated' => '1239569475',
+    'message' => 'novembar'
+  },
+  'october' => {
+    'lastUpdated' => '1239569486',
+    'message' => 'oktobar'
+  },
+  'saturday' => {
+    'lastUpdated' => '1239569496',
+    'message' => 'subota'
+  },
+  'september' => {
+    'lastUpdated' => '1239569506',
+    'message' => 'septembar'
+  },
+  'sunday' => {
+    'lastUpdated' => '1239569518',
+    'message' => 'nedelja'
+  },
+  'thursday' => {
+    'lastUpdated' => '1239569532',
+    'message' => "\x{10d}etvrtak"
+  },
+  'timezone' => {
+    'lastUpdated' => '1239569561',
+    'message' => 'vremenska zona'
+  },
+  'tuesday' => {
+    'lastUpdated' => '1239569543',
+    'message' => 'utorak'
+  },
+  'wednesday' => {
+    'lastUpdated' => '1239569551',
+    'message' => 'sreda'
+  }
+}
+;
 
 1;
