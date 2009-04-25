52ba7e66cf9906a5030d734d999e747e9cbe19aa
Translation Server <translation@plainblack.com>
2009-04-25 22:27:31 +0000

    Update from translation server

--- German/German/Asset_ThingyRecord.pm
+++ German/German/Asset_ThingyRecord.pm
@@ -1,5 +1,39 @@
 package WebGUI::i18n::German::Asset_ThingyRecord;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'assetName' => {
+    'lastUpdated' => '1240698096',
+    'message' => 'Tabelleneintrag'
+  },
+  'duration description' => {
+    'lastUpdated' => '1240698142',
+    'message' => 'Dauer der Gültigkeit eines Tabelleneintrags'
+  },
+  'renew' => {
+    'lastUpdated' => '1240698263',
+    'message' => 'Verlängern'
+  },
+  'renewal added to cart' => {
+    'lastUpdated' => '1240698251',
+    'message' => 'Die Verlängerung wurde zum Warenkorb hinzugefügt '
+  },
+  'saved' => {
+    'lastUpdated' => '1240698274',
+    'message' => 'Gesichert!'
+  },
+  'thingFields label' => {
+    'lastUpdated' => '1240698438',
+    'message' => 'Felder zum Hinzufügen'
+  },
+  'thingId description' => {
+    'lastUpdated' => '1240698413',
+    'message' => 'Die Tabelle, wofür ein Eintrag gekauft werden kann.'
+  },
+  'thingId label' => {
+    'lastUpdated' => '1240698335',
+    'message' => 'Zur Tabelle hinzufügen'
+  }
+}
+;
 
 1;
