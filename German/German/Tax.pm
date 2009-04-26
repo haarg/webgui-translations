938f7b498f786ad192cd1ada901f2d76784f0eea
Translation Server <translation@plainblack.com>
2009-04-26 14:44:56 +0000

    Update from translation server

--- German/German/Tax.pm
+++ German/German/Tax.pm
@@ -2,52 +2,69 @@ package WebGUI::i18n::German::Tax;
 use utf8;
 our $I18N = {
   'add a tax' => {
-    'lastUpdated' => 1225319631,
-    'message' => 'Stuerinformation hinzufügen'
+    'lastUpdated' => '1240756890',
+    'message' => 'Steuerinformation hinzufügen'
   },
   'city' => {
-    'lastUpdated' => 1225319638,
+    'lastUpdated' => '1225319638',
     'message' => 'Ort'
   },
   'city help' => {
-    'lastUpdated' => 1225319660,
+    'lastUpdated' => '1225319660',
     'message' => 'Ort, wofür die Steuer gilt'
   },
   'code' => {
-    'lastUpdated' => 1225319675,
+    'lastUpdated' => '1225319675',
     'message' => 'Postleitzahl'
   },
   'code help' => {
-    'lastUpdated' => 1225319741,
+    'lastUpdated' => '1225319741',
     'message' => 'Postleitzahl wofür die Steuer g&uuml;ltig ist'
   },
   'country' => {
-    'lastUpdated' => 1225319750,
+    'lastUpdated' => '1225319750',
     'message' => 'Land'
   },
   'country help' => {
-    'lastUpdated' => 1225319768,
+    'lastUpdated' => '1225319768',
     'message' => 'Land wofür die Steuer g&uuml;ltig ist'
   },
   'delete' => {
-    'lastUpdated' => 1225319795,
+    'lastUpdated' => '1225319795',
     'message' => 'Löschen'
   },
+  'override tax rate' => {
+    'lastUpdated' => '1240756875',
+    'message' => 'Steuersatz überschreiben?'
+  },
+  'override tax rate help' => {
+    'lastUpdated' => '1240757076',
+    'message' => 'Möchten Sie den Standard Steuersatz für diesen Artikel überschreiben?
+Normalerweise ist dies denkbar bei z.B. Essensware, Bücher usw. '
+  },
   'state' => {
-    'lastUpdated' => 1225319809,
+    'lastUpdated' => '1225319809',
     'message' => 'Bundesland'
   },
   'state help' => {
-    'lastUpdated' => 1225319835,
+    'lastUpdated' => '1225319835',
     'message' => 'Bundesland, wofür die Steuer g&uuml;ltig ist.'
   },
   'tax rate' => {
-    'lastUpdated' => 1225319846,
+    'lastUpdated' => '1225319846',
     'message' => 'Steuersatz'
   },
   'tax rate help' => {
-    'lastUpdated' => 1225319908,
+    'lastUpdated' => '1225319908',
     'message' => 'Geben Sie den Steuersatz in Prozent ein. Z.B.: 19% = 19.'
+  },
+  'tax rate override' => {
+    'lastUpdated' => '1240756920',
+    'message' => 'korrigierter Steuersatz'
+  },
+  'tax rate override help' => {
+    'lastUpdated' => '1240756965',
+    'message' => 'Welcher korrigierten Steuersatz soll für diesen Artikel verwendet werden?'
   }
 }
 ;
