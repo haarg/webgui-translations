c04fae65f51c807007c2f7b8b7b193b9775126d3
Translation Server <translation@plainblack.com>
2009-04-26 14:20:24 +0000

    Update from translation server

--- German/German/Asset_FlatDiscount.pm
+++ German/German/Asset_FlatDiscount.pm
@@ -5,9 +5,85 @@ our $I18N = {
     'lastUpdated' => '1239402679',
     'message' => 'In den Warenkorb legen'
   },
+  'addToCartButton' => {
+    'lastUpdated' => '1240754493',
+    'message' => 'In den Warenkorb'
+  },
+  'alreadyHasCoupon' => {
+    'lastUpdated' => '1240754771',
+    'message' => 'Benutzer hat diesen Coupon bereits im Warenkorb'
+  },
+  'assetName' => {
+    'lastUpdated' => '1240755236',
+    'message' => 'Nachlass Coupon'
+  },
+  'continueShoppingUrl' => {
+    'lastUpdated' => '1240754830',
+    'message' => 'Url zum Reset des Produkts, damit der Benutzer weiter einkaufen kann.'
+  },
+  'default thank you message' => {
+    'lastUpdated' => '1240754863',
+    'message' => 'Der Coupon wurde in den Warenkorb aufgenommen.'
+  },
+  'flat discount coupon template' => {
+    'lastUpdated' => '1240755222',
+    'message' => 'Vorlage Nachlass-Coupon'
+  },
+  'flat discount coupon template help' => {
+    'lastUpdated' => '1240755292',
+    'message' => 'Die folgende Variablen sind bei diesem Asset verfügbar.'
+  },
+  'formFooter' => {
+    'lastUpdated' => '1240755315',
+    'message' => 'Abschluss des Formulars'
+  },
+  'formHeader' => {
+    'lastUpdated' => '1240755327',
+    'message' => 'Formularkopf'
+  },
+  'hasAddedToCart' => {
+    'lastUpdated' => '1240755424',
+    'message' => 'Eine Bedingung, die anzeigt, dass der Benutzer das Produkt in den Warenkorb gelegt hat und eine Danke-Nachricht angezeigt werden kann.'
+  },
+  'must spend' => {
+    'lastUpdated' => '1240755609',
+    'message' => 'Mindesteinkaufswert'
+  },
+  'must spend help' => {
+    'lastUpdated' => '1240755597',
+    'message' => 'Wieviel soll ein Besucher bei dieser Transaction aufwenden um den Nachlasscoupon verwenden zu können.'
+  },
+  'percentage discount' => {
+    'lastUpdated' => '1240754904',
+    'message' => 'Nachlass in Prozent'
+  },
+  'percentage discount help' => {
+    'lastUpdated' => '1240754992',
+    'message' => 'Wieviel Prozent Nachlass wird mit diesem Coupon gewährt?'
+  },
+  'price discount' => {
+    'lastUpdated' => '1240754945',
+    'message' => 'Preisnachlass'
+  },
+  'price discount help' => {
+    'lastUpdated' => '1240755044',
+    'message' => 'Welchen Betrag soll mit diesem Coupon abgezogen werden?'
+  },
   'template' => {
     'lastUpdated' => '1226016811',
     'message' => 'Vorlage'
+  },
+  'template help' => {
+    'lastUpdated' => '1240755172',
+    'message' => 'Wählen Sie eine Vorlage für die Anzeige dieses Coupons'
+  },
+  'thank you message' => {
+    'lastUpdated' => '1240755073',
+    'message' => 'Danke für Ihren Einkauf!'
+  },
+  'thank you message help' => {
+    'lastUpdated' => '1240755139',
+    'message' => 'Schreiben Sie eine Nachricht zum Abschluss. Das kann ein Dankeschön sein, aber auch, dass etwas in den Warenkorb gelegt wurde.'
   }
 }
 ;
