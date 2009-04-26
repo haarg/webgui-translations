42d3d7a3554ced1c26e635656471038669401c03
Translation Server <translation@plainblack.com>
2009-04-26 10:26:09 +0000

    Update from translation server

--- German/German/Asset_Subscription.pm
+++ German/German/Asset_Subscription.pm
@@ -53,9 +53,45 @@ our $I18N = {
     'lastUpdated' => '1226015094',
     'message' => 'Alle anzeigen'
   },
+  'generate batch' => {
+    'lastUpdated' => '1240741056',
+    'message' => 'Codeserie generieren'
+  },
   'list codes in batch' => {
     'lastUpdated' => '1240740803',
     'message' => 'Auflisten der Codes in dieser Serie'
+  },
+  'manage batches' => {
+    'lastUpdated' => '1240741528',
+    'message' => 'Codeserien verwalten'
+  },
+  'manage codes' => {
+    'lastUpdated' => '1240741560',
+    'message' => 'Abonnementscodes verwalten'
+  },
+  'noc' => {
+    'lastUpdated' => '1240741505',
+    'message' => 'Anzahl der Abonnementscodes in der Serie'
+  },
+  'price' => {
+    'lastUpdated' => '1240741414',
+    'message' => 'Abonnementspreis (####.##)'
+  },
+  'purchase button' => {
+    'lastUpdated' => '1240741452',
+    'message' => 'In den Warenkorb legen'
+  },
+  'purchaseButton' => {
+    'lastUpdated' => '1240741477',
+    'message' => 'Kaufen'
+  },
+  'recurring subscription' => {
+    'lastUpdated' => '1240741372',
+    'message' => 'Automatische Verlängerung?'
+  },
+  'recurring subscription description' => {
+    'lastUpdated' => '1240741341',
+    'message' => 'Wenn "Ja", wird der Kunde nach Ablauf der Abonnementszeit belastet und das Abonnement wird erneuert. Wenn "Nein", läuft das Abonnement automatisch ab.'
   }
 }
 ;
