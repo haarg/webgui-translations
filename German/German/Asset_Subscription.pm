5c0fc86bfb64dcd103507b9465167f26171f173a
Translation Server <translation@plainblack.com>
2009-04-26 10:13:36 +0000

    Update from translation server

--- German/German/Asset_Subscription.pm
+++ German/German/Asset_Subscription.pm
@@ -1,17 +1,61 @@
 package WebGUI::i18n::German::Asset_Subscription;
 use utf8;
 our $I18N = {
+  'add subscription' => {
+    'lastUpdated' => '1240740261',
+    'message' => 'Neues Abonnement hinzufügen'
+  },
   'and' => {
-    'lastUpdated' => 1226015077,
+    'lastUpdated' => '1226015077',
     'message' => 'und'
   },
+  'assetName' => {
+    'lastUpdated' => '1240740271',
+    'message' => 'Abonnement'
+  },
+  'association' => {
+    'lastUpdated' => '1240740295',
+    'message' => 'Mit Abonnement verbinden'
+  },
+  'batch description' => {
+    'lastUpdated' => '1240740441',
+    'message' => 'Codeserie Beschreibung'
+  },
+  'batch id' => {
+    'lastUpdated' => '1240740396',
+    'message' => 'CodesatzId'
+  },
+  'batch name' => {
+    'lastUpdated' => '1240740426',
+    'message' => 'Name der Codeserie'
+  },
+  'batchDescription' => {
+    'lastUpdated' => '1240740538',
+    'message' => 'Die Beschreibung der Codeserie, die mit dem Abonnementscode zusammenhängt.'
+  },
   'code' => {
-    'lastUpdated' => 1226019788,
-    'message' => 'Kode'
+    'lastUpdated' => '1240740240',
+    'message' => 'Code'
+  },
+  'code description' => {
+    'lastUpdated' => '1240740623',
+    'message' => 'Der Abonnementscode, den Sie einlösen möchten'
+  },
+  'code length' => {
+    'lastUpdated' => '1240740682',
+    'message' => 'Länge Abonnementscode'
+  },
+  'codes expire' => {
+    'lastUpdated' => '1240740765',
+    'message' => 'Codes ungültig ab'
   },
   'display all' => {
-    'lastUpdated' => 1226015094,
+    'lastUpdated' => '1226015094',
     'message' => 'Alle anzeigen'
+  },
+  'list codes in batch' => {
+    'lastUpdated' => '1240740803',
+    'message' => 'Auflisten der Codes in dieser Serie'
   }
 }
 ;
