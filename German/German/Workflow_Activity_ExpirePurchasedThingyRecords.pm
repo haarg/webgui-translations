0b393f325d811a427c176223251ddfc288dd123f
Translation Server <translation@plainblack.com>
2009-04-26 14:56:33 +0000

    Update from translation server

--- German/German/Workflow_Activity_ExpirePurchasedThingyRecords.pm
+++ German/German/Workflow_Activity_ExpirePurchasedThingyRecords.pm
@@ -1,5 +1,43 @@
 package WebGUI::i18n::German::Workflow_Activity_ExpirePurchasedThingyRecords;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'default notification' => {
+    'lastUpdated' => '1240757240',
+    'message' => 'Ihr Abonnemnt läuft bald ab!'
+  },
+  'default notification subject' => {
+    'lastUpdated' => '1240757266',
+    'message' => 'Wichtige Mitteilung zu Ihrem Abonnement'
+  },
+  'notificationMessage description' => {
+    'lastUpdated' => '1240757312',
+    'message' => 'Der Nachrichtentext zur Mitteilung'
+  },
+  'notificationMessage label' => {
+    'lastUpdated' => '1240757462',
+    'message' => 'Benachrichtigungstext'
+  },
+  'notificationOffset description' => {
+    'lastUpdated' => '1240757684',
+    'message' => 'Die Vorlaufszeit vor Ablauf des Tabelleneintrags, wenn die Benachrichtigung verschickt wird.'
+  },
+  'notificationOffset label' => {
+    'lastUpdated' => '1240757626',
+    'message' => 'Benachrichtigung Vorlauf'
+  },
+  'notificationSubject description' => {
+    'lastUpdated' => '1240757708',
+    'message' => 'Betreff der Benachrichtigung'
+  },
+  'notificationSubject label' => {
+    'lastUpdated' => '1240757718',
+    'message' => 'Betreff'
+  },
+  'topicName' => {
+    'lastUpdated' => '1240757783',
+    'message' => 'Ablauf Einträge zum Kauf '
+  }
+}
+;
 
 1;
