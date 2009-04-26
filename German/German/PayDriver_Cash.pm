e932234027d508555913dfb37be26ce2352c70dd
Translation Server <translation@plainblack.com>
2009-04-26 21:41:59 +0000

    Update from translation server

--- German/German/PayDriver_Cash.pm
+++ German/German/PayDriver_Cash.pm
@@ -1,7 +1,83 @@
 package WebGUI::i18n::German::PayDriver_Cash;
 use utf8;
-
 our $I18N = {
-};
+  'address' => {
+    'lastUpdated' => '1240781768',
+    'message' => 'Adresse'
+  },
+  'cardNumber' => {
+    'lastUpdated' => '1240781786',
+    'message' => 'Kreditkartennummer'
+  },
+  'cash' => {
+    'lastUpdated' => '1240782109',
+    'message' => 'Barzahlung'
+  },
+  'check' => {
+    'lastUpdated' => '1240781821',
+    'message' => 'Scheck'
+  },
+  'city' => {
+    'lastUpdated' => '1240781831',
+    'message' => 'Ort'
+  },
+  'complete transaction' => {
+    'lastUpdated' => '1240781878',
+    'message' => 'Transaktion mit Abschicken abschließen?'
+  },
+  'country' => {
+    'lastUpdated' => '1240781890',
+    'message' => 'Land'
+  },
+  'cvv2' => {
+    'lastUpdated' => '1240781911',
+    'message' => 'Verifizierungsnummer (CVV2)'
+  },
+  'email' => {
+    'lastUpdated' => '1240781919',
+    'message' => 'Email'
+  },
+  'emailMessage' => {
+    'lastUpdated' => '1240781928',
+    'message' => 'Emailnachricht'
+  },
+  'expiration date' => {
+    'lastUpdated' => '1240781938',
+    'message' => 'Ablaufdatum'
+  },
+  'firstName' => {
+    'lastUpdated' => '1240781948',
+    'message' => 'Vorname'
+  },
+  'invalid address' => {
+    'lastUpdated' => '1240781972',
+    'message' => 'Sie müssen eine gültige Adresse angeben.'
+  },
+  'invalid city' => {
+    'lastUpdated' => '1240781991',
+    'message' => 'Sie müssen einen gültigen Ort angeben.'
+  },
+  'invalid email' => {
+    'lastUpdated' => '1240782011',
+    'message' => 'Sie müssen eine gültige Emailadresse angeben.'
+  },
+  'invalid firstName' => {
+    'lastUpdated' => '1240782041',
+    'message' => 'Geben Sie bitte Ihren Vornamen ein.'
+  },
+  'invalid lastName' => {
+    'lastUpdated' => '1240782060',
+    'message' => 'Geben Sie bitte Ihren Nachnamen ein.'
+  },
+  'invalid zip' => {
+    'lastUpdated' => '1240782075',
+    'message' => 'Ungültige Postleitzahl.'
+  },
+  'label' => {
+    'lastUpdated' => '1240782097',
+    'message' => 'Barzahlung'
+  }
+}
+;
 
 1;
