5a213d566d9410befca8b52fbbc1b34f952936a7
Translation Server <translation@plainblack.com>
2009-04-26 20:47:10 +0000

    Update from translation server

--- German/German/Asset_Donation.pm
+++ German/German/Asset_Donation.pm
@@ -2,67 +2,67 @@ package WebGUI::i18n::German::Asset_Donation;
 use utf8;
 our $I18N = {
   'assetName' => {
-    'lastUpdated' => 1217229149,
+    'lastUpdated' => '1217229149',
     'message' => 'Spende'
   },
   'continueShoppingUrl' => {
-    'lastUpdated' => 1226014540,
+    'lastUpdated' => '1226014540',
     'message' => 'Ein URL um das Produkt zurückzusetzen, so dass der Einkauf fortgesetzt werden kann.'
   },
   'default price' => {
-    'lastUpdated' => 1225635093,
+    'lastUpdated' => '1225635093',
     'message' => 'Standardpreis'
   },
   'default price help' => {
-    'lastUpdated' => 1226013228,
+    'lastUpdated' => '1226013228',
     'message' => '<p>Um welchen Betrag bitten Sie den Benutzer? Hinweis: Der Nutzer kann jeden beliebigen Betrag eingeben, dieser Wert ist nur ein Vorschlag.</p>'
   },
   'default thank you message' => {
-    'lastUpdated' => 1217229449,
+    'lastUpdated' => '1217229449',
     'message' => 'Dankeschön für Ihre Spende.'
   },
   'donate button' => {
-    'lastUpdated' => 1226014225,
-    'message' => 'Spende in den Einkaufswagen legen'
+    'lastUpdated' => '1240778791',
+    'message' => 'In den Warenkorb'
   },
   'donate template' => {
-    'lastUpdated' => 1226014242,
+    'lastUpdated' => '1226014242',
     'message' => 'Template für Spende'
   },
   'donate template help' => {
-    'lastUpdated' => 1226014298,
+    'lastUpdated' => '1226014298',
     'message' => 'Wählen Sie eine Vorlage, die für die Anzeige des Spende-Asset genutzt werden soll.'
   },
   'donateButton' => {
-    'lastUpdated' => 1217229381,
+    'lastUpdated' => '1217229381',
     'message' => 'Die Schaltfläche für das Spendenformular.'
   },
   'donation template help' => {
-    'lastUpdated' => 1226014320,
+    'lastUpdated' => '1226014320',
     'message' => 'Template für Spende-Asset'
   },
   'formFooter' => {
-    'lastUpdated' => 1217229361,
+    'lastUpdated' => '1217229361',
     'message' => 'Das Ende des Spendenformulars.'
   },
   'formHeader' => {
-    'lastUpdated' => 1217229347,
+    'lastUpdated' => '1217229347',
     'message' => 'Der Anfang des Spendenformulars.'
   },
   'hasAddedToCart' => {
-    'lastUpdated' => 1226014503,
+    'lastUpdated' => '1226014503',
     'message' => '<p>Die Bedingung, die anzeigt, dass der Nutzer seine Spende in den Einkaufwagen gelegt hat und nun die Dankeschön-Nachtricht angezeigt werden soll.</p>'
   },
   'priceField' => {
-    'lastUpdated' => 1217229314,
+    'lastUpdated' => '1217229314',
     'message' => 'Das Feld im Spendenformular, in das der Benutzer den zu spendenden Betrag eingibt.'
   },
   'thank you message' => {
-    'lastUpdated' => 1217229270,
+    'lastUpdated' => '1217229270',
     'message' => 'Dankeschön Nachricht'
   },
   'thank you message help' => {
-    'lastUpdated' => 1226014423,
+    'lastUpdated' => '1226014423',
     'message' => '<p>Geben Sie hier eine Dankeschön-Nechricht ein, die dem Spender angezeigt wird. Beachten Sie: Die Spende wurde bisher nur in den Einkaufswagen gelegt, dieser aber noch nicht bezahlt.</p>'
   }
 }
