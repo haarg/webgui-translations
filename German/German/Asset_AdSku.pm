21219ec5d1c2b8d71a905df70289420498482826
Translation Server <translation@plainblack.com>
2009-04-28 01:07:50 +0000

    Update from translation server

--- German/German/Asset_AdSku.pm
+++ German/German/Asset_AdSku.pm
@@ -4,6 +4,18 @@ our $I18N = {
   'assetName' => {
     'lastUpdated' => '1240496353',
     'message' => 'Inserat'
+  },
+  'form added to cart thanks' => {
+    'lastUpdated' => '1240880695',
+    'message' => 'Danke für Ihren Einkauf'
+  },
+  'form error min clicks' => {
+    'lastUpdated' => '1240880761',
+    'message' => 'Sie müssen mindestens %d Clicks für diesen Anzeigenraum kaufen.'
+  },
+  'form manage title' => {
+    'lastUpdated' => '1240880833',
+    'message' => 'Meine Inserate verwalten'
   }
 }
 ;
