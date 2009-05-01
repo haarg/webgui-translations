acd73be106c6155957edc7a4ded2fcd37656d4a0
Translation Server <translation@plainblack.com>
2009-05-01 11:57:05 +0000

    Update from translation server

--- French/French/Account_Contributions.pm
+++ French/French/Account_Contributions.pm
@@ -12,6 +12,34 @@ our $I18N = {
   'contrib layout template label' => {
     'lastUpdated' => '1241167374',
     'message' => 'Gabarit de présentation.'
+  },
+  'contrib style template hoverHelp' => {
+    'lastUpdated' => '1241176815',
+    'message' => 'Sélectionner un gabarit de style dans la liste, qui contiendra l\'onglet des contributions.'
+  },
+  'contrib style template label' => {
+    'lastUpdated' => '1241176830',
+    'message' => 'Gabarit de style.'
+  },
+  'contrib view template hoverHelp' => {
+    'lastUpdated' => '1241176872',
+    'message' => 'Choisir le gabarit pour afficher les contributions de l\'utilisateur.'
+  },
+  'contrib view template label' => {
+    'lastUpdated' => '1241176893',
+    'message' => 'Afficher le gabarit des contributions..'
+  },
+  'contribution count' => {
+    'lastUpdated' => '1241176910',
+    'message' => 'Nombre de contributions.'
+  },
+  'contributions_loop' => {
+    'lastUpdated' => '1241176939',
+    'message' => 'Une boucle contenant des informations sur toutes les contributions de l\'utilisateur.'
+  },
+  'contributions_total' => {
+    'lastUpdated' => '1241176965',
+    'message' => 'Le total des contributions de l\'utilisateur.'
   }
 }
 ;
--- French/French/Account_Friends.pm
+++ French/French/Account_Friends.pm
@@ -25,6 +25,26 @@ our $I18N = {
     'lastUpdated' => '1241175620',
     'message' => 'retour aux profils utilisateur'
   },
+  'cancel button label' => {
+    'lastUpdated' => '1241178250',
+    'message' => 'Annuler'
+  },
+  'default friend comments' => {
+    'lastUpdated' => '1241178399',
+    'message' => '%S, j\'aimerais que tu rejoignes mon réseau d\'amis. Merci, %s'
+  },
+  'does not want to be a friend' => {
+    'lastUpdated' => '1241178422',
+    'message' => 'Cet utilisateur préfère ne pas être ajouté à vos amis.'
+  },
+  'edit my friends' => {
+    'lastUpdated' => '1241178430',
+    'message' => 'editer mes amis'
+  },
+  'error label' => {
+    'lastUpdated' => '1241178361',
+    'message' => 'Erreur'
+  },
   'error user is already friend' => {
     'lastUpdated' => '1231231754',
     'message' => 'Cette personne est d&eacute;j&agrave; dans votre liste d\'amis !'
@@ -32,6 +52,134 @@ our $I18N = {
   'error user is already invited' => {
     'lastUpdated' => '1241175556',
     'message' => 'Vous avez déjà envoyé une invitation à rejoindre votre réseau à cet ami'
+  },
+  'friend removed' => {
+    'lastUpdated' => '1241178265',
+    'message' => '%s a été supprimé de votre liste d\'amis'
+  },
+  'friends as others label' => {
+    'lastUpdated' => '1241178280',
+    'message' => 'voir mes amis comme les autres les voient'
+  },
+  'friends confirm template hoverHelp' => {
+    'lastUpdated' => '1241178319',
+    'message' => 'Choisir un modèle pour afficher la confirmation qu\'une requête a été envoyée'
+  },
+  'friends confirm template label' => {
+    'lastUpdated' => '1241178341',
+    'message' => 'Modèle de confirmation de requête d\'amis'
+  },
+  'friends edit template hoverHelp' => {
+    'lastUpdated' => '1241178354',
+    'message' => 'Choisir le modèle pour éditer les amis'
+  },
+  'friends edit template label' => {
+    'lastUpdated' => '1241178462',
+    'message' => 'Editer le modèle'
+  },
+  'friends error template hoverHelp' => {
+    'lastUpdated' => '1241178489',
+    'message' => 'Choisir le modèle pour afficher les erreurs des amis'
+  },
+  'friends error template label' => {
+    'lastUpdated' => '1241178500',
+    'message' => 'Modèle d\'erreur'
+  },
+  'friends is off' => {
+    'lastUpdated' => '1241178542',
+    'message' => 'Vous n\'avez pas activé les amis. Afin de voir cette page vous devez éditer votre profil et activer la préférence amis.'
+  },
+  'friends layout template hoverHelp' => {
+    'lastUpdated' => '1241178601',
+    'message' => 'Choisir une disposition dans la liste pour afficher les divers plugins de compte qui sont éditables par l\'utilisateur courant ainsi que le contenu de celui choisi'
+  },
+  'friends layout template label' => {
+    'lastUpdated' => '1241178632',
+    'message' => 'Disposition du modèle d\'édition'
+  },
+  'friends remove confirm template hoverHelp' => {
+    'lastUpdated' => '1241178657',
+    'message' => 'Choisir le modèle pour afficher la confirmation que vous souhaitez supprimer un ami'
+  },
+  'friends remove confirm template label' => {
+    'lastUpdated' => '1241178684',
+    'message' => 'Modèle de confirmation de suppression d\'amis'
+  },
+  'friends send request template hoverHelp' => {
+    'lastUpdated' => '1241178700',
+    'message' => 'Choisir le modèle pour envoyer des requêtes d\'amis'
+  },
+  'friends send request template label' => {
+    'lastUpdated' => '1241178729',
+    'message' => 'Modèle pour envoyer une requête d\'amis'
+  },
+  'friends style template hoverHelp' => {
+    'lastUpdated' => '1241178791',
+    'message' => 'Choisir un modèle de style dans la liste pour contenir les amis'
+  },
+  'friends style template label' => {
+    'lastUpdated' => '1241178802',
+    'message' => 'Modèle de style'
+  },
+  'friends view template hoverHelp' => {
+    'lastUpdated' => '1241178815',
+    'message' => 'Choisir le modèle principal pour voir les amis'
+  },
+  'friends view template label' => {
+    'lastUpdated' => '1241178822',
+    'message' => 'Voir le modèle'
+  },
+  'member since' => {
+    'lastUpdated' => '1241178667',
+    'message' => 'Membre depuis'
+  },
+  'message sent label' => {
+    'lastUpdated' => '1241178832',
+    'message' => 'Requête d\'amis envoyée'
+  },
+  'no access' => {
+    'lastUpdated' => '1241178855',
+    'message' => 'Vous n\'avez pas la permission de voir la liste d\'amis de cet utilisateur'
+  },
+  'no friend selected' => {
+    'lastUpdated' => '1241178885',
+    'message' => 'Vous n\'avez pas séléctionné d\'amis devant être supprimé de votre liste d\'amis'
+  },
+  'not a friend error' => {
+    'lastUpdated' => '1241178908',
+    'message' => 'L\'utilisateur que vous avez choisi de supprimé n\'est pas dans votre liste d\'amis.'
+  },
+  'remove confirm label' => {
+    'lastUpdated' => '1241178919',
+    'message' => 'Confirmation de suppression d\'ami'
+  },
+  'remove confirm message' => {
+    'lastUpdated' => '1241178938',
+    'message' => 'Etes-vous sûr de vouloir supprimer %s de votre liste d\'amis ?'
+  },
+  'remove confirm no' => {
+    'lastUpdated' => '1241178947',
+    'message' => 'non, je me suis trompé'
+  },
+  'remove confirm yes' => {
+    'lastUpdated' => '1241178959',
+    'message' => 'oui, supprimer cet utilisateur'
+  },
+  'remove from friends label' => {
+    'lastUpdated' => '1241178971',
+    'message' => 'Supprimer des amis'
+  },
+  'send invite button label' => {
+    'lastUpdated' => '1241178981',
+    'message' => 'Envoyer une invitation'
+  },
+  'send private message label' => {
+    'lastUpdated' => '1241178990',
+    'message' => 'Envoyer un message privé'
+  },
+  'sending to message' => {
+    'lastUpdated' => '1241179016',
+    'message' => 'Le message suivant va être envoyé avec votre invitation :'
   },
   'title' => {
     'lastUpdated' => '1241175525',
