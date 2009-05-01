92b61f863e7e3ab2a89531410bb18b2d8f198caa
Translation Server <translation@plainblack.com>
2009-05-01 11:59:46 +0000

    Update from translation server

--- French/French/Workflow_Activity_PurgeOldInboxMessages.pm
+++ French/French/Workflow_Activity_PurgeOldInboxMessages.pm
@@ -1,7 +1,19 @@
 package WebGUI::i18n::French::Workflow_Activity_PurgeOldInboxMessages;
 use utf8;
-
 our $I18N = {
-};
+  'activityName' => {
+    'lastUpdated' => '1241179130',
+    'message' => 'Purger les anciens messages de la boîte de récéption'
+  },
+  'editForm purgeAfter description' => {
+    'lastUpdated' => '1241179166',
+    'message' => 'Durée pendant laquelle un message peut rester dans la boîte de récéption après qu\'il ait été marqué "complété"'
+  },
+  'editForm purgeAfter label' => {
+    'lastUpdated' => '1241179176',
+    'message' => 'Purger après'
+  }
+}
+;
 
 1;
