671ed5e82f6a965b8c13bd39d865a530771780a8
Translation Server <translation@plainblack.com>
2009-05-01 09:39:52 +0000

    Update from translation server

--- German/German/Asset_DataTable.pm
+++ German/German/Asset_DataTable.pm
@@ -2,20 +2,20 @@ package WebGUI::i18n::German::Asset_DataTable;
 use utf8;
 our $I18N = {
   'assetName' => {
-    'lastUpdated' => 1226014165,
-    'message' => 'Daten-Tabelle'
+    'lastUpdated' => '1241167980',
+    'message' => 'Datentabelle'
   },
   'editForm templateId description' => {
-    'lastUpdated' => 1226589586,
+    'lastUpdated' => '1226589586',
     'message' => 'Wähle ein Template um die Datentabellen anzuzeigen.'
   },
   'editForm templateId label' => {
-    'lastUpdated' => 1228175412,
-    'message' => 'Template für Daten-Tabelle'
+    'lastUpdated' => '1241167993',
+    'message' => 'Template für Datentabelle'
   },
   'tab label data' => {
-    'lastUpdated' => 1226014180,
-    'message' => 'Daten-Tabelle'
+    'lastUpdated' => '1241168003',
+    'message' => 'Datentabelle'
   }
 }
 ;
