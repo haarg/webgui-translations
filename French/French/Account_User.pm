2f9afe6bda8b83d1f0b6ef2b51a1ac8e8adb2851
Translation Server <translation@plainblack.com>
2009-05-01 12:25:43 +0000

    Update from translation server

--- French/French/Account_User.pm
+++ French/French/Account_User.pm
@@ -1,5 +1,39 @@
 package WebGUI::i18n::French::Account_User;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'templates in auth method message' => {
+    'lastUpdated' => '1241180499',
+    'message' => 'Le modèle d\'affichage pour ce module peut être trouvé dans les préférences pour '
+  },
+  'title' => {
+    'lastUpdated' => '1241180509',
+    'message' => 'Compte'
+  },
+  'user layout template body' => {
+    'lastUpdated' => '1241180597',
+    'message' => 'Ce plugin de compte est seulement une surchouche du système d\'identification de WebGUI. Merci de voir les modèles d\'affichage d\'information des comptes pour styliser cet écran.'
+  },
+  'user layout template hoverHelp' => {
+    'lastUpdated' => '1241180700',
+    'message' => 'Choisir un modèle dans lequel afficher le contenu des diverses méthodes de l\'onglet compte'
+  },
+  'user layout template label' => {
+    'lastUpdated' => '1241180691',
+    'message' => 'Modèle d\'affichage'
+  },
+  'user layout template title' => {
+    'lastUpdated' => '1241180683',
+    'message' => 'Modèle d\'affichage de compte utilisateur'
+  },
+  'user style template hoverHelp' => {
+    'lastUpdated' => '1241180724',
+    'message' => 'Choisir un modèle de style dans la liste pour afficher l\'onglet de compte.'
+  },
+  'user style template label' => {
+    'lastUpdated' => '1241180735',
+    'message' => 'Modèle de style'
+  }
+}
+;
 
 1;
