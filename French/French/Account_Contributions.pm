da98f1d83e20a052516d55a1b76d72f1f310c6ee
Translation Server <translation@plainblack.com>
2009-05-01 12:31:36 +0000

    Update from translation server

--- French/French/Account_Contributions.pm
+++ French/French/Account_Contributions.pm
@@ -40,6 +40,58 @@ our $I18N = {
   'contributions_total' => {
     'lastUpdated' => '1241176965',
     'message' => 'Le total des contributions de l\'utilisateur.'
+  },
+  'contributions_variables' => {
+    'lastUpdated' => '1241181087',
+    'message' => 'Chaque variable que le module fournit sera dans cette boucle, par nom. Les plus communes et utiles seront title, url, menutitle, dateCreated, synopsis, lastModified, etc.'
+  },
+  'date label' => {
+    'lastUpdated' => '1241181022',
+    'message' => 'Date'
+  },
+  'dateStamp_url' => {
+    'lastUpdated' => '1241181015',
+    'message' => 'URL pour trier les contributions par leur date de création. Noter que cette URL change aussi le sens du tri (ascendant ou descendant).'
+  },
+  'has_contributions' => {
+    'lastUpdated' => '1241180982',
+    'message' => 'Un booléen vrai si l\'utilisateur a fait des contributions au site.'
+  },
+  'no contributions' => {
+    'lastUpdated' => '1241180959',
+    'message' => 'Pas de contributions à afficher'
+  },
+  'rpp_url' => {
+    'lastUpdated' => '1241180946',
+    'message' => 'URL pour changer le nombre de résultats par page.'
+  },
+  'title' => {
+    'lastUpdated' => '1241180765',
+    'message' => 'Contributions'
+  },
+  'title label' => {
+    'lastUpdated' => '1241180772',
+    'message' => 'Titre'
+  },
+  'title_url' => {
+    'lastUpdated' => '1241180866',
+    'message' => 'URL pour trier les contributions par titre. Noter que cette URL change aussi le sens de tri (ascendant ou descendant).'
+  },
+  'type label' => {
+    'lastUpdated' => '1241180820',
+    'message' => 'Type'
+  },
+  'type_url' => {
+    'lastUpdated' => '1241180855',
+    'message' => 'URL pour trier les contributions par type de contribution. Noter que cette URL change aussi le sens du tri (ascendant ou descendant).'
+  },
+  'user_full_name' => {
+    'lastUpdated' => '1241180887',
+    'message' => 'Nom de l\'utilisateur dont les contributions sont affichées.'
+  },
+  'user_member_since' => {
+    'lastUpdated' => '1241180932',
+    'message' => 'La date, au format epoch, à laquelle l\'utilisateur dont les contributions sont affichées a créé son compte sur le site.'
   }
 }
 ;
