d459fbadb878e881616109c8224e61bcc1225c49
Translation Server <translation@plainblack.com>
2009-05-01 08:50:28 +0000

    Update from translation server

--- German/German/PassiveAnalytics.pm
+++ German/German/PassiveAnalytics.pm
@@ -1,9 +1,17 @@
 package WebGUI::i18n::German::PassiveAnalytics;
 use utf8;
 our $I18N = {
+  'Add a bucket' => {
+    'lastUpdated' => '1241167727',
+    'message' => 'Behälter hinzufügen'
+  },
   'Begin analysis' => {
     'lastUpdated' => '1238880849',
     'message' => 'Begin Analyse'
+  },
+  'Bucket Name' => {
+    'lastUpdated' => '1241167803',
+    'message' => 'Behältername'
   },
   'User' => {
     'lastUpdated' => '1238880833',
@@ -30,6 +38,10 @@ Meta-charakter sollten ein "\\" vorangestellt werden, wenn diese als normaler Te
 interpretiert werden müssen, z.B.:
 home\\?func=match
 home\\?name=abc\\.cde   (wenn name "abc.cde" und nicht auch "abczcde" sein soll)'
+  },
+  'workflow deleted' => {
+    'lastUpdated' => '1241167785',
+    'message' => 'Der Workflow zur "Passive Analyse" wurde gelöscht. Bitte kontaktieren Sie umgehend den Administrator.'
   }
 }
 ;
