55d0faa280351aeafbe5bc2cd30908c91930e15a
Translation Server <translation@plainblack.com>
2009-05-01 12:33:09 +0000

    Update from translation server

--- French/French/Form_Captcha.pm
+++ French/French/Form_Captcha.pm
@@ -1,17 +1,23 @@
 package WebGUI::i18n::French::Form_Captcha;
 use utf8;
-
 our $I18N = {
-	'topicName' => {
-		message => q|Captcha|,
-		lastUpdated => 1203424393
-	},
-
-	'verify your humanity' => {
-		message => q|V&eacute;rification de votre humanit&eacute;|,
-		lastUpdated => 1203424514
-	},
-
-};
+  'captcha failure' => {
+    'lastUpdated' => '1241181181',
+    'message' => 'Vous devez taper les 6 caractères que vous voyez dans l\'image pour prouver que vous êtes un humain.'
+  },
+  'recaptcha failure' => {
+    'lastUpdated' => '1241181144',
+    'message' => 'Vous devez entrer correctement les mots que voyez dans l\'image pour prouver que vous êtes un humain.'
+  },
+  'topicName' => {
+    'lastUpdated' => '1203424393',
+    'message' => 'Captcha'
+  },
+  'verify your humanity' => {
+    'lastUpdated' => '1203424514',
+    'message' => 'V&eacute;rification de votre humanit&eacute;'
+  }
+}
+;
 
 1;
