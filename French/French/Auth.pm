a2125dd784eec39f8f982c965a688ccbd77611ca
Translation Server <translation@plainblack.com>
2009-05-01 12:37:28 +0000

    Update from translation server

--- French/French/Auth.pm
+++ French/French/Auth.pm
@@ -1,207 +1,171 @@
 package WebGUI::i18n::French::Auth;
 use utf8;
-
 our $I18N = {
-	'login.form.header' => {
-		message => q|Les &eacute;lements requis du formulaire qui sont au d&eacute;but de la page 'connection' (login).|,
-		lastUpdated => 1203621206
-	},
-
-	'create.form.header' => {
-		message => q|Les &eacute;lements requis du formulaire qui sont au d&eacute;but de la page 'enregistrement anonyme'.|,
-		lastUpdated => 1203619581
-	},
-
-	'create.form.profile.id.formElement' => {
-		message => q|Utilisation (hors de la boucle create.form.profile) des &eacute;l&eacute;ments de formulaire champs du profil. Remplacer id par l'id du profil en cours pour un &eacute;l&eacute;ment.|,
-		lastUpdated => 1203619998
-	},
-
-	'display account template title' => {
-		message => q|Gabarit de l'affichage de l'autentification du compte|,
-		lastUpdated => 1203622097
-	},
-
-	'account.options' => {
-		message => q|Liste des liens pour les options du compte : Passer en mode Admin, Voir le profil, Modifier le profil, Voir la bo&icirc;te de message, ect.|,
-		lastUpdated => 1203619374
-	},
-
-	'create.form.profile' => {
-		message => q|Boucle contenant les champs de profil visible er requis pour un enregistrement anonyme. Ces variables sont aussi disponible en dehors de la boucle (par leur Id).|,
-		lastUpdated => 1203619709
-	},
-
-	'login template title' => {
-		message => q|Gabarit de connection (autentification)|,
-		lastUpdated => 1203621104
-	},
-
-	'no.url' => {
-		message => q|URL pour laisser uniquement le compte uilisateur.|,
-		lastUpdated => 1203621872
-	},
-
-	'login.form.hidden' => {
-		message => q|Champ cach&eacute; requis pour la soumission du formulaire|,
-		lastUpdated => 1203621252
-	},
-
-	'account.form.header' => {
-		message => q|Les &eacute;lements requis du formulaire qui sont au d&eacute;but de l'affichage de la page 'compte'.|,
-		lastUpdated => 1203619615
-	},
-
-	'profile.required' => {
-		message => q|Variable bool&eacute;enne valant &quot;True&quot; si le champ profil est n&eacute;cessaire.|,
-		lastUpdated => 1202831051
-	},
-
-	'account.form.submit' => {
-		message => q|Le bouton 'submit' par d&eacute;faut dans l'affichage du formulaire 'compte'.|,
-		lastUpdated => 1203619295
-	},
-
-	'no.label' => {
-		message => q|Etiquette pour 'NON'.|,
-		lastUpdated => 1203621556
-	},
-
-	'login.form.password.label' => {
-		message => q|Texte par d&eacute;faut pour le champ 'M&ocirc;t de passe' du formulaire|,
-		lastUpdated => 1203621370
-	},
-
-	'yes.label' => {
-		message => q|Etiquette pour &quot;OUI&quot;.|,
-		lastUpdated => 1203621790
-	},
-
-	'login.url' => {
-		message => q|URL de la page de connexion.|,
-		lastUpdated => 1203535238
-	},
-
-	'title' => {
-		message => q|Titre de la page par d&eacute;faut.|,
-		lastUpdated => 1203621815
-	},
-
-	'createAccount.url' => {
-		message => q|URL pour la page 'enregistrement anonyme'|,
-		lastUpdated => 1203620292
-	},
-
-	'anon reg template title' => {
-		message => q|Variable du gabarit 'Enregistrement anonyme'.|,
-		lastUpdated => 1203619412
-	},
-
-	'account.form.karma' => {
-		message => q|Une propri&eacute;t&eacute; de formulaire (en lecture seulement) affichant le montant de karma qu'a l'utilisateur. Karma est une propri&eacute;t&eacute; configurable de l'utilisateur, d&eacute;activ&eacute; par d&eacute;faut.|,
-		lastUpdated => 1203619154
-	},
-
-	'login.form.footer' => {
-		message => q|Les &eacute;lements requis du formulaire qui sont apr&egrave;s le formulaire de la page 'connection' (login).|,
-		lastUpdated => 1203621168
-	},
-
-	'anonymousRegistration.isAllowed' => {
-		message => q|Drapeau (flag) indiquant si l'enregistrement anonyme est autoris&eacute; ou non.|,
-		lastUpdated => 1203619472
-	},
-
-	'account.form.footer' => {
-		message => q|Les &eacute;lements requis du formulaire qui sont apr&egrave;s l'affichage du formulaire de la page 'compte'.|,
-		lastUpdated => 1203621135
-	},
-
-	'login.form.password' => {
-		message => q|Champ 'M&ocirc;t de passe' du formulaire par d&eacute;faut|,
-		lastUpdated => 1203621332
-	},
-
-	'create.form.profile.id.required' => {
-		message => q|Valeur bool&eacute;enne (hors de la boucle) valant 'True' si le champ du profil est requis. Remplacer id par l'id du profil en cours pour un &eacute;l&eacute;ment.|,
-		lastUpdated => 1203620210
-	},
-
-	'create.form.profile.id.formElement.label' => {
-		message => q|Utilisation (hors de la boucle create.form.profile) de l'&eacute;tiquette par d&eacute;faut d'un &eacute;l&eacute;ment du formulaire profil. Remplacer id par l'id du profil en cours pour un &eacute;l&eacute;ment.|,
-		lastUpdated => 1203620199
-	},
-
-	'yes.url' => {
-		message => q|URL pour d&eacute;sactiver le compte de l'utilisateur.|,
-		lastUpdated => 1203621799
-	},
-
-	'create.form.submit' => {
-		message => q|Le bouton 'submit' par d&eacute;faut pour l'enregistrement anonyme.|,
-		lastUpdated => 1203620240
-	},
-
-	'topicName' => {
-		message => q|Autentification|,
-		lastUpdated => 1203621804
-	},
-
-	'login.form.username' => {
-		message => q|Champ 'nom de l'utilisateur' (username) par d&eacute;faut|,
-		lastUpdated => 1203621450
-	},
-
-	'account.form.karma.label' => {
-		message => q|Etiquette pour la valeur du formulaire Karma.|,
-		lastUpdated => 1203619201
-	},
-
-	'deactivate account template title' => {
-		message => q|Gabarit d'autentification pour la d&eacute;sactivation du compte|,
-		lastUpdated => 1203620407
-	},
-
-	'profile.formElement.label' => {
-		message => q|Text par d&eacute;faut des &eacute;l&eacute;ments du formulaire profil.|,
-		lastUpdated => 1203621939
-	},
-
-	'profile.formElement' => {
-		message => q|Element de formulaire pour le champ de profil visible ou requis.|,
-		lastUpdated => 1203621998
-	},
-
-	'login.form.username.label' => {
-		message => q|Texte par d&eacute;faut du champ 'nom de l'utilisateur' (username)<br /> |,
-		lastUpdated => 1203621486
-	},
-
-	'create.form.footer' => {
-		message => q|Les &eacute;lements requis du formulaire qui sont apr&egrave;s le formulaire de la page 'enregistrement anonyme'.|,
-		lastUpdated => 1203619589
-	},
-
-	'question' => {
-		message => q|URL pour laisser uniquement le compte uilisateur.|,
-		lastUpdated => 1203621881
-	},
-
-	'login.form.submit' => {
-		message => q|Le bouton 'submit' par d&eacute;faut du formulaire de connection (login).|,
-		lastUpdated => 1203621407
-	},
-
-	'createAccount.label' => {
-		message => q|Etiquette par d&eacute;faut pour le lien 'enregistrement anonyme'.|,
-		lastUpdated => 1203620270
-	},
-
-	'login.label' => {
-		message => q|Texte par d&eacute;faut pour le lien vers la page de connection (login).|,
-		lastUpdated => 1203621532
-	},
-
-};
+  'account.form.footer' => {
+    'lastUpdated' => '1203621135',
+    'message' => 'Les &eacute;lements requis du formulaire qui sont apr&egrave;s l\'affichage du formulaire de la page \'compte\'.'
+  },
+  'account.form.header' => {
+    'lastUpdated' => '1203619615',
+    'message' => 'Les &eacute;lements requis du formulaire qui sont au d&eacute;but de l\'affichage de la page \'compte\'.'
+  },
+  'account.form.karma' => {
+    'lastUpdated' => '1203619154',
+    'message' => 'Une propri&eacute;t&eacute; de formulaire (en lecture seulement) affichant le montant de karma qu\'a l\'utilisateur. Karma est une propri&eacute;t&eacute; configurable de l\'utilisateur, d&eacute;activ&eacute; par d&eacute;faut.'
+  },
+  'account.form.karma.label' => {
+    'lastUpdated' => '1203619201',
+    'message' => 'Etiquette pour la valeur du formulaire Karma.'
+  },
+  'account.form.submit' => {
+    'lastUpdated' => '1203619295',
+    'message' => 'Le bouton \'submit\' par d&eacute;faut dans l\'affichage du formulaire \'compte\'.'
+  },
+  'account.options' => {
+    'lastUpdated' => '1203619374',
+    'message' => 'Liste des liens pour les options du compte : Passer en mode Admin, Voir le profil, Modifier le profil, Voir la bo&icirc;te de message, ect.'
+  },
+  'anon reg template title' => {
+    'lastUpdated' => '1203619412',
+    'message' => 'Variable du gabarit \'Enregistrement anonyme\'.'
+  },
+  'anonymousRegistration.isAllowed' => {
+    'lastUpdated' => '1203619472',
+    'message' => 'Drapeau (flag) indiquant si l\'enregistrement anonyme est autoris&eacute; ou non.'
+  },
+  'create.form.footer' => {
+    'lastUpdated' => '1203619589',
+    'message' => 'Les &eacute;lements requis du formulaire qui sont apr&egrave;s le formulaire de la page \'enregistrement anonyme\'.'
+  },
+  'create.form.header' => {
+    'lastUpdated' => '1203619581',
+    'message' => 'Les &eacute;lements requis du formulaire qui sont au d&eacute;but de la page \'enregistrement anonyme\'.'
+  },
+  'create.form.profile' => {
+    'lastUpdated' => '1203619709',
+    'message' => 'Boucle contenant les champs de profil visible er requis pour un enregistrement anonyme. Ces variables sont aussi disponible en dehors de la boucle (par leur Id).'
+  },
+  'create.form.profile.id.formElement' => {
+    'lastUpdated' => '1203619998',
+    'message' => 'Utilisation (hors de la boucle create.form.profile) des &eacute;l&eacute;ments de formulaire champs du profil. Remplacer id par l\'id du profil en cours pour un &eacute;l&eacute;ment.'
+  },
+  'create.form.profile.id.formElement.label' => {
+    'lastUpdated' => '1203620199',
+    'message' => 'Utilisation (hors de la boucle create.form.profile) de l\'&eacute;tiquette par d&eacute;faut d\'un &eacute;l&eacute;ment du formulaire profil. Remplacer id par l\'id du profil en cours pour un &eacute;l&eacute;ment.'
+  },
+  'create.form.profile.id.required' => {
+    'lastUpdated' => '1203620210',
+    'message' => 'Valeur bool&eacute;enne (hors de la boucle) valant \'True\' si le champ du profil est requis. Remplacer id par l\'id du profil en cours pour un &eacute;l&eacute;ment.'
+  },
+  'create.form.submit' => {
+    'lastUpdated' => '1203620240',
+    'message' => 'Le bouton \'submit\' par d&eacute;faut pour l\'enregistrement anonyme.'
+  },
+  'createAccount.label' => {
+    'lastUpdated' => '1203620270',
+    'message' => 'Etiquette par d&eacute;faut pour le lien \'enregistrement anonyme\'.'
+  },
+  'createAccount.url' => {
+    'lastUpdated' => '1203620292',
+    'message' => 'URL pour la page \'enregistrement anonyme\''
+  },
+  'deactivate account template title' => {
+    'lastUpdated' => '1203620407',
+    'message' => 'Gabarit d\'autentification pour la d&eacute;sactivation du compte'
+  },
+  'display account template title' => {
+    'lastUpdated' => '1203622097',
+    'message' => 'Gabarit de l\'affichage de l\'autentification du compte'
+  },
+  'login template title' => {
+    'lastUpdated' => '1203621104',
+    'message' => 'Gabarit de connection (autentification)'
+  },
+  'login.form.footer' => {
+    'lastUpdated' => '1203621168',
+    'message' => 'Les &eacute;lements requis du formulaire qui sont apr&egrave;s le formulaire de la page \'connection\' (login).'
+  },
+  'login.form.header' => {
+    'lastUpdated' => '1203621206',
+    'message' => 'Les &eacute;lements requis du formulaire qui sont au d&eacute;but de la page \'connection\' (login).'
+  },
+  'login.form.hidden' => {
+    'lastUpdated' => '1203621252',
+    'message' => 'Champ cach&eacute; requis pour la soumission du formulaire'
+  },
+  'login.form.password' => {
+    'lastUpdated' => '1203621332',
+    'message' => 'Champ \'M&ocirc;t de passe\' du formulaire par d&eacute;faut'
+  },
+  'login.form.password.label' => {
+    'lastUpdated' => '1203621370',
+    'message' => 'Texte par d&eacute;faut pour le champ \'M&ocirc;t de passe\' du formulaire'
+  },
+  'login.form.submit' => {
+    'lastUpdated' => '1203621407',
+    'message' => 'Le bouton \'submit\' par d&eacute;faut du formulaire de connection (login).'
+  },
+  'login.form.username' => {
+    'lastUpdated' => '1203621450',
+    'message' => 'Champ \'nom de l\'utilisateur\' (username) par d&eacute;faut'
+  },
+  'login.form.username.label' => {
+    'lastUpdated' => '1203621486',
+    'message' => 'Texte par d&eacute;faut du champ \'nom de l\'utilisateur\' (username)<br /> '
+  },
+  'login.label' => {
+    'lastUpdated' => '1203621532',
+    'message' => 'Texte par d&eacute;faut pour le lien vers la page de connection (login).'
+  },
+  'login.url' => {
+    'lastUpdated' => '1203535238',
+    'message' => 'URL de la page de connexion.'
+  },
+  'no.label' => {
+    'lastUpdated' => '1203621556',
+    'message' => 'Etiquette pour \'NON\'.'
+  },
+  'no.url' => {
+    'lastUpdated' => '1203621872',
+    'message' => 'URL pour laisser uniquement le compte uilisateur.'
+  },
+  'profile.formElement' => {
+    'lastUpdated' => '1203621998',
+    'message' => 'Element de formulaire pour le champ de profil visible ou requis.'
+  },
+  'profile.formElement.label' => {
+    'lastUpdated' => '1203621939',
+    'message' => 'Text par d&eacute;faut des &eacute;l&eacute;ments du formulaire profil.'
+  },
+  'profile.required' => {
+    'lastUpdated' => '1202831051',
+    'message' => 'Variable bool&eacute;enne valant &quot;True&quot; si le champ profil est n&eacute;cessaire.'
+  },
+  'question' => {
+    'lastUpdated' => '1203621881',
+    'message' => 'URL pour laisser uniquement le compte uilisateur.'
+  },
+  'showMessageOnLogin return' => {
+    'lastUpdated' => '1241181209',
+    'message' => 'Continuer sur le site'
+  },
+  'title' => {
+    'lastUpdated' => '1203621815',
+    'message' => 'Titre de la page par d&eacute;faut.'
+  },
+  'topicName' => {
+    'lastUpdated' => '1203621804',
+    'message' => 'Autentification'
+  },
+  'yes.label' => {
+    'lastUpdated' => '1203621790',
+    'message' => 'Etiquette pour &quot;OUI&quot;.'
+  },
+  'yes.url' => {
+    'lastUpdated' => '1203621799',
+    'message' => 'URL pour d&eacute;sactiver le compte de l\'utilisateur.'
+  }
+}
+;
 
 1;
--- French/French/Tax.pm
+++ French/French/Tax.pm
@@ -2,52 +2,68 @@ package WebGUI::i18n::French::Tax;
 use utf8;
 our $I18N = {
   'add a tax' => {
-    'lastUpdated' => 1231790263,
+    'lastUpdated' => '1231790263',
     'message' => 'ajouter une taxe'
   },
   'city' => {
-    'lastUpdated' => 1231790271,
+    'lastUpdated' => '1231790271',
     'message' => 'ville'
   },
   'city help' => {
-    'lastUpdated' => 1231790293,
+    'lastUpdated' => '1231790293',
     'message' => 'aide ville'
   },
   'code' => {
-    'lastUpdated' => 1231790304,
+    'lastUpdated' => '1231790304',
     'message' => 'code'
   },
   'code help' => {
-    'lastUpdated' => 1231790314,
+    'lastUpdated' => '1231790314',
     'message' => 'aide code'
   },
   'country' => {
-    'lastUpdated' => 1231790338,
+    'lastUpdated' => '1231790338',
     'message' => 'pays'
   },
   'country help' => {
-    'lastUpdated' => 1231790472,
+    'lastUpdated' => '1231790472',
     'message' => 'aide pays'
   },
   'delete' => {
-    'lastUpdated' => 1231790363,
+    'lastUpdated' => '1231790363',
     'message' => 'effacer'
   },
+  'override tax rate' => {
+    'lastUpdated' => '1241181318',
+    'message' => 'Remplacer le taux de taxe ?'
+  },
+  'override tax rate help' => {
+    'lastUpdated' => '1241181389',
+    'message' => 'Voulez-vous remplacer le taux de taxe par défaut pour cet objet ? Généralement utilisé dans les locales qui ont des taux spéciaux ou nuls sur les objets de première nécessité tels que les aliments et les vêtements.'
+  },
   'state' => {
-    'lastUpdated' => 1231790393,
+    'lastUpdated' => '1231790393',
     'message' => '&amp;eacute;tat'
   },
   'state help' => {
-    'lastUpdated' => 1231790405,
+    'lastUpdated' => '1231790405',
     'message' => 'aide &amp;eacute;tat'
   },
   'tax rate' => {
-    'lastUpdated' => 1231790488,
+    'lastUpdated' => '1231790488',
     'message' => 'taux de taxation'
   },
   'tax rate help' => {
-    'lastUpdated' => 1231790439,
+    'lastUpdated' => '1231790439',
     'message' => 'aide taux de taxation'
+  },
+  'tax rate override' => {
+    'lastUpdated' => '1241181409',
+    'message' => 'Remplacement du taux de taxe'
+  },
+  'tax rate override help' => {
+    'lastUpdated' => '1241181437',
+    'message' => 'Quel est le nouveau pourcentage qui doit être utilisé pour calculer la taxe sur cet objet ?'
   }
 }
 ;
