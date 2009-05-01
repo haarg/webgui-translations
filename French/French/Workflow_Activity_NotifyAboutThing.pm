87c39334c87258eba6c89262a7e564926be02531
Translation Server <translation@plainblack.com>
2009-05-01 12:02:58 +0000

    Update from translation server

--- French/French/Workflow_Activity_NotifyAboutThing.pm
+++ French/French/Workflow_Activity_NotifyAboutThing.pm
@@ -1,5 +1,27 @@
 package WebGUI::i18n::French::Workflow_Activity_NotifyAboutThing;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'group to notify' => {
+    'lastUpdated' => '1241179314',
+    'message' => 'Groupe à notifier'
+  },
+  'group to notify help' => {
+    'lastUpdated' => '1241179334',
+    'message' => 'Le groupe à notifier quand cet Objet change'
+  },
+  'notify about thing' => {
+    'lastUpdated' => '1241179347',
+    'message' => 'Notifier à propos d\'un Objet'
+  },
+  'notify message' => {
+    'lastUpdated' => '1241179353',
+    'message' => 'Message'
+  },
+  'notify message help' => {
+    'lastUpdated' => '1241179366',
+    'message' => 'Le message à inclure dans le mail de notification'
+  }
+}
+;
 
 1;
