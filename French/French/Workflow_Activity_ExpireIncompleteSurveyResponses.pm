3c342d1959d5542ad443ac0a9ce84fe246d37819
Translation Server <translation@plainblack.com>
2009-05-01 12:14:04 +0000

    Update from translation server

--- French/French/Workflow_Activity_ExpireIncompleteSurveyResponses.pm
+++ French/French/Workflow_Activity_ExpireIncompleteSurveyResponses.pm
@@ -1,5 +1,47 @@
 package WebGUI::i18n::French::Workflow_Activity_ExpireIncompleteSurveyResponses;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'Delete expired survey responses' => {
+    'lastUpdated' => '1241179850',
+    'message' => 'Supprimer les réponses de sondage expirées'
+  },
+  'Email template sent to user' => {
+    'lastUpdated' => '1241179767',
+    'message' => 'Modèle pour l\'email'
+  },
+  'Email users that responses were deleted' => {
+    'lastUpdated' => '1241179791',
+    'message' => 'Emailer les utilisateurs que les réponses ont été supprimées'
+  },
+  'delete expired' => {
+    'lastUpdated' => '1241179839',
+    'message' => 'Chaque réponse expirée de sondage sera complètement supprimée de la base de données.'
+  },
+  'email template' => {
+    'lastUpdated' => '1241179870',
+    'message' => 'Ceci est le modèle d\'email qui sera envoyé à l\'utilisateur'
+  },
+  'email users' => {
+    'lastUpdated' => '1241179907',
+    'message' => 'Quand une réponse à un sondage est supprimée, est-ce que l\'utilisateur doit en être informé par email ?'
+  },
+  'from' => {
+    'lastUpdated' => '1241179934',
+    'message' => 'Champ de'
+  },
+  'from mouse over' => {
+    'lastUpdated' => '1241179956',
+    'message' => 'Ceci est le champ from qui apparaîtra dans l\'email envoyé.'
+  },
+  'subject' => {
+    'lastUpdated' => '1241179992',
+    'message' => 'Champ sujet'
+  },
+  'subject mouse over' => {
+    'lastUpdated' => '1241180035',
+    'message' => 'Ceci est le champ sujet qui apparaîtra dans le mail envoyé.'
+  }
+}
+;
 
 1;
