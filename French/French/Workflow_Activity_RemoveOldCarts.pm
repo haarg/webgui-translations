25dd4418ccafbd5125799129057ff377fda0ee37
Translation Server <translation@plainblack.com>
2009-05-01 12:01:26 +0000

    Update from translation server

--- French/French/Workflow_Activity_RemoveOldCarts.pm
+++ French/French/Workflow_Activity_RemoveOldCarts.pm
@@ -1,5 +1,19 @@
 package WebGUI::i18n::French::Workflow_Activity_RemoveOldCarts;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'activityName' => {
+    'lastUpdated' => '1241179216',
+    'message' => 'Supprimer les anciens paniers'
+  },
+  'cart timeout' => {
+    'lastUpdated' => '1241179239',
+    'message' => 'Expiration du panier'
+  },
+  'cart timeout help' => {
+    'lastUpdated' => '1241179273',
+    'message' => 'Quel âge doivent avoir les paniers avant qu\'ils soient supprimés ?'
+  }
+}
+;
 
 1;
