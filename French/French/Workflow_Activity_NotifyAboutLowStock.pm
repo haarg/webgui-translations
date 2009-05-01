4c94e4f8f82951b71900f0a0da40db940313c0df
Translation Server <translation@plainblack.com>
2009-05-01 12:04:50 +0000

    Update from translation server

--- French/French/Workflow_Activity_NotifyAboutLowStock.pm
+++ French/French/Workflow_Activity_NotifyAboutLowStock.pm
@@ -1,5 +1,27 @@
 package WebGUI::i18n::French::Workflow_Activity_NotifyAboutLowStock;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'activityName' => {
+    'lastUpdated' => '1241179405',
+    'message' => 'Notifier en cas de stock faible'
+  },
+  'group to notify' => {
+    'lastUpdated' => '1241179414',
+    'message' => 'Groupe à notifier'
+  },
+  'group to notify help' => {
+    'lastUpdated' => '1241179422',
+    'message' => 'Groupe à notifier'
+  },
+  'warning limit' => {
+    'lastUpdated' => '1241179432',
+    'message' => 'Limite d\'avertissement'
+  },
+  'warning limit help' => {
+    'lastUpdated' => '1241179476',
+    'message' => 'Quand une variante de ce produit a moins que cette quantité, vous serez averti.'
+  }
+}
+;
 
 1;
