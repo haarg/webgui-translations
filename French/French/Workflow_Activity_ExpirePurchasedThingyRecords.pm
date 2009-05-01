5eaf7756707327ad3e3ca452141e029a93fb0300
Translation Server <translation@plainblack.com>
2009-05-01 12:08:27 +0000

    Update from translation server

--- French/French/Workflow_Activity_ExpirePurchasedThingyRecords.pm
+++ French/French/Workflow_Activity_ExpirePurchasedThingyRecords.pm
@@ -1,5 +1,43 @@
 package WebGUI::i18n::French::Workflow_Activity_ExpirePurchasedThingyRecords;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'default notification' => {
+    'lastUpdated' => '1241179515',
+    'message' => 'Votre souscription va bientôt expirer !'
+  },
+  'default notification subject' => {
+    'lastUpdated' => '1241179540',
+    'message' => 'Avis important à propos de votre souscription'
+  },
+  'notificationMessage description' => {
+    'lastUpdated' => '1241179556',
+    'message' => 'Le message à envoyer pour la notification'
+  },
+  'notificationMessage label' => {
+    'lastUpdated' => '1241179564',
+    'message' => 'Message de notification'
+  },
+  'notificationOffset description' => {
+    'lastUpdated' => '1241179602',
+    'message' => 'Le temps avant que le ThingyRecord expire quand la notification est envoyée.'
+  },
+  'notificationOffset label' => {
+    'lastUpdated' => '1241179624',
+    'message' => 'Distance de notification'
+  },
+  'notificationSubject description' => {
+    'lastUpdated' => '1241179635',
+    'message' => 'Le sujet du message à envoyer'
+  },
+  'notificationSubject label' => {
+    'lastUpdated' => '1241179647',
+    'message' => 'Sujet du message de notification'
+  },
+  'topicName' => {
+    'lastUpdated' => '1241179677',
+    'message' => 'Expirer les enregistrements de choses achetées'
+  }
+}
+;
 
 1;
