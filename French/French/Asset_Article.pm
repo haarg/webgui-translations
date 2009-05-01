7c8c1a07231e882c8dd33e61d1cbd3fbac0b37a5
Translation Server <translation@plainblack.com>
2009-05-01 12:19:33 +0000

    Update from translation server

--- French/French/Asset_Article.pm
+++ French/French/Asset_Article.pm
@@ -1,237 +1,191 @@
 package WebGUI::i18n::French::Asset_Article;
 use utf8;
-
 our $I18N = {
-	'description.first.sentence' => {
-		message => q|La premi&egrave;re phrase dans la description. |,
-		lastUpdated => 1203652792
-	},
-
-	'article template description' => {
-		message => q|S&eacute;l&eacute;ctionner le gabarit dans la liste pour afficher votre Wobjet. Chaque Wobjet peut seulement utilise les gabarits que pour son type. Par example, les articles ne epuvent utiliser que les gabarits d'article.|,
-		lastUpdated => 1203652600
-	},
-
-	'linkTitle' => {
-		message => q|Le titre du lien ajout&eacute; &agrave; l'article.|,
-		lastUpdated => 1203254809
-	},
-
-	'description.first.10words' => {
-		message => q|Les 10 premiers mots de la description. Les mots sont d&eacute;finis comme les caract&egrave;res s&eacute;par&eacute;s par des espaces, ainsi les entit&eacute;s HTML et le bamises sont consid&eacute;r&eacute;s comme des mots.|,
-		lastUpdated => 1203255787
-	},
-
-	'7' => {
-		message => q|Titre du lien|,
-		lastUpdated => 1203651905
-	},
-
-	'assetName' => {
-		message => q|Article|,
-		lastUpdated => 1203255602
-	},
-
-	'description.first.2paragraphs' => {
-		message => q|Les deux premiers paragraphes de la description. Un paragraphe est d&eacute;termin&eacute; par le premier retour de chariot trouv&eacute; dans le texte.|,
-		lastUpdated => 1203255843
-	},
-
-	'description.first.paragraph' => {
-		message => q|Le premier paragraphe de la description. Le premier paragraphe est d&eacute;termin&eacute; par le premier retour de chariot trouv&eacute; dans le texte.|,
-		lastUpdated => 1203652810
-	},
-
-	'description.full' => {
-		message => q|La description compl&egrave;te sans aucune pagination.|,
-		lastUpdated => 1203652784
-	},
-
-	'72' => {
-		message => q|Gabarit de l'article|,
-		lastUpdated => 1203651918
-	},
-
-	'url' => {
-		message => q|L'url pour t&eacute;l&eacute;charger le fichier.|,
-		lastUpdated => 1203652617
-	},
-
-	'attachments help' => {
-		message => q|Fichiers et images directement attach&eacute;s &agrave; cet article. Ces fichiers ne seront pas accessibles par le gestionnaire de module par les autres modules.|,
-		lastUpdated => 1203652956
-	},
-
-	'28' => {
-		message => q|Voir les reponses|,
-		lastUpdated => 1203651899
-	},
-
-	'description.first.4sentences' => {
-		message => q|Les 4 premi&egrave;res phrases de la description. |,
-		lastUpdated => 1203255928
-	},
-
-	'iconUrl' => {
-		message => q|L'url pointant sur l'ic&ocirc;ne de ce fichier.|,
-		lastUpdated => 1203652747
-	},
-
-	'description' => {
-		message => q|La description pagin&eacute;e.|,
-		lastUpdated => 1203652819
-	},
-
-	'link url description' => {
-		message => q|<p>Si vous avez ajout&eacute; un titre de lien, maintenant ajoutez l'URL ici.</p><p><em>Example:</em> http://www.google.com </p>|,
-		lastUpdated => 1203254902
-	},
-
-	'new.template' => {
-		message => q|<p>Les articles ont la possibilit&eacute; de changer de gabarit ainsi vous pouvez autoriser les utilisateurs &agrave; voir l'articles diff&eacute;rement. Pour se faire, cr&eacute;ez un lien comme ci-dessous (replacez 999 avec l'id du gabarit  que vous voulez utiliser):</p><p>&lt;a href=&quot;&lt;tmpl_var new.template&gt;999&quot;&gt;En savoir plus...&lt;/a&gt; </p>|,
-		lastUpdated => 1203652698
-	},
-
-	'image.url' => {
-		message => q|L'url de l'image attach&eacute;e.|,
-		lastUpdated => 1203652766
-	},
-
-	'isImage' => {
-		message => q|Valeur bool&eacute;enne indiquant si c'est une image ou pas.|,
-		lastUpdated => 1203652733
-	},
-
-	'article asset template variables body' => {
-		message => q|Chaque module poss&egrave;de un jeu de variables utilisable dans ses gabarits bas&eacute; les propri&eacute;t&eacute;s internes du module. Certaines sont des variables utiles, d'autre le sont moins.|,
-		lastUpdated => 1203653369
-	},
-
-	'storageId' => {
-		message => q|L'ID de l'objet de stockage de ce module, utilis&eacute; pour stocker tous les attachements.|,
-		lastUpdated => 1203652660
-	},
-
-	'cache timeout help' => {
-		message => q|Afin d'augmenter les performances d'acc&egrave;s &agrave; ce module par les utilisateurs, nous pouvons le placer le cache. Combien de temps le place-t-on dans le cache?|,
-		lastUpdated => 1203652879
-	},
-
-	'article asset template variables title' => {
-		message => q|Variables de mod&egrave;le de mise en page Article|,
-		lastUpdated => 1203259204
-	},
-
-	'description.first.2sentences' => {
-		message => q|Les deux premi&egrave;res phrases de la description. |,
-		lastUpdated => 1203255917
-	},
-
-	'description.first.3sentences' => {
-		message => q|Les 3 premi&egrave;res phrases de la description. |,
-		lastUpdated => 1203255940
-	},
-
-	'description.first.50words' => {
-		message => q|Les 50 premiers mots de la description. Les mots sont d&eacute;finis comme les caract&egrave;res s&eacute;par&eacute;s par des espaces, ainsi les entit&eacute;s HTML et le bamises sont consid&eacute;r&eacute;s comme des mots.|,
-		lastUpdated => 1203255567
-	},
-
-	'description.first.75words' => {
-		message => q|Les 75 premiers mots de la description. Les mots sont d&eacute;finis comme les caract&egrave;res s&eacute;par&eacute;s par des espaces, ainsi les entit&eacute;s HTML et le bamises sont consid&eacute;r&eacute;s comme des mots.|,
-		lastUpdated => 1203255554
-	},
-
-	'linkURL' => {
-		message => q|L'url pour le lien ajout&eacute; &agrave; l'article.|,
-		lastUpdated => 1203652709
-	},
-
-	'attachment.icon' => {
-		message => q|L'url de l'image de l'ic&ocirc;ne pour ce type d'attachement.|,
-		lastUpdated => 1203653036
-	},
-
-	'cache timeout' => {
-		message => q|Timeout du cache|,
-		lastUpdated => 1203652910
-	},
-
-	'templateId' => {
-		message => q|L'ID de ce gabarit pour afficher ce module.|,
-		lastUpdated => 1203652645
-	},
-
-	'attachments' => {
-		message => q|Attachements|,
-		lastUpdated => 1203652968
-	},
-
-	'thumbnailUrl' => {
-		message => q|L'url de la miniature (thumbnail) de ce fichier.|,
-		lastUpdated => 1203652630
-	},
-
-	'attachment_loop' => {
-		message => q|Une boucle contenant tous les attachements.|,
-		lastUpdated => 1203255713
-	},
-
-	'description.first.100words' => {
-		message => q|Les 100 premiers mots de la description. Les mots sont d&eacute;finis comme les caract&egrave;res s&eacute;par&eacute;s par des espaces, ainsi les entit&eacute;s HTML et le bamises sont consid&eacute;r&eacute;s comme des mots.|,
-		lastUpdated => 1203255771
-	},
-
-	'cacheTimeout' => {
-		message => q|P&eacute;riode durant laquelle cet article sera dans le cache.|,
-		lastUpdated => 1203652853
-	},
-
-	'12' => {
-		message => q|Modifier l'article|,
-		lastUpdated => 1203651890
-	},
-
-	'8' => {
-		message => q|Lier l'URL|,
-		lastUpdated => 1203651959
-	},
-
-	'filename' => {
-		message => q|Le nom du fichier.|,
-		lastUpdated => 1203652774
-	},
-
-	'73' => {
-		message => q|Les variables suivantes sont utilisables dans les gabarits de l'article.|,
-		lastUpdated => 1203651941
-	},
-
-	'attachment.url' => {
-		message => q|L'url pour t&eacute;l&eacute;charger cet attachement.|,
-		lastUpdated => 1203255692
-	},
-
-	'description.first.25words' => {
-		message => q|Les 25 premiers mots de la description. Les mots sont d&eacute;finis comme les caract&egrave;res s&eacute;par&eacute;s par des espaces, ainsi les entit&eacute;s HTML et le bamises sont consid&eacute;r&eacute;s comme des mots.|,
-		lastUpdated => 1203255797
-	},
-
-	'image.thumbnail' => {
-		message => q|L'url de la miniature pour l'image attach&eacute;e.|,
-		lastUpdated => 1203255026
-	},
-
-	'attachment.name' => {
-		message => q|Le nom du fichier de cet attachement.|,
-		lastUpdated => 1203255669
-	},
-
-	'link title description' => {
-		message => q|<p>Si vous voulez ajouter un lien &agrave; votre article, entrez le titre du lein dans ce champ.</p><p><em>Example:</em> Google </p>|,
-		lastUpdated => 1203254948
-	},
-
-};
+  12 => {
+    'lastUpdated' => '1203651890',
+    'message' => 'Modifier l\'article'
+  },
+  28 => {
+    'lastUpdated' => '1203651899',
+    'message' => 'Voir les reponses'
+  },
+  7 => {
+    'lastUpdated' => '1203651905',
+    'message' => 'Titre du lien'
+  },
+  72 => {
+    'lastUpdated' => '1203651918',
+    'message' => 'Gabarit de l\'article'
+  },
+  73 => {
+    'lastUpdated' => '1203651941',
+    'message' => 'Les variables suivantes sont utilisables dans les gabarits de l\'article.'
+  },
+  8 => {
+    'lastUpdated' => '1203651959',
+    'message' => 'Lier l\'URL'
+  },
+  'article asset template variables body' => {
+    'lastUpdated' => '1203653369',
+    'message' => 'Chaque module poss&egrave;de un jeu de variables utilisable dans ses gabarits bas&eacute; les propri&eacute;t&eacute;s internes du module. Certaines sont des variables utiles, d\'autre le sont moins.'
+  },
+  'article asset template variables title' => {
+    'lastUpdated' => '1203259204',
+    'message' => 'Variables de mod&egrave;le de mise en page Article'
+  },
+  'article template description' => {
+    'lastUpdated' => '1203652600',
+    'message' => 'S&eacute;l&eacute;ctionner le gabarit dans la liste pour afficher votre Wobjet. Chaque Wobjet peut seulement utilise les gabarits que pour son type. Par example, les articles ne epuvent utiliser que les gabarits d\'article.'
+  },
+  'assetName' => {
+    'lastUpdated' => '1203255602',
+    'message' => 'Article'
+  },
+  'attachment.icon' => {
+    'lastUpdated' => '1203653036',
+    'message' => 'L\'url de l\'image de l\'ic&ocirc;ne pour ce type d\'attachement.'
+  },
+  'attachment.name' => {
+    'lastUpdated' => '1203255669',
+    'message' => 'Le nom du fichier de cet attachement.'
+  },
+  'attachment.url' => {
+    'lastUpdated' => '1203255692',
+    'message' => 'L\'url pour t&eacute;l&eacute;charger cet attachement.'
+  },
+  'attachment_loop' => {
+    'lastUpdated' => '1203255713',
+    'message' => 'Une boucle contenant tous les attachements.'
+  },
+  'attachments' => {
+    'lastUpdated' => '1203652968',
+    'message' => 'Attachements'
+  },
+  'attachments help' => {
+    'lastUpdated' => '1241180360',
+    'message' => 'Attacher jusqu\'à 2 fichiers et images directement à cet article. Noter que ces fichiers ne seront pas accessibles pour les autres modules à travers le gestionnaire de modules.'
+  },
+  'cache timeout' => {
+    'lastUpdated' => '1203652910',
+    'message' => 'Timeout du cache'
+  },
+  'cache timeout help' => {
+    'lastUpdated' => '1203652879',
+    'message' => 'Afin d\'augmenter les performances d\'acc&egrave;s &agrave; ce module par les utilisateurs, nous pouvons le placer le cache. Combien de temps le place-t-on dans le cache?'
+  },
+  'cacheTimeout' => {
+    'lastUpdated' => '1203652853',
+    'message' => 'P&eacute;riode durant laquelle cet article sera dans le cache.'
+  },
+  'description' => {
+    'lastUpdated' => '1203652819',
+    'message' => 'La description pagin&eacute;e.'
+  },
+  'description.first.100words' => {
+    'lastUpdated' => '1203255771',
+    'message' => 'Les 100 premiers mots de la description. Les mots sont d&eacute;finis comme les caract&egrave;res s&eacute;par&eacute;s par des espaces, ainsi les entit&eacute;s HTML et le bamises sont consid&eacute;r&eacute;s comme des mots.'
+  },
+  'description.first.10words' => {
+    'lastUpdated' => '1203255787',
+    'message' => 'Les 10 premiers mots de la description. Les mots sont d&eacute;finis comme les caract&egrave;res s&eacute;par&eacute;s par des espaces, ainsi les entit&eacute;s HTML et le bamises sont consid&eacute;r&eacute;s comme des mots.'
+  },
+  'description.first.25words' => {
+    'lastUpdated' => '1203255797',
+    'message' => 'Les 25 premiers mots de la description. Les mots sont d&eacute;finis comme les caract&egrave;res s&eacute;par&eacute;s par des espaces, ainsi les entit&eacute;s HTML et le bamises sont consid&eacute;r&eacute;s comme des mots.'
+  },
+  'description.first.2paragraphs' => {
+    'lastUpdated' => '1203255843',
+    'message' => 'Les deux premiers paragraphes de la description. Un paragraphe est d&eacute;termin&eacute; par le premier retour de chariot trouv&eacute; dans le texte.'
+  },
+  'description.first.2sentences' => {
+    'lastUpdated' => '1203255917',
+    'message' => 'Les deux premi&egrave;res phrases de la description. '
+  },
+  'description.first.3sentences' => {
+    'lastUpdated' => '1203255940',
+    'message' => 'Les 3 premi&egrave;res phrases de la description. '
+  },
+  'description.first.4sentences' => {
+    'lastUpdated' => '1203255928',
+    'message' => 'Les 4 premi&egrave;res phrases de la description. '
+  },
+  'description.first.50words' => {
+    'lastUpdated' => '1203255567',
+    'message' => 'Les 50 premiers mots de la description. Les mots sont d&eacute;finis comme les caract&egrave;res s&eacute;par&eacute;s par des espaces, ainsi les entit&eacute;s HTML et le bamises sont consid&eacute;r&eacute;s comme des mots.'
+  },
+  'description.first.75words' => {
+    'lastUpdated' => '1203255554',
+    'message' => 'Les 75 premiers mots de la description. Les mots sont d&eacute;finis comme les caract&egrave;res s&eacute;par&eacute;s par des espaces, ainsi les entit&eacute;s HTML et le bamises sont consid&eacute;r&eacute;s comme des mots.'
+  },
+  'description.first.paragraph' => {
+    'lastUpdated' => '1203652810',
+    'message' => 'Le premier paragraphe de la description. Le premier paragraphe est d&eacute;termin&eacute; par le premier retour de chariot trouv&eacute; dans le texte.'
+  },
+  'description.first.sentence' => {
+    'lastUpdated' => '1203652792',
+    'message' => 'La premi&egrave;re phrase dans la description. '
+  },
+  'description.full' => {
+    'lastUpdated' => '1203652784',
+    'message' => 'La description compl&egrave;te sans aucune pagination.'
+  },
+  'filename' => {
+    'lastUpdated' => '1203652774',
+    'message' => 'Le nom du fichier.'
+  },
+  'iconUrl' => {
+    'lastUpdated' => '1203652747',
+    'message' => 'L\'url pointant sur l\'ic&ocirc;ne de ce fichier.'
+  },
+  'image.thumbnail' => {
+    'lastUpdated' => '1203255026',
+    'message' => 'L\'url de la miniature pour l\'image attach&eacute;e.'
+  },
+  'image.url' => {
+    'lastUpdated' => '1203652766',
+    'message' => 'L\'url de l\'image attach&eacute;e.'
+  },
+  'isImage' => {
+    'lastUpdated' => '1203652733',
+    'message' => 'Valeur bool&eacute;enne indiquant si c\'est une image ou pas.'
+  },
+  'link title description' => {
+    'lastUpdated' => '1203254948',
+    'message' => '<p>Si vous voulez ajouter un lien &agrave; votre article, entrez le titre du lein dans ce champ.</p><p><em>Example:</em> Google </p>'
+  },
+  'link url description' => {
+    'lastUpdated' => '1203254902',
+    'message' => '<p>Si vous avez ajout&eacute; un titre de lien, maintenant ajoutez l\'URL ici.</p><p><em>Example:</em> http://www.google.com </p>'
+  },
+  'linkTitle' => {
+    'lastUpdated' => '1203254809',
+    'message' => 'Le titre du lien ajout&eacute; &agrave; l\'article.'
+  },
+  'linkURL' => {
+    'lastUpdated' => '1203652709',
+    'message' => 'L\'url pour le lien ajout&eacute; &agrave; l\'article.'
+  },
+  'new.template' => {
+    'lastUpdated' => '1203652698',
+    'message' => '<p>Les articles ont la possibilit&eacute; de changer de gabarit ainsi vous pouvez autoriser les utilisateurs &agrave; voir l\'articles diff&eacute;rement. Pour se faire, cr&eacute;ez un lien comme ci-dessous (replacez 999 avec l\'id du gabarit  que vous voulez utiliser):</p><p>&lt;a href=&quot;&lt;tmpl_var new.template&gt;999&quot;&gt;En savoir plus...&lt;/a&gt; </p>'
+  },
+  'storageId' => {
+    'lastUpdated' => '1203652660',
+    'message' => 'L\'ID de l\'objet de stockage de ce module, utilis&eacute; pour stocker tous les attachements.'
+  },
+  'templateId' => {
+    'lastUpdated' => '1203652645',
+    'message' => 'L\'ID de ce gabarit pour afficher ce module.'
+  },
+  'thumbnailUrl' => {
+    'lastUpdated' => '1203652630',
+    'message' => 'L\'url de la miniature (thumbnail) de ce fichier.'
+  },
+  'url' => {
+    'lastUpdated' => '1203652617',
+    'message' => 'L\'url pour t&eacute;l&eacute;charger le fichier.'
+  }
+}
+;
 
 1;
