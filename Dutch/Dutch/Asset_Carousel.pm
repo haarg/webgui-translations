ee9b11142451ebddfd8f69ec615b0df8d4b40962
Translation Server <translation@plainblack.com>
2009-05-19 08:20:53 +0000

    Update from translation server

--- Dutch/Dutch/Asset_Carousel.pm
+++ Dutch/Dutch/Asset_Carousel.pm
@@ -1,5 +1,55 @@
 package WebGUI::i18n::Dutch::Asset_Carousel;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'assetName' => {
+    'lastUpdated' => '1242720820',
+    'message' => 'Carrousel'
+  },
+  'carousel template description' => {
+    'lastUpdated' => '1242720862',
+    'message' => 'Selecteer een sjabloon voor de carrousel'
+  },
+  'carousel template help title' => {
+    'lastUpdated' => '1242720884',
+    'message' => 'Carrousel sjabloon variabelen'
+  },
+  'carousel template label' => {
+    'lastUpdated' => '1242720906',
+    'message' => 'Carrousel sjabloon'
+  },
+  'id description' => {
+    'lastUpdated' => '1242720940',
+    'message' => 'Geef een uniek ID aan voor dit carrousel item'
+  },
+  'id label' => {
+    'lastUpdated' => '1242720970',
+    'message' => 'ID'
+  },
+  'itemId' => {
+    'lastUpdated' => '1242721005',
+    'message' => 'Het ID van het carrousel item'
+  },
+  'item_loop' => {
+    'lastUpdated' => '1242721093',
+    'message' => 'Een loop met daarin alle carrousel items'
+  },
+  'items description' => {
+    'lastUpdated' => '1242721120',
+    'message' => 'Geef de carrousel items in'
+  },
+  'items label' => {
+    'lastUpdated' => '1242721133',
+    'message' => 'Items'
+  },
+  'sequenceNumber' => {
+    'lastUpdated' => '1242721200',
+    'message' => 'Het reeksnummer van het carrousel item'
+  },
+  'text' => {
+    'lastUpdated' => '1242721226',
+    'message' => 'De tekst van het carrousel item'
+  }
+}
+;
 
 1;
