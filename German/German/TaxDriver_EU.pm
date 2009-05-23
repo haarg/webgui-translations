5e52246162ff5c127bf1406bdfa32d166855c4bc
Translation Server <translation@plainblack.com>
2009-05-23 21:55:23 +0000

    Update from translation server

--- German/German/TaxDriver_EU.pm
+++ German/German/TaxDriver_EU.pm
@@ -1,5 +1,63 @@
 package WebGUI::i18n::German::TaxDriver_EU;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'accept when vies unavailable' => {
+    'lastUpdated' => '1243115393',
+    'message' => 'Falls VIES nicht verfügbar, nicht validierte MwSt-Nummern akzeptieren?'
+  },
+  'add' => {
+    'lastUpdated' => '1243115405',
+    'message' => 'Hinzufügen'
+  },
+  'add vat group' => {
+    'lastUpdated' => '1243115433',
+    'message' => 'MwSt-Satz hinzufügen'
+  },
+  'already has vat number' => {
+    'lastUpdated' => '1243115472',
+    'message' => 'Sie haben bereits eine MwSt.-Nummer für dieses Land registriert.'
+  },
+  'approve' => {
+    'lastUpdated' => '1243115498',
+    'message' => 'Überprüfen'
+  },
+  'deny' => {
+    'lastUpdated' => '1243115512',
+    'message' => 'Ablehnen'
+  },
+  'rate' => {
+    'lastUpdated' => '1243115697',
+    'message' => '%-Satz'
+  },
+  'select country' => {
+    'lastUpdated' => '1243115534',
+    'message' => 'Land selektieren'
+  },
+  'shop country' => {
+    'lastUpdated' => '1243115574',
+    'message' => 'Land zum Shop'
+  },
+  'user' => {
+    'lastUpdated' => '1243115588',
+    'message' => 'Benutzer'
+  },
+  'vat group' => {
+    'lastUpdated' => '1243115602',
+    'message' => 'MwSt-Satz'
+  },
+  'vat groups' => {
+    'lastUpdated' => '1243115620',
+    'message' => 'MwSt.-Sätze'
+  },
+  'vat number' => {
+    'lastUpdated' => '1243115634',
+    'message' => 'MwSt.-Nummer'
+  },
+  'vat numbers' => {
+    'lastUpdated' => '1243115652',
+    'message' => 'MwSt.-Nummern'
+  }
+}
+;
 
 1;
