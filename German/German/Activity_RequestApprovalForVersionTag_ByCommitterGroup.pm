189df52805746bc58c73c427197217caf6f690d7
Translation Server <translation@plainblack.com>
2009-06-03 21:21:37 +0000

    Update from translation server

--- German/German/Activity_RequestApprovalForVersionTag_ByCommitterGroup.pm
+++ German/German/Activity_RequestApprovalForVersionTag_ByCommitterGroup.pm
@@ -1,21 +1,25 @@
 package WebGUI::i18n::German::Activity_RequestApprovalForVersionTag_ByCommitterGroup;
 use utf8;
 our $I18N = {
-  "committerGroupId description" => {
-    "lastUpdated" => "1223577296",
-    "message" => "Die Gruppe, die eine Freigabe benötigt, um diese Aktivität zu benutzen. Falls der Committer kein Mitglied dieser Gruppe ist, f&auml;hrt der Workflow mit der n&auml;chsten Aktivit&auml;t fort."
+  'committerGroupId description' => {
+    'lastUpdated' => '1223577296',
+    'message' => 'Die Gruppe, die eine Freigabe benötigt, um diese Aktivität zu benutzen. Falls der Committer kein Mitglied dieser Gruppe ist, f&auml;hrt der Workflow mit der n&auml;chsten Aktivit&auml;t fort.'
   },
-  "committerGroupId label" => {
-    "lastUpdated" => "1223577347",
-    "message" => "Commiter-Gruppe f\374r die Freigabe"
+  'committerGroupId label' => {
+    'lastUpdated' => '1223577347',
+    'message' => 'Commiter-Gruppe für die Freigabe'
   },
-  "invertGroupSetting description" => {
-    "lastUpdated" => "1232694798",
-    "message" => ""
+  'invertGroupSetting description' => {
+    'lastUpdated' => '1244064056',
+    'message' => 'Wenn "ja", werden nur Benutzer, die kein Mitglied der selektierten Freigabegruppe sind, Zustimmung zur Nutzung dieser Aktivität brauchen. Ansonsten wird der Workflow mit der nächsten Aktivität weiter machen.'
   },
-  "topicName" => {
-    "lastUpdated" => "1223577309",
-    "message" => "Anfrage zur Freigabe durch die Committer-Gruppe"
+  'invertGroupSetting label' => {
+    'lastUpdated' => '1244064081',
+    'message' => 'Gruppeneinstellung invertieren.'
+  },
+  'topicName' => {
+    'lastUpdated' => '1223577309',
+    'message' => 'Anfrage zur Freigabe durch die Committer-Gruppe'
   }
 }
 ;
