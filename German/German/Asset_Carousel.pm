b3dd637e40de17fa31d6b3f33b5e51ff2a745c61
Translation Server <translation@plainblack.com>
2009-06-03 21:27:50 +0000

    Update from translation server

--- German/German/Asset_Carousel.pm
+++ German/German/Asset_Carousel.pm
@@ -1,5 +1,55 @@
 package WebGUI::i18n::German::Asset_Carousel;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'assetName' => {
+    'lastUpdated' => '1244064122',
+    'message' => 'Carousel'
+  },
+  'carousel template description' => {
+    'lastUpdated' => '1244064150',
+    'message' => 'Selektiere eine Vorlage zu diesem Carousel'
+  },
+  'carousel template help title' => {
+    'lastUpdated' => '1244064195',
+    'message' => 'Carousel Vorlage Variabele'
+  },
+  'carousel template label' => {
+    'lastUpdated' => '1244064459',
+    'message' => 'Carouselvorlage'
+  },
+  'id description' => {
+    'lastUpdated' => '1244064296',
+    'message' => 'Geben Sie eine eindeutige ID für dieses Carouselitem ein.'
+  },
+  'id label' => {
+    'lastUpdated' => '1244064257',
+    'message' => 'ID'
+  },
+  'itemId' => {
+    'lastUpdated' => '1244064324',
+    'message' => 'Item-Id für dieses Carousel'
+  },
+  'item_loop' => {
+    'lastUpdated' => '1244064403',
+    'message' => 'Eine Schleife mit den Items dieses Carousels.'
+  },
+  'items description' => {
+    'lastUpdated' => '1244064429',
+    'message' => 'Geben Sie die Items zu diesem Carousel ein.'
+  },
+  'items label' => {
+    'lastUpdated' => '1244064335',
+    'message' => 'Items'
+  },
+  'sequenceNumber' => {
+    'lastUpdated' => '1244064369',
+    'message' => 'Folgenummer für dieses Carouselitem.'
+  },
+  'text' => {
+    'lastUpdated' => '1244064238',
+    'message' => 'Text zu diesem Carousel Item'
+  }
+}
+;
 
 1;
