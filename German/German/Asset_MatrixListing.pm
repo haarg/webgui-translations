648130d86163879d5776cd08167f169c0ac898c5
Translation Server <translation@plainblack.com>
2009-06-08 21:24:07 +0000

    Update from translation server

--- German/German/Asset_MatrixListing.pm
+++ German/German/Asset_MatrixListing.pm
@@ -5,6 +5,10 @@ our $I18N = {
     'lastUpdated' => '1238678522',
     'message' => 'Klicks'
   },
+  'comment label' => {
+    'lastUpdated' => '1244495860',
+    'message' => 'Bemerkung'
+  },
   'comments' => {
     'lastUpdated' => '1238678539',
     'message' => 'Kommentare'
@@ -16,6 +20,58 @@ our $I18N = {
   'description' => {
     'lastUpdated' => '1238678555',
     'message' => 'Beschreibung'
+  },
+  'form' => {
+    'lastUpdated' => '1244495883',
+    'message' => 'Das komplette Bearbeitungsformular'
+  },
+  'label' => {
+    'lastUpdated' => '1244495912',
+    'message' => 'Das Label zu diesem Attribut'
+  },
+  'mean label' => {
+    'lastUpdated' => '1244495929',
+    'message' => 'Mittelwert'
+  },
+  'median label' => {
+    'lastUpdated' => '1244496210',
+    'message' => 'Median'
+  },
+  'productName' => {
+    'lastUpdated' => '1244496193',
+    'message' => 'Name dieser Liste. Dieser entspricht dem Titel der Liste'
+  },
+  'screenshots' => {
+    'lastUpdated' => '1244496114',
+    'message' => 'Screenshot zu dieser Liste'
+  },
+  'send button label' => {
+    'lastUpdated' => '1244496069',
+    'message' => 'Abschicken'
+  },
+  'value' => {
+    'lastUpdated' => '1244495944',
+    'message' => 'Wert dieses Attributs'
+  },
+  'version' => {
+    'lastUpdated' => '1244495959',
+    'message' => 'Version dieser Liste'
+  },
+  'version label' => {
+    'lastUpdated' => '1244496082',
+    'message' => 'Version/Modell'
+  },
+  'views' => {
+    'lastUpdated' => '1244495997',
+    'message' => 'Anzahl der Aufrufe dieser Liste'
+  },
+  'worst label' => {
+    'lastUpdated' => '1244496028',
+    'message' => 'Am schlechtesten'
+  },
+  'your email label' => {
+    'lastUpdated' => '1244496044',
+    'message' => 'Ihre Emailadresse'
   }
 }
 ;
