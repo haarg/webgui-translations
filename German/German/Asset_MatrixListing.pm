d999b057e35642c622deadc4e660f7dd1cd9bfed
Translation Server <translation@plainblack.com>
2009-07-01 21:42:26 +0000

    Update from translation server

--- German/German/Asset_MatrixListing.pm
+++ German/German/Asset_MatrixListing.pm
@@ -1,6 +1,34 @@
 package WebGUI::i18n::German::Asset_MatrixListing;
 use utf8;
 our $I18N = {
+  'Approve this listing.' => {
+    'lastUpdated' => '1246483933',
+    'message' => 'Diese Liste freigeben'
+  },
+  'Delete this listing.' => {
+    'lastUpdated' => '1246483998',
+    'message' => 'Diese Liste löschen'
+  },
+  'Edit this listing.' => {
+    'lastUpdated' => '1246484024',
+    'message' => 'Diese Liste bearbeiten.'
+  },
+  'approveOrDenyUrl' => {
+    'lastUpdated' => '1246484063',
+    'message' => 'Die Url zur freigabe oder Ablehung dieser Liste.'
+  },
+  'assetName' => {
+    'lastUpdated' => '1246484105',
+    'message' => 'Matrix'
+  },
+  'best label' => {
+    'lastUpdated' => '1246484120',
+    'message' => 'Beste'
+  },
+  'categoryLabel' => {
+    'lastUpdated' => '1246484153',
+    'message' => 'Bezeichnung dieser Kategorie'
+  },
   'clicks' => {
     'lastUpdated' => '1238678522',
     'message' => 'Klicks'
@@ -13,6 +41,14 @@ our $I18N = {
     'lastUpdated' => '1238678539',
     'message' => 'Kommentare'
   },
+  'compares' => {
+    'lastUpdated' => '1246483977',
+    'message' => 'Anzahl der Vergleiche mit dieser Liste.'
+  },
+  'contact maintainer label' => {
+    'lastUpdated' => '1246484176',
+    'message' => 'Verwalter kontaktieren'
+  },
   'count label' => {
     'lastUpdated' => '1236959869',
     'message' => 'Anzahl'
@@ -20,6 +56,18 @@ our $I18N = {
   'description' => {
     'lastUpdated' => '1238678555',
     'message' => 'Beschreibung'
+  },
+  'description description' => {
+    'lastUpdated' => '1246484277',
+    'message' => 'Bitte geben Sie eine allgemeine Beschreibung für dieses Produkt oder Item ein. Details können in den weiteren Feldern bewertet werden.'
+  },
+  'description label' => {
+    'lastUpdated' => '1246484288',
+    'message' => 'Beschreibung'
+  },
+  'edit label' => {
+    'lastUpdated' => '1246484519',
+    'message' => 'Bearbeiten'
   },
   'form' => {
     'lastUpdated' => '1244495883',
@@ -29,6 +77,18 @@ our $I18N = {
     'lastUpdated' => '1244495912',
     'message' => 'Das Label zu diesem Attribut'
   },
+  'last updated label' => {
+    'lastUpdated' => '1246484361',
+    'message' => 'Zuletzt aktualisiert'
+  },
+  'maintainer label' => {
+    'lastUpdated' => '1246484306',
+    'message' => 'Verwalter'
+  },
+  'manufacturer label' => {
+    'lastUpdated' => '1246484318',
+    'message' => 'Hersteller'
+  },
   'mean label' => {
     'lastUpdated' => '1244495929',
     'message' => 'Mittelwert'
@@ -37,9 +97,17 @@ our $I18N = {
     'lastUpdated' => '1244496210',
     'message' => 'Median'
   },
+  'product name label' => {
+    'lastUpdated' => '1246484332',
+    'message' => 'Produkt/Item'
+  },
   'productName' => {
     'lastUpdated' => '1244496193',
     'message' => 'Name dieser Liste. Dieser entspricht dem Titel der Liste'
+  },
+  'ratings' => {
+    'lastUpdated' => '1246484453',
+    'message' => 'Übersicht der Bewertungen zu dieser Liste mit einem Formular zur Bewertung, falls ein Besucher das Item noch nicht bewertet hat.'
   },
   'screenshots' => {
     'lastUpdated' => '1244496114',
@@ -48,6 +116,14 @@ our $I18N = {
   'send button label' => {
     'lastUpdated' => '1244496069',
     'message' => 'Abschicken'
+  },
+  'show ratings' => {
+    'lastUpdated' => '1246484472',
+    'message' => 'Bewertungen anzeigen'
+  },
+  'template label' => {
+    'lastUpdated' => '1246484494',
+    'message' => 'Matrix Listenvorlage'
   },
   'value' => {
     'lastUpdated' => '1244495944',
@@ -64,6 +140,10 @@ our $I18N = {
   'views' => {
     'lastUpdated' => '1244495997',
     'message' => 'Anzahl der Aufrufe dieser Liste'
+  },
+  'web site label' => {
+    'lastUpdated' => '1246484507',
+    'message' => 'Website'
   },
   'worst label' => {
     'lastUpdated' => '1244496028',
