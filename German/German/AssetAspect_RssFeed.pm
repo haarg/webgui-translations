b7cac6e64f75d1eabfcfe6f2eb8ccdc2940e222c
Translation Server <translation@plainblack.com>
2009-07-06 21:31:27 +0000

    Update from translation server

--- German/German/AssetAspect_RssFeed.pm
+++ German/German/AssetAspect_RssFeed.pm
@@ -1,5 +1,87 @@
 package WebGUI::i18n::German::AssetAspect_RssFeed;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'RSS tab' => {
+    'lastUpdated' => '1246915423',
+    'message' => 'RSS'
+  },
+  'atomLinkOption' => {
+    'lastUpdated' => '1246915429',
+    'message' => 'Atom'
+  },
+  'feedCopyright' => {
+    'lastUpdated' => '1246915440',
+    'message' => 'Feed Copyright'
+  },
+  'feedCopyright hoverHelp' => {
+    'lastUpdated' => '1246915456',
+    'message' => 'Eine zusätzliche Copyright-Nachricht für den Feed.'
+  },
+  'feedDescription' => {
+    'lastUpdated' => '1246915465',
+    'message' => 'Feed Beschreibung'
+  },
+  'feedDescription hoverHelp' => {
+    'lastUpdated' => '1246915588',
+    'message' => 'Eine zusätzliche Beschreibung des Feeds. Wird hier nichts angegeben, wird die Zusammenfassung des Artikels benutzt.'
+  },
+  'feedHeaderLinks' => {
+    'lastUpdated' => '1246915600',
+    'message' => 'HTML Header Feed Links'
+  },
+  'feedHeaderLinks hoverHelp' => {
+    'lastUpdated' => '1246915649',
+    'message' => 'Bitte wählen Sie, welche Feed Typen Sie in Ihrem HTML Header deklarieren möchten. Viele Browser werden diese Feeds anbieten.'
+  },
+  'feedImage' => {
+    'lastUpdated' => '1246915661',
+    'message' => 'Feed Grafik'
+  },
+  'feedImage hoverHelp' => {
+    'lastUpdated' => '1246915678',
+    'message' => 'Ein zusätzliches Bild kann für den Feed hochgeladen werden.'
+  },
+  'feedImageDescription' => {
+    'lastUpdated' => '1246915690',
+    'message' => 'Beschreibung der Feed Grafik'
+  },
+  'feedImageDescription hoverHelp' => {
+    'lastUpdated' => '1246915720',
+    'message' => 'Eine zusätzliche Beschreibung des Bildes. Wenn hier nichts angegeben wird, wird der Titel des Artikels statt dessen verwendet.'
+  },
+  'feedImageLink' => {
+    'lastUpdated' => '1246915733',
+    'message' => 'Link der Feed Grafik'
+  },
+  'feedImageLink hoverHelp' => {
+    'lastUpdated' => '1246915772',
+    'message' => 'Eine zusätzliche Verlinkung des Bildes. Wenn hier nichts angegeben wird, wird der Link des Artikels statt dessen verwendet.'
+  },
+  'feedTitle' => {
+    'lastUpdated' => '1246915780',
+    'message' => 'Titel'
+  },
+  'feedTitle hoverHelp' => {
+    'lastUpdated' => '1246915807',
+    'message' => 'Ein zusätzlicher Titel für den Feed. Wenn hier nichts angegeben wird, wird der Titel des Artikels statt dessen benutzt.'
+  },
+  'itemsPerFeed' => {
+    'lastUpdated' => '1246915818',
+    'message' => 'Einträge pro Feed'
+  },
+  'itemsPerFeed hoverHelp' => {
+    'lastUpdated' => '1246915834',
+    'message' => 'Die Anzahl der Einträge, die ihm Feed erscheinen sollen.'
+  },
+  'rdfLinkOption' => {
+    'lastUpdated' => '1246915844',
+    'message' => 'RDF/RSS 1.0'
+  },
+  'rssLinkOption' => {
+    'lastUpdated' => '1246915851',
+    'message' => 'RSS 2.0'
+  }
+}
+;
 
 1;
--- German/German/Asset_AdSku.pm
+++ German/German/Asset_AdSku.pm
@@ -13,13 +13,89 @@ our $I18N = {
     'lastUpdated' => '1240880761',
     'message' => 'Sie müssen mindestens %d Clicks für diesen Anzeigenraum kaufen.'
   },
+  'form error min impressions' => {
+    'lastUpdated' => '1246915174',
+    'message' => 'Sie müssen mindestens %d Einblendungen für diese Anzeigenraum kaufen.'
+  },
+  'form error no image' => {
+    'lastUpdated' => '1246915192',
+    'message' => 'Bitte weisen Sie diesem Anzeigenraum eine Grafik zu.'
+  },
+  'form error no link' => {
+    'lastUpdated' => '1246915214',
+    'message' => 'Bitte geben Sie eine gültige URL für diesen Anzeigenraum ein.'
+  },
   'form error no title' => {
     'lastUpdated' => '1245621853',
     'message' => 'Bitte geben Sie einen Titel für dieses Inserat an.'
   },
+  'form manage link' => {
+    'lastUpdated' => '1246915229',
+    'message' => 'Meine Anzeigen verwalten'
+  },
+  'form manage table header clicks' => {
+    'lastUpdated' => '1246915237',
+    'message' => 'Klicks'
+  },
+  'form manage table header impressions' => {
+    'lastUpdated' => '1246915246',
+    'message' => 'Einblendungen'
+  },
+  'form manage table header renew' => {
+    'lastUpdated' => '1246915256',
+    'message' => 'Aktualisieren'
+  },
+  'form manage table header title' => {
+    'lastUpdated' => '1246915265',
+    'message' => 'Titel'
+  },
+  'form manage table value deleted' => {
+    'lastUpdated' => '1246915274',
+    'message' => 'Gelöscht'
+  },
+  'form manage table value renew' => {
+    'lastUpdated' => '1246915296',
+    'message' => 'Erneuern'
+  },
   'form manage title' => {
     'lastUpdated' => '1240880833',
     'message' => 'Meine Inserate verwalten'
+  },
+  'form purchase ad image' => {
+    'lastUpdated' => '1246915304',
+    'message' => 'Grafik'
+  },
+  'form purchase ad link' => {
+    'lastUpdated' => '1246915317',
+    'message' => 'Anzeigen-Link'
+  },
+  'form purchase ad title' => {
+    'lastUpdated' => '1246915327',
+    'message' => 'Anzeigen-Titel'
+  },
+  'form purchase button' => {
+    'lastUpdated' => '1246915340',
+    'message' => 'Zum Warenkorb hinzufügen'
+  },
+  'form purchase link' => {
+    'lastUpdated' => '1246915349',
+    'message' => 'Anzeigen kaufen'
+  },
+  'form purchase number of clicks' => {
+    'lastUpdated' => '1246915366',
+    'message' => 'Anzahl der Klicks'
+  },
+  'form purchase number of impressions' => {
+    'lastUpdated' => '1246915377',
+    'message' => 'Anzahl der Einblendungen'
+  },
+  'form purchase per click' => {
+    'lastUpdated' => '1246915400',
+    'message' => 'für %f pro Klick'
+  },
+  'form purchase per impression' => {
+    'lastUpdated' => '1246915411',
+    'message' => 'für %f pro Einblendung'
   }
 }
 ;
