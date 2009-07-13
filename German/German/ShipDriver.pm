3a47a88ee2cc8c58392f4dfa2d255048ace1bc57
Translation Server <translation@plainblack.com>
2009-07-13 18:26:27 +0000

    Update from translation server

--- German/German/ShipDriver.pm
+++ German/German/ShipDriver.pm
@@ -2,20 +2,28 @@ package WebGUI::i18n::German::ShipDriver;
 use utf8;
 our $I18N = {
   'enabled' => {
-    'lastUpdated' => 1226015703,
+    'lastUpdated' => '1226015703',
     'message' => 'Aktiviert?'
   },
   'enabled help' => {
-    'lastUpdated' => 1226015767,
+    'lastUpdated' => '1226015767',
     'message' => 'Sollen die Nutzer des Shop-Systems diese Versandmöglichkeit auswählen k&ouml;nnen?'
   },
   'label' => {
-    'lastUpdated' => 1226015776,
+    'lastUpdated' => '1226015776',
     'message' => 'Label'
   },
   'label help' => {
-    'lastUpdated' => 1226015840,
+    'lastUpdated' => '1226015840',
     'message' => 'Ein Name für die Versandmöglichkeit. Wählen Sie eine eindeutige und verst&auml;ndliche Bezeichnung mit weniger als 100 Zeichen.'
+  },
+  'who can use' => {
+    'lastUpdated' => '1247509540',
+    'message' => 'Gruppe, die diese Versandmöglichkeit verwenden kann.'
+  },
+  'who can use help' => {
+    'lastUpdated' => '1247509577',
+    'message' => 'Legt fest, welche Gruppe erlaubt wird, diese Versandmöglichkeit zu verwenden.'
   }
 }
 ;
