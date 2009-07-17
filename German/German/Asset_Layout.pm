cc75b074d6facb29336deb69aa5b2a44b927c406
Translation Server <translation@plainblack.com>
2009-07-17 08:22:07 +0000

    Update from translation server

--- German/German/Asset_Layout.pm
+++ German/German/Asset_Layout.pm
@@ -1,100 +1,108 @@
 package WebGUI::i18n::German::Asset_Layout;
 use utf8;
 our $I18N = {
-  '823' => {
-    'lastUpdated' => 1178864135,
+  823 => {
+    'lastUpdated' => '1178864135',
     'message' => 'Zur neuen Seite gehen.'
   },
-  '847' => {
-    'lastUpdated' => 1178864141,
+  847 => {
+    'lastUpdated' => '1178864141',
     'message' => 'Zurück zur aktuellen Seite.'
   },
   'asset order asc' => {
-    'lastUpdated' => 1225318059,
+    'lastUpdated' => '1225318059',
     'message' => 'Am Ende der Seite'
   },
   'asset order desc' => {
-    'lastUpdated' => 1225318073,
+    'lastUpdated' => '1225318073',
     'message' => 'AmAnfanf der Seite'
   },
   'asset order hoverHelp' => {
-    'lastUpdated' => 1225318038,
+    'lastUpdated' => '1225318038',
     'message' => 'Wählen Sie, wie neue Assets hinzugefügt werden. (Zum Anfangs- oder zur Endposition der Seite)'
   },
   'asset order label' => {
-    'lastUpdated' => 1217316339,
+    'lastUpdated' => '1217316339',
     'message' => 'Neues Asset hinzufügen'
   },
   'assetName' => {
-    'lastUpdated' => 1149226936,
+    'lastUpdated' => '1149226936',
     'message' => 'Seitenlayout'
   },
   'assets to hide' => {
-    'lastUpdated' => 1228376645,
+    'lastUpdated' => '1228376645',
     'message' => 'Assets zum Verbergen'
   },
   'assets to hide description' => {
-    'lastUpdated' => 1178864160,
+    'lastUpdated' => '1178864160',
     'message' => 'Diese Liste enthält ein Auswahlfeld für jedes Asset auf der Seite. W&auml;hlen Sie hier die Assets aus, die nicht auf der Seite dargestellt werden sollen. '
   },
   'assetsToHide' => {
-    'lastUpdated' => 1170405069,
+    'lastUpdated' => '1170405069',
     'message' => 'Die IDs der Assets, die in diesem Asset nicht gezeigt werden sollen. Getrennt durch Unix-Style Zeilenvorschubzeichen.'
   },
   'content' => {
-    'lastUpdated' => 1178864154,
+    'lastUpdated' => '1178864154',
     'message' => 'Der verarbeitete (rendered) Inhalt diese Assets.'
   },
   'contentPositions' => {
-    'lastUpdated' => 1170405720,
+    'lastUpdated' => '1170405720',
     'message' => 'Zeichenkette, die die verschiedenen Plätze angibt, an denen Assets dargestellt werden. Ausserdem, welche Assets an welchem Platz und in welcher Reihenfolge.<br /> '
   },
   'dragger.icon' => {
-    'lastUpdated' => 1170405906,
+    'lastUpdated' => '1170405906',
     'message' => 'Icon für die Änderung der Position eines Assets via &quot;click and drag&quot;. Wenn showAdmin false ist, ist diese Variable leer. '
   },
   'dragger.init' => {
-    'lastUpdated' => 1170405916,
+    'lastUpdated' => '1170405916',
     'message' => 'HTML und Javascript ist Voraussetzung für &quot;click and drag&quot;. Wenn showAdmin false ist, ist diese Variable leer.<br />'
   },
   'id' => {
-    'lastUpdated' => 1178864167,
+    'lastUpdated' => '1178864167',
     'message' => 'Die Asset ID dieses Assets.'
   },
   'isUncommitted' => {
-    'lastUpdated' => 1217316368,
+    'lastUpdated' => '1217316368',
     'message' => 'Ein Wahrheitswert, der anzeigt, ob das Asset committed wurde oder nicht'
   },
   'layout asset template variables body' => {
-    'lastUpdated' => 1170406352,
+    'lastUpdated' => '1170406352',
     'message' => 'Jedes Asset bietet ihren Templates einen Satz an Variablen, die auf den Asset Eigenschaften basieren. Einige dieser Variablen können nützlich sein, andere nicht.'
   },
   'layout asset template variables title' => {
-    'lastUpdated' => 1170406368,
+    'lastUpdated' => '1170406368',
     'message' => 'Seitenlayout Template Variablen'
   },
   'layout template body' => {
-    'lastUpdated' => 1178864216,
+    'lastUpdated' => '1178864216',
     'message' => 'Die folgenden Variablen sind in Seitenlayout Templates verfügbar:'
   },
   'layout template title' => {
-    'lastUpdated' => 1178864208,
+    'lastUpdated' => '1178864208',
     'message' => 'Seitenlayout Template'
   },
+  'mobileTemplateId description' => {
+    'lastUpdated' => '1247818914',
+    'message' => 'Wähle dieses Template, wenn dieses Seitenlayout in einem Mobile Browser betrachtet wird.'
+  },
+  'mobileTemplateId label' => {
+    'lastUpdated' => '1247818859',
+    'message' => 'Mobile Template'
+  },
   'position1_loop' => {
-    'lastUpdated' => 1178864308,
+    'lastUpdated' => '1178864308',
     'message' => 'Jede Position in dem Template hat eine Schleife, die einen Satz an Assets enthält, die angezeigt werden. Assets, die nicht an einer bestimmten Stelle platziert wurden, werden an Position 1 gesetzt.'
   },
   'showAdmin' => {
-    'lastUpdated' => 1178864350,
+    'lastUpdated' => '1178864350',
     'message' => 'Bedingung, die angibt, ob sich der aktuelle Benutzer im Admin Modus befindet und dieses Asset bearbeiten kann.'
   },
   'template description' => {
-    'lastUpdated' => 1149227092,
+    'lastUpdated' => '1149227092',
     'message' => 'Wählen Sie ein Template zur Darstellung der Assets auf der Seite aus. '
   },
   'templateId' => {
-    'lastUpdated' => 1178864374,
+    'lastUpdated' => '1178864374',
     'message' => 'Die ID des Templates, das für die Anzeige dieses Assets benutzt wird.'
   }
 }
