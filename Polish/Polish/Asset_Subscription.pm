3b71b0568cdee9c887296ff83e2dc8c6b17d8455
Translation Server <translation@plainblack.com>
2009-07-17 07:26:32 +0000

    Update from translation server

--- Polish/Polish/Asset_Subscription.pm
+++ Polish/Polish/Asset_Subscription.pm
@@ -1,7 +1,327 @@
 package WebGUI::i18n::Polish::Asset_Subscription;
 use utf8;
-
 our $I18N = {
-};
+  'add subscription' => {
+    'lastUpdated' => '1247809938',
+    'message' => "Dodaj now\x{105} subskrypcj\x{119}"
+  },
+  'and' => {
+    'lastUpdated' => '1247812355',
+    'message' => 'i'
+  },
+  'assetName' => {
+    'lastUpdated' => '1247809966',
+    'message' => 'Subskrypcja'
+  },
+  'association' => {
+    'lastUpdated' => '1247810040',
+    'message' => "Po\x{142}\x{105}cz z subskrypcj\x{105}"
+  },
+  'association description' => {
+    'lastUpdated' => '1247810591',
+    'message' => 'Która subskrypcja jest stworzona razem z wygenerowanym kodem'
+  },
+  'batch name' => {
+    'lastUpdated' => '1247812553',
+    'message' => ''
+  },
+  'code' => {
+    'lastUpdated' => '1247810165',
+    'message' => 'Kod'
+  },
+  'code description' => {
+    'lastUpdated' => '1247810564',
+    'message' => "Kod subskrypcji kt\x{f3}ry chcesz odzyska\x{107}"
+  },
+  'code length' => {
+    'lastUpdated' => '1247810248',
+    'message' => "D\x{142}ugo\x{15b}\x{107} kodu subskrypcji"
+  },
+  'code length description' => {
+    'lastUpdated' => '1247810294',
+    'message' => "Liczba znak\x{f3}w w wygenerowanym kodzie. Kod musi posiada\x{107} conajmniej 10 znak\x{f3}w d\x{142}ugo\x{15b}ci."
+  },
+  'code length error' => {
+    'lastUpdated' => '1247810421',
+    'message' => "Musisz wprowadzi\x{107} kod subskrypcji o d\x{142}ugo\x{15b}ci pomi\x{119}dzy 10 a 64 (zawarte warto\x{15b}ci graniczne)"
+  },
+  'codeControls' => {
+    'lastUpdated' => '1247813003',
+    'message' => ''
+  },
+  'codeForm' => {
+    'lastUpdated' => '1247810472',
+    'message' => "Formularz w kt\x{f3}rym u\x{17c}ytkownik mo\x{17c}e wprowadzi\x{107} sw\x{f3}j kod subskrypcji."
+  },
+  'codes expire' => {
+    'lastUpdated' => '1247810627',
+    'message' => 'Kod wygasa po'
+  },
+  'codes expire description' => {
+    'lastUpdated' => '1247810642',
+    'message' => "Kod musi by\x{107} u\x{17c}yty przed t\x{105} dat\x{105}."
+  },
+  'continueShoppingUrl' => {
+    'lastUpdated' => '1247812599',
+    'message' => "Adres URL do zresetowania produktu, tak wi\x{119}c u\x{17c}ytkownik mo\x{17c}e kontynuowa\x{107} zakupy.\r\n"
+  },
+  'create batch menu' => {
+    'lastUpdated' => '1247812996',
+    'message' => ''
+  },
+  'creation date' => {
+    'lastUpdated' => '1247810690',
+    'message' => 'Data utworzenia'
+  },
+  'dateUsed' => {
+    'lastUpdated' => '1247810709',
+    'message' => "Data u\x{17c}ycia"
+  },
+  'default thank you message' => {
+    'lastUpdated' => '1247810743',
+    'message' => "Subskrypcja zosta\x{142}a dodana do koszyka."
+  },
+  'delete batch confirm' => {
+    'lastUpdated' => '1247810760',
+    'message' => ''
+  },
+  'delete codes' => {
+    'lastUpdated' => '1247812623',
+    'message' => "Usu\x{144} wszystkie kody."
+  },
+  'delete subscription confirm' => {
+    'lastUpdated' => '1247810867',
+    'message' => "Czy jeste\x{15b} pewien \x{17c}e chcesz usun\x{105}\x{107} t\x{105} subskrypcj\x{119}?"
+  },
+  'display all' => {
+    'lastUpdated' => '1247810944',
+    'message' => "wy\x{15b}wietl wszystkie"
+  },
+  'edit subscription title' => {
+    'lastUpdated' => '1247810958',
+    'message' => "Edytuj subskrypcj\x{119}"
+  },
+  'execute on subscription' => {
+    'lastUpdated' => '1247812803',
+    'message' => ''
+  },
+  'expire subscription codes' => {
+    'lastUpdated' => '1247811077',
+    'message' => ''
+  },
+  'formFooter' => {
+    'lastUpdated' => '1247811118',
+    'message' => "Dolna cz\x{119}\x{15b}\x{107} formularza subskrypcji."
+  },
+  'formHeader' => {
+    'lastUpdated' => '1247811130',
+    'message' => "G\x{f3}rna cz\x{119}\x{15b}\x{107} formularza subskrypcji."
+  },
+  'generate batch' => {
+    'lastUpdated' => '1247812634',
+    'message' => ''
+  },
+  'hasAddedToCart' => {
+    'lastUpdated' => '1247811178',
+    'message' => "Warunek spe\x{142}niony je\x{17c}eli u\x{17c}ytkownika doda\x{142} subskrypcj\x{119} do swojego koszyka, w ten spos\x{f3}b mo\x{17c}na wy\x{15b}wietli\x{107} wiadomo\x{15b}\x{107} podzi\x{119}kowania."
+  },
+  'help redeem code template title' => {
+    'lastUpdated' => '1247811208',
+    'message' => 'Szablon odzyskiwania kodu subskrypcji.'
+  },
+  'list codes in batch' => {
+    'lastUpdated' => '1247811240',
+    'message' => ''
+  },
+  'manage batches' => {
+    'lastUpdated' => '1247811249',
+    'message' => ''
+  },
+  'manage codes' => {
+    'lastUpdated' => '1247812375',
+    'message' => "Zarz\x{105}dzaj kodami subskrypcji"
+  },
+  'message' => {
+    'lastUpdated' => '1247812491',
+    'message' => ''
+  },
+  'no association error' => {
+    'lastUpdated' => '1247811265',
+    'message' => ''
+  },
+  'no subscriptions' => {
+    'lastUpdated' => '1247812704',
+    'message' => "Nie istniej\x{105} aktualnie \x{17c}adne subskrypcje. Mo\x{17c}esz doda\x{107} subskrypcj\x{119} poprzez u\x{17c}ycie opcji 'Dodaj subskrypcje' w menu po prawej stronie ekranu."
+  },
+  'noc' => {
+    'lastUpdated' => '1247812528',
+    'message' => 'Liczba kodów w batch\'u'
+  },
+  'noc description' => {
+    'lastUpdated' => '1247812420',
+    'message' => 'Liczba kodów do stworzenia'
+  },
+  'price' => {
+    'lastUpdated' => '1247812405',
+    'message' => 'Cena dla subskrypcji, sformatowana do dwóch miejsc po przecinku'
+  },
+  'purchase button' => {
+    'lastUpdated' => '1247811271',
+    'message' => 'Dodaj do koszyka'
+  },
+  'purchaseButton' => {
+    'lastUpdated' => '1247811289',
+    'message' => 'Przycisk dla formularza subskrypcji.'
+  },
+  'recurring subscription' => {
+    'lastUpdated' => '1247811313',
+    'message' => "Jest okresow\x{105} subskrypcj\x{105}?"
+  },
+  'recurring subscription description' => {
+    'lastUpdated' => '1247812813',
+    'message' => ''
+  },
+  'redeem code' => {
+    'lastUpdated' => '1247811355',
+    'message' => 'Odzyskaj kod subskrypcji.'
+  },
+  'redeem code ask for code' => {
+    'lastUpdated' => '1247811385',
+    'message' => "Prosz\x{119} wprowadzi\x{107} tw\x{f3}j kod subskrypcji poni\x{17c}ej."
+  },
+  'redeem code failure' => {
+    'lastUpdated' => '1247811468',
+    'message' => "Wprowadzony przez ciebie kod jest nieprawid\x{142}owy, aktualnie u\x{17c}yty lub straci\x{142} wa\x{17c}no\x{15b}\x{107}. Prosz\x{119} wprowadzi\x{107} inny kod poni\x{17c}ej."
+  },
+  'redeem code success' => {
+    'lastUpdated' => '1247812430',
+    'message' => ''
+  },
+  'redeem subscription code template' => {
+    'lastUpdated' => '1247811578',
+    'message' => 'Szablon odzyskiwania subskrypcji'
+  },
+  'redeem subscription code template help' => {
+    'lastUpdated' => '1247811624',
+    'message' => "Wybierz szablon kt\x{f3}ry b\x{119}dzie u\x{17c}yty do wy\x{15b}wietlenia ekranu pozwalaj\x{105}cego u\x{17c}ytkownikom odzyska\x{107} kody subskrypcji."
+  },
+  'redeemCodeLabel' => {
+    'lastUpdated' => '1247811690',
+    'message' => "Mi\x{119}dzynarodowa etykieta dla linku do odzyskiwania kodu subskrypcji. B\x{119}dzie pusta je\x{17c}eli nie b\x{119}dzie kod\x{f3}w subskrypcji."
+  },
+  'redeemCodeUrl' => {
+    'lastUpdated' => '1247811722',
+    'message' => "Adres URL do odzyskiwania kodu subskrypcji. B\x{119}dzie pusta je\x{17c}eli ich nie b\x{119}dzie."
+  },
+  'select' => {
+    'lastUpdated' => '1247811761',
+    'message' => "Poka\x{17c} wybrane"
+  },
+  'selection batch name' => {
+    'lastUpdated' => '1247811765',
+    'message' => ''
+  },
+  'selection created' => {
+    'lastUpdated' => '1247811786',
+    'message' => "data stworzenia pomi\x{119}dzy"
+  },
+  'selection message' => {
+    'lastUpdated' => '1247811827',
+    'message' => "Mo\x{17c}esz wybra\x{107} kody przez:"
+  },
+  'selection used' => {
+    'lastUpdated' => '1247811837',
+    'message' => "data u\x{17c}ycia pomi\x{119}dzy"
+  },
+  'status' => {
+    'lastUpdated' => '1247811843',
+    'message' => 'Status'
+  },
+  'subscription description' => {
+    'lastUpdated' => '1247811849',
+    'message' => 'Opis'
+  },
+  'subscription description description' => {
+    'lastUpdated' => '1247811866',
+    'message' => "Szczeg\x{f3}\x{142}owy opis subskrypcji."
+  },
+  'subscription duration' => {
+    'lastUpdated' => '1247811894',
+    'message' => 'Okres subskrypcji'
+  },
+  'subscription duration description' => {
+    'lastUpdated' => '1247811940',
+    'message' => ''
+  },
+  'subscription group' => {
+    'lastUpdated' => '1247811958',
+    'message' => ''
+  },
+  'subscription group description' => {
+    'lastUpdated' => '1247812036',
+    'message' => "Kiedy u\x{17c}ytkownik zap\x{142}aci, on/ona b\x{119}dzie dodany/a do tej grupy."
+  },
+  'subscription karma' => {
+    'lastUpdated' => '1247812048',
+    'message' => 'Karma'
+  },
+  'subscription karma description' => {
+    'lastUpdated' => '1247812121',
+    'message' => ''
+  },
+  'subscription name' => {
+    'lastUpdated' => '1247812130',
+    'message' => 'Nazwa subskrypcji'
+  },
+  'subscription name description' => {
+    'lastUpdated' => '1247812143',
+    'message' => 'Nazwa dla subskrypcji.'
+  },
+  'subscription price' => {
+    'lastUpdated' => '1247812149',
+    'message' => 'Cena'
+  },
+  'subscription price description' => {
+    'lastUpdated' => '1247812862',
+    'message' => ''
+  },
+  'subscription template' => {
+    'lastUpdated' => '1247812181',
+    'message' => 'Szablon subskrypcji'
+  },
+  'subscriptionId' => {
+    'lastUpdated' => '1247812189',
+    'message' => 'ID subskrypcji'
+  },
+  'template' => {
+    'lastUpdated' => '1247812199',
+    'message' => 'Szablon subskrypcji'
+  },
+  'template help' => {
+    'lastUpdated' => '1247812226',
+    'message' => "Wybierz szablon, kt\x{f3}ry chcesz u\x{17c}y\x{107} do wy\x{15b}wietlenia tej subskrypcji"
+  },
+  'thank you message' => {
+    'lastUpdated' => '1247812242',
+    'message' => "Komunikat podzi\x{119}kowania"
+  },
+  'thank you message help' => {
+    'lastUpdated' => '1247812256',
+    'message' => ''
+  },
+  'topicName' => {
+    'lastUpdated' => '1247812264',
+    'message' => 'Subskrypcje'
+  },
+  'useSalesTax' => {
+    'lastUpdated' => '1247812282',
+    'message' => "U\x{17c}y\x{107} podatk\x{f3}w sprzeda\x{17c}y?"
+  },
+  'useSalesTax description' => {
+    'lastUpdated' => '1247812322',
+    'message' => "Czy subskrypcja powinna stosowa\x{107} podatki sprzeda\x{17c}y?"
+  }
+}
+;
 
 1;
