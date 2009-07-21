f101593ded3fdf1ba26529d7cbc8997c507e0274
Translation Server <translation@plainblack.com>
2009-07-21 12:41:45 +0000

    Update from translation server

--- Dutch/Dutch/Asset_Story.pm
+++ Dutch/Dutch/Asset_Story.pm
@@ -37,6 +37,58 @@ our $I18N = {
     'lastUpdated' => '1246975829',
     'message' => 'Een knop waarmee de gebruiker kan annuleren zonder het formulier op te slaan.'
   },
+  'crumb_loop' => {
+    'lastUpdated' => '1248177919',
+    'message' => 'Een functie die de route ophaalt. Het eerste element is een link naar het archief waar het veraal in zit. Het laatste element is het verhaal, met titel en url. Als er drie elementen zijn zal het middelste het onderwerp zijn.'
+  },
+  'crumb_title' => {
+    'lastUpdated' => '1248177940',
+    'message' => 'De titel van een pagina in een route.'
+  },
+  'crumb_url' => {
+    'lastUpdated' => '1248178099',
+    'message' => 'De URL van een pagina in de route.'
+  },
+  'edit template' => {
+    'lastUpdated' => '1248178117',
+    'message' => 'Bewerk verhaal sjabloon'
+  },
+  'formFooter' => {
+    'lastUpdated' => '1248178164',
+    'message' => 'HTML code om een formulier voor het bewerken of toevoegen van een verhaal te beëndigen.'
+  },
+  'formHeader' => {
+    'lastUpdated' => '1248178189',
+    'message' => 'HTML code om een formulier voor het bewerken of toevoegen van een verhaal te beginnen.<br>'
+  },
+  'formTitle' => {
+    'lastUpdated' => '1248178639',
+    'message' => 'De internationaliseerde titel van dit formulier'
+  },
+  'hasPhotos' => {
+    'lastUpdated' => '1248178674',
+    'message' => 'Deze sjabloon variabele zal waar zijn als er foto\'s aan het verhaal zijn toegevoegd.'
+  },
+  'headline' => {
+    'lastUpdated' => '1248178596',
+    'message' => 'Koptekst'
+  },
+  'headline help' => {
+    'lastUpdated' => '1248179400',
+    'message' => 'Vaak hetzelfde als de titel. Als dit leeggelaten wordt, zal de titel overgenomen worden.'
+  },
+  'headline tmplvar' => {
+    'lastUpdated' => '1248179416',
+    'message' => 'De kop van dit verhaal'
+  },
+  'headlineForm' => {
+    'lastUpdated' => '1248179651',
+    'message' => 'Formulier voor het toevoegen van een koptekst.'
+  },
+  'imageAlt' => {
+    'lastUpdated' => '1248179773',
+    'message' => 'Alternatieve tekst voor de afbeelding, geschikt voor gebruik als ALT parameter in een IMG element.'
+  },
   'imageTitle' => {
     'lastUpdated' => '1248099382',
     'message' => 'Alternatieve tekst voor de afbeelding, geschikt voor een TITLE eigenschap van een IMG object.'
@@ -49,9 +101,113 @@ our $I18N = {
     'lastUpdated' => '1246971044',
     'message' => 'Een trefwoord zonder opmaak'
   },
+  'keyword_url' => {
+    'lastUpdated' => '1248179831',
+    'message' => 'Een URL om alle verhalen in dit archief te bekijken die gerelateerd zijn aan dit trefwoord.'
+  },
   'location' => {
     'lastUpdated' => '1246970988',
     'message' => 'Locatie'
+  },
+  'locationForm' => {
+    'lastUpdated' => '1248179307',
+    'message' => 'Een formulier waar de gebruiker een locatie van het verhaal kan opgeven.'
+  },
+  'photo alt' => {
+    'lastUpdated' => '1248178713',
+    'message' => 'Alternatieve tekst foto'
+  },
+  'photo byline' => {
+    'lastUpdated' => '1248178905',
+    'message' => 'Regel met maker van de foto'
+  },
+  'photo caption' => {
+    'lastUpdated' => '1248178866',
+    'message' => 'Onderschift foto'
+  },
+  'photo delete' => {
+    'lastUpdated' => '1248178354',
+    'message' => 'Verwijder foto'
+  },
+  'photo title' => {
+    'lastUpdated' => '1248178338',
+    'message' => 'Alternatieve titel foto'
+  },
+  'photo url' => {
+    'lastUpdated' => '1248178498',
+    'message' => 'Foto URL'
+  },
+  'photo_loop' => {
+    'lastUpdated' => '1248179864',
+    'message' => 'Een functie die foto\'s en informatie over foto\'s ophaalt.'
+  },
+  'save and add another photo' => {
+    'lastUpdated' => '1248178373',
+    'message' => 'Opslaan en nog een foto toevoegen'
+  },
+  'save story' => {
+    'lastUpdated' => '1248178313',
+    'message' => 'Verhaal opslaan'
+  },
+  'saveAndAddButton' => {
+    'lastUpdated' => '1248178449',
+    'message' => 'Knop waarme de gebruiker het formulier opslaat en heropend om nog een foto toe te voegen.'
+  },
+  'saveButton' => {
+    'lastUpdated' => '1248178302',
+    'message' => 'Knop waar de gebruiker het formulier mee kan opslaan.'
+  },
+  'singlePhoto' => {
+    'lastUpdated' => '1248178271',
+    'message' => 'Deze sjabloon variabele is waar als er maar 1 foto aan het verhaal is toegevoegd.'
+  },
+  'story' => {
+    'lastUpdated' => '1248178222',
+    'message' => 'Verhaal'
+  },
+  'story asset template variables title' => {
+    'lastUpdated' => '1248178731',
+    'message' => 'Verhaal sjabloon variabelen'
+  },
+  'story received' => {
+    'lastUpdated' => '1248178570',
+    'message' => 'Uw verhaal is ontvangen en wordt verwerkt zodat het aan de site kan worden toegevoegd. Het zal beschikbaar zijn voor verdere bewerking als het verwerkt is. Geduld alstublieft.'
+  },
+  'storyForm' => {
+    'lastUpdated' => '1248180077',
+    'message' => 'Formulier waar het eigenlijke verhaal kan worden ingevoerd.'
+  },
+  'subtitle' => {
+    'lastUpdated' => '1248178208',
+    'message' => 'Ondertitel'
+  },
+  'subtitle help' => {
+    'lastUpdated' => '1248179889',
+    'message' => 'Gelijk aan de koptekst, maar bevat normaliter meer informatie.'
+  },
+  'subtitle tmplvar' => {
+    'lastUpdated' => '1248179907',
+    'message' => 'De ondertitel van een verhaal.'
+  },
+  'subtitleForm' => {
+    'lastUpdated' => '1248179954',
+    'message' => 'Formulier waarmee een ondertitel aan dit verhaal kan worde toegevoegd.'
+  },
+  'summaryForm' => {
+    'lastUpdated' => '1248179992',
+    'message' => 'Formulier voor het toevoegen van een samenvatting van dit verhaal.'
+  },
+  'titleForm' => {
+    'lastUpdated' => '1248178413',
+    'message' => 'Formulier waar een gebruiker een titel van het verhaal kan opgeven.'
+  },
+  'updatedTime' => {
+    'lastUpdated' => '1248179352',
+    'message' => 'De tijd waarop dit verhaal is bijgewerkt, in tijd geleden, zoals 1 uur geleden.'
+  },
+  'updatedTimeEpoch' => {
+    'lastUpdated' => '1248180047',
+    'message' => 'De tijd waarop dit verhaal is bijgwerkt, als epoch tijd.'
   },
   'view template' => {
     'lastUpdated' => '1246970968',
