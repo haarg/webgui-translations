607d7a4f1ebfad199f2ba0a79f4d0f2dca5adad0
Translation Server <translation@plainblack.com>
2009-07-21 11:56:26 +0000

    Update from translation server

--- Dutch/Dutch/PassiveAnalytics.pm
+++ Dutch/Dutch/PassiveAnalytics.pm
@@ -1,25 +1,133 @@
 package WebGUI::i18n::Dutch::PassiveAnalytics;
 use utf8;
 our $I18N = {
+  'Add a bucket' => {
+    'lastUpdated' => '1248176592',
+    'message' => 'Voeg een container toe'
+  },
   'Begin analysis' => {
     'lastUpdated' => '1239880331',
     'message' => 'Beginnen met analyse'
+  },
+  'Bucket Name' => {
+    'lastUpdated' => '1248176605',
+    'message' => 'Naam container'
+  },
+  'Bucket Name help' => {
+    'lastUpdated' => '1248176996',
+    'message' => 'Kies een unieke, beschrijbende korte naam voor deze container.'
+  },
+  'Bucket Passive Analytics' => {
+    'lastUpdated' => '1248176965',
+    'message' => 'Passieve analyse container'
+  },
+  'Delete Delta Table?' => {
+    'lastUpdated' => '1248177012',
+    'message' => 'Delta tabel verwijderen?'
+  },
+  'Delete Delta Table? help' => {
+    'lastUpdated' => '1248177043',
+    'message' => 'Moet de delta tabel opgeruimd worden als de passieve analyse afgerond is?'
   },
   'Edit Rule' => {
     'lastUpdated' => '1239889956',
     'message' => 'Bewerk regel'
   },
+  'Enabled?' => {
+    'lastUpdated' => '1248176915',
+    'message' => 'Passieve analyse aanzetten?'
+  },
+  'Enabled? help' => {
+    'lastUpdated' => '1248176943',
+    'message' => 'Passieve analyse zal geen gegevens verwerken als het niet aangezet is.'
+  },
+  'Export bucket data' => {
+    'lastUpdated' => '1248177070',
+    'message' => 'Exporteer container data'
+  },
+  'Export delta data' => {
+    'lastUpdated' => '1248177079',
+    'message' => 'Exporteer delta data'
+  },
+  'Export raw logs' => {
+    'lastUpdated' => '1248177097',
+    'message' => 'Exporteer ruwe logboeken'
+  },
+  'Passive Analytics' => {
+    'lastUpdated' => '1248175934',
+    'message' => 'Passieve analyse'
+  },
+  'Passive Analytics Settings' => {
+    'lastUpdated' => '1248176107',
+    'message' => 'Passive analyse instellingen'
+  },
+  'Regular Expression Error:' => {
+    'lastUpdated' => '1248176632',
+    'message' => 'Fout in reguliere expressie:'
+  },
+  'Summarize Passive Analytics' => {
+    'lastUpdated' => '1248176143',
+    'message' => 'Passieve anlyse samenvatten'
+  },
   'User' => {
     'lastUpdated' => '1239880308',
     'message' => 'Gebruiker'
+  },
+  'User help' => {
+    'lastUpdated' => '1248176885',
+    'message' => 'De gebruiker die een e-mail ontvangt wanneer de container is verwerkt.'
+  },
+  'already active' => {
+    'lastUpdated' => '1248176039',
+    'message' => 'Passieve analyse staat al aan. Probeer in de toekomst de gebruikersinterface niet te ondermijnen.'
+  },
+  'confirm delete rule' => {
+    'lastUpdated' => '1248176062',
+    'message' => 'Weet u zeker dat u deze regel wilt verwijderen?'
+  },
+  'currently running' => {
+    'lastUpdated' => '1248176088',
+    'message' => 'Een Passieve analyse wordt momenteel uitgevoerd.'
+  },
+  'default pause interval' => {
+    'lastUpdated' => '1248177331',
+    'message' => 'Standaard pauze tussen kliks'
+  },
+  'default pause interval help' => {
+    'lastUpdated' => '1248177371',
+    'message' => 'Stel het standaard pauze interval in dat de gebruiker ziet in het scherm van de passieve analyse.'
+  },
+  'error creating workflow' => {
+    'lastUpdated' => '1248177119',
+    'message' => 'Fout bij het aanmaken van de workflow instantie.'
+  },
+  'manage ruleset' => {
+    'lastUpdated' => '1248176159',
+    'message' => 'Beheer regels'
   },
   'other' => {
     'lastUpdated' => '1239876041',
     'message' => 'Anders'
   },
+  'pause interval' => {
+    'lastUpdated' => '1248177318',
+    'message' => 'Pauze tussen kliks'
+  },
+  'pause interval help' => {
+    'lastUpdated' => '1248177216',
+    'message' => 'Stel de tijd tussen kliks in die geïnterpreteerd wordt als en gebruiker die een pagina leest in tegenstelling tot het beginnen van een nieuwe browser sessie of het verlaten van de site.'
+  },
   'regexp' => {
     'lastUpdated' => '1239879897',
     'message' => 'Reguliere expressie'
+  },
+  'regexp help' => {
+    'lastUpdated' => '1248176577',
+    'message' => 'Definiëer een reguliere expressie om regels uit het logboek uit te kiezen voor deze container.<br>^ = begin van een url<br>$ = einde van een url<br>. = willekeurig karakter<br>* = willekeurig aantal<br>+ = 1 of meer<br>? = 0 of1<br>Meta karakters (karkters met functie) moeten met een backslash ge&euml;scaped worden als deze overeen moeten komen met de tekst. Bijv: home\\?func=match'
+  },
+  'workflow deleted' => {
+    'lastUpdated' => '1248176676',
+    'message' => 'De passieve analyse workflow is verwijdererd. Neem direct contact op met een beheerder.'
   }
 }
 ;
--- Dutch/Dutch/ShipDriver.pm
+++ Dutch/Dutch/ShipDriver.pm
@@ -1,27 +1,31 @@
 package WebGUI::i18n::Dutch::ShipDriver;
 use utf8;
-
 our $I18N = {
-	'enabled help' => {
-		message => q|Kunnen mensen die de winkel op de site gebruiken deze verzendings optie gebruiken?|,
-		lastUpdated => 1213625938
-	},
-
-	'label' => {
-		message => q|Label|,
-		lastUpdated => 1213625946
-	},
-
-	'enabled' => {
-		message => q|Aangezet?|,
-		lastUpdated => 1213625867
-	},
-
-	'label help' => {
-		message => q|Een naam voor de verzendings optie. Kies iets duidelijks wat 100 karakters of minder lang is.|,
-		lastUpdated => 1213625990
-	},
-
-};
+  'enabled' => {
+    'lastUpdated' => '1213625867',
+    'message' => 'Aangezet?'
+  },
+  'enabled help' => {
+    'lastUpdated' => '1213625938',
+    'message' => 'Kunnen mensen die de winkel op de site gebruiken deze verzendings optie gebruiken?'
+  },
+  'label' => {
+    'lastUpdated' => '1213625946',
+    'message' => 'Label'
+  },
+  'label help' => {
+    'lastUpdated' => '1213625990',
+    'message' => 'Een naam voor de verzendings optie. Kies iets duidelijks wat 100 karakters of minder lang is.'
+  },
+  'who can use' => {
+    'lastUpdated' => '1248175756',
+    'message' => 'Groep die deze verzendingsmethode kan gebruiken'
+  },
+  'who can use help' => {
+    'lastUpdated' => '1248175818',
+    'message' => 'Dit specificeerd welke groep deze verzendingsmethode mag gebruiken.'
+  }
+}
+;
 
 1;
