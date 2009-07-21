1a2aa79c4a4d0864763ad15ab2fb528ede8468fd
Translation Server <translation@plainblack.com>
2009-07-21 09:54:00 +0000

    Update from translation server

--- Dutch/Dutch/TaxDriver_EU.pm
+++ Dutch/Dutch/TaxDriver_EU.pm
@@ -1,5 +1,127 @@
 package WebGUI::i18n::Dutch::TaxDriver_EU;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'accept when vies unavailable' => {
+    'lastUpdated' => '1248169198',
+    'message' => 'Ongecontroleerde BTW nummers toestaan als de BTW nummercheckservice niet beschikbaar is?'
+  },
+  'accept when vies unavailable help' => {
+    'lastUpdated' => '1248169584',
+    'message' => 'Als een van de databases van de BTW nummercheckservice tijdelijk onbeschikbaar is of als de connectie niet tot stand gebracht kon worden, kunnen BTW nummers niet gecontroleerd worden. Meestal is dit een tijdelijk probleem. Als deze optie op ja staat, worden BTW nummers die niet gecontroleerd konden worden alsnog gebruikt. Let erop dat de samenstelling van BTW nummers altijd gecontroleerd wordt, of de nummercheckservice beschikbaar is of niet.'
+  },
+  'add' => {
+    'lastUpdated' => '1248168540',
+    'message' => 'Toevoegen'
+  },
+  'add vat group' => {
+    'lastUpdated' => '1248168566',
+    'message' => 'Voeg BTW groep toe'
+  },
+  'already has vat number' => {
+    'lastUpdated' => '1248168598',
+    'message' => 'Er is al een BTW nummer voor dit land geregistreerd.'
+  },
+  'approve' => {
+    'lastUpdated' => '1248168573',
+    'message' => 'Goedkeuren'
+  },
+  'auto vies approval' => {
+    'lastUpdated' => '1248169013',
+    'message' => 'Automatische BTW nummercheck goedkeuring?'
+  },
+  'auto vies approval help' => {
+    'lastUpdated' => '1248169148',
+    'message' => 'Als dit op ja gezet is, worden BTW nummers die door de BTW nummerchecksercive gecontroleerd zijn meteen bruikbaar voor de klanten. Als het op nee gezet is moeten de de nummers expliciet goedgekeurd zijn voordat ze bruikbaar zijn.'
+  },
+  'default group' => {
+    'lastUpdated' => '1248168753',
+    'message' => 'Standaard groep'
+  },
+  'delete group' => {
+    'lastUpdated' => '1248168678',
+    'message' => 'Verwijderen'
+  },
+  'deny' => {
+    'lastUpdated' => '1248168663',
+    'message' => 'Afkeuren'
+  },
+  'general configuration' => {
+    'lastUpdated' => '1248168781',
+    'message' => 'Algemene configuratie'
+  },
+  'group name' => {
+    'lastUpdated' => '1248168726',
+    'message' => 'Groepsnaam'
+  },
+  'illegal country code' => {
+    'lastUpdated' => '1248168799',
+    'message' => 'Ongeldige landcode'
+  },
+  'make default' => {
+    'lastUpdated' => '1248168741',
+    'message' => 'Stel in als standaard'
+  },
+  'rate' => {
+    'lastUpdated' => '1248168718',
+    'message' => 'Tarief'
+  },
+  'select country' => {
+    'lastUpdated' => '1248168765',
+    'message' => 'Selecteer een land'
+  },
+  'shop country' => {
+    'lastUpdated' => '1248168691',
+    'message' => 'Land van verblijf'
+  },
+  'shop country help' => {
+    'lastUpdated' => '1248170026',
+    'message' => 'Selecteer het land waar de winkel zich bevindt. Als het land niet in de lijst staat, is het land geen onderdeel van de Europese Unie en kan deze belastingapplicatie niet gebruikt worden.'
+  },
+  'user' => {
+    'lastUpdated' => '1248168553',
+    'message' => 'Gebruiker'
+  },
+  'user template' => {
+    'lastUpdated' => '1248168706',
+    'message' => 'Gebruiker scherm sjabloon'
+  },
+  'user template help' => {
+    'lastUpdated' => '1248168984',
+    'message' => 'Het sjabloon voor het scherm waar de gebruikers hun BTW nummers in kunnen voeren.'
+  },
+  'vat group' => {
+    'lastUpdated' => '1248168809',
+    'message' => 'BTW groep'
+  },
+  'vat groups' => {
+    'lastUpdated' => '1248168820',
+    'message' => 'BTW groepen'
+  },
+  'vat number' => {
+    'lastUpdated' => '1248168828',
+    'message' => 'BTW nummer'
+  },
+  'vat number invalid' => {
+    'lastUpdated' => '1248168850',
+    'message' => 'Het opgegeven BTW nummer is ongeldig.'
+  },
+  'vat numbers' => {
+    'lastUpdated' => '1248168861',
+    'message' => 'BTW nummers'
+  },
+  'vies error code' => {
+    'lastUpdated' => '1248168934',
+    'message' => 'BTW nummercheck foutcode'
+  },
+  'vies unavailable' => {
+    'lastUpdated' => '1248168650',
+    'message' => 'Nummer validatie momenteel niet beschikbaar. Probeer het later nogmaals.'
+  },
+  'vies validated' => {
+    'lastUpdated' => '1248168955',
+    'message' => 'BTW nummercheck gecontroleerd'
+  }
+}
+;
 
 1;
--- Dutch/Dutch/WebGUIProfile.pm
+++ Dutch/Dutch/WebGUIProfile.pm
@@ -1,296 +1,304 @@
 package WebGUI::i18n::Dutch::WebGUIProfile;
 use utf8;
 our $I18N = {
-  '466' => {
-    'lastUpdated' => 1216586665,
+  466 => {
+    'lastUpdated' => '1216586665',
     'message' => 'Weet u zeker dat u deze categorie en alle bijbehorende velden wilt verwijderen?'
   },
-  '467' => {
-    'lastUpdated' => 1154080674,
+  467 => {
+    'lastUpdated' => '1154080674',
     'message' => 'Weet u zeker dat u dit veld en alle daaraan&nbsp;gekoppelde data wilt verwijderen?'
   },
-  '469' => {
-    'lastUpdated' => 1154080715,
+  469 => {
+    'lastUpdated' => '1154080715',
     'message' => 'Id'
   },
-  '470' => {
-    'lastUpdated' => 1154080722,
+  470 => {
+    'lastUpdated' => '1154080722',
     'message' => 'Categorie naam'
   },
   '470 description' => {
-    'lastUpdated' => 1154080730,
+    'lastUpdated' => '1154080730',
     'message' => 'De naam van deze categorie'
   },
-  '471' => {
-    'lastUpdated' => 1154080751,
+  471 => {
+    'lastUpdated' => '1154080751',
     'message' => 'Bewerk gebruikersprofiel veld'
   },
-  '472' => {
-    'lastUpdated' => 1154080759,
+  472 => {
+    'lastUpdated' => '1154080759',
     'message' => 'Label'
   },
   '472 description' => {
-    'lastUpdated' => 1168959810,
+    'lastUpdated' => '1168959810',
     'message' => 'Een kort, beschrijvend label dat getoond wordt aan de gebruiker. Dit kan een call zijn naar het WebGUI Internationalisatie systeem als de labels gelocaliseerd moeten worden.'
   },
-  '473' => {
-    'lastUpdated' => 1154082756,
+  473 => {
+    'lastUpdated' => '1154082756',
     'message' => 'Zichtbaar?'
   },
   '473 description' => {
-    'lastUpdated' => 1154082772,
+    'lastUpdated' => '1154082772',
     'message' => 'Moet de categorie zichtbaar zijn voor gebruikers?'
   },
   '473a' => {
-    'lastUpdated' => 1154082781,
+    'lastUpdated' => '1154082781',
     'message' => 'Zichtbaar?'
   },
   '473a description' => {
-    'lastUpdated' => 1154082801,
+    'lastUpdated' => '1154082801',
     'message' => 'Moet het veld zichtbaar zijn voor gebruikers?'
   },
-  '474' => {
-    'lastUpdated' => 1154082810,
+  474 => {
+    'lastUpdated' => '1154082810',
     'message' => 'Vereist?'
   },
   '474 description' => {
-    'lastUpdated' => 1154082826,
+    'lastUpdated' => '1154082826',
     'message' => 'Moet dit veld verplicht worden ingevuld?'
   },
-  '475' => {
-    'lastUpdated' => 1154082856,
+  475 => {
+    'lastUpdated' => '1154082856',
     'message' => 'Veldnaam '
   },
   '475 description' => {
-    'lastUpdated' => 1154082879,
+    'lastUpdated' => '1154082879',
     'message' => 'Naam van het veld, wordt gebruikt in de database.'
   },
-  '486' => {
-    'lastUpdated' => 1155815745,
+  486 => {
+    'lastUpdated' => '1155815745',
     'message' => 'Data type'
   },
   '486 description' => {
-    'lastUpdated' => 1208856876,
+    'lastUpdated' => '1208856876',
     'message' => 'Kies het type veld. Deze keuze bepaalt ook welke validatie er gedaan moet worden.'
   },
-  '487' => {
-    'lastUpdated' => 1154613357,
+  487 => {
+    'lastUpdated' => '1154613357',
     'message' => 'Mogelijke waarden'
   },
   '487 description' => {
-    'lastUpdated' => 1155815753,
+    'lastUpdated' => '1155815753',
     'message' => 'Dit veld wordt gebruikt om de lijst-types te tonen (zoals Select List en Checkbox List).<br />Voer de waarde en sleutelparen,per lijn in.<br />Zie het voorbeeld hieronder:&nbsp;</p><p>{<br />&quot;database waarde 1&quot; =&gt; &quot;form label 1&quot;,<br />&quot;database waarde 2&quot; =&gt; &quot;form label 2&quot;,<br />&quot;database waarde 3&quot; =&gt; &quot;form label 3&quot;,<br />}'
   },
-  '488' => {
-    'lastUpdated' => 1154613963,
+  488 => {
+    'lastUpdated' => '1154613963',
     'message' => 'Standaard waarde(s)'
   },
   '488 description' => {
-    'lastUpdated' => 1154613951,
+    'lastUpdated' => '1154613951',
     'message' => 'Voer de standaard waarde (als er een is) voor het veld in. Voor ja/nee velden, voer &quot;ja&quot; in om Ja standaard te selecteren en voer &quot;nee&quot; in om standaard Nee te selecteren.'
   },
-  '489' => {
-    'lastUpdated' => 1154613673,
+  489 => {
+    'lastUpdated' => '1154613673',
     'message' => 'Profiel categorie'
   },
   '489 description' => {
-    'lastUpdated' => 1154614027,
+    'lastUpdated' => '1154614027',
     'message' => 'Selecteer een categorie waar dit veld onder geplaatst kan worden.'
   },
-  '490' => {
-    'lastUpdated' => 1154614038,
+  490 => {
+    'lastUpdated' => '1154614038',
     'message' => 'Voeg een profiel categorie toe.'
   },
-  '491' => {
-    'lastUpdated' => 1154614065,
+  491 => {
+    'lastUpdated' => '1154614065',
     'message' => 'Voeg een profielveld toe.'
   },
-  '492' => {
-    'lastUpdated' => 1154614086,
+  492 => {
+    'lastUpdated' => '1154614086',
     'message' => 'Profielvelden lijst'
   },
-  '787' => {
-    'lastUpdated' => 1155815773,
+  787 => {
+    'lastUpdated' => '1155815773',
     'message' => 'Bewerk dit profielveld.'
   },
-  '788' => {
-    'lastUpdated' => 1155815787,
+  788 => {
+    'lastUpdated' => '1155815787',
     'message' => 'Verwijder dit profielveld.'
   },
-  '789' => {
-    'lastUpdated' => 1155815803,
+  789 => {
+    'lastUpdated' => '1155815803',
     'message' => 'Bewerk profiel categorie.'
   },
-  '790' => {
-    'lastUpdated' => 1155815816,
+  790 => {
+    'lastUpdated' => '1155815816',
     'message' => 'Verwijder deze profiel categorie.'
   },
-  '897' => {
-    'lastUpdated' => 1155815863,
+  897 => {
+    'lastUpdated' => '1155815863',
     'message' => 'Bewerkbaar'
   },
   '897 description' => {
-    'lastUpdated' => 1155815895,
+    'lastUpdated' => '1155815895',
     'message' => 'Moet de categorie bewerkbaar zijn voor gebruikers?'
   },
   '897a' => {
-    'lastUpdated' => 1155815909,
+    'lastUpdated' => '1155815909',
     'message' => 'Bewerkbaar?'
   },
   '897a description' => {
-    'lastUpdated' => 1155815929,
+    'lastUpdated' => '1155815929',
     'message' => 'Moet een gebruiker dit veld kunnen bewerken?'
   },
   'account.options' => {
-    'lastUpdated' => 1214557426,
+    'lastUpdated' => '1214557426',
     'message' => 'Een link met een label naar een account optie.'
   },
   'category short name' => {
-    'lastUpdated' => 1227603858,
+    'lastUpdated' => '1227603858',
     'message' => 'Korte naam categorie'
   },
   'category short name description' => {
-    'lastUpdated' => 1227603878,
+    'lastUpdated' => '1227603878',
     'message' => 'Een verkorte naam voor deze categorie.'
   },
+  'default privacy setting description' => {
+    'lastUpdated' => '1248168513',
+    'message' => 'Selecteer de standaard privacy instelling voor dit profielveld. Dit wordt gebruikt bij het aanmaken van een nieuwe gebruiker.'
+  },
+  'default privacy setting label' => {
+    'lastUpdated' => '1248168524',
+    'message' => 'Standard privacy instelling'
+  },
   'displayTitle' => {
-    'lastUpdated' => 1214557397,
+    'lastUpdated' => '1214557397',
     'message' => 'Een titel die de gebruikersnaam bevat.'
   },
   'edit profile template body' => {
-    'lastUpdated' => 1214557376,
+    'lastUpdated' => '1214557376',
     'message' => 'Dit sjabloon bouwt een formulier voor de gebruiker waarin ze hun Gebruikersprofiel kunnen bewerken.'
   },
   'edit profile template title' => {
-    'lastUpdated' => 1214557335,
+    'lastUpdated' => '1214557335',
     'message' => 'Bewerk profiel sjabloon'
   },
   'forceImageOnly description' => {
-    'lastUpdated' => 1166637362,
+    'lastUpdated' => '1166637362',
     'message' => 'Wanneer uw profielveld vraagt om het toevoegen van een afbeelding, krijgt u een additioneel veld te zien dat het mogelijk maakt om alleen bestandstypen voor afbeeldingen toe te voegen.'
   },
   'forceImageOnly hoverHelp' => {
-    'lastUpdated' => 1166637399,
+    'lastUpdated' => '1166637399',
     'message' => 'Selecteer "Ja" om alleen bestandstypen voor afbeeldingen toe te staan.'
   },
   'forceImageOnly label' => {
-    'lastUpdated' => 1166637435,
+    'lastUpdated' => '1166637435',
     'message' => 'Sta alleen het toevoegen van afbeeldingen toe'
   },
   'profile field extras hoverHelp' => {
-    'lastUpdated' => 1225914448,
+    'lastUpdated' => '1225914448',
     'message' => 'Extra HTML die bij dit profielveld gevoegd moet worden.'
   },
   'profile field extras label' => {
-    'lastUpdated' => 1225914460,
+    'lastUpdated' => '1225914460',
     'message' => 'Extras'
   },
   'profile.accountOptions' => {
-    'lastUpdated' => 1214557321,
+    'lastUpdated' => '1214557321',
     'message' => 'Een functie die opties bevat voor acties met andere accounts, zoals het bewerken en bekijken van een profiel, het veranderen van je account en meer.'
   },
   'profile.category' => {
-    'lastUpdated' => 1214557260,
+    'lastUpdated' => '1214557260',
     'message' => 'De naam van de huidige categorie. Deze variabele bestaat alleen voor het eerste profielveld in elke categorie.'
   },
   'profile.elements' => {
-    'lastUpdated' => 1214557216,
+    'lastUpdated' => '1214557216',
     'message' => 'Een functie die alle elementen van de profielvelden ophaalt.'
   },
   'profile.extras' => {
-    'lastUpdated' => 1225914478,
+    'lastUpdated' => '1225914478',
     'message' => 'Extra HTML voor dit veld.'
   },
   'profile.form.cancel' => {
-    'lastUpdated' => 1222162909,
+    'lastUpdated' => '1222162909',
     'message' => 'Een knop waarmee de gebruiker teruggebracht wordt naar de laatste pagina die bekeken is en er geen data wordt ingevoerd.<br /> '
   },
   'profile.form.category' => {
-    'lastUpdated' => 1222162923,
+    'lastUpdated' => '1222162923',
     'message' => 'De naam van de categorie.'
   },
   'profile.form.category.loop' => {
-    'lastUpdated' => 1222162943,
+    'lastUpdated' => '1222162943',
     'message' => 'Een functie die alle velden in deze categorie ophaalt.'
   },
   'profile.form.element' => {
-    'lastUpdated' => 1222162965,
+    'lastUpdated' => '1222162965',
     'message' => 'Het formulier element voor dit profielveld.'
   },
   'profile.form.element.label' => {
-    'lastUpdated' => 1222162985,
+    'lastUpdated' => '1222162985',
     'message' => 'Het label dat bij dit profielveld hoort.'
   },
   'profile.form.element.subtext' => {
-    'lastUpdated' => 1222162858,
+    'lastUpdated' => '1222162858',
     'message' => 'Als dit profielveld vereist is, zal hier een asterisk &quot;*&quot; bij staan.'
   },
   'profile.form.elements' => {
-    'lastUpdated' => 1222162814,
+    'lastUpdated' => '1222162814',
     'message' => 'Een functie die alle profielvelden ophaalt per categorie.'
   },
   'profile.form.extras' => {
-    'lastUpdated' => 1225914496,
+    'lastUpdated' => '1225914496',
     'message' => 'Extra HTML voor dit veld.'
   },
   'profile.form.footer' => {
-    'lastUpdated' => 1214557540,
+    'lastUpdated' => '1214557540',
     'message' => 'HTML code waarmee het formulier wordt afgesloten.'
   },
   'profile.form.header' => {
-    'lastUpdated' => 1214557167,
+    'lastUpdated' => '1214557167',
     'message' => 'HTML code waarmee het formulier wordt begonnen'
   },
   'profile.form.hidden' => {
-    'lastUpdated' => 1214557146,
+    'lastUpdated' => '1214557146',
     'message' => 'HTML code om de actie van het formulier te geleiden.'
   },
   'profile.form.submit' => {
-    'lastUpdated' => 1222162775,
+    'lastUpdated' => '1222162775',
     'message' => 'Een knop waarmee de bewerkingen aan het profiel van de gebruiker worden ingevoerd.'
   },
   'profile.label' => {
-    'lastUpdated' => 1214557051,
+    'lastUpdated' => '1214557051',
     'message' => 'Het label voor dit profiel veld'
   },
   'profile.message' => {
-    'lastUpdated' => 1214557034,
+    'lastUpdated' => '1214557034',
     'message' => 'Berichten van het systeem, in het geval van fouten of als er actie van de gebruiker nodig is.'
   },
   'profile.value' => {
-    'lastUpdated' => 1214556980,
+    'lastUpdated' => '1214556980',
     'message' => 'De waarde van dit profiel veld'
   },
   'requiredForPasswordRecovery hoverHelp' => {
-    'lastUpdated' => 1166637614,
+    'lastUpdated' => '1166637614',
     'message' => 'Vraag gebruikers de inhoud van dit veld - bij voorkeur in combinatie met andere velden uit het profiel - op te geven wanneer ze verzoeken hun wachtwoord opnieuw toe te sturen. Alleen gebruikers die de inhoud van dit veld correct en direct weten op te geven kunnen hun wachtwoord opvragen.'
   },
   'requiredForPasswordRecovery label' => {
-    'lastUpdated' => 1166637652,
+    'lastUpdated' => '1166637652',
     'message' => 'Naar de inhoud van dit veld vragen bij het opnieuw opvragen van het wachtwoord?'
   },
   'showAtRegistration hoverHelp' => {
-    'lastUpdated' => 1166637733,
+    'lastUpdated' => '1166637733',
     'message' => 'Vraag gebruikers dit veld in te vullen bij registratie. Dit veld is niet verplicht, tenzij de optie verplicht ook wordt aangevinkt.'
   },
   'showAtRegistration label' => {
-    'lastUpdated' => 1166637761,
+    'lastUpdated' => '1166637761',
     'message' => 'Weergeven bij registratie?'
   },
   'topicName' => {
-    'lastUpdated' => 1155815942,
+    'lastUpdated' => '1155815942',
     'message' => 'Gebruikersprofiel'
   },
   'user profiling' => {
-    'lastUpdated' => 1157013417,
+    'lastUpdated' => '1157013417',
     'message' => 'Gebruikers profielen'
   },
   'view profile template body' => {
-    'lastUpdated' => 1214557489,
+    'lastUpdated' => '1214557489',
     'message' => 'Dit sjabloon wordt gebruikt voor het tonen van het Gebruikersprofiel aan de gebruiker.'
   },
   'view profile template title' => {
-    'lastUpdated' => 1214557503,
+    'lastUpdated' => '1214557503',
     'message' => 'Bekijk profiel sjabloon'
   }
 }
