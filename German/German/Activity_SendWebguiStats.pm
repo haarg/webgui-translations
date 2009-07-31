826e68d10540b1b7f3a9888b7baf3187562a00ae
Translation Server <translation@plainblack.com>
2009-07-31 13:30:29 +0000

    Update from translation server

--- German/German/Activity_SendWebguiStats.pm
+++ German/German/Activity_SendWebguiStats.pm
@@ -1,5 +1,27 @@
 package WebGUI::i18n::German::Activity_SendWebguiStats;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'disable' => {
+    'lastUpdated' => '1249046756',
+    'message' => 'Ausschalten'
+  },
+  'enable' => {
+    'lastUpdated' => '1249046764',
+    'message' => 'Einschalten'
+  },
+  'topicName' => {
+    'lastUpdated' => '1249046778',
+    'message' => 'WebGUI-Statistik senden'
+  },
+  'why to send' => {
+    'lastUpdated' => '1249046961',
+    'message' => 'Sie können auswählen, ob Sie statistische Informationen über Ihre WebGUI-Site an das zentrale WebGUI.org-Statistik-Repository senden möchten. Dies hilft den Entwicklern, WebGUI zu verbessern. Sie erhalten Informationen über die Größe der Sites, wie schnell sie wachsen und welche Assets am meisten benutzt werden. Es werden keine persönlichen Daten übertragen!'
+  },
+  'would you participate' => {
+    'lastUpdated' => '1249047013',
+    'message' => 'Möchten Sie am WebGUI-Community-Statistik-Programm teilnehmen?'
+  }
+}
+;
 
 1;
