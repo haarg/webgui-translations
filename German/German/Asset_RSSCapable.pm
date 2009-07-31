c9aa90764fa53de077170bd22cf401a691401055
Translation Server <translation@plainblack.com>
2009-07-31 13:31:43 +0000

    Update from translation server

--- German/German/Asset_RSSCapable.pm
+++ German/German/Asset_RSSCapable.pm
@@ -2,23 +2,31 @@ package WebGUI::i18n::German::Asset_RSSCapable;
 use utf8;
 our $I18N = {
   'assetName' => {
-    'lastUpdated' => 1164282518,
+    'lastUpdated' => '1164282518',
     'message' => 'RSSFähigkeit'
   },
   'rssEnabled hoverHelp' => {
-    'lastUpdated' => 1226019299,
+    'lastUpdated' => '1226019299',
     'message' => '<p>RSS-Feed für dieses Asset einschalten? Wenn eingeschaltet, wird ein RSS-from-Parent-Asset erzeugt und als zusätzliches Kind f&uuml;r diesen Zweck verwaltet, Wenn nicht eingeschaltet, wird kein solches Kind erzeugt und existierende werden gelöscht</p>'
   },
   'rssEnabled label' => {
-    'lastUpdated' => 1226019145,
+    'lastUpdated' => '1226019145',
     'message' => 'RSS einschalten'
   },
+  'rssLimit hoverHelp' => {
+    'lastUpdated' => '1249047078',
+    'message' => 'Wie viele RSS-Items sollen angezeigt werden?'
+  },
+  'rssLimit label' => {
+    'lastUpdated' => '1249047094',
+    'message' => 'RSS-Anzeigelimit'
+  },
   'rssTemplateId hoverHelp' => {
-    'lastUpdated' => 1226019132,
+    'lastUpdated' => '1226019132',
     'message' => 'Die Vorlage für den RSS-Feed dieses Assets'
   },
   'rssTemplateId label' => {
-    'lastUpdated' => 1226019107,
+    'lastUpdated' => '1226019107',
     'message' => 'RSS-Vorlage'
   }
 }
