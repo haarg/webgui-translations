07d560bffb64f7ee4a3efc7bdd3dca39374eac46
Translation Server <translation@plainblack.com>
2009-08-22 21:06:30 +0000

    Update from translation server

--- German/German/PayDriver_ExpressCheckout.pm
+++ German/German/PayDriver_ExpressCheckout.pm
@@ -1,5 +1,31 @@
 package WebGUI::i18n::German::PayDriver_ExpressCheckout;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'api' => {
+    'lastUpdated' => '1250975049',
+    'message' => 'API URL'
+  },
+  'api error' => {
+    'lastUpdated' => '1250975077',
+    'message' => 'Fehler bei der Kommunikation mit PayPal: %s'
+  },
+  'currency' => {
+    'lastUpdated' => '1250975094',
+    'message' => 'Währungscode'
+  },
+  'name' => {
+    'lastUpdated' => '1250975112',
+    'message' => 'PayPal Express Checkout'
+  },
+  'password' => {
+    'lastUpdated' => '1250975128',
+    'message' => 'Paßwort'
+  },
+  'paypal' => {
+    'lastUpdated' => '1250975146',
+    'message' => 'PayPal URL'
+  }
+}
+;
 
 1;
