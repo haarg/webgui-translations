2fe84efb7ad57ebad6941d7b09af7d00f6430e87
Translation Server <translation@plainblack.com>
2009-08-22 20:50:49 +0000

    Update from translation server

--- German/German/PayDriver_PayPal.pm
+++ German/German/PayDriver_PayPal.pm
@@ -1,5 +1,47 @@
 package WebGUI::i18n::German::PayDriver_PayPal;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'api' => {
+    'lastUpdated' => '1250974043',
+    'message' => 'API URL'
+  },
+  'api error' => {
+    'lastUpdated' => '1250974075',
+    'message' => 'Fehler bei der Kommunikation mit dem PayPal API: %s'
+  },
+  'currency' => {
+    'lastUpdated' => '1250974089',
+    'message' => 'Währungscode'
+  },
+  'name' => {
+    'lastUpdated' => '1250974168',
+    'message' => 'PayPal'
+  },
+  'password' => {
+    'lastUpdated' => '1250974106',
+    'message' => 'Paßwort'
+  },
+  'payment status' => {
+    'lastUpdated' => '1250974126',
+    'message' => 'Zahlungsstatus: %s'
+  },
+  'paypal' => {
+    'lastUpdated' => '1250974139',
+    'message' => 'PayPal URL'
+  },
+  'testMode' => {
+    'lastUpdated' => '1250974189',
+    'message' => 'Testmodus'
+  },
+  'user' => {
+    'lastUpdated' => '1250974150',
+    'message' => 'Benutzername'
+  },
+  'user help' => {
+    'lastUpdated' => '1250974226',
+    'message' => 'Benutzername PayPal Konto'
+  }
+}
+;
 
 1;
