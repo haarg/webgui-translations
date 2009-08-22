6fee1aaf1d597392533d46b13a4c3c1b0c5aa09a
Translation Server <translation@plainblack.com>
2009-08-22 23:34:30 +0000

    Update from translation server

--- German/German/Asset_Map.pm
+++ German/German/Asset_Map.pm
@@ -9,6 +9,26 @@ our $I18N = {
     'lastUpdated' => '1250971837',
     'message' => 'Landkarte'
   },
+  'error add unauthorized' => {
+    'lastUpdated' => '1250984055',
+    'message' => 'Sie haben keine Berechtigung, um Positionen hinzuzufügen'
+  },
+  'error delete unauthorized' => {
+    'lastUpdated' => '1250984023',
+    'message' => 'Sie haben keine Berechtigung, um diese Position zu löschen'
+  },
+  'error edit unauthorized' => {
+    'lastUpdated' => '1250984000',
+    'message' => 'Sie haben keine Berechtigung, um diese Position zu bearbeiten'
+  },
+  'error set center unauthorized' => {
+    'lastUpdated' => '1250983972',
+    'message' => 'Sie haben keine Berechtigung um den Standardansichtsbereich festzulegen'
+  },
+  'groupIdAddPoint description' => {
+    'lastUpdated' => '1250983937',
+    'message' => 'Gruppe, die Positionen zur Karte hinzufügen darf.'
+  },
   'groupIdAddPoint label' => {
     'lastUpdated' => '1250972470',
     'message' => 'Gruppe Position hinzufügen '
@@ -25,21 +45,37 @@ our $I18N = {
     'lastUpdated' => '1250971957',
     'message' => 'Holen Sie Ihren Google Maps API Schlüssel'
   },
+  'mapHeight description' => {
+    'lastUpdated' => '1250983905',
+    'message' => 'Höhe der erzeugten Karte. Geben Sie die Einheit an in px=Pixel oder %=Prozent'
+  },
   'mapHeight label' => {
     'lastUpdated' => '1250971972',
     'message' => 'Kartenhöhe'
   },
+  'mapWidth description' => {
+    'lastUpdated' => '1250983869',
+    'message' => 'Breite der erzeugten Karte. Geben Sie die Einheit an in px=Pixel oder %=Prozent'
+  },
   'mapWidth label' => {
     'lastUpdated' => '1250971984',
     'message' => 'Kartenbreite'
+  },
+  'message delete success' => {
+    'lastUpdated' => '1250983808',
+    'message' => 'Position gelöscht'
+  },
+  'message set center success' => {
+    'lastUpdated' => '1250983775',
+    'message' => 'Standardansichtsbereich erfolgreich definiert'
   },
   'message set point location' => {
     'lastUpdated' => '1250972486',
     'message' => 'Position aufgenommen'
   },
   'set default viewing area label' => {
-    'lastUpdated' => '1250972151',
-    'message' => 'Einstellung standard Ansichtsbereich'
+    'lastUpdated' => '1250983738',
+    'message' => 'Einstellung Standardansichtsbereich'
   },
   'startLatitude description' => {
     'lastUpdated' => '1250972509',
@@ -65,9 +101,17 @@ our $I18N = {
     'lastUpdated' => '1250972407',
     'message' => 'Startfaktor Vergrößerung'
   },
+  'templateIdEditPoint description' => {
+    'lastUpdated' => '1250983710',
+    'message' => 'Vorlage um eine Kartenposition zu bearbeiten. Erscheint innerhalb der Karte.'
+  },
   'templateIdEditPoint label' => {
-    'lastUpdated' => '1250972571',
-    'message' => 'Vorlaage Bearbeitung Position'
+    'lastUpdated' => '1250983640',
+    'message' => 'Vorlage Bearbeitung Position'
+  },
+  'templateIdView description' => {
+    'lastUpdated' => '1250983661',
+    'message' => 'Vorlage für die Kartenansicht'
   },
   'templateIdView label' => {
     'lastUpdated' => '1250972607',
@@ -86,8 +130,8 @@ our $I18N = {
     'message' => 'Der Workflow, der abläuft, wenn eine Position hinzugefügt oder bearbeitet wird. '
   },
   'workflowIdPoint label' => {
-    'lastUpdated' => '1250972456',
-    'message' => 'Wrokflow zur Freigabe Kartenpositionen'
+    'lastUpdated' => '1250983513',
+    'message' => 'Workflow zur Freigabe Kartenpositionen'
   }
 }
 ;
