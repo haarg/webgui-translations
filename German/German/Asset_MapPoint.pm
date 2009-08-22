e22400467322f75c9fe9618f2aaaa04f707fad0c
Translation Server <translation@plainblack.com>
2009-08-22 20:34:00 +0000

    Update from translation server

--- German/German/Asset_MapPoint.pm
+++ German/German/Asset_MapPoint.pm
@@ -1,5 +1,115 @@
 package WebGUI::i18n::German::Asset_MapPoint;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'address1 description' => {
+    'lastUpdated' => '1250972984',
+    'message' => 'Erste Zeile der Adresse'
+  },
+  'address1 label' => {
+    'lastUpdated' => '1250972954',
+    'message' => 'Adresse 1'
+  },
+  'address2 description' => {
+    'lastUpdated' => '1250973002',
+    'message' => 'Zweite Zeile der Adresse'
+  },
+  'address2 label' => {
+    'lastUpdated' => '1250972962',
+    'message' => 'Adresse 2'
+  },
+  'assetName' => {
+    'lastUpdated' => '1250972940',
+    'message' => 'Kartenposition'
+  },
+  'city description' => {
+    'lastUpdated' => '1250973029',
+    'message' => 'Ortsname zur Kartenposition'
+  },
+  'city label' => {
+    'lastUpdated' => '1250972928',
+    'message' => 'Ort'
+  },
+  'country description' => {
+    'lastUpdated' => '1250973043',
+    'message' => 'Land zur Kartenposition'
+  },
+  'country label' => {
+    'lastUpdated' => '1250972921',
+    'message' => 'Land'
+  },
+  'email description' => {
+    'lastUpdated' => '1250973063',
+    'message' => 'Emailadresse zur Kartenposition'
+  },
+  'email label' => {
+    'lastUpdated' => '1250972913',
+    'message' => 'Email'
+  },
+  'fax description' => {
+    'lastUpdated' => '1250973075',
+    'message' => 'Fax zur Kartenposition'
+  },
+  'fax label' => {
+    'lastUpdated' => '1250972905',
+    'message' => 'Fax'
+  },
+  'latitude description' => {
+    'lastUpdated' => '1250973101',
+    'message' => 'Geografische Breite (Latitude) der Position'
+  },
+  'latitude label' => {
+    'lastUpdated' => '1250972899',
+    'message' => 'Geografische Breite'
+  },
+  'longitude description' => {
+    'lastUpdated' => '1250973126',
+    'message' => 'Geografische Länge (Longitude) der Kartenposition'
+  },
+  'longitude label' => {
+    'lastUpdated' => '1250972887',
+    'message' => 'Geografische Länge'
+  },
+  'phone description' => {
+    'lastUpdated' => '1250973141',
+    'message' => 'Telefonnummer zur Position'
+  },
+  'phone label' => {
+    'lastUpdated' => '1250972874',
+    'message' => 'Telefon'
+  },
+  'state description' => {
+    'lastUpdated' => '1250973154',
+    'message' => 'Bundesland zur Kartenposition'
+  },
+  'state label' => {
+    'lastUpdated' => '1250972863',
+    'message' => 'Bundesland'
+  },
+  'storageIdPhoto description' => {
+    'lastUpdated' => '1250973187',
+    'message' => 'Bild der Kartenposition'
+  },
+  'storageIdPhoto label' => {
+    'lastUpdated' => '1250972851',
+    'message' => 'Bild'
+  },
+  'website description' => {
+    'lastUpdated' => '1250973211',
+    'message' => 'Eine Website URL'
+  },
+  'website label' => {
+    'lastUpdated' => '1250972842',
+    'message' => 'Website URL'
+  },
+  'zipCode description' => {
+    'lastUpdated' => '1250973230',
+    'message' => 'Postleitzahl zur Kartenposition'
+  },
+  'zipCode label' => {
+    'lastUpdated' => '1250972832',
+    'message' => 'Postleitzahl'
+  }
+}
+;
 
 1;
