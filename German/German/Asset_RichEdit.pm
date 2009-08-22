0f2e17443aed094ba4d648e30338c684f313bb12
Translation Server <translation@plainblack.com>
2009-08-22 11:57:40 +0000

    Update from translation server

--- German/German/Asset_RichEdit.pm
+++ German/German/Asset_RichEdit.pm
@@ -2,419 +2,423 @@ package WebGUI::i18n::German::Asset_RichEdit;
 use utf8;
 our $I18N = {
   'Copy' => {
-    'lastUpdated' => 1226013097,
+    'lastUpdated' => '1226013097',
     'message' => 'Kopieren'
   },
   'Server Side Spell Checker' => {
-    'lastUpdated' => 1215500479,
+    'lastUpdated' => '1215500479',
     'message' => 'Serverseitige Rechtschreibprüfung'
   },
+  'Toggle editor' => {
+    'lastUpdated' => '1250942248',
+    'message' => 'Editor wechseln'
+  },
   'advhr' => {
-    'lastUpdated' => 1215500555,
+    'lastUpdated' => '1215500555',
     'message' => '<p>Erweiterte horizontale Linie </p>'
   },
   'anchor' => {
-    'lastUpdated' => 1215500565,
+    'lastUpdated' => '1215500565',
     'message' => 'Anker'
   },
   'assetName' => {
-    'lastUpdated' => 1215500578,
+    'lastUpdated' => '1215500578',
     'message' => 'Rich Editor'
   },
   'backcolor' => {
-    'lastUpdated' => 1215500590,
+    'lastUpdated' => '1215500590',
     'message' => 'Hintergrundfarbe'
   },
   'bold' => {
-    'lastUpdated' => 1215500601,
+    'lastUpdated' => '1215500601',
     'message' => 'Fett'
   },
   'bottom' => {
-    'lastUpdated' => 1215500657,
+    'lastUpdated' => '1215500657',
     'message' => 'Ende'
   },
   'bullist' => {
-    'lastUpdated' => 1215500670,
+    'lastUpdated' => '1215500670',
     'message' => 'Bullet List'
   },
   'button' => {
-    'lastUpdated' => 1215500680,
+    'lastUpdated' => '1215500680',
     'message' => 'Schaltfläche'
   },
   'charmap' => {
-    'lastUpdated' => 1215500693,
+    'lastUpdated' => '1215500693',
     'message' => 'Sonderzeichen'
   },
   'cleanup' => {
-    'lastUpdated' => 1215500708,
+    'lastUpdated' => '1215500708',
     'message' => 'Code aufräumen'
   },
   'code' => {
-    'lastUpdated' => 1215500753,
+    'lastUpdated' => '1215500753',
     'message' => 'Quellcode betrachten/bearbeiten'
   },
   'collateral' => {
-    'lastUpdated' => 1215500766,
+    'lastUpdated' => '1215500766',
     'message' => 'WebGUI Makro'
   },
   'copy' => {
-    'lastUpdated' => 1215500775,
+    'lastUpdated' => '1215500775',
     'message' => 'Kopieren'
   },
   'css file' => {
-    'lastUpdated' => 1215500791,
+    'lastUpdated' => '1215500791',
     'message' => 'CSS Datei'
   },
   'css file description' => {
-    'lastUpdated' => 1215500852,
+    'lastUpdated' => '1215500852',
     'message' => 'Gibt eine CSS Datei an, die für das Layout des Editorinhalts benutzt wird. Falls nicht angegeben, wird ein Standard CSS benutzt.'
   },
   'cut' => {
-    'lastUpdated' => 1215500863,
+    'lastUpdated' => '1215500863',
     'message' => 'Ausschneiden'
   },
   'directionality' => {
-    'lastUpdated' => 1215500875,
+    'lastUpdated' => '1215500875',
     'message' => 'Textausrichtung'
   },
   'directionality description' => {
-    'lastUpdated' => 1215500918,
+    'lastUpdated' => '1215500918',
     'message' => 'Erlaubt Behandlung von Sprachen, die von rechts nach links bzw. von links nach rechts geschrieben werden.'
   },
   'disable rich edit' => {
-    'lastUpdated' => 1215500935,
+    'lastUpdated' => '1215500935',
     'message' => 'Diesen Rich Editor deaktivieren?'
   },
   'disable rich edit description' => {
-    'lastUpdated' => 1215501116,
+    'lastUpdated' => '1215501116',
     'message' => 'Deaktiviert den Rich Editor systemweit, ohne Interaktion der Benutzer.'
   },
   'editForm allowMedia description' => {
-    'lastUpdated' => 1215502288,
+    'lastUpdated' => '1215502288',
     'message' => 'Falls hier &quot;ja&quot; gesetzt ist, wird das Einbetten von Flash und anderen Medien durch den Editor erlaubt. Hinweis: Das Setzen auf &quot;nein&quot; verbietet Flash nicht explizit.'
   },
   'editForm allowMedia label' => {
-    'lastUpdated' => 1215501130,
+    'lastUpdated' => '1215501130',
     'message' => 'Medien erlauben'
   },
   'editor height' => {
-    'lastUpdated' => 1215501146,
+    'lastUpdated' => '1215501146',
     'message' => 'Höhe des Editors'
   },
   'editor height description' => {
-    'lastUpdated' => 1215501274,
+    'lastUpdated' => '1215501274',
     'message' => 'Die Höhe des gesamten Editors, inklusive der Menüs, gemessen in Pixel.'
   },
   'editor width' => {
-    'lastUpdated' => 1215501285,
+    'lastUpdated' => '1215501285',
     'message' => 'Breite des Editors'
   },
   'editor width description' => {
-    'lastUpdated' => 1215501298,
+    'lastUpdated' => '1215501298',
     'message' => 'Die Breite des gesamten Editors, inklusive der Menüs, gemessen in Pixel.'
   },
   'elements' => {
-    'lastUpdated' => 1215501312,
+    'lastUpdated' => '1215501312',
     'message' => 'Gültige Elemente'
   },
   'elements description' => {
-    'lastUpdated' => 1215501378,
+    'lastUpdated' => '1215501378',
     'message' => 'Eine Liste von HTML Tags und ihrer Attribute, die der Editor akzeptiert. Alle anderen werden aus dem Quellcode entfernt. Um alles zu erlauben, setzen Sie *[*].'
   },
   'elements subtext' => {
-    'lastUpdated' => 1215501466,
+    'lastUpdated' => '1215501466',
     'message' => 'Alles in einer Zeile, keine Zeilenumschaltung und im Format tag1[attr1|attr2],tag2[attr1].'
   },
   'emotions' => {
-    'lastUpdated' => 1215501477,
+    'lastUpdated' => '1215501477',
     'message' => 'Emoticons'
   },
   'enable context menu' => {
-    'lastUpdated' => 1215501489,
+    'lastUpdated' => '1215501489',
     'message' => 'Kontextmenü aktivieren'
   },
   'enable context menu description' => {
-    'lastUpdated' => 1215501520,
+    'lastUpdated' => '1215501520',
     'message' => 'Fügt das Kontextmen&uuml; zur Werkzeugleiste in Reihe 1 hinzu.'
   },
   'fontselect' => {
-    'lastUpdated' => 1215501544,
+    'lastUpdated' => '1215501544',
     'message' => 'Schriftart'
   },
   'fontsizeselect' => {
-    'lastUpdated' => 1215501556,
+    'lastUpdated' => '1215501556',
     'message' => 'Schriftgröße'
   },
   'forecolor' => {
-    'lastUpdated' => 1211352279,
+    'lastUpdated' => '1211352279',
     'message' => 'Vordergrundfarbe'
   },
   'formatselect' => {
-    'lastUpdated' => 1211352295,
+    'lastUpdated' => '1211352295',
     'message' => 'Format übernehmen'
   },
   'fullscreen' => {
-    'lastUpdated' => 1215501648,
+    'lastUpdated' => '1215501648',
     'message' => 'Vollbild'
   },
   'help' => {
-    'lastUpdated' => 1215501636,
+    'lastUpdated' => '1215501636',
     'message' => 'Hilfe'
   },
   'hr' => {
-    'lastUpdated' => 1215501662,
+    'lastUpdated' => '1215501662',
     'message' => 'Horizontale Linie einfügen'
   },
   'iespell' => {
-    'lastUpdated' => 1215501695,
+    'lastUpdated' => '1215501695',
     'message' => 'Rechtschreibprüfung (nur IE)'
   },
   'image' => {
-    'lastUpdated' => 1215501703,
+    'lastUpdated' => '1215501703',
     'message' => 'Bild'
   },
   'indent' => {
-    'lastUpdated' => 1215501722,
+    'lastUpdated' => '1215501722',
     'message' => 'Zeileneinzug'
   },
   'inline popups' => {
-    'lastUpdated' => 1215501743,
+    'lastUpdated' => '1215501743',
     'message' => 'Inline Popups'
   },
   'inline popups description' => {
-    'lastUpdated' => 1215501771,
+    'lastUpdated' => '1215501771',
     'message' => 'Zeigt Popups inline anstatt in einem neuen Browser Fenster.'
   },
   'insertImage' => {
-    'lastUpdated' => 1215501782,
+    'lastUpdated' => '1215501782',
     'message' => 'WebGUI Bild'
   },
   'insertdate' => {
-    'lastUpdated' => 1215501818,
+    'lastUpdated' => '1215501818',
     'message' => 'Einfügedatum'
   },
   'inserttime' => {
-    'lastUpdated' => 1215501828,
+    'lastUpdated' => '1215501828',
     'message' => 'Einfügezeit'
   },
   'italic' => {
-    'lastUpdated' => 1215501843,
+    'lastUpdated' => '1215501843',
     'message' => 'Kursiv'
   },
   'justifycenter' => {
-    'lastUpdated' => 1215502148,
+    'lastUpdated' => '1215502148',
     'message' => 'Zentriert ausrichten'
   },
   'justifyfull' => {
-    'lastUpdated' => 1215502128,
+    'lastUpdated' => '1215502128',
     'message' => 'Als Blocksatz ausrichten'
   },
   'justifyleft' => {
-    'lastUpdated' => 1215502160,
+    'lastUpdated' => '1215502160',
     'message' => 'Links ausrichten'
   },
   'justifyright' => {
-    'lastUpdated' => 1215502173,
+    'lastUpdated' => '1215502173',
     'message' => 'Rechts ausrichten'
   },
   'left to right' => {
-    'lastUpdated' => 1215502049,
+    'lastUpdated' => '1215502049',
     'message' => 'Von links nach rechts'
   },
   'link' => {
-    'lastUpdated' => 1215502080,
+    'lastUpdated' => '1215502080',
     'message' => 'Erzeuge Hyperlink'
   },
   'media' => {
-    'lastUpdated' => 1215502317,
+    'lastUpdated' => '1215502317',
     'message' => 'Eingebettete Medien / Flash'
   },
   'no dictionaries' => {
-    'lastUpdated' => 1215502411,
+    'lastUpdated' => '1215502411',
     'message' => 'Es wurden keine Wörterbücher konfiguriert. Die Rechtschreibpr&uuml;fung wird deshalb nicht aktiviert.'
   },
   'no wrap' => {
-    'lastUpdated' => 1215502441,
+    'lastUpdated' => '1215502441',
     'message' => 'Text im Editor nicht umbrechen?'
   },
   'no wrap description' => {
-    'lastUpdated' => 1215502480,
+    'lastUpdated' => '1215502480',
     'message' => 'Verhindert das umbrechen von Text/Wörtern im Editor.'
   },
   'numlist' => {
-    'lastUpdated' => 1215502495,
+    'lastUpdated' => '1215502495',
     'message' => 'Numerierte Liste'
   },
   'outdent' => {
-    'lastUpdated' => 1215502555,
+    'lastUpdated' => '1215502555',
     'message' => 'Ausrücken'
   },
   'pagetree' => {
-    'lastUpdated' => 1215502576,
+    'lastUpdated' => '1215502576',
     'message' => 'WebGUI Seitenbaum Link'
   },
   'paste' => {
-    'lastUpdated' => 1215502600,
+    'lastUpdated' => '1215502600',
     'message' => 'Einfügen'
   },
   'pastetext' => {
-    'lastUpdated' => 1215502615,
+    'lastUpdated' => '1215502615',
     'message' => 'Füge als reinen Text ein'
   },
   'pasteword' => {
-    'lastUpdated' => 1215502629,
+    'lastUpdated' => '1215502629',
     'message' => 'Füge aus Word ein'
   },
   'preformatted' => {
-    'lastUpdated' => 1215502706,
+    'lastUpdated' => '1215502706',
     'message' => 'Erhalte Leerzeichen als vorformatierten Text?'
   },
   'preformatted description' => {
-    'lastUpdated' => 1215503147,
+    'lastUpdated' => '1215503147',
     'message' => 'Normalerweise werden in HTML mehrere Leerzeichen, Tabs und Zeilenumschaltungen als ein einzelnes Leerzeichen interpretiert. Falls diese Option gesetzt ist, bleiben alle Leerzeichen als solche erhalten.'
   },
   'preview' => {
-    'lastUpdated' => 1215503418,
+    'lastUpdated' => '1215503418',
     'message' => 'Vorschau'
   },
   'print' => {
-    'lastUpdated' => 1215503428,
+    'lastUpdated' => '1215503428',
     'message' => 'Drucken'
   },
   'redo' => {
-    'lastUpdated' => 1215503457,
+    'lastUpdated' => '1215503457',
     'message' => 'Redo'
   },
   'remove line breaks' => {
-    'lastUpdated' => 1215503492,
+    'lastUpdated' => '1215503492',
     'message' => 'Zeilenumbrüche aus HTML entfernen?'
   },
   'remove line breaks description' => {
-    'lastUpdated' => 1215503517,
+    'lastUpdated' => '1215503517',
     'message' => 'Entfernt Zeilenumbrüche aus generiertem HTML.'
   },
   'removeformat' => {
-    'lastUpdated' => 1215503544,
+    'lastUpdated' => '1215503544',
     'message' => 'Entferne Formatierung'
   },
   'replace' => {
-    'lastUpdated' => 1215503556,
+    'lastUpdated' => '1215503556',
     'message' => 'Suchen und Ersetzen'
   },
   'rich edit edit config' => {
-    'lastUpdated' => 1215503582,
+    'lastUpdated' => '1215503582',
     'message' => 'Konfiguration des Rich Editors'
   },
   'right to left' => {
-    'lastUpdated' => 1215503610,
+    'lastUpdated' => '1215503610',
     'message' => 'Rechts nach links'
   },
   'row 1' => {
-    'lastUpdated' => 1215503628,
+    'lastUpdated' => '1215503628',
     'message' => 'Reihe 1'
   },
   'row 2' => {
-    'lastUpdated' => 1215503638,
+    'lastUpdated' => '1215503638',
     'message' => 'Reihe 2'
   },
   'row 3' => {
-    'lastUpdated' => 1215503648,
+    'lastUpdated' => '1215503648',
     'message' => 'Reihe 3'
   },
   'save' => {
-    'lastUpdated' => 1215503664,
+    'lastUpdated' => '1215503664',
     'message' => 'Speichern / Übertragen'
   },
   'search' => {
-    'lastUpdated' => 1215503674,
+    'lastUpdated' => '1215503674',
     'message' => 'Suchen'
   },
   'source editor height' => {
-    'lastUpdated' => 1215503691,
+    'lastUpdated' => '1215503691',
     'message' => 'Quellcode Editor Höhe'
   },
   'source editor height description' => {
-    'lastUpdated' => 1215508149,
+    'lastUpdated' => '1215508149',
     'message' => 'Die Höhe des Bereichs des Rich Editors, in dem der Benutzer Quellcode eingeben kann, gemessen in Pixel.'
   },
   'source editor width' => {
-    'lastUpdated' => 1215503706,
+    'lastUpdated' => '1215503706',
     'message' => 'Quellcode Editor Breite'
   },
   'source editor width description' => {
-    'lastUpdated' => 1215508165,
+    'lastUpdated' => '1215508165',
     'message' => 'Die Breite des Bereichs des Rich Editors, in dem der Benutzer Quellcode eingeben kann, gemessen in Pixel.'
   },
   'strikethrough' => {
-    'lastUpdated' => 1223577059,
+    'lastUpdated' => '1223577059',
     'message' => 'Durchstreichen'
   },
   'styleselect' => {
-    'lastUpdated' => 1215508245,
+    'lastUpdated' => '1215508245',
     'message' => 'Style anwenden'
   },
   'sub' => {
-    'lastUpdated' => 1215508274,
+    'lastUpdated' => '1215508274',
     'message' => 'tiefgestellter Index'
   },
   'sup' => {
-    'lastUpdated' => 1215508305,
+    'lastUpdated' => '1215508305',
     'message' => 'oberer Index'
   },
   'tablecontrols' => {
-    'lastUpdated' => 1215508330,
+    'lastUpdated' => '1215508330',
     'message' => 'Steuerelemente für Tabellen'
   },
   'toolbar buttons' => {
-    'lastUpdated' => 1215508355,
+    'lastUpdated' => '1215508355',
     'message' => 'Schaltflächen der Werkzeugleiste'
   },
   'toolbar buttons description' => {
-    'lastUpdated' => 1229992226,
+    'lastUpdated' => '1229992226',
     'message' => '<p>Zeigt, welche Schaltflächen der Werkzeugleiste in einer bestimmten Werkzeugleiste benutzt werden. Um eine Werkzeugleiste zu entfernen, deaktivieren Sie alle Schaltfl&auml;chen in der Werkzeugleistenspalte. Beachten Sie, dass andere Felder Schaltfl&auml;chen zu einer Werkzeugleiste hinzufügen können, wie z.B. &quot;Aktiviere das Kontextmen&uuml;&quot;. Hinzuf&uuml;gen von Schaltfl&auml;chen f&uuml;gt noch keine neuen Funktionen hinzu! Sie m&uuml;ssen ebenso die &quot;Valid Elements&quot; f&uuml;r diesen Editor aktualisieren.</p>'
   },
   'toolbar location' => {
-    'lastUpdated' => 1215508544,
+    'lastUpdated' => '1215508544',
     'message' => 'Lage der Werkzeugleiste'
   },
   'toolbar location description' => {
-    'lastUpdated' => 1215508917,
+    'lastUpdated' => '1215508917',
     'message' => 'Bestimmt, ob die Werkzeugleisten ober- oder unterhalb der Eingabemaske platziert werden.'
   },
   'top' => {
-    'lastUpdated' => 1215508647,
+    'lastUpdated' => '1215508647',
     'message' => 'Anfang'
   },
   'underline' => {
-    'lastUpdated' => 1215508678,
+    'lastUpdated' => '1215508678',
     'message' => 'Unterstreichung'
   },
   'undo' => {
-    'lastUpdated' => 1215508687,
+    'lastUpdated' => '1215508687',
     'message' => 'Undo'
   },
   'unlink' => {
-    'lastUpdated' => 1215508715,
+    'lastUpdated' => '1215508715',
     'message' => 'Unlink'
   },
   'use br' => {
-    'lastUpdated' => 1215508745,
+    'lastUpdated' => '1215508745',
     'message' => 'Benutze &lt;br/&gt; anstatt &lt;p&gt;, falls &quot;Enter&quot; gedrückt wird?'
   },
   'use br description' => {
-    'lastUpdated' => 1215508786,
+    'lastUpdated' => '1215508786',
     'message' => 'Benutze &lt;BR&gt; Tag anstatt &lt;P&gt; Tag, wenn eine neue Zeile im Editor angefangen wird.'
   },
   'using rich edit' => {
-    'lastUpdated' => 1215508815,
+    'lastUpdated' => '1215508815',
     'message' => 'Benutzer fragen, ob der Rich Editor benutzt werden soll?'
   },
   'using rich edit description' => {
-    'lastUpdated' => 1215508894,
+    'lastUpdated' => '1215508894',
     'message' => 'Frage den Benutzer, ob der Rich Editor für diese Eingabemaske benutzt werden soll (bei jedem Zugriff darauf).'
   },
   'visualaid' => {
-    'lastUpdated' => 1215508975,
+    'lastUpdated' => '1215508975',
     'message' => 'Umschalten auf Visuelle Hilfe für Tabellen'
   }
 }
