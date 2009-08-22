8d04bde739caa40317b5b428fc6debf14ac96996
Translation Server <translation@plainblack.com>
2009-08-22 20:26:49 +0000

    Update from translation server

--- German/German/Asset_Map.pm
+++ German/German/Asset_Map.pm
@@ -1,5 +1,95 @@
 package WebGUI::i18n::German::Asset_Map;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'add point label' => {
+    'lastUpdated' => '1250972542',
+    'message' => 'Position hinzufügen'
+  },
+  'assetName' => {
+    'lastUpdated' => '1250971837',
+    'message' => 'Landkarte'
+  },
+  'groupIdAddPoint label' => {
+    'lastUpdated' => '1250972470',
+    'message' => 'Gruppe Position hinzufügen '
+  },
+  'mapApiKey description' => {
+    'lastUpdated' => '1250971899',
+    'message' => 'Der Google Maps API Schlüssel für diese Website'
+  },
+  'mapApiKey label' => {
+    'lastUpdated' => '1250971919',
+    'message' => 'Google Maps Schlüssel'
+  },
+  'mapApiKey link' => {
+    'lastUpdated' => '1250971957',
+    'message' => 'Holen Sie Ihren Google Maps API Schlüssel'
+  },
+  'mapHeight label' => {
+    'lastUpdated' => '1250971972',
+    'message' => 'Kartenhöhe'
+  },
+  'mapWidth label' => {
+    'lastUpdated' => '1250971984',
+    'message' => 'Kartenbreite'
+  },
+  'message set point location' => {
+    'lastUpdated' => '1250972486',
+    'message' => 'Position aufgenommen'
+  },
+  'set default viewing area label' => {
+    'lastUpdated' => '1250972151',
+    'message' => 'Einstellung standard Ansichtsbereich'
+  },
+  'startLatitude description' => {
+    'lastUpdated' => '1250972509',
+    'message' => 'Geografische Breite der Startposition auf der Landkarte'
+  },
+  'startLatitude label' => {
+    'lastUpdated' => '1250972225',
+    'message' => 'Start geografische Breite (Latitude)'
+  },
+  'startLongitude description' => {
+    'lastUpdated' => '1250972783',
+    'message' => 'Geografische Länge zur Ausgangsposition auf der Karte'
+  },
+  'startLongitude label' => {
+    'lastUpdated' => '1250972338',
+    'message' => 'Start geografischer Länge (Longitude)'
+  },
+  'startZoom description' => {
+    'lastUpdated' => '1250972523',
+    'message' => 'Vergrößerungsfaktor des standard Startposition auf der Landkarte'
+  },
+  'startZoom label' => {
+    'lastUpdated' => '1250972407',
+    'message' => 'Startfaktor Vergrößerung'
+  },
+  'templateIdEditPoint label' => {
+    'lastUpdated' => '1250972571',
+    'message' => 'Vorlaage Bearbeitung Position'
+  },
+  'templateIdView label' => {
+    'lastUpdated' => '1250972607',
+    'message' => 'Vorlage Kartenansicht'
+  },
+  'templateIdViewPoint description' => {
+    'lastUpdated' => '1250972721',
+    'message' => 'Vorlage zur Ansicht einer Kartenposition. Diese erscheint innerhalb der Karte.'
+  },
+  'templateIdViewPoint label' => {
+    'lastUpdated' => '1250972589',
+    'message' => 'Vorlage Ansicht Position'
+  },
+  'workflowIdPoint description' => {
+    'lastUpdated' => '1250972666',
+    'message' => 'Der Workflow, der abläuft, wenn eine Position hinzugefügt oder bearbeitet wird. '
+  },
+  'workflowIdPoint label' => {
+    'lastUpdated' => '1250972456',
+    'message' => 'Wrokflow zur Freigabe Kartenpositionen'
+  }
+}
+;
 
 1;
