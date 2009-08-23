72238beaed63c6d621c75716813997fec137a87a
Translation Server <translation@plainblack.com>
2009-08-23 19:36:19 +0000

    Update from translation server

--- German/German/Asset_SyndicatedContent.pm
+++ German/German/Asset_SyndicatedContent.pm
@@ -1,233 +1,187 @@
 package WebGUI::i18n::German::Asset_SyndicatedContent;
 use utf8;
-
-
 our $I18N = {
-	'displayMode' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'rss.url.1.0' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'rss.url.2.0' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'rssUrl' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'72 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'assetName' => {
-		message => q|Syndizierte Inhalte|,
-		lastUpdated => 1179838422
-	},
-
-	'rss.url.0.91' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'site_link' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'channel.link' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'displayModeLabel' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'1' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'syndicated content asset template variables title' => {
-		message => q|Syndizierte Inhalte Asset Template Variablen|,
-		lastUpdated => 1170406566
-	},
-
-	'72' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'1 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'get syndicated content' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'hasTermsLabel description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'process macros in rss url' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'rssTabName' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'item_loop' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'hasTerms' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'site_title' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'rss.url' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'hasTermsLabel' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'title' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'interleaved' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'3 description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'cache timeout help' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'channel.title' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'displayModeSubtext' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'channel.description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'process macros in rss url description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'cache timeout' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'displayModeLabel description' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'templateId' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'link' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'grouped' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'3' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'cacheTimeout' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'maxHeadlines' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'new_rss_site' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'4' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'rss.url.0.9' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'RSS Feed Title Suffix' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-	'processMacrosInRssUrl' => {
-		message => q||,
-		lastUpdated => 
-	},
-
-};
+  1 => {
+    'lastUpdated' => '1251052536',
+    'message' => 'URL zur RSS-Datei'
+  },
+  '1 description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  3 => {
+    'lastUpdated' => '1251052566',
+    'message' => 'Maximale Anzahl der Überschriften'
+  },
+  '3 description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  4 => {
+    'lastUpdated' => '1251052590',
+    'message' => 'Syndizierten Inhalt bearbeiten'
+  },
+  72 => {
+    'lastUpdated' => '1251052617',
+    'message' => 'Vorlage für syndizierten Inhalt'
+  },
+  '72 description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'RSS Feed Title Suffix' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'assetName' => {
+    'lastUpdated' => '1179838422',
+    'message' => 'Syndizierte Inhalte'
+  },
+  'cache timeout' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'cache timeout help' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'cacheTimeout' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'channel.description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'channel.link' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'channel.title' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'displayMode' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'displayModeLabel' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'displayModeLabel description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'displayModeSubtext' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'get syndicated content' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'grouped' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'hasTerms' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'hasTermsLabel' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'hasTermsLabel description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'interleaved' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'item_loop' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'link' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'maxHeadlines' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'new_rss_site' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'process macros in rss url' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'process macros in rss url description' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'processMacrosInRssUrl' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'rss.url' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'rss.url.0.9' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'rss.url.0.91' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'rss.url.1.0' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'rss.url.2.0' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'rssTabName' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'rssUrl' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'site_link' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'site_title' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'syndicated content asset template variables title' => {
+    'lastUpdated' => '1170406566',
+    'message' => 'Syndizierte Inhalte Asset Template Variablen'
+  },
+  'templateId' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  },
+  'title' => {
+    'lastUpdated' => undef,
+    'message' => ''
+  }
+}
+;
 
 1;
