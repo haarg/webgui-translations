46c9fd83e4e9b87a942902066cef71d099655a5e
Translation Server <translation@plainblack.com>
2009-08-28 23:54:54 +0000

    Update from translation server

--- German/German/Asset_TimeTracking.pm
+++ German/German/Asset_TimeTracking.pm
@@ -2,368 +2,377 @@ package WebGUI::i18n::German::Asset_TimeTracking;
 use utf8;
 our $I18N = {
   'Add' => {
-    'lastUpdated' => 1212756820,
+    'lastUpdated' => '1251501670',
     'message' => 'Neu'
   },
   'Add Row' => {
-    'lastUpdated' => 1212756829,
+    'lastUpdated' => '1212756829',
     'message' => 'Neue Zeile'
   },
   'Are you sure you want to delete this activity' => {
-    'lastUpdated' => 1212756861,
+    'lastUpdated' => '1212756861',
     'message' => 'Sind Sie sicher, dass Sie diese Aufgabe löschen wollen?'
   },
   'Choose One' => {
-    'lastUpdated' => 1212756880,
+    'lastUpdated' => '1212756880',
     'message' => 'Wählen Sie'
   },
   'Choose yes to pull projects and task information from the various project management assets on your site' => {
-    'lastUpdated' => 1225635353,
+    'lastUpdated' => '1225635353',
     'message' => 'Wählen Sie \'ja\', um die Projekt- und Aufgabeninformationen aus den verschiedenen Projektmanagement-Best&auml;nden Ihrer Site zu holen (pull)'
   },
   'New Task Name' => {
-    'lastUpdated' => 1212756899,
+    'lastUpdated' => '1212756899',
     'message' => 'Neuer Aufgabenname'
   },
   'No  name was entered for new task' => {
-    'lastUpdated' => 1225635270,
+    'lastUpdated' => '1225635270',
     'message' => 'Sie haben keine Bezeichnung für die neue Aufgabe eingegeben.'
   },
   'Project Management Integration' => {
-    'lastUpdated' => 1226015330,
+    'lastUpdated' => '1226015330',
     'message' => 'Projektmanagementintegration'
   },
   'Report Complete' => {
-    'lastUpdated' => 1212756922,
+    'lastUpdated' => '1212756922',
     'message' => 'Bericht fertig'
   },
   'Save' => {
-    'lastUpdated' => 1157856592,
+    'lastUpdated' => '1157856592',
     'message' => 'Speichern'
   },
   'Tasks' => {
-    'lastUpdated' => 1212756807,
+    'lastUpdated' => '1212756807',
     'message' => 'Aufgaben'
   },
   'The highlighted fields are required if you wish to submit this form.' => {
-    'lastUpdated' => 1225635389,
+    'lastUpdated' => '1225635389',
     'message' => 'Bitte füllen Sie die hervorgehobenen Felder aus, damit das Formular &uuml;bermittelt werden kann.'
   },
   'There must be at least one row.  Please add more rows if you wish to delete this one' => {
-    'lastUpdated' => 1225635432,
+    'lastUpdated' => '1225635432',
     'message' => 'Mindestens eine Zeile muss existieren. Wenn Sie die aktuelle Zeile löschen wollen, fügen Sie mindestens eine neue Zeile hinzu.'
   },
   'This project cannot be deleted as it is currently being used by existing time entry records and would corrupt this data.  The records must be deleted if you wish to remove this project' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251497932',
+    'message' => 'Dieses Projekt kann nicht gelöscht werden, weil es derzeit von einem Zeit-Eintrag genutzt wird und dessen Daten beschädigen würde.'
   },
   'You may not submit more hours than are available during any given week.' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251497968',
+    'message' => 'Sie können nicht mehr Stunden zuweisen als pro Woche zur Verfügung stehen.'
   },
   'add project label' => {
-    'lastUpdated' => 1230371487,
+    'lastUpdated' => '1230371487',
     'message' => 'Neues Projekt hinzufügen'
   },
   'assetName' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251498170',
+    'message' => 'Time Tracker (beta)'
   },
   'edit project id hoverhelp' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251498213',
+    'message' => 'Projektbezeichner'
   },
   'edit project id label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251498224',
+    'message' => 'Projekt-ID'
   },
   'edit project label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251498234',
+    'message' => 'Projekt bearbeiten'
   },
   'edit project name hoverhelp' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251498290',
+    'message' => 'Geben Sie den Namen für das Projekt ein, wie er im Auswahlmenü für diesen Time Tracker erscheinen soll.'
   },
   'edit project name label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251498300',
+    'message' => 'Projektname'
   },
   'edit project resource hoverhelp' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251498406',
+    'message' => 'Wählen Sie die Ressourcen aus, die diesem Projekt Zeit zubuchen können.'
   },
   'edit project resource label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251498416',
+    'message' => 'Projektressourcen'
   },
   'edit project screen label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251498424',
+    'message' => 'Projekt bearbeiten'
   },
   'edit project tasks hoverhelp' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251498475',
+    'message' => 'Geben Sie die Projektaufgaben zeilenweise ein, die für dieses Projekt die Zeit verfolgfen sollen.'
   },
   'edit project tasks label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251498485',
+    'message' => 'Projektaufgaben'
   },
   'entry.hours' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251498597',
+    'message' => 'Ähnlich zu form.hours, enthält aber immer die Anzahl Stunden, die der Nutzer als Tagesarbeitszeit für diese Aufgabe eingegeben hat. Wurden keine Stunden eingegeben ist die Anzahl 0.'
   },
   'extras' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251498643',
+    'message' => 'URL zum TimeTacking-Bereich im extras-Verzeichnis wobject/TimeTracking.'
   },
   'form.comments' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251499104',
+    'message' => 'Wenn der Bericht als fertig markiert wurde, enthält diese Variable Bemerkungen der Nutzer für die Arbeit eines Tages. Ist die Aufgabe noch nicht beendet, enthält diese Variable den Formular-Code für ein Textfeld, in der der Nutzer seine Bemerkungen eingeben kann.'
   },
   'form.date' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251499135',
+    'message' => 'Wenn der Bericht als fertig markiert wurde, enthält diese Variable das Projekt, für das der Status eingegeben wurde. Ist die Aufgabe noch nicht beendet, enthält diese Variable den Formular-Code für eine Auswahlbox, mit der der Nutzer den Wochentag auswählen kann, für den er den Status eingeben möchte.'
   },
   'form.footer' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251499184',
+    'message' => 'HTML und JavaScript für das Formularende'
   },
   'form.header' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251499205',
+    'message' => 'HTML und JavaScript für den Formularanfang'
   },
   'form.hours' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251499405',
+    'message' => 'Wenn der Bericht als fertig markiert wurde, enthält diese Variable die Tagesstundenzahl, die für die Aufgabe gebraucht wurden. Ist die Aufgabe noch nicht beendet, enthält diese Variable den Formular-Code für ein Feld in das der Nutzer die geleisteten Stunden als Fließkommazahl eingeben kann.'
   },
   'form.isComplete' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251499460',
+    'message' => 'Ein Auswahlfeld, in dem der Nutzer markieren kann, dass der Bericht fertig ist.'
   },
   'form.project' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251503188',
+    'message' => 'Wenn der Bericht als abgeschlossen markiert wurde, enthält diese Variable den Projektnamen. Wenn die Aufgabe noch nicht abgeschlossen ist, enthält diese Variable Formularcode für eine Auswahlbox, mit der der Nutzer den Projektnamen für die Eingabe der Statusinformationen auswählen kann.'
   },
   'form.task' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251503295',
+    'message' => 'Wenn der Bericht als abgeschlossen markiert wurde, enthält diese Variable den Namen der Aufgabe im Projekt. Wenn die Aufgabe noch nicht abgeschlossen ist, enthält diese Variable Formularcode für eine Auswahlbox, mit der der Nutzer einen Aufgabennamen des Projekts auswählen kann.'
   },
   'form.taskEntryId' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251503008',
+    'message' => 'Benötigter Formularcode, der die taskEntryId enthält'
   },
   'form.timetracker' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251502970',
+    'message' => 'Ausgabe des timeRowTemplate für dieses Asset'
   },
   'groupToManage' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251502940',
+    'message' => 'Die ID der Gruppe, die alle Stundenzettel und abgeschlossene Zeitberichte sehen kann.'
   },
   'groupToManage hoverhelp' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251502833',
+    'message' => 'Die Gruppe, die alle Stundenzettel sehen kann und abgeschlossene Zeitberichte aktualisieren darf.'
   },
   'groupToManage label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501164',
+    'message' => 'Gruppe, die Zeit verwalten darf'
   },
   'js.alert.removeRow.error' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251502764',
+    'message' => 'Internationalisierte Fehlermeldung, falls die letzte Zeile gelöscht werden soll'
   },
   'js.alert.validate.hours.error' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251502704',
+    'message' => 'Internationalisierte Fehlermeldung, wenn mehr Stunden zugewiesen wurden als pro Woche verfügbar'
   },
   'js.alert.validate.incomplete.error' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251502712',
+    'message' => 'Internationalisierte Fehlermeldung, wenn nicht alle Pflichtfelder ausgefüllt sind'
+  },
+  'last week' => {
+    'lastUpdated' => '1251502582',
+    'message' => 'Vorwoche'
   },
   'manage project available task label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501245',
+    'message' => 'Verfügbare Aufgaben'
   },
   'manage project name label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501253',
+    'message' => 'Projektname'
   },
   'manage project resource label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501265',
+    'message' => 'Verfügbare Ressourcen'
   },
   'manage projects screen label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501274',
+    'message' => 'Projekte verwalten'
   },
   'managerViewTemplate hoverhelp' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251503351',
+    'message' => 'Template für die Verwaltungsansicht der TimeTracker-Anwendung'
   },
   'managerViewTemplate label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501320',
+    'message' => 'Standardtemplate TimeTracker Manageransicht'
   },
   'managerViewTemplateId' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251503378',
+    'message' => 'Template für die Verwaltungsansicht'
+  },
+  'next week' => {
+    'lastUpdated' => '1251502571',
+    'message' => 'Folgewoche'
   },
   'no project message' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251502561',
+    'message' => 'Kein Projekt erstellt. Klicken Sie <a href="%s">hier</a> für eine neues Projekt'
   },
   'pmIntegration' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251502398',
+    'message' => 'Eine Bedingung, die anzeigt, ob dieses TimeTracking Asses für eine Einbindung in den Projektmanager konfiguriert wurde.'
   },
   'project manage label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501331',
+    'message' => 'Projekte verwalten'
   },
   'project.manage.label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501358',
+    'message' => 'Einen Bezeichner für project.manage.url'
   },
   'project.manage.url' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251502492',
+    'message' => 'URL für die Projektverwaltung  in diesem Asset. Wenn der Benutzer kein Gruppenmitglied von groupToManage ist, ist diese Variable leer.'
   },
   'project.task.array' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251502325',
+    'message' => 'JavaScript, das Daten über das Projekt und die Aufgaben enthält'
   },
   'report.isComplete' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251502266',
+    'message' => 'Boolscher Wert, der wahr ist, wenn das Formular komplett ist.'
   },
   'report.lastWeek.url' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251502207',
+    'message' => 'URL zu Informationen für die Vorwoche'
   },
   'report.nextWeek.url' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251502227',
+    'message' => 'URL zu Informationen für die Folgewoche'
   },
   'row.id' => {
-    'lastUpdated' => 1212756988,
+    'lastUpdated' => '1212756988',
     'message' => 'Ein eindeutiger Bezeichner für diese Zeile'
   },
   'time report comments label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501366',
+    'message' => 'Bemerkungen'
   },
   'time report date label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501371',
+    'message' => 'Datum'
   },
   'time report header' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251502131',
+    'message' => 'Zeiteintrag für die Woche endend auf %s'
   },
   'time report hours label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501385',
+    'message' => 'Stunden'
   },
   'time report project label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501393',
+    'message' => 'Projekt'
   },
   'time report task label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501399',
+    'message' => 'Aufgabe'
   },
   'time row template title' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251502100',
+    'message' => 'Timetracking Zeitzeilen-Template'
   },
   'time tracking asset template variables title' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251502070',
+    'message' => 'Asset-Templatevariablen des TimeTrackingsystems'
   },
   'time.add.row.label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251499757',
+    'message' => 'Der internationalisierte Bezeichner für "Add Row"'
   },
   'time.entry.loop' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251503682',
+    'message' => 'Eine Schleife, die Informationen und Formularelemente für jede Aufgabe enthält, an der in dieser Woche gearbeitet wurde. Wenn eine Aufgabe als abgeschlossen markiert wurde, enthält die Zeile read-only-Informationen für diese Aufgabe. Andernfalls wird ein Formularfeld zur Eingabe der Statusinformationen generiert. Die Stautsinformationen enthalten das Projekt, an dem gearbeitet wurde, an welchen Tagen Arbeiten ausgeführt wurden und die Anzahl der verwandten Arbeitsstunden.'
   },
   'time.report.comments.label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501424',
+    'message' => 'Der internationalisierte Begriff "Comments"'
   },
   'time.report.complete.label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501454',
+    'message' => 'Der internationalisierte Begriff "Report Complete"'
   },
   'time.report.date.label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501479',
+    'message' => 'Der internationalisierte Begriff "Datum"'
   },
   'time.report.header' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251502027',
+    'message' => 'Kopf des Zeitberichts für die Anzeige des Enddatums der Woche'
   },
   'time.report.hours.label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501507',
+    'message' => 'Der internationalisierte Begriff "Hours"
+ '
   },
   'time.report.project.label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501539',
+    'message' => 'Der internationalisierte Begriff "Project"'
   },
   'time.report.rows.total' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501979',
+    'message' => 'Zeilenanzahl plus 1 in diesem Bericht'
   },
   'time.report.task.label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501561',
+    'message' => 'Der internationalisierte Begriff "Task"'
   },
   'time.report.totalHours.label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501593',
+    'message' => 'Der internationalisierte Begriff "Total Hours"'
   },
   'time.save.label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251499727',
+    'message' => 'Der internationalisierte Bezeichner für "Add rows"'
   },
   'time.totalHours' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501937',
+    'message' => 'Die Stundensumme aller Aufgaben des aktuellen Nutzers in dieser Woche'
   },
   'timeRowTemplateId' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501886',
+    'message' => 'Die ID des Templates, das eine Zeile im Stundenzettel erzeugt'
   },
   'timeRowTemplateId hoverhelp' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501821',
+    'message' => 'Template für die Zeilen im TimeTracker'
   },
   'timeRowTemplateId label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501619',
+    'message' => 'Standardzeile Time Tracker'
   },
   'total hours label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251499684',
+    'message' => 'Gesamtstunden'
   },
   'user view template title' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501796',
+    'message' => 'Template für die TimeTracker Nutzeransicht'
   },
   'userViewTemplate hoverhelp' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501763',
+    'message' => 'Template für die Hauptansicht der TimeTracker-Anwendung'
   },
   'userViewTemplate label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501650',
+    'message' => 'Standardtemplate für TimeTracker-Benutzeransicht'
   },
   'userViewTemplateId' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251501720',
+    'message' => 'Das Template für die Anzeige für einen normalen Nutzer '
   }
 }
 ;
