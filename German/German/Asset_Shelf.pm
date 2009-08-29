02a6240a45d8fad35762b11ab85ef0c5e1609530
Translation Server <translation@plainblack.com>
2009-08-29 00:02:10 +0000

    Update from translation server

--- German/German/Asset_Shelf.pm
+++ German/German/Asset_Shelf.pm
@@ -1,6 +1,10 @@
 package WebGUI::i18n::German::Asset_Shelf;
 use utf8;
 our $I18N = {
+  'You do not have permission to view the products on this shelf' => {
+    'lastUpdated' => '1251504120',
+    'message' => 'Sie haben keine Berechtigung, um die Produkte in diesem Regal zu sehen. '
+  },
   'addToCartForm' => {
     'lastUpdated' => '1238881121',
     'message' => 'Wenn dieses Produkt es unterstützt, das Formular zum Aufnehmen in den Warenkorb.
@@ -9,6 +13,10 @@ Es enthält einen Knopf und alle weiter notwendigen Elemente dazu.'
   'assetName' => {
     'lastUpdated' => '1228688960',
     'message' => 'Regal'
+  },
+  'emptyShelf' => {
+    'lastUpdated' => '1251504037',
+    'message' => 'Ein Boolscher Wert der wahr ist, falls dieses Regal gar keine Produkte enthält '
   },
   'export' => {
     'lastUpdated' => '1226014779',
@@ -22,6 +30,10 @@ Es enthält einen Knopf und alle weiter notwendigen Elemente dazu.'
     'lastUpdated' => '1228688927',
     'message' => 'Ihre Produkte wurden importiert'
   },
+  'noViewableSkus' => {
+    'lastUpdated' => '1251503978',
+    'message' => 'Ein Boolscher Wert der wahr ist, falls das Regal kein Produkt enthält, das der Nutzer sehen darf.'
+  },
   'price' => {
     'lastUpdated' => '1226014842',
     'message' => 'Der Preis für diesen Artikel'
@@ -31,8 +43,8 @@ Es enthält einen Knopf und alle weiter notwendigen Elemente dazu.'
     'message' => 'Die Url zu dieser Artikelnummer'
   },
   'products' => {
-    'lastUpdated' => '1228689124',
-    'message' => '<p>Eine Schleife, die eine Liste der Produkte enthält, die den Schlüsselworten entsprechen, die f&uuml;r dieses Regal festgelegt wurden. Jeder Eintrag in der Schleife enth&auml;lt alle Eigenschaften der passenden Artikelnummer zuz&uuml;glich der folgenden Variablen.</p>'
+    'lastUpdated' => '1251503895',
+    'message' => 'Eine Schleife, die eine Liste der Produkte enthält, die den Schlüsselworten entsprechen, die f&uuml;r dieses Regal festgelegt wurden oder die Kinder dieses Regals sind. Die Schleife enthält nur Produkte, die der Nutzer sehen darf. Jeder Eintrag in der Schleife enth&auml;lt alle Eigenschaften der passenden Artikelnummer zuz&uuml;glich der folgenden Variablen.'
   },
   'shelf template' => {
     'lastUpdated' => '1228689139',
@@ -42,6 +54,14 @@ Es enthält einen Knopf und alle weiter notwendigen Elemente dazu.'
     'lastUpdated' => '1228689182',
     'message' => 'Wählen Sie die Vorlage, die die Liste der Produkte anzeigt, die diesem Regal zugeordnet sind.'
   },
+  'shelf_title' => {
+    'lastUpdated' => '1251503762',
+    'message' => 'Name des Regals'
+  },
+  'shelf_url' => {
+    'lastUpdated' => '1251503748',
+    'message' => 'URL des Regals'
+  },
   'shelves' => {
     'lastUpdated' => '1228689245',
     'message' => 'Eine Schleife, die eine Liste der Regale enthält, die Kinder dieser im Asset-Baum sind. Jeder Eintrag der Schleife einh&auml;lt alle Eigenschaften des Regals.'
@@ -49,6 +69,10 @@ Es enthält einen Knopf und alle weiter notwendigen Elemente dazu.'
   'subcategories' => {
     'lastUpdated' => '1228689254',
     'message' => 'Unterkategorien'
+  },
+  'this shelf is empty' => {
+    'lastUpdated' => '1251503738',
+    'message' => 'Dieses Regal ist leer'
   },
   'thumbnailUrl' => {
     'lastUpdated' => '1228689305',
