ccc9689f97e550d7a205e12c919cdb3ecf0d4528
Translation Server <translation@plainblack.com>
2009-08-30 20:35:44 +0000

    Update from translation server

--- German/German/Asset_Shortcut.pm
+++ German/German/Asset_Shortcut.pm
@@ -58,8 +58,8 @@ our $I18N = {
     'message' => 'Merkmale'
   },
   'Criteria description' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251662268',
+    'message' => 'Eine Anweisung, was gespiegelt werden soll. Form: "color = blue and weight != heavy". Mehrere Ausdrücke können durch "and" und "or" verbunden werden. Eine Eigenschaft muss in Anführungszeichen stehen, wenn sie Leerzeichen enthält. Sie können den Kriterienbilder nutzen, um die Anweisung zu erstellen. '
   },
   'Default Value for this field' => {
     'lastUpdated' => '1178710540',
@@ -130,8 +130,10 @@ our $I18N = {
     'message' => 'Überschreibung'
   },
   'Place something in this box if you dont want to use the automatically generated field' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251662502',
+    'message' => 'Geben Sie etwas in dieses Feld ein, wenn Sie nicht das automatisch generierte Feld nutzen wollen. Sie können auch Benutzerpräferenzen in dieses Feld mit folgender Syntax eingeben:
+Um  auszugeben "Meine Lieblingsfarbe ist blau" und es in den Benutzerpräferenezen ein Feld 
+\'myFavColor\' gibt, tragen Sie hier ein: "Meine Lieblingsfarbe ist ##userPref:myFavColor##".'
   },
   'Possible Values' => {
     'lastUpdated' => '1178710733',
@@ -162,32 +164,32 @@ our $I18N = {
     'message' => 'Mehrere auflösen?'
   },
   'Resolve Multiples description' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251662789',
+    'message' => 'Legt die Reihenfolge fest, wenn mehrere Assets ausgewählt wurden. "Zufall" bedeutet, dass wenn mehrere Assets das Shortcut-Kriterium erfüllen, das Shortcut ein zufälliges Asset auswählt. Durch "Letztes"  wird das letzte Asset gewählt, das die Auswahlkriterien erfüllt.'
   },
   'Shortcut by alternate criteria' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251662808',
+    'message' => 'Shortcut mit anderen Kriterien?'
   },
   'Shortcut by alternate criteria description' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251662945',
+    'message' => 'Wählen Sie "Ja", wenn Sie ein Asset auf Basis eigener Kriterien auswählen möchten. Metadten muss aktiviert sein, damit diese Option funktionieren kann.'
   },
   'The unique name of a user preference parameter you are inventing' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251663009',
+    'message' => 'Der eindeutige Name einer Benutzerpräferenz, die Sie nutzen möchten.'
   },
   'This is the example output of the field when parsed for user preference macros' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251663055',
+    'message' => 'Das ist die Beispielausgabe des Feldes, wenn die Benutzerpräferenz-Makros abgearbeitet wurden.'
   },
   'Type of Field' => {
     'lastUpdated' => '1251590891',
     'message' => 'Feldtyp'
   },
   'Use this field to edit the override using the native form handler for this field type' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251663156',
+    'message' => 'Benutzen Sie dieses Feld um das Überschreiben zu bearbeiten unter Verwendung des nativen Formular-Handlers für diesen Feldtyp'
   },
   'assetName' => {
     'lastUpdated' => '1178710891',
@@ -198,16 +200,16 @@ our $I18N = {
     'message' => 'Sie haben keine Berechtigung, das Dashboard zu personalisieren'
   },
   'disable content lock' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251663177',
+    'message' => 'Inhaltssperre ausschalten'
   },
   'disable content lock description' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251664534',
+    'message' => 'Wenn Sie mit alternativen Kriterien spiegeln, wird das Shortcut standardmäßig gesperrt auf ein bestimmten, einzelnen Inhalt und zeigt nur diesen einzelnen Inhalt bis zum Sessionende an. Wenn Sie aus bestimmten Gründen den Inhalt rotieren möchten, können Sie die Inhaltssperre aufheben.'
   },
   'edit delete fieldname' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251663575',
+    'message' => 'Feldname bearbeiten/löschen'
   },
   'equal to' => {
     'lastUpdated' => '1251590992',
@@ -230,8 +232,8 @@ our $I18N = {
     'message' => 'ist'
   },
   'isShortcut' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251663722',
+    'message' => 'Ein boolscher Wert, der angibt, dass dieses Asset ein Shortcut ist. Kann in Zusammenhang mit einem anderen boolschen Wert für Adminmodus, um Redakteuren einfach aufzuzeigen, dass dies ein Shortcut-Asset ist. '
   },
   'isnt' => {
     'lastUpdated' => '1251590769',
@@ -258,44 +260,44 @@ our $I18N = {
     'message' => 'Sie müssen angemeldet sein, um das Dashboard zu personalisieren'
   },
   'originalURL' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251663595',
+    'message' => 'URL zum Asset, das durch diesen  Shortcut gespiegelt wird'
   },
   'override asset template' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251663968',
+    'message' => 'Template des Überschreibungsasset.'
   },
   'override asset template description' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251663942',
+    'message' => 'Wählen Sie ein Template, das das Originalasset optional überschreiben kann.'
   },
   'pref fields to import' => {
     'lastUpdated' => '1251591326',
     'message' => 'Zu importierende Präferenzfelder'
   },
   'pref fields to import description' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251663888',
+    'message' => 'Dies sind die Benutzerprofilfelder die Sie zum Überschreiben freigeben wollen (Form ##userPref:nameOfProfileField##). Wählen Sie diese aus, wenn Sie Profilfelder aus anderen Bereichen der Site oder aus den allgemeinen Benutzerprofilfeldern freigeben möchten.'
   },
   'pref fields to show' => {
     'lastUpdated' => '1251591357',
     'message' => 'Anzuzeigende Präferenzfelder'
   },
   'pref fields to show description' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251663539',
+    'message' => 'Dies sind die Benutzerprofil-Felder, die sie von den Nutzern verwendbar sein sollen, um das Dashboard zu personalisieren.'
   },
   'shortcut template title' => {
     'lastUpdated' => '1251591376',
     'message' => 'Shortcut Template '
   },
   'shortcut template title description' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251663416',
+    'message' => 'Wählen Sie ein Template aus der List, um den Shortcut darzustellen.'
   },
   'shortcut.content' => {
-    'lastUpdated' => '1251591382',
-    'message' => ''
+    'lastUpdated' => '1251663384',
+    'message' => 'Der Inhalt eines gespiegelten Assets. Sobald eine Überschreibung eingerichtet wurde, wird der Überscheibungs-Inhalt statt der Inhalt des gespiegelten Assets verwandt.'
   },
   'shortcut.label' => {
     'lastUpdated' => '1251590839',
@@ -310,8 +312,8 @@ our $I18N = {
     'message' => 'Zeige Reload Icon?'
   },
   'show reload icon description' => {
-    'lastUpdated' => '1251591128',
-    'message' => ''
+    'lastUpdated' => '1251663274',
+    'message' => 'Soll das gelbe Dashlet-neu-laden-Icon in diesem Shortcut/Dashlet.'
   }
 }
 ;
