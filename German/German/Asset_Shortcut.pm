d2bbe1156a8ea3fa7397b05f863a45b6a1d716c9
Translation Server <translation@plainblack.com>
2009-08-30 00:17:19 +0000

    Update from translation server

--- German/German/Asset_Shortcut.pm
+++ German/German/Asset_Shortcut.pm
@@ -1,60 +1,60 @@
 package WebGUI::i18n::German::Asset_Shortcut;
 use utf8;
 our $I18N = {
-  '1' => {
-    'lastUpdated' => 1178710118,
+  1 => {
+    'lastUpdated' => '1178710118',
     'message' => 'Zu spiegelndes Asset'
   },
   '1 description' => {
-    'lastUpdated' => 1178710140,
+    'lastUpdated' => '1178710140',
     'message' => 'Geben sie den Link des zu spiegelnden Assets an.'
   },
   '10 description' => {
-    'lastUpdated' => 1178710195,
+    'lastUpdated' => '1178710195',
     'message' => 'Setzen Sie hier "Ja", wenn das Template des Shortcuts an statt des originalen Templates benutzt werden soll.'
   },
-  '2' => {
-    'lastUpdated' => 1178710206,
+  2 => {
+    'lastUpdated' => '1178710206',
     'message' => 'Shortcut bearbeiten'
   },
-  '4' => {
-    'lastUpdated' => 1178710240,
+  4 => {
+    'lastUpdated' => '1178710240',
     'message' => 'Spiegeln des Assets fehlgeschlagen. Vielleicht wurde das originale Asset gelöscht.'
   },
-  '7' => {
-    'lastUpdated' => 1178710279,
+  7 => {
+    'lastUpdated' => '1178710279',
     'message' => 'Titel überschreiben?'
   },
   '7 description' => {
-    'lastUpdated' => 1178710432,
+    'lastUpdated' => '1178710432',
     'message' => 'Setzen Sie hier &quot;Ja&quot;, falls der Titel des Shortcuts anstatt des Titels des originalen Assets benutzt werden soll.'
   },
-  '8' => {
-    'lastUpdated' => 1178710449,
+  8 => {
+    'lastUpdated' => '1178710449',
     'message' => 'Menütitel &uuml;berschreiben?'
   },
   '8 description' => {
-    'lastUpdated' => 1178710494,
+    'lastUpdated' => '1178710494',
     'message' => 'Setzen Sie hier "Ja", falls der Menütitel des Shortcuts anstatt des Men&uuml;titels des originale Assets benutzt werden soll.'
   },
-  '9' => {
-    'lastUpdated' => 1178710373,
+  9 => {
+    'lastUpdated' => '1178710373',
     'message' => 'Beschreibung überschreiben?'
   },
   '9 description' => {
-    'lastUpdated' => 1178710420,
+    'lastUpdated' => '1178710420',
     'message' => 'Setzen Sie hier "Ja", falls die Beschreibung des Shortcuts anstatt der Beschreibung des originalen Assets benutzt werden soll.'
   },
   'AND' => {
-    'lastUpdated' => 1157856426,
+    'lastUpdated' => '1157856426',
     'message' => 'UND'
   },
   'Back to Edit Shortcut' => {
-    'lastUpdated' => 1178710511,
+    'lastUpdated' => '1178710511',
     'message' => 'Zurück zum Bearbeiten des Shortcuts'
   },
   'Criteria' => {
-    'lastUpdated' => 1179838332,
+    'lastUpdated' => '1179838332',
     'message' => 'Merkmale'
   },
   'Criteria description' => {
@@ -62,104 +62,104 @@ our $I18N = {
     'message' => ''
   },
   'Default Value for this field' => {
-    'lastUpdated' => 1178710540,
+    'lastUpdated' => '1178710540',
     'message' => 'Standard Wert dieses Feldes.'
   },
   'Displaying this shortcut would cause a feedback loop' => {
-    'lastUpdated' => 1228691735,
+    'lastUpdated' => '1228691735',
     'message' => 'Das Anzeigen dieser Verknüpfung w&uuml;rde eine Endlosschleife verursachen.'
   },
   'Edit Field Directly' => {
-    'lastUpdated' => 1178710560,
+    'lastUpdated' => '1178710560',
     'message' => 'Bearbeite Feld direkt'
   },
   'Edit Override' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251590601',
+    'message' => 'Überschreibungen bearbeiten'
   },
   'Edit User Preference Field' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251590688',
+    'message' => 'Feld Benutzerpräferenzen bearbeiten'
   },
   'Field' => {
-    'lastUpdated' => 1178710585,
+    'lastUpdated' => '1178710585',
     'message' => 'Feld'
   },
   'Hover Help Description for this Field' => {
-    'lastUpdated' => 1178710617,
+    'lastUpdated' => '1178710617',
     'message' => '"Hover" Hilfe (Beschreibung) für dieses Feld.'
   },
   'Label for This Field.' => {
-    'lastUpdated' => 1178710631,
+    'lastUpdated' => '1178710631',
     'message' => 'Label für dieses Feld.'
   },
   'Manage Profile Fields' => {
-    'lastUpdated' => 1178710642,
+    'lastUpdated' => '1178710642',
     'message' => 'Verwalte Profil Felder'
   },
   'Manage Shortcut Overrides' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251590720',
+    'message' => 'Shortcut-Überschreibungen bearbeiten'
   },
   'Manage User Preferences' => {
-    'lastUpdated' => 1228691783,
+    'lastUpdated' => '1228691783',
     'message' => 'Benutzereinstellungen bearbeiten'
   },
   'Most Recent' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251590731',
+    'message' => 'Letzte'
   },
   'New Override Value' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251590881',
+    'message' => 'Neuer überschriebener Wert.'
   },
   'New value' => {
-    'lastUpdated' => 1178710685,
+    'lastUpdated' => '1178710685',
     'message' => 'Neuer Wert'
   },
   'OR' => {
-    'lastUpdated' => 1178710693,
+    'lastUpdated' => '1178710693',
     'message' => 'ODER'
   },
   'Original Value' => {
-    'lastUpdated' => 1178710703,
+    'lastUpdated' => '1178710703',
     'message' => 'Originalwert'
   },
   'Overrides' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251591213',
+    'message' => 'Überschreibung'
   },
   'Place something in this box if you dont want to use the automatically generated field' => {
     'lastUpdated' => undef,
     'message' => ''
   },
   'Possible Values' => {
-    'lastUpdated' => 1178710733,
+    'lastUpdated' => '1178710733',
     'message' => 'Mögliche Werte'
   },
   'Possible values for this Field Only applies to selectList and checkList' => {
-    'lastUpdated' => 1178710858,
+    'lastUpdated' => '1178710858',
     'message' => 'Mögliche Werte für dieses Feld. Nur wirksam in selectList und checkList.'
   },
   'Preferences' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251590743',
+    'message' => 'Einstellungen'
   },
   'Random' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251590749',
+    'message' => 'Zufall'
   },
   'Replacement Value' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251590942',
+    'message' => 'Ersetzungswert'
   },
   'Replacement value' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251590951',
+    'message' => 'Ersetzungswert'
   },
   'Resolve Multiples' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251590966',
+    'message' => 'Mehrere auflösen?'
   },
   'Resolve Multiples description' => {
     'lastUpdated' => undef,
@@ -182,16 +182,20 @@ our $I18N = {
     'message' => ''
   },
   'Type of Field' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251590891',
+    'message' => 'Feldtyp'
   },
   'Use this field to edit the override using the native form handler for this field type' => {
     'lastUpdated' => undef,
     'message' => ''
   },
   'assetName' => {
-    'lastUpdated' => 1178710891,
+    'lastUpdated' => '1178710891',
     'message' => 'Shortcut'
+  },
+  'cannot personalize' => {
+    'lastUpdated' => '1251591419',
+    'message' => 'Sie haben keine Berechtigung, das Dashboard zu personalisieren'
   },
   'disable content lock' => {
     'lastUpdated' => undef,
@@ -206,44 +210,52 @@ our $I18N = {
     'message' => ''
   },
   'equal to' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251590992',
+    'message' => 'gleich'
   },
   'field add/edit title' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251591177',
+    'message' => 'Benutzerpräferenzen hinzufügen/bearbeiten'
   },
   'fieldName' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251591148',
+    'message' => 'Feldname'
   },
   'greater than' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251590986',
+    'message' => 'größer als'
   },
   'is' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251590757',
+    'message' => 'ist'
   },
   'isShortcut' => {
     'lastUpdated' => undef,
     'message' => ''
   },
   'isnt' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251590769',
+    'message' => 'ist nicht'
   },
   'less than' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251590777',
+    'message' => 'kleiner als'
+  },
+  'no longer exists' => {
+    'lastUpdated' => '1251590821',
+    'message' => 'Das Asset, auf das dieser Shortcut verweist, existiert nicht mehr. Löschen Sie diesen Shortcut.'
   },
   'no metadata yet' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251591280',
+    'message' => 'Noch keine Metadaten definiert. Klicken Sie <a href="%s">hier</a>, um Metadaten-Attribute zu definieren.'
   },
   'not equal to' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251590905',
+    'message' => 'ungleich'
+  },
+  'not logged in' => {
+    'lastUpdated' => '1251591017',
+    'message' => 'Sie müssen angemeldet sein, um das Dashboard zu personalisieren'
   },
   'originalURL' => {
     'lastUpdated' => undef,
@@ -258,47 +270,47 @@ our $I18N = {
     'message' => ''
   },
   'pref fields to import' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251591326',
+    'message' => 'Zu importierende Präferenzfelder'
   },
   'pref fields to import description' => {
     'lastUpdated' => undef,
     'message' => ''
   },
   'pref fields to show' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251591357',
+    'message' => 'Anzuzeigende Präferenzfelder'
   },
   'pref fields to show description' => {
     'lastUpdated' => undef,
     'message' => ''
   },
   'shortcut template title' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251591376',
+    'message' => 'Shortcut Template '
   },
   'shortcut template title description' => {
     'lastUpdated' => undef,
     'message' => ''
   },
   'shortcut.content' => {
-    'lastUpdated' => undef,
+    'lastUpdated' => '1251591382',
     'message' => ''
   },
   'shortcut.label' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251590839',
+    'message' => 'Das Wort "Shortcut"'
   },
   'shortcut.properties' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1251591103',
+    'message' => 'Alle dem Shortcut zugewiesenen Eigenschaften sind im Template mit ihrem Namen nutzbar.'
   },
   'show reload icon' => {
-    'lastUpdated' => 1178710810,
+    'lastUpdated' => '1178710810',
     'message' => 'Zeige Reload Icon?'
   },
   'show reload icon description' => {
-    'lastUpdated' => undef,
+    'lastUpdated' => '1251591128',
     'message' => ''
   }
 }
