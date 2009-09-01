905d337164ed5346746beba19d1c4c861603a48e
Translation Server <translation@plainblack.com>
2009-09-01 01:28:17 +0000

    Update from translation server

--- German/German/Asset_Story.pm
+++ German/German/Asset_Story.pm
@@ -1,6 +1,10 @@
 package WebGUI::i18n::German::Asset_Story;
 use utf8;
 our $I18N = {
+  'Replace image with new image' => {
+    'lastUpdated' => '1251768110',
+    'message' => 'Grafik durch neue ersetzen'
+  },
   'Source' => {
     'lastUpdated' => '1250933784',
     'message' => 'Quelle'
@@ -28,6 +32,38 @@ our $I18N = {
   'bylineForm' => {
     'lastUpdated' => '1250974396',
     'message' => 'Benutzerformular zur Eingabe einer Autorenzeile für diesen Bericht.'
+  },
+  'canEdit' => {
+    'lastUpdated' => '1251768157',
+    'message' => 'Ein boolscher Wert, der wahr ist, wenn der aktuelle Benutzer diesen Bericht bearbeiten darf'
+  },
+  'cancelButton' => {
+    'lastUpdated' => '1251768195',
+    'message' => 'Button für den Benutzer, um dieses Formular ohne Speichern abzubrechen'
+  },
+  'headline' => {
+    'lastUpdated' => '1251768363',
+    'message' => 'Überschrift'
+  },
+  'keyword' => {
+    'lastUpdated' => '1251768340',
+    'message' => 'Ein Schlüsselwort, ohne Formataierung'
+  },
+  'story' => {
+    'lastUpdated' => '1251768283',
+    'message' => 'Bericht'
+  },
+  'subtitle' => {
+    'lastUpdated' => '1251768302',
+    'message' => 'Untertitel'
+  },
+  'titleForm' => {
+    'lastUpdated' => '1251768265',
+    'message' => 'Formular zur Eingabe des Berichttitels'
+  },
+  'view template' => {
+    'lastUpdated' => '1251768236',
+    'message' => 'Template Bericht anzeigen'
   }
 }
 ;
