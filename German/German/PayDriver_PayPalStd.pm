5a8334ff0e88d42a529ac05ba806666d82c4e173
Translation Server <translation@plainblack.com>
2009-09-02 14:09:14 +0000

    Update from translation server

--- German/German/PayDriver_PayPalStd.pm
+++ German/German/PayDriver_PayPalStd.pm
@@ -1,5 +1,140 @@
 package WebGUI::i18n::German::PayDriver_PayPalStd;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'PayPal' => {
+    'lastUpdated' => '1251899421',
+    'message' => 'PayPal'
+  },
+  'button image' => {
+    'lastUpdated' => '1251899437',
+    'message' => 'PayPal Button Bild-URL'
+  },
+  'button image help' => {
+    'lastUpdated' => '1251899473',
+    'message' => 'Setzen Sie diese Option, um Paypal-Grafiken aus Bezahlbuttons zu verwenden.'
+  },
+  'cart transaction mismatch' => {
+    'lastUpdated' => '1251899520',
+    'message' => 'Ein Warenkorbfehler ist aufgetreten. Dieser Vorfall wird geloggt.'
+  },
+  'currency' => {
+    'lastUpdated' => '1251899480',
+    'message' => 'Währung'
+  },
+  'currency help' => {
+    'lastUpdated' => '1251899576',
+    'message' => 'Die Währung Ihrer Transaktion mit dem PayPal-Account'
+  },
+  'emailMessage' => {
+    'lastUpdated' => '1251899593',
+    'message' => 'E-Mail-Nachricht'
+  },
+  'emailMessage help' => {
+    'lastUpdated' => '1251899639',
+    'message' => 'Die Nachricht die an die E-Mail angehängt werden soll, die der Nutzer von PayPal erhält.'
+  },
+  'error occurred message' => {
+    'lastUpdated' => '1251899826',
+    'message' => 'Die folgenden Fehler sind aufgetreten:'
+  },
+  'extra info' => {
+    'lastUpdated' => '1251899968',
+    'message' => 'Vergessen Sie nicht "Payment Data Transfer" UND "Auto Return" auf der <a href="https://www.paypal.com/us/cgi-bin/webscr?cmd=_profile-website-payments">Website "Payments section"</a> Ihres PayPal-Profils einzuschalten.
+Zusätzlich die folgende "Return URL" setzen:'
+  },
+  'identity token' => {
+    'lastUpdated' => '1251899980',
+    'message' => 'PDT Identity Token'
+  },
+  'identity token help' => {
+    'lastUpdated' => '1251900546',
+    'message' => 'Das Identitätstoken, das unter dem Payment-Data-Transfer-Auswahlknopf auf Ihren Website Zahlungseinstellungen auf PayPal aufgelistet ist.'
+  },
+  'invalid address' => {
+    'lastUpdated' => '1251900003',
+    'message' => 'Sie müssen eine gültige Adresse eingeben.'
+  },
+  'invalid city' => {
+    'lastUpdated' => '1251900024',
+    'message' => 'Sie müssen eine gültige Stadt eingeben.'
+  },
+  'invalid email' => {
+    'lastUpdated' => '1251900041',
+    'message' => 'Sie müssen eine gültige E-Mail-Adresse eingeben.'
+  },
+  'invalid firstName' => {
+    'lastUpdated' => '1251900063',
+    'message' => 'Sie müssen einen gültigen Vornamen eingeben.'
+  },
+  'invalid lastName' => {
+    'lastUpdated' => '1251900081',
+    'message' => 'Sie müssen einen gültigen Familiennamen eingeben.'
+  },
+  'invalid zip' => {
+    'lastUpdated' => '1251900104',
+    'message' => 'Sie müssen eine gültige Postleitzahl eingeben.'
+  },
+  'label' => {
+    'lastUpdated' => '1251900117',
+    'message' => 'PayPal'
+  },
+  'live url' => {
+    'lastUpdated' => '1251900126',
+    'message' => 'Live URL'
+  },
+  'live url help' => {
+    'lastUpdated' => '1251900181',
+    'message' => 'URL an die gesendet werden soll, wenn live (nicht bei Verwendung der Sandbox)'
+  },
+  'module name' => {
+    'lastUpdated' => '1251900191',
+    'message' => 'PayPal'
+  },
+  'no description' => {
+    'lastUpdated' => '1251900199',
+    'message' => 'Keine Beschreibung'
+  },
+  'password' => {
+    'lastUpdated' => '1251900207',
+    'message' => 'Passwort'
+  },
+  'password help' => {
+    'lastUpdated' => '1251900226',
+    'message' => 'Passwort für Ihren PayPal-Account'
+  },
+  'sandbox url' => {
+    'lastUpdated' => '1251900232',
+    'message' => 'Sandbox URL'
+  },
+  'sandbox url help' => {
+    'lastUpdated' => '1251900263',
+    'message' => 'URL an die beim Testen versandt werden soll (Benutzung der Sandbox)'
+  },
+  'signature' => {
+    'lastUpdated' => '1251900276',
+    'message' => 'Signatur'
+  },
+  'signature help' => {
+    'lastUpdated' => '1251900297',
+    'message' => 'Die Accountsigantur für Ihren PayPal-Account.'
+  },
+  'use sandbox' => {
+    'lastUpdated' => '1251900309',
+    'message' => 'Sandbox benutzen'
+  },
+  'use sandbox help' => {
+    'lastUpdated' => '1251900394',
+    'message' => 'Setzen Sie diese Option auf "Ja", wenn Sie die PayPal-SANDBOX-Entwicklungsumgebung (nicht PRODUKTION) benutzen möchten. Empfohlen für Testzwecke.'
+  },
+  'vendorId' => {
+    'lastUpdated' => '1251900408',
+    'message' => 'PayPal-Account '
+  },
+  'vendorId help' => {
+    'lastUpdated' => '1251900435',
+    'message' => 'Geben Sie die E-Mail-Adresse ein, mit der sie sich beim PayPal identifizieren.'
+  }
+}
+;
 
 1;
