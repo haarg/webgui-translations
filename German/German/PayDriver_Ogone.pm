9a5a49fa1a0c56f8034b7417d9d015580746b78b
Translation Server <translation@plainblack.com>
2009-09-02 14:21:42 +0000

    Update from translation server

--- German/German/PayDriver_Ogone.pm
+++ German/German/PayDriver_Ogone.pm
@@ -1,5 +1,55 @@
 package WebGUI::i18n::German::PayDriver_Ogone;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'Ogone' => {
+    'lastUpdated' => '1251900963',
+    'message' => 'Ogone'
+  },
+  'choose billing address' => {
+    'lastUpdated' => '1251900975',
+    'message' => 'Rechnungsadresse auswählen'
+  },
+  'currency' => {
+    'lastUpdated' => '1251900997',
+    'message' => 'Währung (ISO Alpha Code)'
+  },
+  'locale' => {
+    'lastUpdated' => '1251901016',
+    'message' => 'Ogone Sprache'
+  },
+  'locale help' => {
+    'lastUpdated' => '1251901087',
+    'message' => 'Der Locale-Wert für die Sprache, in der das Ogono-Interface angezeigt werden soll (z.B. nl_NL oder en_US)'
+  },
+  'pay' => {
+    'lastUpdated' => '1251901104',
+    'message' => 'Bezahlen'
+  },
+  'please choose a billing address' => {
+    'lastUpdated' => '1251901121',
+    'message' => 'Bitte wählen Sie ein Rechnungsadresse'
+  },
+  'postback secret help' => {
+    'lastUpdated' => '1251901208',
+    'message' => 'Die Passphrase, die Sie in Sektion 4.4 der technischen Informationsseite im Ogono-Interface angegeben haben.'
+  },
+  'psp id' => {
+    'lastUpdated' => '1251901292',
+    'message' => 'PSP ID '
+  },
+  'psp id help' => {
+    'lastUpdated' => '1251901274',
+    'message' => 'Ihr Ogono-Benutzername'
+  },
+  'sha secret help' => {
+    'lastUpdated' => '1251901254',
+    'message' => 'Die Passphrase, die Sie in Sektion 3.2 der technischen Informationsseite im Ogono-Interface angegeben haben.'
+  },
+  'use test mode' => {
+    'lastUpdated' => '1251901141',
+    'message' => 'Im Testmodus benutzen?'
+  }
+}
+;
 
 1;
