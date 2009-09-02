d2b789a7055e7824eff25330df6e4e51fb143b30
Translation Server <translation@plainblack.com>
2009-09-02 14:15:34 +0000

    Update from translation server

--- German/German/PayDriver_PayPal.pm
+++ German/German/PayDriver_PayPal.pm
@@ -9,17 +9,41 @@ our $I18N = {
     'lastUpdated' => '1250974075',
     'message' => 'Fehler bei der Kommunikation mit dem PayPal API: %s'
   },
+  'api help' => {
+    'lastUpdated' => '1251900636',
+    'message' => 'Basis-URL für PayPal\'s NVP api'
+  },
+  'apiSandbox' => {
+    'lastUpdated' => '1251900643',
+    'message' => 'API Sandbox URL'
+  },
+  'apiSandbox help' => {
+    'lastUpdated' => '1251900665',
+    'message' => 'URL der Paypal API im Testmodus'
+  },
   'currency' => {
     'lastUpdated' => '1250974089',
     'message' => 'Währungscode'
+  },
+  'currency help' => {
+    'lastUpdated' => '1251900694',
+    'message' => 'Der PayPal-Währungscode der genutzt wird (z.B. USD)'
+  },
+  'internal paypal error' => {
+    'lastUpdated' => '1251900712',
+    'message' => 'Interner PayPal-Fehler'
   },
   'name' => {
     'lastUpdated' => '1250974168',
     'message' => 'PayPal'
   },
   'password' => {
-    'lastUpdated' => '1250974106',
-    'message' => 'Paßwort'
+    'lastUpdated' => '1251900770',
+    'message' => 'Passwort'
+  },
+  'password help' => {
+    'lastUpdated' => '1251900758',
+    'message' => 'Passwort der PayPal credentials'
   },
   'payment status' => {
     'lastUpdated' => '1250974126',
@@ -29,9 +53,33 @@ our $I18N = {
     'lastUpdated' => '1250974139',
     'message' => 'PayPal URL'
   },
+  'paypal help' => {
+    'lastUpdated' => '1251900812',
+    'message' => 'URL für den Redirect zu PayPal'
+  },
+  'sandbox' => {
+    'lastUpdated' => '1251900820',
+    'message' => 'Sandbox URL'
+  },
+  'sandbox help' => {
+    'lastUpdated' => '1251900872',
+    'message' => 'URL für den Redirect zu PayPal im Testmodus'
+  },
+  'signature' => {
+    'lastUpdated' => '1251900883',
+    'message' => 'Signatur'
+  },
+  'signature help' => {
+    'lastUpdated' => '1251900897',
+    'message' => 'Signature der PayPal credentials'
+  },
   'testMode' => {
     'lastUpdated' => '1250974189',
     'message' => 'Testmodus'
+  },
+  'testMode help' => {
+    'lastUpdated' => '1251900925',
+    'message' => 'Ob die PayPal Sandbox benutzt wird.'
   },
   'user' => {
     'lastUpdated' => '1250974150',
