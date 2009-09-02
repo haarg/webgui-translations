661d43ef4b31b92a19780bf2e71a667931d7299d
Translation Server <translation@plainblack.com>
2009-09-02 13:49:22 +0000

    Update from translation server

--- German/German/PassiveAnalytics.pm
+++ German/German/PassiveAnalytics.pm
@@ -6,35 +6,132 @@ our $I18N = {
     'message' => 'Behälter hinzufügen'
   },
   'Begin analysis' => {
-    'lastUpdated' => '1238880849',
-    'message' => 'Begin Analyse'
+    'lastUpdated' => '1251897643',
+    'message' => 'Analyse starten'
   },
   'Bucket Name' => {
     'lastUpdated' => '1241167803',
     'message' => 'Behältername'
   },
+  'Bucket Name help' => {
+    'lastUpdated' => '1251897685',
+    'message' => 'Wählen Sie einen eindeutigen, beschreibenden, kurzen Namen für diesen Behälter'
+  },
+  'Bucket Passive Analytics' => {
+    'lastUpdated' => '1251899313',
+    'message' => 'Behälter Passive Analytics'
+  },
+  'Delete Delta Table?' => {
+    'lastUpdated' => '1251899293',
+    'message' => 'Deltatabelle löschen?'
+  },
+  'Delete Delta Table? help' => {
+    'lastUpdated' => '1251898978',
+    'message' => 'Sollen die Deltadaten gelöscht werden, wenn der Passive Analytics Lauf abgeschlossen ist?'
+  },
+  'Edit Rule' => {
+    'lastUpdated' => '1251897961',
+    'message' => 'Regel bearbeiten'
+  },
+  'Enabled?' => {
+    'lastUpdated' => '1251897946',
+    'message' => 'Passive Analytics einschalten?'
+  },
+  'Enabled? help' => {
+    'lastUpdated' => '1251898931',
+    'message' => 'Passive Analytics sammelt keine Daten, solange Sie nicht aktiviert. '
+  },
+  'Export bucket data' => {
+    'lastUpdated' => '1251898882',
+    'message' => 'Behälterdaten exportieren'
+  },
+  'Export delta data' => {
+    'lastUpdated' => '1251898608',
+    'message' => 'Delta-Daten exportieren'
+  },
+  'Export raw logs' => {
+    'lastUpdated' => '1251898588',
+    'message' => 'Rohdaten-Log exportieren'
+  },
+  'Passive Analytics' => {
+    'lastUpdated' => '1251898574',
+    'message' => 'Passive Analytics
+'
+  },
+  'Passive Analytics Settings' => {
+    'lastUpdated' => '1251898565',
+    'message' => 'Einstellungen Passive Analytics'
+  },
+  'Regular Expression Error:' => {
+    'lastUpdated' => '1251898538',
+    'message' => 'Fehler im regulären Ausdruck:'
+  },
+  'Summarize Passive Analytics' => {
+    'lastUpdated' => '1251898306',
+    'message' => 'Zusammenfassung Passive Analytics'
+  },
   'User' => {
     'lastUpdated' => '1238880833',
     'message' => 'Benutzer'
   },
+  'User help' => {
+    'lastUpdated' => '1251898187',
+    'message' => 'Der Be nutzer der eine E-Mail erhält, wenn die Behälter-Verarbeitung abgeschlossen ist'
+  },
+  'already active' => {
+    'lastUpdated' => '1251898220',
+    'message' => 'Passive Analytics ist schon aktiviert. '
+  },
+  'confirm delete rule' => {
+    'lastUpdated' => '1251898277',
+    'message' => 'Sind Sie sicher, dass Sie diese Regel löschen möchten?'
+  },
+  'currently running' => {
+    'lastUpdated' => '1251898258',
+    'message' => 'Zur Zeit läuft eine Analyse'
+  },
+  'default pause interval' => {
+    'lastUpdated' => '1251898234',
+    'message' => 'Standardpausenintervall'
+  },
+  'default pause interval help' => {
+    'lastUpdated' => '1251898088',
+    'message' => 'Das Standardpausenintervall festlegen, das der Nutzer auf der PassiveAnalytics-Seite sieht'
+  },
+  'error creating workflow' => {
+    'lastUpdated' => '1251898042',
+    'message' => 'Fehler beim Erstellen der Workflow-Instanz'
+  },
+  'manage ruleset' => {
+    'lastUpdated' => '1251897894',
+    'message' => 'Regeln verwalten'
+  },
   'other' => {
     'lastUpdated' => '1238880823',
     'message' => 'sonstige'
+  },
+  'pause interval' => {
+    'lastUpdated' => '1251897805',
+    'message' => 'Pausenwert'
+  },
+  'pause interval help' => {
+    'lastUpdated' => '1251897880',
+    'message' => 'Legen Sie die Zeit zwischen zwei Klicks fest, in der angenommen wird, dass der Benutzer die Seite liest und nicht die Seite verlässt oder eine neue Sitzung beginnt.'
   },
   'regexp' => {
     'lastUpdated' => '1238880390',
     'message' => 'Regulärer Ausdruck'
   },
   'regexp help' => {
-    'lastUpdated' => '1238880790',
-    'message' => 'Definiere ein regulärer Ausdruck um Log-Einträge zu selektieren:
+    'lastUpdated' => '1251897774',
+    'message' => 'Definiere einen regulärer Ausdruck um Log-Einträge zu selektieren:
 ^ = Anfang der URL
 $ = Ende der URL
 . = willkürlicher Charakter
 * = beliebige Wiederholung
 + = 1 oder mehrere
 ? = 0 oder 1
-Meta-charakter sollten ein "\\" vorangestellt werden, wenn diese als normaler Text
+Metazeichen sollten ein "\\" vorangestellt werden, wenn diese als normaler Text
 interpretiert werden müssen, z.B.:
 home\\?func=match
 home\\?name=abc\\.cde   (wenn name "abc.cde" und nicht auch "abczcde" sein soll)'
