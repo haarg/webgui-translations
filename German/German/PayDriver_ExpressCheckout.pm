f01eea5cd9801986323e66b6ccf47367dc45929f
Translation Server <translation@plainblack.com>
2009-09-02 16:19:57 +0000

    Update from translation server

--- German/German/PayDriver_ExpressCheckout.pm
+++ German/German/PayDriver_ExpressCheckout.pm
@@ -2,28 +2,92 @@ package WebGUI::i18n::German::PayDriver_ExpressCheckout;
 use utf8;
 our $I18N = {
   'api' => {
-    'lastUpdated' => '1250975049',
+    'lastUpdated' => '1251907979',
     'message' => 'API URL'
   },
   'api error' => {
-    'lastUpdated' => '1250975077',
+    'lastUpdated' => '1251907985',
     'message' => 'Fehler bei der Kommunikation mit PayPal: %s'
   },
+  'api help' => {
+    'lastUpdated' => '1251907998',
+    'message' => 'Base URL für PayPal\'s NVP api'
+  },
+  'apiSandbox' => {
+    'lastUpdated' => '1251908006',
+    'message' => 'API Sandbox URL '
+  },
+  'apiSandbox help' => {
+    'lastUpdated' => '1251908031',
+    'message' => 'URL für Paypal API im Testmodus'
+  },
   'currency' => {
-    'lastUpdated' => '1250975094',
+    'lastUpdated' => '1251908051',
     'message' => 'Währungscode'
   },
+  'currency help' => {
+    'lastUpdated' => '1251908073',
+    'message' => 'Paypal Währungscode (z.B. USD)'
+  },
+  'internal paypal error' => {
+    'lastUpdated' => '1251908091',
+    'message' => 'Interner Paypal Fehler'
+  },
   'name' => {
-    'lastUpdated' => '1250975112',
+    'lastUpdated' => '1251908104',
     'message' => 'PayPal Express Checkout'
   },
   'password' => {
-    'lastUpdated' => '1250975128',
-    'message' => 'Paßwort'
+    'lastUpdated' => '1251908099',
+    'message' => 'Passwort'
+  },
+  'password help' => {
+    'lastUpdated' => '1251908129',
+    'message' => 'Passwort von PayPal credentials'
+  },
+  'payment status' => {
+    'lastUpdated' => '1251908150',
+    'message' => 'Bezahlstatus: %s'
   },
   'paypal' => {
-    'lastUpdated' => '1250975146',
+    'lastUpdated' => '1251908251',
     'message' => 'PayPal URL'
+  },
+  'paypal help' => {
+    'lastUpdated' => '1251908295',
+    'message' => 'URL die für den Redirect zu Paypal benutzt wird'
+  },
+  'sandbox' => {
+    'lastUpdated' => '1251908280',
+    'message' => 'Sandbox URL'
+  },
+  'sandbox help' => {
+    'lastUpdated' => '1251908306',
+    'message' => 'URL die für den Redirect zu Paypal im Testmodus benutzt wird'
+  },
+  'signature' => {
+    'lastUpdated' => '1251908318',
+    'message' => 'Signatur'
+  },
+  'signature help' => {
+    'lastUpdated' => '1251908332',
+    'message' => 'Signatur für PayPal credentials'
+  },
+  'testMode' => {
+    'lastUpdated' => '1251908340',
+    'message' => 'Testmodus'
+  },
+  'testMode help' => {
+    'lastUpdated' => '1251908360',
+    'message' => 'Ob Paypals Sandbox benutzt wird'
+  },
+  'user' => {
+    'lastUpdated' => '1251908374',
+    'message' => 'Benutztername'
+  },
+  'user help' => {
+    'lastUpdated' => '1251908388',
+    'message' => 'Benutzername für Paypal credentials'
   }
 }
 ;
