733a4bc68c5aa94df142738aade8b242fa1a6a56
Translation Server <translation@plainblack.com>
2009-09-06 22:03:39 +0000

    Update from translation server

--- German/German/Asset_SyndicatedContent.pm
+++ German/German/Asset_SyndicatedContent.pm
@@ -6,16 +6,16 @@ our $I18N = {
     'message' => 'URL zur RSS-Datei'
   },
   '1 description' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1252267824',
+    'message' => "<p>Geben Sie den exakten URL (beginnt mit http://) zur RDF- oder RSS-Datei&nbsp; des syndizierten Inhaltes an. Die syndizierten Inhalte werden von dieser URL st\x{fc}ndlich heruntergealden.</p>\r\n<p>Sie finden syndizierte Inhalte zum Beispiel an folgenden Stellen:</p>\r\n<div>\r\n<ul>\r\n<li><a href=\"http://www.newsisfree.com/\">http://www.newsisfree.com</a></li>\r\n<li><a href=\"http://www.syndic8.com/\">http://www.syndic8.com</a></li>\r\n<li><a href=\"http://www.voidstar.com/node.php?id=144\">http://www.voidstar.com/node.php?id=144</a></li>\r\n<li><a href=\"http://my.userland.com/\">http://my.userland.com</a></li>\r\n<li><a href=\"http://www.webreference.com/services/news/\">http://www.webreference.com/services/news/</a></li>\r\n<li><a href=\"http://w.moreover.com/\">http://w.moreover.com/</a></li>\r\n</ul>\r\n</div>\r\n<p>Zur Zeit kann WebGUI folgende RSS-Versions verarbeiten: .90, .91, 1.0 und 2.0; Atom .3 und 1.0. Evetnuelle funktionieren auch andere Dateien im RSS\x{20ac}-format. </p>\r\n<p>Um einen aggregierten RSS-Feed zu erstellen (der Informationen von&nbsp; mehreren RSS-Feeds einsammelt), geben Sie statt&nbsp; einem einzelnen URL eine Liste mit mehreren URLS zeilenweise ein.&nbsp; Die Sortierreihenfolge richtet sich nach dem Datum, an dem WebGUI die Geschichte erstmals erhalten hat.</p>"
   },
   3 => {
     'lastUpdated' => '1251052566',
     'message' => 'Maximale Anzahl der Überschriften'
   },
   '3 description' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1252267971',
+    'message' => 'Geben Sie die Maximalzahl an Überschriften an, die&nbsp; angezeigt werden sollen. Null steht für eine beliebige Anzahl. Beachten Sie, dass immer alle Überschriften von allen RSS-URLs geholt werden, auch wenn Sie dann nicht angezeigt werden.'
   },
   4 => {
     'lastUpdated' => '1251052590',
@@ -26,8 +26,8 @@ our $I18N = {
     'message' => 'Vorlage für syndizierten Inhalt'
   },
   '72 description' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1252268002',
+    'message' => 'Wählen Sie ein Template für diesen Inhalt.'
   },
   'RSS Feed Title Suffix' => {
     'lastUpdated' => undef,
@@ -77,17 +77,101 @@ our $I18N = {
     'lastUpdated' => '1251057719',
     'message' => 'Urheberrechtsinformation. Diese Variable wird in einer Multi-Feed-Liste beim ersten Feed ausgegeben.'
   },
+  'channel_date' => {
+    'lastUpdated' => '1252270726',
+    'message' => 'Datum an dem dieser Kanal aktualisiert wurde. Richtet sich bei einer Multifeedliste nach dem ersten Feed.'
+  },
+  'channel_description' => {
+    'lastUpdated' => '1252270892',
+    'message' => 'Eine Beschreibung des Inhalts, den dieser Kanal bereitstellt. Richtet sich bei einer Multifeedliste nach dem ersten Feed.'
+  },
+  'channel_image_description' => {
+    'lastUpdated' => '1252271112',
+    'message' => 'Die Beschreibung des Bildes, das diesem Kanal zugeordnet ist.  Richtet sich bei einer Multifeedliste nach dem ersten Feed.'
+  },
+  'channel_image_height' => {
+    'lastUpdated' => '1252271178',
+    'message' => 'Höhe des Feedbildes in Pixeln. Richtet sich bei einer Multifeedliste nach dem ersten Feed.'
+  },
+  'channel_image_link' => {
+    'lastUpdated' => '1252271534',
+    'message' => 'URL mit dem das Feedbild verlinkt ist. Richtet sich bei einer Multifeedliste nach dem ersten Feed.'
+  },
+  'channel_image_title' => {
+    'lastUpdated' => '1252271559',
+    'message' => 'Titel des Feedbildes. Richtet sich bei einer Multifeedliste nach dem ersten Feed.'
+  },
+  'channel_image_url' => {
+    'lastUpdated' => '1252271203',
+    'message' => 'URL des Feedbildes. Richtet sich bei einer Multifeedliste nach dem ersten Feed.'
+  },
+  'channel_image_width' => {
+    'lastUpdated' => '1252271171',
+    'message' => 'Breite des Feedbildes in Pixeln. Richtet sich bei einer Multifeedliste nach dem ersten Feed.'
+  },
+  'channel_link' => {
+    'lastUpdated' => '1252271665',
+    'message' => 'URL zur Ursprungssite dieses Kanals. Richtet sich bei einer Multifeedliste nach dem ersten Feed.'
+  },
+  'channel_title' => {
+    'lastUpdated' => '1252271252',
+    'message' => 'Titel dieses Einzelteils an syndiziertem Inhalt. Richtet sich bei einer Multifeedliste nach dem ersten Feed.'
+  },
   'date' => {
     'lastUpdated' => '1251056539',
     'message' => 'Der Veröffentlichungszeitpunkt dieser Meldung.'
   },
   'description' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1252272001',
+    'message' => 'Beschreibung des Inhalts. Wenn Sie nach Ausdrücken filtern, wird dieses Feld auch ausgewertet.'
+  },
+  'descriptionFirst100words' => {
+    'lastUpdated' => '1252272046',
+    'message' => 'Die ersten 100 Worte der Beschreibung.'
+  },
+  'descriptionFirst10words' => {
+    'lastUpdated' => '1252272098',
+    'message' => 'Die ersten 10 Worte der Beschreibung.'
+  },
+  'descriptionFirst25words' => {
+    'lastUpdated' => '1252272088',
+    'message' => 'Die ersten 25 Worte der Beschreibung.'
+  },
+  'descriptionFirst2paragraphs' => {
+    'lastUpdated' => '1252272111',
+    'message' => 'Die ersten 2 Absätze der Beschreibung.'
+  },
+  'descriptionFirst2sentences' => {
+    'lastUpdated' => '1252272129',
+    'message' => 'Die ersten 2 Sätze der Beschreibung.'
+  },
+  'descriptionFirst3sentences' => {
+    'lastUpdated' => '1252272136',
+    'message' => 'Die ersten 3 Sätze der Beschreibung.'
+  },
+  'descriptionFirst4sentences' => {
+    'lastUpdated' => '1252272145',
+    'message' => 'Die ersten 4 Sätze der Beschreibung.'
+  },
+  'descriptionFirst50words' => {
+    'lastUpdated' => '1252272160',
+    'message' => 'Die ersten 50 Worte der Beschreibung.'
+  },
+  'descriptionFirst75words' => {
+    'lastUpdated' => '1252272168',
+    'message' => 'Die ersten 75 Worte der Beschreibung.'
+  },
+  'descriptionFirstParagraph' => {
+    'lastUpdated' => '1252272192',
+    'message' => 'Der erste Absatz der Beschreibung.'
+  },
+  'descriptionFirstSentence' => {
+    'lastUpdated' => '1252272207',
+    'message' => 'Der erste Satz der Beschreibung.'
   },
   'displayMode' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1252272389',
+    'message' => 'Wenn das Asset so eingestellt ist, dass RSS-Überschriften nach dem Titel der Ursprungs-RSS-Site sortiert werden, ist dies der String "gruppiert" Sonst ist er "verschachtelt".'
   },
   'displayModeLabel' => {
     'lastUpdated' => undef,
@@ -102,8 +186,8 @@ our $I18N = {
     'message' => ''
   },
   'get syndicated content' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1252272404',
+    'message' => 'Syndizierten Inhalt holen'
   },
   'grouped' => {
     'lastUpdated' => undef,
@@ -114,48 +198,56 @@ our $I18N = {
     'message' => 'Eine eindeutige ID für diese Meldung'
   },
   'hasTerms' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1252272439',
+    'message' => 'Ausdrücke nach denen RSS-Inhalte gefiltert werden'
   },
   'hasTermsLabel' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1252272452',
+    'message' => 'Mit jedem dieser Ausdrücke'
   },
   'hasTermsLabel description' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1252272688',
+    'message' => 'Geben Sie Ausdrücke komma-separiert ein, nach denen Sie die Feed filtern möchten. Wenn Sie z.B. 
+
+linux, windows development, blogs
+
+eingeben, zeigt das Syndizierte Inhalte Weboject Meldungen aus allen aggregierten Feeds an, die "linux", "windows development" oder "blogs" (im Titel oder der Beschreibung der Meldung) enthalten.'
   },
   'interleaved' => {
     'lastUpdated' => undef,
     'message' => ''
   },
   'item_loop' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1252272711',
+    'message' => 'Eine Schleife, die die Daten dieses Kanals enthält.'
   },
   'link' => {
     'lastUpdated' => '1251056694',
     'message' => 'Der URL direkt zum Inhalt der Meldung'
   },
   'maxHeadlines' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1252274111',
+    'message' => 'Die Maximalzahl von Überschriften, die angezeigt werden.'
   },
   'new_rss_site' => {
     'lastUpdated' => undef,
     'message' => ''
   },
   'process macros in rss url' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1252274270',
+    'message' => 'Makros in RSS URLs ausführen'
   },
   'process macros in rss url description' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1252274320',
+    'message' => 'Wenn Sie hier Ja auswählen, können Sie in URS Makros benutzen'
   },
   'processMacrosInRssUrl' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1252274394',
+    'message' => 'Eine Bedingung die anzeigt, ob in diesem Asset Makros im RSS URL-Feld ausgeführt werden. '
+  },
+  'rdf_url' => {
+    'lastUpdated' => '1252274442',
+    'message' => 'Der Inhalt dieses Assets als RDF/RSS 1.0 Feed.'
   },
   'rss.url' => {
     'lastUpdated' => undef,
@@ -178,12 +270,16 @@ our $I18N = {
     'message' => ''
   },
   'rssTabName' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1252274450',
+    'message' => 'RSS'
   },
   'rssUrl' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1252274481',
+    'message' => 'Eine zeilenweise Auflistung aller RSS URLs'
+  },
+  'rss_url' => {
+    'lastUpdated' => '1252274575',
+    'message' => 'Dies ist der URL, um den Inhalt dieses Syndizierte Inhalte-Assets als RSS 2.0 Feed zu erhalten Zusätzlich können Sie die RSS-Version mit den folgenden Templatevariablen festlegen:'
   },
   'site_link' => {
     'lastUpdated' => undef,
@@ -202,8 +298,8 @@ our $I18N = {
     'message' => 'Die ID des Templates mit dem dieses Asset angezeigt wird.'
   },
   'title' => {
-    'lastUpdated' => undef,
-    'message' => ''
+    'lastUpdated' => '1252274609',
+    'message' => 'Der Title einer Meldung. Wenn Sie nach Ausdrücken filtern, wird dieses Feld untersucht.'
   }
 }
 ;
