1a7cfd79b7e3ba61b5711977dc3ca49980cdf19a
Translation Server <translation@plainblack.com>
2009-09-09 05:53:29 +0000

    Update from translation server

--- German/German/Friends.pm
+++ German/German/Friends.pm
@@ -2,219 +2,219 @@ package WebGUI::i18n::German::Friends;
 use utf8;
 our $I18N = {
   'add' => {
-    'lastUpdated' => 1221289516,
+    'lastUpdated' => '1221289516',
     'message' => 'hinzufügen'
   },
   'add to friends' => {
-    'lastUpdated' => 1226019814,
-    'message' => 'Zu Freunden hinzufügen'
+    'lastUpdated' => '1252475406',
+    'message' => 'Zu Kontakten hinzufügen'
   },
   'add to friends confirmation' => {
-    'lastUpdated' => 1228685796,
-    'message' => '<p>Eine E-Mail wurde an %s geschickt, weil Sieum eine Aufnahme in das Freune-Netzwerk gebeten haben.</p>'
+    'lastUpdated' => '1252475431',
+    'message' => 'Eine E-Mail wurde an %s geschickt, weil Sie um eine Aufnahme in das Kontakte-Netzwerk gebeten haben.'
   },
   'add to friends description' => {
-    'lastUpdated' => 1226019842,
-    'message' => 'Möchten Sie wirklich %s zu Ihren Freunden hinzufügen?'
+    'lastUpdated' => '1252475462',
+    'message' => 'Möchten Sie %s wirklich zu Ihren Kontakten hinzufügen?'
   },
   'add to friends list' => {
-    'lastUpdated' => 1226019872,
-    'message' => 'Diese Person zur Liste meiner Freunde hinzufügen'
+    'lastUpdated' => '1252475487',
+    'message' => 'Diese Person zu meiner Kontakten hinzufügen'
   },
   'add to friends profile' => {
-    'lastUpdated' => 1226019892,
+    'lastUpdated' => '1226019892',
     'message' => 'Zurück zum Profil von %s.'
   },
   'already a friend' => {
-    'lastUpdated' => 1221289484,
-    'message' => 'Du kannst ein Freund nicht zweimal angeben.'
+    'lastUpdated' => '1252475510',
+    'message' => 'Sie können einen Kontakt nicht zweimal angeben.'
   },
   'checkboxForm' => {
-    'lastUpdated' => 1228685886,
+    'lastUpdated' => '1228685886',
     'message' => 'HTML-code für eine Auswahlbox. Diese Auswahlbox wird benutzt, um dem Freund eine E-Mail zu seden, oder ihn zu löschen.'
   },
   'confirm remove friends' => {
-    'lastUpdated' => 1228686232,
-    'message' => 'Sin Sie sicher, dass Sire die ausgewählten Freunde von Ihrer Liste löschen m&ouml;chten?'
+    'lastUpdated' => '1252475543',
+    'message' => 'Sind Sie sicher, dass Sie die ausgewählten Kontakte von Ihrer Liste löschen möchten?'
   },
   'default friend comments' => {
-    'lastUpdated' => 1228686290,
-    'message' => '%s, ich möchte Sie in mein Freunde-Netzwerk aufnehmen. Vielen Dank, %s'
+    'lastUpdated' => '1252475565',
+    'message' => '%s, ich möchte Sie in mein Kontakte-Netzwerk aufnehmen. Vielen Dank, %s'
   },
   'deny unanswered friends' => {
-    'lastUpdated' => 1228686324,
-    'message' => 'Freunde ablehnen, die nicht antworten.'
+    'lastUpdated' => '1252475586',
+    'message' => 'Kontakte ablehnen, die nicht antworten.'
   },
   'does not want to be a friend' => {
-    'lastUpdated' => 1228686347,
+    'lastUpdated' => '1228686347',
     'message' => 'Dieser Benutzer möchte nicht als Freund hinzugefügt werden.'
   },
   'error user is already friend' => {
-    'lastUpdated' => 1228686384,
+    'lastUpdated' => '1228686384',
     'message' => 'Dieser Benutzer ist bereits Mitglied in Ihrem Freunde-Netzwerk'
   },
   'error user is already invited' => {
-    'lastUpdated' => 1228686414,
+    'lastUpdated' => '1228686414',
     'message' => 'Sie haben diesem Benutzer schon ein Einladung zu Ihrem Freunde-Netzwerk geschickt.'
   },
   'formFooter' => {
-    'lastUpdated' => 1228686454,
+    'lastUpdated' => '1228686454',
     'message' => 'HTML-Code der dieses Formular beendet.'
   },
   'formHeader' => {
-    'lastUpdated' => 1228686473,
+    'lastUpdated' => '1228686473',
     'message' => 'HTM-Code, um dieses Formular zu beginnen'
   },
   'friend request' => {
-    'lastUpdated' => 1228686490,
-    'message' => 'Freunde-Anfrage'
+    'lastUpdated' => '1252475599',
+    'message' => 'Kontakt-Anfrage'
   },
   'friend request description' => {
-    'lastUpdated' => 1228686537,
+    'lastUpdated' => '1228686537',
     'message' => '%s hat mit nachfolgendem Kommentar darum gebeten, in Ihre Freunde-Liste aufgenommen zu werden:'
   },
   'friends' => {
-    'lastUpdated' => 1228686613,
+    'lastUpdated' => '1228686613',
     'message' => 'Eine Schleife, die Informationen über Freunde enthält und Formluar-Kontrollfelder, um Freunde zu löschen oder ihnen eine Nachricht zu senden.'
   },
   'friends invitation accepted' => {
-    'lastUpdated' => 1228686640,
+    'lastUpdated' => '1228686640',
     'message' => 'Freunde-Einladung angenommen.'
   },
   'friends invitation not accepted' => {
-    'lastUpdated' => 1228686653,
-    'message' => 'Freunde-Einladung nicht angenommen.'
+    'lastUpdated' => '1252475381',
+    'message' => 'Kontakte-Einladung nicht angenommen.'
   },
   'friends invitation not accepted by user' => {
-    'lastUpdated' => 1228686677,
+    'lastUpdated' => '1228686677',
     'message' => 'Ihre Einladung wurde von %s nicht angenommen.'
   },
   'friends network invitation' => {
-    'lastUpdated' => 1228686694,
-    'message' => 'Einladung zum Freunde-Netzwerk'
+    'lastUpdated' => '1252475361',
+    'message' => 'Einladung zum Kontakte-Netzwerk'
   },
   'invalid invite code' => {
-    'lastUpdated' => 1228686712,
+    'lastUpdated' => '1228686712',
     'message' => 'Ungültiger Einladungscode'
   },
   'invalid invite code message' => {
-    'lastUpdated' => 1228686740,
+    'lastUpdated' => '1228686740',
     'message' => 'Der Einladungscode in Ihrer URL ist ungültig.'
   },
   'invitation accepted by user' => {
-    'lastUpdated' => 1228686755,
-    'message' => 'Ihre einaldung wurde von %s angenommen.'
+    'lastUpdated' => '1252475341',
+    'message' => 'Ihre Einladung wurde von %s angenommen.'
   },
   'invitation approval email' => {
-    'lastUpdated' => 1228686868,
-    'message' => '<p>%s has requested that you join their friend network on this site %s.  %s  Please visit the following url to accept or deny the request:  %s</p><p>&nbsp;</p><p>%s hat Sie eingeladen dem Freunde-Netzwerk der Site %s beizutreten. %s bitte besuchen Sie den folgenden Link um die Einladung anzunehmen oder abzulehnen: %s</p>'
+    'lastUpdated' => '1252475325',
+    'message' => '%s hat Sie eingeladen dem Kontake-Netzwerk der Site %s beizutreten. %s bitte besuchen Sie den folgenden Link um die Einladung anzunehmen oder abzulehnen: %s'
   },
   'manage friends' => {
-    'lastUpdated' => 1228686891,
-    'message' => 'Freunde verwalten und ihnen Nachrichten senden.'
+    'lastUpdated' => '1252475275',
+    'message' => 'Kontakte verwalten oder ihnen Nachrichten senden.'
   },
   'manage friends template' => {
-    'lastUpdated' => 1228686907,
-    'message' => 'Vorlage Freunde verwalten'
+    'lastUpdated' => '1252475231',
+    'message' => 'Vorlage Kontakte verwalten'
   },
   'manage friends template help' => {
-    'lastUpdated' => 1228686973,
-    'message' => 'Welche Vorlage möchten Sie für die &quot;Meine Freunde zeigen&quot;-Anzeige im Benutzer-Account verwenden?'
+    'lastUpdated' => '1252475216',
+    'message' => 'Welche Vorlage möchten Sie für die "Meine Kontakte zeigen"-Anzeige im Benutzer-Account verwenden?'
   },
   'manage friends template title' => {
-    'lastUpdated' => 1228687003,
-    'message' => 'Vorlagen-Variablen für Freunde verwalten'
+    'lastUpdated' => '1252475176',
+    'message' => 'Vorlagen-Variablen für Kontakte verwalten'
   },
   'message' => {
-    'lastUpdated' => 1226020458,
+    'lastUpdated' => '1226020458',
     'message' => 'Nachricht'
   },
   'messageForm' => {
-    'lastUpdated' => 1226020448,
-    'message' => 'HTML-Code für ein Textformular f&uuml;r den Nachrichtentext, der an Ihre Freunde versandt wird.'
+    'lastUpdated' => '1252475160',
+    'message' => 'HTML-Code für ein Textformular für den Nachrichtentext, der an Ihre Konatkte versendet wird.'
   },
   'my friends' => {
-    'lastUpdated' => 1203222390,
-    'message' => 'Meine Freunde'
+    'lastUpdated' => '1252475127',
+    'message' => 'Meine Konatkte'
   },
   'name' => {
-    'lastUpdated' => 1221289449,
-    'message' => 'Der Name dieses Freundes'
+    'lastUpdated' => '1252475116',
+    'message' => 'Der Name dieses Kontaktes'
   },
   'no friends' => {
-    'lastUpdated' => 1203222482,
-    'message' => 'Du hast keine Freunde angemeldet.'
+    'lastUpdated' => '1252475101',
+    'message' => 'Sie haben keine Kontakte registriert.'
   },
   'not the right user' => {
-    'lastUpdated' => 1226020386,
+    'lastUpdated' => '1252475033',
     'message' => 'Der Einladungscode, den Sie benutzen, ist nicht für Sie bestimmt!'
   },
   'offline' => {
-    'lastUpdated' => 1226020359,
+    'lastUpdated' => '1226020359',
     'message' => 'offline'
   },
   'online' => {
-    'lastUpdated' => 1226020352,
+    'lastUpdated' => '1226020352',
     'message' => 'online'
   },
   'profileUrl' => {
-    'lastUpdated' => 1226020344,
-    'message' => 'URL zum Benutzerprofil des Freundes'
+    'lastUpdated' => '1252475022',
+    'message' => 'URL zum Benutzerprofil des Kontaktes'
   },
   'remove' => {
-    'lastUpdated' => 1203222634,
+    'lastUpdated' => '1203222634',
     'message' => 'entfernen'
   },
   'removeFriendButton' => {
-    'lastUpdated' => 1226020327,
-    'message' => 'HTML-Code für einen Butten, mit dem die ausgewählten Freunde gelöscht werden.'
+    'lastUpdated' => '1252475008',
+    'message' => 'HTML-Code für einen Button, mit dem die ausgewählten Kontakte gelöscht werden.'
   },
   'see my friends' => {
-    'lastUpdated' => 1203222711,
-    'message' => 'Meine Freunde sehen'
+    'lastUpdated' => '1252474988',
+    'message' => 'Meine Kontakte sehen'
   },
   'send friend email instructions' => {
-    'lastUpdated' => 1226020269,
-    'message' => 'Wähle die Freunde aus, an die die E-Mail versandt werden soll. Verfassen Sie dann unten die Nachricht.'
+    'lastUpdated' => '1252474974',
+    'message' => 'Wähle die Kontakte aus, an die die E-Mail versandt werden soll. Verfassen Sie dann unten die Nachricht.'
   },
   'send message' => {
-    'lastUpdated' => 1226020221,
+    'lastUpdated' => '1226020221',
     'message' => 'Nachricht abschicken'
   },
   'sendMessageButton' => {
-    'lastUpdated' => 1226020207,
-    'message' => 'HTML-Code für einen Butten, mit dem eine E-Mail an die ausgewählten Freunde verschickt wird.'
+    'lastUpdated' => '1252474953',
+    'message' => 'HTML-Code für einen Butten, mit dem eine E-Mail an die ausgewählten Kontakte verschickt wird.'
   },
   'status' => {
-    'lastUpdated' => 1226015896,
+    'lastUpdated' => '1226015896',
     'message' => 'Der Status des Benutzers, entweder online oder offline'
   },
   'subject' => {
-    'lastUpdated' => 1226020161,
+    'lastUpdated' => '1226020161',
     'message' => 'Betreff'
   },
   'subjectForm' => {
-    'lastUpdated' => 1226020153,
-    'message' => 'HTML-Code für ein Textformular f&uuml;r den Betreff der E-Mail, die an Ihre Freunde geschickt wird.'
+    'lastUpdated' => '1252474927',
+    'message' => 'HTML-Code für ein Textformular für den Betreff der E-Mail, die an Ihre Kontakte geschickt wird.'
   },
   'timeout' => {
-    'lastUpdated' => 1226019914,
+    'lastUpdated' => '1226019914',
     'message' => 'Timeout'
   },
   'timeout help' => {
-    'lastUpdated' => 1226020079,
-    'message' => 'Wie lange dürfen Einladungen in ein Freunde-Netzwerk unbeantwortet bleiben, bevor sie automatisch abgelehnt werden.'
+    'lastUpdated' => '1252474904',
+    'message' => 'Wie lange dürfen Einladungen in ein Kontakt-Netzwerk unbeantwortet bleiben, bevor sie automatisch abgelehnt werden?'
   },
   'topicName' => {
-    'lastUpdated' => 1226019956,
-    'message' => 'Freunde-Netzwerk'
+    'lastUpdated' => '1252474862',
+    'message' => 'Kontakt-Netzwerk'
   },
   'you have been added' => {
-    'lastUpdated' => 1226019980,
-    'message' => 'Sie wurden der Feunde-Liste von %s hizugefügt'
+    'lastUpdated' => '1252474884',
+    'message' => 'Sie wurden der Kontakt-Liste von %s hizugefügt'
   },
   'you have not been added' => {
-    'lastUpdated' => 1226019999,
+    'lastUpdated' => '1226019999',
     'message' => 'Sie haben die Anfrage von %s abgelehnt.'
   }
 }
