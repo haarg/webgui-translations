3fb8198abb3109174b9add02aaaba10f5555ebbb
Translation Server <translation@plainblack.com>
2009-09-09 09:16:29 +0000

    Update from translation server

--- BrazilianPortuguese/BrazilianPortuguese/AdSpace.pm
+++ BrazilianPortuguese/BrazilianPortuguese/AdSpace.pm
@@ -1,262 +1,211 @@
 package WebGUI::i18n::BrazilianPortuguese::AdSpace;
 use utf8;
-
 our $I18N = {
-	'is active' => {
-		message => q|Est&aacute; ativo?|,
-		lastUpdated => 1202573825
-	},
-
-	'priority' => {
-		message => q|Prioridade|,
-		lastUpdated => 1202574072
-	},
-
-	'add an ad' => {
-		message => q|Acrescente um an&uacute;ncio.|,
-		lastUpdated => 1202572903
-	},
-
-	'image help' => {
-		message => q|O arquivo de imagem a ser exibido em cada impress&atilde;o do an&uacute;ncio.|,
-		lastUpdated => 1202573632
-	},
-
-	'text color' => {
-		message => q|Cor do Texto|,
-		lastUpdated => 1168970991
-	},
-
-	'edit ad space' => {
-		message => q|Editar Espa&ccedil;o de An&uacute;ncio|,
-		lastUpdated => 1202573403
-	},
-
-	'ad text' => {
-		message => q|Texto do An&uacute;ncio|,
-		lastUpdated => 1202572128
-	},
-
-	'url' => {
-		message => q|URL|,
-		lastUpdated => 1202574506
-	},
-
-	'background color' => {
-		message => q|Color de Fundo|,
-		lastUpdated => 1202572940
-	},
-
-	'manage ad spaces' => {
-		message => q|Gerenciar espa&ccedil;os de an&uacute;ncio.|,
-		lastUpdated => 1202573929
-	},
-
-	'title help' => {
-		message => q|Um nome leg&iacute;vel.|,
-		lastUpdated => 1202574645
-	},
-
-	'text color help' => {
-		message => q|O valor hexadecimal da cor do texto neste an&uacute;ncio.|,
-		lastUpdated => 1198000615
-	},
-
-	'description help' => {
-		message => q|A descri&ccedil;&atilde;o detalhada de onde este espa&ccedil;o de an&uacute;ncio se localiza no site.<br />A full description of where this ad space will be located on the site.|,
-		lastUpdated => 1202573392
-	},
-
-	'border color help' => {
-		message => q|O valor, em hexadecimal, da cor da borda a ser utilizada neste an&uacute;ncio de texto.|,
-		lastUpdated => 1202573024
-	},
-
-	'rich help' => {
-		message => q|Inclua aqui o c&oacute;digo que ser&aacute; usado para gerar o an&uacute;ncio a ser exibido.|,
-		lastUpdated => 1202574453
-	},
-
-	'name' => {
-		message => q|Nome|,
-		lastUpdated => 1197658894
-	},
-
-	'border color' => {
-		message => q|Cor da Borda|,
-		lastUpdated => 1197658828
-	},
-
-	'description' => {
-		message => q|Descri&ccedil;&atilde;o|,
-		lastUpdated => 1197658812
-	},
-
-	'priority help' => {
-		message => q|<p>Mecanismo para determinar o qu&atilde;o frequente um an&uacute;ncio deve ser exibido. Quanto menor este n&uacute;mero, maior a frequencia de exibi&ccedil;&atilde;o do an&uacute;ncio. Este par&acirc;metro tem pouco ou nenhum efeito em sites com pouco tr&aacute;fego. Por exemplo, se o an&uacute;ncio A tem prioridade 0 e o an&uacute;ncio B tem prioridade 10, em um site com aproximadamente uma exibi&ccedil;&atilde;o de an&uacute;ncio por segudo, o an&uacute;ncio A ser&aacute; exibido aproximadamente 100 vezes mais do que o an&uacute;ncio B. Note que essa propriedade &eacute; um mecanismo aproximado, os valores n&atilde;o sao exatos. Quanto maior o tr&aacute;fego no site, maior o impacto dessa propriedade. </p>|,
-		lastUpdated => 1202574355
-	},
-
-	'height help' => {
-		message => q|A altura (em pixels) para este espa&ccedil;o de an&uacute;ncio.|,
-		lastUpdated => 1202574589
-	},
-
-	'width help' => {
-		message => q|Largura (em pixels) para este espa&ccedil;o de an&uacute;ncio.|,
-		lastUpdated => 1202574576
-	},
-
-	'clicks bought' => {
-		message => q|Clicks Comprados|,
-		lastUpdated => 1202573042
-	},
-
-	'ad id' => {
-		message => q|Id do An&uacute;ncio|,
-		lastUpdated => 1198000541
-	},
-
-	'used' => {
-		message => q|Utilizado|,
-		lastUpdated => 1202574551
-	},
-
-	'error heading' => {
-		message => q|Houve um erro com seu Espa&ccedil;o de An&uacute;ncio:|,
-		lastUpdated => 1202573456
-	},
-
-	'title' => {
-		message => q|T&iacute;tulo|,
-		lastUpdated => 1197658931
-	},
-
-	'type' => {
-		message => q|Tipo|,
-		lastUpdated => 1197658954
-	},
-
-	'type help' => {
-		message => q|Que tipo de an&uacute;ncio estamos exibindo?|,
-		lastUpdated => 1202574623
-	},
-
-	'add ad space body' => {
-		message => q|<p>Este di&aacute;logo lista todos os espa&ccedil;os de an&uacute;ncio que foram criados neste WebGUI. Os &iacute;cones pr&oacute;ximos de cada espa&ccedil;o de an&uacute;ncio permitem que voc&ecirc; o edite ou apague. Tamb&eacute;m est&aacute; presente no di&aacute;logo um link para acrescentar um novo espa&ccedil;o de an&uacute;ncio.</p>|,
-		lastUpdated => 1202572870
-	},
-
-	'width' => {
-		message => q|Largura|,
-		lastUpdated => 1197680576
-	},
-
-	'impressions bought' => {
-		message => q|Impress&otilde;es Compradas|,
-		lastUpdated => 1202573649
-	},
-
-	'topicName' => {
-		message => q|An&uacute;ncio|,
-		lastUpdated => 1202574601
-	},
-
-	'is active help' => {
-		message => q|Este an&uacute;ncio deve ser inclu&iacute;do na rota&ccedil;&atilde;o?|,
-		lastUpdated => 1202573855
-	},
-
-	'confirm ad space delete' => {
-		message => q|Voc&ecirc; tem certeza de que quer apagar este espa&ccedil;o de an&uacute;ncio e todos os an&uacute;ncios associados ao mesmo?|,
-		lastUpdated => 1202573355
-	},
-
-	'name help' => {
-		message => q|Essa sequ&ecirc;ncia de caracteres &eacute; usada em combina&ccedil;&atilde;o com o macro AdSpacestring para posicionar an&uacute;ncios no site. Ela deve conter apenas carateres alfanum&eacute;ricos, sem espa&ccedil;os.|,
-		lastUpdated => 1202574047
-	},
-
-	'url help' => {
-		message => q|O URL para o qual apontar&aacute; o link deste an&uacute;ncio. Note que esse campo n&atilde;o &eacute; usado para an&uacute;ncios do tipo Conte&uacute;do Variado (javascript, flash, formul&aacute;rios html, etc).|,
-		lastUpdated => 1202574730
-	},
-
-	'text' => {
-		message => q|Texto|,
-		lastUpdated => 1197658911
-	},
-
-	'edit this ad space' => {
-		message => q|Editar este espa&ccedil;o de an&uacute;ncio.|,
-		lastUpdated => 1202573435
-	},
-
-	'ad text help' => {
-		message => q|<p>Entre com at&eacute; o texto que voc&ecirc; quer que apare&ccedil;a neste an&uacute;ncio (at&eacute; 255 caracteres). Tenha em mente que, dependendo do tamanho do an&uacute;ncio, pode n&atilde;o ser poss&iacute;vel exibir todo o texto no espa&ccedil;o do an&uacute;ncio. Usualmente se recomenda n&atilde;o ultrapassar de 60 caracteres.</p>|,
-		lastUpdated => 1202572617
-	},
-
-	'rich' => {
-		message => q|Conte&uacute;do Variado (javascript, flash, formul&aacute;rios html, etc).|,
-		lastUpdated => 1202574477
-	},
-
-	'background color help' => {
-		message => q|O valor, em hexadecimal, para a cor de fundo deste an&uacute;ncio de texto.|,
-		lastUpdated => 1202572976
-	},
-
-	'confirm ad delete' => {
-		message => q|Voce tem certeza de que quer apagar este an&uacute;ncio?|,
-		lastUpdated => 1202573300
-	},
-
-	'ad space id' => {
-		message => q|Id do Espa&ccedil;o de An&uacute;ncio|,
-		lastUpdated => 1202572631
-	},
-
-	'error invalid characters' => {
-		message => q|Existem caracteres inv&aacute;lidos no campo de t&iacute;tulo. Por gentileza, utilize apenas letras, n&uacute;meros e espa&ccedil;os.|,
-		lastUpdated => 1202573508
-	},
-
-	'clicks bought help' => {
-		message => q|Quantos cliks foram comprados pelo anunciante? Se foi comprado um n&uacute;mero de impress&otilde;es, voce pode deixar esse valor como zero.|,
-		lastUpdated => 1202573757
-	},
-
-	'height' => {
-		message => q|Altura|,
-		lastUpdated => 1197680565
-	},
-
-	'image' => {
-		message => q|Imagem|,
-		lastUpdated => 1197658852
-	},
-
-	'macro code prompt' => {
-		message => q|Utilize este c&oacute;digo para posicionar este espa&ccedil;o de an&uacute;ncio no site:|,
-		lastUpdated => 1202574062
-	},
-
-	'add ad space' => {
-		message => q|Acrescentar espa&ccedil;o de an&uacute;ncio.|,
-		lastUpdated => 1202572672
-	},
-
-	'edit advertisement' => {
-		message => q|Editar An&uacute;ncio|,
-		lastUpdated => 1202573416
-	},
-
-	'impressions bought help' => {
-		message => q|<p>Quantas impress&otilde;es (exibi&ccedil;&otilde;es individuais do an&uacute;ncio) foram compradas pelo anunciante ? Se foi comprado um n&uacute;mero de clicks, voce pode deixar este valor como zero.&nbsp;</p>|,
-		lastUpdated => 1202573782
-	},
-
-};
+  'ad id' => {
+    'lastUpdated' => '1252487268',
+    'message' => 'Id do Anuncio'
+  },
+  'ad space id' => {
+    'lastUpdated' => '1252487370',
+    'message' => 'Id do Espaço do Anuncio'
+  },
+  'ad text' => {
+    'lastUpdated' => '1252487406',
+    'message' => 'Texto do Anuncio'
+  },
+  'ad text help' => {
+    'lastUpdated' => '1252487600',
+    'message' => '<p>Entre com at&eacute; o texto que voc&ecirc; quer que apare&ccedil;a neste an&uacute;ncio (at&eacute; 255 caracteres). Tenha em mente que, dependendo do tamanho do an&uacute;ncio, pode n&atilde;o ser poss&iacute;vel exibir todo o texto no espa&ccedil;o do an&uacute;ncio. Usualmente se recomenda n&atilde;o ultrapassar de 60 caracteres.</p>'
+  },
+  'add ad space' => {
+    'lastUpdated' => '1202572672',
+    'message' => 'Acrescentar espa&ccedil;o de an&uacute;ncio.'
+  },
+  'add ad space body' => {
+    'lastUpdated' => '1202572870',
+    'message' => '<p>Este di&aacute;logo lista todos os espa&ccedil;os de an&uacute;ncio que foram criados neste WebGUI. Os &iacute;cones pr&oacute;ximos de cada espa&ccedil;o de an&uacute;ncio permitem que voc&ecirc; o edite ou apague. Tamb&eacute;m est&aacute; presente no di&aacute;logo um link para acrescentar um novo espa&ccedil;o de an&uacute;ncio.</p>'
+  },
+  'add an ad' => {
+    'lastUpdated' => '1202572903',
+    'message' => 'Acrescente um an&uacute;ncio.'
+  },
+  'background color' => {
+    'lastUpdated' => '1252487221',
+    'message' => 'Cor de Fundo'
+  },
+  'background color help' => {
+    'lastUpdated' => '1202572976',
+    'message' => 'O valor, em hexadecimal, para a cor de fundo deste an&uacute;ncio de texto.'
+  },
+  'border color' => {
+    'lastUpdated' => '1197658828',
+    'message' => 'Cor da Borda'
+  },
+  'border color help' => {
+    'lastUpdated' => '1202573024',
+    'message' => 'O valor, em hexadecimal, da cor da borda a ser utilizada neste an&uacute;ncio de texto.'
+  },
+  'clicks bought' => {
+    'lastUpdated' => '1202573042',
+    'message' => 'Clicks Comprados'
+  },
+  'clicks bought help' => {
+    'lastUpdated' => '1202573757',
+    'message' => 'Quantos cliks foram comprados pelo anunciante? Se foi comprado um n&uacute;mero de impress&otilde;es, voce pode deixar esse valor como zero.'
+  },
+  'confirm ad delete' => {
+    'lastUpdated' => '1202573300',
+    'message' => 'Voce tem certeza de que quer apagar este an&uacute;ncio?'
+  },
+  'confirm ad space delete' => {
+    'lastUpdated' => '1202573355',
+    'message' => 'Voc&ecirc; tem certeza de que quer apagar este espa&ccedil;o de an&uacute;ncio e todos os an&uacute;ncios associados ao mesmo?'
+  },
+  'description' => {
+    'lastUpdated' => '1197658812',
+    'message' => 'Descri&ccedil;&atilde;o'
+  },
+  'description help' => {
+    'lastUpdated' => '1202573392',
+    'message' => 'A descri&ccedil;&atilde;o detalhada de onde este espa&ccedil;o de an&uacute;ncio se localiza no site.<br />A full description of where this ad space will be located on the site.'
+  },
+  'edit ad space' => {
+    'lastUpdated' => '1202573403',
+    'message' => 'Editar Espa&ccedil;o de An&uacute;ncio'
+  },
+  'edit advertisement' => {
+    'lastUpdated' => '1202573416',
+    'message' => 'Editar An&uacute;ncio'
+  },
+  'edit this ad space' => {
+    'lastUpdated' => '1202573435',
+    'message' => 'Editar este espa&ccedil;o de an&uacute;ncio.'
+  },
+  'error heading' => {
+    'lastUpdated' => '1202573456',
+    'message' => 'Houve um erro com seu Espa&ccedil;o de An&uacute;ncio:'
+  },
+  'error invalid characters' => {
+    'lastUpdated' => '1202573508',
+    'message' => 'Existem caracteres inv&aacute;lidos no campo de t&iacute;tulo. Por gentileza, utilize apenas letras, n&uacute;meros e espa&ccedil;os.'
+  },
+  'height' => {
+    'lastUpdated' => '1197680565',
+    'message' => 'Altura'
+  },
+  'height help' => {
+    'lastUpdated' => '1202574589',
+    'message' => 'A altura (em pixels) para este espa&ccedil;o de an&uacute;ncio.'
+  },
+  'image' => {
+    'lastUpdated' => '1197658852',
+    'message' => 'Imagem'
+  },
+  'image help' => {
+    'lastUpdated' => '1202573632',
+    'message' => 'O arquivo de imagem a ser exibido em cada impress&atilde;o do an&uacute;ncio.'
+  },
+  'impressions bought' => {
+    'lastUpdated' => '1202573649',
+    'message' => 'Impress&otilde;es Compradas'
+  },
+  'impressions bought help' => {
+    'lastUpdated' => '1202573782',
+    'message' => '<p>Quantas impress&otilde;es (exibi&ccedil;&otilde;es individuais do an&uacute;ncio) foram compradas pelo anunciante ? Se foi comprado um n&uacute;mero de clicks, voce pode deixar este valor como zero.&nbsp;</p>'
+  },
+  'is active' => {
+    'lastUpdated' => '1202573825',
+    'message' => 'Est&aacute; ativo?'
+  },
+  'is active help' => {
+    'lastUpdated' => '1202573855',
+    'message' => 'Este an&uacute;ncio deve ser inclu&iacute;do na rota&ccedil;&atilde;o?'
+  },
+  'macro code prompt' => {
+    'lastUpdated' => '1202574062',
+    'message' => 'Utilize este c&oacute;digo para posicionar este espa&ccedil;o de an&uacute;ncio no site:'
+  },
+  'manage ad spaces' => {
+    'lastUpdated' => '1202573929',
+    'message' => 'Gerenciar espa&ccedil;os de an&uacute;ncio.'
+  },
+  'name' => {
+    'lastUpdated' => '1197658894',
+    'message' => 'Nome'
+  },
+  'name help' => {
+    'lastUpdated' => '1202574047',
+    'message' => 'Essa sequ&ecirc;ncia de caracteres &eacute; usada em combina&ccedil;&atilde;o com o macro AdSpacestring para posicionar an&uacute;ncios no site. Ela deve conter apenas carateres alfanum&eacute;ricos, sem espa&ccedil;os.'
+  },
+  'priority' => {
+    'lastUpdated' => '1202574072',
+    'message' => 'Prioridade'
+  },
+  'priority help' => {
+    'lastUpdated' => '1202574355',
+    'message' => '<p>Mecanismo para determinar o qu&atilde;o frequente um an&uacute;ncio deve ser exibido. Quanto menor este n&uacute;mero, maior a frequencia de exibi&ccedil;&atilde;o do an&uacute;ncio. Este par&acirc;metro tem pouco ou nenhum efeito em sites com pouco tr&aacute;fego. Por exemplo, se o an&uacute;ncio A tem prioridade 0 e o an&uacute;ncio B tem prioridade 10, em um site com aproximadamente uma exibi&ccedil;&atilde;o de an&uacute;ncio por segudo, o an&uacute;ncio A ser&aacute; exibido aproximadamente 100 vezes mais do que o an&uacute;ncio B. Note que essa propriedade &eacute; um mecanismo aproximado, os valores n&atilde;o sao exatos. Quanto maior o tr&aacute;fego no site, maior o impacto dessa propriedade. </p>'
+  },
+  'rich' => {
+    'lastUpdated' => '1202574477',
+    'message' => 'Conte&uacute;do Variado (javascript, flash, formul&aacute;rios html, etc).'
+  },
+  'rich help' => {
+    'lastUpdated' => '1202574453',
+    'message' => 'Inclua aqui o c&oacute;digo que ser&aacute; usado para gerar o an&uacute;ncio a ser exibido.'
+  },
+  'text' => {
+    'lastUpdated' => '1197658911',
+    'message' => 'Texto'
+  },
+  'text color' => {
+    'lastUpdated' => '1168970991',
+    'message' => 'Cor do Texto'
+  },
+  'text color help' => {
+    'lastUpdated' => '1198000615',
+    'message' => 'O valor hexadecimal da cor do texto neste an&uacute;ncio.'
+  },
+  'title' => {
+    'lastUpdated' => '1197658931',
+    'message' => 'T&iacute;tulo'
+  },
+  'title help' => {
+    'lastUpdated' => '1202574645',
+    'message' => 'Um nome leg&iacute;vel.'
+  },
+  'topicName' => {
+    'lastUpdated' => '1202574601',
+    'message' => 'An&uacute;ncio'
+  },
+  'type' => {
+    'lastUpdated' => '1197658954',
+    'message' => 'Tipo'
+  },
+  'type help' => {
+    'lastUpdated' => '1202574623',
+    'message' => 'Que tipo de an&uacute;ncio estamos exibindo?'
+  },
+  'url' => {
+    'lastUpdated' => '1202574506',
+    'message' => 'URL'
+  },
+  'url help' => {
+    'lastUpdated' => '1202574730',
+    'message' => 'O URL para o qual apontar&aacute; o link deste an&uacute;ncio. Note que esse campo n&atilde;o &eacute; usado para an&uacute;ncios do tipo Conte&uacute;do Variado (javascript, flash, formul&aacute;rios html, etc).'
+  },
+  'used' => {
+    'lastUpdated' => '1202574551',
+    'message' => 'Utilizado'
+  },
+  'width' => {
+    'lastUpdated' => '1197680576',
+    'message' => 'Largura'
+  },
+  'width help' => {
+    'lastUpdated' => '1202574576',
+    'message' => 'Largura (em pixels) para este espa&ccedil;o de an&uacute;ncio.'
+  }
+}
+;
 
 1;
