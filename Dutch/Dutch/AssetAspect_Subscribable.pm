9bd81ad322d3fbc5cf40effee0a3ae467143702a
Translation Server <translation@plainblack.com>
2009-09-24 13:21:19 +0000

    Update from translation server

--- Dutch/Dutch/AssetAspect_Subscribable.pm
+++ Dutch/Dutch/AssetAspect_Subscribable.pm
@@ -1,5 +1,27 @@
 package WebGUI::i18n::Dutch::AssetAspect_Subscribable;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'Email Template' => {
+    'lastUpdated' => '1253798307',
+    'message' => 'E-mail sjabloon'
+  },
+  'Email Template help' => {
+    'lastUpdated' => '1253798325',
+    'message' => 'Het sjabloon voor het verzenden van notificatie e-mails'
+  },
+  'Subscription Group' => {
+    'lastUpdated' => '1253798350',
+    'message' => 'Ingeschreven groep'
+  },
+  'Subscription Group help' => {
+    'lastUpdated' => '1253798415',
+    'message' => 'De gebruikers die -mail meldingen willen ontavngen voor dit asset.'
+  },
+  'new post' => {
+    'lastUpdated' => '1253798440',
+    'message' => 'heeft gereageerd op 1 van uw abonnementen.'
+  }
+}
+;
 
 1;
--- Dutch/Dutch/Form_Date.pm
+++ Dutch/Dutch/Form_Date.pm
@@ -1,5 +1,15 @@
 package WebGUI::i18n::Dutch::Form_Date;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'Choose a date' => {
+    'lastUpdated' => '1253798454',
+    'message' => 'Kies een datum'
+  },
+  'Invalid year' => {
+    'lastUpdated' => '1253798469',
+    'message' => 'Kies een geldig jaar.'
+  }
+}
+;
 
 1;
