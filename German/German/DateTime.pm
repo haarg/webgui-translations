76b654723121c16f3ba39b44f106d4cbc5decb70
Translation Server <translation@plainblack.com>
2009-09-25 07:45:38 +0000

    Update from translation server

--- German/German/DateTime.pm
+++ German/German/DateTime.pm
@@ -1,107 +1,227 @@
 package WebGUI::i18n::German::DateTime;
 use utf8;
-
 our $I18N = {
-	'sunday' => {
-		message => q|Sonntag|,
-		lastUpdated => 1152970328
-	},
-
-	'october' => {
-		message => q|Oktober|,
-		lastUpdated => 1152970307
-	},
-
-	'monday' => {
-		message => q|Montag|,
-		lastUpdated => 1152970294
-	},
-
-	'july' => {
-		message => q|Juli|,
-		lastUpdated => 1152970266
-	},
-
-	'april' => {
-		message => q|April|,
-		lastUpdated => 1152970213
-	},
-
-	'march' => {
-		message => q|März|,
-		lastUpdated => 1152970283
-	},
-
-	'december' => {
-		message => q|Dezember|,
-		lastUpdated => 1152970222
-	},
-
-	'january' => {
-		message => q|Januar|,
-		lastUpdated => 1152970252
-	},
-
-	'timezone' => {
-		message => q|Zeitzone|,
-		lastUpdated => 1152970346
-	},
-
-	'friday' => {
-		message => q|Freitag|,
-		lastUpdated => 1152970247
-	},
-
-	'may' => {
-		message => q|Mai|,
-		lastUpdated => 1152970288
-	},
-
-	'tuesday' => {
-		message => q|Dienstag|,
-		lastUpdated => 1152970355
-	},
-
-	'june' => {
-		message => q|Juni|,
-		lastUpdated => 1152970276
-	},
-
-	'saturday' => {
-		message => q|Samstag|,
-		lastUpdated => 1152970313
-	},
-
-	'wednesday' => {
-		message => q|Mittwoch|,
-		lastUpdated => 1152970366
-	},
-
-	'thursday' => {
-		message => q|Donnerstag|,
-		lastUpdated => 1152970340
-	},
-
-	'august' => {
-		message => q|August|,
-		lastUpdated => 1152970217
-	},
-
-	'november' => {
-		message => q|November|,
-		lastUpdated => 1152970301
-	},
-
-	'february' => {
-		message => q|Februar|,
-		lastUpdated => 1152970242
-	},
-
-	'september' => {
-		message => q|September|,
-		lastUpdated => 1152970322
-	},
-
-};
+  'Month' => {
+    'lastUpdated' => '1253864458',
+    'message' => 'Monat'
+  },
+  'Year' => {
+    'lastUpdated' => '1253864463',
+    'message' => 'Jahr'
+  },
+  'april' => {
+    'lastUpdated' => '1152970213',
+    'message' => 'April'
+  },
+  'april short' => {
+    'lastUpdated' => '1253864468',
+    'message' => 'Apr'
+  },
+  'august' => {
+    'lastUpdated' => '1152970217',
+    'message' => 'August'
+  },
+  'august short' => {
+    'lastUpdated' => '1253864473',
+    'message' => 'Aug'
+  },
+  'december' => {
+    'lastUpdated' => '1152970222',
+    'message' => 'Dezember'
+  },
+  'december short' => {
+    'lastUpdated' => '1253864478',
+    'message' => 'Dez'
+  },
+  'february' => {
+    'lastUpdated' => '1152970242',
+    'message' => 'Februar'
+  },
+  'february short' => {
+    'lastUpdated' => '1253864483',
+    'message' => 'Feb'
+  },
+  'friday' => {
+    'lastUpdated' => '1152970247',
+    'message' => 'Freitag'
+  },
+  'friday 1char' => {
+    'lastUpdated' => '1253864487',
+    'message' => 'F'
+  },
+  'friday med' => {
+    'lastUpdated' => '1253864494',
+    'message' => 'Fre'
+  },
+  'friday short' => {
+    'lastUpdated' => '1253864500',
+    'message' => 'Fr'
+  },
+  'january' => {
+    'lastUpdated' => '1152970252',
+    'message' => 'Januar'
+  },
+  'january short' => {
+    'lastUpdated' => '1253864507',
+    'message' => 'Jan'
+  },
+  'july' => {
+    'lastUpdated' => '1152970266',
+    'message' => 'Juli'
+  },
+  'july short' => {
+    'lastUpdated' => '1253864512',
+    'message' => 'Jul'
+  },
+  'june' => {
+    'lastUpdated' => '1152970276',
+    'message' => 'Juni'
+  },
+  'june short' => {
+    'lastUpdated' => '1253864516',
+    'message' => 'Jun'
+  },
+  'march' => {
+    'lastUpdated' => '1152970283',
+    'message' => 'März'
+  },
+  'march short' => {
+    'lastUpdated' => '1253864719',
+    'message' => 'Mär'
+  },
+  'may' => {
+    'lastUpdated' => '1152970288',
+    'message' => 'Mai'
+  },
+  'may short' => {
+    'lastUpdated' => '1253864713',
+    'message' => 'Mai'
+  },
+  'monday' => {
+    'lastUpdated' => '1152970294',
+    'message' => 'Montag'
+  },
+  'monday 1char' => {
+    'lastUpdated' => '1253864708',
+    'message' => 'M'
+  },
+  'monday med' => {
+    'lastUpdated' => '1253864703',
+    'message' => 'Mon'
+  },
+  'monday short' => {
+    'lastUpdated' => '1253864697',
+    'message' => 'Mo'
+  },
+  'november' => {
+    'lastUpdated' => '1152970301',
+    'message' => 'November'
+  },
+  'november short' => {
+    'lastUpdated' => '1253864690',
+    'message' => 'Nov'
+  },
+  'october' => {
+    'lastUpdated' => '1152970307',
+    'message' => 'Oktober'
+  },
+  'october short' => {
+    'lastUpdated' => '1253864683',
+    'message' => 'Okt'
+  },
+  'saturday' => {
+    'lastUpdated' => '1152970313',
+    'message' => 'Samstag'
+  },
+  'saturday 1char' => {
+    'lastUpdated' => '1253864677',
+    'message' => 'S'
+  },
+  'saturday med' => {
+    'lastUpdated' => '1253864671',
+    'message' => 'Sam'
+  },
+  'saturday short' => {
+    'lastUpdated' => '1253864666',
+    'message' => 'Sa'
+  },
+  'september' => {
+    'lastUpdated' => '1152970322',
+    'message' => 'September'
+  },
+  'september short' => {
+    'lastUpdated' => '1253864659',
+    'message' => 'Sep'
+  },
+  'sunday' => {
+    'lastUpdated' => '1152970328',
+    'message' => 'Sonntag'
+  },
+  'sunday 1char' => {
+    'lastUpdated' => '1253864653',
+    'message' => 'S'
+  },
+  'sunday med' => {
+    'lastUpdated' => '1253864648',
+    'message' => 'Son'
+  },
+  'sunday short' => {
+    'lastUpdated' => '1253864643',
+    'message' => 'So'
+  },
+  'thursday' => {
+    'lastUpdated' => '1152970340',
+    'message' => 'Donnerstag'
+  },
+  'thursday 1char' => {
+    'lastUpdated' => '1253864637',
+    'message' => 'D'
+  },
+  'thursday med' => {
+    'lastUpdated' => '1253864632',
+    'message' => 'Don'
+  },
+  'thursday short' => {
+    'lastUpdated' => '1253864620',
+    'message' => 'Do'
+  },
+  'timezone' => {
+    'lastUpdated' => '1152970346',
+    'message' => 'Zeitzone'
+  },
+  'tuesday' => {
+    'lastUpdated' => '1152970355',
+    'message' => 'Dienstag'
+  },
+  'tuesday 1char' => {
+    'lastUpdated' => '1253864613',
+    'message' => 'D'
+  },
+  'tuesday med' => {
+    'lastUpdated' => '1253864608',
+    'message' => 'Die'
+  },
+  'tuesday short' => {
+    'lastUpdated' => '1253864597',
+    'message' => 'Di'
+  },
+  'wednesday' => {
+    'lastUpdated' => '1152970366',
+    'message' => 'Mittwoch'
+  },
+  'wednesday 1char' => {
+    'lastUpdated' => '1253864569',
+    'message' => 'M'
+  },
+  'wednesday med' => {
+    'lastUpdated' => '1253864582',
+    'message' => 'Mit'
+  },
+  'wednesday short' => {
+    'lastUpdated' => '1253864587',
+    'message' => 'Mi'
+  }
+}
+;
 
 1;
