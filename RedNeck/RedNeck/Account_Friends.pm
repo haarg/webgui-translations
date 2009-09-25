2410e9f16a581fdc6c9088233edfe40fd26ab76e
Translation Server <translation@plainblack.com>
2009-09-25 15:21:53 +0000

    Updating RedNeck on translation server

--- RedNeck/RedNeck/Account_Friends.pm
+++ RedNeck/RedNeck/Account_Friends.pm
@@ -1,5 +1,11 @@
 package WebGUI::i18n::RedNeck::Account_Friends;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'friend removed' => {
+    'lastUpdated' => '1253892104',
+    'message' => 'Kicked %s off the property.'
+  }
+}
+;
 
 1;
---
+++ RedNeck/RedNeck/Form_Date.pm
@@ -0,0 +1,5 @@
+package WebGUI::i18n::RedNeck::Form_Date;
+use utf8;
+our $I18N = {};
+
+1;
