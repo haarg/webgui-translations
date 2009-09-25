cba00359811a51d635b13e4bbf4d172f6e439de8
Translation Server <translation@plainblack.com>
2009-09-25 07:56:40 +0000

    Update from translation server

--- German/German/Form_Date.pm
+++ German/German/Form_Date.pm
@@ -1,5 +1,15 @@
 package WebGUI::i18n::German::Form_Date;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'Choose a date' => {
+    'lastUpdated' => '1253865368',
+    'message' => 'Datum wählen'
+  },
+  'Invalid year' => {
+    'lastUpdated' => '1253865392',
+    'message' => 'Geben Sie ein zulässiges Jahr ein'
+  }
+}
+;
 
 1;
