47960b55e46df1b9bd9e5bcf42014402ed277d13
Translation Server <translation@plainblack.com>
2009-09-30 08:25:11 +0000

    Updating Russian on translation server

--- Russian/Russian/DateTime.pm
+++ Russian/Russian/DateTime.pm
@@ -1,107 +1,227 @@
 package WebGUI::i18n::Russian::DateTime;
 use utf8;
-
 our $I18N = {
-	'sunday' => {
-		message => q|воскресенье|,
-		lastUpdated => 1149245402
-	},
-
-	'october' => {
-		message => q|октябрь|,
-		lastUpdated => 1149245371
-	},
-
-	'monday' => {
-		message => q|понедельник|,
-		lastUpdated => 1149245351
-	},
-
-	'july' => {
-		message => q|июль|,
-		lastUpdated => 1149245305
-	},
-
-	'april' => {
-		message => q|апрель|,
-		lastUpdated => 1149245247
-	},
-
-	'march' => {
-		message => q|март|,
-		lastUpdated => 1149245333
-	},
-
-	'december' => {
-		message => q|декабрь|,
-		lastUpdated => 1149245265
-	},
-
-	'january' => {
-		message => q|январь|,
-		lastUpdated => 1149245295
-	},
-
-	'timezone' => {
-		message => q|Временной пояс|,
-		lastUpdated => 1149245423
-	},
-
-	'friday' => {
-		message => q|пятница|,
-		lastUpdated => 1149245285
-	},
-
-	'may' => {
-		message => q|май|,
-		lastUpdated => 1149245342
-	},
-
-	'tuesday' => {
-		message => q|вторник|,
-		lastUpdated => 1149245434
-	},
-
-	'june' => {
-		message => q|июнь|,
-		lastUpdated => 1149245315
-	},
-
-	'saturday' => {
-		message => q|суббота|,
-		lastUpdated => 1149245385
-	},
-
-	'wednesday' => {
-		message => q|среда|,
-		lastUpdated => 1149245445
-	},
-
-	'thursday' => {
-		message => q|четверг|,
-		lastUpdated => 1149245412
-	},
-
-	'august' => {
-		message => q|август|,
-		lastUpdated => 1149245256
-	},
-
-	'november' => {
-		message => q|ноябрь|,
-		lastUpdated => 1149245361
-	},
-
-	'february' => {
-		message => q|февраль|,
-		lastUpdated => 1149245275
-	},
-
-	'september' => {
-		message => q|сентябрь|,
-		lastUpdated => 1149245394
-	},
-
-};
+  'Month' => {
+    'lastUpdated' => '1254298521',
+    'message' => "\x{41c}\x{435}\x{441}\x{44f}\x{446}"
+  },
+  'Year' => {
+    'lastUpdated' => '1254298510',
+    'message' => "\x{413}\x{43e}\x{434}"
+  },
+  'april' => {
+    'lastUpdated' => '1149245247',
+    'message' => "\x{430}\x{43f}\x{440}\x{435}\x{43b}\x{44c}"
+  },
+  'april short' => {
+    'lastUpdated' => '1254298531',
+    'message' => "\x{430}\x{43f}\x{440}"
+  },
+  'august' => {
+    'lastUpdated' => '1149245256',
+    'message' => "\x{430}\x{432}\x{433}\x{443}\x{441}\x{442}"
+  },
+  'august short' => {
+    'lastUpdated' => '1254298542',
+    'message' => "\x{430}\x{432}\x{433}"
+  },
+  'december' => {
+    'lastUpdated' => '1149245265',
+    'message' => "\x{434}\x{435}\x{43a}\x{430}\x{431}\x{440}\x{44c}"
+  },
+  'december short' => {
+    'lastUpdated' => '1254298554',
+    'message' => "\x{434}\x{435}\x{43a}"
+  },
+  'february' => {
+    'lastUpdated' => '1149245275',
+    'message' => "\x{444}\x{435}\x{432}\x{440}\x{430}\x{43b}\x{44c}"
+  },
+  'february short' => {
+    'lastUpdated' => '1254298563',
+    'message' => "\x{444}\x{435}\x{432}\x{440}"
+  },
+  'friday' => {
+    'lastUpdated' => '1149245285',
+    'message' => "\x{43f}\x{44f}\x{442}\x{43d}\x{438}\x{446}\x{430}"
+  },
+  'friday 1char' => {
+    'lastUpdated' => '1254298592',
+    'message' => "\x{43f}"
+  },
+  'friday med' => {
+    'lastUpdated' => '1254298608',
+    'message' => "\x{43f}\x{442}\x{43d}"
+  },
+  'friday short' => {
+    'lastUpdated' => '1254298620',
+    'message' => "\x{43f}\x{442}"
+  },
+  'january' => {
+    'lastUpdated' => '1149245295',
+    'message' => "\x{44f}\x{43d}\x{432}\x{430}\x{440}\x{44c}"
+  },
+  'january short' => {
+    'lastUpdated' => '1254298634',
+    'message' => "\x{44f}\x{43d}\x{432}"
+  },
+  'july' => {
+    'lastUpdated' => '1149245305',
+    'message' => "\x{438}\x{44e}\x{43b}\x{44c}"
+  },
+  'july short' => {
+    'lastUpdated' => '1254298645',
+    'message' => "\x{438}\x{44e}\x{43b}"
+  },
+  'june' => {
+    'lastUpdated' => '1149245315',
+    'message' => "\x{438}\x{44e}\x{43d}\x{44c}"
+  },
+  'june short' => {
+    'lastUpdated' => '1254298658',
+    'message' => "\x{438}\x{44e}\x{43d}"
+  },
+  'march' => {
+    'lastUpdated' => '1149245333',
+    'message' => "\x{43c}\x{430}\x{440}\x{442}"
+  },
+  'march short' => {
+    'lastUpdated' => '1254298668',
+    'message' => "\x{43c}\x{430}\x{440}"
+  },
+  'may' => {
+    'lastUpdated' => '1149245342',
+    'message' => "\x{43c}\x{430}\x{439}"
+  },
+  'may short' => {
+    'lastUpdated' => '1254298676',
+    'message' => "\x{43c}\x{430}\x{439}"
+  },
+  'monday' => {
+    'lastUpdated' => '1149245351',
+    'message' => "\x{43f}\x{43e}\x{43d}\x{435}\x{434}\x{435}\x{43b}\x{44c}\x{43d}\x{438}\x{43a}"
+  },
+  'monday 1char' => {
+    'lastUpdated' => '1254298686',
+    'message' => "\x{43f}"
+  },
+  'monday med' => {
+    'lastUpdated' => '1254298703',
+    'message' => "\x{43f}\x{43e}\x{43d}"
+  },
+  'monday short' => {
+    'lastUpdated' => '1254298714',
+    'message' => "\x{43f}\x{43d}"
+  },
+  'november' => {
+    'lastUpdated' => '1149245361',
+    'message' => "\x{43d}\x{43e}\x{44f}\x{431}\x{440}\x{44c}"
+  },
+  'november short' => {
+    'lastUpdated' => '1254298733',
+    'message' => "\x{43d}\x{431}\x{440}"
+  },
+  'october' => {
+    'lastUpdated' => '1149245371',
+    'message' => "\x{43e}\x{43a}\x{442}\x{44f}\x{431}\x{440}\x{44c}"
+  },
+  'october short' => {
+    'lastUpdated' => '1254298740',
+    'message' => "\x{43e}\x{43a}\x{442}"
+  },
+  'saturday' => {
+    'lastUpdated' => '1149245385',
+    'message' => "\x{441}\x{443}\x{431}\x{431}\x{43e}\x{442}\x{430}"
+  },
+  'saturday 1char' => {
+    'lastUpdated' => '1254298750',
+    'message' => "\x{441}"
+  },
+  'saturday med' => {
+    'lastUpdated' => '1254298760',
+    'message' => "\x{441}\x{443}\x{431}"
+  },
+  'saturday short' => {
+    'lastUpdated' => '1254299029',
+    'message' => "\x{441}\x{431}"
+  },
+  'september' => {
+    'lastUpdated' => '1149245394',
+    'message' => "\x{441}\x{435}\x{43d}\x{442}\x{44f}\x{431}\x{440}\x{44c}"
+  },
+  'september short' => {
+    'lastUpdated' => '1254298778',
+    'message' => "\x{441}\x{435}\x{43d}"
+  },
+  'sunday' => {
+    'lastUpdated' => '1149245402',
+    'message' => "\x{432}\x{43e}\x{441}\x{43a}\x{440}\x{435}\x{441}\x{435}\x{43d}\x{44c}\x{435}"
+  },
+  'sunday 1char' => {
+    'lastUpdated' => '1254298787',
+    'message' => "\x{432}"
+  },
+  'sunday med' => {
+    'lastUpdated' => '1254298796',
+    'message' => "\x{432}\x{43e}\x{441}"
+  },
+  'sunday short' => {
+    'lastUpdated' => '1254298802',
+    'message' => "\x{432}\x{441}"
+  },
+  'thursday' => {
+    'lastUpdated' => '1149245412',
+    'message' => "\x{447}\x{435}\x{442}\x{432}\x{435}\x{440}\x{433}"
+  },
+  'thursday 1char' => {
+    'lastUpdated' => '1254298820',
+    'message' => "\x{447}"
+  },
+  'thursday med' => {
+    'lastUpdated' => '1254298829',
+    'message' => "\x{447}\x{442}\x{432}"
+  },
+  'thursday short' => {
+    'lastUpdated' => '1254298836',
+    'message' => "\x{447}\x{442}"
+  },
+  'timezone' => {
+    'lastUpdated' => '1149245423',
+    'message' => "\x{412}\x{440}\x{435}\x{43c}\x{435}\x{43d}\x{43d}\x{43e}\x{439} \x{43f}\x{43e}\x{44f}\x{441}"
+  },
+  'tuesday' => {
+    'lastUpdated' => '1149245434',
+    'message' => "\x{432}\x{442}\x{43e}\x{440}\x{43d}\x{438}\x{43a}"
+  },
+  'tuesday 1char' => {
+    'lastUpdated' => '1254298934',
+    'message' => "\x{432}"
+  },
+  'tuesday med' => {
+    'lastUpdated' => '1254298925',
+    'message' => "\x{432}\x{442}\x{440}"
+  },
+  'tuesday short' => {
+    'lastUpdated' => '1254298915',
+    'message' => "\x{432}\x{442}"
+  },
+  'wednesday' => {
+    'lastUpdated' => '1149245445',
+    'message' => "\x{441}\x{440}\x{435}\x{434}\x{430}"
+  },
+  'wednesday 1char' => {
+    'lastUpdated' => '1254298958',
+    'message' => "\x{441}"
+  },
+  'wednesday med' => {
+    'lastUpdated' => '1254298967',
+    'message' => "\x{441}\x{440}\x{434}"
+  },
+  'wednesday short' => {
+    'lastUpdated' => '1254298975',
+    'message' => "\x{441}\x{440}"
+  }
+}
+;
 
 1;
