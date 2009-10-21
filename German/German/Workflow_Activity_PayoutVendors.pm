2362177f5c7c1fce796492e808162ec50cec8203
Translation Server <translation@plainblack.com>
2009-10-21 08:18:17 +0000

    Updating German on translation server

--- German/German/Workflow_Activity_PayoutVendors.pm
+++ German/German/Workflow_Activity_PayoutVendors.pm
@@ -1,5 +1,43 @@
 package WebGUI::i18n::German::Workflow_Activity_PayoutVendors;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'Currency code' => {
+    'lastUpdated' => '1256112931',
+    'message' => 'Währungskode'
+  },
+  'Notify on error' => {
+    'lastUpdated' => '1256112942',
+    'message' => 'Fehlernachricht'
+  },
+  'PayPal password' => {
+    'lastUpdated' => '1256112955',
+    'message' => 'PayPal-Passwort'
+  },
+  'PayPal signature' => {
+    'lastUpdated' => '1256112971',
+    'message' => 'PayPal-Signatur'
+  },
+  'PayPal username' => {
+    'lastUpdated' => '1256112979',
+    'message' => 'PayPal-Nutzername'
+  },
+  'Subject for vendor notification email' => {
+    'lastUpdated' => '1256113024',
+    'message' => 'Betreff E-Mail-Benachrichtigung an den Verkäufer'
+  },
+  'Use in Sandbox (test-mode)' => {
+    'lastUpdated' => '1256113041',
+    'message' => 'Sandbox(Test)-Modus'
+  },
+  'Vendor Payout' => {
+    'lastUpdated' => '1256113068',
+    'message' => 'Verkäufer-Auszahlung'
+  },
+  'Vendor payout from' => {
+    'lastUpdated' => '1256113087',
+    'message' => 'Verkäufer-Auszahlung von'
+  }
+}
+;
 
 1;
