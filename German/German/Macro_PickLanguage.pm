5cbd404c1e60182aeeb3954d3f5bbae5af1be8b3
Translation Server <translation@plainblack.com>
2009-11-04 09:04:30 +0000

    Updating German on translation server

--- German/German/Macro_PickLanguage.pm
+++ German/German/Macro_PickLanguage.pm
@@ -1,5 +1,47 @@
 package WebGUI::i18n::German::Macro_PickLanguage;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'delete' => {
+    'lastUpdated' => '1257325126',
+    'message' => 'Zurück zur Profil-Sprache'
+  },
+  'delete_label' => {
+    'lastUpdated' => '1257325153',
+    'message' => 'Ein Name für den Link zurück zur Profil-Sprache'
+  },
+  'delete_url' => {
+    'lastUpdated' => '1257325216',
+    'message' => 'Der URL der die Scratch-Sprache dieser Session löscht'
+  },
+  'lang_loop' => {
+    'lastUpdated' => '1257325243',
+    'message' => 'Eine Schleife, die alle installierten Sprachen enthält'
+  },
+  'language_lang' => {
+    'lastUpdated' => '1257325266',
+    'message' => 'Name der Sprache in dieser Sprache'
+  },
+  'language_langAbbr' => {
+    'lastUpdated' => '1257325301',
+    'message' => 'Ein Standardkode für die Sprache, z.B: "en" oder "de".'
+  },
+  'language_langAbbrLoc' => {
+    'lastUpdated' => '1257325339',
+    'message' => 'Ein abgekürzter Standardname für die Sprache, z.B. "US"'
+  },
+  'language_langEng' => {
+    'lastUpdated' => '1257325353',
+    'message' => 'Der englische Name der Sprache'
+  },
+  'language_url' => {
+    'lastUpdated' => '1257325390',
+    'message' => 'URL der die WebGUI-Spracheinstellung auf die gewählte Sprache konfiguriert'
+  },
+  'picklanguage title' => {
+    'lastUpdated' => '1257325460',
+    'message' => 'Template-Variablen des PickLanguage-Makros.'
+  }
+}
+;
 
 1;
