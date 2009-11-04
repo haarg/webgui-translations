e691fa1f6082c693d7745bd6b57f74b4beb2dc3b
Translation Server <translation@plainblack.com>
2009-11-04 10:59:38 +0000

    Updating Dutch on translation server

--- Dutch/Dutch/ShipDriver_UPS.pm
+++ Dutch/Dutch/ShipDriver_UPS.pm
@@ -1,5 +1,123 @@
 package WebGUI::i18n::Dutch::ShipDriver_UPS;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'commercial' => {
+    'lastUpdated' => '1257331044',
+    'message' => 'Commerci&euml;el'
+  },
+  'customer classification' => {
+    'lastUpdated' => '1257331060',
+    'message' => 'Klassificatie klant'
+  },
+  'customer classification 03' => {
+    'lastUpdated' => '1257331082',
+    'message' => 'Af en toe'
+  },
+  'customer classification 04' => {
+    'lastUpdated' => '1257331091',
+    'message' => 'Doorverkoop'
+  },
+  'customer classification help' => {
+    'lastUpdated' => '1257331311',
+    'message' => 'Het type klant'
+  },
+  'flatFee' => {
+    'lastUpdated' => '1257331990',
+    'message' => 'Vast bedrag'
+  },
+  'flatFee help' => {
+    'lastUpdated' => '1257332022',
+    'message' => 'Een vast bedrag wat berekend wordt voor de verzending.'
+  },
+  'instructions' => {
+    'lastUpdated' => '1257332067',
+    'message' => 'Registratie instructies'
+  },
+  'license' => {
+    'lastUpdated' => '1257331975',
+    'message' => 'UPS toegangsnummer licentie'
+  },
+  'license help' => {
+    'lastUpdated' => '1257331948',
+    'message' => 'U ontvangt een licentie bij uw gebruikersnaam en wachtwoord tijdens de registratie.'
+  },
+  'password' => {
+    'lastUpdated' => '1257331957',
+    'message' => 'UPS wachtwoord'
+  },
+  'password help' => {
+    'lastUpdated' => '1257332053',
+    'message' => 'U ontvangt een wachtwoord samen met uw gebruikers id tijdens de registratie.'
+  },
+  'pickup code 01' => {
+    'lastUpdated' => '1257331335',
+    'message' => 'Dagelijks ophalen'
+  },
+  'pickup code 06' => {
+    'lastUpdated' => '1257332172',
+    'message' => 'Eenmalig ophalen'
+  },
+  'pickup type' => {
+    'lastUpdated' => '1257332208',
+    'message' => 'Type ophaaldienst'
+  },
+  'pickup type help' => {
+    'lastUpdated' => '1257332237',
+    'message' => 'Selecteer hoe de pakketten aan de UPS worden overgedragen.'
+  },
+  'ship service' => {
+    'lastUpdated' => '1257332107',
+    'message' => 'Verzend dienst'
+  },
+  'ship type' => {
+    'lastUpdated' => '1257331760',
+    'message' => 'Type verzending'
+  },
+  'ship type help' => {
+    'lastUpdated' => '1257331918',
+    'message' => 'Kies een type verzending. Verschillende types hebben verschillende diensten beschikbaar. Niet alle diensten zijn beschikbaar in alle typen, of in alle landen. Het veranderen van het type zal de diensten aanpassen.'
+  },
+  'source country' => {
+    'lastUpdated' => '1257332293',
+    'message' => 'Land herkomst'
+  },
+  'source country help' => {
+    'lastUpdated' => '1257332257',
+    'message' => 'Het land waarvan verzonden wordt.'
+  },
+  'source zipcode' => {
+    'lastUpdated' => '1257332304',
+    'message' => 'Postcode herkomst'
+  },
+  'source zipcode help' => {
+    'lastUpdated' => '1257332328',
+    'message' => 'De postcode van de locatie vanwaar verzonden wordt.'
+  },
+  'us domestic' => {
+    'lastUpdated' => '1257331480',
+    'message' => 'VS intern'
+  },
+  'us domestic 01' => {
+    'lastUpdated' => '1257331502',
+    'message' => 'UPS luchtpost volgende dag'
+  },
+  'us domestic 02' => {
+    'lastUpdated' => '1257331524',
+    'message' => 'UPS luchtpost tweede dag'
+  },
+  'us domestic 12' => {
+    'lastUpdated' => '1257332354',
+    'message' => 'UPS selecteer uit drie dagen'
+  },
+  'userid' => {
+    'lastUpdated' => '1257331771',
+    'message' => 'UPS gebruikers id'
+  },
+  'userid help' => {
+    'lastUpdated' => '1257332148',
+    'message' => 'U kunt een gebruikers id ontvangen door u eerst bij de UPS te registreren.'
+  }
+}
+;
 
 1;
