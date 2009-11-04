78948d3645e3a478a39e0096366cbc322918e260
Translation Server <translation@plainblack.com>
2009-11-04 09:33:56 +0000

    Updating German on translation server

--- German/German/ShipDriver_UPS.pm
+++ German/German/ShipDriver_UPS.pm
@@ -1,5 +1,23 @@
 package WebGUI::i18n::German::ShipDriver_UPS;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'commercial' => {
+    'lastUpdated' => '1257327133',
+    'message' => 'Kommerziell'
+  },
+  'customer classification' => {
+    'lastUpdated' => '1257327164',
+    'message' => 'Kundenklassifikation'
+  },
+  'customer classification help' => {
+    'lastUpdated' => '1257327207',
+    'message' => 'Welche Art Kunde sind Sie'
+  },
+  'ship type' => {
+    'lastUpdated' => '1257327146',
+    'message' => 'Versandart'
+  }
+}
+;
 
 1;
