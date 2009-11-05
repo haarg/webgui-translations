8dfe349356380cd4d94f9508ef08e10f913c2038
Translation Server <translation@plainblack.com>
2009-11-05 13:24:02 +0000

    Updating German on translation server

--- German/German/ShipDriver_UPS.pm
+++ German/German/ShipDriver_UPS.pm
@@ -9,13 +9,97 @@ our $I18N = {
     'lastUpdated' => '1257327164',
     'message' => 'Kundenklassifikation'
   },
+  'customer classification 01' => {
+    'lastUpdated' => '1257426897',
+    'message' => 'Wiederverkäufer'
+  },
+  'customer classification 03' => {
+    'lastUpdated' => '1257426834',
+    'message' => 'Einzelkauf'
+  },
+  'customer classification 04' => {
+    'lastUpdated' => '1257426857',
+    'message' => 'Ladenverkauf'
+  },
   'customer classification help' => {
     'lastUpdated' => '1257327207',
     'message' => 'Welche Art Kunde sind Sie'
   },
+  'flatFee' => {
+    'lastUpdated' => '1257427021',
+    'message' => 'Versandkostenpauschale'
+  },
+  'flatFee help' => {
+    'lastUpdated' => '1257427119',
+    'message' => 'Ein Festbetrag, der bei einer Bestellung für die Bearbeitung und den Versand berechnet wird.'
+  },
+  'instructions' => {
+    'lastUpdated' => '1257427173',
+    'message' => 'Anmeldebeschreibung'
+  },
+  'license' => {
+    'lastUpdated' => '1257336631',
+    'message' => 'UPS Access License Number'
+  },
+  'license help' => {
+    'lastUpdated' => '1257427323',
+    'message' => 'Sobald Sie sich registrieren, werden Sie eine Lizent mit Ihrer UserId und Ihrem Passwort bekommen. '
+  },
+  'password' => {
+    'lastUpdated' => '1257336508',
+    'message' => 'UPS-Passwort'
+  },
+  'password help' => {
+    'lastUpdated' => '1257336555',
+    'message' => 'Sie erhalten ein Passwort zu Ihrer UserID wenn Sie sich bei UPS registrieren.'
+  },
+  'pickup code 01' => {
+    'lastUpdated' => '1257427347',
+    'message' => 'Tägliche Abholung'
+  },
+  'pickup code 03' => {
+    'lastUpdated' => '1257427398',
+    'message' => 'Kunden-Dienststelle'
+  },
+  'pickup code 06' => {
+    'lastUpdated' => '1257427418',
+    'message' => 'Einzelabholung'
+  },
+  'ship service' => {
+    'lastUpdated' => '1257426953',
+    'message' => 'Versand Information'
+  },
   'ship type' => {
     'lastUpdated' => '1257327146',
     'message' => 'Versandart'
+  },
+  'source country' => {
+    'lastUpdated' => '1257426572',
+    'message' => 'Land Versandort'
+  },
+  'source country help' => {
+    'lastUpdated' => '1257426655',
+    'message' => 'Von diesem Land aus wird die Sendung verschickt.'
+  },
+  'source zipcode' => {
+    'lastUpdated' => '1257426549',
+    'message' => 'Postleitzahl Versandort'
+  },
+  'source zipcode help' => {
+    'lastUpdated' => '1257426501',
+    'message' => 'Die Postleitzahl des Absenderorts'
+  },
+  'us international' => {
+    'lastUpdated' => '1257426979',
+    'message' => 'US International'
+  },
+  'userid' => {
+    'lastUpdated' => '1257336577',
+    'message' => 'UPS UserId'
+  },
+  'userid help' => {
+    'lastUpdated' => '1257336607',
+    'message' => 'Sie erhalten eine UserId bei Ihrer Anmeldung bei UPS'
   }
 }
 ;
