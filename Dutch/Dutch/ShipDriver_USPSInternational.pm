1bc5b65816cc389d75dbdf0f677094fe5a4ac552
Translation Server <translation@plainblack.com>
2009-11-18 15:03:01 +0000

    Updating Dutch on translation server

--- Dutch/Dutch/ShipDriver_USPSInternational.pm
+++ Dutch/Dutch/ShipDriver_USPSInternational.pm
@@ -8,6 +8,34 @@ our $I18N = {
   'express mail international' => {
     'lastUpdated' => '1258535715',
     'message' => 'Expres mail internationaal'
+  },
+  'first class mail international parcels' => {
+    'lastUpdated' => '1258556359',
+    'message' => 'Eerste klas internationale pakketpost'
+  },
+  'global express guaranteed non-rectangular' => {
+    'lastUpdated' => '1258556479',
+    'message' => 'Wereldwijde gegarandeerde expres niet-document, niet rechthoekig'
+  },
+  'global express guaranteed rectangular' => {
+    'lastUpdated' => '1258556487',
+    'message' => 'Wereldwijde gegarandeerde expres niet-document, rechthoekig'
+  },
+  'priority mail flat rate box' => {
+    'lastUpdated' => '1258556511',
+    'message' => 'Prioriteit post vast tarief'
+  },
+  'priority mail international' => {
+    'lastUpdated' => '1258556527',
+    'message' => 'Prioriteitsmail Internationaal'
+  },
+  'priority mail large flat rate box' => {
+    'lastUpdated' => '1258556558',
+    'message' => 'Prioriteitspost groot vast tarief'
+  },
+  'priority mail small flat rate box' => {
+    'lastUpdated' => '1258556566',
+    'message' => 'Prioriteitspost klein vast tarief'
   }
 }
 ;
