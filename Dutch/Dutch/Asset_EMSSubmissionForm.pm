b7f4932bd18d00531edda27456bceea1b725152e
Translation Server <translation@plainblack.com>
2009-11-18 15:27:13 +0000

    Updating Dutch on translation server

--- Dutch/Dutch/Asset_EMSSubmissionForm.pm
+++ Dutch/Dutch/Asset_EMSSubmissionForm.pm
@@ -1,5 +1,83 @@
 package WebGUI::i18n::Dutch::Asset_EMSSubmissionForm;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'activity title approve submissions' => {
+    'lastUpdated' => '1258557908',
+    'message' => 'Verwerk goedgekeurde EMS bijdragen'
+  },
+  'activity title cleanup submissions' => {
+    'lastUpdated' => '1258557891',
+    'message' => 'EMS bijdragen opschonen'
+  },
+  'assetName' => {
+    'lastUpdated' => '1258556719',
+    'message' => 'EMS evenement invoerformulier'
+  },
+  'can submit group label' => {
+    'lastUpdated' => '1258557923',
+    'message' => 'Groep voor invoeren bijdragen'
+  },
+  'can submit group label help' => {
+    'lastUpdated' => '1258556770',
+    'message' => 'Selecteer een groep die via dit formulier objecten kan invoeren.'
+  },
+  'days before cleanup label' => {
+    'lastUpdated' => '1258556783',
+    'message' => 'Aantal dagen voor opschonen'
+  },
+  'days before cleanup label help' => {
+    'lastUpdated' => '1258557969',
+    'message' => 'Geef het aantal dagen op dat bijdragen in de rij staan, voordat ze verwijderd worden. Bij "0" worden bijdragen niet verwijderd. Zorg dat de EMSCleanup activiteit geactiveerd is in een workflow, die regelmatig draait.'
+  },
+  'delete created items label' => {
+    'lastUpdated' => '1258556879',
+    'message' => 'Aangemaakte items verwijderen?'
+  },
+  'delete created items label help' => {
+    'lastUpdated' => '1258557989',
+    'message' => 'Kies "ja" wanneer ingevoerde bijdragen verwijderd moeten worden nadat ze zijn omgezet naar een EMS kaartje.'
+  },
+  'edit form' => {
+    'lastUpdated' => '1258556663',
+    'message' => 'Bewerk formulier'
+  },
+  'form dscription label' => {
+    'lastUpdated' => '1258557205',
+    'message' => 'Beschrijving formulier'
+  },
+  'form dscription label help' => {
+    'lastUpdated' => '1258558003',
+    'message' => 'Deze JSON tekst beschrijft het formulier dat gemaakt wordt voor de gebruiker wanneer ze iets willen bijdragen aan dit EMS. Tenzij je echt weet wat je doet, is het geen goed idee dit te bewerken.'
+  },
+  'new form' => {
+    'lastUpdated' => '1258556654',
+    'message' => 'Nieuw formulier'
+  },
+  'past deadline label' => {
+    'lastUpdated' => '1258557102',
+    'message' => 'Deadline verlopen tekst'
+  },
+  'past deadline label help' => {
+    'lastUpdated' => '1258558019',
+    'message' => 'Voer een bericht in zodat gebruikers weten dat ze geen bijdragen kunnen toevoegen omdat de deadline voorbij is.'
+  },
+  'past deadline message' => {
+    'lastUpdated' => '1258557183',
+    'message' => 'De deadline voor invoeren is voorbij, er worden geen bijdragen meer geaccepteerd.'
+  },
+  'submission deadline label' => {
+    'lastUpdated' => '1258557846',
+    'message' => 'Deadline bijdragen'
+  },
+  'submission deadline label help' => {
+    'lastUpdated' => '1258557877',
+    'message' => 'Voer een datum in waarna er geen bijdragen meer geaccepteerd worden.'
+  },
+  'turn on one field' => {
+    'lastUpdated' => '1258556688',
+    'message' => 'Er moet minstens 1 veld aangezet worden.'
+  }
+}
+;
 
 1;
