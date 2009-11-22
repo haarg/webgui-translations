0d5dd05fbaec405cba16c758597791a2d880a87d
Translation Server <translation@plainblack.com>
2009-11-22 18:43:34 +0000

    Updating German on translation server

--- German/German/ShipDriver_USPSInternational.pm
+++ German/German/ShipDriver_USPSInternational.pm
@@ -1,5 +1,43 @@
 package WebGUI::i18n::German::ShipDriver_USPSInternational;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'U.S. Postal Service, International' => {
+    'lastUpdated' => '1258915151',
+    'message' => 'U.S. Postal Service, International'
+  },
+  'express mail international' => {
+    'lastUpdated' => '1258915183',
+    'message' => 'Expressversand International'
+  },
+  'first class mail international parcels' => {
+    'lastUpdated' => '1258915296',
+    'message' => 'First-Class-Paketversand International'
+  },
+  'global express guaranteed non-rectangular' => {
+    'lastUpdated' => '1258915352',
+    'message' => 'Weltweiter Expressversand, garantiert. Keine Dokument, nicht rechteckig'
+  },
+  'global express guaranteed rectangular' => {
+    'lastUpdated' => '1258915380',
+    'message' => 'Weltweiter Expressversand, garantiert. Kein Dokument, rechteckig.'
+  },
+  'priority mail flat rate box' => {
+    'lastUpdated' => '1258915226',
+    'message' => 'Priority Mail Flat Rate Box'
+  },
+  'priority mail international' => {
+    'lastUpdated' => '1258915233',
+    'message' => 'Priority Mail International '
+  },
+  'priority mail large flat rate box' => {
+    'lastUpdated' => '1258915248',
+    'message' => 'Priority Mail Large Flat Rate Box'
+  },
+  'priority mail small flat rate box' => {
+    'lastUpdated' => '1258915261',
+    'message' => 'Priority Mail Small Flat Rate Box'
+  }
+}
+;
 
 1;
