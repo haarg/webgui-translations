24f80328b407b51f2787ee0bf1c1bfa7f02085c1
Translation Server <translation@plainblack.com>
2009-11-22 18:38:41 +0000

    Updating German on translation server

--- German/German/Asset_EMSSubmission.pm
+++ German/German/Asset_EMSSubmission.pm
@@ -1,5 +1,39 @@
 package WebGUI::i18n::German::Asset_EMSSubmission;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'assetName' => {
+    'lastUpdated' => '1258914873',
+    'message' => 'Veranstaltungsplaner - Veranstaltungsregistrierung'
+  },
+  'comments' => {
+    'lastUpdated' => '1258914423',
+    'message' => 'Kommentare'
+  },
+  'edit asset' => {
+    'lastUpdated' => '1258914430',
+    'message' => 'Asset bearbeiten'
+  },
+  'send email label' => {
+    'lastUpdated' => '1258914905',
+    'message' => 'E-Mail versenden, wenn Veranstaltungsregistrierung bearbeitet wurde'
+  },
+  'send email label help' => {
+    'lastUpdated' => '1258914924',
+    'message' => 'Markieren Sie dieses Kästchen, wenn Sie über alle Änderungen Ihrer Veranstaltungsregistrierung per E-Mail informiert werden möchten'
+  },
+  'submission status' => {
+    'lastUpdated' => '1258914944',
+    'message' => 'Veranstaltungsregistrierungsstatus'
+  },
+  'submission status help' => {
+    'lastUpdated' => '1258915108',
+    'message' => 'Status Ihrer Registrierung: Wartend - wartet auf Registrierung; Feedback - wartet auf Eigentümer; Abgelehnt - abgelehnt; Angenommen - wartet auf Ticketerstellung; Erstellt - Ticket wurde erfolgreich erstellt; Fehler - Ticket konnte nicht erstellt werden.'
+  },
+  'your submission has been updated' => {
+    'lastUpdated' => '1258914971',
+    'message' => 'Ihre Veranstaltungsregistrierung wurde aktualisiert.'
+  }
+}
+;
 
 1;
