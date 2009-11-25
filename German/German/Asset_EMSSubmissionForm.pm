20958a04816bd90df59a8d026d4a65a70cfab605
Translation Server <translation@plainblack.com>
2009-11-25 10:55:30 +0000

    Updating German on translation server

--- German/German/Asset_EMSSubmissionForm.pm
+++ German/German/Asset_EMSSubmissionForm.pm
@@ -1,9 +1,25 @@
 package WebGUI::i18n::German::Asset_EMSSubmissionForm;
 use utf8;
 our $I18N = {
+  'activity title approve submissions' => {
+    'lastUpdated' => '1259145236',
+    'message' => 'Freigegebene EMS-Registrierung verarbeiten'
+  },
+  'activity title cleanup submissions' => {
+    'lastUpdated' => '1259145408',
+    'message' => 'EMS-Registrierungen aufräumen'
+  },
   'assetName' => {
     'lastUpdated' => '1258915524',
     'message' => 'Veranstaltungsplaner - Veranstaltungsregistrierungsformular'
+  },
+  'can submit group label' => {
+    'lastUpdated' => '1259145443',
+    'message' => 'Nutzergruppe, die Registrierungen erstellen darf'
+  },
+  'can submit group label help' => {
+    'lastUpdated' => '1259146520',
+    'message' => 'Wählen Sie eine Nutzergruppe, die mit diesem Formular speichern darf'
   },
   'edit form' => {
     'lastUpdated' => '1258915587',
