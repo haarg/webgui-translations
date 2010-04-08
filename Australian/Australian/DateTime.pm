9ee68c61304529adc1c7d892a26109f8f07bce45
Translation Server <translation@plainblack.com>
2010-04-08 20:33:44 +0000

    Updating Australian on translation server

--- Australian/Australian/DateTime.pm
+++ Australian/Australian/DateTime.pm
@@ -2,5 +2,259 @@ package WebGUI::i18n::Australian::DateTime;
 
 # ABSTRACT: Australian WebGUI (auto) Translation
 use strict;
-our $I18N = {};
+our $I18N = {
+    'Month' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'Month'
+    },
+    'Year' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'Year'
+    },
+    'april' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'April'
+    },
+    'april short' => {
+        'context'     => '3 letter abbreviation for the month',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Apr'
+    },
+    'august' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'August'
+    },
+    'august short' => {
+        'context'     => '3 letter abbreviation for the month',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Aug'
+    },
+    'december' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'December'
+    },
+    'december short' => {
+        'context'     => '3 letter abbreviation for the month',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Dec'
+    },
+    'february' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'February'
+    },
+    'february short' => {
+        'context'     => '3 letter abbreviation for the month',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Feb'
+    },
+    'friday' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'Friday'
+    },
+    'friday 1char' => {
+        'context'     => '1 letter abbreviation for the day',
+        'lastUpdated' => 1270759152,
+        'message'     => 'F'
+    },
+    'friday med' => {
+        'context'     => '3 letter abbreviation for the day',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Fri'
+    },
+    'friday short' => {
+        'context'     => '2 letter abbreviation for the day',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Fr'
+    },
+    'january' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'January'
+    },
+    'january short' => {
+        'context'     => '3 letter abbreviation for the month',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Jan'
+    },
+    'july' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'July'
+    },
+    'july short' => {
+        'context'     => '3 letter abbreviation for the month',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Jul'
+    },
+    'june' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'June'
+    },
+    'june short' => {
+        'context'     => '3 letter abbreviation for the month',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Jun'
+    },
+    'march' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'March'
+    },
+    'march short' => {
+        'context'     => '3 letter abbreviation for the month',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Mar'
+    },
+    'may' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'May'
+    },
+    'may short' => {
+        'context'     => '3 letter abbreviation for the month',
+        'lastUpdated' => 1270759152,
+        'message'     => 'May'
+    },
+    'monday' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'Monday'
+    },
+    'monday 1char' => {
+        'context'     => '1 letter abbreviation for the day',
+        'lastUpdated' => 1270759152,
+        'message'     => 'M'
+    },
+    'monday med' => {
+        'context'     => '3 letter abbreviation for the day',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Mon'
+    },
+    'monday short' => {
+        'context'     => '2 letter abbreviation for the day',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Mo'
+    },
+    'november' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'November'
+    },
+    'november short' => {
+        'context'     => '3 letter abbreviation for the month',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Nov'
+    },
+    'october' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'October'
+    },
+    'october short' => {
+        'context'     => '3 letter abbreviation for the month',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Oct'
+    },
+    'saturday' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'Saturday'
+    },
+    'saturday 1char' => {
+        'context'     => '1 letter abbreviation for the day',
+        'lastUpdated' => 1270759152,
+        'message'     => 'S'
+    },
+    'saturday med' => {
+        'context'     => '3 letter abbreviation for the day',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Sat'
+    },
+    'saturday short' => {
+        'context'     => '2 letter abbreviation for the day',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Sa'
+    },
+    'september' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'September'
+    },
+    'september short' => {
+        'context'     => '3 letter abbreviation for the month',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Sep'
+    },
+    'sunday' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'Sunday'
+    },
+    'sunday 1char' => {
+        'context'     => '1 letter abbreviation for the day',
+        'lastUpdated' => 1270759152,
+        'message'     => 'S'
+    },
+    'sunday med' => {
+        'context'     => '3 letter abbreviation for the day',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Sun'
+    },
+    'sunday short' => {
+        'context'     => '2 letter abbreviation for the day',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Su'
+    },
+    'thursday' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'Thursday'
+    },
+    'thursday 1char' => {
+        'context'     => '1 letter abbreviation for the day',
+        'lastUpdated' => 1270759152,
+        'message'     => 'T'
+    },
+    'thursday med' => {
+        'context'     => '3 letter abbreviation for the day',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Thu'
+    },
+    'thursday short' => {
+        'context'     => '2 letter abbreviation for the day',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Th'
+    },
+    'timezone' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'Time Zone'
+    },
+    'tuesday' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'Tuesday'
+    },
+    'tuesday 1char' => {
+        'context'     => '1 letter abbreviation for the day',
+        'lastUpdated' => 1270759152,
+        'message'     => 'T'
+    },
+    'tuesday med' => {
+        'context'     => '3 letter abbreviation for the day',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Tue'
+    },
+    'tuesday short' => {
+        'context'     => '2 letter abbreviation for the day',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Tu'
+    },
+    'wednesday' => {
+        'lastUpdated' => 1270759152,
+        'message'     => 'Wednesday'
+    },
+    'wednesday 1char' => {
+        'context'     => '1 letter abbreviation for the day',
+        'lastUpdated' => 1270759152,
+        'message'     => 'W'
+    },
+    'wednesday med' => {
+        'context'     => '3 letter abbreviation for the day',
+        'lastUpdated' => 1270759152,
+        'message'     => 'Wed'
+    },
+    'wednesday short' => {
+        'context'     => '2 letter abbreviation for the day',
+        'lastUpdated' => 1270759152,
+        'message'     => 'We'
+    }
+};
 1;
