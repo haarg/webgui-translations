085d0a65bf8e1d48ce467df2ebeff7d66b3f7ed8
Translation Server <translation@plainblack.com>
2010-04-21 18:24:40 +0000

    Manual checkin of translation

--- RedNeck/RedNeck/Account_Shop.pm
+++ RedNeck/RedNeck/Account_Shop.pm
@@ -1,5 +1,11 @@
 package WebGUI::i18n::RedNeck::Account_Shop;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'Products' => {
+    'lastUpdated' => '1271874103',
+    'message' => 'Stuff, fer yer back yard'
+  }
+}
+;
 
 1;
