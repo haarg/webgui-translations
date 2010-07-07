2620df1918f2abe3f71fca19f7a4e4a185a4fa3f
Translation Server <translation@plainblack.com>
2010-07-07 20:58:48 +0000

    Updating German on translation server

--- German/German/Auth_Twitter.pm
+++ German/German/Auth_Twitter.pm
@@ -1,5 +1,39 @@
 package WebGUI::i18n::German::Auth_Twitter;
 use utf8;
-our $I18N = {};
+our $I18N = {
+  'choose username template' => {
+    'lastUpdated' => '1278536304',
+    'message' => 'Wähle-Benutzername-Vorlage'
+  },
+  'choose username template help' => {
+    'lastUpdated' => '1278536276',
+    'message' => 'Das Template um einen Benutzernamen zu wählen, falls der Screenname des Benutzers schon vergeben ist.'
+  },
+  'choose username title' => {
+    'lastUpdated' => '1278536218',
+    'message' => 'Wählen Sie einen Benutzernamen.'
+  },
+  'enabled' => {
+    'lastUpdated' => '1278536031',
+    'message' => 'Aktiviert'
+  },
+  'enabled help' => {
+    'lastUpdated' => '1278536051',
+    'message' => 'Twitter-basiertes Login aktiviert'
+  },
+  'get key' => {
+    'lastUpdated' => '1278536145',
+    'message' => 'Hole einen Twitter-API-Schlüssel von "%s"'
+  },
+  'twitter screen name taken' => {
+    'lastUpdated' => '1278536199',
+    'message' => 'Der Twitter Screenname "%s" ist vergeben, bitte wählen Sie einen anderen Benutzernamen.'
+  },
+  'webgui username taken' => {
+    'lastUpdated' => '1278536113',
+    'message' => 'Der Benutzername "%s" ist vergeben, bitte wählen Sie einen anderen.'
+  }
+}
+;
 
 1;
